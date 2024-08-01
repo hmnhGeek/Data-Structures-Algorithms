680d19df50bd99da8ec9804b644122cf1bd86f7f
hmnhGeek <hmnhsharma97@gmail.com>
2024-08-01 11:53:19 +0530

    Word Ladder II problem solved using BFS technique.

--- Graphs/word_ladder_2.py
+++ Graphs/word_ladder_2.py
@@ -1,3 +1,6 @@
+# Problem link - https://www.geeksforgeeks.org/problems/word-ladder-ii/1
+# Solution - https://www.youtube.com/watch?v=DREutrv2XD0&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=30
+
 class Node:
     def __init__(self, data):
         self.data = data
