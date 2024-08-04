f4476d3e58ddd92e4cae9b50060c16b229f93bb7
hmnhGeek <hmnhsharma97@gmail.com>
2024-08-04 14:31:36 +0530

    Added space optimized solution for frog jump DP problem

--- Dynamic Programming/frog_jump.py
+++ Dynamic Programming/frog_jump.py
@@ -170,4 +170,77 @@ def tabulation():
     )
 
 
+def space_optimized():
+    def total_min_energy(energies):
+        # Time complexity is O(n) and space complexity is O(1) with only prev1 and prev2
+        # being used for any problem.
+
+        # initialize prev1 and prev2 such that prev1 denotes the energy taken from 0th to 0th
+        # index and prev2 denotes the energy for -1st to 0th index which is infinity.
+        prev1 = 0
+        prev2 = float('inf')
+
+        # reverse the direction of memoization and start from index = 1.
+        for index in range(1, len(energies)):
+            # solve for previous lower stair and add |E_i - E_{i - 1}|
+            one_step = prev1 + abs(energies[index] - energies[index - 1])
+
+            # assume that taking two steps will make you go out of bounds, so it would require
+            # infinite energy
+            two_steps = float('inf')
+
+            # now, if taking two steps is possible, go two steps previous and also add |E_i - E_{i - 2}|
+            # energy while going down.
+            if index > 1:
+                two_steps = prev2 + abs(energies[index] - energies[index - 2])
+
+            # update the minimum energy that is required to jump from index = `index` to index = 0.
+            curr = min(one_step, two_steps)
+
+            # update the previous pointers
+            prev2 = prev1
+            prev1 = curr
+
+        # return the energy stored in prev1 which denotes the minimum energy required to
+        # jump from n - 1 index to 0th index.
+        return prev1
+
+    print(
+        total_min_energy(
+            [10, 20, 30, 10]
+        )
+    )
+
+    print(
+        total_min_energy(
+            [10, 50, 10]
+        )
+    )
+
+    print(
+        total_min_energy(
+            [7, 4, 4, 2, 6, 6, 3, 4]
+        )
+    )
+
+    print(
+        total_min_energy(
+            [4, 8, 3, 10, 4, 4]
+        )
+    )
+
+
+print("Recursive Solution")
+recursive()
+
+print()
+print("Memoized Solution")
+memoized()
+
+print()
+print("Tabulation Solution")
 tabulation()
+
+print()
+print("Space Optimized Solution")
+space_optimized()
