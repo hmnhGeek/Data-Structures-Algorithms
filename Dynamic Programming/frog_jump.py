b6d98068248a2faba1455197e389afb575c6537b
hmnhGeek <hmnhsharma97@gmail.com>
2024-08-04 14:24:20 +0530

    Added tabulation solution for frog jump DP problem

--- Dynamic Programming/frog_jump.py
+++ Dynamic Programming/frog_jump.py
@@ -55,7 +55,8 @@ def recursive():
 
 def memoized():
     def solve(energies, index, dp):
-        # The time complexity is O(2^n) with O(n) space complexity (max recursion stack depth)
+        # The time complexity is O(n) with O(n + n) space complexity,
+        # with stack depth and dp array of length n.
 
         # if the frog reached 1st stair (0th index), there is no more to go,
         # hence no energy differential is there, thus return 0 energy.
@@ -112,4 +113,61 @@ def memoized():
     )
 
 
-memoized()
+def tabulation():
+    def total_min_energy(energies):
+        # Time complexity is O(n) and space complexity is O(n) with only dp array consuming space
+        # and no recursion stack being used.
+
+        # initialize dp array of size n with each stair having energy infinite.
+        dp = {i: float('inf') for i in range(len(energies))}
+
+        # initialize the base case, basically the energy required for the frog to jump
+        # from 0th stair to 0th stair.
+        dp[0] = 0
+
+        # reverse the direction of memoization and start from index = 1.
+        for index in range(1, len(energies)):
+            # solve for previous lower stair and add |E_i - E_{i - 1}|
+            one_step = dp[index - 1] + abs(energies[index] - energies[index - 1])
+
+            # assume that taking two steps will make you go out of bounds, so it would require
+            # infinite energy
+            two_steps = float('inf')
+
+            # now, if taking two steps is possible, go two steps previous and also add |E_i - E_{i - 2}|
+            # energy while going down.
+            if index > 1:
+                two_steps = dp[index - 2] + abs(energies[index] - energies[index - 2])
+
+            # update the minimum energy that is required to jump from index = `index` to index = 0.
+            dp[index] = min(one_step, two_steps)
+
+        # minimum energy would be stored at the last index in the dp dictionary.
+        return dp[len(energies) - 1]
+
+    print(
+        total_min_energy(
+            [10, 20, 30, 10]
+        )
+    )
+
+    print(
+        total_min_energy(
+            [10, 50, 10]
+        )
+    )
+
+    print(
+        total_min_energy(
+            [7, 4, 4, 2, 6, 6, 3, 4]
+        )
+    )
+
+    print(
+        total_min_energy(
+            [4, 8, 3, 10, 4, 4]
+        )
+    )
+
+
+tabulation()
