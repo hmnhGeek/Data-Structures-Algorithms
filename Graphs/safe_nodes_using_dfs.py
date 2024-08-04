415f97544c410e4dee3546f3158de7445098ebb3
hmnhGeek <hmnhsharma97@gmail.com>
2024-08-04 21:13:42 +0530

    Detection of safe nodes using DFS without topological sort.

--- Graphs/safe_nodes_using_dfs.py
+++ Graphs/safe_nodes_using_dfs.py
@@ -36,85 +36,176 @@ class Stack:
         return item
 
 
-def dfs(graph, node, visited, path_visited, stack):
-    # mark the node as visited and path visited.
-    visited[node] = True
-    path_visited[node] = True
+def safe_nodes_by_dfs_topo_sort():
+    def dfs(graph, node, visited, path_visited, stack):
+        # mark the node as visited and path visited.
+        visited[node] = True
+        path_visited[node] = True
 
-    # for adjacent node in graph's node
-    for adj_node in graph[node]:
-        # if the node is not visited, run a dfs on it.
-        if not visited[adj_node]:
-            # at any point, if a cycle is detected, return True and no need for adding this node into the stack.
-            if dfs(graph, adj_node, visited, path_visited, stack):
+        # for adjacent node in graph's node
+        for adj_node in graph[node]:
+            # if the node is not visited, run a dfs on it.
+            if not visited[adj_node]:
+                # at any point, if a cycle is detected, return True and no need for adding this node into the stack.
+                if dfs(graph, adj_node, visited, path_visited, stack):
+                    return True
+
+            # if the node is visited and path visited also, there's a cycle, return True, no need to add on stack.
+            if path_visited[adj_node]:
                 return True
 
-        # if the node is visited and path visited also, there's a cycle, return True, no need to add on stack.
-        if path_visited[adj_node]:
-            return True
+        # if there are no nodes, or the adjacent nodes are not getting into a cycle, push the node to the stack.
+        stack.push(node)
 
-    # if there are no nodes, or the adjacent nodes are not getting into a cycle, push the node to the stack.
-    stack.push(node)
+        # unmark the pushed node, because it's a safe node and we don't want it to give false negative (that there's a
+        # cycle) where there is another path leading to the same safe node. Return false, no cycle is there.
+        path_visited[node] = False
+        return False
 
-    # unmark the pushed node, because it's a safe node and we don't want it to give false negative (that there's a
-    # cycle) where there is another path leading to the same safe node. Return false, no cycle is there.
-    path_visited[node] = False
-    return False
+    def get_safe_nodes(graph):
+        # This function uses topological sort with time complexity O(V + E) and space complexity is O(V).
 
+        # initialize a visited and path visited dictionary to track traversal and detect loops (if any).
+        visited = {i: False for i in graph}
+        path_visited = {i: False for i in graph}
 
-def get_safe_nodes(graph):
-    # This function uses topological sort with time complexity O(V + E) and space complexity is O(V).
+        # stack will be used to store safe nodes.
+        stack = Stack()
 
-    # initialize a visited and path visited dictionary to track traversal and detect loops (if any).
-    visited = {i: False for i in graph}
-    path_visited = {i: False for i in graph}
+        # to run on all the connected components.
+        for node in graph:
+            if not visited[node]:
+                dfs(graph, node, visited, path_visited, stack)
 
-    # stack will be used to store safe nodes.
-    stack = Stack()
-
-    # to run on all the connected components.
-    for node in graph:
-        if not visited[node]:
-            dfs(graph, node, visited, path_visited, stack)
-
-    while not stack.is_empty():
-        print(stack.pop(), end=" ")
+        while not stack.is_empty():
+            print(stack.pop(), end=" ")
 
 
-get_safe_nodes(
-    {
-        1: [2],
-        2: [3],
-        3: [4, 7],
-        4: [5],
-        5: [6],
-        6: [],
-        7: [5],
-        8: [2, 9],
-        9: [10],
-        10: [8]
-    }
-)
+    get_safe_nodes(
+        {
+            1: [2],
+            2: [3],
+            3: [4, 7],
+            4: [5],
+            5: [6],
+            6: [],
+            7: [5],
+            8: [2, 9],
+            9: [10],
+            10: [8]
+        }
+    )
 
+    print()
+    get_safe_nodes(
+        {
+            0: [1, 2],
+            1: [2, 3],
+            2: [5],
+            3: [0],
+            4: [5],
+            5: [],
+            6: []
+        }
+    )
+
+    print()
+    get_safe_nodes(
+        {
+            0: [1],
+            1: [2],
+            2: [0, 3],
+            3: []
+        }
+    )
+
+
+def safe_nodes_without_topo_sort():
+    def dfs(graph, node, visited, path_visited, check):
+        # mark the node as visited and path visited.
+        visited[node] = True
+        path_visited[node] = True
+
+        # ensure that for now, the node is unsafe
+        check[node] = False
+
+        for adj_node in graph[node]:
+            # if the node is not visited, run a dfs on it.
+            if not visited[adj_node]:
+                if dfs(graph, adj_node, visited, path_visited, check):
+                    # at any point, if a cycle is detected, return True.
+                    # this is indeed an unsafe node, ensure check is false for this adjacent node.
+                    check[adj_node] = False
+                    return True
+
+            # if the node is visited and path visited also, there's a cycle, return True and uncheck as safe node.
+            if path_visited[adj_node]:
+                check[adj_node] = False
+                return True
+
+        # unmark the node from path visited, because it's a safe node and we don't want it to give false negative
+        # (that there's a cycle) where there is another path leading to the same safe node. Return false, no cycle
+        # is there.
+        check[node] = True
+        path_visited[node] = False
+        return False
+
+    def get_safe_nodes(graph):
+        # This function uses topological sort with time complexity O(V + E) and space complexity is O(V).
+
+        # initialize a visited and path visited dictionary to track traversal and detect loops (if any).
+        visited = {i: False for i in graph}
+        path_visited = {i: False for i in graph}
+
+        # check node will be used to track safe nodes. All the true values will denote safe nodes.
+        check = {i: False for i in graph}
+
+        # to run on all the connected components.
+        for node in graph:
+            if not visited[node]:
+                dfs(graph, node, visited, path_visited, check)
+
+        print([k for k, v in check.items() if v])
+
+    get_safe_nodes(
+        {
+            1: [2],
+            2: [3],
+            3: [4, 7],
+            4: [5],
+            5: [6],
+            6: [],
+            7: [5],
+            8: [2, 9],
+            9: [10],
+            10: [8]
+        }
+    )
+
+    print()
+    get_safe_nodes(
+        {
+            0: [1, 2],
+            1: [2, 3],
+            2: [5],
+            3: [0],
+            4: [5],
+            5: [],
+            6: []
+        }
+    )
+
+    print()
+    get_safe_nodes(
+        {
+            0: [1],
+            1: [2],
+            2: [0, 3],
+            3: []
+        }
+    )
+
+
+safe_nodes_by_dfs_topo_sort()
 print()
-get_safe_nodes(
-    {
-        0: [1, 2],
-        1: [2, 3],
-        2: [5],
-        3: [0],
-        4: [5],
-        5: [],
-        6: []
-    }
-)
-
-print()
-get_safe_nodes(
-    {
-        0: [1],
-        1: [2],
-        2: [0, 3],
-        3: []
-    }
-)
+safe_nodes_without_topo_sort()
