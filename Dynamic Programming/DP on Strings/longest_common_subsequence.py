086a32074217094459856f7eb8ba3ce6337e6a9a
hmnhGeek <hmnhsharma97@gmail.com>
2024-08-15 14:34:44 +0530

    Longest common subsequence using tabulation.

--- Dynamic Programming/DP on Strings/longest_common_subsequence.py
+++ Dynamic Programming/DP on Strings/longest_common_subsequence.py
@@ -167,6 +167,95 @@ def memoized():
     )
 
 
+def tabulation():
+    def longest_common_subsequence(string1, string2):
+        # Time complexity would be O(n + m) and space complexity would be O(n*m) for the dp array.
+
+        # if either of the string is empty, return 0 as there will be no common subsequence at all
+        if len(string1) == 0 or len(string2) == 0:
+            return 0
+
+        n, m = len(string1), len(string2)
+        dp = {i: {j: 0 for j in range(m)} for i in range(n)}
+
+        # special base case for checking the first character of the strings
+        dp[0][0] = 1 if string1[0] == string2[0] else 0
+
+        # fill the first column of the dp matrix for index1 starting from 1 to n - 1.
+        for index1 in range(1, n):
+            # if there is a match, put 1, else put max(dp[index1 - 1][0], -inf) = dp[index1 - 1][0] as index2 cannot
+            # go down from 0.
+            dp[index1][0] = 1 if string1[index1] == string2[0] else dp[index1 - 1][0]
+
+        # fill the first row of the dp matrix for index2 starting from 1 to m - 1.
+        for index2 in range(1, m):
+            # if there is a match, put 1, else put max(-inf, dp[0][index2 - 1]) = dp[0][index2 - 1] as index1 cannot
+            # go down from 0.
+            dp[0][index2] = 1 if string1[0] == string2[index2] else dp[0][index2 - 1]
+
+        for index1 in range(1, n):
+            for index2 in range(1, m):
+                # if the characters from string 1 and string 2 match at their respective indices, this means we can
+                # add 1 to the LCS solve for lower indices on both the strings.
+                if string1[index1] == string2[index2]:
+                    dp[index1][index2] = 1 + dp[index1 - 1][index2 - 1]
+                else:
+                    # if they do not match, we will add a 0 to LCS and add maximum value obtained once by reducing
+                    # index 1 and keeping index 2 intact and vice versa.
+                    dp[index1][index2] = 0 + max(dp[index1 - 1][index2], dp[index1][index2 - 1])
+
+        return dp[n - 1][m - 1]
+
+    print(
+        longest_common_subsequence(
+            "adebc",
+            "dcadb"
+        )
+    )
+
+    print(
+        longest_common_subsequence(
+            "ab",
+            "defg"
+        )
+    )
+
+    print(
+        longest_common_subsequence(
+            "abcde",
+            "ace"
+        )
+    )
+
+    print(
+        longest_common_subsequence(
+            "abc",
+            "abc"
+        )
+    )
+
+    print(
+        longest_common_subsequence(
+            "abc",
+            "acd"
+        )
+    )
+
+    print(
+        longest_common_subsequence(
+            "aggtab",
+            "gxtxayb"
+        )
+    )
+
+    print(
+        longest_common_subsequence(
+            "abc",
+            "cba"
+        )
+    )
+
+
 recursive()
 print()
-memoized()
+tabulation()
