7f481bb73bc2c9eff686b634a6fcdf574b166caa
hmnhGeek <hmnhsharma97@gmail.com>
2024-08-15 17:48:21 +0530

    Longest common substring using tabulation and space optimization, no memoization available

--- Dynamic Programming/DP on Strings/longest_common_substring.py
+++ Dynamic Programming/DP on Strings/longest_common_substring.py
@@ -108,4 +108,210 @@ def recursive():
     )
 
 
+def tabulation_from_longest_common_subsequence():
+    def get_longest_common_substring(string1, string2):
+        # Time complexity would be O(n*m) and space complexity would be O(n*m) for the dp array.
+
+        # if either of the string is empty, return 0 as there will be no common subsequence at all
+        if len(string1) == 0 or len(string2) == 0:
+            return 0
+
+        n, m = len(string1), len(string2)
+        dp = {i: {j: 0 for j in range(m)} for i in range(n)}
+
+        # special base case for checking the first character of the strings
+        dp[0][0] = 1 if string1[0] == string2[0] else 0
+
+        # fill the first column of the dp matrix for index1 starting from 1 to n - 1.
+        for index1 in range(1, n):
+            # if there is a match, put 1, else put 0
+            dp[index1][0] = 1 if string1[index1] == string2[0] else 0
+
+        # fill the first row of the dp matrix for index2 starting from 1 to m - 1.
+        for index2 in range(1, m):
+            # if there is a match, put 1, else put 0
+            dp[0][index2] = 1 if string1[0] == string2[index2] else 0
+
+        # variable to store the maximum length obtained for the substring.
+        max_length_obtained = 0
+
+        for index1 in range(1, n):
+            for index2 in range(1, m):
+                # if the characters from string 1 and string 2 match at their respective indices, this means we can
+                # add 1 to the LCS and solve for lower indices on both the strings. Also, update the variable
+                # max_length_obtained.
+                if string1[index1] == string2[index2]:
+                    dp[index1][index2] = 1 + dp[index1 - 1][index2 - 1]
+                    max_length_obtained = max(max_length_obtained, dp[index1][index2])
+                else:
+                    # if they do not match, simply put a 0 in the matrix to denote a discontinuity.
+                    dp[index1][index2] = 0
+
+        return max_length_obtained
+
+    print(
+        get_longest_common_substring(
+            "abcjklp",
+            "acjkp"
+        )
+    )
+
+    print(
+        get_longest_common_substring(
+            "ab",
+            "defg"
+        )
+    )
+
+    print(
+        get_longest_common_substring(
+            "adebc",
+            "dcadb"
+        )
+    )
+
+    print(
+        get_longest_common_substring(
+            "tyfg",
+            "cvbnuty"
+        )
+    )
+
+    print(
+        get_longest_common_substring(
+            "wasdijkl",
+            "wsdjkl"
+        )
+    )
+
+    print(
+        get_longest_common_substring(
+            "geeksforgeeks",
+            "geeksquiz"
+        )
+    )
+
+    print(
+        get_longest_common_substring(
+            "abcdxyz",
+            "xyzabcd"
+        )
+    )
+
+    print(
+        get_longest_common_substring(
+            "zxabcdezy",
+            "yzabcdezx"
+        )
+    )
+
+
+def space_optimized():
+    def get_longest_common_substring(string1, string2):
+        # Time complexity would be O(n*m) and space complexity would be O(m) for the dp array.
+
+        # if either of the string is empty, return 0 as there will be no common subsequence at all
+        if len(string1) == 0 or len(string2) == 0:
+            return 0
+
+        n, m = len(string1), len(string2)
+        prev = {j: 0 for j in range(m)}
+
+        # special base case for checking the first character of the strings
+        prev[0] = 1 if string1[0] == string2[0] else 0
+
+        # fill the first row of the dp matrix for index2 starting from 1 to m - 1.
+        for index2 in range(1, m):
+            # if there is a match, put 1, else put 0
+            prev[index2] = 1 if string1[0] == string2[index2] else 0
+
+        # variable to store the maximum length obtained for the substring.
+        max_length_obtained = 0
+
+        for index1 in range(1, n):
+            curr = {j: 0 for j in range(m)}
+
+            # note this... compare with subsequence problem code.
+            curr[0] = 1 if string1[index1] == string2[0] else 0
+
+            for index2 in range(1, m):
+                # if the characters from string 1 and string 2 match at their respective indices, this means we can
+                # add 1 to the LCS and solve for lower indices on both the strings. Also, update the variable
+                # max_length_obtained.
+                if string1[index1] == string2[index2]:
+                    curr[index2] = 1 + prev[index2 - 1]
+                    max_length_obtained = max(max_length_obtained, curr[index2])
+                else:
+                    # if they do not match, simply put a 0 in the matrix to denote a discontinuity.
+                    curr[index2] = 0
+            prev = curr
+
+        return max_length_obtained
+
+    print(
+        get_longest_common_substring(
+            "abcjklp",
+            "acjkp"
+        )
+    )
+
+    print(
+        get_longest_common_substring(
+            "ab",
+            "defg"
+        )
+    )
+
+    print(
+        get_longest_common_substring(
+            "adebc",
+            "dcadb"
+        )
+    )
+
+    print(
+        get_longest_common_substring(
+            "tyfg",
+            "cvbnuty"
+        )
+    )
+
+    print(
+        get_longest_common_substring(
+            "wasdijkl",
+            "wsdjkl"
+        )
+    )
+
+    print(
+        get_longest_common_substring(
+            "geeksforgeeks",
+            "geeksquiz"
+        )
+    )
+
+    print(
+        get_longest_common_substring(
+            "abcdxyz",
+            "xyzabcd"
+        )
+    )
+
+    print(
+        get_longest_common_substring(
+            "zxabcdezy",
+            "yzabcdezx"
+        )
+    )
+
+
+print("Recursion Solution")
 recursive()
+
+print()
+print("Tabulation Solution")
+tabulation_from_longest_common_subsequence()
+
+print()
+print("Space Optimized Solution")
+space_optimized()
