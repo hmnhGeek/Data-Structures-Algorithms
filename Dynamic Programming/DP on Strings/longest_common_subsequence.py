9b87b3bd7c7758a689859bd3ebb79801f46bb4d2
hmnhGeek <hmnhsharma97@gmail.com>
2024-08-15 14:48:03 +0530

    Longest common subsequence using tabulation with space optimization.

--- Dynamic Programming/DP on Strings/longest_common_subsequence.py
+++ Dynamic Programming/DP on Strings/longest_common_subsequence.py
@@ -3,7 +3,7 @@
 
 def recursive():
     def solve_for_longest_common_subsequence(string1, index1, string2, index2):
-        # Time complexity would be O(2^{n + m}) and space complexity would be O(n + m).
+        # Time complexity would be O(2^{nm}) and space complexity would be O(nm).
 
         # if at any point, any or both indices becomes negative, return zero, as a negative index would mean that one
         # of the string has been completely exhausted.
@@ -83,7 +83,7 @@ def recursive():
 
 def memoized():
     def solve_for_longest_common_subsequence(string1, index1, string2, index2, dp):
-        # Time complexity would be O(n + m) and space complexity would be O(n + m + n*m) for the dp array.
+        # Time complexity would be O(n*m) and space complexity would be O(n*m + n*m) for the dp array.
 
         # if at any point, any or both indices becomes negative, return zero, as a negative index would mean that one
         # of the string has been completely exhausted.
@@ -169,7 +169,7 @@ def memoized():
 
 def tabulation():
     def longest_common_subsequence(string1, string2):
-        # Time complexity would be O(n + m) and space complexity would be O(n*m) for the dp array.
+        # Time complexity would be O(n*m) and space complexity would be O(n*m) for the dp array.
 
         # if either of the string is empty, return 0 as there will be no common subsequence at all
         if len(string1) == 0 or len(string2) == 0:
@@ -256,6 +256,101 @@ def tabulation():
     )
 
 
+def space_optimized():
+    def longest_common_subsequence(string1, string2):
+        # Time complexity would be O(n*m) and space complexity would be O(m) for the prev row.
+
+        # if either of the string is empty, return 0 as there will be no common subsequence at all
+        if len(string1) == 0 or len(string2) == 0:
+            return 0
+
+        n, m = len(string1), len(string2)
+
+        # prev denotes the case index1 = 0
+        prev = {j: 0 for j in range(m)}
+
+        # special base case for checking the first character of the strings
+        prev[0] = 1 if string1[0] == string2[0] else 0
+
+        # fill the prev row for index2 starting from 1 to m - 1.
+        for index2 in range(1, m):
+            # if there is a match, put 1, else put max(-inf, prev[index2 - 1]) = prev[index2 - 1] as index1 cannot
+            # go down from 0.
+            prev[index2] = 1 if string1[0] == string2[index2] else prev[index2 - 1]
+
+        for index1 in range(1, n):
+            curr = {j: 0 for j in range(m)}
+
+            # Add the base case for curr row. curr is the row for index1 from 1 to n - 1. # if there is a match,
+            # put 1, else put max(dp[index1 - 1][0], -inf) = dp[index1 - 1][0] = prev[0] as index1 cannot go down
+            # from 0. This way, you can start the loop for index2 from 1 to m - 1 and avoiding checks for index1 > 0
+            # and index2 > 0 in the loop.
+            curr[0] = 1 if string1[index1] == string2[0] else prev[0]
+
+            for index2 in range(1, m):
+                # if the characters from string 1 and string 2 match at their respective indices, this means we can
+                # add 1 to the LCS solve for lower indices on both the strings.
+                if string1[index1] == string2[index2]:
+                    curr[index2] = 1 + prev[index2 - 1]
+                else:
+                    # if they do not match, we will add a 0 to LCS and add maximum value obtained once by reducing
+                    # index 1 and keeping index 2 intact and vice versa.
+                    curr[index2] = 0 + max(prev[index2], curr[index2 - 1])
+            prev = curr
+
+        # result is always stored in the prev's last index.
+        return prev[m - 1]
+
+    print(
+        longest_common_subsequence(
+            "adebc",
+            "dcadb"
+        )
+    )
+
+    print(
+        longest_common_subsequence(
+            "ab",
+            "defg"
+        )
+    )
+
+    print(
+        longest_common_subsequence(
+            "abcde",
+            "ace"
+        )
+    )
+
+    print(
+        longest_common_subsequence(
+            "abc",
+            "abc"
+        )
+    )
+
+    print(
+        longest_common_subsequence(
+            "abc",
+            "acd"
+        )
+    )
+
+    print(
+        longest_common_subsequence(
+            "aggtab",
+            "gxtxayb"
+        )
+    )
+
+    print(
+        longest_common_subsequence(
+            "abc",
+            "cba"
+        )
+    )
+
+
 recursive()
 print()
-tabulation()
+space_optimized()
