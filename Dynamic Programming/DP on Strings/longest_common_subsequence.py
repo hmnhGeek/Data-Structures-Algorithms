999c4a16cb667a18b4b4ec9eab6df85241afa868
hmnhGeek <hmnhsharma97@gmail.com>
2024-08-15 14:06:18 +0530

    Longest common subsequence using recursion.

---
+++ Dynamic Programming/DP on Strings/longest_common_subsequence.py
@@ -0,0 +1,84 @@
+# Problem link - https://www.naukri.com/code360/problems/longest-common-subsequence_624879?source=youtube&campaign=striver_dp_videos
+# Solution - https://www.youtube.com/watch?v=NPZn9jBrX8U&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=26
+
+def recursive():
+    def solve_for_longest_common_subsequence(string1, index1, string2, index2):
+        # Time complexity would be O(2^{n + m}) and space complexity would be O(n + m).
+
+        # if at any point, any or both indices becomes negative, return zero, as a negative index would mean that one
+        # of the string has been completely exhausted.
+        if index1 < 0 or index2 < 0:
+            return 0
+
+        # if the characters from string 1 and string 2 match at their respective indices, this means we can add 1 to
+        # the LCS solve for lower indices on both the strings.
+        if string1[index1] == string2[index2]:
+            return 1 + solve_for_longest_common_subsequence(string1, index1 - 1, string2, index2 - 1)
+
+        # if they do not match, we will add a 0 to LCS and add maximum value obtained once by reducing index 1 and
+        # keeping index 2 intact and vice versa.
+        return 0 + max(
+            solve_for_longest_common_subsequence(string1, index1 - 1, string2, index2),
+            solve_for_longest_common_subsequence(string1, index1, string2, index2 - 1)
+        )
+
+    def longest_common_subsequence(string1, string2):
+        # if either of the string is empty, return 0 as there will be no common subsequence at all
+        if len(string1) == 0 or len(string2) == 0:
+            return 0
+
+        # recursively solve for LCS
+        n, m = len(string1), len(string2)
+        return solve_for_longest_common_subsequence(string1, n - 1, string2, m - 1)
+
+    print(
+        longest_common_subsequence(
+            "adebc",
+            "dcadb"
+        )
+    )
+
+    print(
+        longest_common_subsequence(
+            "ab",
+            "defg"
+        )
+    )
+
+    print(
+        longest_common_subsequence(
+            "abcde",
+            "ace"
+        )
+    )
+
+    print(
+        longest_common_subsequence(
+            "abc",
+            "abc"
+        )
+    )
+
+    print(
+        longest_common_subsequence(
+            "abc",
+            "acd"
+        )
+    )
+
+    print(
+        longest_common_subsequence(
+            "aggtab",
+            "gxtxayb"
+        )
+    )
+
+    print(
+        longest_common_subsequence(
+            "abc",
+            "cba"
+        )
+    )
+
+
+recursive()
