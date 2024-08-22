26b96e4afc310c66a6fa8066732732ba1300655b
hmnhGeek <hmnhsharma97@gmail.com>
2024-08-22 19:51:05 +0530

    solved number of islands 2 problem

--- Graphs/number_of_islands_2.py
+++ Graphs/number_of_islands_2.py
@@ -26,3 +26,72 @@ class DisjointSet:
 
     def in_same_component(self, node1, node2):
         return self.find_ultimate_parent(node1) == self.find_ultimate_parent(node2)
+
+
+class NumberOfIslands:
+    def __init__(self, ocean_map):
+        self.graph = ocean_map
+        self.num_islands = 0
+        self.n = len(ocean_map)
+        self.m = len(ocean_map[0])
+        self.visited = [[False for _ in range(self.m)] for _ in range(self.n)]
+        self.disjoint_set = DisjointSet([i for i in range(self.n * self.m)])
+
+    def get_neighbours(self, i, j):
+        neighbours = []
+
+        if 0 <= i - 1 < self.n:
+            neighbours.append((i - 1, j))
+        if 0 <= j + 1 < self.m:
+            neighbours.append((i, j + 1))
+        if 0 <= i + 1 < self.n:
+            neighbours.append((i + 1, j))
+        if 0 <= j - 1 < self.m:
+            neighbours.append((i, j - 1))
+
+        return neighbours
+
+    def mark_land(self, i, j):
+        self.graph[i][j] = 1
+        self.visited[i][j] = True
+        neighbouring_cells = self.get_neighbours(i, j)
+        node = i*self.m + j
+        self.num_islands += 1
+
+        for neighbour in neighbouring_cells:
+            x, y = neighbour
+            adj_node = x*self.m + y
+            if self.graph[x][y] == 1:
+                if not self.disjoint_set.in_same_component(node, adj_node):
+                    self.disjoint_set.union_by_rank(node, adj_node)
+                    self.num_islands -= 1
+
+    def live_track(self, cell):
+        track = []
+        for i, j in cell:
+            if not self.visited[i][j]:
+                self.mark_land(i, j)
+            track.append(self.num_islands)
+        return track
+
+
+print(
+    NumberOfIslands(
+        [[0 for _ in range(5)] for _ in range(4)]
+    ).live_track(
+        [
+            (0, 0),
+            (0, 0),
+            (1, 1),
+            (1, 0),
+            (0, 1),
+            (0, 3),
+            (1, 3),
+            (0, 4),
+            (3, 2),
+            (2, 2),
+            (1, 2),
+            (0, 2)
+        ]
+    )
+)
