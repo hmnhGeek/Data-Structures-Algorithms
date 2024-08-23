b3b59b8361458c8e57c570aafcb7d639114a1b4e
hmnhGeek <hmnhsharma97@gmail.com>
2024-08-23 16:40:50 +0530

    added tabulation solution

--- Dynamic Programming/DP on Strings/distinct_subsequences.py
+++ Dynamic Programming/DP on Strings/distinct_subsequences.py
@@ -80,3 +80,47 @@ def memoized():
     print(get_distinct_subsequences("dingdingdingding", "ing"))
     print(get_distinct_subsequences("aaaaa", "a"))
 
+
+def tabulation():
+    def get_distinct_subsequences(string, lookup):
+        # Time complexity is O(n*m) and space is also O(n*m) with no recursion.
+
+        n = len(string)
+        m = len(lookup)
+        dp = {i: {j: 0 for j in range(m)} for i in range(n)}
+
+        # fill out first col, i.e., lookup[0] column. No need to fill row 0, i.e. string[0] row because for columns
+        # j = 1 to m - 1, lookup[j] would basically mean that you're at index 0 of string but still lookup string has
+        # at least one character left to compare, so all will be 0 in that case. However, the lookup[0] case will not
+        # be so. In tabulation, we build from bottom to up.
+        # set dp[0][0] meaning string[0] and lookup[0] to 1 if both of them are equal, meaning that we have 1
+        # subsequence found. Now, we must add 1 to (dp[i - 1][j - 1] + dp[i - 1][j]), but j = 0. Hence, dp[i - 1][j - 1]
+        # will always return 0. Hence, we have to add a 1 or a 0 only to dp[i - 1][j].
+        # we will use recursion code above to write the base case.
+        dp[0][0] = 1 if string[0] == lookup[0] else 0
+        for i in range(1, n):
+            dp[i][0] = dp[i - 1][0] + (1 if string[i] == lookup[0] else 0)
+
+        for i in range(1, n):
+            for j in range(1, m):
+                # again, if the characters in both the strings match, then you've two options at this juncture:
+                # 1. reduce indices of both and consider getting a match.
+                # 2. reduce only `i` and stay at `j`, basically not considering `i` even though it's a match with `j`.
+                if string[i] == lookup[j]:
+                    left = dp[i - 1][j - 1]
+                    right = dp[i - 1][j]
+                    # return the sum of distinct subsequences so obtained.
+                    dp[i][j] = left + right
+                # if there is not a match, you have no option but to stay at same `j` but move lower on `i`.
+                else:
+                    dp[i][j] = dp[i - 1][j]
+
+        return dp[n - 1][m - 1]
+
+    print(get_distinct_subsequences("brootgroot", "brt"))
+    print(get_distinct_subsequences("dingdingdingding", "ing"))
+    print(get_distinct_subsequences("aaaaa", "a"))
+
+memoized()
+print()
+tabulation()
