4314b307d05314bb8039cdf51932751852bd0398
hmnhGeek <hmnhsharma97@gmail.com>
2024-08-24 14:38:51 +0530

    Solved median in a row wise sorted matrix

--- Matrix/median_in_a_row_wise_sorted_mtx.py
+++ Matrix/median_in_a_row_wise_sorted_mtx.py
@@ -6,17 +6,17 @@ class MinHeap:
         return len(self.heap) == 0
 
     def get_lci(self, pi):
-        lci = 2*pi + 1
+        lci = 2 * pi + 1
         return lci if lci in range(len(self.heap)) else None
 
     def get_rci(self, pi):
-        rci = 2*pi + 2
+        rci = 2 * pi + 2
         return rci if rci in range(len(self.heap)) else None
 
     def get_pi(self, ci):
         if ci == 0:
             return
-        pi = int((ci - 1)/2)
+        pi = int((ci - 1) / 2)
         return pi if pi in range(len(self.heap)) else None
 
     def get_min_child_index(self, lci, rci):
@@ -68,3 +68,69 @@ class MinHeap:
         return item
 
 
+class MatrixMedianFinder:
+    def __init__(self, mtx):
+        self.mtx = mtx
+        self.min_heap = MinHeap()
+        self.num_elements = len(mtx) * len(mtx[0])
+        self.n = len(mtx)
+        self.m = len(mtx[0])
+
+    def find_median(self):
+        required_term = 1 + (self.num_elements - 1) // 2
+
+        for i in range(self.n):
+            self.min_heap.insert((self.mtx[i][0], i, 0))
+
+        counter = 0
+        while not self.min_heap.is_empty():
+            term, row, col = self.min_heap.pop()
+            counter += 1
+            if counter == required_term:
+                return term
+            if col + 1 < self.m:
+                self.min_heap.insert((self.mtx[row][col + 1], row, col + 1))
+        return None
+
+
+print(
+    MatrixMedianFinder(
+        [
+            [1, 2, 3, 4, 5],
+            [8, 9, 11, 12, 13],
+            [21, 23, 25, 27, 29]
+        ]
+    ).find_median()
+)
+
+print(
+    MatrixMedianFinder(
+        [
+            [1, 5, 7, 9, 11],
+            [2, 3, 4, 8, 9],
+            [4, 11, 14, 19, 20],
+            [6, 10, 22, 99, 100],
+            [7, 15, 17, 24, 28]
+        ]
+    ).find_median()
+)
+
+print(
+    MatrixMedianFinder(
+        [[1, 3, 5],
+         [2, 6, 9],
+         [3, 6, 9]]
+    ).find_median()
+)
+
+print(MatrixMedianFinder([[1], [2], [3]]).find_median())
+
+print(
+    MatrixMedianFinder(
+        [
+            [1, 3, 8],
+            [2, 3, 4],
+            [1, 2, 5]
+        ]
+    ).find_median()
+)
