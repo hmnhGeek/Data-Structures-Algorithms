38044e5813aac9c4c5901cc685db5fdd0b48c2ea
hmnhGeek <hmnhsharma97@gmail.com>
2024-08-26 11:05:22 +0530

    Find min characters to be added in front to make palindrome

--- Strings/min_chars_in_front_to_make_palindrome.py
+++ Strings/min_chars_in_front_to_make_palindrome.py
@@ -1,16 +1,24 @@
 def find_min_chars_to_make_palindrome(string):
+    # Time complexity is O(N) and space complexity is O(1)
+
     i, j = 0, len(string) - 1
     temp = j
     result = 0
 
     while i < j:
+        # if the characters at front and at end match, reduce the window size
         if string[i] == string[j]:
             i += 1
             j -= 1
         else:
+            # if they don't match, then increase the count of the characters that need to be added.
             result += 1
+
+            # start searching again from front and move j back to the position of temp - 1.
             i = 0
             j = temp - 1
+
+            # update temp value to j
             temp = j
     return result
 
