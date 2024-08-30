45601f1c7381ab5452607707c3a87355f09d5b31
hmnhGeek <hmnhsharma97@gmail.com>
2024-08-30 14:37:51 +0530

    best time to buy and sell stocks only once

--- Dynamic Programming/DP on Stocks/best_time_to_buy_and_sell.py
+++ Dynamic Programming/DP on Stocks/best_time_to_buy_and_sell.py
@@ -1,13 +1,25 @@
 def get_right_maximums(prices):
+    # Overall time and space complexity is O(n) and O(n) respectively.
+
+    # store a max value for reference, initialized to minimum price value = 0.
     max_val = 0
+
+    # store a result array with all days having 0 as the max selling price.
     result = [0 for i in range(len(prices))]
 
+    # loop on the prices array from right to left.
     for i in range(-1, -len(prices) - 1, -1):
+        # if the selling price on ith day is less than the max selling price, then set result[i]
+        # to max selling price because that will yield more profit.
         if prices[i] < max_val:
             result[i] = max_val
+        # else if the price at ith day is more than the max selling price, we must update the max
+        # selling price and nothing needs to be done on result array as there is no better price
+        # to sell at after this index (or day).
         else:
             max_val = prices[i]
 
+    # return the right maximums
     return result
 
 
