7b88d3c835e39398e400c8f969df82d194932a36
hmnhGeek <hmnhsharma97@gmail.com>
2024-08-30 14:34:40 +0530

    best time to buy and sell stocks only once

--- Dynamic Programming/DP on Stocks/best_time_to_buy_and_sell.py
+++ Dynamic Programming/DP on Stocks/best_time_to_buy_and_sell.py
@@ -1,6 +1,6 @@
 def get_right_maximums(prices):
-    max_val = float('-inf')
-    result = [float('-inf') for i in range(len(prices))]
+    max_val = 0
+    result = [0 for i in range(len(prices))]
 
     for i in range(-1, -len(prices) - 1, -1):
         if prices[i] < max_val:
@@ -12,17 +12,33 @@ def get_right_maximums(prices):
 
 
 def best_time(prices):
+    """
+        Overall time complexity is O(n) and space complexity is O(n).
+    """
+
+    # for a given index we need to check for the maximum price that we can sell at post this index, and thus,
+    # we need to find the right maximums for each index in the prices array. This will take O(n) time and
+    # O(n) space.
     right_max = get_right_maximums(prices)
-    max_profit = float('-inf')
+
+    # initially, assume the maximum profit to be 0
+    max_profit = 0
+
+    # now loop on indices of right_max (or prices array); basically index denotes the day.
+    # This will take O(n) time.
     for i in range(len(right_max)):
-        if right_max[i] != float('-inf'):
-            max_profit = max(max_profit, right_max[i] - prices[i])
-    return max_profit if max_profit != float('-inf') else 0
+        # update the maximum profit by taking maximum of existing max profit and the profit
+        # that will be obtained on ith day.
+        max_profit = max(max_profit, right_max[i] - prices[i])
+
+    # return max profit that can be made.
+    return max_profit
 
 
 print(best_time([7, 1, 5, 3, 6, 4]))
-print(best_time([ 2, 100, 150, 120]))
+print(best_time([2, 100, 150, 120]))
 print(best_time([1, 2, 3, 4]))
 print(best_time([2, 2, 2, 2]))
 print(best_time([17, 20, 11, 9, 12, 6]))
 print(best_time([98, 101, 66, 72]))
+print(best_time([7, 6, 4, 3, 1]))
