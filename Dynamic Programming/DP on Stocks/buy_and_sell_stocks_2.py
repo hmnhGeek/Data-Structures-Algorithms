446eb4c5bbf850aa7f67c398388f7269f616e592
hmnhGeek <hmnhsharma97@gmail.com>
2024-08-30 17:45:26 +0530

    Tabulation code added

--- Dynamic Programming/DP on Stocks/buy_and_sell_stocks_2.py
+++ Dynamic Programming/DP on Stocks/buy_and_sell_stocks_2.py
@@ -90,6 +90,41 @@ def memoized():
     print(buy_and_sell([1, 2, 3, 4, 5]))
 
 
+def tabulation():
+    def buy_and_sell(stock_prices_day_wise):
+        # Overall time complexity is O(n) and space is O(n)
+
+        num_days = len(stock_prices_day_wise)
+        dp = {i: {True: 0, False: 0} for i in range(num_days + 1)}
+
+        for i in range(num_days - 1, -1, -1):
+            for can_buy in dp[i]:
+                # if there is a possibility to buy, then we have 2 cases: 1. you buy and move on next index with
+                # can_buy set to False as you cannot buy further until you sell the current one. 2. You don't do
+                # anything, simply move to next day with same possibility of can_buy set as True. Finally,
+                # return the max profit out of these 2 cases.
+                if can_buy:
+                    dp[i][can_buy] = max(-stock_prices_day_wise[i] + dp[i + 1][False], 0 + dp[i + 1][True])
+
+                # if there is a possibility to not buy, then we have 2 cases:
+                # 1. you sell and move on next index with can_buy set to True as you can now buy on upcoming days.
+                # 2. You don't do anything, simply move to next day with same possibility of can_buy set as False.
+                # Finally, return the max profit out of these 2 cases.
+                else:
+                    dp[i][can_buy] = max(stock_prices_day_wise[i] + dp[i + 1][True], 0 + dp[i + 1][False])
+
+        # on the 0th day, you have the option to buy, and that's why passing it as True. This variable
+        # is not `should_buy` but `can_buy`; basically, it just denotes a possibility. Thus, no need to
+        # take max(can_buy = False, can_buy = True).
+        return dp[0][True]
+
+    print(buy_and_sell([1, 2, 3, 4, 5, 6, 7]))
+    print(buy_and_sell([7, 6, 5, 4, 3, 2, 1]))
+    print(buy_and_sell([1, 2, 3, 4, 5]))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
