f2dccd099597703f5814db8c071e60514460fff5
hmnhGeek <hmnhsharma97@gmail.com>
2024-09-01 13:09:17 +0530

    remove stones using disjoint set

--- Graphs/most_stones_removed.py
+++ Graphs/most_stones_removed.py
@@ -45,11 +45,18 @@ def get_stones(mtx, n, m):
 
 
 def remove_stones(mtx):
+    # If number of stones is N, then the time complexity is O(N^2) and space complexity is O(N^2 + N) for matrix and DS.
+
     n = len(mtx)
     m = len(mtx[0])
+
+    # get all the 1-cells from the matrix; their coordinates and the node numbers
     stones = get_stones(mtx, n, m)
+
+    # A stone is of type (i, j, node_number); initialize a disjoint set with node numbers
     disjoint_set = DisjointSet([i[2] for i in stones])
 
+    # connect all the nodes based on same rows into a single component
     for stone in stones:
         x, y, node = stone
         for adj_stone in stones:
@@ -57,6 +64,7 @@ def remove_stones(mtx):
             if x == x0:
                 disjoint_set.union_by_rank(node, adj_node)
 
+    # connect all the nodes based on same columns into a single component
     for stone in stones:
         x, y, node = stone
         for adj_stone in stones:
@@ -64,6 +72,11 @@ def remove_stones(mtx):
             if y == y0:
                 disjoint_set.union_by_rank(node, adj_node)
 
+    # as per the video solution, the number of stones that can be removed from a component is
+    # equal to the number of nodes in that component - 1. If there are x components and each component
+    # has x1, x2, x3, x4, ... number of nodes, then (x1 + x2 + x3 + x4 + ... = N), i.e., total number
+    # of nodes in the matrix. Now, the number of stones that can be removed from each component is
+    # (x1 - 1 + x2 - 1 + x3 - 1 + x4 - 1 + ... = N - 1*number_of_components). That's what is returned at last.
     return len(stones) - disjoint_set.get_num_components()
 
 
