17336b264ca176fc4190e78efba12ce9ceb86cff
hmnhGeek <hmnhsharma97@gmail.com>
2024-09-01 11:22:13 +0530

    Make a large island DSU

--- Graphs/making_large_island_dsu.py
+++ Graphs/making_large_island_dsu.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/maximum-connected-group/1
+# Solution - https://www.youtube.com/watch?v=lgiz0Oup6gM&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=52
+
+
 class DisjointSet:
     def __init__(self, nodes):
         self.sizes = {i: 1 for i in nodes}
@@ -43,23 +47,45 @@ def get_neighbours(i, j, n):
 
 
 def make_large_island(mtx):
+    """
+        Overall time complexity is O(n^2) and overall space complexity is O(n^2).
+    """
+
     n = len(mtx)
+
+    # create a disjoint set storing n^2 nodes starting from 0. This will take up O(n^2) space.
     disjoint_set = DisjointSet([i for i in range(n**2)])
+
+    # store max_size that can be obtained by switching one 0 to 1 as -inf.
     max_size = float('-inf')
 
+    # Using O(n^2) time, connect all the connected 1s.
     for i in range(n):
         for j in range(n):
+            # if the current cell is a 1, then only there is a point of grouping.
             if mtx[i][j] == 1:
+                # to get any node value in disjoint set by the cell coordinates, the formula is node = i*num_cols + j.
                 node = i*n + j
+                # loop on the 4 neighbours of the current node, and if the neighbour is a 1, then only do a union by
+                # size on the disjoint set.
                 neighbours = get_neighbours(i, j, n)
                 for adj_cell in neighbours:
                     x, y = adj_cell
                     adj_node = x*n + y
                     if mtx[x][y] == 1:
                         disjoint_set.union_by_size(node, adj_node)
+
+    # create a blank set which will be used to store the ultimate parents of each connected component. We are not
+    # directly summing up the size of connected components in the below loop (and instead using a set), because it is
+    # possible that two or more neighbours of the 0-cell can be part of the same component, and we do not want to
+    # add the size of same component multiple times in that case. This will again take O(n^2) time. Also, the set will
+    # hold at max 4 distinct components, and so we can say that the set occupies O(1) space. Also, disjoint set
+    # operations done above and below take O(1) time at average.
     temp_set = set()
     for i in range(n):
         for j in range(n):
+            # if the current cell is 0, then add all the ultimate parents of connected components who have the adjacent
+            # 1s of this 0-cell as their nodes.
             if mtx[i][j] == 0:
                 neighbours = get_neighbours(i, j, n)
                 for adj_cell in neighbours:
@@ -67,12 +93,20 @@ def make_large_island(mtx):
                     adj_node = x*n + y
                     if mtx[x][y] == 1:
                         temp_set.add(disjoint_set.find_ultimate_parent(adj_node))
+
+                # once we have all the unique neighbouring components of this 0-cell, add up the sizes of these
+                # components, then add 1 for the size of this 0-cell (because we are assuming that we've converted it
+                # to a 1). Finally, update the max_size.
                 local_size = 0
                 for ulp in temp_set:
                     local_size += disjoint_set.sizes[ulp]
                 local_size += 1
                 max_size = max(max_size, local_size)
+
+                # ensure that you clear the set for next 0-cell.
                 temp_set.clear()
+
+    # return the maximum size that was obtained.
     return max_size
 
 
