12c32bcbb28a1127d4224ab3507985d4af5c1149
hmnhGeek <hmnhsharma97@gmail.com>
2024-09-01 14:32:56 +0530

    Added more examples and corrected the reverse graph code.

--- Graphs/strongly_connected_components_kosaraju_algorithm.py
+++ Graphs/strongly_connected_components_kosaraju_algorithm.py
@@ -47,6 +47,7 @@ class Graph:
         stack.push(node)
 
     def _reverse(self):
+        # Reversing the graph will take O(V + E) time and O(V + E) space for storing this graph separately.
         reversed_graph = {}
         for node in self.graph:
             for adj_node in self.graph[node]:
@@ -55,6 +56,7 @@ class Graph:
                 else:
                     reversed_graph[adj_node].append(node)
 
+        # it could be possible that certain nodes are missing in the reversed graph, add them from the original graph.
         for node in self.graph:
             if node not in reversed_graph:
                 reversed_graph[node] = []
@@ -62,6 +64,7 @@ class Graph:
         return reversed_graph
 
     def _get_topological_order(self, source_node):
+        # This is the standard DFS topological sort algorithm which will take O(V + E) time and O(V) space.
         stack = Stack()
         if source_node not in self.graph:
             return stack
@@ -73,23 +76,39 @@ class Graph:
         return stack
 
     def get_strongly_connected_components(self, source):
+        """
+            Overall time complexity is O(V + E) and overall space complexity is O(V + E).
+        """
+
+        # get topological sorted stack in O(V + E) time and O(V) space.
         ordered_stack = self._get_topological_order(source)
+
+        # get the reversed version of the graph in O(V + E) time and space each.
         reversed_graph = self._reverse()
+
+        # let's start the dfs traversal on the nodes stored in stack
         visited = {i: False for i in self.graph}
         strongly_connected_components = []
 
+        # run this loop till the stack is not empty. This will run for O(V + E) time with O(V) space
         while not ordered_stack.is_empty():
             node = ordered_stack.pop()
 
+            # if the popped node is not visited
             if not visited[node]:
+                # do a dfs traversal starting on this node and populate a local stack (on the reversed graph)
                 component_stack = Stack()
                 self._dfs(reversed_graph, node, visited, component_stack)
                 component = []
 
+                # pop from this local stack until you have all the nodes handy within this component.
                 while not component_stack.is_empty():
                     component.append(component_stack.pop())
+
+                # once the component is populated, add it to strongly connected components list.
                 strongly_connected_components.append(component)
 
+        # finally, return the strongly connected components.
         return strongly_connected_components
 
 
