caf9814702a05420f689b6c044b5b2734fe3e98a
hmnhGeek <hmnhsharma97@gmail.com>
2024-09-06 13:36:07 +0530

    solved the problem using tabulation

--- Dynamic Programming/DP on Subsequences and Subsets/longest_increasing_subsequence.py
+++ Dynamic Programming/DP on Subsequences and Subsets/longest_increasing_subsequence.py
@@ -156,8 +156,71 @@ def memoized():
     print(get_length_of_longest_increasing_subsequence([0, 1, 0, 3, 2, 3]))
 
 
+def tabulation():
+    def get_length_of_longest_increasing_subsequence(arr):
+        # Time complexity is O(n^2) and space complexity is O(n^2).
+
+        n = len(arr)
+
+        # create a DP array
+        dp = {i: {j: 0 for j in range(n)} for i in range(n)}
+        # introduce a None key for each index.
+        for i in dp:
+            dp[i][None] = 0
+
+        for j in dp[0]:
+            dp[0][j] = 1 if arr[0] < get_value(arr, j) else 0
+
+        for index in range(1, n):
+            # For every index, specifically do a `None` prev_index code run
+
+            # now assume that the left recursion is not possible.
+            left = float('-inf')
+
+            # check if the value at current index is less than the max value. If yes, then
+            # this index value can be taken into consideration.
+            if arr[index] < get_value(arr, None):
+                # take this index value and increase the length. Also, update the max value
+                # with the current index value.
+                left = 1 + dp[index - 1][index]
+
+            # if we don't pick the current index in the sequence, then go to next index, with
+            # same length and same max value.
+            right = dp[index - 1][None]
+            dp[index][None] = max(left, right)
+
+            # since prev_index = None is done, now populate for j = 0 to n - 1.
+            for prev_index in range(n):
+                # now assume that the left recursion is not possible.
+                left = float('-inf')
+
+                # check if the value at current index is less than the max value. If yes, then
+                # this index value can be taken into consideration.
+                if arr[index] < get_value(arr, prev_index):
+                    # take this index value and increase the length. Also, update the max value
+                    # with the current index value.
+                    left = 1 + dp[index - 1][index]
+
+                # if we don't pick the current index in the sequence, then go to next index, with
+                # same length and same max value.
+                right = dp[index - 1][prev_index]
+                dp[index][prev_index] = max(left, right)
+
+        # assuming that you've got current max value as infinite start with last index
+        # with the longest increasing subsequence length of 0.
+        return dp[n - 1][None]
+
+    print(get_length_of_longest_increasing_subsequence([10, 9, 2, 5, 3, 7, 101, 18]))
+    print(get_length_of_longest_increasing_subsequence([5, 4, 11, 1, 16, 8]))
+    print(get_length_of_longest_increasing_subsequence([1, 2, 2]))
+    print(get_length_of_longest_increasing_subsequence([7, 7, 7, 7, 7, 7, 7]))
+    print(get_length_of_longest_increasing_subsequence([0, 1, 0, 3, 2, 3]))
+
+
 recursive()
 print()
 recursive1()
 print()
 memoized()
+print()
+tabulation()
