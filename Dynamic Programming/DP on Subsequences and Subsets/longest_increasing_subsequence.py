a7268fe362f4ac9fa4cefe6934bee69e4158e568
hmnhGeek <hmnhsharma97@gmail.com>
2024-09-06 12:21:39 +0530

    added comments and time and space analysis

--- Dynamic Programming/DP on Subsequences and Subsets/longest_increasing_subsequence.py
+++ Dynamic Programming/DP on Subsequences and Subsets/longest_increasing_subsequence.py
@@ -1,25 +1,47 @@
-def solve(arr, index, length, max_val):
-    if index == 0:
-        if arr[0] < max_val:
-            return length + 1
-        return length
+def recursive():
+    def solve(arr, index, length, max_val):
+        # Time complexity is O(2^n) and space complexity is O(n).
 
-    left = float('-inf')
-    if arr[index] < max_val:
-        left = solve(arr, index - 1, length + 1, arr[index])
+        # if the index 0 is reached
+        if index == 0:
+            # check if the 0th element is less than the max value stored till now, if yes,
+            # then return length + 1 because 0th element will also be included. Note that
+            # we are doing `<` and not `<=` because we will be taking only strictly increasing
+            # subsequence.
+            if arr[0] < max_val:
+                return length + 1
 
-    right = solve(arr, index - 1, length, max_val)
-    return max(left, right)
+            # otherwise simply return length
+            return length
+
+        # now assume that the left recursion is not possible.
+        left = float('-inf')
+
+        # check if the value at current index is less than the max value. If yes, then
+        # this index value can be taken into consideration.
+        if arr[index] < max_val:
+            # take this index value and increase the length. Also, update the max value
+            # with the current index value.
+            left = solve(arr, index - 1, length + 1, arr[index])
+
+        # if we don't pick the current index in the sequence, then go to next index, with
+        # same length and same max value.
+        right = solve(arr, index - 1, length, max_val)
+        return max(left, right)
+
+    def get_length_of_longest_increasing_subsequence(arr):
+        # Time complexity is O(2^n) and space complexity is O(n).
+
+        n = len(arr)
+        # assuming that you've got current max value as infinite start with last index
+        # with the longest increasing subsequence length of 0.
+        return solve(arr, n - 1, 0, float('inf'))
+
+    print(get_length_of_longest_increasing_subsequence([10, 9, 2, 5, 3, 7, 101, 18]))
+    print(get_length_of_longest_increasing_subsequence([5, 4, 11, 1, 16, 8]))
+    print(get_length_of_longest_increasing_subsequence([1, 2, 2]))
+    print(get_length_of_longest_increasing_subsequence([7, 7, 7, 7, 7, 7, 7]))
+    print(get_length_of_longest_increasing_subsequence([0, 1, 0, 3, 2, 3]))
 
 
-def get_length_of_longest_increasing_subsequence(arr):
-    n = len(arr)
-    return max(
-        solve(arr, n - 1, 0, float('inf')),
-        solve(arr, n - 1, 0, float('inf'))
-    )
-
-
-print(get_length_of_longest_increasing_subsequence([10, 9, 2, 5, 3, 7, 101, 18]))
-print(get_length_of_longest_increasing_subsequence([5, 4, 11, 1, 16, 8]))
-print(get_length_of_longest_increasing_subsequence([1, 2, 2]))
+recursive()
