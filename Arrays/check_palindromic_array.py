6e98f34114caf27cb6caffc8f3e5235a45a02d74
hmnhGeek <hmnhsharma97@gmail.com>
2024-09-08 14:39:45 +0530

    Check if an array of integers contain all palindromes or not

---
+++ Arrays/check_palindromic_array.py
@@ -0,0 +1,13 @@
+def is_palindrome(string):
+    return string == string[-1:-len(string)-1:-1]
+
+
+def is_palindromic_array(arr):
+    for i in arr:
+        if not is_palindrome(str(i)):
+            return False
+    return True
+
+
+print(is_palindromic_array([111, 222, 333, 444, 555]))
+print(is_palindromic_array([21, 131, 20]))
