30faec1e95253a33492e7c3003b72d81ffb28039
hmnhGeek <hmnhsharma97@gmail.com>
2024-09-08 15:26:16 +0530

    Row with max 1s in a matrix

--- Matrix/rows_with_max_1s.py
+++ Matrix/rows_with_max_1s.py
@@ -1,10 +1,18 @@
+# Problem link - https://www.geeksforgeeks.org/problems/row-with-max-1s0023/1
+
+
 def count_1s(row, low, high):
+    # Time complexity is O(log(len(row)))
+    # Typical binary search algorithm to find that index (given by `low` value at end), which denotes
+    # the index of first 1 in the row.
     while low <= high:
         mid = int(low + (high - low) / 2)
         if row[mid] == 1:
             high = mid - 1
         else:
             low = mid + 1
+
+    # return the count of 1s in this row.
     return len(row) - low
 
 
@@ -12,6 +20,8 @@ def get_row_with_max_1s(mtx):
     result = None
     mx_count = 0
     n, m = len(mtx), len(mtx[0])
+
+    # This will take O(n*log(m)) time
     for row in range(n):
         one_count = count_1s(mtx[row], 0, m - 1)
         if mx_count < one_count:
