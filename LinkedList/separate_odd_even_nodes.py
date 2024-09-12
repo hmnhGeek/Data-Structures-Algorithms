93c8fb0a5634bfa5958331da5f59a869f0b68235
hmnhGeek <hmnhsharma97@gmail.com>
2024-09-12 18:03:20 +0530

    Segregate even odd numbers in a linked list

---
+++ LinkedList/separate_odd_even_nodes.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = self.tail = None
+
+    def push(self, x):
+        node = Node(x)
+
+        if self.head is None:
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+
+    def build(self, _list):
+        for i in _list:
+            self.push(i)
+
+    def show(self):
+        curr = self.head
+        while curr is not None:
+            print(curr.data, end=" ")
+            curr = curr.next
+        print()
+
+
+class LinkedListSplitter:
+    def __init__(self, linked_list: LinkedList):
+        pass
