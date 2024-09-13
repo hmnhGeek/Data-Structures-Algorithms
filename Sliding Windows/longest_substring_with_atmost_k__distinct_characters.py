6ba373caaa550e2a36ab29bb87ff236170acc71b
hmnhGeek <hmnhsharma97@gmail.com>
2024-09-13 13:35:27 +0530

    longest substring with at most k distinct characters

---
+++ Sliding Windows/longest_substring_with_atmost_k__distinct_characters.py
@@ -0,0 +1,29 @@
+def get_longest_substring_with_k_distinct_characters(string, k):
+    n = len(string)
+    left = right = 0
+    max_length = 1
+    last_indices = dict()
+    characters_used = set()
+
+    for character in string:
+        last_indices[character] = None
+
+    while right < n:
+        character = string[right]
+        last_indices[character] = right
+        characters_used.add(character)
+
+        if len(characters_used) > k:
+            character_to_remove = string[left]
+            left = last_indices[character_to_remove] + 1
+            characters_used.remove(character_to_remove)
+
+        max_length = max(max_length, right - left + 1)
+        right += 1
+
+    return max_length
+
+
+print(get_longest_substring_with_k_distinct_characters("aaabbccd", 2))
+print(get_longest_substring_with_k_distinct_characters("abbbbbbc", 2))
+print(get_longest_substring_with_k_distinct_characters("abcddefg", 3))
