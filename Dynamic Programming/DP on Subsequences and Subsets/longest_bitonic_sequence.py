fa0fa90474b1908ffa553b039ff8d98a63779214
hmnhGeek <hmnhsharma97@gmail.com>
2024-09-14 18:07:19 +0530

    added more usage examples

--- Dynamic Programming/DP on Subsequences and Subsets/longest_bitonic_sequence.py
+++ Dynamic Programming/DP on Subsequences and Subsets/longest_bitonic_sequence.py
@@ -34,3 +34,6 @@ def get_longest_bitonic_sequence(arr):
 
 print(get_longest_bitonic_sequence([1, 2, 1, 2, 1]))
 print(get_longest_bitonic_sequence([1, 2, 1, 3, 4]))
+print(get_longest_bitonic_sequence([1, 2, 5, 3, 2]))
+print(get_longest_bitonic_sequence([1, 11, 2, 10, 4, 5, 2, 1]))
+print(get_longest_bitonic_sequence([10, 20, 30]))
