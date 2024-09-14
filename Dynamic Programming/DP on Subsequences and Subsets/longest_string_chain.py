c6c8a68b694d7449f42720a3b84a2d50273990cd
hmnhGeek <hmnhsharma97@gmail.com>
2024-09-14 17:05:00 +0530

    Longest string chain - tabulation solution

--- Dynamic Programming/DP on Subsequences and Subsets/longest_string_chain.py
+++ Dynamic Programming/DP on Subsequences and Subsets/longest_string_chain.py
@@ -114,8 +114,49 @@ def memoized():
     print(get_longest_string_chain(["a", "bc", "ad", "adc", "bcd"]))
 
 
+def tabulation():
+    def get_longest_string_chain(strings):
+        # Overall time complexity is O(n^2) and space complexity is O(n^2).
+
+        n = len(strings)
+        # we need to sort in order to maintain the correct usage of `have_unit_diff()` function because in this function
+        # string 1 is always assumed to be shorter than string 2. This will take O(n*log(n)) time.
+        strings.sort()
+
+        dp = {i: {j: 0 for j in strings} for i in range(n)}
+        for i in dp:
+            dp[i][None] = 0
+
+        for prev in dp[0]:
+            dp[0][prev] = 1 if have_unit_diff(strings[0], prev) else 0
+
+        for index in range(1, n):
+            for prev in dp[index]:
+                left = float('-inf')
+                # if the index element and prev have a unit difference only, then only perform left recursion
+                if have_unit_diff(strings[index], prev):
+                    # perform left recursion by making prev = strings[index]
+                    left = 1 + dp[index - 1][strings[index]]
+
+                # perform only the right recursion with same prev
+                right = dp[index - 1][prev]
+
+                # return max out of left and right to get the longest chain.
+                dp[index][prev] = max(left, right)
+
+        return dp[n - 1][None]
+
+    print(get_longest_string_chain(["a", "b", "ba", "bca", "bda", "bdca"]))
+    print(get_longest_string_chain(["x", "xx", "y", "xyx"]))
+    print(get_longest_string_chain(["m", "nm", "mmm"]))
+    print(get_longest_string_chain(["a", "bc", "ad", "adc", "bcd"]))
+
+
 print("Recursive Solution")
 recursive()
 
 print("Memoized Solution")
 memoized()
+
+print("Tabulation Solution")
+tabulation()
