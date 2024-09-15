1f516ba593f2231d4918fddc76bc9a86cb287a37
hmnhGeek <hmnhsharma97@gmail.com>
2024-09-15 11:50:32 +0530

    detect duplicate subtrees

--- Binary Tree/duplicate_subtrees.py
+++ Binary Tree/duplicate_subtrees.py
@@ -13,15 +13,29 @@ def solve_duplicate_subtrees(root, mp, result):
 
     if subtree_string in mp:
         if mp[subtree_string] == 1:
-            result.append(root)
+            result.append(root.data)
         mp[subtree_string] += 1
     else:
         mp[subtree_string] = 1
 
     return subtree_string
 
+
 def find_duplicate_subtrees(root):
     mp = {}
     result = []
     solve_duplicate_subtrees(root, mp, result)
-    return result
+    for i in mp:
+        if mp[i] > 1:
+            print(i)
+
+
+# Example 1
+one, two, three, four, two1, four2, four3 = Node(1), Node(2), Node(3), Node(4), Node(2), Node(4), Node(4)
+one.left = two
+two.left = four
+three.left = two1
+two1.left = four3
+one.right = three
+three.right = four2
+find_duplicate_subtrees(one)
