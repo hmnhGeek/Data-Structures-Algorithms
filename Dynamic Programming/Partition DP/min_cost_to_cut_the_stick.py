386cc07105fbbae62bcaa9dfc94de1ce0f2a4c4d
hmnhGeek <hmnhsharma97@gmail.com>
2024-09-20 17:56:57 +0530

    Minimum cost to cut a stick

--- Dynamic Programming/Partition DP/min_cost_to_cut_the_stick.py
+++ Dynamic Programming/Partition DP/min_cost_to_cut_the_stick.py
@@ -3,20 +3,29 @@
 
 
 def recursive():
+    # The time complexity will be exponential with O(n) stack space.
     def solve(cuts, i, j):
+        # if you've exhausted the rod, return 0.
         if i > j:
             return 0
 
         mini = float('inf')
+        # we start partition from i and go till j (inclusive) because j + 1 represents the end point of rod
+        # which we have added manually in the parent function.
         for index in range(i, j + 1):
+            # the rod length in which the partition index `index` lies is of length
+            # cuts[j + 1] - cuts[i - 1], thus add it to cost and solve for left recursion and right recursion.
             cost = cuts[j + 1] - cuts[i - 1] + solve(cuts, i, index - 1) + solve(cuts, index + 1, j)
+            # update the minimum cost
             mini = min(mini, cost)
+        # finally return `mini`.
         return mini
 
     def min_cost(stick_size, cuts):
         n = len(cuts)
         cuts.append(stick_size)
         cuts = [0] + cuts
+        # this will take O(nlog(n))
         cuts.sort()
         return solve(cuts, 1, n)
 
