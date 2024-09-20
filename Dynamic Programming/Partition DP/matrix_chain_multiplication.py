df7d7898ef2c3fb771f0e2c0341a8f32baa5b9d0
hmnhGeek <hmnhsharma97@gmail.com>
2024-09-20 13:21:44 +0530

    added code explanation for recursive solution

--- Dynamic Programming/Partition DP/matrix_chain_multiplication.py
+++ Dynamic Programming/Partition DP/matrix_chain_multiplication.py
@@ -60,25 +60,43 @@ def recursive():
 
 
 def memoized():
+    """
+        Overall time complexity is O(N^3) and space complexity is O(N + N^2).
+    """
+
     def solve_problem(sizes_array, i, j, dp):
+        # if we have a single matrix, then there is no multiplication needed, return 0 operations.
         if i == j:
             return 0
+
+        # we are about to start the partition algorithm, store minimum number of operations as infinite.
+        min_operations = float('inf')
 
         if dp[i][j] is not None:
             return dp[i][j]
 
-        mini = float('inf')
+        # assume `k` to be the partition index. Partitions would be like (A), (AB), (ABC).
+        # So k would assume values from 1 to j - 1.
         for k in range(i, j):
+            # the number of operations involved for the current partition would be
+            # { A[i - 1] * A[k] * A[j] } + f(i, k) + f(k + 1, j). The number of operations in matrix multiplication is
+            # usually x * y * z where first matrix is of size x * y and the second matrix is of size y * z. Please watch
+            # the solution video for more understanding.
             steps = sizes_array[i - 1] * sizes_array[k] * sizes_array[j] + \
                 solve_problem(sizes_array, i, k, dp) + solve_problem(sizes_array, k + 1, j, dp)
-            mini = min(mini, steps)
 
-        dp[i][j] = mini
+            # update the minimum operations required by performing this partition.
+            min_operations = min(min_operations, steps)
+
+        # return the overall minimum number of operations required.
+        dp[i][j] = min_operations
         return dp[i][j]
 
     def matrix_chain_multiplication(sizes_array):
         n = len(sizes_array)
         dp = {i: {j: None for j in range(n)} for i in range(n)}
+        # Solve for matrices (ABCD), i.e., i = 1 to i = n - 1. Note that the dimension of ith matrix is
+        # A[i - 1] X A[i].
         return solve_problem(sizes_array, 1, n - 1, dp)
 
     print(matrix_chain_multiplication([4, 5, 3, 2]))
