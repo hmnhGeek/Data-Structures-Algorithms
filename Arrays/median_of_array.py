77fd90730432763bcf20f8af8c3412a0e6ec81a9
hmnhGeek <hmnhsharma97@gmail.com>
2024-09-21 15:54:05 +0530

    Implemented the solution of median problem.

--- Arrays/median_of_array.py
+++ Arrays/median_of_array.py
@@ -38,3 +38,31 @@ class MergeSort:
     def sort(self, arr):
         self._sort(arr, 0, len(arr) - 1)
 
+
+def find_median(arr):
+    """
+        Overall time complexity is O(nlog(n)) and space complexity is O(n).
+    """
+
+    # instead of modifying the original array, create a deep copy. This will take O(n) space.
+    copied_arr = [i for i in arr]
+
+    # apply merge sort on the copied array. This will take O(nlog(n)) time and O(n) space internally.
+    merge_sort = MergeSort()
+    merge_sort.sort(copied_arr)
+    n = len(arr)
+
+    # if the length of the array is even, return the avg of two middle elements.
+    if n % 2 == 0:
+        i = n // 2
+        j = i - 1
+        return (copied_arr[i] + copied_arr[j])/2
+
+    # else return the middle element itself.
+    return copied_arr[n//2]
+
+
+print(find_median([90, 100, 78, 89, 67]))
+print(find_median([56, 67, 30, 79]))
+print(find_median([1, 3, 4, 2, 6, 5, 8, 7]))
+print(find_median([4]*5))
