ad0695bc8a03503332c4daac54f562d1821b5d71
hmnhGeek <hmnhsharma97@gmail.com>
2024-09-26 13:32:41 +0530

    Subarrays with K Different Integers

--- Sliding Windows/subarrays_with_k_distinct_integers.py
+++ Sliding Windows/subarrays_with_k_distinct_integers.py
@@ -1,3 +1,7 @@
+# Problem link - https://leetcode.com/problems/subarrays-with-k-different-integers/description/
+# Solution - https://www.youtube.com/watch?v=7wYGbV_LsX4&list=PLgUwDviBIf0q7vrFA_HEWcqRqMpCXzYAL&index=11
+
+
 def get_distincts(mp):
     count = 0
     for i in mp:
@@ -7,6 +11,13 @@ def get_distincts(mp):
 
 
 def get_less_than_equal_to(arr, k):
+    # This function is again similar to what is written in binary_subarray_sum.py. The basic idea is to count
+    # all the subarrays in which we have the number of distinct elements <= k.
+
+    # The time complexity would be O(N^2) and space would be O(N) because `get_distincts()` function would iterate
+    # N times to find the elements whose count > 0. This can be optimized. The space will be O(N) to store each element
+    # in the `mp` dictionary.
+
     if k < 0:
         return 0
     i, j, count = 0, 0, 0
