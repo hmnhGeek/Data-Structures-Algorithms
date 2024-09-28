cc58193325f395e8907cf86b855180423700bf2e
hmnhGeek <hmnhsharma97@gmail.com>
2024-09-28 20:10:33 +0530

    Partition for maximum sum using tabulation

--- Dynamic Programming/Partition DP/partition_for_max_sum.py
+++ Dynamic Programming/Partition DP/partition_for_max_sum.py
@@ -89,6 +89,55 @@ def memoized():
     print(partition_for_max_sum([1], 1))
 
 
+def tabulation():
+    """
+        Time complexity is O(n^2) and space complexity is O(n).
+    """
+
+    def partition_for_max_sum(arr, k):
+        n = len(arr)
+        dp = {i: float('-inf') for i in range(n + 1)}
+
+        # base case
+        dp[n] = 0
+
+        for i in range(n - 1, -1, -1):
+            # store a global max value for sum.
+            max_ans = float('-inf')
+
+            # store variables for local checks.
+            length = 0
+            local_max = float('-inf')
+
+            # loop on the partitions till `kth` partition or `n` whichever is minimum.
+            for j in range(i, min(i + k, n)):
+                # increment the length of the partition and update the local maxima
+                length += 1
+                local_max = max(local_max, arr[j])
+
+                # update the local sum and solve for right part.
+                local_sum = (length * local_max) + dp[j + 1]
+
+                # update the global max
+                max_ans = max(max_ans, local_sum)
+
+            # return the global max
+            dp[i] = max_ans
+
+        return dp[0]
+
+    print(partition_for_max_sum([1, 15, 7, 9, 2, 5, 10], 3))
+    print(partition_for_max_sum([1, 4, 1, 5, 7, 3, 6, 1, 9, 9, 3], 4))
+    print(partition_for_max_sum([1], 1))
+
+
+print("Recursion Solution")
 recursive()
+
 print()
+print("Memoized Solution")
 memoized()
+
+print()
+print("Tabulation Solution")
+tabulation()
