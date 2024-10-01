36375570c627a6fd21fc151136326bb173424722
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-01 17:10:26 +0530

    Wrote a utility class to find the largest rectangle in a histogram

--- Matrix/largest_rectangle.py
+++ Matrix/largest_rectangle.py
@@ -12,6 +12,11 @@ class Stack:
     def is_empty(self):
         return self.length == 0
 
+    def top(self):
+        if self.is_empty():
+            return -1
+        return self.head.data
+
     def push(self, x):
         node = Node(x)
 
@@ -33,3 +38,31 @@ class Stack:
         self.length -= 1
         return item
 
+
+class Utility:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_largest_area_rectangle_in_histogram(histogram):
+        stack = Stack()
+        max_area = 0
+        n = len(histogram)
+        for i in range(n):
+            while not stack.is_empty() and histogram[stack.top()] >= histogram[i]:
+                bar = stack.pop()
+                area = histogram[bar] * ((i - 1) - (stack.top() + 1) + 1)
+                max_area = max(max_area, area)
+            else:
+                stack.push(i)
+
+        while not stack.is_empty():
+            bar = stack.pop()
+            area = histogram[bar] * ((n - 1) - (stack.top() + 1) + 1)
+            max_area = max(max_area, area)
+
+        return max_area
+
+
+print(Utility.get_largest_area_rectangle_in_histogram([60, 20, 50, 40, 10, 50, 60]))
+print(Utility.get_largest_area_rectangle_in_histogram([3, 5, 1, 7, 5, 9]))
