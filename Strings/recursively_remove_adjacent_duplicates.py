7e2f1c8eb96e9411cb803172d3770066161ae786
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-01 18:04:59 +0530

    Remove all consecutive duplicates from the string

--- Strings/recursively_remove_adjacent_duplicates.py
+++ Strings/recursively_remove_adjacent_duplicates.py
@@ -1,16 +1,27 @@
+# Problem link - https://www.geeksforgeeks.org/remove-consecutive-duplicates-string/
+
+
 def remove_adjacent_duplicates(string, index, result):
+    # if index becomes negative, return whatever result that has been formed till now.
     if index < 0:
         return result
 
+    # if this is the first time, i.e., index == n - 1, simply prepend the last character to the result
+    # as there is no character to compare with.
     if len(result) == 0:
         return remove_adjacent_duplicates(string, index - 1, string[index] + result)
 
+    # if there is already some characters in result, compare the current index value with front of the result.
+    # if they do not match, then we can prepend this character and move to next index.
     if string[index] != result[0]:
         return remove_adjacent_duplicates(string, index - 1, string[index] + result)
+
+    # however, if they match, don't do anything to the result, and simply move to the next index.
     return remove_adjacent_duplicates(string, index - 1, result)
 
 
 def remove_adjacent_duplicate_characters(string):
+    # Time complexity is O(n) and space complexity is O(n).
     n = len(string)
     result = ""
     return remove_adjacent_duplicates(string, n - 1, result)
