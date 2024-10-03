815d33cd4c9f9775738f47cf30885eb13d9db38e
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-03 12:27:57 +0530

    Game with string problem

--- Heap/game_with_string.py
+++ Heap/game_with_string.py
@@ -1,6 +1,7 @@
 # Problem link - https://www.geeksforgeeks.org/problems/game-with-string4100/1
 from collections import Counter
 
+
 class MaxHeap:
     def __init__(self):
         self.heap = []
@@ -72,24 +73,49 @@ class MaxHeap:
 class Solution:
     @staticmethod
     def game_with_string(string: str, k: int) -> int:
+        """
+            Overall time complexity is O(n*log(n)) and space complexity is O(n).
+        """
+
+        # if you want to remove all the characters from the string or more than those, return 0.
         if k >= len(string):
             return 0
+
+        # get the frequencies of each character as a list. No need for characters, we just want distinct frequencies.
+        # This will take O(n) time and O(n) space in worst case when all characters are unique.
         frequencies = list(dict(Counter(string)).values())
+
+        # initialize a max heap to always get the max frequency in O(log(n)) time.
         max_heap = MaxHeap()
+        # push the frequencies into the max heap. This will occupy O(n) space in worst case.
         for frequency in frequencies:
             max_heap.push(frequency)
 
+        # initialize a counter = k, so as not to modify actual `k` value.
         counter = k
+
+        # since counter < len(string), heap will always contain data even if counter becomes 0.
+        # This will take O(k*log(n)) time.
         while counter != 0:
+            # pop the highest frequency
             max_freq = max_heap.pop()
+
+            # decrement it and also decrement the counter, which means we have removed a character.
             max_freq -= 1
             counter -= 1
+
+            # if max_freq is still greater than 0, push it back to heap.
             if max_freq > 0:
                 max_heap.push(max_freq)
 
+        # once counter turns 0, i.e., all the `k` characters have been removed, start popping the leftover
+        # frequencies from the max heap. This will again take O(n*log(n)) time.
         result = 0
         while not max_heap.is_empty():
+            # add these frequencies by squaring them into the result.
             result += (max_heap.pop())**2
+
+        # finally return the result.
         return result
 
 
