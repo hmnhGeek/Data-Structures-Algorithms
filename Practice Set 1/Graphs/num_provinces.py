fad32a265975d9690c48ceb2641959fcc5624a6b
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-03 19:47:40 +0530

    Number of provinces using DFS

---
+++ Practice Set 1/Graphs/num_provinces.py
@@ -0,0 +1,44 @@
+class Graph:
+    @classmethod
+    def _dfs(cls, graph, node, visited):
+        visited[node] = True
+        for adj_node in graph[node]:
+            if not visited[adj_node]:
+                Graph._dfs(graph, adj_node, visited)
+
+    @staticmethod
+    def get_num_components(adjacency_list):
+        visited = {i: False for i in adjacency_list}
+        num_components = 0
+        for node in visited:
+            if not visited[node]:
+                num_components += 1
+                Graph._dfs(adjacency_list, node, visited)
+        return num_components
+
+
+class Solution:
+    @staticmethod
+    def find_num_provinces(graph):
+        return Graph.get_num_components(graph)
+
+
+print(
+    Solution.find_num_provinces(
+        {
+            1: [3],
+            2: [],
+            3: [1]
+        }
+    )
+)
+
+print(
+    Solution.find_num_provinces(
+        {
+            1: [],
+            2: [],
+            3: []
+        }
+    )
+)
