a4b29e39bee414553641c4b7cf9fe2926b1b75e8
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-04 20:10:05 +0530

    Climb stairs memoized

--- Practice Set 1/Dynamic Programming/climb_stairs.py
+++ Practice Set 1/Dynamic Programming/climb_stairs.py
@@ -16,4 +16,30 @@ def recursive():
     print(climb_stairs(5))
 
 
+def memoized():
+    # T: O(n) and space is O(n + n)
+    def solve(index, dp):
+        if index < 0:
+            return 0
+        if index == 0:
+            return 1
+
+        if dp[index] is not None:
+            return dp[index]
+
+        dp[index] = solve(index - 1, dp) + solve(index - 2, dp)
+        return dp[index]
+
+    def climb_stairs(num_stairs):
+        dp = {i: None for i in range(num_stairs + 1)}
+        return solve(num_stairs, dp)
+
+    print(climb_stairs(2))
+    print(climb_stairs(3))
+    print(climb_stairs(4))
+    print(climb_stairs(5))
+
+
 recursive()
+print()
+memoized()
