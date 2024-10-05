6ff974b730da380e688ec2af89f7ae015442ec02
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-05 16:33:36 +0530

    house robber using tabulation

--- Practice Set 1/Dynamic Programming/DP5 - house_robber.py
+++ Practice Set 1/Dynamic Programming/DP5 - house_robber.py
@@ -35,7 +35,7 @@ def recursive():
 
 def memoized():
     def solve(arr, index, pick, dp):
-        # T: O(2^n) and S: O(n)
+        # T: O(n) and S: O(2n)
         if index < 0:
             return 0
         if index == 0:
@@ -73,6 +73,29 @@ def memoized():
     print(house_robber([1, 2, 3, 1, 3, 5, 8, 1, 9]))
 
 
+def tabulation():
+    # T: O(n) and S: O(n)
+    def house_robber(arr):
+        n = len(arr)
+        dp = {i: {True: -1e6, False: -1e6} for i in range(n)}
+        dp[0][True] = arr[0]
+        dp[0][False] = 0
+
+        for index in range(1, n):
+            if index - 2 >= 0:
+                dp[index][True] = arr[index] + max(dp[index - 2][True], dp[index - 2][False])
+            dp[index][False] = max(dp[index - 1][True], dp[index - 1][False])
+
+        return max(dp[n - 1][True], dp[n - 1][False])
+
+    print(house_robber([2, 1, 4, 9]))
+    print(house_robber([1, 2, 4]))
+    print(house_robber([1, 2, 3, 5, 4]))
+    print(house_robber([1, 2, 3, 1, 3, 5, 8, 1, 9]))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
