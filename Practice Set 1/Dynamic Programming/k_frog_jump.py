a0f85e22b0e832eaf533582f51fdd860f156c287
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-05 16:13:13 +0530

    frog jump with k steps using tabulation

--- Practice Set 1/Dynamic Programming/k_frog_jump.py
+++ Practice Set 1/Dynamic Programming/k_frog_jump.py
@@ -46,6 +46,29 @@ def memoized():
     print(k_frog_jump([40, 10, 20, 70, 80, 10, 20, 70, 80, 60], 4))
 
 
+def tabulation():
+    # T: O(n) and S: O(n)
+    def k_frog_jump(arr, k):
+        n = len(arr)
+        dp = {i: float('inf') for i in range(n)}
+        dp[0] = 0
+
+        for index in range(1, n):
+            min_energy = float('inf')
+            for j in range(1, k + 1):
+                if index - j >= 0:
+                    energy = dp[index - j] + abs(arr[index] - arr[index - j])
+                    min_energy = min(min_energy, energy)
+            dp[index] = min_energy
+        return dp[n - 1]
+
+    print(k_frog_jump([10, 30, 40, 50, 20], 3))
+    print(k_frog_jump([10, 20, 10], 1))
+    print(k_frog_jump([40, 10, 20, 70, 80, 10, 20, 70, 80, 60], 4))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
