68073e3a6024c52b0e2d4dc9dd0a566a952ca7c9
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-05 18:15:42 +0530

    BFS traversal using queue.

--- Graphs/bfs.py
+++ Graphs/bfs.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/bfs-traversal-of-graph/1
+# Solution - https://www.youtube.com/watch?v=-tgVpUgsQ5k&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=5
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -37,25 +41,46 @@ class Graph:
         self.graph = adjacency_list
 
     def bfs(self, start_node):
+        """
+            Time complexity is O(V + E) and space complexity is O(V) for the queue.
+        """
+
+        # if the node is not there in the graph, there is no need to perform BFS.
         if start_node not in self.graph:
             return
+
+        # initialize a queue to start BFS and push the starting node with level = 0 in the queue.
         queue = Queue()
         queue.push((start_node, 0))
+
+        # create a blank visited array for each node
         visited = {i: False for i in self.graph}
+
+        # create a variable to store BFS traversal
         result = {}
+
+        # till every node is visited
         while not queue.is_empty():
+            # pop the current node and its level
             node, level = queue.pop()
 
+            # if the popped node is not visited, visit it and update it in the result.
             if not visited[node]:
                 visited[node] = True
+
+                # if witnessing this level for the first time, add this node with this level
                 if level not in result:
                     result[level] = [node]
                 else:
+                    # else push this new node into the level
                     result[level].append(node)
 
+            # traverse on the adjacent nodes of the current node.
             for adj_node in self.graph[node]:
+                # if the adjacent node is not visited, push it to the queue with +1 level
                 if not visited[adj_node]:
                     queue.push((adj_node, level + 1))
+        # return the BFS traversal.
         return result
 
 
@@ -72,4 +97,18 @@ print(
             8: [6]
         }
     ).bfs(5)
+)
+
+print(
+    Graph(
+        {
+            1: [2, 3],
+            2: [5],
+            3: [1, 4, 6],
+            4: [3],
+            5: [7],
+            6: [3, 7],
+            7: [5, 6]
+        }
+    ).bfs(1)
 )
