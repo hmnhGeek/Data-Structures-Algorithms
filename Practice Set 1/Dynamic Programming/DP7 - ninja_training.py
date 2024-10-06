1475ff968edda317070f65fe6df6ffd98f57e54f
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-06 15:50:27 +0530

    Ninja training using space optimization

--- Practice Set 1/Dynamic Programming/DP7 - ninja_training.py
+++ Practice Set 1/Dynamic Programming/DP7 - ninja_training.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.naukri.com/code360/problems/ninja-s-training_3621003?source=youtube&campaign=striver_dp_videos
+# Solution - https://www.youtube.com/watch?v=AE39gJYuRog&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=8
+
+
 def recursive():
     def solve(arr, day_index, activity_index, num_activities):
         if day_index == 0:
