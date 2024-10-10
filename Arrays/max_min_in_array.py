84b0b39153087a3a2b3b7de945c14cd03d927dd3
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-10 11:56:51 +0530

    Finding min max in an array using merge sort

--- Arrays/max_min_in_array.py
+++ Arrays/max_min_in_array.py
@@ -26,6 +26,47 @@ class Utility:
         n = len(arr)
         Utility._quick_sort(arr, 0, n - 1)
 
+    @staticmethod
+    def _merge(arr, low, high):
+        mid = int(low + (high - low)/2)
+        left = arr[low:mid+1]
+        right = arr[mid+1:high+1]
+        i, j = 0, 0
+        merged = []
+
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                merged.append(left[i])
+                i += 1
+            else:
+                merged.append(right[j])
+                j += 1
+
+        while i < len(left):
+            merged.append(left[i])
+            i += 1
+
+        while j < len(right):
+            merged.append(right[j])
+            j += 1
+
+        return merged
+
+    @staticmethod
+    def _merge_sort(arr, low, high):
+        if low >= high:
+            return
+
+        mid = int(low + (high - low)/2)
+        Utility._merge_sort(arr, low, mid)
+        Utility._merge_sort(arr, mid + 1, high)
+        merged = Utility._merge(arr, low, high)
+        arr[low:high+1] = merged
+
+    @staticmethod
+    def merge_sort(arr):
+        Utility._merge_sort(arr, 0, len(arr) - 1)
+
 
 def get_min_max(arr):
     copy = [i for i in arr]
@@ -34,5 +75,14 @@ def get_min_max(arr):
     return copy[0], copy[-1]
 
 
+def get_min_max_merge_sort(arr):
+    copy = [i for i in arr]
+    # sort in O(nlog(n)) and get the min from 0th index and max from last index
+    Utility.merge_sort(copy)
+    return copy[0], copy[-1]
+
+
 print(get_min_max([3, 5, 4, 1, 9]))
 print(get_min_max([22, 14, 8, 17, 35, 3]))
+print(get_min_max_merge_sort([3, 5, 4, 1, 9]))
+print(get_min_max_merge_sort([22, 14, 8, 17, 35, 3]))
