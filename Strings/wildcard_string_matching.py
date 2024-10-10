236c3ceae0fdf5bfc69f76984d11cdce6ae17e27
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-10 19:28:27 +0530

    Wildcard string matching using space optimization.

--- Strings/wildcard_string_matching.py
+++ Strings/wildcard_string_matching.py
@@ -1,3 +1,6 @@
+# Problem link - https://www.geeksforgeeks.org/dynamic-programming-wildcard-pattern-matching-linear-time-constant-space/
+
+
 def recursive():
     """
         Time complexity is exponential and space complexity is O(n1 + n2)
@@ -189,8 +192,71 @@ def tabulation():
     print(match_strings("a*ab", "baaabab"))
 
 
+def space_optimized():
+    """
+        Time complexity is O(n1 * n2) and space complexity is O(n2)
+    """
+    def match_strings(s1: str, s2: str) -> bool:
+        n1 = len(s1)
+        n2 = len(s2)
+        prev = {j: False for j in range(n2 + 1)}
+        prev[0] = True
+
+        for i in range(1, n1 + 1):
+            curr = {j: False for j in range(n2 + 1)}
+            got_all_stars = True
+            # check for all the leftover characters in the pattern string
+            for k in range(i, 0, -1):
+                # if any character in the pattern is not a "*", return False, because if all of them are *s,
+                # then only we can match the pattern string with an empty string.
+                if s1[k - 1] != "*":
+                    got_all_stars = False
+                    break
+            curr[0] = got_all_stars
+
+            for j in range(1, n2 + 1):
+                # if characters from both strings match or the character in the pattern string is a "?", then there
+                # is a match, call the recursion for next characters in both the strings.
+                if s1[i - 1] == s2[j - 1] or s1[i - 1] == "?":
+                    curr[j] = prev[j - 1]
+                elif s1[i - 1] == "*":
+                    # if the current character from the pattern string is a "*", then the * can match with either 0
+                    # characters from the other string, or a number of continuous characters from the other string.
+
+                    # if we say that * does not match with any character in the other string, then simply ignore the
+                    # * and move to the next character in the pattern, but stay on the same character in the other
+                    # string.
+                    star_matches_no_one = prev[j]
+
+                    # if we say that * can match with the current character of the other string, then we match the
+                    # other string's character by moving to the next character in the other string but staying at the
+                    # * in the pattern string, because we need to check if * can match with the next character of the
+                    # other string as well and so on.
+                    star_matches_with_current_char_in_other_string = curr[j - 1]
+
+                    # take a logical OR of both the cases and return. Basically, return a true if either of the ways
+                    # the string match.
+                    curr[j] = star_matches_no_one or star_matches_with_current_char_in_other_string
+                else:
+                    curr[j] = False
+            prev = curr
+
+        # start checking for pattern match from the last indices of both the strings.
+        return prev[n2]
+
+    print(match_strings("aa", "a"))
+    print(match_strings("cb", "?a"))
+    print(match_strings("*", "aa"))
+    print(match_strings("ge?ks*", "geeksforgeeks"))
+    print(match_strings("ge*ks", "geeks"))
+    print(match_strings("ba*a?", "baaabab"))
+    print(match_strings("a*ab", "baaabab"))
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
+print()
+space_optimized()
