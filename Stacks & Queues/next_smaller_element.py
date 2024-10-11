5bb3d82bf8e84c3a22065c8f1d6c45827de08e82
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-11 15:21:20 +0530

    Next smaller element using stack

--- Stacks & Queues/next_smaller_element.py
+++ Stacks & Queues/next_smaller_element.py
@@ -44,18 +44,37 @@ class Solution:
     @staticmethod
     def next_smaller(arr):
         """
-            Time complexity is O(n)
+            Time complexity is O(n) and space complexity is also O(n).
         """
+
+        # create a blank stack
         stack = Stack()
+        # also create a blank result array of same length
         result = [None for i in range(len(arr))]
+
+        # traverse the array from reverse direction as we are trying to find next smaller which lie on the right
+        # side; therefore, we must first generate information on the right side.
         for i in range(len(arr) - 1, -1, -1):
+            # while the stack is not empty and the top element on the stack is greater than or equal to the current
+            # element. In that case, continuously pop from the stack.
             while not stack.is_empty() and arr[stack.top()] >= arr[i]:
                 stack.pop()
+
+            # since while condition has failed, check if the stack is empty now.
             if stack.is_empty():
+                # if the stack is empty, there is nothing on the right which is smaller than the current element and
+                # therefore, assign -1 at the same index in result.
                 result[i] = -1
+
+            # other reason for while loop to stop could be that the stack is not empty but the top element is smaller
+            # than the current element. In this case, simply assign the top element at result[i].
             elif arr[stack.top()] < arr[i]:
                 result[i] = arr[stack.top()]
+
+            # in any case, at last push the index `i` into the stack.
             stack.push(i)
+
+        # finally, return the result containing next smaller elements.
         return result
 
 
