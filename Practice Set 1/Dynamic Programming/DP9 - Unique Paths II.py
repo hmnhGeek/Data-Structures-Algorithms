16c1bfb39ac89f48b589a04a904be96d33934e66
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-11 18:18:29 +0530

    Unique paths with obstacles - memoization

--- Practice Set 1/Dynamic Programming/DP9 - Unique Paths II.py
+++ Practice Set 1/Dynamic Programming/DP9 - Unique Paths II.py
@@ -48,4 +48,61 @@ def recursive():
     )
 
 
+def memoized():
+    def solve(mtx, i, j, n, m, dp):
+        # Time complexity is O(n*m) and space complexity is O(n + m + n*m)
+
+        if i == 0 and j == 0:
+            return 1
+
+        if dp[i][j] is not None:
+            return dp[i][j]
+
+        left = 0
+        if 0 <= i - 1 < n and mtx[i - 1][j] != -1:
+            left = solve(mtx, i - 1, j, n, m, dp)
+
+        right = 0
+        if 0 <= j - 1 < m and mtx[i][j - 1] != -1:
+            right = solve(mtx, i, j - 1, n, m, dp)
+
+        dp[i][j] = left + right
+        return left + right
+
+    def unique_paths(mtx):
+        n, m = len(mtx), len(mtx[0])
+        dp = {i: {j: None for j in range(m)} for i in range(n)}
+        return solve(mtx, n - 1, m - 1, n, m, dp)
+
+    print(
+        unique_paths(
+            [
+                [0, 0, 0],
+                [0, -1, 0],
+                [0, 0, 0]
+            ]
+        )
+    )
+
+    print(
+        unique_paths(
+            [
+                [0, -1],
+                [-1, 0]
+            ]
+        )
+    )
+
+    print(
+        unique_paths(
+            [
+                [0, -1],
+                [0, 0]
+            ]
+        )
+    )
+
+
 recursive()
+print()
+memoized()
