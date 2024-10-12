bab3ddd1b8934999de59e7637d955f50137bfd67
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-12 19:01:15 +0530

    Triangle problem using tabulation

--- Practice Set 1/Dynamic Programming/DP11 - Triangle.py
+++ Practice Set 1/Dynamic Programming/DP11 - Triangle.py
@@ -155,6 +155,84 @@ def memoized():
     )
 
 
+def tabulation():
+    """
+        T: O(mn) and space is O(mn)
+    """
+    def triangle(mtx):
+        n = len(mtx)
+        m = len(mtx[-1])
+        min_cost = 1e6
+        dp = {i: {j: 1e6 for j in range(m)} for i in range(n)}
+        dp[0][0] = mtx[0][0]
+
+        for i in range(1, n):
+            for j in range(len(mtx[i])):
+                left = 1e6
+                if 0 <= i - 1 < n and 0 <= j - 1 < len(mtx[i - 1]):
+                    left = dp[i - 1][j - 1]
+
+                right = 1e6
+                if 0 <= i - 1 < n and 0 <= j < len(mtx[i - 1]):
+                    right = dp[i - 1][j]
+                dp[i][j] = mtx[i][j] + min(left, right)
+
+        for j in range(m):
+            min_cost = min(min_cost, dp[n - 1][j])
+        return min_cost
+
+    print(
+        triangle(
+            [
+                [1],
+                [2, 3],
+                [3, 6, 7],
+                [8, 9, 6, 10]
+            ]
+        )
+    )
+
+    print(
+        triangle(
+            [
+                [2],
+                [3, 4],
+                [6, 5, 7],
+                [4, 1, 8, 3]
+            ]
+        )
+    )
+
+    print(
+        triangle(
+            [[-10]]
+        )
+    )
+
+    print(
+        triangle(
+            [
+                [1],
+                [2, 3],
+                [4, 5, 6],
+                [7, 8, 9, 10]
+            ]
+        )
+    )
+
+    print(
+        triangle(
+            [
+                [5],
+                [-1, 3],
+                [22, 1, -9]
+            ]
+        )
+    )
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
