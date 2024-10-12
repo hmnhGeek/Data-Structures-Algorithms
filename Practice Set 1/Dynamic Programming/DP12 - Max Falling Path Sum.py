763abc4e759b3bb7a156faf5853b340ddf35b677
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-12 19:56:29 +0530

    Max falling path sum - space optimized

--- Practice Set 1/Dynamic Programming/DP12 - Max Falling Path Sum.py
+++ Practice Set 1/Dynamic Programming/DP12 - Max Falling Path Sum.py
@@ -223,8 +223,86 @@ def tabulation():
     )
 
 
+def space_optimized():
+    """
+        Time complexity is O(mn) and space complexity is O(m)
+    """
+    def max_path(mtx):
+        n, m = len(mtx), len(mtx[0])
+        max_val = -1e6
+        prev = {j: -1e6 for j in range(m)}
+        for j in prev:
+            prev[j] = mtx[0][j]
+
+        for i in range(1, n):
+            curr = {j: -1e6 for j in range(m)}
+            for j in range(m):
+                left = -1e6
+                if 0 <= i - 1 < n and 0 <= j - 1 < m:
+                    left = prev[j - 1]
+
+                middle = -1e6
+                if 0 <= i - 1 < n and 0 <= j < m:
+                    middle = prev[j]
+
+                right = -1e6
+                if 0 <= i - 1 < n and 0 <= j + 1 < m:
+                    right = prev[j + 1]
+
+                curr[j] = mtx[i][j] + max(left, middle, right)
+            prev = curr
+
+        for j in range(m):
+            max_val = max(max_val, prev[j])
+        return max_val
+
+    print(
+        max_path(
+            [
+                [1, 2, 10, 4],
+                [100, 3, 2, 1],
+                [1, 1, 20, 2],
+                [1, 2, 2, 1]
+            ]
+        )
+    )
+
+    print(
+        max_path(
+            [
+                [10, 2, 3],
+                [3, 7, 2],
+                [8, 1, 5]
+            ]
+        )
+    )
+
+    print(
+        max_path(
+            [
+                [1, 2, 3],
+                [9, 8, 7],
+                [4, 5, 6]
+            ]
+        )
+    )
+
+    print(
+        max_path(
+            [
+                [10, 10, 2, -13, 20, 4],
+                [1, -9, -81, 30, 2, 5],
+                [0, 10, 4, -79, 2, -10],
+                [1, -5, 2, 20, -11, 4]
+            ]
+        )
+    )
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
+print()
+space_optimized()
