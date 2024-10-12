5271086778ef1656793d19ef6bb1e6a2b9adbc52
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-12 17:17:58 +0530

    Minimum path sum using space optimization

--- Practice Set 1/Dynamic Programming/DP10 - Minimum Sum Path.py
+++ Practice Set 1/Dynamic Programming/DP10 - Minimum Sum Path.py
@@ -162,8 +162,75 @@ def tabulation():
     )
 
 
+def space_optimized():
+    """
+        Time complexity is O(m*n) and space complexity is O(m)
+    """
+
+    def min_path_sum(mtx):
+        n, m = len(mtx), len(mtx[0])
+        prev = {j: 1e6 for j in range(m)}
+
+        for i in range(n):
+            curr = {j: 1e6 for j in range(m)}
+            for j in range(m):
+                if i == 0 and j == 0:
+                    curr[0] = mtx[0][0]
+                else:
+                    left = 1e6
+                    if 0 <= i - 1 < n:
+                        left = prev[j]
+
+                    right = 1e6
+                    if 0 <= j - 1 < m:
+                        right = curr[j - 1]
+                    curr[j] = mtx[i][j] + min(left, right)
+            prev = curr
+        return prev[m - 1]
+
+    print(
+        min_path_sum(
+            [
+                [5, 9, 6],
+                [11, 5, 2]
+            ]
+        )
+    )
+
+    print(
+        min_path_sum(
+            [
+                [1, 2, 3],
+                [4, 5, 4],
+                [7, 5, 9]
+            ]
+        )
+    )
+
+    print(
+        min_path_sum(
+            [
+                [5, 6],
+                [1, 2],
+            ]
+        )
+    )
+
+    print(
+        min_path_sum(
+            [
+                [1, 2, 3],
+                [4, 8, 2],
+                [1, 5, 3]
+            ]
+        )
+    )
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
+print()
+space_optimized()
