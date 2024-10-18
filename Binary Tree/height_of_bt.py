7de6b4f6b54d02a0c7c84402da111fd3693a6e59
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-18 12:41:38 +0530

    Height of a binary tree using recursion -  added more examples

--- Binary Tree/height_of_bt.py
+++ Binary Tree/height_of_bt.py
@@ -48,3 +48,22 @@ n8.left = n5
 n12.right = n18
 n8.right = n11
 print(BinaryTree.get_height(n12))
+
+# Example 4
+n1, n2, n3, n4, n5, n6, n7 = Node(1), Node(2), Node(3), Node(4), Node(5), Node(6), Node(7)
+n1.left = n2
+n2.left = n4
+n5.left = n6
+n1.right = n3
+n3.right = n5
+n5.right = n7
+print(BinaryTree.get_height(n1))
+
+# Example 5
+n1, n2, n5, n4, n6, n55 = Node(1), Node(2), Node(5), Node(4), Node(6), Node(5)
+n1.left = n2
+n5.left = n4
+n4.left = n55
+n1.right = n5
+n5.right = n6
+print(BinaryTree.get_height(n1))
