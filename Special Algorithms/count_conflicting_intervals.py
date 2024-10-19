33bda4ee875bef01d1605d2daaa7ed28bc30d12f
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-19 14:59:57 +0530

    Get conflicting intervals

--- Special Algorithms/count_conflicting_intervals.py
+++ Special Algorithms/count_conflicting_intervals.py
@@ -1,11 +1,18 @@
+# Problem link - https://www.geeksforgeeks.org/given-n-appointments-find-conflicting-appointments/
+
+
 def search_conflicting_intervals(intervals, low, high, end_time):
     conflicting_intervals = []
     last_slot = low
     while low <= high:
         mid = int(low + (high - low) / 2)
         possible_interval = intervals[mid]
+        # if the start time of the mid-interval is less than or equal to the end time,
+        # then there is an overlap and conflict
         if possible_interval[0] <= end_time:
-            conflicting_intervals.extend(intervals[last_slot:mid+1])
+            # add all the conflicting intervals
+            conflicting_intervals.extend(intervals[last_slot:mid + 1])
+            # set low and last slot to mid + 1
             low = last_slot = mid + 1
         else:
             high = mid - 1
@@ -13,10 +20,16 @@ def search_conflicting_intervals(intervals, low, high, end_time):
 
 
 def get_conflicting_intervals(intervals):
+    # sort the intervals based on starting time in O(n*log(n)) time.
     intervals.sort(key=lambda x: x[0])
+
+    # create a blank dictionary to store the conflicting intervals.
     conflicting_intervals = {}
+
+    # loop on all the intervals... this will also take O(n*log(n)).
     for idx in range(len(intervals)):
         current_interval = intervals[idx]
+        # get all the conflicting intervals for this interval from idx +1 till end using binary search.
         conflicting_intervals[current_interval] = search_conflicting_intervals(intervals, idx + 1, len(intervals) - 1,
                                                                                current_interval[1])
     for i in conflicting_intervals:
@@ -25,5 +38,5 @@ def get_conflicting_intervals(intervals):
 
 
 get_conflicting_intervals(
-        [(1, 5), (3, 7), (2, 6), (10, 15), (5, 6), (4, 100)]
-    )
+    [(1, 5), (3, 7), (2, 6), (10, 15), (5, 6), (4, 100)]
+)
