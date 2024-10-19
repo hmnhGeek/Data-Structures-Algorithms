1fe8d390628e87d69f6a127a3ab152f355a5aedc
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-19 18:16:50 +0530

    Memoized subset sum

--- Practice Set 1/Dynamic Programming/DP16 - Partition with minimum difference.py
+++ Practice Set 1/Dynamic Programming/DP16 - Partition with minimum difference.py
@@ -14,3 +14,28 @@ def recursive():
     def subset_sum(arr, target):
         n = len(arr)
         return solve(arr, n - 1, target)
+
+
+def memoized():
+    def solve(arr, index, target, dp):
+        if target == 0:
+            return True
+        if index == 0:
+            return arr[0] == target
+
+        if dp[index][target] is not None:
+            return dp[index][target]
+
+        left = False
+        if arr[index] <= target:
+            left = solve(arr, index - 1, target - arr[index], dp)
+        right = solve(arr, index - 1, target, dp)
+        dp[index][target] = left or right
+        return dp[index][target]
+
+    def subset_sum(arr, target):
+        n = len(arr)
+        dp = {i: {j: None for j in range(target + 1)} for i in range(n)}
+        return solve(arr, n - 1, target, dp)
+
+    print(subset_sum([1, 6, 11, 5], 15))
