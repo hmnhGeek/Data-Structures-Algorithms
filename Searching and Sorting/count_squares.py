25827c8c4c02951945c946b4aec10433798b975c
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-19 12:06:29 +0530

    Consider a sample space S consisting of all perfect squares starting from 1, 4, 9 and so on. You are given a number N, you have to output the number of integers less than N in the sample space S.

--- Searching and Sorting/count_squares.py
+++ Searching and Sorting/count_squares.py
@@ -1,14 +1,33 @@
+# Problem link - https://www.geeksforgeeks.org/problems/count-squares3649/1
+
+
 def count_squares(n):
+    """
+        Time complexity is O(log(n)) and space complexity is O(1).
+    """
+
+    # define a search space from 1 to n (n may or may not be a perfect square)
     low = 1
     high = n
+
+    # typical binary search algorithm
     while low <= high:
         mid = int(low + (high - low)/2)
+
+        # if the square of mid == n, then there would be exactly mid - 1 perfect squares
+        # before n.
         if mid*mid == n:
             return mid - 1
+
+        # else if square of mid is less than n, we must increase low to get closer to n.
         elif mid*mid < n:
             low = mid + 1
+
+        # else we must decrement high to come closer to n.
         else:
             high = mid - 1
+
+    # finally, high would point to the number of perfect squares less than n.
     return high
 
 
