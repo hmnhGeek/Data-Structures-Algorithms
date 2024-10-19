186c40f96ec9c8ab68a2cdb6965f86774898bf37
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-19 18:14:41 +0530

    Recursive subset sum

---
+++ Practice Set 1/Dynamic Programming/DP16 - Partition with minimum difference.py
@@ -0,0 +1,16 @@
+def recursive():
+    def solve(arr, index, target):
+        if target == 0:
+            return True
+        if index == 0:
+            return arr[0] == target
+
+        left = False
+        if arr[index] <= target:
+            left = solve(arr, index - 1, target - arr[index])
+        right = solve(arr, index - 1, target)
+        return left or right
+
+    def subset_sum(arr, target):
+        n = len(arr)
+        return solve(arr, n - 1, target)
