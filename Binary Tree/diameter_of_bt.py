246169bb164303086d25ff43e0429585ad08f6e9
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-19 13:07:11 +0530

    Diameter of a binary tree

--- Binary Tree/diameter_of_bt.py
+++ Binary Tree/diameter_of_bt.py
@@ -6,14 +6,19 @@ class Node:
 
 class BinaryTree:
     @staticmethod
-    def _get_height(root: Node) -> int:
+    def _get_height(root: Node, maxi: int):
         if root is None:
-            return 0
-        return 1 + max(BinaryTree._get_height(root.left), BinaryTree._get_height(root.right))
+            return 0, maxi
+        left_height, maxi = BinaryTree._get_height(root.left, maxi)
+        right_height, maxi = BinaryTree._get_height(root.right, maxi)
+        maxi = max(maxi, 1 + left_height + right_height)
+        return 1 + max(left_height, right_height), maxi
 
     @staticmethod
     def get_diameter(root: Node) -> int:
-        return 1 + BinaryTree._get_height(root.left) + BinaryTree._get_height(root.right)
+        _, diameter = BinaryTree._get_height(root, 0)
+        return diameter
+
 
 
 # Example 1
@@ -30,3 +35,15 @@ one, two, three = Node(1), Node(2), Node(3)
 one.left = two
 one.right = three
 print(BinaryTree.get_diameter(one))
+
+# Example 3
+n1, n2, n3, n4, n5, n6, n7, n8, n9 = Node(1), Node(2), Node(3), Node(4), Node(5), Node(6), Node(7), Node(8), Node(9)
+n1.left = n2
+n3.left = n4
+n4.left = n5
+n5.left = n6
+n1.right = n3
+n3.right = n7
+n7.right = n8
+n8.right = n9
+print(BinaryTree.get_diameter(n1))
