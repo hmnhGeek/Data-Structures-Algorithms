a1b20ddf1d08760a6abd8db67c689e0fdf16201d
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-19 18:37:23 +0530

    Partition sum minimized

--- Practice Set 1/Dynamic Programming/DP16 - Partition with minimum difference.py
+++ Practice Set 1/Dynamic Programming/DP16 - Partition with minimum difference.py
@@ -87,6 +87,34 @@ def subset_sum(arr, target):
 
 
 def min_partition(arr):
+    """
+        Let us assume that there are two subsets with sums s1 and s2. Let the sum of whole array
+        be s. We have these two equations now:
+
+        s1 + s2 = s
+        |s1 - s2| = d
+
+        We have to minimize the value of `d`. Let's assume that s1 > s2. In that case, the 2nd equation
+        becomes s1 - s2 = d.
+
+        Using both the equations, we have d = 2*s1 - s.
+        For s1 > s2, our search space will be [s//2, s). Even if you want to take the search space as
+        (0, s//2], then s2 > s1. But that doesn't matter because both the search spaces will yield the same
+        result.
+
+        Now assuming the search space to be [s//2, s), the first target that is found in this space will give
+        us the answer. Why first such target? Because as you move to the right in the search space, s1 starts
+        to increase, but at the same time, s2 starts to decrease. Therefore, `d` or the gap between s1 and s2
+        starts to increase. We don't want this to happen. The best result would come when s1 and s2 are near
+        to s//2.
+
+        Similarly, if your search space had been (0, s//2], then the loop should start in reverse so that you
+        can detect the nearest target to s//2.
+
+        Once we get the desired target, d_min = |2*s1 - s|.
+        Return this d_min value.
+    """
+
     sigma = sum(arr)
     for target in range(sigma//2, sigma):
         if subset_sum(arr, target):
