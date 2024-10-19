28185c612f95aa530881389acb1bcca96927dcae
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-19 11:44:42 +0530

    Minimum operations to convert string s1 to string s2

--- Strings/min_ops_to_convert_string.py
+++ Strings/min_ops_to_convert_string.py
@@ -1,12 +1,10 @@
 # Problem link - https://www.geeksforgeeks.org/transform-one-string-to-another-using-minimum-number-of-given-operation/
 
 
-class StringConverter:
-    @staticmethod
-    def _put_front(string, index):
-        string = string[index] + string[:index] + string[index + 1:]
-        return string
+from collections import Counter
 
+
+class StringConverter:
     @staticmethod
     def convert(string1, string2):
         """
@@ -16,27 +14,28 @@ class StringConverter:
         n1 = len(string1)
         n2 = len(string2)
 
-        # if the strings are not of the same length, return
-        if n1 != n2:
-            return
+        # if the strings are not of the same length or if both of them do not have exactly same characters, return
+        if n1 != n2 or Counter(string1) != Counter(string2):
+            return -1
 
         # initialize a variable to traverse both the strings.
         i = n1 - 1
+        j = n2 - 1
 
         # store the number of operations to 0.
         num_operations = 0
 
         # while the string traversal is possible...
-        while i > 0:
-            # if the characters of both the strings at index i do not match.
-            if string1[i] != string2[i]:
-                # put the ith character of string1 to front.
-                string1 = StringConverter._put_front(string1, i)
+        while i >= 0:
+            # if the characters of both the strings do not match...
+            if string1[i] != string2[j]:
                 # increase the number of operations.
                 num_operations += 1
             else:
-                # decrement the index i, if the characters do not match.
-                i -= 1
+                # decrement the index j, if the characters match.
+                j -= 1
+            # irrespective, decrement index i.
+            i -= 1
 
         # return the total number of operations.
         return num_operations
