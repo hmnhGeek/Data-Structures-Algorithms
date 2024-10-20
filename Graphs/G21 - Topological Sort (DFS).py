da3d4a80873e442d6c8b384840cbb28b06899c8f
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-20 14:34:06 +0530

    Topological sort for DFS

---
+++ Graphs/G21 - Topological Sort (DFS).py
@@ -0,0 +1,85 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.head = self.tail = None
+        self.length = 0
+
+    def is_empty(self):
+        return self.length == 0
+
+    def push(self, x):
+        node = Node(x)
+        if self.is_empty():
+            self.head = self.tail = node
+        else:
+            node.next = self.head
+            self.head = node
+        self.length += 1
+
+    def pop(self):
+        if self.is_empty():
+            return
+        item = self.head.data
+        node = self.head
+        self.head = self.head.next
+        del node
+        self.length -= 1
+        return item
+
+
+class Graph:
+    @staticmethod
+    def _dfs(graph, node, visited, stack):
+        visited[node] = True
+        for adj_node in graph[node]:
+            if not visited[adj_node]:
+                Graph._dfs(graph, adj_node, visited, stack)
+        stack.push(node)
+
+    @staticmethod
+    def get_topological_sort(graph):
+        visited = {i: False for i in graph}
+        stack = Stack()
+        for node in graph:
+            if not visited[node]:
+                Graph._dfs(graph, node, visited, stack)
+        while not stack.is_empty():
+            print(stack.pop(), end=" ")
+        print()
+
+
+Graph.get_topological_sort(
+    {
+        0: [],
+        1: [],
+        2: [3],
+        3: [1],
+        4: [0, 1],
+        5: [0, 2]
+    }
+)
+
+Graph.get_topological_sort(
+    {
+        0: [],
+        1: [0],
+        2: [0],
+        3: [0]
+    }
+)
+
+Graph.get_topological_sort(
+    {
+        0: [],
+        1: [3],
+        2: [3],
+        3: [],
+        4: [0, 1],
+        5: [0, 2]
+    }
+)
