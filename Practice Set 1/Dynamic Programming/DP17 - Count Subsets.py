00005b2963d5741d4e19430aa33894c11a997654
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-20 14:54:51 +0530

    tabulation count subsets problem

--- Practice Set 1/Dynamic Programming/DP17 - Count Subsets.py
+++ Practice Set 1/Dynamic Programming/DP17 - Count Subsets.py
@@ -47,6 +47,31 @@ def memoized():
     print(count_subsets([2, 34, 5], 40))
 
 
+def tabulation():
+    def count_subsets(arr, target):
+        n = len(arr)
+        dp = {i: {j: 0 for j in range(target + 1)} for i in range(n)}
+        for i in dp:
+            dp[i][0] = 1
+        dp[0][arr[0]] = 1
+
+        for index in range(1, n):
+            for tgt in range(target + 1):
+                left = 0
+                if arr[index] <= tgt:
+                    left = dp[index - 1][tgt - arr[index]]
+                right = dp[index - 1][tgt]
+                dp[index][tgt] = left + right
+
+        return dp[n - 1][target]
+
+    print(count_subsets([1, 2, 2, 3], 3))
+    print(count_subsets([1, 1, 4, 5], 5))
+    print(count_subsets([2, 34, 5], 40))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
