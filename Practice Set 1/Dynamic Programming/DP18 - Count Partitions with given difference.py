1b17dacd18406a2826a1bb858e5889a6c5a7dc6b
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-20 16:09:18 +0530

    tabulation subset sum

--- Practice Set 1/Dynamic Programming/DP18 - Count Partitions with given difference.py
+++ Practice Set 1/Dynamic Programming/DP18 - Count Partitions with given difference.py
@@ -45,6 +45,46 @@ def memoized():
     print(subset_sum([2, 5, 1, 6, 7], 4))
 
 
+def tabulation():
+    def solve(arr, index, target, dp):
+        if target == 0:
+            return True
+        if index == 0:
+            return arr[0] == target
+
+        if dp[index][target] is not None:
+            return dp[index][target]
+
+        left = False
+        if arr[index] <= target:
+            left = solve(arr, index - 1, target - arr[index], dp)
+        right = solve(arr, index - 1, target, dp)
+        dp[index][target] = left or right
+        return left or right
+
+    def subset_sum(arr, target):
+        n = len(arr)
+        dp = {i: {j: False for j in range(target + 1)} for i in range(n)}
+        for i in dp:
+            dp[i][0] = True
+        dp[0][arr[0]] = True
+
+        for index in range(1, n):
+            for tgt in range(target + 1):
+                left = False
+                if arr[index] <= tgt:
+                    left = dp[index - 1][tgt - arr[index]]
+                right = dp[index - 1][tgt]
+                dp[index][tgt] = left or right
+
+        return dp[n - 1][target]
+
+    print(subset_sum([4, 3, 2, 1], 5))
+    print(subset_sum([2, 5, 1, 6, 7], 4))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
