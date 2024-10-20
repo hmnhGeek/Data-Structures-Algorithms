0801d44468e4ea4294751beb907340abe9fe3981
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-20 14:52:46 +0530

    memoized count subsets problem

--- Practice Set 1/Dynamic Programming/DP17 - Count Subsets.py
+++ Practice Set 1/Dynamic Programming/DP17 - Count Subsets.py
@@ -19,4 +19,34 @@ def recursive():
     print(count_subsets([1, 1, 4, 5], 5))
     print(count_subsets([2, 34, 5], 40))
 
+
+def memoized():
+    def solve(arr, index, target, dp):
+        if target == 0:
+            return 1
+        if index == 0:
+            return 1 if arr[0] == target else 0
+
+        if dp[index][target] is not None:
+            return dp[index][target]
+
+        left = 0
+        if arr[index] <= target:
+            left = solve(arr, index - 1, target - arr[index], dp)
+        right = solve(arr, index - 1, target, dp)
+        dp[index][target] = left + right
+        return left + right
+
+    def count_subsets(arr, target):
+        n = len(arr)
+        dp = {i: {j: None for j in range(target + 1)} for i in range(n)}
+        return solve(arr, n - 1, target, dp)
+
+    print(count_subsets([1, 2, 2, 3], 3))
+    print(count_subsets([1, 1, 4, 5], 5))
+    print(count_subsets([2, 34, 5], 40))
+
+
 recursive()
+print()
+memoized()
