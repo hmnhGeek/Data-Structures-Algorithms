0dc4ecc699aa3dfd49a1fa04a807402cf4967f23
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-20 14:51:16 +0530

    Recursive count subsets problem

---
+++ Practice Set 1/Dynamic Programming/DP17 - Count Subsets.py
@@ -0,0 +1,22 @@
+def recursive():
+    def solve(arr, index, target):
+        if target == 0:
+            return 1
+        if index == 0:
+            return 1 if arr[0] == target else 0
+
+        left = 0
+        if arr[index] <= target:
+            left = solve(arr, index - 1, target - arr[index])
+        right = solve(arr, index - 1, target)
+        return left + right
+
+    def count_subsets(arr, target):
+        n = len(arr)
+        return solve(arr, n - 1, target)
+
+    print(count_subsets([1, 2, 2, 3], 3))
+    print(count_subsets([1, 1, 4, 5], 5))
+    print(count_subsets([2, 34, 5], 40))
+
+recursive()
