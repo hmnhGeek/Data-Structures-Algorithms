50e6b620cfed68c778bd56f59ea71b524cb3ce86
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-20 16:54:56 +0530

    Cycle detection in directed graph using topological sort (Kahn's Algorithm)

---
+++ Graphs/G23 - Cycle Detection in Directed Graph using Topological Sorting.py
@@ -0,0 +1,102 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Queue:
+    def __init__(self):
+        self.head = self.tail = None
+        self.length = 0
+
+    def is_empty(self):
+        return self.length == 0
+
+    def push(self, x):
+        node = Node(x)
+        if self.is_empty():
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+        self.length += 1
+
+    def pop(self):
+        if self.is_empty():
+            return
+        item = self.head.data
+        node = self.head
+        self.head = self.head.next
+        del node
+        self.length -= 1
+        return item
+
+
+class Graph:
+    @staticmethod
+    def _get_indegrees(graph, indegrees):
+        for node in graph:
+            for adj_node in graph[node]:
+                indegrees[adj_node] += 1
+
+    @staticmethod
+    def detect_cycle(graph):
+        indegrees = {i: 0 for i in graph}
+        Graph._get_indegrees(graph, indegrees)
+        queue = Queue()
+        for node in graph:
+            if indegrees[node] == 0:
+                queue.push(node)
+        count = 0
+        while not queue.is_empty():
+            node = queue.pop()
+            count += 1
+            for adj_node in graph[node]:
+                indegrees[adj_node] -= 1
+                if indegrees[adj_node] == 0:
+                    queue.push(adj_node)
+        return count != len(graph)
+
+
+print(
+    Graph.detect_cycle(
+        {
+            0: [1],
+            1: [2],
+            2: [3],
+            3: [3]
+        }
+    )
+)
+
+print(
+    Graph.detect_cycle(
+        {
+            0: [1],
+            1: [2],
+            2: []
+        }
+    )
+)
+
+print(
+    Graph.detect_cycle(
+        {
+            0: [1, 2],
+            1: [2],
+            2: [0, 3],
+            3: [3]
+        }
+    )
+)
+
+print(
+    Graph.detect_cycle(
+        {
+            0: [1, 2],
+            1: [2],
+            2: [3],
+            3: []
+        }
+    )
+)
