2fe03dcaf431bec82df767851d5ad94bf497d453
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-24 19:24:46 +0530

    Eventual safe nodes using Kahn's Algorithm

--- Graphs/G25 - Eventual Safe Nodes (Kahn's Algorithm).py
+++ Graphs/G25 - Eventual Safe Nodes (Kahn's Algorithm).py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/eventual-safe-states/1
+# Solution - https://www.youtube.com/watch?v=2gtg3VsDGyc&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=26
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -41,22 +45,38 @@ class Graph:
 
     @staticmethod
     def _topological_sort(graph):
+        """
+            Overall time complexity is O(V + E) and space complexity is O(V).
+        """
+
+        # calculate indegrees of each node in O(V + E) time and O(V) space.
         indegrees = {i: 0 for i in graph}
         Graph._get_indegrees(graph, indegrees)
+
+        # initialize queue for implementing Kahn's Algorithm
         queue = Queue()
         topo_sort = []
 
+        # push all the starting nodes into the queue (nodes having 0 indegrees)
         for node in indegrees:
             if indegrees[node] == 0:
                 queue.push(node)
 
+        # typical BFS...
         while not queue.is_empty():
+            # pop the current node and push it to topo sort array.
             current_node = queue.pop()
             topo_sort.append(current_node)
+
+            # iterate on the adjacent nodes of the current popped node.
             for adj_node in graph[current_node]:
+                # reduce the indegree of the adjacent node.
                 indegrees[adj_node] -= 1
+                # if the indegree reaches 0, push it to the queue.
                 if indegrees[adj_node] == 0:
                     queue.push(adj_node)
+
+        # finally return the topo sort array.
         return topo_sort
 
     @staticmethod
@@ -69,9 +89,16 @@ class Graph:
 
     @staticmethod
     def get_eventual_safe_nodes(graph):
+        """
+            Time complexity is O(V + E) and space complexity is O(V + E).
+        """
+        # find the reverse graph representation in O(V + E) time and O(V + E) space.
         reversed_graph = Graph._get_reversed_graph(graph)
+        # do a toposort on the reversed graph in O(V + E) time and O(V) space.
         topo_sort = Graph._topological_sort(reversed_graph)
+        # return topo sort.
         return topo_sort
+
 
 print(
     Graph.get_eventual_safe_nodes(
@@ -84,6 +111,17 @@ print(
             5: [],
             6: [],
             7: [1]
+        }
+    )
+)
+
+print(
+    Graph.get_eventual_safe_nodes(
+        {
+            0: [1],
+            1: [2],
+            2: [0, 3],
+            3: []
         }
     )
 )
