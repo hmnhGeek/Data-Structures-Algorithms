03ed3cfa589c67c4b69b0f4a51d5d098de8f40d4
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-24 10:38:31 +0530

    Minimum coins using tabulation

--- Practice Set 1/Dynamic Programming/DP20 - Minimum Coins.py
+++ Practice Set 1/Dynamic Programming/DP20 - Minimum Coins.py
@@ -65,6 +65,41 @@ def memoized():
     print(minimum_coins([4, 6, 2], 5))
 
 
+def tabulation():
+    """
+        Time complexity is O(n*amount) and space complexity is O(n*amount).
+    """
+    def minimum_coins(denominations, amount):
+        num_denominations = len(denominations)
+        dp = {i: {j: 1e6 for j in range(amount + 1)} for i in range(num_denominations)}
+        for i in dp:
+            dp[i][0] = 0
+        for j in dp[0]:
+            if j % denominations[0] == 0:
+                dp[0][j] = j // denominations[0]
+
+        for index in range(1, num_denominations):
+            for target in range(1, amount + 1):
+                left = 1e6
+                if target >= denominations[index]:
+                    left = 1 + dp[index][target - denominations[index]]
+                right = dp[index - 1][target]
+                dp[index][target] = min(left, right)
+
+        num_coins = dp[num_denominations - 1][amount]
+        return num_coins if num_coins != 1e6 else -1
+
+    print(minimum_coins([1, 2, 3], 7))
+    print(minimum_coins([12, 1, 3], 4))
+    print(minimum_coins([1, 2], 11))
+    print(minimum_coins([25, 10, 5], 30))
+    print(minimum_coins([9, 6, 5, 1], 19))
+    print(minimum_coins([5, 1], 0))
+    print(minimum_coins([4, 6, 2], 5))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
