9203e5a858ec78a8f049b0f956314b4c93a4fe44
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-24 12:17:25 +0530

    Coin change 2 - recursive

---
+++ Practice Set 1/Dynamic Programming/DP22 - Coint Change 2.py
@@ -0,0 +1,25 @@
+def recursive():
+    def solve(arr, index, target):
+        if target == 0:
+            return 1
+
+        if index == 0:
+            return 1 if target % arr[0] == 0 else 0
+
+        left = 0
+        if arr[index] <= target:
+            left = solve(arr, index, target - arr[index])
+        right = solve(arr, index - 1, target)
+        return left + right
+
+    def coin_change_2(coins, target):
+        n = len(coins)
+        return solve(coins, n - 1, target)
+
+    print(coin_change_2([3, 1, 2], 4))
+    print(coin_change_2([5, 3, 2], 1))
+    print(coin_change_2([1, 2, 5], 5))
+    print(coin_change_2([2], 3))
+
+
+recursive()
