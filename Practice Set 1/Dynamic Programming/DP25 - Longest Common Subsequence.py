6a5372622a5200eedd6e4000ea978f91303fce74
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-25 19:41:01 +0530

    LCS using memoization

--- Practice Set 1/Dynamic Programming/DP25 - Longest Common Subsequence.py
+++ Practice Set 1/Dynamic Programming/DP25 - Longest Common Subsequence.py
@@ -24,4 +24,37 @@ def recursive():
     print(lcs_length("ABC", "CBA"))
 
 
+def memoized():
+    """
+        Time complexity is O(n1*n2) and space is O(n1 + n2 + n1*n2)
+    """
+    def solve(s1, i, s2, j, dp):
+        if i == 0 or j == 0:
+            return 0
+
+        if dp[i][j] is not None:
+            return dp[i][j]
+
+        if s1[i - 1] == s2[j - 1]:
+            dp[i][j] = 1 + solve(s1, i - 1, s2, j - 1, dp)
+        else:
+            dp[i][j] = max(
+                solve(s1, i, s2, j - 1, dp),
+                solve(s1, i - 1, s2, j, dp)
+            )
+        return dp[i][j]
+
+    def lcs_length(s1, s2):
+        n1, n2 = len(s1), len(s2)
+        dp = {i: {j: None for j in range(n2 + 1)} for i in range(n1 + 1)}
+        return solve(s1, n1, s2, n2, dp)
+
+    print(lcs_length("adebc", "dcadb"))
+    print(lcs_length("ab", "defg"))
+    print(lcs_length("AGGTAB", "GXTXAYB"))
+    print(lcs_length("ABC", "CBA"))
+
+
 recursive()
+print()
+memoized()
