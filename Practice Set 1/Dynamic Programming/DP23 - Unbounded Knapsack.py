0e1e7cc33ca640d036f63af64943ae4156caabba
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-25 18:37:13 +0530

    Unbounded knapsack - memoized

--- Practice Set 1/Dynamic Programming/DP23 - Unbounded Knapsack.py
+++ Practice Set 1/Dynamic Programming/DP23 - Unbounded Knapsack.py
@@ -24,4 +24,37 @@ def recursive():
     print(unbounded_knapsack([4, 17], [6, 12], 3))
 
 
+def memoized():
+    """
+        Time complexity is O(capacity*n) and space complexity is O(n + capacity*n).
+    """
+    def solve(weights, values, index, capacity, dp):
+        if capacity == 0:
+            return 0
+
+        if index == 0:
+            return (capacity // weights[0]) * values[0]
+
+        if dp[index][capacity] is not None:
+            return dp[index][capacity]
+
+        left = 0
+        if weights[index] <= capacity:
+            left = values[index] + solve(weights, values, index, capacity - weights[index], dp)
+        right = solve(weights, values, index - 1, capacity, dp)
+        dp[index][capacity] = max(left, right)
+        return dp[index][capacity]
+
+    def unbounded_knapsack(weights, values, capacity):
+        n = len(weights)
+        dp = {i: {j: None for j in range(capacity + 1)} for i in range(n)}
+        return solve(weights, values, n - 1, capacity, dp)
+
+    print(unbounded_knapsack([2, 4, 6], [5, 11, 13], 10))
+    print(unbounded_knapsack([5, 10, 20], [7, 2, 4], 15))
+    print(unbounded_knapsack([4, 17], [6, 12], 3))
+
+
 recursive()
+print()
+memoized()
