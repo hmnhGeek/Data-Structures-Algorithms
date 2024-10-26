36e6b9aa1d02d3738c73f6e3e9208e1bd601148b
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-26 20:35:46 +0530

    Mirror of a binary tree in O(n) time and O(n) space.

--- Binary Tree/mirror_of_bt.py
+++ Binary Tree/mirror_of_bt.py
@@ -1,3 +1,6 @@
+# Problem link - https://www.geeksforgeeks.org/create-a-mirror-tree-from-the-given-binary-tree/
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -14,14 +17,26 @@ class TreeInorder:
 
 
 class Solution:
+    """
+        Overall time complexity is O(n) and space complexity is O(n).
+    """
+
     @staticmethod
     def _get_mirror(root: Node):
+        # if the node you're asking for does not exist, simply end the recursion.
         if root is None:
             return
 
+        # store the reference to the left node of the root.
         left_subtree_root = root.left
+
+        # LATERAL INVERSION
+        # update the left node of the root to its right node
         root.left = root.right
+        # update the right node of the root to the stored left node.
         root.right = left_subtree_root
+
+        # recursively call this mirror method on left and right subtrees of the root.
         Solution._get_mirror(root.left)
         Solution._get_mirror(root.right)
 
@@ -51,3 +66,19 @@ two.right = eight
 eight.right = nine
 TreeInorder.get(two)
 Solution.get_mirror(two)
+
+print()
+print()
+# Example 3
+five, three, six, two, four, eight, ten, eleven, twelve, thirteen = Node(5), Node(3), Node(6), Node(2), Node(4), Node(8), Node(10), Node(11), Node(12), Node(13)
+five.left = three
+three.left = two
+six.left = eight
+eight.left = eleven
+twelve.left = thirteen
+five.right = six
+three.right = four
+six.right = ten
+eight.right = twelve
+TreeInorder.get(five)
+Solution.get_mirror(five)
