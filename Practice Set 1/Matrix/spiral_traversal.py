86301ab73fb4088a44a13790910c9a224858184b
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-27 15:33:45 +0530

    Spiral traversal of a matrix

--- Practice Set 1/Matrix/spiral_traversal.py
+++ Practice Set 1/Matrix/spiral_traversal.py
@@ -1,3 +1,6 @@
+# Problem link - https://www.geeksforgeeks.org/problems/spirally-traversing-a-matrix-1587115621/1
+
+
 class Matrix:
     RIGHT = "R"
     LEFT = "L"
@@ -6,8 +9,16 @@ class Matrix:
 
     @staticmethod
     def spiral_traverse(mtx):
+        """
+            Overall time complexity is O(nm) and space complexity is O(nm).
+        """
+
         n, m = len(mtx), len(mtx[0])
+
+        # initialize pointers to left, right, up and down corners of the matrix.
         left, right, up, down = 0, m - 1, 0, n - 1
+
+        # set the initial traversal direction as right.
         direction = Matrix.RIGHT
         traversal = []
 
@@ -44,5 +55,17 @@ print(
             [9, 10, 11, 12],
             [13, 14, 15, 16]
         ]
+    )
+)
+
+print(
+    Matrix.spiral_traverse(
+        [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+    )
+)
+
+print(
+    Matrix.spiral_traverse(
+        [[32, 44, 27, 23], [54, 28, 50, 62]]
     )
 )
