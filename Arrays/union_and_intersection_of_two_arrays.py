53aedfbf8e97746d1ef36f77af8e1adadd658ef6
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-27 15:13:09 +0530

    Union and intersection of two lists

--- Arrays/union_and_intersection_of_two_arrays.py
+++ Arrays/union_and_intersection_of_two_arrays.py
@@ -5,22 +5,43 @@ from typing import List
 
 
 def union(arr1: List[int], arr2: List[int]) -> set:
+    """
+        Overall time complexity is O(n + m) and overall space complexity is O(n + m).
+    """
+
     union_set = set()
+
+    # This will take O(n) time.
     for i in arr1:
         union_set.add(i)
+
+    # This will take O(m) time.
     for i in arr2:
         union_set.add(i)
+
+    # The set will take O(n + m) space in worst case.
     return union_set
 
 
 def intersection(arr1: List[int], arr2: List[int]) -> set:
+    """
+        Overall time complexity is O(m + n) and overall space complexity is O(m + n).
+    """
+
     l1_set = set()
     intersection_set = set()
+
+    # This will take O(n) time.
     for i in arr1:
         l1_set.add(i)
+
+    # This will take O(m) time.
     for i in arr2:
+        # lookup is amortized O(1).
         if i in l1_set:
             intersection_set.add(i)
+
+    # The intersection will hold O(n + m) space in worst case.
     return intersection_set
 
 
