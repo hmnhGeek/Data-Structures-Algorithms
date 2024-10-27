d7b1ed6032cde1cf74881a433bcabfd90fee404c
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-27 17:39:56 +0530

    Inorder traversal of binary tree using recursion and iterative approaches.

--- Binary Tree/inorder_traversal_bt.py
+++ Binary Tree/inorder_traversal_bt.py
@@ -1,3 +1,6 @@
+# Problem link - https://www.techiedelight.com/inorder-tree-traversal-iterative-recursive/
+
+
 class StackNode:
     def __init__(self, data):
         self.data = data
@@ -48,22 +51,43 @@ class InorderTraversal:
 
     @staticmethod
     def recursive(root: Node):
+        """
+            Time complexity is O(n) and space complexity is O(n) for the recursion stack.
+        """
         InorderTraversal._recursive(root)
         print()
 
     @staticmethod
     def iterative(root: Node):
+        """
+            Time complexity is O(n) and space complexity is O(n).
+        """
+
+        # initialize a stack because the recursion stack is no longer there.
         stack = Stack()
 
+        # start from the root node.
         curr = root
+
+        # ALGORITHM
+        # For a given node, go all the way to leftmost leaf node while pushing to stack.
+        # While popping the current node, print it, and go top its right subtree and follow step 1 on right subtree root
+
+        # initially, push the left border of the tree into stack
         while curr is not None:
             stack.push(curr)
             curr = curr.left
 
+        # update stack until it gets empty.
         while not stack.is_empty():
+            # pop the current node and print it.
             node = stack.pop()
             print(node.data, end=" ")
+
+            # get the right subtree root (basically we printed left, node, and now going to right subtree)
             right_subtree = node.right
+
+            # continuously push left boundary of this subtree into the stack.
             while right_subtree is not None:
                 stack.push(right_subtree)
                 right_subtree = right_subtree.left
@@ -81,6 +105,7 @@ InorderTraversal.iterative(n1)
 InorderTraversal.recursive(n1)
 
 print()
+# Example 2
 n1, n1r, n2, n2r, n4, n6, n9, n9l, n9r = Node(1), Node(1), Node(2), Node(2), Node(4), Node(6), Node(9), Node(9), Node(9)
 n6.left = n9
 n9.left = n9l
@@ -92,3 +117,16 @@ n9.right = n1r
 n9l.right = n4
 InorderTraversal.iterative(n6)
 InorderTraversal.recursive(n6)
+
+print()
+# Example 3
+n1, n2, n3, n4, n5, n6, n7, n8 = Node(1), Node(2), Node(3), Node(4), Node(5), Node(6), Node(7), Node(8)
+n1.left = n2
+n2.left = n4
+n3.left = n5
+n5.left = n7
+n1.right = n3
+n3.right = n6
+n5.right = n8
+InorderTraversal.iterative(n1)
+InorderTraversal.recursive(n1)
