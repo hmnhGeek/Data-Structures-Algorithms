742a6cdaf2df8b25d373a5ef4e6b56c8ff87da90
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-27 15:30:59 +0530

    Spiral traversal of a matrix

---
+++ Practice Set 1/Matrix/spiral_traversal.py
@@ -0,0 +1,48 @@
+class Matrix:
+    RIGHT = "R"
+    LEFT = "L"
+    UP = "U"
+    DOWN = "D"
+
+    @staticmethod
+    def spiral_traverse(mtx):
+        n, m = len(mtx), len(mtx[0])
+        left, right, up, down = 0, m - 1, 0, n - 1
+        direction = Matrix.RIGHT
+        traversal = []
+
+        while left <= right and up <= down:
+            if direction == Matrix.RIGHT:
+                for i in range(left, right + 1):
+                    traversal.append(mtx[up][i])
+                up += 1
+                direction = Matrix.DOWN
+            elif direction == Matrix.DOWN:
+                for j in range(up, down + 1):
+                    traversal.append(mtx[j][right])
+                right -= 1
+                direction = Matrix.LEFT
+            elif direction == Matrix.LEFT:
+                for i in range(right, left - 1, -1):
+                    traversal.append(mtx[down][i])
+                down -= 1
+                direction = Matrix.UP
+            elif direction == Matrix.UP:
+                for j in range(down, up - 1, -1):
+                    traversal.append(mtx[j][left])
+                left += 1
+                direction = Matrix.RIGHT
+
+        return traversal
+
+
+print(
+    Matrix.spiral_traverse(
+        [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+            [13, 14, 15, 16]
+        ]
+    )
+)
