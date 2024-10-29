75133b3946a4f6b561a67599786ffa6c78c3ab90
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-29 13:52:53 +0530

    Printing the longest common subsequence - tabulation solution

--- Practice Set 1/Dynamic Programming/DP26 - Printing the Longest Common Subsequence.py
+++ Practice Set 1/Dynamic Programming/DP26 - Printing the Longest Common Subsequence.py
@@ -31,6 +31,9 @@ def memoized():
         if i == 0 or j == 0:
             return ""
 
+        if dp[i][j] is not None:
+            return dp[i][j]
+
         if s1[i - 1] == s2[j - 1]:
             return solve(s1, i - 1, s2, j - 1, dp) + s1[i - 1]
 
@@ -50,6 +53,32 @@ def memoized():
     print(get_lcs("ab", "defg"))
 
 
+def tabulation():
+    """
+        Time complexity is O(n1*n2) and space complexity is O(n1*n2)
+    """
+    def get_lcs(s1: str, s2: str) -> str:
+        n1 = len(s1)
+        n2 = len(s2)
+        dp = {i: {j: "" for j in range(n2 + 1)} for i in range(n1 + 1)}
+
+        for i in range(1, n1 + 1):
+            for j in range(1, n2 + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + s1[i - 1]
+                else:
+                    left = dp[i - 1][j]
+                    right = dp[i][j - 1]
+                    dp[i][j] = max(left, right, key=len)
+        return dp[n1][n2]
+
+    print(get_lcs("abcde", "bdgek"))
+    print(get_lcs("adebc", "dcadb"))
+    print(get_lcs("ab", "defg"))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
