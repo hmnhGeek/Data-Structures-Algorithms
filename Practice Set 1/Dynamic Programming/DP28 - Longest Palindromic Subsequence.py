99c1bea58ca89c91c8a969fd5b8ac5d7314c25d3
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-29 17:49:15 +0530

    Longest palindromic subsequence using LCS

--- Practice Set 1/Dynamic Programming/DP28 - Longest Palindromic Subsequence.py
+++ Practice Set 1/Dynamic Programming/DP28 - Longest Palindromic Subsequence.py
@@ -54,17 +54,36 @@ def tabulation():
     print(get_lcs("abcd", "abzde"))
 
 
-def space_optimized():
-    def get_lcs(s1: str, s2: str) -> str:
-        n1 = len(s1)
-        n2 = len(s2)
-        dp = {i: {j: "" for j in range(n2 + 1)} for i in range(n1 + 1)}
-        for i in range(1, n1 + 1):
-            for j in range(1, n2 + 1):
-                if s1[i - 1] == s2[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1] + s1[i - 1]
-                else:
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-        return dp[n1][n2]
+def get_lcs(s1: str, s2: str) -> str:
+    n1 = len(s1)
+    n2 = len(s2)
+    dp = {i: {j: "" for j in range(n2 + 1)} for i in range(n1 + 1)}
+    for i in range(1, n1 + 1):
+        for j in range(1, n2 + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + s1[i - 1]
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
-    print(get_lcs("abcd", "abzde"))
+    result = ""
+    i, j = n1, n2
+    while i > 0 and j > 0:
+        if s1[i - 1] == s2[j - 1]:
+            result += s1[i - 1]
+            i -= 1
+            j -= 1
+        else:
+            if dp[i - 1][j] >= dp[i][j - 1]:
+                i -= 1
+            else:
+                j -= 1
+    return result[-1:-len(result) - 1:-1]
+
+
+def get_longest_palindromic_subsequence(string: str) -> str:
+    reversed = string[-1:-len(string)-1:-1]
+    return get_lcs(string, reversed)
+
+
+print(get_longest_palindromic_subsequence("bbbab"))
+print(get_longest_palindromic_subsequence("bbabcbcab"))
