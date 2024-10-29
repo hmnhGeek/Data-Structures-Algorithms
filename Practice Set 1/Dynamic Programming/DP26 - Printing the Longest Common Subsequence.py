29ea1bc4796304631905ae2e886bde27eea7e0a8
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-29 13:49:07 +0530

    Printing the longest common subsequence - memoized solution

--- Practice Set 1/Dynamic Programming/DP26 - Printing the Longest Common Subsequence.py
+++ Practice Set 1/Dynamic Programming/DP26 - Printing the Longest Common Subsequence.py
@@ -22,4 +22,34 @@ def recursive():
     print(get_lcs("adebc", "dcadb"))
     print(get_lcs("ab", "defg"))
 
+
+def memoized():
+    """
+        Time complexity is O(n1*n2) and space complexity is O(n1 + n2 + n1*n2)
+    """
+    def solve(s1: str, i: int, s2: str, j: int, dp) -> str:
+        if i == 0 or j == 0:
+            return ""
+
+        if s1[i - 1] == s2[j - 1]:
+            return solve(s1, i - 1, s2, j - 1, dp) + s1[i - 1]
+
+        left = solve(s1, i - 1, s2, j, dp)
+        right = solve(s1, i, s2, j - 1, dp)
+        dp[i][j] = max(left, right, key=len)
+        return dp[i][j]
+
+    def get_lcs(s1: str, s2: str) -> str:
+        n1 = len(s1)
+        n2 = len(s2)
+        dp = {i: {j: None for j in range(n2 + 1)} for i in range(n1 + 1)}
+        return solve(s1, n1, s2, n2, dp)
+
+    print(get_lcs("abcde", "bdgek"))
+    print(get_lcs("adebc", "dcadb"))
+    print(get_lcs("ab", "defg"))
+
+
 recursive()
+print()
+memoized()
