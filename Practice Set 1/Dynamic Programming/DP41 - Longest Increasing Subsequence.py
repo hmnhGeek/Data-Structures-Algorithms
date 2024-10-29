b7944f53569619ecc072f344f282f3e19a2b0d50
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-29 18:35:39 +0530

    Longest increasing subsequence length using memoization

--- Practice Set 1/Dynamic Programming/DP41 - Longest Increasing Subsequence.py
+++ Practice Set 1/Dynamic Programming/DP41 - Longest Increasing Subsequence.py
@@ -18,4 +18,35 @@ def recursive():
     print(longest_increasing_subsequence([1, 2, 2]))
 
 
+def memoized():
+    def solve(arr, index, prev, dp):
+        if index == 0:
+            return 0
+
+        if dp[index][prev] is not None:
+            return dp[index][prev]
+
+        left = 0
+        if arr[index - 1] < prev:
+            left = 1 + solve(arr, index - 1, arr[index - 1], dp)
+        right = solve(arr, index - 1, prev, dp)
+        dp[index][prev] = max(left, right)
+        return dp[index][prev]
+
+    def longest_increasing_subsequence(arr):
+        n = len(arr)
+        maxval = max(arr)
+        dp = {i: {j: None for j in arr} for i in range(n + 1)}
+        for i in dp:
+            dp[i][maxval + 1] = None
+
+        return solve(arr, n - 1, maxval + 1, dp)
+
+    print(longest_increasing_subsequence([10, 9, 2, 5, 3, 7, 101, 18]))
+    print(longest_increasing_subsequence([5, 4, 11, 1, 16, 8]))
+    print(longest_increasing_subsequence([1, 2, 2]))
+
+
 recursive()
+print()
+memoized()
