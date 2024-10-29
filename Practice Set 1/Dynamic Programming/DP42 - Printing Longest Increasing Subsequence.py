edb67cc883364802272664b7c7103f2e7e325028
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-29 19:43:10 +0530

    Printing the longest increasing subsequence

--- Practice Set 1/Dynamic Programming/DP42 - Printing Longest Increasing Subsequence.py
+++ Practice Set 1/Dynamic Programming/DP42 - Printing Longest Increasing Subsequence.py
@@ -8,21 +8,38 @@ def print_lis(arr):
 
     for i in range(1, len(arr)):
         for prev in range(i):
+            # if the prev value is less than current value...
             if arr[prev] < arr[i]:
+                # then it can be considered in increasing sequence. But will including it
+                # in the increasing sequence give us even a longer increasing sequence?
                 if 1 + dp[prev] > dp[i]:
+                    # if yes, then update the dp and parents dictionary.
                     parents[i] = prev
                     dp[i] = 1 + dp[prev]
 
+    # get the index of the LIS from the dp array.
     last_index_of_lis = max(dp, key=dp.get)
+    # get the length of LIS as the max increasing sequence length from dp array.
     lis_length = max(dp.values())
+
+    # create a result array which would be of the size of LIS.
     result = [0]*lis_length
+
+    # create a starting node (just like disjoint set, we will traverse the parents).
     start_node = last_index_of_lis
+    # we will fill the array from back side.
     i = lis_length - 1
 
+    # until the start node is a self parent
     while parents[start_node] != start_node:
+        # update the ith index of the result
         result[i] = arr[start_node]
+        # move to next lower index
         i -= 1
+        # update start node.
         start_node = parents[start_node]
+
+    # at last, update for self parent start node as well.
     result[i] = arr[start_node]
     print(result)
 
