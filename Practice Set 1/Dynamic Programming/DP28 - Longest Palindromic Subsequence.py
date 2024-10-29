dbdf1f5edbb9a2a9a3f29d455550bb189d1b4f58
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-29 17:45:21 +0530

    LCS using tabulation

--- Practice Set 1/Dynamic Programming/DP28 - Longest Palindromic Subsequence.py
+++ Practice Set 1/Dynamic Programming/DP28 - Longest Palindromic Subsequence.py
@@ -37,3 +37,34 @@ def memoized():
 
     print(get_lcs("abcd", "abzde"))
 
+
+def tabulation():
+    def get_lcs(s1: str, s2: str) -> str:
+        n1 = len(s1)
+        n2 = len(s2)
+        dp = {i: {j: "" for j in range(n2 + 1)} for i in range(n1 + 1)}
+        for i in range(1, n1 + 1):
+            for j in range(1, n2 + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + s1[i - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return dp[n1][n2]
+
+    print(get_lcs("abcd", "abzde"))
+
+
+def space_optimized():
+    def get_lcs(s1: str, s2: str) -> str:
+        n1 = len(s1)
+        n2 = len(s2)
+        dp = {i: {j: "" for j in range(n2 + 1)} for i in range(n1 + 1)}
+        for i in range(1, n1 + 1):
+            for j in range(1, n2 + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + s1[i - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return dp[n1][n2]
+
+    print(get_lcs("abcd", "abzde"))
