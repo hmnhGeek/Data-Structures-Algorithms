ef679c635e7f3b08114284210c7350652ce94acf
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-29 17:43:09 +0530

    LCS using memoization

--- Practice Set 1/Dynamic Programming/DP28 - Longest Palindromic Subsequence.py
+++ Practice Set 1/Dynamic Programming/DP28 - Longest Palindromic Subsequence.py
@@ -12,3 +12,28 @@ def recursive():
         n1 = len(s1)
         n2 = len(s2)
         return solve(s1, n1, s2, n2)
+
+
+def memoized():
+    def solve(s1, i, s2, j, dp):
+        if i == 0 or j == 0:
+            return ""
+
+        if dp[i][j] is not None:
+            return dp[i][j]
+
+        if s1[i - 1] == s2[j - 1]:
+            dp[i][j] = solve(s1, i - 1, s2, j - 1, dp) + s1[i - 1]
+        else:
+            dp[i][j] = max(solve(s1, i - 1, s2, j, dp), solve(s1, i, s2, j - 1, dp))
+
+        return dp[i][j]
+
+    def get_lcs(s1: str, s2: str) -> str:
+        n1 = len(s1)
+        n2 = len(s2)
+        dp = {i: {j: None for j in range(n2 + 1)} for i in range(n1 + 1)}
+        return solve(s1, n1, s2, n2, dp)
+
+    print(get_lcs("abcd", "abzde"))
+
