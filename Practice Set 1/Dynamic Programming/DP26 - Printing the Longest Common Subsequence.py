e4a23c04a559c2c9a3624ebdc4111a219641574f
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-29 14:15:43 +0530

    Printing the longest common subsequence - space optimized solution 2 using the tabulation DP array

--- Practice Set 1/Dynamic Programming/DP26 - Printing the Longest Common Subsequence.py
+++ Practice Set 1/Dynamic Programming/DP26 - Printing the Longest Common Subsequence.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.naukri.com/code360/problems/longest-common-subsequence_624879?source=youtube&campaign=striver_dp_videos
+# Solution - https://www.youtube.com/watch?v=-zI4mrF2Pb4&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=27
+
+
 def recursive():
     """
         Time complexity is exponential and space complexity is O(n1 + n2)
@@ -103,6 +107,43 @@ def space_optimized():
     print(get_lcs("ab", "defg"))
 
 
+def space_optimized_approach_2():
+    """
+        Time complexity is O(n1*n2) and space complexity is O(n1*n2)
+    """
+    def get_lcs(s1: str, s2: str) -> str:
+        n1 = len(s1)
+        n2 = len(s2)
+        dp = {i: {j: 0 for j in range(n2 + 1)} for i in range(n1 + 1)}
+
+        for i in range(1, n1 + 1):
+            for j in range(1, n2 + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    left = dp[i - 1][j]
+                    right = dp[i][j - 1]
+                    dp[i][j] = max(left, right)
+
+        result = ""
+        i, j = n1, n2
+        while i > 0 and j > 0:
+            if s1[i - 1] == s2[j - 1]:
+                result += s1[i - 1]
+                i -= 1
+                j -= 1
+            else:
+                if dp[i - 1][j] >= dp[i][j - 1]:
+                    i -= 1
+                else:
+                    j -= 1
+        return result[-1:-len(result)-1:-1]
+
+    print(get_lcs("abcde", "bdgek"))
+    print(get_lcs("adebc", "dcadb"))
+    print(get_lcs("ab", "defg"))
+
+
 recursive()
 print()
 memoized()
@@ -110,3 +151,5 @@ print()
 tabulation()
 print()
 space_optimized()
+print()
+space_optimized_approach_2()
