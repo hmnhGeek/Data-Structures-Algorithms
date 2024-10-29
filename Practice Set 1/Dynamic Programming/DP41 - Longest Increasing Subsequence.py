b652bb0b13bb375110c0d7b0a527a305f21632ec
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-29 18:38:15 +0530

    Longest increasing subsequence length using tabulation

--- Practice Set 1/Dynamic Programming/DP41 - Longest Increasing Subsequence.py
+++ Practice Set 1/Dynamic Programming/DP41 - Longest Increasing Subsequence.py
@@ -40,7 +40,30 @@ def memoized():
         for i in dp:
             dp[i][maxval + 1] = None
 
-        return solve(arr, n - 1, maxval + 1, dp)
+        return solve(arr, n, maxval + 1, dp)
+
+    print(longest_increasing_subsequence([10, 9, 2, 5, 3, 7, 101, 18]))
+    print(longest_increasing_subsequence([5, 4, 11, 1, 16, 8]))
+    print(longest_increasing_subsequence([1, 2, 2]))
+
+
+def tabulation():
+    def longest_increasing_subsequence(arr):
+        n = len(arr)
+        maxval = max(arr)
+        dp = {i: {j: 0 for j in arr} for i in range(n + 1)}
+        for i in dp:
+            dp[i][maxval + 1] = 0
+
+        for index in range(1, n + 1):
+            for prev in dp[index]:
+                left = 0
+                if arr[index - 1] < prev:
+                    left = 1 + dp[index - 1][arr[index - 1]]
+                right = dp[index - 1][prev]
+                dp[index][prev] = max(left, right)
+
+        return dp[n][maxval + 1]
 
     print(longest_increasing_subsequence([10, 9, 2, 5, 3, 7, 101, 18]))
     print(longest_increasing_subsequence([5, 4, 11, 1, 16, 8]))
@@ -50,3 +73,5 @@ def memoized():
 recursive()
 print()
 memoized()
+print()
+tabulation()
