c383f62f9b7b1ae8d34840ec6daaae4d1507b347
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-30 20:44:40 +0530

    Space optimized 2 LCS

--- Practice Set 1/Dynamic Programming/DP29 - Minimum Insertions to make string palindrome.py
+++ Practice Set 1/Dynamic Programming/DP29 - Minimum Insertions to make string palindrome.py
@@ -93,6 +93,37 @@ def space_optimized():
     print(lcs("abcd", "abzde"))
 
 
+def space_optimized_2():
+    def lcs(s1, s2):
+        n1 = len(s1)
+        n2 = len(s2)
+        dp = {i: {j: 0 for j in range(n2 + 1)} for i in range(n1 + 1)}
+
+        for i in range(1, n1 + 1):
+            for j in range(1, n2 + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(
+                        dp[i - 1][j],
+                        dp[i][j - 1]
+                    )
+
+        result = ""
+        i, j = n1, n2
+        while i > 0 and j > 0:
+            if s1[i - 1] == s2[j - 1]:
+                result += s1[i - 1]
+            else:
+                if dp[i - 1][j] >= dp[i][j - 1]:
+                    i -= 1
+                else:
+                    j -= 1
+        return result[-1:-len(result)-1:-1]
+
+    print(lcs("abcd", "abzde"))
+
+
 recursive()
 print()
 memoized()
@@ -100,3 +131,5 @@ print()
 tabulation()
 print()
 space_optimized()
+print()
+space_optimized_2()
