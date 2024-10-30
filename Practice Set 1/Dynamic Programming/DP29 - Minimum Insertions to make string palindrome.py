3a3dc12d4195b54ff4a08a5226dbe38f238e53b9
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-30 20:40:22 +0530

    Tabulation LCS

--- Practice Set 1/Dynamic Programming/DP29 - Minimum Insertions to make string palindrome.py
+++ Practice Set 1/Dynamic Programming/DP29 - Minimum Insertions to make string palindrome.py
@@ -47,6 +47,47 @@ def memoized():
     print(lcs("abcd", "abzde"))
 
 
+def tabulation():
+    def solve(s1, i, s2, j, dp):
+        if i == 0 or j == 0:
+            return ""
+
+        if dp[i][j] is not None:
+            return dp[i][j]
+
+        if s1[i - 1] == s2[j - 1]:
+            dp[i][j] = solve(s1, i - 1, s2, j - 1, dp) + s1[i - 1]
+        else:
+            dp[i][j] = max(
+                solve(s1, i - 1, s2, j, dp),
+                solve(s1, i, s2, j - 1, dp),
+                key=len
+            )
+        return dp[i][j]
+
+    def lcs(s1, s2):
+        n1 = len(s1)
+        n2 = len(s2)
+        dp = {i: {j: "" for j in range(n2 + 1)} for i in range(n1 + 1)}
+
+        for i in range(1, n1 + 1):
+            for j in range(1, n2 + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + s1[i - 1]
+                else:
+                    dp[i][j] = max(
+                        dp[i - 1][j],
+                        dp[i][j - 1],
+                        key=len
+                    )
+
+        return dp[n1][n2]
+
+    print(lcs("abcd", "abzde"))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
