18d4cf7515a3ffd568b923c1faa7a8b3d4d8f27a
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-30 20:38:49 +0530

    Memoized LCS

--- Practice Set 1/Dynamic Programming/DP29 - Minimum Insertions to make string palindrome.py
+++ Practice Set 1/Dynamic Programming/DP29 - Minimum Insertions to make string palindrome.py
@@ -20,4 +20,33 @@ def recursive():
     print(lcs("abcd", "abzde"))
 
 
+def memoized():
+    def solve(s1, i, s2, j, dp):
+        if i == 0 or j == 0:
+            return ""
+
+        if dp[i][j] is not None:
+            return dp[i][j]
+
+        if s1[i - 1] == s2[j - 1]:
+            dp[i][j] = solve(s1, i - 1, s2, j - 1, dp) + s1[i - 1]
+        else:
+            dp[i][j] = max(
+                solve(s1, i - 1, s2, j, dp),
+                solve(s1, i, s2, j - 1, dp),
+                key=len
+            )
+        return dp[i][j]
+
+    def lcs(s1, s2):
+        n1 = len(s1)
+        n2 = len(s2)
+        dp = {i: {j: None for j in range(n2 + 1)} for i in range(n1 + 1)}
+        return solve(s1, n1, s2, n2, dp)
+
+    print(lcs("abcd", "abzde"))
+
+
 recursive()
+print()
+memoized()
