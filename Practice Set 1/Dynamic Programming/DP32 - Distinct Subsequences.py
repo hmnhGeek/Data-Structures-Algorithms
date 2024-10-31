23fee10ba05c98f8bb8960f6aa247e76078f1d43
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-31 14:10:05 +0530

    Distinct subsequences using memoization

--- Practice Set 1/Dynamic Programming/DP32 - Distinct Subsequences.py
+++ Practice Set 1/Dynamic Programming/DP32 - Distinct Subsequences.py
@@ -20,6 +20,43 @@ def recursive():
     print(distinct_subsequences("brootgroot", "brt"))
     print(distinct_subsequences("dingdingdingding", "ing"))
     print(distinct_subsequences("aaaaa", "a"))
+    print(distinct_subsequences("rabbbit", "rabbit"))
+    print(distinct_subsequences("banana", "ban"))
+    print(distinct_subsequences("abcd", "abe"))
+
+
+def memoized():
+    def solve(string, i, search_term, j, dp):
+        if j == 0:
+            return 1
+        if i == 0:
+            return 0
+
+        if dp[i][j] is not None:
+            return dp[i][j]
+
+        left = 0
+        if string[i - 1] == search_term[j - 1]:
+            left = solve(string, i - 1, search_term, j - 1, dp)
+        right = solve(string, i - 1, search_term, j, dp)
+        dp[i][j] = left + right
+        return dp[i][j]
+
+    def distinct_subsequences(string, search_term):
+        n = len(string)
+        m = len(search_term)
+        dp = {i: {j: None for j in range(m + 1)} for i in range(n + 1)}
+        return solve(string, n, search_term, m, dp)
+
+    print(distinct_subsequences("babgbag", "bag"))
+    print(distinct_subsequences("brootgroot", "brt"))
+    print(distinct_subsequences("dingdingdingding", "ing"))
+    print(distinct_subsequences("aaaaa", "a"))
+    print(distinct_subsequences("rabbbit", "rabbit"))
+    print(distinct_subsequences("banana", "ban"))
+    print(distinct_subsequences("abcd", "abe"))
 
 
 recursive()
+print()
+memoized()
