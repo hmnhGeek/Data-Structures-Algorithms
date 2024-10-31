1cc7854bf3f787d0039614eceda04226d7a8ef80
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-31 17:34:15 +0530

    Wildcard matching tabulation

--- Practice Set 1/Dynamic Programming/DP34 - Wildcard Matching.py
+++ Practice Set 1/Dynamic Programming/DP34 - Wildcard Matching.py
@@ -38,7 +38,7 @@ def recursive():
 
 def memoized():
     """
-        Time complexity is exponential and space complexity O(n + m).
+        Time complexity is O(nm) and space complexity O(n + m + nm).
     """
     def solve(s1, i, s2, j, dp):
         if i == 0 and j == 0:
@@ -79,6 +79,48 @@ def memoized():
     print(match("**", ""))
 
 
+def tabulation():
+    """
+        Time complexity is O(nm) and space complexity O(nm).
+    """
+    def match(s1, s2):
+        n = len(s1)
+        m = len(s2)
+        dp = {i: {j: False for j in range(m + 1)} for i in range(n + 1)}
+        dp[0][0] = True
+        for j in dp[0]:
+            if j > 0:
+                dp[0][j] = False
+        for i in range(1, n + 1):
+            dp[i][0] = True
+            for k in range(1, i + 1):
+                if s1[k - 1] != "*":
+                    dp[i][0] = False
+                    break
+
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if s1[i - 1] == s2[j - 1] or s1[i - 1] == "?":
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif s1[i - 1] == "*":
+                    dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
+                else:
+                    dp[i][j] = False
+
+        return dp[n][m]
+
+    print(match("?ay", "ray"))
+    print(match("ab*cd", "abdefcd"))
+    print(match("ab?d", "abcc"))
+    print(match("ba*a?", "baaabab"))
+    print(match("a", "aa"))
+    print(match("*", "aa"))
+    print(match("?a", "cb"))
+    print(match("**", ""))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
