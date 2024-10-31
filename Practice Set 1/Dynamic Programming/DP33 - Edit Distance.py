ee3f22358da1132d7fda51392ddc3e3301f14480
hmnhGeek <hmnhsharma97@gmail.com>
2024-10-31 16:35:42 +0530

    Space optimized edit distance

--- Practice Set 1/Dynamic Programming/DP33 - Edit Distance.py
+++ Practice Set 1/Dynamic Programming/DP33 - Edit Distance.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.naukri.com/code360/problems/edit-distance_630420?source=youtube&campaign=striver_dp_videos
+# Solution - https://www.youtube.com/watch?v=fJaKO8FbDdo&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=34
+
+
 def recursive():
     # Time complexity is exponential and space complexity is O(n + m).
     def solve(s1, i, s2, j):
@@ -97,8 +101,42 @@ def tabulation():
     print(edit_distance("sunday", "saturday"))
 
 
+def space_optimized():
+    # Time complexity is O(nm) and space complexity is O(m).
+    def edit_distance(s1, s2):
+        n1 = len(s1)
+        n2 = len(s2)
+        prev = {j: 1e6 for j in range(n2 + 1)}
+        prev[0] = 0
+        for j in prev:
+            prev[j] = j
+
+        for i in range(1, n1 + 1):
+            curr = {j: 1e6 for j in range(n2 + 1)}
+            curr[0] = i
+            for j in range(1, n2 + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    curr[j] = prev[j - 1]
+                else:
+                    curr[j] = min(
+                        1 + prev[j],
+                        1 + curr[j - 1],
+                        1 + prev[j - 1]
+                    )
+            prev = curr
+        return prev[n2]
+
+    print(edit_distance("abc", "dc"))
+    print(edit_distance("whgtdwhgtdg", "aswcfg"))
+    print(edit_distance("geek", "gesek"))
+    print(edit_distance("cat", "cut"))
+    print(edit_distance("sunday", "saturday"))
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
+print()
+space_optimized()
