11832c96c853dd83ab48e6c0c09cdb683f176bcf
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-01 15:52:39 +0530

    Find the City With the Smallest Number of Neighbours at a Threshold Distance

--- Practice Set 1/Graphs/G43 - City With the Smallest Number of Neighbours at a Threshold Distance.py
+++ Practice Set 1/Graphs/G43 - City With the Smallest Number of Neighbours at a Threshold Distance.py
@@ -28,9 +28,31 @@ class Graph:
         return graph
 
 
+class Solution:
+    @staticmethod
+    def _get_count(row, threshold):
+        count = 0
+        for i in row:
+            if i <= threshold:
+                count += 1
+        return count
+
+    @staticmethod
+    def get(edge_list, n, threshold):
+        min_distances = Graph.floyd_warshall(edge_list, n)
+        min_count = 1e6
+        city = -1
+        for row in range(n):
+            count = Solution._get_count(min_distances[row], threshold)
+            if min_count >= (count - 1):
+                min_count = count - 1
+                city = row
+        return f"Minimum reachable cities is {min_count} with city = {city}"
+
+
 print(
-    Graph.floyd_warshall(
+    Solution.get(
         [[0, 1, 3], [1, 2, 1], [1, 3, 4], [2, 3, 1]],
+        4,
         4
-    )
-)
+    ))
