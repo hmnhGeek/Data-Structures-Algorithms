9dcd312510a92a0c7e9b613caad0e46856b8e032
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-01 16:54:02 +0530

    Best time to buy and sell stocks 2 - tabulation solution

--- Practice Set 1/Dynamic Programming/DP36 - Best Time to Buy and Sell Stocks 2.py
+++ Practice Set 1/Dynamic Programming/DP36 - Best Time to Buy and Sell Stocks 2.py
@@ -63,6 +63,30 @@ def memoized():
     print(best_time([1, 2, 3, 4, 5]))
 
 
+def tabulation():
+    """
+        Time complexity is O(n) and space complexity is O(n)
+    """
+    def best_time(prices):
+        n = len(prices)
+        dp = {i: {True: 0, False: 0} for i in range(n + 1)}
+
+        for index in range(n - 1, -1, -1):
+            for can_buy in [True, False]:
+                if can_buy:
+                    dp[index][can_buy] = max(-prices[index] + dp[index + 1][False], dp[index + 1][True])
+                else:
+                    dp[index][can_buy] = max(prices[index] + dp[index + 1][True], dp[index + 1][False])
+        return dp[0][True]
+
+    print(best_time([7, 1, 5, 3, 6, 4]))
+    print(best_time([1, 2, 3, 4, 5, 6, 7]))
+    print(best_time([7, 6, 5, 4, 3, 2, 1]))
+    print(best_time([1, 2, 3, 4, 5]))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
