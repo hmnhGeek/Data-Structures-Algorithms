0e27c131a362663dc3a1e461200ffcb53897395c
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-01 17:08:47 +0530

    Best time to buy and sell stocks 3 & 4 - memoized solution

--- Practice Set 1/Dynamic Programming/DP38 - Best Time to Buy and Sell Stocks 4.py
+++ Practice Set 1/Dynamic Programming/DP38 - Best Time to Buy and Sell Stocks 4.py
@@ -32,4 +32,45 @@ def recursive():
     print(best_time([1, 2, 3, 5], 2))
 
 
+def memoized():
+    """
+        Time complexity is O(n*k) and space complexity is O(n + n*k).
+    """
+    def solve(prices, index, can_buy, n, k, dp):
+        if k == 0:
+            return 0
+
+        if index == n:
+            return 0
+
+        if dp[index][can_buy][k] is not None:
+            return dp[index][can_buy][k]
+
+        if can_buy:
+            dp[index][can_buy][k] = max(
+                -prices[index] + solve(prices, index + 1, False, n, k, dp),
+                solve(prices, index + 1, True, n, k, dp)
+            )
+        else:
+            dp[index][can_buy][k] = max(
+                prices[index] + solve(prices, index + 1, True, n, k - 1, dp),
+                solve(prices, index + 1, False, n, k, dp)
+            )
+        return dp[index][can_buy][k]
+
+    def best_time(prices, num_transactions):
+        n = len(prices)
+        dp = {i: {j: {k: None for k in range(num_transactions + 1)} for j in [True, False]} for i in range(n)}
+        return solve(prices, 0, True, n, num_transactions, dp)
+
+    print(best_time([3, 3, 5, 0, 0, 3, 1, 4], 2))
+    print(best_time([1, 3, 1, 2, 4, 8], 2))
+    print(best_time([5, 4, 3, 2, 1], 2))
+    print(best_time([8, 5, 1, 3, 10], 2))
+    print(best_time([2, 6, 5, 2], 0))
+    print(best_time([1, 2, 3, 5], 2))
+
+
 recursive()
+print()
+memoized()
