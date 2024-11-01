8e186e697c89707cd4fe02816406b596fdc9becd
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-01 16:50:34 +0530

    Best time to buy and sell stocks 2 - memoization solution

--- Practice Set 1/Dynamic Programming/DP36 - Best Time to Buy and Sell Stocks 2.py
+++ Practice Set 1/Dynamic Programming/DP36 - Best Time to Buy and Sell Stocks 2.py
@@ -28,4 +28,41 @@ def recursive():
     print(best_time([1, 2, 3, 4, 5]))
 
 
+def memoized():
+    """
+        Time complexity is O(n) and space complexity is O(n + n)
+    """
+
+    def solve(prices, index, can_buy, n, dp):
+        if index == n:
+            return 0
+
+        if dp[index][can_buy] is not None:
+            return dp[index][can_buy]
+
+        if can_buy:
+            dp[index][can_buy] = max(
+                -prices[index] + solve(prices, index + 1, False, n, dp),
+                solve(prices, index + 1, True, n, dp)
+            )
+        else:
+            dp[index][can_buy] = max(
+                prices[index] + solve(prices, index + 1, True, n, dp),
+                solve(prices, index + 1, False, n, dp)
+            )
+        return dp[index][can_buy]
+
+    def best_time(prices):
+        n = len(prices)
+        dp = {i: {True: None, False: None} for i in range(n)}
+        return solve(prices, 0, True, n, dp)
+
+    print(best_time([7, 1, 5, 3, 6, 4]))
+    print(best_time([1, 2, 3, 4, 5, 6, 7]))
+    print(best_time([7, 6, 5, 4, 3, 2, 1]))
+    print(best_time([1, 2, 3, 4, 5]))
+
+
 recursive()
+print()
+memoized()
