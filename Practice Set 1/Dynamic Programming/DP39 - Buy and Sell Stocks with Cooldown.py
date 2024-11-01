3042dd311479c78f5d0828c92800b03b767c051c
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-01 17:23:01 +0530

    Best time to buy and sell stocks with cooldown - memoized solution

--- Practice Set 1/Dynamic Programming/DP39 - Buy and Sell Stocks with Cooldown.py
+++ Practice Set 1/Dynamic Programming/DP39 - Buy and Sell Stocks with Cooldown.py
@@ -27,4 +27,40 @@ def recursive():
     print(best_time([5, 4, 3]))
 
 
+def memoized():
+    """
+        Time complexity is O(n) and space complexity is O(2n).
+    """
+
+    def solve(prices, index, can_buy, n, dp):
+        if index >= n:
+            return 0
+
+        if dp[index][can_buy] is not None:
+            return dp[index][can_buy]
+
+        if can_buy:
+            dp[index][can_buy] = max(
+                -prices[index] + solve(prices, index + 1, False, n, dp),
+                solve(prices, index + 1, True, n, dp)
+            )
+        else:
+            dp[index][can_buy] = max(
+                prices[index] + solve(prices, index + 2, True, n, dp),
+                solve(prices, index + 1, False, n, dp)
+            )
+        return dp[index][can_buy]
+
+    def best_time(prices):
+        n = len(prices)
+        dp = {i: {j: None for j in [True, False]} for i in range(n)}
+        return solve(prices, 0, True, n, dp)
+
+    print(best_time([4, 9, 0, 4, 10]))
+    print(best_time([1, 2, 3, 4]))
+    print(best_time([5, 4, 3]))
+
+
 recursive()
+print()
+memoized()
