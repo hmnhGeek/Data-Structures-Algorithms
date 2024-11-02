35546c1d84ea472bdc3a97f295305f012740664c
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-02 17:31:08 +0530

    Merge k sorted lists of different sizes

--- Heap/merge_k_sorted_lists.py
+++ Heap/merge_k_sorted_lists.py
@@ -1,3 +1,6 @@
+# Problem link - https://www.geeksforgeeks.org/problems/merge-k-sorted-arrays/1
+
+
 class MinHeap:
     def __init__(self):
         self.heap = []
@@ -64,3 +67,69 @@ class MinHeap:
         return item
 
 
+class Solution:
+    @staticmethod
+    def merge_k_sorted_lists(lists):
+        """
+            Overall time complexity is O(n*log(k)) and space complexity is O(k).
+        """
+
+        min_heap = MinHeap()
+        result = []
+
+        # This will take O(k*log(k)) where k is the number of lists
+        for i in range(len(lists)):
+            if len(lists[i]) > 0:
+                min_heap.insert((lists[i][0], i, 0))
+
+        # This will store n nodes in worst case
+        while not min_heap.is_empty():
+            # this will take O(log(k))
+            item, x, y = min_heap.pop()
+            result.append(item)
+
+            # Another O(log(k))
+            if 0 <= y + 1 < len(lists[x]):
+                min_heap.insert((lists[x][y + 1], x, y + 1))
+
+        return result
+
+
+print(
+    Solution.merge_k_sorted_lists(
+        [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ]
+    )
+)
+
+print(
+    Solution.merge_k_sorted_lists(
+        [
+            [1, 4, 5],
+            [1, 3, 4],
+            [2, 6]
+        ]
+    )
+)
+
+print(
+    Solution.merge_k_sorted_lists([])
+)
+
+print(
+    Solution.merge_k_sorted_lists([[]])
+)
+
+print(
+    Solution.merge_k_sorted_lists(
+        [
+            [1],
+            [2, 4],
+            [3, 7, 9, 11],
+            [13]
+        ]
+    )
+)
