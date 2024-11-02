4ef145c292f81e4cabd225a9e5e07b562d6c041b
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-02 13:00:58 +0530

    Search in a 2D matrix

--- Practice Set 1/Matrix/search_in_a_2d_mtx.py
+++ Practice Set 1/Matrix/search_in_a_2d_mtx.py
@@ -2,6 +2,9 @@
 
 
 def search(mtx, target):
+    """
+        Time complexity is O(log(mn)) and space complexity is O(1).
+    """
     n, m = len(mtx), len(mtx[0])
     low = 0
     high = m * n - 1
@@ -30,3 +33,5 @@ print(
 )
 
 print(search([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 13))
+
+print(search([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]], 8))
