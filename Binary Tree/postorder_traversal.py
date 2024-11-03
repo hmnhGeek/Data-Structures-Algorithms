88020b9575f3a60ad1e94211188672cdaa9b8d36
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-03 17:31:04 +0530

    Iterative postorder in binary tree

--- Binary Tree/postorder_traversal.py
+++ Binary Tree/postorder_traversal.py
@@ -1,3 +1,6 @@
+# Problem link - https://www.techiedelight.com/postorder-tree-traversal-iterative-recursive/
+
+
 class StackNode:
     def __init__(self, data):
         self.data = data
@@ -72,3 +75,22 @@ class PostOrder:
         result = result[-1:-len(result)-1:-1]
         return result
 
+
+# Example 1
+n1 = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+n4 = Node(4)
+n5 = Node(5)
+n6 = Node(6)
+n7 = Node(7)
+n8 = Node(8)
+n1.left = n2
+n2.left = n4
+n5.left = n7
+n3.left = n5
+n1.right = n3
+n3.right = n6
+n5.right = n8
+print(PostOrder.recursive(n1))
+print(PostOrder.iterative(n1))
