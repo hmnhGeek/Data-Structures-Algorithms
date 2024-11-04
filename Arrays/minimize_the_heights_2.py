dfe26ca256e43d899c0bb0282607bd268591fff9
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-04 14:30:49 +0530

    Added Quick Sort Algorithm to solve this problem

--- Arrays/minimize_the_heights_2.py
+++ Arrays/minimize_the_heights_2.py
@@ -1,6 +1,34 @@
 # Problem link - https://www.geeksforgeeks.org/problems/minimize-the-heights3351/1
 
 
+class QuickSort:
+    @staticmethod
+    def _get_partition_index(arr, low, high):
+        pivot = arr[low]
+        i, j = low, high
+
+        while i < j:
+            while arr[i] <= pivot and i <= high - 1:
+                i += 1
+            while arr[j] > pivot and j >= low + 1:
+                j -= 1
+            if i < j:
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[low], arr[j] = arr[j], arr[low]
+        return j
+
+    @staticmethod
+    def _sort(arr, low, high):
+        if low < high:
+            partition_index = QuickSort._get_partition_index(arr, low, high)
+            QuickSort._sort(arr, low, partition_index - 1)
+            QuickSort._sort(arr, partition_index + 1, high)
+
+    @staticmethod
+    def sort(arr):
+        return QuickSort._sort(arr, 0, len(arr) - 1)
+
+
 class RecursiveSolution:
     @staticmethod
     def solve(arr, index, k, tracking_arr):
