55b1c257457fef699005cd0bb8ba69e18432ccb5
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-07 12:33:10 +0530

    Recursive longest increasing subsequence using space optimization

--- Practice Set 2/Dynamic Programming/DP41 - Longest Increasing Subsequence.py
+++ Practice Set 2/Dynamic Programming/DP41 - Longest Increasing Subsequence.py
@@ -91,8 +91,39 @@ def tabulation():
     print(get_lis([10, 20, 35, 80]))
 
 
+def space_optimized():
+    """
+        Time complexity is O(n^2) and space complexity is O(n).
+    """
+    def get_lis(arr):
+        n = len(arr)
+        prev = {j: 0 for j in arr}
+        prev[1e6] = 0
+        for index in range(1, n + 1):
+            curr = {j: 0 for j in arr}
+            curr[1e6] = 0
+            for prev_val in prev:
+                left = -1e6
+                if arr[index - 1] < prev_val:
+                    left = 1 + prev[arr[index - 1]]
+                right = prev[prev_val]
+                curr[prev_val] = max(left, right)
+            prev = curr
+        return prev[1e6]
+
+    print(get_lis([10, 9, 2, 5, 3, 7, 101, 18]))
+    print(get_lis([5, 4, 11, 1, 16, 8]))
+    print(get_lis([1, 2, 2]))
+    print(get_lis([3, 10, 2, 1, 20]))
+    print(get_lis([30, 20, 10]))
+    print(get_lis([2, 2, 2]))
+    print(get_lis([10, 20, 35, 80]))
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
+print()
+space_optimized()
