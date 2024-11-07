7ba65f9a39a928a5597f73a0d41dd95151911a47
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-07 13:48:56 +0530

    Kruskal's Algorithm using Disjoint Set (Rank variant)

--- Practice Set 1/Graphs/G47 - Kruskal's Algorithm.py
+++ Practice Set 1/Graphs/G47 - Kruskal's Algorithm.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/minimum-spanning-tree/1
+# Solution - https://www.youtube.com/watch?v=DMnDM_sxVig&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=47
+
+
 class DisjointSet:
     def __init__(self, nodes):
         self.ranks = {i: 0 for i in nodes}
@@ -40,13 +44,23 @@ class Graph:
 
     @staticmethod
     def get_mst(graph):
+        """
+            Overall time complexity is O(V + E + Elog(E)) and space is O(V + E).
+        """
+
+        # O(V + 2E + E*log(E)) time and O(2E) space.
         edges = Graph._get_sorted_edges(graph)
+        # O(V) space.
         disjoint_set = DisjointSet([i for i in graph])
         mst_wt = 0
         mst = []
+
+        # O(2E) time.
         for edge_tuple in edges:
             edge_wt, src, dest = edge_tuple
+            # in O(1) time
             if not disjoint_set.in_same_components(src, dest):
+                # O(1) time
                 disjoint_set.union(src, dest)
                 mst_wt += edge_wt
                 mst.append((src, dest))
@@ -62,6 +76,16 @@ print(
             4: [[5, 9], [1, 1], [2, 3], [3, 5]],
             5: [[1, 4], [4, 9]],
             6: [[2, 7], [3, 8]]
+        }
+    )
+)
+
+print(
+    Graph.get_mst(
+        {
+            0: [[1, 5], [2, 1]],
+            1: [[0, 5], [2, 3]],
+            2: [[0, 1], [1, 3]]
         }
     )
 )
