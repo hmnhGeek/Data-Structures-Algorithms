79bb517ce521e3eee4ee81739259519263aa67ea
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-08 14:54:57 +0530

    Added comments

--- Practice Set 1/Graphs/G50 - Accounts Merge DSU.py
+++ Practice Set 1/Graphs/G50 - Accounts Merge DSU.py
@@ -1,3 +1,7 @@
+# Problem link - https://leetcode.com/problems/accounts-merge/description/
+# Solution - https://www.youtube.com/watch?v=FMwpt_aQOGw&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=51
+
+
 class DisjointSet:
     def __init__(self, nodes):
         self.sizes = {i: 1 for i in nodes}
@@ -54,15 +58,26 @@ class Solution:
     @staticmethod
     def accounts_merge(accounts):
         n = len(accounts)
+
+        # create name and node mappings where name is the 0th index of each row and node is the index of each row.
         name_node_mapping = {i: accounts[i][0] for i in range(n)}
+
+        # create a disjoint set for each node.
         ds = DisjointSet([i for i in name_node_mapping])
 
+        # now we will be looping on all the emails and assign them their parent nodes. At the same time, if the email
+        # was found earlier and is now present in the `emails_to_node_mapping`, we will perform union of the current
+        # row `node` and the node from already existing key-value pair.
         emails_to_node_mapping = {}
         Solution._perform_union_operations(ds, emails_to_node_mapping, accounts, n)
 
+        # now we will group the emails based on nodes and the emails will be appended to their ultimate parents from
+        # the disjoint set.
         node_to_emails_mapping = {i: [] for i in name_node_mapping}
         Solution._group_nodes_and_emails(ds, node_to_emails_mapping, emails_to_node_mapping)
 
+        # finally, using the node-name mapping created above, we will restore the names and sort the emails for each
+        # row.
         final_result = []
         Solution._prepare_final_result(node_to_emails_mapping, final_result, name_node_mapping)
         return final_result
