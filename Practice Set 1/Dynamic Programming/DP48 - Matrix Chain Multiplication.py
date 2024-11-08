9ce96d7cae64e851d7f28f86f4eabe32a9ad0abf
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-08 18:56:10 +0530

    Tabulation matrix chain multiplication

--- Practice Set 1/Dynamic Programming/DP48 - Matrix Chain Multiplication.py
+++ Practice Set 1/Dynamic Programming/DP48 - Matrix Chain Multiplication.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.naukri.com/code360/problems/matrix-chain-multiplication_975344?source=youtube&campaign=striver_dp_videos
+# Solution - https://www.youtube.com/watch?v=pDCXsbAw5Cg&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=50
+
+
 def recursive():
     """
         Time complexity is exponential
@@ -53,6 +57,34 @@ def memoized():
     print(matrix_chain_multiplication([1, 4, 3, 2]))
 
 
+def tabulation():
+    """
+        Time complexity is O(n^3) and space complexity is (n^2).
+    """
+    def matrix_chain_multiplication(arr):
+        n = len(arr)
+        dp = {i: {j: 1e6 for j in range(n)} for i in range(n)}
+        for i in dp:
+            dp[i][i] = 0
+
+        for i in range(n - 1, 0, -1):
+            for j in range(i + 1, n):
+                min_val = 1e6
+                for k in range(i, j):
+                    steps = (arr[i - 1] * arr[k] * arr[j]) + dp[i][k] + dp[k + 1][j]
+                    min_val = min(min_val, steps)
+                dp[i][j] = min_val
+
+        return dp[1][n - 1]
+
+    print(matrix_chain_multiplication([10, 20, 30, 40, 50]))
+    print(matrix_chain_multiplication([4, 5, 3, 2]))
+    print(matrix_chain_multiplication([10, 15, 20, 25]))
+    print(matrix_chain_multiplication([1, 4, 3, 2]))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
