129569c1c40f94c0182ad8288911736fdf5e2260
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-08 19:31:00 +0530

    Remove most stones using disjoint set

--- Practice Set 1/Graphs/G53 - Most Stones Removed with Same Row or Column - DSU.py
+++ Practice Set 1/Graphs/G53 - Most Stones Removed with Same Row or Column - DSU.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/maximum-stone-removal-1662179442/1
+# Solution - https://www.youtube.com/watch?v=OwMNX8SPavM&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=53
+
+
 class DisjointSet:
     def __init__(self, nodes):
         self.sizes = {i: 1 for i in nodes}
@@ -23,3 +27,56 @@ class DisjointSet:
 
     def in_same_components(self, node1, node2):
         return self.find_ultimate_parents(node1) == self.find_ultimate_parents(node2)
+
+
+class Solution:
+    @staticmethod
+    def max_stone_removal(mtx):
+        n, m = len(mtx), len(mtx[0])
+        ds = DisjointSet([i for i in range(n*m)])
+
+        for i in range(n):
+            for j in range(m):
+                if mtx[i][j] == 1:
+                    node = i*m + j
+                    for j0 in range(m):
+                        if j0 != j and mtx[i][j0] == 1:
+                            adj_node = i*m + j0
+                            if not ds.in_same_components(node, adj_node):
+                                ds.union(node, adj_node)
+                    for i0 in range(n):
+                        if i0 != i and mtx[i0][j] == 1:
+                            adj_node = i0*m + j
+                            if not ds.in_same_components(node, adj_node):
+                                ds.union(node, adj_node)
+
+        num_removed = 0
+        for parent in ds.parents:
+            if parent == ds.parents[parent]:
+                num_removed += (ds.sizes[parent] - 1)
+        return num_removed
+
+
+print(
+    Solution.max_stone_removal(
+        [
+            [1, 0, 1, 0],
+            [0, 0, 0, 1],
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 1]
+        ]
+    )
+)
+
+
+print(
+    Solution.max_stone_removal(
+        [
+            [1, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0],
+            [0, 0, 1, 1, 1],
+            [0, 0, 1, 0, 0]
+        ]
+    )
+)
