dc24841015f2d074be96ed13a12e5e7438c0c84e
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-08 15:30:06 +0530

    Solved number of islands 2 problem using disjoint set by rank

--- Practice Set 1/Graphs/G51 - Number of Islands - II.py
+++ Practice Set 1/Graphs/G51 - Number of Islands - II.py
@@ -32,3 +32,50 @@ class DisjointSet:
                 count += 1
         return count
 
+
+class Solution:
+    @staticmethod
+    def _get_node(cell: tuple, m):
+        x, y = cell
+        return m * x + y, x, y
+
+    @staticmethod
+    def number_of_islands(n, m, cells):
+        ds = DisjointSet([i for i in range(n * m)])
+        visited = [[0 for _ in range(m)] for _ in range(n)]
+        result = []
+        for cell in cells:
+            node, x, y = Solution._get_node(cell, m)
+            visited[x][y] = 1
+
+            if 0 <= x - 1 < n and visited[x - 1][y]:
+                adj_node, _, _ = Solution._get_node((x - 1, y), m)
+                if not ds.in_same_components(node, adj_node):
+                    ds.union(node, adj_node)
+
+            elif 0 <= y + 1 < m and visited[x][y + 1]:
+                adj_node, _, _ = Solution._get_node((x, y + 1), m)
+                if not ds.in_same_components(node, adj_node):
+                    ds.union(node, adj_node)
+
+            elif 0 <= x + 1 < n and visited[x + 1][y]:
+                adj_node, _, _ = Solution._get_node((x + 1, y), m)
+                if not ds.in_same_components(node, adj_node):
+                    ds.union(node, adj_node)
+
+            elif 0 <= y - 1 < m and visited[x][y - 1]:
+                adj_node, _, _ = Solution._get_node((x, y - 1), m)
+                if not ds.in_same_components(node, adj_node):
+                    ds.union(node, adj_node)
+
+            connected_components = ds.get_num_components()
+            for i in range(n):
+                for j in range(m):
+                    if visited[i][j] == 0:
+                        connected_components -= 1
+            result.append(connected_components)
+        return result
+
+
+print(Solution.number_of_islands(4, 5, [(1, 1), (0, 1), (3, 3), (3, 4)]))
+print(Solution.number_of_islands(4, 5, [(0, 0), (1, 1), (2, 2), (3, 3)]))
