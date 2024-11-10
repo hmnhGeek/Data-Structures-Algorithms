6500f7b53cbec7a0cf9f716612828f36379b2ba9
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-10 14:05:11 +0530

    Minimum jumps in linear time.

--- Arrays/min_jumps.py
+++ Arrays/min_jumps.py
@@ -91,8 +91,47 @@ def tabulation():
     print(min_jumps([2, 3, 0, 1, 4]))
 
 
+def is_end_reachable(arr):
+    max_index_reachable = 0
+    n = len(arr)
+    for i in range(n):
+        if i <= max_index_reachable:
+            if arr[i] + i > max_index_reachable:
+                max_index_reachable = arr[i] + i
+        else:
+            return False
+    return max_index_reachable >= n - 1
+
+
+def min_jumps(arr):
+    if not is_end_reachable(arr):
+        return -1
+    jumps = 0
+    left, right = 0, 0
+    n = len(arr)
+    while right < n - 1:
+        farthest = 0
+        for index in range(left, right + 1):
+            farthest = max(farthest, index + arr[index])
+        left = right + 1
+        right = farthest
+        jumps += 1
+    return jumps
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
+
+print()
+print()
+print(min_jumps([1, 2, 3, 1, 1, 0, 2, 5]))
+print(min_jumps([1, 2, 4, 1, 1, 0, 2, 5]))
+print(min_jumps([1, 4, 3, 2, 6, 7]))
+print(min_jumps([1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9]))
+print(min_jumps([0, 10, 20]))
+print(min_jumps([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+print(min_jumps([2, 3, 1, 1, 4]))
+print(min_jumps([2, 3, 0, 1, 4]))
