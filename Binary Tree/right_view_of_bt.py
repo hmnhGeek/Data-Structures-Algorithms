e4cb0123eb2638613ba1037daf88895dd22f1c2e
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-10 17:35:06 +0530

    Get right view of the tree

--- Binary Tree/right_view_of_bt.py
+++ Binary Tree/right_view_of_bt.py
@@ -31,3 +31,65 @@ class Queue:
         self.length -= 1
         return item
 
+
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = self.right = None
+
+
+class Solution:
+    @staticmethod
+    def get_right_view_of_a_binary_tree(root: TreeNode):
+        level = 0
+        queue = Queue()
+        queue.push((root, level))
+        while not queue.is_empty():
+            node, node_level = queue.pop()
+            if level == node_level:
+                print(node.data, end=" ")
+                level += 1
+            if node.right is not None:
+                queue.push((node.right, node_level + 1))
+            if node.left is not None:
+                queue.push((node.left, node_level + 1))
+        print()
+
+
+# Example 1
+n1 = TreeNode(1)
+n2 = TreeNode(2)
+n3 = TreeNode(3)
+n4 = TreeNode(4)
+n5 = TreeNode(5)
+n6 = TreeNode(6)
+n7 = TreeNode(7)
+n8 = TreeNode(8)
+n1.left = n2
+n2.left = n4
+n3.left = n6
+n1.right = n3
+n3.right = n7
+n2.right = n5
+n4.right = n8
+Solution.get_right_view_of_a_binary_tree(n1)
+
+
+# Example 2
+n1, n2, n3 = TreeNode(1), TreeNode(2), TreeNode(3)
+n1.left = n3
+n1.right = n2
+Solution.get_right_view_of_a_binary_tree(n1)
+
+
+# Example 3
+n10 = TreeNode(10)
+n20 = TreeNode(20)
+n30 = TreeNode(30)
+n40 = TreeNode(40)
+n60 = TreeNode(60)
+n10.left = n20
+n20.left = n40
+n10.right = n30
+n20.right = n60
+Solution.get_right_view_of_a_binary_tree(n10)
