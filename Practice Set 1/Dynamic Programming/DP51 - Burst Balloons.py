26c101a8d44b66a93174a5c9b3bc3d08b8029fe0
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-13 16:02:08 +0530

    Burst balloons using tabulation

--- Practice Set 1/Dynamic Programming/DP51 - Burst Balloons.py
+++ Practice Set 1/Dynamic Programming/DP51 - Burst Balloons.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.naukri.com/code360/problems/mining-diamonds_4244494?source=youtube&campaign=striver_dp_videos
+# Solution - https://www.youtube.com/watch?v=Yz4LlDSlkns&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=52
+
+
 def recursive():
     """
         Time complexity is exponential and space complexity is O(n).
@@ -26,7 +30,7 @@ def recursive():
 
 def memoized():
     """
-        Time complexity is O(n^2) and space complexity is O(n + n^2).
+        Time complexity is O(n^3) and space complexity is O(n + n^2).
     """
 
     def solve(points, i, j, dp):
@@ -54,6 +58,33 @@ def memoized():
     print(burst_balloons([1, 5, 2, 8]))
 
 
+def tabulation():
+    """
+        Time complexity is O(n^3) and space complexity is O(n^2).
+    """
+    def burst_balloons(arr):
+        n = len(arr)
+        points = [1] + arr + [1]
+        dp = {i: {j: 0 for j in range(n + 2)} for i in range(n + 2)}
+        for i in range(n, 0, -1):
+            for j in range(i, n + 1):
+                maxi = 0
+                for k in range(i, j + 1):
+                    earnings = points[i - 1] * points[k] * points[j + 1] + dp[i][k - 1] + dp[k + 1][j]
+                    maxi = max(maxi, earnings)
+                dp[i][j] = maxi
+
+        return dp[1][n]
+
+    print(burst_balloons([3, 1, 5, 8]))
+    print(burst_balloons([7, 1, 8]))
+    print(burst_balloons([9, 1]))
+    print(burst_balloons([1, 2, 3, 4, 5]))
+    print(burst_balloons([1, 5, 2, 8]))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
