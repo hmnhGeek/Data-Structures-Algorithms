c42e8f91ee513862145c0940100615520e8873f1
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-13 15:59:18 +0530

    Burst balloons using memoization

--- Practice Set 1/Dynamic Programming/DP51 - Burst Balloons.py
+++ Practice Set 1/Dynamic Programming/DP51 - Burst Balloons.py
@@ -1,4 +1,8 @@
 def recursive():
+    """
+        Time complexity is exponential and space complexity is O(n).
+    """
+
     def solve(points, i, j):
         if i > j:
             return 0
@@ -20,4 +24,36 @@ def recursive():
     print(burst_balloons([1, 5, 2, 8]))
 
 
+def memoized():
+    """
+        Time complexity is O(n^2) and space complexity is O(n + n^2).
+    """
+
+    def solve(points, i, j, dp):
+        if i > j:
+            return 0
+        if dp[i][j] is not None:
+            return dp[i][j]
+        maxi = 0
+        for k in range(i, j + 1):
+            earnings = points[i - 1] * points[k] * points[j + 1] + solve(points, i, k - 1, dp) + solve(points, k + 1, j, dp)
+            maxi = max(maxi, earnings)
+        dp[i][j] = maxi
+        return maxi
+
+    def burst_balloons(arr):
+        n = len(arr)
+        points = [1] + arr + [1]
+        dp = {i: {j: None for j in range(n + 1)} for i in range(n + 1)}
+        return solve(points, 1, len(arr), dp)
+
+    print(burst_balloons([3, 1, 5, 8]))
+    print(burst_balloons([7, 1, 8]))
+    print(burst_balloons([9, 1]))
+    print(burst_balloons([1, 2, 3, 4, 5]))
+    print(burst_balloons([1, 5, 2, 8]))
+
+
 recursive()
+print()
+memoized()
