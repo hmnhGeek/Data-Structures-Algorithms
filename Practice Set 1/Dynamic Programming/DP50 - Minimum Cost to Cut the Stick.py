d4f89a023a1d2b8a7e48ca7310e8173677e7f97a
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-13 12:45:42 +0530

    Minimum cost to cut the stick - tabulation

--- Practice Set 1/Dynamic Programming/DP50 - Minimum Cost to Cut the Stick.py
+++ Practice Set 1/Dynamic Programming/DP50 - Minimum Cost to Cut the Stick.py
@@ -56,6 +56,35 @@ def memoized():
     print(get_min_cost_to_cut([1, 3], 10))
 
 
+def tabulation():
+    """
+        Time complexity is O(n^3) and space complexity is O(n^2), where n is the length of the cuts array.
+    """
+    def get_min_cost_to_cut(cut_coordinates, length_of_rod):
+        cut_coordinates.sort()
+        rod = [0,] + cut_coordinates + [length_of_rod,]
+        dp = {i: {j: 0 for j in range(len(cut_coordinates) + 2)} for i in range(len(cut_coordinates) + 2)}
+
+        for i in range(len(cut_coordinates), 0, -1):
+            for j in range(i, len(cut_coordinates) + 1):
+                min_cost = 1e6
+                # will take additional n iterations.
+                for k in range(i, j + 1):
+                    cost = rod[j + 1] - rod[i - 1] + dp[i][k - 1] + dp[k + 1][j]
+                    min_cost = min(min_cost, cost)
+                dp[i][j] = min_cost
+
+        return dp[1][len(cut_coordinates)]
+
+    print(get_min_cost_to_cut([1, 3, 4, 5], 7))
+    print(get_min_cost_to_cut([1, 3], 4))
+    print(get_min_cost_to_cut([1, 3, 4], 5))
+    print(get_min_cost_to_cut([1, 3, 4, 7], 10))
+    print(get_min_cost_to_cut([1, 3], 10))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
