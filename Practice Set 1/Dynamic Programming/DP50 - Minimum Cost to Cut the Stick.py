910385ef3791f7f323e220723c9a4d37a8686242
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-13 12:20:48 +0530

    Minimum cost to cut the stick - memoized

--- Practice Set 1/Dynamic Programming/DP50 - Minimum Cost to Cut the Stick.py
+++ Practice Set 1/Dynamic Programming/DP50 - Minimum Cost to Cut the Stick.py
@@ -24,4 +24,38 @@ def recursive():
     print(get_min_cost_to_cut([1, 3], 10))
 
 
+def memoized():
+    """
+        Time complexity is O(n^3) and space complexity is O(n + n^2), where n is the length of the cuts array.
+    """
+
+    def solve(rod, i, j, dp):
+        if i > j:
+            return 0
+
+        if dp[i][j] is not None:
+            return dp[i][j]
+        min_cost = 1e6
+        # will take additional n iterations.
+        for k in range(i, j + 1):
+            cost = rod[j + 1] - rod[i - 1] + solve(rod, i, k - 1, dp) + solve(rod, k + 1, j, dp)
+            min_cost = min(min_cost, cost)
+        dp[i][j] = min_cost
+        return min_cost
+
+    def get_min_cost_to_cut(cut_coordinates, length_of_rod):
+        cut_coordinates.sort()
+        rod = [0,] + cut_coordinates + [length_of_rod,]
+        dp = {i: {j: None for j in range(len(rod))} for i in range(len(rod))}
+        return solve(rod, 1, len(cut_coordinates), dp)
+
+    print(get_min_cost_to_cut([1, 3, 4, 5], 7))
+    print(get_min_cost_to_cut([1, 3], 4))
+    print(get_min_cost_to_cut([1, 3, 4], 5))
+    print(get_min_cost_to_cut([1, 3, 4, 7], 10))
+    print(get_min_cost_to_cut([1, 3], 10))
+
+
 recursive()
+print()
+memoized()
