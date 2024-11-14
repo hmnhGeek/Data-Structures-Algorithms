8342b1aece2a323b78c2fc2ea57cce5bfe8a03b7
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-14 18:39:05 +0530

    Check if a binary tree is balanced or not

--- Binary Tree/check_balanced_bt.py
+++ Binary Tree/check_balanced_bt.py
@@ -1,3 +1,6 @@
+# Video - https://www.youtube.com/watch?v=Yt50Jfbd8Po&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=16
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -18,19 +21,35 @@ class BalancedStatus:
 class Solution:
     @staticmethod
     def _update_heights(root: Node, balanced_object: BalancedStatus):
+        # if the node is null, return 0 height.
         if root is None:
             return 0
+
+        # get left and right height from the subtrees.
         left_ht = Solution._update_heights(root.left, balanced_object)
         right_ht = Solution._update_heights(root.right, balanced_object)
+
+        # if the difference in heights is more than 1, set balanced object to false, implying that the
+        # tree is not balanced.
         if abs(left_ht - right_ht) > 1:
             balanced_object.set_is_balanced(False)
+
+        # continue with same evaluation of the height at root node.
         height_at_root = 1 + max(left_ht, right_ht)
         return height_at_root
 
     @staticmethod
     def check_if_balanced(root: Node):
+        """
+            Time complexity is O(n) and space complexity is O(n).
+        """
+
+        # initialize an object which stores the current status of the binary tree being balanced as True.
+        # Since in Python, objects are passed by reference, we can set its value any time in the recursion.
         balance_object = BalancedStatus(True)
+        # calculate the height of each node in O(n) time and O(n) space.
         Solution._update_heights(root, balance_object)
+        # return the balanced status of the binary tree.
         return balance_object.get_is_balanced()
 
 
