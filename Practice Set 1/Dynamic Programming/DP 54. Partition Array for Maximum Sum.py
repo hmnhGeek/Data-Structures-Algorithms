43ab3aa95b519aaf762a3814343e12bedf2c28cd
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-14 14:27:11 +0530

    Partition for max sum - tabulation

--- Practice Set 1/Dynamic Programming/DP 54. Partition Array for Maximum Sum.py
+++ Practice Set 1/Dynamic Programming/DP 54. Partition Array for Maximum Sum.py
@@ -1,3 +1,7 @@
+# Problem link - https://leetcode.com/problems/partition-array-for-maximum-sum/description/
+# Solution - https://www.youtube.com/watch?v=PhWWJmaKfMc&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=55
+
+
 def recursive():
     """
         Time complexity is exponential and space complexity is O(n).
@@ -54,6 +58,33 @@ def memoized():
     print(partition_for_max_sum([1], 1))
 
 
+def tabulation():
+    """
+        Time complexity is O(n^2) and space complexity is O(n).
+    """
+    def partition_for_max_sum(arr, k):
+        n = len(arr)
+        dp = {i: 0 for i in range(n + 1)}
+
+        for i in range(n - 1, -1, -1):
+            ans = -1e6
+            length = 0
+            max_elem = -1e6
+            for j in range(i, min(n, i + k)):
+                length += 1
+                max_elem = max(max_elem, arr[j])
+                s = (length * max_elem) + dp[j + 1]
+                ans = max(ans, s)
+            dp[i] = ans
+        return dp[0]
+
+    print(partition_for_max_sum([1, 15, 7, 9, 2, 5, 10], 3))
+    print(partition_for_max_sum([1,4,1,5,7,3,6,1,9,9,3], 4))
+    print(partition_for_max_sum([1], 1))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
