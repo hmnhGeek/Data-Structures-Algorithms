b338de64ea4a0f1293a2f811fe23d1f0772b0034
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-15 13:53:41 +0530

    Leetcode 287 using extra space

--- Arrays/find_the_duplicate_number_leetcode_287.py
+++ Arrays/find_the_duplicate_number_leetcode_287.py
@@ -1,3 +1,7 @@
+# Problem link - https://leetcode.com/problems/find-the-duplicate-number/description/
+# Solution - https://www.youtube.com/watch?v=hCIdhv7-5Ms
+
+
 class DuplicateFinder:
     @staticmethod
     def using_extra_space(arr):
@@ -20,7 +24,40 @@ class DuplicateFinder:
                 return i
         return -1
 
+    @staticmethod
+    def using_floyd_cycle_detection(arr):
+        """
+            This takes O(n) time and O(1) space.
+        """
+
+        # start with a slow pointer pointed at index arr[0] (not 0, but .next).
+        slow = arr[0]
+
+        # start with a fast pointer pointing at index arr[arr[0]] (.next.next).
+        fast = arr[arr[0]]
+
+        # typical Floyd Cycle Detection
+        while slow != fast:
+            # move slow one at a time.
+            slow = arr[slow]
+            # move fast two places at a time.
+            fast = arr[arr[fast]]
+
+        # reset slow to 0, once slow and fast match
+        slow = 0
+        while slow != fast:
+            # now move both pointers one at a time.
+            slow = arr[slow]
+            fast = arr[fast]
+
+        # once they again match, that's the index which represents the repeated integer.
+        return slow
+
 
 print(DuplicateFinder.using_extra_space([1, 3, 4, 2, 2]))
 print(DuplicateFinder.using_extra_space([3, 1, 3, 4, 2]))
 print(DuplicateFinder.using_extra_space([3, 3, 3, 3, 3]))
+print()
+print(DuplicateFinder.using_floyd_cycle_detection([3, 1, 3, 4, 2]))
+print(DuplicateFinder.using_floyd_cycle_detection([3, 3, 3, 3, 3]))
+print(DuplicateFinder.using_floyd_cycle_detection([1, 3, 4, 2, 2]))
