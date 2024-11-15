0b678e4cee6a8714d87f3b7adb15fcff45b21ecd
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-15 15:44:35 +0530

    Space Optimized Stickler's Thief

--- Searching and Sorting/stickler's_thief.py
+++ Searching and Sorting/stickler's_thief.py
@@ -1,3 +1,6 @@
+# Problem link - https://www.geeksforgeeks.org/find-maximum-possible-stolen-value-houses/
+
+
 def recursive():
     """
         Time complexity is O(2^n) and space complexity is O(n).
@@ -80,8 +83,36 @@ def tabulation():
     print(stickler([5, 3, 4, 11, 2]))
 
 
+def space_optimized():
+    """
+        Time complexity is O(n) and space complexity is O(1).
+    """
+    def stickler(arr):
+        n = len(arr)
+        prev1 = 0
+        prev2 = 0
+        for index in range(n):
+            # if thief wants to steal from `index`, skip the immediate next index.
+            left = arr[index] + prev2
+            # if the thief does not steal from current index, move to the next index.
+            right = prev1
+            # return the max loot.
+            curr = max(left, right)
+            prev2 = prev1
+            prev1 = curr
+        return prev1
+
+    print(stickler([6, 5, 5, 7, 4]))
+    print(stickler([1, 5, 3]))
+    print(stickler([4, 4, 4, 4]))
+    print(stickler([6, 7, 1, 3, 8, 2, 4]))
+    print(stickler([5, 3, 4, 11, 2]))
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
+print()
+space_optimized()
