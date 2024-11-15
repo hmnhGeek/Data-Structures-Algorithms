d8b56f33fe0b10a0acedd843d78460705f47327d
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-15 19:21:52 +0530

    Merge k sorted linked lists

--- Practice Set 1/Heap/merge_k_sorted_linked_lists.py
+++ Practice Set 1/Heap/merge_k_sorted_linked_lists.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -33,20 +36,20 @@ class MinHeap:
         self.heap = []
 
     def is_empty(self):
-        return len(self.heap)
+        return len(self.heap) == 0
 
     def get_lci(self, pi):
-        lci = 2*pi + 1
+        lci = 2 * pi + 1
         return lci if lci in range(len(self.heap)) else None
 
     def get_rci(self, pi):
-        rci = 2*pi + 2
+        rci = 2 * pi + 2
         return rci if rci in range(len(self.heap)) else None
 
     def get_pi(self, ci):
         if ci == 0:
             return
-        pi = int((ci - 1)/2)
+        pi = int((ci - 1) / 2)
         return pi if pi in range(len(self.heap)) else None
 
     def get_min_child_index(self, lci, rci):
@@ -93,3 +96,61 @@ class MinHeap:
         self.min_heapify_down(0)
         return item
 
+
+class Solution:
+    @staticmethod
+    def merge_sorted_linked_lists(linked_lists: List[LinkedList]) -> LinkedList:
+        min_heap = MinHeap()
+        dummy_node = starter = Node(-1)
+        for linked_list in linked_lists:
+            min_heap.insert(linked_list.head)
+
+        while not min_heap.is_empty():
+            node = min_heap.pop()
+            starter.next = node
+            if node.next is not None:
+                min_heap.insert(node.next)
+            node.next = None
+            starter = node
+
+        result = LinkedList()
+        result.head = dummy_node.next
+        result.tail = starter
+        return result
+
+
+def test(lists: List[List[int]]):
+    input_linked_lists = []
+    for l in lists:
+        ll = LinkedList()
+        ll._build(l)
+        input_linked_lists.append(ll)
+    print("Input linked lists")
+    for ll in input_linked_lists:
+        ll.show()
+    print()
+    merged_linked_list = Solution.merge_sorted_linked_lists(input_linked_lists)
+    print("Merged linked list")
+    merged_linked_list.show()
+    print()
+    print("-----------------------------")
+
+
+test(
+    [
+        [1, 2, 3],
+        [4, 5],
+        [5, 6],
+        [7, 8]
+    ]
+)
+
+test(
+    [
+        [1, 3],
+        [4, 5, 6],
+        [8]
+    ]
+)
+
+test([[1, 4, 5], [1, 3, 4], [2, 6]])
