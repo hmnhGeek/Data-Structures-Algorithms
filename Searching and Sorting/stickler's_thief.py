c3e3d94715637005334e4bd61f38bd7574f75d5b
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-15 15:42:18 +0530

    Tabulation Stickler's Thief

--- Searching and Sorting/stickler's_thief.py
+++ Searching and Sorting/stickler's_thief.py
@@ -46,8 +46,32 @@ def memoized():
 
     def stickler(arr):
         n = len(arr)
-        dp = {i: None for i in range(n + 1)}
+        dp = {i: None for i in range(n)}
         return solve(arr, n - 1, dp)
+
+    print(stickler([6, 5, 5, 7, 4]))
+    print(stickler([1, 5, 3]))
+    print(stickler([4, 4, 4, 4]))
+    print(stickler([6, 7, 1, 3, 8, 2, 4]))
+    print(stickler([5, 3, 4, 11, 2]))
+
+
+def tabulation():
+    """
+        Time complexity is O(n) and space complexity is O(n).
+    """
+    def stickler(arr):
+        n = len(arr)
+        dp = {i: 0 for i in range(n)}
+        dp[-1] = dp[-2] = 0
+        for index in range(n):
+            # if thief wants to steal from `index`, skip the immediate next index.
+            left = arr[index] + dp[index - 2]
+            # if the thief does not steal from current index, move to the next index.
+            right = dp[index - 1]
+            # return the max loot.
+            dp[index] = max(left, right)
+        return dp[n - 1]
 
     print(stickler([6, 5, 5, 7, 4]))
     print(stickler([1, 5, 3]))
@@ -59,3 +83,5 @@ def memoized():
 recursive()
 print()
 memoized()
+print()
+tabulation()
