f9cfbaf85b80066f356a02229c49546323c19bf2
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-16 13:41:50 +0530

    Find specific pair in matrix

--- Practice Set 1/Matrix/specific_pair_in_mtx.py
+++ Practice Set 1/Matrix/specific_pair_in_mtx.py
@@ -1,6 +1,14 @@
+# Problem link - https://www.naukri.com/code360/problems/find-a-specific-pair-in-the-matrix_1115467
+# Solution - https://www.youtube.com/watch?v=aUhR_T5J9is&t=1929s
+
+
 class Solution:
     @staticmethod
     def find_specific_pair(mtx):
+        """
+            Time complexity is O(n^2) and space complexity is O(n^2).
+        """
+
         n = len(mtx)
         max_mtx = [[-1e6 for _ in range(n)] for _ in range(n)]
         max_mtx[n - 1][n - 1] = mtx[n - 1][n - 1]
@@ -29,5 +37,36 @@ Solution.find_specific_pair(
         [3, 8, 6, 1, 3],
         [-4, -1, 1, 7, -6],
         [0, -4, 10, -5, 1]
+    ]
+)
+
+Solution.find_specific_pair(
+    [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+)
+
+Solution.find_specific_pair(
+    [
+        [-1, -2, -3],
+        [-4, -5, -6],
+        [-7, -8, -9]
+    ]
+)
+
+Solution.find_specific_pair(
+    [
+        [1, 5],
+        [4, 2]
+    ]
+)
+
+Solution.find_specific_pair(
+    [
+        [-1, 5, -3],
+        [-14, -5, -2],
+        [-7, 8, -9]
     ]
 )
