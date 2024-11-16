e33a7670f5d3b662a2afec96b4f8758a78e5b856
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-16 14:23:41 +0530

    Longest repeating subsequence using memoization - 1 based indexing

--- Strings/DP - Longest Repeating Subsequence.py
+++ Strings/DP - Longest Repeating Subsequence.py
@@ -33,14 +33,14 @@ def memoized():
             difference is the explicit check for index i = j.
         """
 
-        if i < 0 or j < 0:
+        if i == 0 or j == 0:
             return 0
 
         if dp[i][j] is not None:
             return dp[i][j]
 
         # if the characters match at indices i and j and i != j, then we can consume these indices in the subsequence.
-        if string[i] == string[j] and i != j:
+        if string[i - 1] == string[j - 1] and i != j:
             dp[i][j] = 1 + solve(string, i - 1, j - 1, dp)
         else:
             dp[i][j] = max(solve(string, i - 1, j, dp), solve(string, i, j - 1, dp))
@@ -48,8 +48,8 @@ def memoized():
 
     def get_longest_repeating_subsequence(string: str):
         n = len(string)
-        dp = {i: {j: None for j in range(n)} for i in range(n)}
-        return solve(string, n - 1, n - 1, dp)
+        dp = {i: {j: None for j in range(n + 1)} for i in range(n + 1)}
+        return solve(string, n, n, dp)
 
     print(get_longest_repeating_subsequence("axxxy"))
     print(get_longest_repeating_subsequence("axxzxy"))
