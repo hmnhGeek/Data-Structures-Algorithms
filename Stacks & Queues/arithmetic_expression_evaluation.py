d50fa0202a1a6bf8f812025d91131453e6af08eb
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-16 20:09:22 +0530

    Arithmetic Expression Evaluation

--- Stacks & Queues/arithmetic_expression_evaluation.py
+++ Stacks & Queues/arithmetic_expression_evaluation.py
@@ -1,3 +1,8 @@
+# Problem link - https://www.naukri.com/code360/problems/arithmetic-expression-evaluation_1170517
+# Solution (infix to postfix) - https://www.youtube.com/watch?v=_PU5t-gk_B4
+# Solution (evaluating postfix) - https://www.youtube.com/watch?v=m7SGekhd1mQ
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -43,35 +48,69 @@ class ExpressionEvaluator:
 
     @staticmethod
     def infix_to_postfix(infix_expression: str):
+        """
+            Time complexity is O(n) and space complexity is O(n).
+        """
+
         stack = Stack()
         n = len(infix_expression)
         postfix = ""
+
+        # iterate over each character in the infix expression.
         for i in range(n):
             elem = infix_expression[i]
+
+            # if the character is an operand (or a whitespace), add it to the postfix expression.
             if elem not in ExpressionEvaluator._brackets and elem not in ExpressionEvaluator._precedences:
                 postfix += elem
+
+            # else if the character is an opening bracket, then push it to the stack.
             elif elem == ExpressionEvaluator._brackets[0]:
                 stack.push(elem)
+
+            # else if the character is a closing bracket,
             elif elem == ExpressionEvaluator._brackets[1]:
+                # then continuously pop from the stack until you get the corresponding opening bracket.
                 while stack.top() != ExpressionEvaluator._brackets[0]:
+                    # and while popping, push the popped characters into the postfix expression.
                     postfix += stack.pop()
+                # pop the opening bracket.
                 stack.pop()
             else:
+                # finally, ensure that the top element is an operator and continuously pop from the stack unless the
+                # top operator has a lower precedence than the current character.
                 while stack.top() in ExpressionEvaluator._precedences and ExpressionEvaluator._precedences[stack.top()] >= ExpressionEvaluator._precedences[elem]:
                     postfix += stack.pop()
+                # once the top is either an operator with lower precedence or just an opening bracket, push the current
+                # character on the stack.
                 stack.push(elem)
+
+        # finally, if the expression got exhausted, pop from the stack until the stack gets empty.
         while not stack.is_empty():
             postfix += stack.pop()
+
+        # return the postfix expression.
         return postfix.strip()
 
     @staticmethod
     def evaluate_postfix(postfix: str):
+        """
+            Overall time complexity is O(n) and space complexity is O(n).
+        """
+
         stack = Stack()
+
+        # get a list of operators and operands.
         literals = postfix.split()
+
+        # loop on the literals in O(n) time.
         for literal in literals:
+            # if literal is an operand, push the integer version of it in the stack.
             if literal not in ExpressionEvaluator._precedences:
                 stack.push(int(literal))
             else:
+                # else, if literal is an operator, pop the top 2 elements from the stack and perform the operation.
+                # once operation is performed, push back the integer into the stack.
                 x = stack.pop()
                 y = stack.pop()
                 if literal == "+":
@@ -84,11 +123,19 @@ class ExpressionEvaluator:
                     stack.push(y // x)
                 elif literal == "^":
                     stack.push(y ** x)
+
+        # at last, there will be only one element in the stack which would be the final result.
         return stack.pop()
 
     @staticmethod
     def evaluate(expression):
+        """
+            Overall time complexity is O(n) and space complexity is O(n).
+        """
+
+        # get the corresponding postfix expression in O(n) time and O(n) space.
         postfix = ExpressionEvaluator.infix_to_postfix(expression)
+        # return the evaluated postfix expression in O(n) time and O(n) space.
         return ExpressionEvaluator.evaluate_postfix(postfix)
 
 
