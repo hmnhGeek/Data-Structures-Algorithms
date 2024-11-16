c0b1d5c5b8d3545d8a030ec9c6aaff60cba52070
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-16 21:11:31 +0530

    Smallest range in K lists

--- Heap/Smallest range in K lists.py
+++ Heap/Smallest range in K lists.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/find-smallest-range-containing-elements-from-k-lists/1
+# Solution - https://www.youtube.com/watch?v=0IqFMBatlhU
+
+
 class MinHeap:
     def __init__(self):
         self.heap = []
@@ -67,22 +71,50 @@ class MinHeap:
 class Solution:
     @staticmethod
     def get_min_range(lists):
+        """
+            Time complexity is O(n*k*log(k)) and space complexity is O(k) for the min heap.
+        """
+
         min_heap = MinHeap()
+
+        # assume the range to be from -inf to inf
         result = [-1e6, 1e6]
+
+        # take a max element with initial value as -inf.
         max_elem = -1e6
+
+        # loop on the k lists and push their 0th elements into the min heap. This will take O(log(k)) time.
         for i in range(len(lists)):
             min_heap.insert((lists[i][0], i, 0))
+            # while pushing, ensure that you get the maximum element out of the 0th elements of these k lists.
+            # minimum will be given by min heap later.
             max_elem = max(max_elem, lists[i][0])
+
+        # run an infinite loop. This loop will run for all the elements from all the lists in worst case, i.e.,
+        # n*k times.
         while 1:
+            # pop the minimum element. This will take O(log(k)) time.
             min_elem, i, j = min_heap.pop()
+
+            # if the range max_elem - min_elem is less than current range (note that we did not include "="),
+            # then update the current range to this new shorter range. If the difference had been equal, then
+            # also we would keep the previous result only because as per the question, we need range with the smallest
+            # min_elem. That is, if there are two valid ranges [1, 6] and [10, 16], the question prefers one with
+            # the lower min_elem, i.e., [1, 6].
             if max_elem - min_elem < result[1] - result[0]:
                 result = [min_elem, max_elem]
 
+            # push the next element from the popped element list. This will take O(log(k)) time.
             if 0 <= j + 1 < len(lists[i]):
                 min_heap.insert((lists[i][j + 1], i, j + 1))
+                # ensure to update the max element by comparing the current max element with the new entered element.
                 max_elem = max(max_elem, lists[i][j + 1])
             else:
+                # if any of the lists get exhausted, break from the infinite loop. You now have the smallest range
+                # including at least one element from each sorted lists.
                 break
+
+        # return the result range.
         return result
 
 
