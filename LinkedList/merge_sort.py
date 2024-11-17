011b40cf97669e8b09f2f216677c6dd2c2a80bb0
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-17 15:17:47 +0530

    Merge sort on linked list

--- LinkedList/merge_sort.py
+++ LinkedList/merge_sort.py
@@ -38,3 +38,85 @@ class LinkedList:
             self.push(i)
 
 
+class Solution:
+    @staticmethod
+    def _merge(left_linked_list, right_linked_list):
+        curr1, curr2 = left_linked_list.head, right_linked_list.head
+        dummy_node = temp = Node(None)
+        while curr1 is not None and curr2 is not None:
+            if curr1.data <= curr2.data:
+                temp.next = curr1
+                temp = curr1
+                curr1 = curr1.next
+            else:
+                temp.next = curr2
+                temp = curr2
+                curr2 = curr2.next
+            temp.next = None
+        while curr1 is not None:
+            temp.next = curr1
+            temp = curr1
+            curr1 = curr1.next
+            temp.next = None
+        while curr2 is not None:
+            temp.next = curr2
+            temp = curr2
+            curr2 = curr2.next
+            temp.next = None
+        merged_linked_list = LinkedList()
+        merged_linked_list.head = dummy_node.next
+        merged_linked_list.tail = temp
+        return merged_linked_list
+
+    @staticmethod
+    def _merge_sort(linked_list: LinkedList):
+        if linked_list.head.next is None:
+            return linked_list
+
+        mid = linked_list.get_middle_node()
+        next_linked_list = LinkedList()
+        next_linked_list.head = mid.next
+        next_linked_list.tail = linked_list.tail
+        linked_list.tail = mid
+        mid.next = None
+
+        left_sorted_linked_list = Solution._merge_sort(linked_list)
+        right_sorted_linked_list = Solution._merge_sort(next_linked_list)
+        return Solution._merge(left_sorted_linked_list, right_sorted_linked_list)
+
+    @staticmethod
+    def sort(linked_list: LinkedList):
+        sorted = Solution._merge_sort(linked_list)
+        linked_list.head = sorted.head
+        linked_list.tail = sorted.tail
+
+
+l = LinkedList()
+l.build(3, 4, 2, 1, 5)
+l.show()
+Solution.sort(l)
+l.show()
+
+print()
+
+l = LinkedList()
+l.build(9, 15, 0)
+l.show()
+Solution.sort(l)
+l.show()
+
+print()
+
+l = LinkedList()
+l.build(40, 20, 60, 10, 50, 30)
+l.show()
+Solution.sort(l)
+l.show()
+
+print()
+
+l = LinkedList()
+l.build(9, 5, 2, 8)
+l.show()
+Solution.sort(l)
+l.show()
