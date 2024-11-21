4d9ff7dba44cb05f94cc79bb53df1e51e11e8bbc
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-21 12:44:21 +0530

    Implemented climbing stairs using tabulation

--- Practice Set 2/Dynamic Programming/DP2 - Climbing Stairs.py
+++ Practice Set 2/Dynamic Programming/DP2 - Climbing Stairs.py
@@ -49,6 +49,28 @@ def memoized():
     print(climbing_stairs(2))
 
 
+def tabulation():
+    """
+        Time complexity is O(n) and space complexity is O(n).
+    """
+    def climbing_stairs(n):
+        dp = {i: 0 for i in range(n + 1)}
+        dp[0] = 1
+        for i in range(1, n + 1):
+            left = dp[i - 1]
+            right = dp[i - 2] if i > 1 else 0
+            dp[i] = left + right
+        return dp[n]
+
+    print(climbing_stairs(3))
+    print(climbing_stairs(4))
+    print(climbing_stairs(5))
+    print(climbing_stairs(1))
+    print(climbing_stairs(2))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
