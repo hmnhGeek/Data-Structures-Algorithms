f20a36fe937ceddfc8e360a7148d413be5f6079a
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-21 12:42:32 +0530

    Implemented climbing stairs using memoization

--- Practice Set 2/Dynamic Programming/DP2 - Climbing Stairs.py
+++ Practice Set 2/Dynamic Programming/DP2 - Climbing Stairs.py
@@ -22,4 +22,33 @@ def recursive():
     print(climbing_stairs(2))
 
 
+def memoized():
+    """
+        Time complexity is O(n) and space complexity is O(2n).
+    """
+    def solve(i, dp):
+        if i < 0:
+            return 0
+        if i == 0:
+            return 1
+        if dp[i] is not None:
+            return dp[i]
+        left = solve(i - 1, dp)
+        right = solve(i - 2, dp)
+        dp[i] = left + right
+        return dp[i]
+
+    def climbing_stairs(n):
+        dp = {i: None for i in range(n + 1)}
+        return solve(n, dp)
+
+    print(climbing_stairs(3))
+    print(climbing_stairs(4))
+    print(climbing_stairs(5))
+    print(climbing_stairs(1))
+    print(climbing_stairs(2))
+
+
 recursive()
+print()
+memoized()
