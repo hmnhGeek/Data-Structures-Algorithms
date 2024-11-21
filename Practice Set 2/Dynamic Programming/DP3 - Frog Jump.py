50d924d6a4638265ec3b88940b9a7e131525a666
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-21 14:43:07 +0530

    Frog Jump - Memoized

--- Practice Set 2/Dynamic Programming/DP3 - Frog Jump.py
+++ Practice Set 2/Dynamic Programming/DP3 - Frog Jump.py
@@ -26,4 +26,39 @@ def recursive():
     print(frog_jump([30, 20, 50, 10, 40]))
 
 
+def memoized():
+    """
+        Time complexity is O(n) and space complexity is O(2n).
+    """
+
+    def solve(arr, index, dp):
+        if index == 0:
+            return 0
+
+        if dp[index] is not None:
+            return dp[index]
+
+        left = 1e6
+        if index - 1 >= 0:
+            left = abs(arr[index] - arr[index - 1]) + solve(arr, index - 1, dp)
+        right = 1e6
+        if index - 2 >= 0:
+            right = abs(arr[index] - arr[index - 2]) + solve(arr, index - 2, dp)
+        dp[index] = min(left, right)
+        return dp[index]
+
+    def frog_jump(arr):
+        n = len(arr)
+        dp = {i: None for i in range(n)}
+        return solve(arr, n - 1, dp)
+
+    print(frog_jump([10, 20, 30, 10]))
+    print(frog_jump([10, 50, 10]))
+    print(frog_jump([7, 4, 4, 2, 6, 6, 3, 4]))
+    print(frog_jump([4, 8, 3, 10, 4, 4]))
+    print(frog_jump([30, 20, 50, 10, 40]))
+
+
 recursive()
+print()
+memoized()
