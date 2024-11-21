7e5f2a93d647ce1df2c4d91c5f9804d9edaf0219
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-21 10:32:03 +0530

    Implemented Lower Bound

---
+++ Binary Search/BS2 - Implement Lower and Upper Bounds.py
@@ -0,0 +1,19 @@
+class Solution:
+    @staticmethod
+    def get_lower_bound(arr, x):
+        low, high = 0, len(arr) - 1
+        while low <= high:
+            mid = int(low + (high - low)/2)
+            if arr[mid] < x:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return low if 0 <= low < len(arr) else -1
+
+
+print(Solution.get_lower_bound([3, 5, 8, 15, 19], 5))
+print(Solution.get_lower_bound([3, 5, 8, 15, 19], 8))
+print(Solution.get_lower_bound([3, 5, 8, 15, 19], 9))
+print(Solution.get_lower_bound([3, 5, 8, 15, 19], 800))
+print(Solution.get_lower_bound([3, 5, 8, 15, 19], 2))
+print(Solution.get_lower_bound([1, 2, 2, 3, 3, 5], 0))
