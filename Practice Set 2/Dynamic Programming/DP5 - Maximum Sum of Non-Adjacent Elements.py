da46dfc3920ec0315f7d9d16381d3f3cf393b457
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-22 14:54:15 +0530

    Tabulation house robber

--- Practice Set 2/Dynamic Programming/DP5 - Maximum Sum of Non-Adjacent Elements.py
+++ Practice Set 2/Dynamic Programming/DP5 - Maximum Sum of Non-Adjacent Elements.py
@@ -53,6 +53,31 @@ def memoized():
     print(house_robber([1, 5, 2, 1, 6]))
 
 
+def tabulation():
+    """
+        Time complexity is O(n) and space complexity is O(n).
+    """
+    def house_robber(arr):
+        n = len(arr)
+        dp = {i: 0 for i in range(n)}
+        dp[-1] = dp[-2] = 0
+        for index in range(n):
+            left = arr[index] + dp[index - 2]
+            right = dp[index - 1]
+            dp[index] = max(left, right)
+        return dp[n - 1]
+
+    print(house_robber([2, 1, 4, 9]))
+    print(house_robber([1, 2, 4]))
+    print(house_robber([1, 2, 3, 5, 4]))
+    print(house_robber([1, 2, 3, 1, 3, 5, 8, 1, 9]))
+    print(house_robber([2,7,9,3,1]))
+    print(house_robber([1,2,3,1]))
+    print(house_robber([1, 5, 2, 1, 6]))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
