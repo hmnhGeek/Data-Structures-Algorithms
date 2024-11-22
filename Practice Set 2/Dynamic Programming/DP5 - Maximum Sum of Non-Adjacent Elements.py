1bb2aaed13d009b7b0af522a70dab900970f46d9
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-22 14:52:42 +0530

    Memoized house robber

--- Practice Set 2/Dynamic Programming/DP5 - Maximum Sum of Non-Adjacent Elements.py
+++ Practice Set 2/Dynamic Programming/DP5 - Maximum Sum of Non-Adjacent Elements.py
@@ -23,4 +23,36 @@ def recursive():
     print(house_robber([1, 5, 2, 1, 6]))
 
 
+def memoized():
+    """
+        Time complexity is O(n) and space complexity is O(2n).
+    """
+
+    def solve(arr, index, dp):
+        if index < 0:
+            return 0
+        if dp[index] is not None:
+            return dp[index]
+        left = arr[index] + solve(arr, index - 2, dp)
+        right = solve(arr, index - 1, dp)
+        dp[index] = max(left, right)
+        return dp[index]
+
+    def house_robber(arr):
+        n = len(arr)
+        dp = {i: None for i in range(n)}
+        dp[-1] = dp[-2] = None
+        return solve(arr, n - 1, dp)
+
+    print(house_robber([2, 1, 4, 9]))
+    print(house_robber([1, 2, 4]))
+    print(house_robber([1, 2, 3, 5, 4]))
+    print(house_robber([1, 2, 3, 1, 3, 5, 8, 1, 9]))
+    print(house_robber([2,7,9,3,1]))
+    print(house_robber([1,2,3,1]))
+    print(house_robber([1, 5, 2, 1, 6]))
+
+
 recursive()
+print()
+memoized()
