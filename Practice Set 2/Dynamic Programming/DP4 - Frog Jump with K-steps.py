a5eb2939bc80fc78af6363fd33332f1039f31d45
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-22 12:08:26 +0530

    Frog jump with at most k steps using tabulation

--- Practice Set 2/Dynamic Programming/DP4 - Frog Jump with K-steps.py
+++ Practice Set 2/Dynamic Programming/DP4 - Frog Jump with K-steps.py
@@ -61,6 +61,35 @@ def memoized():
     print(frog_jump([40, 10, 20, 70, 80, 10, 20, 70, 80, 60], 4))
 
 
+def tabulation():
+    """
+        Time complexity is O(n*k) and space complexity is O(n).
+    """
+    def frog_jump(arr, k):
+        if k <= 0:
+            return
+        n = len(arr)
+        dp = {i: 1e6 for i in range(n)}
+        dp[0] = 0
+        for index in range(1, n):
+            min_energy = 1e6
+            for i in range(1, k + 1):
+                if index - i >= 0:
+                    min_energy = min(min_energy, abs(arr[index] - arr[index - i]) + dp[index - i])
+            dp[index] = min_energy
+        return dp[n - 1]
+
+    print(frog_jump([10, 30, 50, 60, 20, 10], 3))
+    print(frog_jump([10, 30, 50, 60, 20, 10], 2))
+    print(frog_jump([10, 30, 50, 60, 20, 10], 4))
+    print(frog_jump([10, 30, 50, 60, 20, 10], 1))
+    print(frog_jump([10, 30, 40, 50, 20], 3))
+    print(frog_jump([10, 20, 10], 1))
+    print(frog_jump([40, 10, 20, 70, 80, 10, 20, 70, 80, 60], 4))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
