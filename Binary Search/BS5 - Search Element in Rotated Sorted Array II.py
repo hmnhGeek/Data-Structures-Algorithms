c9de3a37ad01e5a432b015b19d4b9435fe30caac
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-22 12:35:30 +0530

    Search element in rotated sorted array - II

---
+++ Binary Search/BS5 - Search Element in Rotated Sorted Array II.py
@@ -0,0 +1,30 @@
+class Solution:
+    @staticmethod
+    def search(arr, x):
+        low, high = 0, len(arr) - 1
+        while low <= high:
+            mid = int(low + (high - low)/2)
+            if arr[mid] == x:
+                return mid
+            if arr[low] == arr[mid] == arr[high]:
+                low += 1
+                high -= 1
+            if arr[mid] < arr[high]:
+                if arr[mid] < x <= arr[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            else:
+                if arr[low] <= x < arr[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+        return -1
+
+
+print(Solution.search([7, 8, 1, 2, 3, 3, 3, 4, 5, 6], 3))
+print(Solution.search([3, 4, 5, 0, 0, 1, 2], 4))
+print(Solution.search([31, 44, 56, 0, 10, 13], 47))
+print(Solution.search([3, 3, 3, 1, 3, 3], 1))
+print(Solution.search([3, 3, 3, 1, 3, 3], 3))
+print(Solution.search([3, 1, 2, 3, 3, 3, 3], 1))
