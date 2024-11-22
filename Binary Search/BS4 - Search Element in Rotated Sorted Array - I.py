5e3b066eac9e417a1d0ec661e6400549a3b1c8e0
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-22 10:21:14 +0530

    Search in a rotated sorted array with unique elements only.

---
+++ Binary Search/BS4 - Search Element in Rotated Sorted Array - I.py
@@ -0,0 +1,28 @@
+class Solution:
+    @staticmethod
+    def search_in_rotated_sorted(arr, x):
+        low, high = 0, len(arr) - 1
+        while low <= high:
+            mid = int(low + (high - low)/2)
+            if arr[mid] == x:
+                return mid
+            if arr[mid] < arr[high]:
+                if arr[mid] < x <= arr[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            else:
+                if arr[low] <= x < arr[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+        return -1
+
+
+def test(arr):
+    for i in arr:
+        print(f"{i} found at index = {Solution.search_in_rotated_sorted(arr, i)}")
+    print()
+
+
+test([7, 8, 9, 1, 2, 3, 4, 5, 6])
