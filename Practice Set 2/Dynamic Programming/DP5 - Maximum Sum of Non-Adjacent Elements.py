007be4a86150c85a03ea6897c3f74e2ca852330d
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-22 14:49:56 +0530

    Recursive house robber

---
+++ Practice Set 2/Dynamic Programming/DP5 - Maximum Sum of Non-Adjacent Elements.py
@@ -0,0 +1,26 @@
+def recursive():
+    """
+        Time complexity is O(2^n) and space complexity is O(n).
+    """
+
+    def solve(arr, index):
+        if index < 0:
+            return 0
+        left = arr[index] + solve(arr, index - 2)
+        right = solve(arr, index - 1)
+        return max(left, right)
+
+    def house_robber(arr):
+        n = len(arr)
+        return solve(arr, n - 1)
+
+    print(house_robber([2, 1, 4, 9]))
+    print(house_robber([1, 2, 4]))
+    print(house_robber([1, 2, 3, 5, 4]))
+    print(house_robber([1, 2, 3, 1, 3, 5, 8, 1, 9]))
+    print(house_robber([2,7,9,3,1]))
+    print(house_robber([1,2,3,1]))
+    print(house_robber([1, 5, 2, 1, 6]))
+
+
+recursive()
