edb2b499959032dbd3e596734cf698afab9cd7aa
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-22 14:36:20 +0530

    Number of islands using DFS

--- Practice Set 2/Graphs/G8 - Number of Islands.py
+++ Practice Set 2/Graphs/G8 - Number of Islands.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/find-the-number-of-islands/1
+# Solution - https://www.youtube.com/watch?v=muncqlKJrH0&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=8
+
+
 class Solution:
     @staticmethod
     def _get_valid_neighbours(mtx, i, j, n, m):
@@ -22,8 +26,11 @@ class Solution:
 
     @staticmethod
     def _dfs(mtx, i, j, n, m, visited):
+        # mark the current cell as visited
         visited[i][j] = True
+        # get all non-zero neighbouring cells of cell (i, j) from 8 directions in O(1) time.
         neighbours = Solution._get_valid_neighbours(mtx, i, j, n, m)
+        # perform DFS on those neighbours which are not visited.
         for neighbour in neighbours:
             x, y = neighbour
             if not visited[x][y]:
@@ -31,14 +38,26 @@ class Solution:
 
     @staticmethod
     def get_num_islands(mtx):
+        """
+            Overall time complexity is O(n*m) and space complexity is O(n*m) for visited matrix.
+        """
+
+        # get the dimensions of the matrix
         n, m = len(mtx), len(mtx[0])
+        # store the count of the islands as 0.
         num_islands = 0
+        # create a visited matrix.
         visited = [[False for _ in range(m)] for _ in range(n)]
+        # loop on the matrix in O(n*m).
         for i in range(n):
             for j in range(m):
+                # if the current cell is 1 and is not visited, we have found a starting cell for an island.
                 if not visited[i][j] and mtx[i][j] == 1:
+                    # increment the count of founded islands.
                     num_islands += 1
+                    # perform DFS on (i, j) cell.
                     Solution._dfs(mtx, i, j, n, m, visited)
+        # return the number of islands.
         return num_islands
 
 
