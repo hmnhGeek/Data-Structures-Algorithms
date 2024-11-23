7af03d3e219699dac39518dd8130fd361aa43ce4
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-23 13:45:52 +0530

    Merge Intervals using Binary Search

--- Arrays/merge_overlapping_intervals.py
+++ Arrays/merge_overlapping_intervals.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.naukri.com/code360/problems/merge-overlapping-intervals_1082151
+# Solution - https://www.youtube.com/watch?v=IexN60k62jo&t=306s
+
+
 from typing import List
 
 
@@ -16,53 +20,88 @@ class Interval:
         return f"({self.start}, {self.end})"
 
 
-class Solution:
+class MySolution:
     @staticmethod
-    def _merge(intervals, interval, index, merged_intervals):
+    def _merge(intervals, interval, index):
+        # define the search space.
         low = index
         high = len(intervals) - 1
+
+        # start the binary search
         while low <= high:
             mid = int(low + (high - low) / 2)
             target_interval = intervals[mid]
+
+            # if the overlap is possible, then we can search for intervals to the right. Who knows, we can get even
+            # higher end time to maximize the range.
             if interval.get_end_time() >= target_interval.get_start_time():
                 low = mid + 1
             else:
+                # if the overlap is not possible, maybe we can decrease the search space to the left side as lower
+                # intervals can have some overlap.
                 high = mid - 1
+
+        # at the end, `high` will point to a possible overlapping interval.
         return high
 
     @staticmethod
     def merge_intervals(intervals: List[Interval]):
+        """
+            Time complexity is O(n*log(n)) and space complexity is O(n) for the merged array in the worst case.
+        """
+
+        # This will take O(n*log(n)) time
         intervals.sort(key=lambda x: x.start)
+        # store a merged array for the merged intervals.
         merged_intervals = []
+        # start with current interval as 0th interval.
         interval = intervals[0]
+        # start checking for overlapping intervals from 1st index till end.
         idx = 1
+
+        # assuming that in the worst case it runs for about all the intervals, i.e., `n`, hence overall time complexity
+        # of the while loop will be O(n*log(n)).
         while idx < len(intervals):
-            mid = Solution._merge(intervals, interval, idx, merged_intervals)
-            if mid < idx:
+            # find the index of an interval which is overlapping with the interval `interval`. This will take O(log(n))
+            # time due to a binary search operation.
+            mid = MySolution._merge(intervals, interval, idx)
+
+            # if the `mid` index is on the left side of the search space, you can't merge it as it was already merged
+            # in some previous iterations. This means that the next interval to `mid` (a non-overlapping interval)
+            # should be added to our merged intervals. Do that!
+            if mid < idx and 0 <= mid + 1 < len(intervals):
                 merged_intervals.append(intervals[mid + 1])
+                # update the current interval `interval` to this non-overlapping interval for the next iteration.
                 interval = intervals[mid + 1]
             else:
+                # however, if a `mid` indexed interval is found which is overlapping with `interval`, then merge the
+                # intervals into a merged_interval variable.
                 target_interval = intervals[mid]
-                if interval.get_end_time() >= target_interval.get_start_time():
-                    merged_interval = Interval(
-                        min(interval.get_start_time(), target_interval.get_start_time()),
-                        max(interval.get_end_time(), target_interval.get_end_time())
-                    )
-                    if len(merged_intervals) > 0:
-                        merged_intervals.pop(-1)
-                    merged_intervals.append(merged_interval)
-                    interval = merged_interval
-                else:
-                    merged_intervals.append(target_interval)
-                    interval = target_interval
+                merged_interval = Interval(
+                    min(interval.get_start_time(), target_interval.get_start_time()),
+                    max(interval.get_end_time(), target_interval.get_end_time())
+                )
+                # it is ensured that the last interval in the merged intervals is actually `interval`, which we have
+                # just merged with the target interval. Hence, we do not need this last interval in the merged
+                # intervals. So pop it.
+                if len(merged_intervals) > 0:
+                    merged_intervals.pop(-1)
+                # append the merged interval into merged intervals list.
+                merged_intervals.append(merged_interval)
+                # and as you can see, we have updated current interval `interval` to the last interval from the merged
+                # intervals.
+                interval = merged_interval
+            # finally, move `idx` to `mid + 1` because all the intervals till `mid` index will actually get merged in
+            # to the merged intervals.
             idx = mid + 1
 
+        # print the merged intervals.
         for merged_interval in merged_intervals:
             print(merged_interval, end=" ")
         print()
 
 
-Solution.merge_intervals([
+MySolution.merge_intervals([
     Interval(1, 4),
     Interval(3, 5),
     Interval(6, 8),
@@ -71,7 +110,7 @@ Solution.merge_intervals([
 ])
 
 print()
-Solution.merge_intervals(
+MySolution.merge_intervals(
     [
         Interval(1, 3),
         Interval(2, 7),
@@ -87,7 +126,7 @@ Solution.merge_intervals(
 )
 
 print()
-Solution.merge_intervals(
+MySolution.merge_intervals(
     [
         Interval(1, 2),
         Interval(4, 6),
