e164bf2023777f95e0c13ecba079a94089ae89cc
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-24 11:44:42 +0530

    Diagonal traversal of a binary tree using Queue

--- Binary Tree/diagonal_traversal.py
+++ Binary Tree/diagonal_traversal.py
@@ -71,3 +71,46 @@ n6.left = n4
 n6.right = n7
 n14.left = n13
 Solution.print_diagonal_traversal(n8)
+
+
+# Example 2
+n1, n2, n3, n4, n5, n6, n7, n8, n9 = TreeNode(1), TreeNode(2), TreeNode(3), TreeNode(4), TreeNode(5), TreeNode(6), TreeNode(7), TreeNode(8), TreeNode(9)
+n1.left = n2
+n1.right = n3
+n2.left = n4
+n2.right = n5
+n3.right = n6
+n4.left = n7
+n4.right = n8
+n6.left = n9
+Solution.print_diagonal_traversal(n1)
+
+
+# Example 3
+n1, n2, n3, n4, n5, n6 = TreeNode(1), TreeNode(2), TreeNode(3), TreeNode(4), TreeNode(5), TreeNode(6)
+n1.left = n2
+n1.right = n3
+n2.left = n4
+n3.left = n5
+n3.right = n6
+Solution.print_diagonal_traversal(n1)
+
+
+# Example 4
+n1, n2, n3, n4, n5, n6, n7 = TreeNode(1), TreeNode(2), TreeNode(3), TreeNode(4), TreeNode(5), TreeNode(6), TreeNode(7)
+n1.left = n2
+n1.right = n3
+n2.left = n4
+n2.right = n5
+n3.left = n6
+n3.right = n7
+Solution.print_diagonal_traversal(n1)
+
+
+# Example 5
+n1, n2, n3, n4, n5 = TreeNode(1), TreeNode(2), TreeNode(3), TreeNode(4), TreeNode(5)
+n1.left = n2
+n1.right = n3
+n3.left = n4
+n3.right = n5
+Solution.print_diagonal_traversal(n1)
