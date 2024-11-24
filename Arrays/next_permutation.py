5c9f01fc1d555154b19825818d0bc95ed3336a6b
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-24 09:40:41 +0530

    Next permutation using optimal approach

--- Arrays/next_permutation.py
+++ Arrays/next_permutation.py
@@ -11,7 +11,7 @@ class Solution:
     def _get_next_permutation(arr, index, n, permutations):
         if index == n:
             temp = [i for i in arr]
-            permutations.append(temp)
+            permutations.add(tuple(temp))
             return
         for i in range(index, n):
             arr[index], arr[i] = arr[i], arr[index]
@@ -22,13 +22,40 @@ class Solution:
     def get_next_permutation(arr):
         n = len(arr)
         number = Solution._get_numerical_representation(arr)
-        permutations = []
+        permutations = set()
         Solution._get_next_permutation(arr, 0, n, permutations)
-        permutations.sort(key=lambda x: Solution._get_numerical_representation(x))
-        index_of_arr = permutations.index(arr)
+        permutations = list(permutations)
+        permutations.sort()
+        index_of_arr = permutations.index(tuple(arr))
         if 0 <= index_of_arr + 1 < len(permutations):
-            return permutations[index_of_arr + 1]
-        return permutations[0]
+            return list(permutations[index_of_arr + 1])
+        return list(permutations[0])
+
+
+class OptimalSolution:
+    @staticmethod
+    def _reverse(arr, low, high):
+        while low <= high:
+            arr[low], arr[high] = arr[high], arr[low]
+            low += 1
+            high -= 1
+
+    @staticmethod
+    def get_next_permutation(arr):
+        n = len(arr)
+        breakpoint_index = None
+        for i in range(n - 2, -1, -1):
+            if arr[i] < arr[i + 1]:
+                breakpoint_index = i
+                break
+        if breakpoint_index is None:
+            OptimalSolution._reverse(arr, 0, n - 1)
+            return
+        for i in range(n - 1, breakpoint_index, -1):
+            if arr[i] > arr[breakpoint_index]:
+                arr[i], arr[breakpoint_index] = arr[breakpoint_index], arr[i]
+                break
+        OptimalSolution._reverse(arr, breakpoint_index + 1, n - 1)
 
 
 print(Solution.get_next_permutation([1, 2, 3]))
@@ -37,3 +64,18 @@ print(Solution.get_next_permutation([3, 2, 1]))
 print(Solution.get_next_permutation([2, 4, 1, 7, 5, 0]))
 print(Solution.get_next_permutation([3, 4, 2, 5, 1]))
 print(Solution.get_next_permutation([2, 3, 1, 5, 4]))
+print()
+
+
+def test(*args):
+    l = [i for i in args]
+    OptimalSolution.get_next_permutation(l)
+    print(l)
+
+
+test(1, 2, 3)
+test(1, 1, 5)
+test(3, 2, 1)
+test(2, 4, 1, 7, 5, 0)
+test(3, 4, 2, 5, 1)
+test(2, 3, 1, 5, 4)
