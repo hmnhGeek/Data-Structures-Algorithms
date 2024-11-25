895e392065430fbe0920ab6c789ac0a021f8ff79
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-25 07:52:45 +0530

    Minimum in a rotated sorted array

--- Binary Search/BS6 - Minimum in a rotated sorted array.py
+++ Binary Search/BS6 - Minimum in a rotated sorted array.py
@@ -5,16 +5,26 @@ class Solution:
         low, high = 0, n - 1
         while low <= high:
             mid = int(low + (high - low) / 2)
+
+            # if the previous element to mid is greater than mid, then mid is minimum.
             if 0 <= mid - 1 < n and arr[mid - 1] > arr[mid]:
                 return arr[mid]
+
+            # if the next element to mid is smaller than mid, then the next element, i.e., mid + 1 is minimum.
             if 0 <= mid + 1 < n and arr[mid + 1] < arr[mid]:
                 return arr[mid + 1]
+
+            # if left part is unsorted, minimum must lie here.
             if arr[low] > arr[mid]:
                 high = mid - 1
+
+            # else if the right part is unsorted, minimum must lie in right part.
             elif arr[high] < arr[mid]:
                 low = mid + 1
+
+            # if however, the entire space is sorted, then return arr[low] as minimum
             elif arr[low] <= arr[mid] <= arr[high]:
-                high = mid - 1
+                return arr[low]
         return arr[low] if low in range(n) else -1
 
 
@@ -24,3 +34,6 @@ print(Solution.min_in_uniques([3, 4, 5, 1, 2]))
 print(Solution.min_in_uniques([3, 4, 1, 2]))
 print(Solution.min_in_uniques([25, 30, 5, 10, 15, 20]))
 print(Solution.min_in_uniques([11, 13, 15, 17]))
+print(Solution.min_in_uniques([7, 8, 1, 2, 3, 4, 5, 6]))
+print(Solution.min_in_uniques([1, 2]))
+print(Solution.min_in_uniques([2, 1]))
