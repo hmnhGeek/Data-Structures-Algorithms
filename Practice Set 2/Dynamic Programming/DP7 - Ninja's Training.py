f917b076e64732ba8e653587c7fe7af4f1c39e88
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-25 11:26:14 +0530

    Recursive ninja training

---
+++ Practice Set 2/Dynamic Programming/DP7 - Ninja's Training.py
@@ -0,0 +1,54 @@
+def recursive():
+    """
+        Time complexity is exponential and space complexity is O(n).
+    """
+
+    def solve(arr, i, j, m):
+        if i == 0:
+            return arr[0][j]
+
+        max_pts = 0
+        for k in range(m):
+            if k != j:
+                max_pts = max(max_pts, solve(arr, i - 1, k, m))
+        return arr[i][j] + max_pts
+
+    def ninja_training(arr):
+        n = len(arr)
+        m = len(arr[0])
+        max_pts = 0
+        for i in range(m):
+            max_pts = max(max_pts, solve(arr, n - 1, i, m))
+        return max_pts
+
+    print(ninja_training([[1, 2, 5], [3, 1, 1], [3, 3, 3]]))
+    print(
+        ninja_training(
+            [
+                [10, 40, 70],
+                [20, 50, 80],
+                [30, 60, 90]
+            ]
+        )
+    )
+    print(
+        ninja_training(
+            [
+                [18, 11, 19],
+                [4, 13, 7],
+                [1, 8, 13]
+            ]
+        )
+    )
+
+    print(
+        ninja_training(
+            [
+                [10, 50, 1],
+                [5, 100, 11]
+            ]
+        )
+    )
+
+
+recursive()
