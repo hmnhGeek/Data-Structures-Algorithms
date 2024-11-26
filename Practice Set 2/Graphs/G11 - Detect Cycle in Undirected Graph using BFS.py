5074a6d352305b07ec0153c5151272536a78efc6
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-26 11:41:13 +0530

    Cycle detection in undirected graph using BFS

--- Practice Set 2/Graphs/G11 - Detect Cycle in Undirected Graph using BFS.py
+++ Practice Set 2/Graphs/G11 - Detect Cycle in Undirected Graph using BFS.py
@@ -32,3 +32,56 @@ class Queue:
         return item
 
 
+class Solution:
+    @staticmethod
+    def _detect_cycle(graph, node, visited):
+        queue = Queue()
+        queue.push(node)
+        while not queue.is_empty():
+            node = queue.pop()
+            if visited[node]:
+                return True
+            visited[node] = True
+            for adj_node in graph[node]:
+                if not visited[adj_node]:
+                    queue.push(adj_node)
+        return False
+
+    @staticmethod
+    def detect_cycle(graph):
+        visited = {i: False for i in graph}
+        for node in graph:
+            if not visited[node]:
+                component_has_cycle = Solution._detect_cycle(graph, node, visited)
+                if component_has_cycle:
+                    return True
+        return False
+
+
+print(
+    Solution.detect_cycle(
+        {
+            1: [2, 3],
+            2: [1, 5],
+            3: [1, 4, 6],
+            4: [3],
+            5: [2, 7],
+            6: [3, 7],
+            7: [5, 6]
+        }
+    )
+)
+
+print(
+    Solution.detect_cycle(
+        {
+            1: [2, 3],
+            2: [1, 5],
+            3: [1, 4, 6],
+            4: [3],
+            5: [2, 7],
+            6: [3],
+            7: [5]
+        }
+    )
+)
