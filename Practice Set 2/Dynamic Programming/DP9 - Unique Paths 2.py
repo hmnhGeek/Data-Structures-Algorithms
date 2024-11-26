01e8db0b15a9d0b5355049634f8a41ec5ae29cb3
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-26 12:07:11 +0530

    Tabulation grid paths with obstacles

--- Practice Set 2/Dynamic Programming/DP9 - Unique Paths 2.py
+++ Practice Set 2/Dynamic Programming/DP9 - Unique Paths 2.py
@@ -122,6 +122,95 @@ def memoized():
     )
 
 
+def tabulation():
+    """
+        Time complexity is O(mn) and space complexity is O(mn).
+    """
+    def unique_paths(mtx):
+        n, m = len(mtx), len(mtx[0])
+        dp = {i: {j: 0 for j in range(m)} for i in range(n)}
+
+        obstacle_found = False
+        for i in range(n):
+            if mtx[i][0] != 0:
+                obstacle_found = True
+                break
+        if obstacle_found:
+            for i in range(1, n):
+                dp[i][0] = 0
+        else:
+            for i in range(1, n):
+                dp[i][0] = 1
+
+        obstacle_found = False
+        for j in range(m):
+            if mtx[0][j] != 0:
+                obstacle_found = True
+                break
+        if obstacle_found:
+            for j in range(1, m):
+                dp[0][j] = 0
+        else:
+            for j in range(1, m):
+                dp[0][j] = 1
+
+        dp[0][0] = 1
+
+        for i in range(1, n):
+            for j in range(1, m):
+                if mtx[i][j] != 0:
+                    dp[i][j] = 0
+                else:
+                    left = 0
+                    if 0 <= i - 1 < n:
+                        left = dp[i - 1][j]
+                    right = 0
+                    if 0 <= j - 1 < m:
+                        right = dp[i][j - 1]
+                    dp[i][j] = left + right
+
+        return dp[n - 1][m - 1]
+
+    print(
+        unique_paths(
+            [
+                [0, 0, 0],
+                [0, -1, 0],
+                [0, 0, 0]
+            ]
+        )
+    )
+
+    print(
+        unique_paths(
+            [
+                [0, 0],
+                [0, 0]
+            ]
+        )
+    )
+
+    print(
+        unique_paths(
+            [
+                [0, -1],
+                [-1, 0]
+            ]
+        )
+    )
+
+    print(
+        unique_paths(
+            [
+                [0, -1],
+                [0, 0]
+            ]
+        )
+    )
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
