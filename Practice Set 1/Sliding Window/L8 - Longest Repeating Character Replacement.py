23e84bcd1f906af1934e2b9718c1a991dc8b7ca6
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-26 11:12:01 +0530

    Longest repeating character replacement

--- Practice Set 1/Sliding Window/L8 - Longest Repeating Character Replacement.py
+++ Practice Set 1/Sliding Window/L8 - Longest Repeating Character Replacement.py
@@ -5,21 +5,18 @@ class Solution:
         n = len(string)
         longest_length = 0
         start_index = 0
+        max_freq = 0
         d = {i: 0 for i in string}
         while right < n:
             d[string[right]] += 1
-            if sum(1 for v in d.values() if v != 0) <= 1:
-                longest_length = max(longest_length, right - left + 1)
-                start_index = left
-            elif k > 0:
-                min_freq_character = min(d, key=d.get)
-                d[min_freq_character] -= 1
-                k -= 1
-                longest_length = max(longest_length, right - left + 1)
-                start_index = left
-            else:
+            max_freq = max(max_freq, d[string[right]])
+            if right - left + 1 - max_freq > k:
                 d[string[left]] -= 1
                 left += 1
+                max_freq = max(d.values())
+            if right - left + 1 - max_freq <= k:
+                longest_length = max(longest_length, right - left + 1)
+                start_index = left
             right += 1
         return longest_length, string[start_index:start_index+longest_length]
 
@@ -29,3 +26,5 @@ print(Solution.longest_repeating_character_replacement("ABAB", 2))
 print(Solution.longest_repeating_character_replacement("ADBD", 1))
 print(Solution.longest_repeating_character_replacement("ABBA", 2))
 print(Solution.longest_repeating_character_replacement("AAABBCCD", 2))
+print(Solution.longest_repeating_character_replacement("ABABA", 2))
+print(Solution.longest_repeating_character_replacement("HHHHHH", 4))
