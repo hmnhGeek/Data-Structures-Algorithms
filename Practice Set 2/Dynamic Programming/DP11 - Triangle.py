bb657a22c1dcd2c4e2e1c9c55dcb538f6a41b001
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-27 14:54:18 +0530

    Memoized triangle minimum path sum problem

--- Practice Set 2/Dynamic Programming/DP11 - Triangle.py
+++ Practice Set 2/Dynamic Programming/DP11 - Triangle.py
@@ -90,4 +90,105 @@ def recursive():
     )
 
 
+def memoized():
+    """
+        Time complexity is O(m^2 *n) and space complexity is O(m + n + mn).
+    """
+
+    def solve(mtx, i, j, n, dp):
+        if i == 0 and j == 0:
+            return mtx[i][j]
+
+        if dp[i][j] is not None:
+            return dp[i][j]
+
+        up = 1e6
+        if 0 <= i - 1 < n and 0 <= j < len(mtx[i - 1]):
+            up = mtx[i][j] + solve(mtx, i - 1, j, n, dp)
+        diagonal = 1e6
+        if 0 <= i - 1 < n and 0 <= j - 1 < len(mtx[i - 1]):
+            diagonal = mtx[i][j] + solve(mtx, i - 1, j - 1, n, dp)
+        dp[i][j] = min(diagonal, up)
+        return dp[i][j]
+
+    def triangle(mtx):
+        n = len(mtx)
+        min_path_sum = 1e6
+        dp = {i: {j: None for j in range(len(mtx[i]))} for i in range(n)}
+
+        # O(m) time X O(mn) = O(m^2 *n)
+        for j in range(len(mtx[-1]) - 1, -1, -1):
+            min_path_sum = min(min_path_sum, solve(mtx, n - 1, j, n, dp))
+        return min_path_sum
+
+    print(
+        triangle(
+            [
+                [1],
+                [2, 3],
+                [3, 6, 7],
+                [8, 9, 6, 10]
+            ]
+        )
+    )
+
+    print(
+        triangle(
+            [
+                [2],
+                [3, 4],
+                [6, 5, 7],
+                [4, 1, 8, 3]
+            ]
+        )
+    )
+
+    print(triangle([[-10]]))
+
+    print(
+        triangle(
+            [
+                [1],
+                [2, 3],
+                [4, 5, 6],
+                [7, 8, 9, 10]
+            ]
+        )
+    )
+
+    print(
+        triangle(
+            [
+                [5],
+                [-1, 3],
+                [22, 1, -9]
+            ]
+        )
+    )
+
+    print(
+        triangle(
+            [
+                [2],
+                [3, 7],
+                [8, 5, 6],
+                [6, 1, 9, 3]
+            ]
+        )
+    )
+
+    print(
+        triangle(
+            [
+                [3],
+                [6, 9],
+                [8, 7, 1],
+                [9, 6, 8, 2]
+            ]
+        )
+    )
+
+
 recursive()
+print()
+memoized()
