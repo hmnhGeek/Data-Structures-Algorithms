d16be388d7f2fe5d26cdc2d67c1012ca14b5d78b
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-27 18:47:58 +0530

    Subarray with k different integers

--- Practice Set 1/Sliding Window/L11 - Subarray with k different integers.py
+++ Practice Set 1/Sliding Window/L11 - Subarray with k different integers.py
@@ -1,15 +1,31 @@
+"""
+    Problem link - https://leetcode.com/problems/subarrays-with-k-different-integers/description/
+    Solution - https://www.youtube.com/watch?v=7wYGbV_LsX4&list=PLgUwDviBIf0q7vrFA_HEWcqRqMpCXzYAL&index=11
+    Please refer to comments in file L9. The problem is exactly same to that.
+"""
+
+
 class Solution:
     @staticmethod
     def _get_subarray_with_k_distinct_integers(arr, k):
+        """
+            Time complexity is O(n) and space complexity is O(n) for the dictionary.
+        """
+
         if k < 0:
             return 0
+
+        # window variables
         left = right = 0
         n = len(arr)
         count_less_than_equal_to_k = 0
-        distinct_integers_count = 0
+
+        # bucket to store the count of all the integers in the window.
         bucket = {i: 0 for i in arr}
         while right < n:
             bucket[arr[right]] += 1
+
+            # sum(1 for v in bucket.values() if v != 0) gives the count of integers present in the window.
             while sum(1 for v in bucket.values() if v != 0) > k:
                 bucket[arr[left]] -= 1
                 left += 1
@@ -29,3 +45,5 @@ print(Solution.get_subarray_with_k_distinct_integers([1, 2, 1, 2, 3], 2))
 print(Solution.get_subarray_with_k_distinct_integers([1, 2, 1, 3, 4], 3))
 print(Solution.get_subarray_with_k_distinct_integers([2, 1, 2, 1, 6], 2))
 print(Solution.get_subarray_with_k_distinct_integers([1, 2, 3, 4, 5], 1))
+print(Solution.get_subarray_with_k_distinct_integers([2, 1, 3, 2, 4], 2))
+print(Solution.get_subarray_with_k_distinct_integers([1, 2, 3, 4, 5], 4))
