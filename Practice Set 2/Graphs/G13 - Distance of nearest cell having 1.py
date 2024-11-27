5e28e1b7dc7298e26cc37974749d303450f7920b
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-27 14:06:09 +0530

    Distance of the nearest cell having 1.

--- Practice Set 2/Graphs/G13 - Distance of nearest cell having 1.py
+++ Practice Set 2/Graphs/G13 - Distance of nearest cell having 1.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/distance-of-nearest-cell-having-1-1587115620/1
+# Solution - https://www.youtube.com/watch?v=edXdVwkYHF8&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=13
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -80,6 +84,50 @@ class Solution:
         print()
 
 
+class BetterSolution:
+    @staticmethod
+    def _get_valid_neighbours(i, j, n, m, visited):
+        neighbours = []
+        if 0 <= i - 1 < n and not visited[i - 1][j]:
+            neighbours.append((i - 1, j))
+        if 0 <= j + 1 < m and not visited[i][j + 1]:
+            neighbours.append((i, j + 1))
+        if 0 <= i + 1 < n and not visited[i + 1][j]:
+            neighbours.append((i + 1, j))
+        if 0 <= j - 1 < m and not visited[i][j - 1]:
+            neighbours.append((i, j - 1))
+        return neighbours
+
+    @staticmethod
+    def get_nearest_1s(mtx):
+        """
+            Overall time complexity is O(mn) and space is O(mn).
+        """
+
+        n, m = len(mtx), len(mtx[0])
+        result = [[1e6 for _ in range(m)] for _ in range(n)]
+        visited = [[False for _ in range(m)] for _ in range(n)]
+        queue = Queue()
+
+        # push all the starting 1s into the queue in O(mn) time.
+        for i in range(n):
+            for j in range(m):
+                if mtx[i][j] == 1:
+                    queue.push((i, j, 0))
+
+        while not queue.is_empty():
+            x, y, d = queue.pop()
+            visited[x][y] = True
+            result[x][y] = min(d, result[x][y])
+            neighbours = BetterSolution._get_valid_neighbours(x, y, n, m, visited)
+            for neighbour in neighbours:
+                queue.push((*neighbour, d + 1))
+
+        for i in result:
+            print(i)
+        print()
+
+
 Solution.get_nearest_1s(
     [
         [0, 1, 1, 0],
@@ -98,6 +146,34 @@ Solution.get_nearest_1s(
 
 Solution.get_nearest_1s([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
 Solution.get_nearest_1s(
+    [
+        [0, 0, 0, 1],
+        [0, 0, 1, 1],
+        [0, 1, 1, 0]
+    ]
+)
+
+print()
+print()
+
+BetterSolution.get_nearest_1s(
+    [
+        [0, 1, 1, 0],
+        [1, 1, 0, 0],
+        [0, 0, 1, 1]
+    ]
+)
+
+BetterSolution.get_nearest_1s(
+    [
+        [1, 0, 1],
+        [1, 1, 0],
+        [1, 0, 0]
+    ]
+)
+
+BetterSolution.get_nearest_1s([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+BetterSolution.get_nearest_1s(
     [
         [0, 0, 0, 1],
         [0, 0, 1, 1],
