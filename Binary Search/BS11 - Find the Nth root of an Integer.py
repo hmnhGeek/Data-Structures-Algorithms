43b87fea0048368dbb2acefed5e047281d4d7824
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-27 18:28:04 +0530

    nth root of a number using binary search

--- Binary Search/BS11 - Find the Nth root of an Integer.py
+++ Binary Search/BS11 - Find the Nth root of an Integer.py
@@ -4,12 +4,19 @@
 
 class Solution:
     @staticmethod
-    def _pow(x, n):
+    def _pow(mid, n, x):
         # Time complexity is O(n) and space is O(1).
         prod = 1
         for i in range(n):
-            prod = prod * x
-        return prod
+            prod = prod * mid
+            if prod > x:
+                # answer not possible, eliminate right half
+                return 2
+        if prod == x:
+            # answer is possible
+            return 1
+        # answer is again not possible
+        return 0
 
     @staticmethod
     def get_nth_root(x, n: int):
@@ -25,10 +32,10 @@ class Solution:
             mid = int(low + (high - low)/2)
             # the only change is here... instead of computing simply a square, we raise mid to the power n and
             # then check if the result is equal to x or not. It will take O(n) time.
-            possible_ans = Solution._pow(mid, n)
-            if possible_ans == x:
+            is_power_possible = Solution._pow(mid, n, x)
+            if is_power_possible == 1:
                 return mid
-            if possible_ans < x:
+            if is_power_possible == 0:
                 low = mid + 1
             else:
                 high = mid - 1
@@ -42,3 +49,4 @@ print(Solution.get_nth_root(27, 3))
 print(Solution.get_nth_root(14, 1))
 print(Solution.get_nth_root(1000, 3))
 print(Solution.get_nth_root(625, 4))
+print(Solution.get_nth_root(10**9, 9))
