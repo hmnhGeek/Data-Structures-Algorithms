d5812c316f603d4bfc411836382c3f2c01f8ce6e
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-28 19:02:26 +0530

    Cherry pickup II using tabulation

--- Practice Set 2/Dynamic Programming/DP13 - Cherry Pickup II.py
+++ Practice Set 2/Dynamic Programming/DP13 - Cherry Pickup II.py
@@ -103,7 +103,7 @@ def recursive():
 
 def memoized():
     """
-        Time complexity is O(m^2 * n) and space complexity is O(m + n + m^2 * n)
+        Time complexity is O(m^4 * n) and space complexity is O(m + n + m^2 * n)
     """
 
     def solve(mtx, a, b, i, n, m, dp):
@@ -209,6 +209,118 @@ def memoized():
     )
 
 
+def tabulation():
+    """
+        Time complexity is O(m^4 * n) and space complexity is O(m^2 * n)
+    """
+    def cherry_pickup(mtx):
+        n, m = len(mtx), len(mtx[0])
+        max_chocolates = 0
+
+        # O(m^2)
+        for alice in range(m):
+            for bob in range(m):
+                dp = {i: {a: {b: 0 for b in range(m)} for a in range(m)} for i in range(n)}
+
+                # O(m^2)
+                for a in dp[0]:
+                    for b in dp[0][a]:
+                        chocolates = 0
+                        if a == 0:
+                            chocolates += mtx[0][0]
+                        if b == m - 1:
+                            chocolates += mtx[0][m - 1]
+                        dp[0][a][b] = chocolates
+
+                # O(n * m^2)
+                for i in range(1, n):
+                    for a in range(m):
+                        for b in range(m):
+                            sub_max_chocolates = 0
+                            deltas = [-1, 0, 1]
+                            for da in deltas:
+                                for db in deltas:
+                                    if a == b:
+                                        if 0 <= a + da < m and 0 <= b + db < m and 0 <= i - 1 < n:
+                                            sub_max_chocolates = max(
+                                                sub_max_chocolates,
+                                                mtx[i][a] + dp[i - 1][a + da][b + db]
+                                            )
+                                    else:
+                                        if 0 <= a + da < m and 0 <= b + db < m and 0 <= i - 1 < n:
+                                            sub_max_chocolates = max(
+                                                sub_max_chocolates,
+                                                mtx[i][a] + mtx[i][b] + dp[i - 1][a + da][b + db]
+                                            )
+                            dp[i][a][b] = sub_max_chocolates
+                max_chocolates = max(max_chocolates, dp[n - 1][alice][bob])
+        return max_chocolates
+
+    print(
+        cherry_pickup(
+            [
+                [2, 3, 1, 2],
+                [3, 4, 2, 2],
+                [5, 6, 3, 5]
+            ]
+        )
+    )
+
+    print(
+        cherry_pickup(
+            [
+                [1, 1],
+                [1, 2]
+            ]
+        )
+    )
+
+    print(
+        cherry_pickup(
+            [
+                [3, 7],
+                [7, 6]
+            ]
+        )
+    )
+
+    print(
+        cherry_pickup(
+            [
+                [4, 5],
+                [3, 7],
+                [4, 2]
+            ]
+        )
+    )
+
+    print(cherry_pickup([[3, 1, 1], [2, 5, 1], [1, 5, 5], [2, 1, 1]]))
+    print(
+        cherry_pickup(
+            [
+                [4, 1, 2],
+                [3, 6, 1],
+                [1, 6, 6],
+                [3, 1, 2]
+            ]
+        )
+    )
+
+    print(
+        cherry_pickup(
+            [
+                [2, 0, 0, 0, 0, 0, 2],
+                [2, 1, 0, 0, 0, 4, 0],
+                [2, 0, 10, 0, 1, 0, 0],
+                [0, 3, 0, 6, 5, 0, 0],
+                [1, 0, 3, 4, 0, 0, 6]
+            ]
+        )
+    )
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
