b9d2bf4b645e9d3b1cb205bb97503e935d6b36c0
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-28 15:03:15 +0530

    Koko eating bananas using binary search

--- Binary Search/BS12 - Koko Eating Bananas.py
+++ Binary Search/BS12 - Koko Eating Bananas.py
@@ -6,19 +6,29 @@ class Solution:
     def _is_possible(piles, time_limit, mid):
         time_taken = 0
         for i in range(len(piles)):
-            time_taken += ceil(piles[i]/mid)
+            time_taken += ceil(piles[i] / mid)
         return time_taken <= time_limit
 
     @staticmethod
     def find_rate(piles, time_limit):
+        """
+            Time complexity is O(n*log(max(arr))) and space complexity is O(1).
+        """
+
+        # minimum rate should be 1 banana/hr and max rate should be max(piles) bananas/hr.
         low, high = 1, max(piles)
         while low <= high:
-            mid = int(low + (high - low)/2)
+            mid = int(low + (high - low) / 2)
+            # check if all the bananas can be eaten within the time limit in O(n) time.
             all_can_be_eaten = Solution._is_possible(piles, time_limit, mid)
+
+            # if they can be, we can reduce the rate as we need to find min rate.
             if all_can_be_eaten:
                 high = mid - 1
             else:
+                # else we can increase the rate.
                 low = mid + 1
+        # finally, `low` would point to the correct rate.
         return low
 
 
@@ -26,3 +36,5 @@ print(Solution.find_rate([3, 6, 7, 11], 8))
 print(Solution.find_rate([3, 6, 2, 8], 7))
 print(Solution.find_rate([7, 15, 6, 3], 8))
 print(Solution.find_rate([25, 12, 8, 14, 19], 5))
+print(Solution.find_rate([30, 11, 23, 4, 20], 5))
+print(Solution.find_rate([30, 11, 23, 4, 20], 6))
