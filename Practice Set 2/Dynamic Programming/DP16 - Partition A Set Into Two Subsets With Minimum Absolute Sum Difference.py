847db3116a3df82383ec7a250b0bc2679fc07bea
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-29 17:37:17 +0530

    Tabulation subset sum check

--- Practice Set 2/Dynamic Programming/DP16 - Partition A Set Into Two Subsets With Minimum Absolute Sum Difference.py
+++ Practice Set 2/Dynamic Programming/DP16 - Partition A Set Into Two Subsets With Minimum Absolute Sum Difference.py
@@ -1,7 +1,7 @@
 def recursive():
     def solve(arr, i, j):
         if j == 0:
-            return False
+            return True
         if i == 0:
             return arr[0] == j
 
@@ -22,7 +22,7 @@ def recursive():
 def memoized():
     def solve(arr, i, j, dp):
         if j == 0:
-            return False
+            return True
         if i == 0:
             return arr[0] == j
 
@@ -45,6 +45,28 @@ def memoized():
     print(subset_sum([1, 2, 3, 4], 5))
 
 
+def tabulation():
+    def subset_sum(arr, target):
+        n = len(arr)
+        dp = {i: {j: False for j in range(target + 1)} for i in range(n)}
+        for i in dp:
+            dp[i][0] = True
+        dp[0][arr[0]] = True
+        for i in range(1, n):
+            for j in range(target + 1):
+                left = False
+                if arr[i] <= j:
+                    left = dp[i - 1][j - arr[i]]
+                right = dp[i - 1][j]
+                dp[i][j] = left or right
+        return dp[n - 1][target]
+
+    print(subset_sum([1, 2, 3, 4], 9))
+    print(subset_sum([1, 2, 3, 4], 5))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
