edd51545a0af80edfd3479e583c60f028aa81b35
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-29 14:40:55 +0530

    Bipartite Graph using BFS

--- Practice Set 2/Graphs/G17 - Bipartite Graoh using BFS.py
+++ Practice Set 2/Graphs/G17 - Bipartite Graoh using BFS.py
@@ -32,3 +32,51 @@ class Queue:
         return item
 
 
+class Solution:
+    @staticmethod
+    def is_bipartite(graph, source_node):
+        if source_node not in graph:
+            return
+        colors = {i: None for i in graph}
+        queue = Queue()
+        queue.push((source_node, True))
+        while not queue.is_empty():
+            node, color = queue.pop()
+            if colors[node] is not None and colors[node] == (not color):
+                return False
+            colors[node] = color
+            for adj_node in graph[node]:
+                if colors[adj_node] is None:
+                    queue.push((adj_node, not color))
+        return True
+
+
+print(
+    Solution.is_bipartite(
+        {
+            1: [2],
+            2: [3, 6],
+            3: [2, 4],
+            4: [3, 5, 7],
+            5: [4, 6],
+            6: [2, 5],
+            7: [4, 8],
+            8: [7]
+        },
+        1
+    )
+)
+
+print(
+    Solution.is_bipartite(
+        {
+            1: [2],
+            2: [1, 3, 5],
+            3: [2, 4],
+            4: [3, 5, 6],
+            5: [2, 4],
+            6: [4]
+        },
+        3
+    )
+)
