cb917e951ee04bf9d672dc1632be1fef86f2575c
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-29 17:45:16 +0530

    Space optimized subset sum check

--- Practice Set 2/Dynamic Programming/DP16 - Partition A Set Into Two Subsets With Minimum Absolute Sum Difference.py
+++ Practice Set 2/Dynamic Programming/DP16 - Partition A Set Into Two Subsets With Minimum Absolute Sum Difference.py
@@ -86,10 +86,42 @@ def space_optimized():
     print(subset_sum([1, 2, 3, 4], 5))
 
 
-recursive()
-print()
-memoized()
-print()
-tabulation()
-print()
-space_optimized()
+class Solution:
+    @staticmethod
+    def _subset_sum(arr, target):
+        n = len(arr)
+        prev = {j: False for j in range(target + 1)}
+        prev[0] = True
+        prev[arr[0]] = True
+        for i in range(1, n):
+            curr = {j: False for j in range(target + 1)}
+            for j in range(target + 1):
+                left = False
+                if arr[i] <= j:
+                    left = prev[j - arr[i]]
+                right = prev[j]
+                curr[j] = left or right
+            prev = curr
+        return prev[target]
+
+    @staticmethod
+    def min_diff_subset(arr):
+        """
+            Time complexity is O(n^2) and space complexity is O(n).
+        """
+        _sum = sum(arr)
+        if _sum % 2 == 0 and Solution._subset_sum(arr, _sum // 2):
+            return 0
+        min_dff = 1e6
+        for s1 in range((_sum // 2) + 2):
+            if Solution._subset_sum(arr, s1):
+                min_dff = min(min_dff, abs(2*s1 - _sum))
+        return min_dff
+
+
+print(Solution.min_diff_subset([1, 2, 3, 4]))
+print(Solution.min_diff_subset([3, 1, 5, 2, 8]))
+print(Solution.min_diff_subset([8, 6, 5]))
+print(Solution.min_diff_subset([3, 9, 7, 3]))
+print(Solution.min_diff_subset([1, 6, 11, 5]))
+print(Solution.min_diff_subset([1, 5, 11, 5]))
