5666b02b06a97205df1043f1b0e3f89ada9466dd
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-29 16:45:55 +0530

    Bipartite Graph using DFS

---
+++ Practice Set 2/Graphs/G18 - Bipartite Graph using DFS.py
@@ -0,0 +1,92 @@
+class Solution:
+    @staticmethod
+    def _dfs(graph, colors, node, color):
+        colors[node] = color
+        for adj_node in graph[node]:
+            if colors[adj_node] is None:
+                if not Solution._dfs(graph, colors, adj_node, not color):
+                    return False
+            elif colors[adj_node] is not None and colors[adj_node] != (not color):
+                return False
+        return True
+
+    @staticmethod
+    def is_bipartite(graph):
+        colors = {i: None for i in graph}
+        for node in graph:
+            if colors[node] is None:
+                if not Solution._dfs(graph, colors, node, True):
+                    return False
+        return True
+
+
+print(
+    Solution.is_bipartite(
+        {
+            1: [2],
+            2: [3, 6],
+            3: [2, 4],
+            4: [3, 5, 7],
+            5: [4, 6],
+            6: [2, 5],
+            7: [4, 8],
+            8: [7]
+        }
+    )
+)
+
+print(
+    Solution.is_bipartite(
+        {
+            1: [2],
+            2: [1, 3, 5],
+            3: [2, 4],
+            4: [3, 5, 6],
+            5: [2, 4],
+            6: [4]
+        }
+    )
+)
+
+print(
+    Solution.is_bipartite(
+        {
+            0: [1],
+            1: [0, 2],
+            2: [1]
+        }
+    )
+)
+
+print(
+    Solution.is_bipartite(
+        {
+            0: [2, 3],
+            1: [2],
+            2: [0, 1, 3],
+            3: [0, 2]
+        }
+    )
+)
+
+print(
+    Solution.is_bipartite(
+        {
+            0: [1, 2, 3],
+            1: [0, 2],
+            2: [0, 1, 3],
+            3: [0, 2]
+        }
+    )
+)
+
+print(
+    Solution.is_bipartite(
+        {
+            0: [1, 3],
+            1: [0, 2],
+            2: [1, 3],
+            3: [0, 2]
+        }
+    )
+)
