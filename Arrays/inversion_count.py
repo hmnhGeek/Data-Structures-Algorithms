f8f5ffabd04ea0b989fb38a70a67220eb40b2f54
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-30 15:02:42 +0530

    Count inversions using no extra space.

--- Arrays/inversion_count.py
+++ Arrays/inversion_count.py
@@ -1,10 +1,17 @@
 class Solution:
     @staticmethod
     def no_space(arr):
+        """
+            Total time complexity is O(n^2) and space complexity is O(1).
+        """
+
         count = 0
         n = len(arr)
+        # loop on the array
         for i in range(n):
+            # take `j` from `i + 1` --> `n - 1`
             for j in range(i + 1, n):
+                # if at any point, a_i > a_j, increment the inversion count.
                 if arr[i] > arr[j]:
                     count += 1
         return count
