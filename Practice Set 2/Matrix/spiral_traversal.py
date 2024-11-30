b51b14932ef9f2aac52b40ae2d5e77b434a5e223
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-30 15:44:47 +0530

    Spiral traversal of a matrix

--- Practice Set 2/Matrix/spiral_traversal.py
+++ Practice Set 2/Matrix/spiral_traversal.py
@@ -68,3 +68,5 @@ Solution.spiral_traversal(
 )
 
 Solution.spiral_traversal([[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], [13, 14, 15, 16, 17, 18]])
+
+Solution.spiral_traversal([[32, 44, 27, 23], [54, 28, 50, 62]])
