fe83db675630855d1977a77af04e13fff9ee75cf
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-30 20:01:25 +0530

    Sort a stack using recursion

--- Practice Set 1/Stacks and Queues/sort_a_stack.py
+++ Practice Set 1/Stacks and Queues/sort_a_stack.py
@@ -37,3 +37,46 @@ class Stack:
         return self.head.data
 
 
+class Solution:
+    @staticmethod
+    def _sorted_insert(stack, top, element):
+        if element is None:
+            return stack
+        if stack.top() <= element:
+            stack.push(element)
+            if top:
+                stack.push(top)
+            return stack
+
+        stack = Solution._sorted_insert(stack, stack.pop(), element)
+        if top:
+            stack.push(top)
+        return stack
+
+    @staticmethod
+    def _sort(stack: Stack, element):
+        if stack.is_empty():
+            stack.push(element)
+            return stack
+        stack = Solution._sort(stack, stack.pop())
+        stack = Solution._sorted_insert(stack, None, element)
+        return stack
+
+    @staticmethod
+    def sort_stack(stack: Stack):
+        Solution._sort(stack, None)
+        while not stack.is_empty():
+            print(stack.pop(), end=" ")
+        print()
+
+
+def test(*args):
+    stack = Stack()
+    for i in args:
+        stack.push(i)
+    Solution.sort_stack(stack)
+
+
+test(11, 2, 32, 3, 41)
+test(-3, 14, 18, -5, 30)
+test(1, 2, 3)
