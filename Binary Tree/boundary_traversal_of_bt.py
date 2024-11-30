4b1dc100cdfdd0726e692695f6080d50434a9686
hmnhGeek <hmnhsharma97@gmail.com>
2024-11-30 18:35:18 +0530

    Boundary Traversal of a Binary Tree

--- Binary Tree/boundary_traversal_of_bt.py
+++ Binary Tree/boundary_traversal_of_bt.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/boundary-traversal-of-binary-tree/1
+# Solution - https://www.youtube.com/watch?v=0ca1nvR0be4&t=305s
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -42,6 +46,7 @@ class Solution:
     @staticmethod
     def _traverse_left_boundary_excluding_leaf(root: TreeNode, boundary: list):
         while 1:
+            # prioritize left node first, then right, and if none, then it's a leaf node and break.
             if root.left is not None:
                 boundary.append(root.data)
                 root = root.left
@@ -63,6 +68,7 @@ class Solution:
     def _traverse_right_boundary_excluding_leaf(root: TreeNode, boundary: list):
         stack = Stack()
         while 1:
+            # prioritize right node first, then left, and if none, then it's a leaf node and break.
             if root.right is not None:
                 stack.push(root.data)
                 root = root.right
@@ -72,14 +78,21 @@ class Solution:
             else:
                 break
 
-        while not stack.is_empty():
+        while not stack.length == 1:
             boundary.append(stack.pop())
 
     @staticmethod
     def boundary_traversal(root: TreeNode):
+        """
+            Time complexity will be O(N) and space complexity is O(N) using recursion for inorder traversal.
+        """
+
         boundary = []
+        # This will take O(H) time
         Solution._traverse_left_boundary_excluding_leaf(root, boundary)
+        # This will take O(N) time
         Solution._inorder_traversal_for_leaf_nodes(root, boundary)
+        # This will take O(H) time
         Solution._traverse_right_boundary_excluding_leaf(root, boundary)
         print(boundary)
 
@@ -94,4 +107,40 @@ n3.left = n6
 n3.right = n7
 n5.left = n8
 n5.right = n9
+Solution.boundary_traversal(n1)
+
+# Example 2
+n1, n2, n3, n4, n5, n6, n7, n8, n9 = TreeNode(1), TreeNode(2), TreeNode(3), TreeNode(4), TreeNode(5), TreeNode(6), TreeNode(7), TreeNode(8), TreeNode(9)
+n1.left = n2
+n2.left = n4
+n2.right = n9
+n4.left = n6
+n4.right = n5
+n9.right = n3
+n3.left = n7
+n3.right = n8
+Solution.boundary_traversal(n1)
+
+# Example 3
+n1, n2, n3, n4 = TreeNode(1), TreeNode(2), TreeNode(3), TreeNode(4)
+n1.right = n2
+n2.right = n3
+n3.right = n4
+Solution.boundary_traversal(n1)
+
+# Example 4
+n1, n2, n3, n4, n5, n6, n7, n8, n9 = TreeNode(1), TreeNode(2), TreeNode(3), TreeNode(4), TreeNode(5), TreeNode(6), TreeNode(7), TreeNode(8), TreeNode(9)
+n10, n11, n12, n13 = TreeNode(10), TreeNode(11), TreeNode(12), TreeNode(13)
+n1.left = n2
+n2.left = n4
+n4.left = n7
+n5.left = n8
+n10.left = n12
+n1.right = n3
+n3.right = n6
+n6.right = n10
+n2.right = n5
+n5.right = n9
+n7.right = n11
+n11.right = n13
 Solution.boundary_traversal(n1)
