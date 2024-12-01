82d17265b9bab5cccad2492a0b195ba48e850f36
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-01 12:21:16 +0530

    Minimum swaps to sort the array - Added custom quick sort logic

--- Searching and Sorting/min_swaps_to_sort.py
+++ Searching and Sorting/min_swaps_to_sort.py
@@ -2,6 +2,34 @@
 # Solution - https://www.youtube.com/watch?v=kFe_LRWuZjE
 
 
+class QuickSort:
+    @staticmethod
+    def _get_partition_index(arr, low, high):
+        pivot = arr[low][0]
+        i, j = low, high
+        while i < j:
+            while arr[i][0] <= pivot and i <= high - 1:
+                i += 1
+            while arr[j][0] > pivot and j >= low + 1:
+                j -= 1
+            if i < j:
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[low], arr[j] = arr[j], arr[low]
+        return j
+
+    @staticmethod
+    def _sort(arr, low, high):
+        if low >= high:
+            return
+        partition_index = QuickSort._get_partition_index(arr, low, high)
+        QuickSort._sort(arr, low, partition_index - 1)
+        QuickSort._sort(arr, partition_index + 1, high)
+
+    @staticmethod
+    def sort(arr):
+        QuickSort._sort(arr, 0, len(arr) - 1)
+
+
 class Solution:
     @staticmethod
     def find_min_swaps(arr):
@@ -16,7 +44,7 @@ class Solution:
         temp = [(arr[i], i) for i in range(n)]
 
         # sort the temp array based on the 0th element of each tuple, i.e., original elements, in O(n * log(n)) time.
-        temp.sort(key=lambda x: x[0])
+        QuickSort.sort(temp)
 
         # store the swaps count.
         swaps_count = 0
