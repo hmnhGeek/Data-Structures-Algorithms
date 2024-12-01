90ebf560e02d9ceab22a0f05d4fcb7eb35559a8f
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-01 16:04:24 +0530

    Min Heap to Max Heap

--- Practice Set 1/Heap/min_heap_to_max_heap.py
+++ Practice Set 1/Heap/min_heap_to_max_heap.py
@@ -1,3 +1,6 @@
+# Problem link - https://www.geeksforgeeks.org/convert-min-heap-to-max-heap/
+
+
 class MinHeap:
     def __init__(self):
         self.heap = []
@@ -64,3 +67,89 @@ class MinHeap:
         return item
 
 
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def is_empty(self):
+        return len(self.heap) == 0
+
+    def get_lci(self, pi):
+        lci = 2*pi + 1
+        return lci if lci in range(len(self.heap)) else None
+
+    def get_rci(self, pi):
+        rci = 2*pi + 2
+        return rci if rci in range(len(self.heap)) else None
+
+    def get_pi(self, ci):
+        if ci == 0:
+            return
+        pi = int((ci - 1)/2)
+        return pi if pi in range(len(self.heap)) else None
+
+    def get_max_child_index(self, lci, rci):
+        if lci is None and rci is None:
+            return
+        if lci is None:
+            return rci
+        if rci is None:
+            return lci
+        max_child_index = lci
+        if self.heap[rci] > self.heap[max_child_index]:
+            max_child_index = rci
+        return max_child_index
+
+    def max_heapify_up(self, start_index):
+        if start_index == 0:
+            return
+        pi = self.get_pi(start_index)
+        lci, rci = self.get_lci(pi), self.get_rci(pi)
+        max_child_index = self.get_max_child_index(lci, rci)
+        if max_child_index is not None:
+            if self.heap[pi] < self.heap[max_child_index]:
+                self.heap[pi], self.heap[max_child_index] = self.heap[max_child_index], self.heap[pi]
+            self.max_heapify_up(pi)
+
+    def max_heapify_down(self, pi):
+        lci, rci = self.get_lci(pi), self.get_rci(pi)
+        max_child_index = self.get_max_child_index(lci, rci)
+        if max_child_index is not None:
+            if self.heap[pi] < self.heap[max_child_index]:
+                self.heap[pi], self.heap[max_child_index] = self.heap[max_child_index], self.heap[pi]
+            self.max_heapify_down(max_child_index)
+
+    def insert(self, x):
+        self.heap.append(x)
+        self.max_heapify_up(len(self.heap) - 1)
+
+    def pop(self):
+        if self.is_empty():
+            return
+        item = self.heap[0]
+        self.heap[0], self.heap[-1] = self.heap[-1], self.heap[0]
+        del self.heap[-1]
+        self.max_heapify_down(0)
+        return item
+
+
+class Solution:
+    @staticmethod
+    def convert(min_heap: MinHeap):
+        """
+            Time complexity is O(nlog(n)) and space complexity is O(n).
+        """
+
+        print(min_heap.heap)
+        max_heap = MaxHeap()
+        # This will take O(n*log(n)) time and O(n) space.
+        while not min_heap.is_empty():
+            max_heap.insert(min_heap.pop())
+        return max_heap.heap
+
+
+# Example 1
+min_heap = MinHeap()
+for i in [3, 5, 9, 6, 8, 20, 10, 12, 18, 9]:
+    min_heap.insert(i)
+print(Solution.convert(min_heap))
