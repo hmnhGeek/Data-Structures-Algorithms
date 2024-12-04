db2570f7ffb4df0eab93a8c8d03b2c9633838666
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-04 09:04:24 +0530

    Aggressive Cows - added custom code for quick sort

--- Binary Search/BS17 - Aggressive Cows.py
+++ Binary Search/BS17 - Aggressive Cows.py
@@ -1,3 +1,30 @@
+class QuickSort:
+    @staticmethod
+    def sort(arr):
+        QuickSort._sort(arr, 0, len(arr) - 1)
+
+    @staticmethod
+    def _sort(arr, low, high):
+        if low < high:
+            partition_index = QuickSort._get_partition_index(arr, low, high)
+            QuickSort._sort(arr, low, partition_index - 1)
+            QuickSort._sort(arr, partition_index + 1, high)
+
+    @staticmethod
+    def _get_partition_index(arr, low, high):
+        pivot = arr[low]
+        i, j = low, high
+        while i < j:
+            while arr[i] <= pivot and i <= high - 1:
+                i += 1
+            while arr[j] > pivot and j >= low + 1:
+                j -= 1
+            if i < j:
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[low], arr[j] = arr[j], arr[low]
+        return j
+
+
 class Solution:
     @staticmethod
     def _placing_all_cows_possible(arr, mid, k):
@@ -13,7 +40,7 @@ class Solution:
     def aggressive_cows(arr, k):
         if k <= 0:
             return
-        arr.sort()
+        QuickSort.sort(arr)
         low, high = 1, arr[-1] - arr[0]
         while low <= high:
             mid = int(low + (high - low)/2)
