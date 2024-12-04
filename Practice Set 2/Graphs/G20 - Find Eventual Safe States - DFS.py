9e56e7e977ed281f3f614f41b0af40b345b5328a
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-04 11:45:46 +0530

    Detect eventual safe nodes using DFS

--- Practice Set 2/Graphs/G20 - Find Eventual Safe States - DFS.py
+++ Practice Set 2/Graphs/G20 - Find Eventual Safe States - DFS.py
@@ -1,25 +1,45 @@
+# Problem link - https://www.geeksforgeeks.org/problems/eventual-safe-states/1
+# Solution - https://www.youtube.com/watch?v=uRbJ1OF9aYM&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=20
+
+
 class Solution:
     @staticmethod
     def _has_cycle(graph, node, visited, path_visited, safe_nodes):
+        # mark nodes as visited and path visited.
         visited[node] = True
         path_visited[node] = True
+
+        # loop on the adjacent nodes.
         for adj_node in graph[node]:
+            # typical cycle detection code from G19.
             if not visited[adj_node]:
                 if Solution._has_cycle(graph, adj_node, visited, path_visited, safe_nodes):
                     return True
             elif path_visited[adj_node]:
                 return True
+
+        # unmark this node from path visited
         path_visited[node] = False
+        # mark this node as safe as no adjacent node gave a cycle.
         safe_nodes[node] = True
+        # return False for this node, as no cycle was found.
         return False
 
     @staticmethod
     def find_safe_states(graph):
+        """
+            Overall time complexity is O(V + E) and space is O(3V).
+        """
+
+        # define variables for visited, path visited and safe nodes. This will take O(3V) space.
         visited = {i: False for i in graph}
         path_visited = {i: False for i in graph}
         safe_nodes = {i: False for i in graph}
+
+        # loop on all the components.
         for node in graph:
             if not visited[node]:
+                # if there's a cycle, don't break, continue checking for all the nodes.
                 Solution._has_cycle(graph, node, visited, path_visited, safe_nodes)
         return safe_nodes
 
