49202831f9c03a03257075a5d7890a5bce8c55ee
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-05 19:36:47 +0530

    Memoized 0/1 knapsack problem

--- Practice Set 2/Dynamic Programming/DP19 - 01 Knapsack.py
+++ Practice Set 2/Dynamic Programming/DP19 - 01 Knapsack.py
@@ -22,4 +22,34 @@ def recursive():
     print(knapsack([5, 4, 6, 3], [10, 40, 30, 50], 5))
 
 
+def memoized():
+    """
+        Time complexity is O(n * capacity) and space complexity is O(n + n * capacity).
+    """
+
+    def solve(weights, cost, index, capacity, dp):
+        if index == 0:
+            return cost[0] if capacity >= weights[0] else 0
+        if dp[index][capacity] is not None:
+            return dp[index][capacity]
+        left = -1e6
+        if weights[index] <= capacity:
+            left = cost[index] + solve(weights, cost, index - 1, capacity - weights[index], dp)
+        right = solve(weights, cost, index - 1, capacity, dp)
+        dp[index][capacity] = max(left, right)
+        return dp[index][capacity]
+
+    def knapsack(weights, cost, capacity):
+        n = len(weights)
+        dp = {i: {j: None for j in range(capacity + 1)} for i in range(n)}
+        return solve(weights, cost, n - 1, capacity, dp)
+
+    print(knapsack([1, 2, 4, 5], [5, 4, 8, 6], 5))
+    print(knapsack([4, 5, 1], [1, 2, 3], 4))
+    print(knapsack([4, 5, 6], [1, 2, 3], 3))
+    print(knapsack([5, 4, 6, 3], [10, 40, 30, 50], 5))
+
+
 recursive()
+print()
+memoized()
