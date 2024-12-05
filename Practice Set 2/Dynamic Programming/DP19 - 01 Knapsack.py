72c224e29193d1b13ddb0d64e80ebf45878379e5
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-05 19:35:00 +0530

    Recursive 0/1 knapsack problem

---
+++ Practice Set 2/Dynamic Programming/DP19 - 01 Knapsack.py
@@ -0,0 +1,25 @@
+def recursive():
+    """
+        Time complexity is O(2^n) and space complexity is O(n).
+    """
+
+    def solve(weights, cost, index, capacity):
+        if index == 0:
+            return cost[0] if capacity >= weights[0] else 0
+        left = -1e6
+        if weights[index] <= capacity:
+            left = cost[index] + solve(weights, cost, index - 1, capacity - weights[index])
+        right = solve(weights, cost, index - 1, capacity)
+        return max(left, right)
+
+    def knapsack(weights, cost, capacity):
+        n = len(weights)
+        return solve(weights, cost, n - 1, capacity)
+
+    print(knapsack([1, 2, 4, 5], [5, 4, 8, 6], 5))
+    print(knapsack([4, 5, 1], [1, 2, 3], 4))
+    print(knapsack([4, 5, 6], [1, 2, 3], 3))
+    print(knapsack([5, 4, 6, 3], [10, 40, 30, 50], 5))
+
+
+recursive()
