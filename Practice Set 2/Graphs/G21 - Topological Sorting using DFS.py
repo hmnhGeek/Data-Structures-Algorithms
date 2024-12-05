433f655465fcce744709852aa684f3f67af83641
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-05 13:40:28 +0530

    Topological sorting

--- Practice Set 2/Graphs/G21 - Topological Sorting using DFS.py
+++ Practice Set 2/Graphs/G21 - Topological Sorting using DFS.py
@@ -32,3 +32,35 @@ class Stack:
         return item
 
 
+class Solution:
+    @staticmethod
+    def _dfs(graph, node, stack, visited):
+        visited[node] = True
+        for adj_node in graph[node]:
+            if not visited[adj_node]:
+                Solution._dfs(graph, adj_node, stack, visited)
+        stack.push(node)
+
+    @staticmethod
+    def topological_sort(graph):
+        visited = {i: False for i in graph}
+        stack = Stack()
+        for node in graph:
+            if not visited[node]:
+                Solution._dfs(graph, node, stack, visited)
+
+        while not stack.is_empty():
+            print(stack.pop(), end=" ")
+        print()
+
+
+Solution.topological_sort(
+    {
+        0: [],
+        1: [],
+        2: [3],
+        3: [1],
+        4: [0, 1],
+        5: [0, 2]
+    }
+)
