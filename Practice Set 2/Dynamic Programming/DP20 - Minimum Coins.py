357d547f5731b0ea645612bc56ed00106d1bbce6
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-05 20:13:49 +0530

    Recursive minimum coins

---
+++ Practice Set 2/Dynamic Programming/DP20 - Minimum Coins.py
@@ -0,0 +1,35 @@
+def recursive():
+    """
+        Time complexity is O(2^n) and space complexity is O(n)
+    """
+
+    def solve(arr, index, target):
+        if target == 0:
+            return 0
+        if index == 0:
+            return target//arr[0] if target % arr[0] == 0 else 1e6
+        left = 1e6
+        if arr[index] <= target:
+            left = 1 + solve(arr, index, target - arr[index])
+        right = solve(arr, index - 1, target)
+        return min(left, right)
+
+    def min_coins(arr, k):
+        n = len(arr)
+        ans = solve(arr, n - 1, k)
+        return ans if ans != 1e6 else -1
+
+    print(min_coins([1, 2, 3], 7))
+    print(min_coins([1], 0))
+    print(min_coins([12, 1, 3], 4))
+    print(min_coins([2, 1], 11))
+    print(min_coins([1, 2, 5], 11))
+    print(min_coins([2], 3))
+    print(min_coins([1,], 0))
+    print(min_coins([25, 10, 5], 30))
+    print(min_coins([9, 6, 5, 1], 19))
+    print(min_coins([5, 1], 0))
+    print(min_coins([4, 6, 2], 5))
+
+
+recursive()
