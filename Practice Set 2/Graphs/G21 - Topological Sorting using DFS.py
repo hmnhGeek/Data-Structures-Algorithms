7f795a890e10ce59a293285e43888b12de162c19
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-05 13:43:06 +0530

    Topological sorting using DFS

--- Practice Set 2/Graphs/G21 - Topological Sorting using DFS.py
+++ Practice Set 2/Graphs/G21 - Topological Sorting using DFS.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/topological-sort/1
+# Solution - https://www.youtube.com/watch?v=5lZ0iJMrUMk&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=21
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -35,20 +39,30 @@ class Stack:
 class Solution:
     @staticmethod
     def _dfs(graph, node, stack, visited):
+        # mark the node visited
         visited[node] = True
+        # perform DFS on adjacent nodes
         for adj_node in graph[node]:
             if not visited[adj_node]:
                 Solution._dfs(graph, adj_node, stack, visited)
+        # push the node to stack
         stack.push(node)
 
     @staticmethod
     def topological_sort(graph):
+        """
+            Time complexity is O(V + E) and space complexity is O(V).
+        """
+
         visited = {i: False for i in graph}
         stack = Stack()
+
+        # handling components in the graph
         for node in graph:
             if not visited[node]:
                 Solution._dfs(graph, node, stack, visited)
 
+        # print the topological sort
         while not stack.is_empty():
             print(stack.pop(), end=" ")
         print()
@@ -60,6 +74,26 @@ Solution.topological_sort(
         1: [],
         2: [3],
         3: [1],
+        4: [0, 1],
+        5: [0, 2]
+    }
+)
+
+Solution.topological_sort(
+    {
+        0: [],
+        1: [0],
+        2: [0],
+        3: [0]
+    }
+)
+
+Solution.topological_sort(
+    {
+        0: [],
+        1: [3],
+        2: [3],
+        3: [],
         4: [0, 1],
         5: [0, 2]
     }
