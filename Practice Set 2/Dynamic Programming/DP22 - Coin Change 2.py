fc8d95767480e1b6c43217e4c14238416c962890
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-07 13:37:46 +0530

    Coin change 2 memoized

--- Practice Set 2/Dynamic Programming/DP22 - Coin Change 2.py
+++ Practice Set 2/Dynamic Programming/DP22 - Coin Change 2.py
@@ -20,4 +20,32 @@ def recursive():
     print(coin_change_2([1, 2, 3], 4))
 
 
+def memoized():
+    """
+        T: O(nk) and S: O(n + nk)
+    """
+    def solve(arr, index, target, dp):
+        if target == 0:
+            return 1
+        if index == 0:
+            return 1 if target % arr[0] == 0 else 0
+        if dp[index][target] is not None:
+            return dp[index][target]
+        left = 0
+        if arr[index] <= target:
+            left = solve(arr, index, target - arr[index], dp)
+        right = solve(arr, index - 1, target, dp)
+        dp[index][target] = left + right
+        return dp[index][target]
+
+    def coin_change_2(arr, k):
+        n = len(arr)
+        dp = {i: {j: None for j in range(k + 1)} for i in range(n)}
+        return solve(arr, n - 1, k, dp)
+
+    print(coin_change_2([1, 2, 3], 4))
+
+
 recursive()
+print()
+memoized()
