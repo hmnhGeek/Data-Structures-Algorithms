0a8297634b58423209c106cc7125b1fc2a98ead6
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-07 10:06:36 +0530

    Minimize the max distance between gas stations using Max Heap

--- Binary Search/BS20 - Minimise Maximum Distance between Gas Stations.py
+++ Binary Search/BS20 - Minimise Maximum Distance between Gas Stations.py
@@ -6,17 +6,17 @@ class MaxHeap:
         return len(self.heap) == 0
 
     def get_lci(self, pi):
-        lci = 2*pi + 1
+        lci = 2 * pi + 1
         return lci if lci in range(len(self.heap)) else None
 
     def get_rci(self, pi):
-        rci = 2*pi + 2
+        rci = 2 * pi + 2
         return rci if rci in range(len(self.heap)) else None
 
     def get_pi(self, ci):
         if ci == 0:
             return
-        pi = int((ci - 1)/2)
+        pi = int((ci - 1) / 2)
         return pi if pi in range(len(self.heap)) else None
 
     def get_max_child_index(self, lci, rci):
@@ -64,4 +64,28 @@ class MaxHeap:
         return item
 
 
+class Solution:
+    @staticmethod
+    def place_gas_stations(arr, k):
+        n = len(arr)
+        slots = [0] * (n - 1)
+        max_heap = MaxHeap()
+        for i in range(n - 1):
+            diff = arr[i + 1] - arr[i]
+            slot_length = (diff / (slots[i] + 1))
+            max_heap.insert((slot_length, i))
+        for i in range(k):
+            diff, idx = max_heap.pop()
+            new_diff = arr[idx + 1] - arr[idx]
+            slots[idx] += 1
+            new_length = (new_diff / (slots[idx] + 1))
+            max_heap.insert((new_length, idx))
+        max_diff_between_stations, _ = max_heap.pop()
+        return max_diff_between_stations
 
+
+print(Solution.place_gas_stations([1, 2, 3, 4, 5], 4))
+print(Solution.place_gas_stations([1, 2, 3, 4, 5, 6, 7], 6))
+print(Solution.place_gas_stations([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1))
+print(Solution.place_gas_stations([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 9))
+print(Solution.place_gas_stations([3, 6, 12, 19, 33, 44, 67, 72, 89, 95], 2))
