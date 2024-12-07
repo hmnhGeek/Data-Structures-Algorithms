243698d33d54b0ba3a25839b7810bb42518ac5ee
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-07 13:22:07 +0530

    Memoized subset sum

--- Practice Set 2/Dynamic Programming/DP21 - Target Sum.py
+++ Practice Set 2/Dynamic Programming/DP21 - Target Sum.py
@@ -22,4 +22,35 @@ def recursive():
     print(target_sum([1, 2, 3, 1], 3))
 
 
+def memoized():
+    """
+        T: O(n*k) and S: O(n + nk)
+    """
+
+    def solve(arr, index, target, dp):
+        if target == 0:
+            return 1
+        if index == 0:
+            return 1 if arr[0] == target else 0
+
+        if dp[index][target] is not None:
+            return dp[index][target]
+
+        left = 0
+        if arr[index] <= target:
+            left = solve(arr, index - 1, target - arr[index], dp)
+        right = solve(arr, index - 1, target, dp)
+        dp[index][target] = left + right
+        return dp[index][target]
+
+    def target_sum(arr, k):
+        n = len(arr)
+        dp = {i: {j: None for j in range(k + 1)} for i in range(n)}
+        return solve(arr, n - 1, k, dp)
+
+    print(target_sum([1, 2, 3, 1], 3))
+
+
 recursive()
+print()
+memoized()
