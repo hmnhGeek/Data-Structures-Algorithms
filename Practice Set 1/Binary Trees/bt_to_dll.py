fcaae816b15ebdcaa666eef1add22b5e057274d2
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-08 16:26:10 +0530

    Flatten a Binary Tree to Linked List

--- Practice Set 1/Binary Trees/bt_to_dll.py
+++ Practice Set 1/Binary Trees/bt_to_dll.py
@@ -1,3 +1,9 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = self.right = None
+
+
 class StackNode:
     def __init__(self, data):
         self.data = data
@@ -32,17 +38,31 @@ class Stack:
         return item
 
 
+class Solution:
+    @staticmethod
+    def flatten(root):
+        stack = Stack()
+        stack.push(root)
+        while not stack.is_empty():
+            curr = stack.pop()
+            if curr.right is not None:
+                stack.push(curr.right)
+            if curr.left is not None:
+                stack.push(curr.left)
+            if not stack.is_empty():
+                curr.right = stack.head.data
+            curr.left = None
 
 
 # Example 1
-# n10, n12, n15, n25, n30, n36 = Node(10), Node(12), Node(15), Node(25), Node(30), Node(36)
-# n10.left = n12
-# n10.right = n15
-# n12.left = n25
-# n12.right = n30
-# n15.left = n36
-# Solution.convert_to_dll(n10)
-# curr = n25
-# while curr is not None:
-#     print(curr.data, end=" ")
-#     curr = curr.right
+n10, n12, n15, n25, n30, n36 = Node(10), Node(12), Node(15), Node(25), Node(30), Node(36)
+n10.left = n12
+n10.right = n15
+n12.left = n25
+n12.right = n30
+n15.left = n36
+Solution.flatten(n10)
+curr = n25
+while curr is not None:
+    print(curr.data, end=" ")
+    curr = curr.right
