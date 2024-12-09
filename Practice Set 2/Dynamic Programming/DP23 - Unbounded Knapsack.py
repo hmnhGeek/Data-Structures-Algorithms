092b536603c29783d0ea01171fdc6e49c62c4c6a
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-09 11:14:52 +0530

    Tabulation unbounded knapsack

--- Practice Set 2/Dynamic Programming/DP23 - Unbounded Knapsack.py
+++ Practice Set 2/Dynamic Programming/DP23 - Unbounded Knapsack.py
@@ -62,6 +62,37 @@ def memoized():
     print(knapsack([2, 3, 4, 5], [6, 8, 7, 100], 1))
 
 
+def tabulation():
+    """
+        Time complexity is O(n * capacity) and space complexity is O(n * capacity).
+    """
+    def knapsack(weights, values, capacity):
+        n = len(weights)
+        dp = {i: {j: 0 for j in range(capacity + 1)} for i in range(n)}
+        for j in dp[0]:
+            dp[0][j] = (j // weights[0]) * values[0]
+        for i in dp:
+            dp[i][0] = 0
+        for index in range(1, n):
+            for cap in range(capacity + 1):
+                left = -1e6
+                if weights[index] <= cap:
+                    left = values[index] + dp[index][cap - weights[index]]
+                right = dp[index - 1][cap]
+                dp[index][cap] = max(left, right)
+        return dp[n - 1][capacity]
+
+    print(knapsack([2, 4, 6], [5, 11, 13], 10))
+    print(knapsack([5, 10, 20], [7, 2, 4], 15))
+    print(knapsack([6, 12], [4, 17], 3))
+    print(knapsack([1, 50], [1, 30], 100))
+    print(knapsack([1, 3, 4, 5], [10, 40, 50, 70], 8))
+    print(knapsack([1, 3, 4, 5], [6, 1, 7, 7], 8))
+    print(knapsack([2, 3, 4, 5], [6, 8, 7, 100], 1))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
