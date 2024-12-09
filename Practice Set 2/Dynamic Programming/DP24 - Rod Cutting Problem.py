c80a1108b137758e3e010a0f73dd161d367d5d65
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-09 11:36:25 +0530

    Recursive Rod Cutting Problem

---
+++ Practice Set 2/Dynamic Programming/DP24 - Rod Cutting Problem.py
@@ -0,0 +1,29 @@
+def recursive():
+    """
+        Time complexity is O(2^n) and space complexity is O(n).
+    """
+
+    def solve(prices, index, length):
+        if length == 0:
+            return 0
+        if index == 0:
+            return length * prices[0]
+        left = 0
+        if index + 1 <= length:
+            left = prices[index] + solve(prices, index, length - index - 1)
+        right = solve(prices, index - 1, length)
+        return max(left, right)
+
+    def rod_cut(prices, rod_length):
+        n = len(prices)
+        return solve(prices, n - 1, rod_length)
+
+    print(rod_cut([2, 5, 7, 8, 10], 5))
+    print(rod_cut([3, 5, 8, 9, 10, 17, 17, 20], 8))
+    print(rod_cut([3, 5, 6, 7, 10, 12], 6))
+    print(rod_cut([1, 10, 3, 1, 3, 1, 5, 9], 8))
+    print(rod_cut([1, 5, 8, 9, 10, 17, 17, 20], 8))
+    print(rod_cut([3], 1))
+
+
+recursive()
