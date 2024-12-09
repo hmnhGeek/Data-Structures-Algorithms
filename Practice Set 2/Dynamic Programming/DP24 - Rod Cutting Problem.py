6e8785f85968a24faa87d853a53cf5594b8bfa22
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-09 11:40:19 +0530

    Tabulation Rod Cutting Problem

--- Practice Set 2/Dynamic Programming/DP24 - Rod Cutting Problem.py
+++ Practice Set 2/Dynamic Programming/DP24 - Rod Cutting Problem.py
@@ -58,6 +58,36 @@ def memoized():
     print(rod_cut([3], 1))
 
 
+def tabulation():
+    """
+        Time complexity is O(n * rod_length) and space complexity is O(n + n * rod_length).
+    """
+    def rod_cut(prices, rod_length):
+        n = len(prices)
+        dp = {i: {j: 0 for j in range(rod_length + 1)} for i in range(n)}
+        for j in dp[0]:
+            dp[0][j] = j * prices[0]
+        for i in dp:
+            dp[i][0] = 0
+        for index in range(1, n):
+            for length in range(rod_length + 1):
+                left = 0
+                if index + 1 <= length:
+                    left = prices[index] + dp[index][length - index - 1]
+                right = dp[index - 1][length]
+                dp[index][length] = max(left, right)
+        return dp[n - 1][rod_length]
+
+    print(rod_cut([2, 5, 7, 8, 10], 5))
+    print(rod_cut([3, 5, 8, 9, 10, 17, 17, 20], 8))
+    print(rod_cut([3, 5, 6, 7, 10, 12], 6))
+    print(rod_cut([1, 10, 3, 1, 3, 1, 5, 9], 8))
+    print(rod_cut([1, 5, 8, 9, 10, 17, 17, 20], 8))
+    print(rod_cut([3], 1))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
