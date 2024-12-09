d0be37a7760df8f7f14d7c053f46978f6f0be4ba
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-09 11:42:12 +0530

    Rod Cutting Problem

--- Practice Set 2/Dynamic Programming/DP24 - Rod Cutting Problem.py
+++ Practice Set 2/Dynamic Programming/DP24 - Rod Cutting Problem.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.naukri.com/code360/problems/rod-cutting-problem_800284?source=youtube&campaign=striver_dp_videos
+# Solution - https://www.youtube.com/watch?v=mO8XpGoJwuo&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=25
+
+
 def recursive():
     """
         Time complexity is O(2^n) and space complexity is O(n).
