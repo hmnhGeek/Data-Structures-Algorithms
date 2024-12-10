505d5fb4da12b9126cd0e93219b09e0db522406e
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-10 10:05:52 +0530

    Longest substring without repeating characters

--- Practice Set 3/Sliding Windows/L3 - Longest Substring Without Repeating Characters.py
+++ Practice Set 3/Sliding Windows/L3 - Longest Substring Without Repeating Characters.py
@@ -1,19 +1,43 @@
+# Problem link - https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+# Solution - https://www.youtube.com/watch?v=-zSxTJkcdAo&list=PLgUwDviBIf0q7vrFA_HEWcqRqMpCXzYAL&index=3
+
+
 class Solution:
     @staticmethod
     def longest_all(string):
+        """
+            Time complexity is O(n) and space complexity is O(1).
+        """
+
+        # define window variables
         n = len(string)
         left = right = 0
+
+        # define a tracker dictionary
         d = {i: 0 for i in string}
+
+        # define result variables
         longest_length, start_index = 0, -1
+
+        # while there is ground to cover
         while right < n:
+            # increment the count of right indexed character
             d[string[right]] += 1
-            while any(v > 1 for v in d.values()):
+
+            # if there are any duplicates in the window, keep the max window and decrement a unit size from left side.
+            if any(v > 1 for v in d.values()):
                 d[string[left]] -= 1
                 left += 1
-            if right - left + 1 > longest_length:
+
+            # if all characters either don't appear or appear only once, update the max length
+            if all(v <= 1 for v in d.values()):
                 longest_length = max(longest_length, right - left + 1)
                 start_index = left
+
+            # increment the right pointer
             right += 1
+
+        # return the valid substring.
         return string[start_index:start_index+longest_length] if start_index != -1 else ""
 
 
