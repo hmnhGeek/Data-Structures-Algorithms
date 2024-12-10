f31e6280f5fc8ba9e1f04a941fafa6b40f5d8767
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-10 10:27:23 +0530

    Shortest path in a graph using topo sort

--- Practice Set 2/Graphs/G27 - Shortest Path in Directed Acyclic Graph - Topological Sort.py
+++ Practice Set 2/Graphs/G27 - Shortest Path in Directed Acyclic Graph - Topological Sort.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/shortest-path-in-undirected-graph/1
+# Solution - https://www.youtube.com/watch?v=ZUFQfFaU-8U&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=27
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -49,20 +53,30 @@ class Solution:
     
     @staticmethod
     def get_shortest_paths(graph, source):
+        """
+            Time complexity is O(V + E) and space complexity is O(V).
+        """
+
+        # get the topo sort of the graph in the stack
         stack = Stack()
         visited = {i: False for i in graph}
         for node in graph:
             if not visited[node]:
                 Solution._dfs(graph, node, visited, stack)
-        
+
+        # mark the distance of the source node as 0.
         distances = {i: 1e6 for i in graph}
         distances[source] = 0
+
+        # until the stack gets empty, relax the edges and update the minimum distances.
         while not stack.is_empty():
             node = stack.pop()
             for adj in graph[node]:
                 adj_node, wt = adj
                 if distances[adj_node] > distances[node] + wt:
                     distances[adj_node] = distances[node] + wt
+
+        # return the shortest paths
         return distances.values()
     
 
