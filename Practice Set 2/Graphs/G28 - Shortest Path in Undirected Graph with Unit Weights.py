9c25ef413b5b0b4dbdfde74e5900058a7e737d21
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-10 10:37:16 +0530

    Shortest path in an undirected graph using BFS

--- Practice Set 2/Graphs/G28 - Shortest Path in Undirected Graph with Unit Weights.py
+++ Practice Set 2/Graphs/G28 - Shortest Path in Undirected Graph with Unit Weights.py
@@ -31,3 +31,66 @@ class Queue:
         self.length -= 1
         return item
 
+
+class Solution:
+    @staticmethod
+    def get_shortest_path(graph, source):
+        if source not in graph:
+            return
+        queue = Queue()
+        distances = {i: 1e6 for i in graph}
+        distances[source] = 0
+        queue.push((source, 0))
+        while not queue.is_empty():
+            node, distance = queue.pop()
+            for adj_node in graph[node]:
+                if distances[adj_node] > distance + 1:
+                    distances[adj_node] = distance + 1
+                    queue.push((adj_node, distances[adj_node]))
+        return distances.values()
+
+
+print(
+    Solution.get_shortest_path(
+        {
+            0: [1, 3],
+            1: [0, 2],
+            2: [1, 6],
+            3: [0, 4],
+            4: [3, 5],
+            5: [4, 6],
+            6: [2, 5, 7, 8],
+            7: [6, 8],
+            8: [6, 7]
+        },
+        0
+    )
+)
+
+print(
+    Solution.get_shortest_path(
+        {
+            0: [3],
+            1: [3],
+            2: [],
+            3: [0, 1]
+        },
+        3
+    )
+)
+
+print(
+    Solution.get_shortest_path(
+        {
+            0: [1, 3],
+            1: [0, 2],
+            2: [1],
+            3: [0, 4, 7],
+            4: [3, 5, 6, 7],
+            5: [4, 6],
+            6: [4, 5, 7],
+            7: [3, 4, 6]
+        },
+        0
+    )
+)
