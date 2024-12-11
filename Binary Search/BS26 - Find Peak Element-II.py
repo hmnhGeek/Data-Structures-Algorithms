0b56c30ad7ae1e2a6a4691f53a330bbe3daba35e
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-11 09:52:59 +0530

    Peak element in 2D Matrix

--- Binary Search/BS26 - Find Peak Element-II.py
+++ Binary Search/BS26 - Find Peak Element-II.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/find-peak-element-2d-array/
+# Solution - https://www.youtube.com/watch?v=nGGp5XBzC4g&list=PLgUwDviBIf0pMFMWuuvDNMAkoQFi-h0ZF&index=28
+
+
 class Solution:
     @staticmethod
     def _get_max(mtx, mid, n, m):
@@ -10,19 +14,34 @@ class Solution:
 
     @staticmethod
     def find_peak(mtx):
+        """
+            Overall time complexity is O(n * log(m)) and space complexity is O(1).
+        """
+
         n, m = len(mtx), len(mtx[0])
+        # define the search space as the columns.
         low, high = 0, m - 1
         while low <= high:
+            # get the mid-column index
             mid = int(low + (high - low) / 2)
+            # find the row index of the max element in the mid-column. This will take O(n) time.
             max_elem_row_idx = Solution._get_max(mtx, mid, n, m)
+
+            # get the left and right element of the mid. No need to check for top and bottom because the element is max
+            # in its column.
             left = mtx[max_elem_row_idx][mid - 1] if mid - 1 >= 0 else -1
             right = mtx[max_elem_row_idx][mid + 1] if mid + 1 < n else -1
             possible_peak = mtx[max_elem_row_idx][mid]
+
+            # if it is a peak from left and right perspective also, then return this element's coordinates.
             if possible_peak > left and possible_peak > right:
                 return max_elem_row_idx, mid
+
+            # if left is greater, the peak might lie in the left half columns
             elif left > possible_peak:
                 high = mid - 1
             else:
+                # else peak must be in the right half columns
                 low = mid + 1
         return None, None
 
