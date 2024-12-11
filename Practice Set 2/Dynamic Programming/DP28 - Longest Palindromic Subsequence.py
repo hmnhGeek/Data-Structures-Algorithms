6d9d30ea3e2e026dfa24dfd08f3040755c7b4f65
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-11 08:21:01 +0530

    Space Optimized LCS

--- Practice Set 2/Dynamic Programming/DP28 - Longest Palindromic Subsequence.py
+++ Practice Set 2/Dynamic Programming/DP28 - Longest Palindromic Subsequence.py
@@ -69,8 +69,33 @@ def tabulation():
     print(lcs("abcd", "abzd"))
 
 
+def space_optimized():
+    """
+        Time complexity is O(n * m) and space complexity is O(m).
+    """
+    def lcs(s1, s2):
+        n, m = len(s1), len(s2)
+        prev = {j: 0 for j in range(m + 1)}
+        for i in range(1, n + 1):
+            curr = {j: 0 for j in range(m + 1)}
+            for j in range(1, m + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    curr[j] = 1 + prev[j - 1]
+                else:
+                    curr[j] = max(
+                        prev[j],
+                        curr[j - 1]
+                    )
+            prev = curr
+        return prev[m]
+
+    print(lcs("abcd", "abzd"))
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
+print()
+space_optimized()
