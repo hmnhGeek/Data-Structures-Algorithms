894f0220d7f630b554bb0ff555c8117cb23105d9
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-11 07:47:40 +0530

    Word Ladder 2 using BFS

--- Practice Set 2/Graphs/G30 - Word Ladder 2.py
+++ Practice Set 2/Graphs/G30 - Word Ladder 2.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/word-ladder-ii/1
+# Solution - https://www.youtube.com/watch?v=DREutrv2XD0&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=30
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -35,37 +39,76 @@ class Queue:
 class Solution:
     @staticmethod
     def word_ladder(word_list, start_word, end_word):
+        """
+            Time complexity is O(n * word_length * 26) and space complexity is O(n).
+        """
+
+        # same as word ladder 1 code.
         hash_set = set(word_list)
         queue = Queue()
         queue.push([start_word])
+
+        # create an array to store the words used on a level
         used_on_level = [start_word]
+
+        # take initial level as 0
         level = 0
+
+        # take a result variable to store all the answers.
         result = []
+
+        # standard BFS...
         while not queue.is_empty():
+            # pop the vector
             vector = queue.pop()
+
+            # if length of vector is greater than level, it means we have crossed to the next level.
             if len(vector) > level:
+                # increment the level
                 level += 1
+
+                # remove all the used words from the set.
                 for used in used_on_level:
                     if used in hash_set:
                         hash_set.remove(used)
+
+            # get the last word from the sequence
             word = vector[-1]
 
+            # if the last word is the end word
             if word == end_word:
+                # and this is the first time we are seeing an answer, push it to result.
                 if len(result) == 0:
                     result.append(vector)
+                # or if this vector length is same as the length of first vector, we have another answer, push it also.
                 elif len(result[0]) == len(vector):
                     result.append(vector)
 
+            # now loop on the index of the word
             for i in range(len(word)):
+                # create a temp array to modify the word
                 temp = [w for w in word]
+
+                # replace ith character from all the alphabets.
                 for char in "abcdefghijklmnopqrstuvwxyz":
                     temp[i] = char
+
+                    # form the new word
                     new_word = "".join(temp)
+
+                    # check if new word is present in hash set
                     if new_word in hash_set:
+                        # add the new word into a new vector
                         new_sq = [s for s in vector]
                         new_sq += [new_word]
+
+                        # push the new vector into the queue
                         queue.push(new_sq)
+
+                        # add the new word in used_on_level.
                         used_on_level.append(new_word)
+
+        # return the result.
         return result
 
 
