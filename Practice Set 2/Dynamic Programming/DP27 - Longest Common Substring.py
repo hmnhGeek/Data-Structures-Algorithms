c46f5d36f6e5257789b5aa38af065b44fd8fe9a0
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-11 08:06:47 +0530

    Longest Common Substring using Space Optimization

--- Practice Set 2/Dynamic Programming/DP27 - Longest Common Substring.py
+++ Practice Set 2/Dynamic Programming/DP27 - Longest Common Substring.py
@@ -28,4 +28,38 @@ def tabulation():
     print(lcs("abc", ""))
 
 
+def space_optimized():
+    def lcs(s1, s2):
+        """
+            Overall time complexity is O(nm) and space complexity is O(m).
+        """
+
+        n, m = len(s1), len(s2)
+        prev = {j: 0 for j in range(m + 1)}
+        longest_length = 0
+        for i in range(1, n + 1):
+            curr = {j: 0 for j in range(m + 1)}
+            for j in range(1, m + 1):
+                # if there's a match in the characters, set dp[i][j] = 1 + dp[i - 1[j - 1] to maintain the continuity
+                if s1[i - 1] == s2[j - 1]:
+                    curr[j] = 1 + prev[j - 1]
+                    # also ensure to always keep the maximum length
+                    longest_length = max(longest_length, curr[j])
+                else:
+                    # if the characters don't match, break the continuity and assign 0.
+                    curr[j] = 0
+            prev = curr
+        return longest_length
+
+    print(lcs("abcd", "abzd"))
+    print(lcs("abcjklp", "acjkp"))
+    print(lcs("wasdijkl", "wsdjkl"))
+    print(lcs("tyfg", "cvbnuty"))
+    print(lcs("GeeksforGeeks", "GeeksQuiz"))
+    print(lcs("abcdxyz", "xyzabcd"))
+    print(lcs("abc", ""))
+
+
 tabulation()
+print()
+space_optimized()
