fdf19834109a24c62308f845eed008d3721ee11c
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-11 08:18:09 +0530

    Memoized LCS

--- Practice Set 2/Dynamic Programming/DP28 - Longest Palindromic Subsequence.py
+++ Practice Set 2/Dynamic Programming/DP28 - Longest Palindromic Subsequence.py
@@ -21,4 +21,33 @@ def recursive():
     print(lcs("abcd", "abzd"))
 
 
+def memoized():
+    """
+        Time complexity is O(n * m) and space complexity is O(n + m + n*m).
+    """
+
+    def solve(s1, i, s2, j, dp):
+        if i == 0 or j == 0:
+            return 0
+        if dp[i][j] is not None:
+            return dp[i][j]
+        if s1[i - 1] == s2[j - 1]:
+            dp[i][j] = 1 + solve(s1, i - 1, s2, j - 1, dp)
+        else:
+            dp[i][j] = max(
+                solve(s1, i - 1, s2, j, dp),
+                solve(s1, i, s2, j - 1, dp)
+            )
+        return dp[i][j]
+
+    def lcs(s1, s2):
+        n, m = len(s1), len(s2)
+        dp = {i: {j: None for j in range(m + 1)} for i in range(n + 1)}
+        return solve(s1, n, s2, m, dp)
+
+    print(lcs("abcd", "abzd"))
+
+
 recursive()
+print()
+memoized()
