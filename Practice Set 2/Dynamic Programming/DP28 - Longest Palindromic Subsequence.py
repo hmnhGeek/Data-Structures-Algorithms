69b89ac9e269e7b8ff163d26ad7ee0f0fd1901ac
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-11 08:23:32 +0530

    Longest Palindromic Subsequence Length

--- Practice Set 2/Dynamic Programming/DP28 - Longest Palindromic Subsequence.py
+++ Practice Set 2/Dynamic Programming/DP28 - Longest Palindromic Subsequence.py
@@ -92,6 +92,28 @@ def space_optimized():
     print(lcs("abcd", "abzd"))
 
 
+class Solution:
+    @staticmethod
+    def lcs(s1, s2):
+        n, m = len(s1), len(s2)
+        dp = {i: {j: 0 for j in range(m + 1)} for i in range(n + 1)}
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(
+                        dp[i - 1][j],
+                        dp[i][j - 1]
+                    )
+        return dp[n][m]
+
+    @staticmethod
+    def lps_length(s):
+        reversed = s[-1:-len(s)-1:-1]
+        return Solution.lcs(s, reversed)
+
+
 recursive()
 print()
 memoized()
@@ -99,3 +121,7 @@ print()
 tabulation()
 print()
 space_optimized()
+
+print()
+print(Solution.lps_length("bbbab"))
+print(Solution.lps_length("bbabcbcab"))
