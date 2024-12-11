234a71fe0c53dc053be6f533e274b736e3d9e83d
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-11 08:27:32 +0530

    Longest Palindromic Subsequence String

--- Practice Set 2/Dynamic Programming/DP28 - Longest Palindromic Subsequence.py
+++ Practice Set 2/Dynamic Programming/DP28 - Longest Palindromic Subsequence.py
@@ -109,9 +109,46 @@ class Solution:
         return dp[n][m]
 
     @staticmethod
+    def lcs_string(s1, s2):
+        n, m = len(s1), len(s2)
+        dp = {i: {j: 0 for j in range(m + 1)} for i in range(n + 1)}
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(
+                        dp[i - 1][j],
+                        dp[i][j - 1]
+                    )
+        i, j = n, m
+        result = ""
+        while i > 0 and j > 0:
+            if s1[i - 1] == s2[j - 1]:
+                result += s1[i - 1]
+                i -= 1
+                j -= 1
+            elif dp[i - 1][j] > dp[i][j - 1]:
+                i -= 1
+            else:
+                j -= 1
+        return result[-1:-len(result)-1:-1]
+
+    @staticmethod
     def lps_length(s):
+        """
+            Time complexity is O(n^2) and Space complexity is O(n).
+        """
         reversed = s[-1:-len(s)-1:-1]
         return Solution.lcs(s, reversed)
+
+    @staticmethod
+    def lps_string(s):
+        """
+            Time complexity is O(n^2) and Space complexity is O(n).
+        """
+        reversed = s[-1:-len(s) - 1:-1]
+        return Solution.lcs_string(s, reversed)
 
 
 recursive()
@@ -125,3 +162,5 @@ space_optimized()
 print()
 print(Solution.lps_length("bbbab"))
 print(Solution.lps_length("bbabcbcab"))
+print(Solution.lps_string("bbbab"))
+print(Solution.lps_string("bbabcbcab"))
