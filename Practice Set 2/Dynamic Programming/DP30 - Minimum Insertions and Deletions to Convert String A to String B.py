1d331aa624e3c388039428ba56470448e3c87767
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-12 09:06:39 +0530

    Recursive LCS

---
+++ Practice Set 2/Dynamic Programming/DP30 - Minimum Insertions and Deletions to Convert String A to String B.py
@@ -0,0 +1,21 @@
+def recursive():
+    """
+        Time complexity is O(2^{m + n}) and space complexity is O(m + n).
+    """
+
+    def solve(s1, i, s2, j):
+        if i == 0 or j == 0:
+            return 0
+        if s1[i - 1] == s2[j - 1]:
+            return 1 + solve(s1, i - 1, s2, j - 1)
+        else:
+            return max(solve(s1, i - 1, s2, j), solve(s1, i, s2, j - 1))
+
+    def lcs_length(s1, s2):
+        n, m = len(s1), len(s2)
+        return solve(s1, n, s2, m)
+
+    print(lcs_length("abcd", "abzd"))
+
+
+recursive()
