9f76257722a781c9149f53e7ae4073df8181b2ae
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-12 08:56:02 +0530

    Tabulation LCS

--- Practice Set 2/Dynamic Programming/DP29 - Minimum Insertions to Make String Palindrome.py
+++ Practice Set 2/Dynamic Programming/DP29 - Minimum Insertions to Make String Palindrome.py
@@ -42,6 +42,27 @@ def memoized():
     print(min_inserts_for_palindrome("abcd", "abzd"))
 
 
+def tabulation():
+    """
+        Time complexity is O(m * n) and space complexity is O(m * n).
+    """
+    def min_inserts_for_palindrome(s1, s2):
+        n = len(s1)
+        m = len(s2)
+        dp = {i: {j: 0 for j in range(m + 1)} for i in range(n + 1)}
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return dp[n][m]
+
+    print(min_inserts_for_palindrome("abcd", "abzd"))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
