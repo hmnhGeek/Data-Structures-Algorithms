b51260c04fbb7a0e481a88f581e71f095a265bbf
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-12 10:09:15 +0530

    Get floor index using Binary Search

--- Practice Set 1/Binary Search/BS2 - Implement Lower Bound and Upper Bound.py
+++ Practice Set 1/Binary Search/BS2 - Implement Lower Bound and Upper Bound.py
@@ -41,6 +41,22 @@ class Solution:
     def get_insert_position(arr, x):
         return Solution.get_lower_bound(arr, x)
 
+    @staticmethod
+    def get_floor(arr, x):
+        """
+            Gives the largest index i such that arr[i] <= x.
+
+            Time complexity is O(log(n)) and space complexity is O(1).
+        """
+        low, high = 0, len(arr) - 1
+        while low <= high:
+            mid = int(low + (high - low)/2)
+            if arr[mid] > x:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return high
+
 
 print("Lower Bounds")
 print(Solution.get_lower_bound([3, 5, 8, 15, 19], 10))
@@ -60,3 +76,7 @@ print(Solution.get_insert_position([1, 2, 2, 4, 7], 6))
 print(Solution.get_insert_position([1, 2, 4, 7], 9))
 print(Solution.get_insert_position([2, 5, 7], 1))
 print(Solution.get_insert_position([1, 2, 4, 7], 2))
+print()
+print("Floor")
+print(Solution.get_floor([10, 20, 30, 40, 50], 25))
+print(Solution.get_floor([10, 20, 30, 40, 50], 30))
