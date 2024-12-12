0c7bde9e637fd20fea1f5c57f14c021537e86ccc
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-12 08:54:24 +0530

    Memoized LCS

--- Practice Set 2/Dynamic Programming/DP29 - Minimum Insertions to Make String Palindrome.py
+++ Practice Set 2/Dynamic Programming/DP29 - Minimum Insertions to Make String Palindrome.py
@@ -1,4 +1,7 @@
 def recursive():
+    """
+        Time complexity is O(2^{n + m}) and space complexity is O(m + n).
+    """
     def solve(s1, i, s2, j):
         if i == 0 or j == 0:
             return 0
@@ -15,4 +18,30 @@ def recursive():
     print(min_inserts_for_palindrome("abcd", "abzd"))
 
 
+def memoized():
+    """
+        Time complexity is O(m * n) and space complexity is O(m + n + m*n).
+    """
+    def solve(s1, i, s2, j, dp):
+        if i == 0 or j == 0:
+            return 0
+        if dp[i][j] is not None:
+            return dp[i][j]
+        if s1[i - 1] == s2[j - 1]:
+            dp[i][j] = 1 + solve(s1, i - 1, s2, j - 1, dp)
+        else:
+            dp[i][j] = max(solve(s1, i - 1, s2, j, dp), solve(s1, i, s2, j - 1, dp))
+        return dp[i][j]
+
+    def min_inserts_for_palindrome(s1, s2):
+        n = len(s1)
+        m = len(s2)
+        dp = {i: {j: None for j in range(m + 1)} for i in range(n + 1)}
+        return solve(s1, n, s2, m, dp)
+
+    print(min_inserts_for_palindrome("abcd", "abzd"))
+
+
 recursive()
+print()
+memoized()
