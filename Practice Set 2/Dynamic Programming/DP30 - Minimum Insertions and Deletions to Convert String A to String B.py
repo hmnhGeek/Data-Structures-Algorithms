393aaa036129aaa2c08282cd56cb8ce529959447
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-12 09:09:36 +0530

    Tabulation LCS

--- Practice Set 2/Dynamic Programming/DP30 - Minimum Insertions and Deletions to Convert String A to String B.py
+++ Practice Set 2/Dynamic Programming/DP30 - Minimum Insertions and Deletions to Convert String A to String B.py
@@ -42,6 +42,27 @@ def memoized():
     print(lcs_length("abcd", "abzd"))
 
 
+def tabulation():
+    """
+        Time complexity is O(m * n) and space complexity is O(m * n).
+    """
+
+    def lcs_length(s1, s2):
+        n, m = len(s1), len(s2)
+        dp = {i: {j: 0 for j in range(m + 1)} for i in range(n + 1)}
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return dp[n][m]
+
+    print(lcs_length("abcd", "abzd"))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
