f1b30bb55d5143b95e771e04f4673ac82a1d4279
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-12 09:15:55 +0530

    Minimum Insertions/Deletions to Convert String A to String B

--- Practice Set 2/Dynamic Programming/DP30 - Minimum Insertions and Deletions to Convert String A to String B.py
+++ Practice Set 2/Dynamic Programming/DP30 - Minimum Insertions and Deletions to Convert String A to String B.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.naukri.com/code360/problems/can-you-make_4244510?source=youtube&campaign=striver_dp_videos
+# Solution - https://www.youtube.com/watch?v=yMnH0jrir0Q&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=31
+
+
 def recursive():
     """
         Time complexity is O(2^{m + n}) and space complexity is O(m + n).
@@ -82,10 +86,34 @@ def space_optimized():
     print(lcs_length("abcd", "abzd"))
 
 
-recursive()
-print()
-memoized()
-print()
-tabulation()
-print()
-space_optimized()
+class Solution:
+    @staticmethod
+    def _lcs_length(s1, s2):
+        n, m = len(s1), len(s2)
+        prev = {j: 0 for j in range(m + 1)}
+        for i in range(1, n + 1):
+            curr = {j: 0 for j in range(m + 1)}
+            for j in range(1, m + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    curr[j] = 1 + prev[j - 1]
+                else:
+                    curr[j] = max(prev[j], curr[j - 1])
+            prev = curr
+        return prev[m]
+
+    @staticmethod
+    def min_ops(s1, s2):
+        """
+            Overall time complexity is O(n * m) and space complexity is O(m).
+        """
+        lcs_length = Solution._lcs_length(s1, s2)
+        num_deletions = len(s1) - lcs_length
+        num_insertions = len(s2) - lcs_length
+        return num_deletions + num_insertions
+
+
+print(Solution.min_ops("abcd", "anc"))
+print(Solution.min_ops("aaa", "aa"))
+print(Solution.min_ops("edl", "xcqja"))
+print(Solution.min_ops("heap", "pea"))
+print(Solution.min_ops("geeksforgeeks", "geeks"))
