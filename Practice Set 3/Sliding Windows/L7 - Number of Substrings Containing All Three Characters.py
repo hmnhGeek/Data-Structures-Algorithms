14becb834e9181e4db2e484e69e4caa846804a97
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-12 10:46:05 +0530

    Number of Substrings Containing All Three Characters

--- Practice Set 3/Sliding Windows/L7 - Number of Substrings Containing All Three Characters.py
+++ Practice Set 3/Sliding Windows/L7 - Number of Substrings Containing All Three Characters.py
@@ -1,17 +1,38 @@
+# Problem link - https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/description/
+# Solution - https://www.youtube.com/watch?v=xtqN4qlgr8s&list=PLgUwDviBIf0q7vrFA_HEWcqRqMpCXzYAL&index=7
+
+
 class Solution:
     @staticmethod
     def get_substrings(string):
+        """
+            Time complexity O(n) and space complexity is O(1).
+        """
+
+        # define window variables
         n = len(string)
         left = right = 0
+
+        # make a dictionary to track character count
         d = {"a": 0, "b": 0, "c": 0}
+
+        # count the number of substrings in `count`.
         count = 0
+
+        # while there is ground to cover.
         while right < n:
+            # increment d_right.
             d[string[right]] += 1
+            # while all 3 characters are available in the window
             while sum(1 for v in d.values() if v > 0) == 3:
+                # increment the count
                 count += (n - right)
+                # and shrink from the left
                 d[string[left]] -= 1
                 left += 1
+            # increment right
             right += 1
+        # return the count.
         return count
 
 
