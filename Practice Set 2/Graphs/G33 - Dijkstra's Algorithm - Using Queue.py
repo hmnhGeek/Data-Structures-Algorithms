6281e5daa39660ddf919b016754145b9a075cf41
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-12 08:41:43 +0530

    Dijkstra Algorithm using Queue

--- Practice Set 2/Graphs/G33 - Dijkstra's Algorithm - Using Queue.py
+++ Practice Set 2/Graphs/G33 - Dijkstra's Algorithm - Using Queue.py
@@ -31,3 +31,58 @@ class Queue:
         self.length -= 1
         return item
 
+
+class Solution:
+    @staticmethod
+    def get_shortest_path(graph, source):
+        if source not in graph:
+            return
+        distances = {i: 1e6 for i in graph}
+        distances[source] = 0
+        queue = Queue()
+        queue.push((source, 0))
+        while not queue.is_empty():
+            node, distance = queue.pop()
+            for adj in graph[node]:
+                adj_node, wt = adj
+                if distances[adj_node] > distance + wt:
+                    distances[adj_node] = distance + wt
+                    queue.push((adj_node, distances[adj_node]))
+        return distances.values()
+
+
+print(
+    Solution.get_shortest_path(
+        {
+            0: [[1, 4], [2, 4]],
+            1: [[0, 4], [2, 2]],
+            2: [[0, 4], [1, 2], [3, 3], [5, 6], [4, 1]],
+            3: [[2, 3], [5, 2]],
+            4: [[2, 1], [5, 3]],
+            5: [[3, 2], [2, 6], [4, 3]]
+        },
+        0
+    )
+)
+
+
+print(
+    Solution.get_shortest_path(
+        {
+            0: [[1, 9]],
+            1: [[0, 9]]
+        },
+        0
+    )
+)
+
+print(
+    Solution.get_shortest_path(
+        {
+            0: [[1, 1], [2, 6]],
+            1: [[0, 1], [2, 3]],
+            2: [[0, 6], [1, 3]]
+        },
+        2
+    )
+)
