91b36f0e701f3bd916c18a3195cb388e1276853a
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-13 18:35:48 +0530

    Shortest distance in a binary maze

--- Practice Set 2/Graphs/G36 - Shortest Distance in a Binary Maze.py
+++ Practice Set 2/Graphs/G36 - Shortest Distance in a Binary Maze.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/shortest-path-in-a-binary-maze-1655453161/1
+# Solution - https://www.youtube.com/watch?v=U5Mw4eyUmw4&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=36
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -48,23 +52,39 @@ class Solution:
 
     @staticmethod
     def shortest_distance(mtx, source, destination):
+        """
+            Overall time complexity is O(m*n) and space complexity is O(m*n).
+        """
+
         n, m = len(mtx), len(mtx[0])
+
+        # edge case checks
         if source[0] not in range(n) or source[1] not in range(m):
             return
         if destination[0] not in range(n) or destination[1] not in range(m):
             return
+
+        # mark source node in distance matrix as 0.
         distances = [[1e6 for _ in range(m)] for _ in range(n)]
         distances[source[0]][source[1]] = 0
+
+        # push the source node into the queue.
         queue = Queue()
         queue.push((*source, 0))
+
         while not queue.is_empty():
+            # pop the current cell
             x, y, distance = queue.pop()
+            # get its valid neighbours
             neighbours = Solution._get_neighbours(mtx, x, y, n, m)
             for neighbour in neighbours:
                 p, q = neighbour
+                # update the distances if required and push it to the queue
                 if distances[p][q] > distance + 1:
                     distances[p][q] = distance + 1
                     queue.push((p, q, distances[p][q]))
+
+        # return the shortest distance of the destination cell from the source cell.
         return distances[destination[0]][destination[1]]
 
 
