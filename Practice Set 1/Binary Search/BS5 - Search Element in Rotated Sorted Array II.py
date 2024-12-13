f4993964be91a4fc9e3e83f7a64a680f99049416
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-13 15:39:05 +0530

    Search in a rotated sorted array II, using binary search

--- Practice Set 1/Binary Search/BS5 - Search Element in Rotated Sorted Array II.py
+++ Practice Set 1/Binary Search/BS5 - Search Element in Rotated Sorted Array II.py
@@ -1,24 +1,44 @@
+# Problem link - https://www.naukri.com/code360/problems/search-in-a-rotated-sorted-array-ii_7449547
+# Solution - https://www.youtube.com/watch?v=w2G2W8l__pc&list=PLgUwDviBIf0pMFMWuuvDNMAkoQFi-h0ZF&index=6
+
+
 class Solution:
     @staticmethod
     def search(arr, x):
+        """
+            Time complexity is O(log(n)) and space complexity is O(1).
+        """
+
+        # define search space
         low, high = 0, len(arr) - 1
         while low <= high:
             mid = int(low + (high - low) / 2)
+            # return mid if x is found.
             if arr[mid] == x:
                 return mid
+            # if all three indices point to the same value, it is impossible to find the sorted/unsorted parts, hence,
+            # shrink the search space and continue.
             if arr[low] == arr[mid] == arr[high]:
                 low += 1
                 high -= 1
                 continue
+            # if right part is sorted
             if arr[mid] <= arr[high]:
+                # and x could be in right part
                 if arr[mid] <= x <= arr[high]:
+                    # search in right part
                     low = mid + 1
                 else:
+                    # else, search in left part
                     high = mid - 1
+            # if left part is sorted and x could be in left part
             elif arr[low] <= x <= arr[mid]:
+                # search in left part
                 high = mid - 1
             else:
+                # else, search in right part
                 low = mid + 1
+        # return -1, if element is not found
         return -1
 
 
