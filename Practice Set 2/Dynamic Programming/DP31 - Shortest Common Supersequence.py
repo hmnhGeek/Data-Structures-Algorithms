8cd9c3828da6ba0fd9cf0e30dd0ccfbd9183b669
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-13 17:22:16 +0530

    Tabulation LCS

--- Practice Set 2/Dynamic Programming/DP31 - Shortest Common Supersequence.py
+++ Practice Set 2/Dynamic Programming/DP31 - Shortest Common Supersequence.py
@@ -19,7 +19,7 @@ def recursive():
 
 def memoized():
     """
-        Time complexity is O(2^{m + n}) and space complexity is O(m + n).
+        Time complexity is O(mn) and space complexity is O(m + n + mn).
     """
     def solve(s1, i, s2, j, dp):
         if i == 0 or j == 0:
@@ -40,6 +40,26 @@ def memoized():
     print(lcs_length("abcd", "abzd"))
 
 
+def tabulation():
+    """
+        Time complexity is O(mn) and space complexity is O(mn).
+    """
+    def lcs_length(s1, s2):
+        n, m = len(s1), len(s2)
+        dp = {i: {j: 0 for j in range(m + 1)} for i in range(n + 1)}
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return dp[n][m]
+
+    print(lcs_length("abcd", "abzd"))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
