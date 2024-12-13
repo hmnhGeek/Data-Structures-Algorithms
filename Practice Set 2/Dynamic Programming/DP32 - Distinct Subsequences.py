356afa5aaad0c9d99c96c0dfab4b0db3f4559553
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-13 16:40:37 +0530

    Memoized distinct subsequences

--- Practice Set 2/Dynamic Programming/DP32 - Distinct Subsequences.py
+++ Practice Set 2/Dynamic Programming/DP32 - Distinct Subsequences.py
@@ -1,6 +1,6 @@
 def recursive():
     """
-        Time complexity is O(2^n) and space complexity is O(n).
+        Time complexity is O(2^{n + m}) and space complexity is O(n + m).
     """
     def solve(s1, i, s2, j):
         # if both the strings get exhausted at the same time, return 1 as we have found a match
@@ -36,4 +36,48 @@ def recursive():
     print(distinct_subsequences("geeksforgeeks", "ge"))
 
 
+def memoized():
+    """
+        Time complexity is O(n * m) and space complexity is O(n + m + nm).
+    """
+    def solve(s1, i, s2, j, dp):
+        # if both the strings get exhausted at the same time, return 1 as we have found a match
+        if i == 0 and j == 0:
+            return 1
+        # if s1 is exhausted but s2 still has characters, we have not found a match.
+        if i == 0:
+            return 0
+        # if s2 is exhausted but s1 has characters, we have found a match, return 1.
+        if j == 0:
+            return 1
+
+        if dp[i][j] is not None:
+            return dp[i][j]
+
+        # if ith and jth characters match, then we have two options.
+        if s1[i - 1] == s2[j - 1]:
+            # option 1: use the match and decrement both indices
+            # option 2: don't match with current i character, match with some other lower index in s1.
+            # add the results from above two options
+            return solve(s1, i - 1, s2, j - 1, dp) + solve(s1, i - 1, s2, j, dp)
+        else:
+            # if there is no match, use option 2 from above.
+            return solve(s1, i - 1, s2, j, dp)
+
+    def distinct_subsequences(s1, s2):
+        n, m = len(s1), len(s2)
+        dp = {i: {j: None for j in range(m + 1)} for i in range(n + 1)}
+        return solve(s1, n, s2, m, dp)
+
+    print(distinct_subsequences("babgbag", "bag"))
+    print(distinct_subsequences("brootgroot", "brt"))
+    print(distinct_subsequences("dingdingdingding", "ing"))
+    print(distinct_subsequences("aaaaa", "a"))
+    print(distinct_subsequences("rabbbit", "rabbit"))
+    print(distinct_subsequences("banana", "ban"))
+    print(distinct_subsequences("geeksforgeeks", "ge"))
+
+
 recursive()
+print()
+memoized()
