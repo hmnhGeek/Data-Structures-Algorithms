98275fde6025d26bd224621b1a5b7a1a08216e0a
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-13 18:28:43 +0530

    Shortest distance in a binary maze

--- Practice Set 2/Graphs/G36 - Shortest Distance in a Binary Maze.py
+++ Practice Set 2/Graphs/G36 - Shortest Distance in a Binary Maze.py
@@ -32,3 +32,53 @@ class Queue:
         return item
 
 
+class Solution:
+    @staticmethod
+    def _get_neighbours(mtx, i, j, n, m):
+        neighbours = []
+        if 0 <= i - 1 < n and mtx[i - 1][j] == 1:
+            neighbours.append((i - 1, j))
+        if 0 <= j + 1 < m and mtx[i][j + 1] == 1:
+            neighbours.append((i, j + 1))
+        if 0 <= i + 1 < n and mtx[i + 1][j] == 1:
+            neighbours.append((i + 1, j))
+        if 0 <= j - 1 < m and mtx[i][j - 1] == 1:
+            neighbours.append((i, j - 1))
+        return neighbours
+
+    @staticmethod
+    def shortest_distance(mtx, source, destination):
+        n, m = len(mtx), len(mtx[0])
+        if source[0] not in range(n) or source[1] not in range(m):
+            return
+        if destination[0] not in range(n) or destination[1] not in range(m):
+            return
+        distances = [[1e6 for _ in range(m)] for _ in range(n)]
+        distances[source[0]][source[1]] = 0
+        queue = Queue()
+        queue.push((*source, 0))
+        while not queue.is_empty():
+            x, y, distance = queue.pop()
+            neighbours = Solution._get_neighbours(mtx, x, y, n, m)
+            for neighbour in neighbours:
+                p, q = neighbour
+                if distances[p][q] > distance + 1:
+                    distances[p][q] = distance + 1
+                    queue.push((p, q, distances[p][q]))
+        return distances[destination[0]][destination[1]]
+
+
+print(
+    Solution.shortest_distance(
+        [
+            [1, 1, 1, 1],
+            [1, 1, 0, 1],
+            [1, 1, 1, 1],
+            [1, 1, 0, 0],
+            [1, 0, 0, 0]
+        ],
+        (0, 1),
+        (2, 2)
+    )
+)
+
