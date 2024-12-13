ca98fa7da846fcf5b0e6e0dd77ee282295702c13
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-13 15:57:55 +0530

    Longest repeating character replacement

---
+++ Practice Set 3/Sliding Windows/L8 - Longest Repeating Character Replacement.py
@@ -0,0 +1,24 @@
+class Solution:
+    @staticmethod
+    def solve(string, k):
+        left = right = 0
+        n = len(string)
+        longest_length = 0
+        start_index = -1
+        d = {i: 0 for i in string}
+        while right < n:
+            d[string[right]] += 1
+            if (right - left + 1) - max(d.values()) > k:
+                d[string[left]] -= 1
+                left += 1
+            if (right - left + 1) - max(d.values()) <= k:
+                longest_length = max(longest_length, right - left + 1)
+                start_index = left
+            right += 1
+        return string[start_index:start_index+longest_length] if start_index != -1 else ""
+
+
+print(Solution.solve("AABABBA", 2))
+print(Solution.solve("ABAB", 2))
+print(Solution.solve("AABABBA", 1))
+print(Solution.solve("ADBD", 1))
