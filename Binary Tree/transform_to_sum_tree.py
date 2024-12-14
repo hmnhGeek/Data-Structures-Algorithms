cf8a04fdaf85714aef4fa5b67a8e70439fd92943
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-14 12:35:01 +0530

    Transform Binary Tree to Sum Tree -  Added more examples

--- Binary Tree/transform_to_sum_tree.py
+++ Binary Tree/transform_to_sum_tree.py
@@ -7,6 +7,8 @@ class Node:
 class Solution:
     @staticmethod
     def convert(root: Node):
+        if root is None:
+            return 0
         if root.left is None and root.right is None:
             temp = root.data
             root.data = 0
@@ -41,3 +43,23 @@ n6.right = n5
 Solution.show(n10)
 Solution.convert(n10)
 Solution.show(n10)
+
+# Example 2
+n5, n1, n8, n4, n6 = Node(5), Node(1), Node(-8), Node(4), Node(6)
+n5.right = n1
+n1.left = n8
+n1.right = n4
+n8.left = n6
+Solution.show(n5)
+Solution.convert(n5)
+Solution.show(n5)
+
+# Example 3
+n1, n11, n2, n3, n111 = Node(1), Node(1), Node(-2), Node(3), Node(1)
+n1.left = n2
+n1.right = n11
+n2.left = n3
+n2.right = n111
+Solution.show(n1)
+Solution.convert(n1)
+Solution.show(n1)
