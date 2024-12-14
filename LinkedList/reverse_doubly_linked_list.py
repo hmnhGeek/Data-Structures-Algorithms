be589b869369836e56aabcdacc0673f4788826e9
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-14 11:21:17 +0530

    Reversing a doubly linked list

---
+++ LinkedList/reverse_doubly_linked_list.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, data):
+        self.prev = self.next = None
+        self.data = data
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = self.tail = None
+        self.length = 0
+
+    def is_empty(self):
+        return self.length == 0
+
+    def push(self, x):
+        node = Node(x)
+        if self.is_empty():
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = node
+        self.length += 1
+
+    def build(self, *args):
+        for i in args:
+            self.push(i)
+
+    def show(self):
+        curr = self.head
+        while curr is not None:
+            print(curr.data, end=" ")
+            curr = curr.next
+        print()
+
+    def reverse(self):
+        prev, curr = None, self.head
+        while curr is not None:
+            next_curr = curr.next
+            curr.next = prev
+            curr.prev = next_curr
+            prev = curr
+            curr = next_curr
+        self.head, self.tail = self.tail, self.head
+
+
+def test(*args):
+    dll = DoublyLinkedList()
+    dll.build(*args)
+    print("Before reversing")
+    dll.show()
+    dll.reverse()
+    print("After reversing")
+    dll.show()
+    print()
+
+
+test(3, 4, 5)
