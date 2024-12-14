dc97709db47557a86d69c78c23e506bb99dde0de
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-14 12:36:59 +0530

    Transform Binary Tree to Sum Tree

--- Binary Tree/transform_to_sum_tree.py
+++ Binary Tree/transform_to_sum_tree.py
@@ -1,3 +1,6 @@
+# Problem link - https://www.geeksforgeeks.org/problems/transform-to-sum-tree/1
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -7,16 +10,31 @@ class Node:
 class Solution:
     @staticmethod
     def convert(root: Node):
+        """
+            Overall time complexity is O(n) and space complexity is O(h).
+        """
+
+        # if the root node is None, return 0 sum.
         if root is None:
             return 0
+
+        # if it's a leaf node
         if root.left is None and root.right is None:
             temp = root.data
+            # assign its data to be 0.
             root.data = 0
+            # return previous data + 0
             return temp + root.data
+
+        # compute sum from left subtree
         left_val = Solution.convert(root.left)
+        # compute sum from right subtree
         right_val = Solution.convert(root.right)
+        # store the current root data in temp
         temp = root.data
+        # reassign root's data to be left subtree sum + right subtree sum
         root.data = left_val + right_val
+        # and return temp + current root data
         return temp + root.data
 
     @staticmethod
