d3dc8e1457c516230490130a32a20c225eb9d282
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-15 20:00:06 +0530

    Tabulation word break problem

--- Dynamic Programming/word_break.py
+++ Dynamic Programming/word_break.py
@@ -32,7 +32,7 @@ def recursive():
 
 def memoized():
     """
-        Time complexity is O(n) and space complexity is O(n + n).
+        Time complexity is O(n^2) and space complexity is O(n + n).
     """
     def solve(string, i, dictionary, n, dp):
         # if the whole string is exhausted, it's because the whole string was able to call solve(). Thus all the word
@@ -67,6 +67,36 @@ def memoized():
     print(word_break("applepenapple", ["apple", "pen"]))
 
 
+def tabulation():
+    """
+        Time complexity is O(n^2) and space complexity is O(n).
+    """
+    def word_break(string, dictionary):
+        n = len(string)
+        dp = {i: False for i in range(n + 1)}
+        dp[n] = True
+        for i in range(n - 1, -1, -1):
+            # Typical front-partition DP from Striver's playlist.
+            temp = ""
+            for j in range(i, n):
+                temp += string[j]
+                # check if current substring is present in dictionary, and if it is, then check for the next
+                # partition as well, and if both return true, then we must return True.
+                if temp in dictionary and dp[j + 1]:
+                    dp[i] = True
+                    break
+        return dp[0]
+
+    print(word_break("ilike", ["i", "like", "sam", "sung", "samsung", "mobile"]))
+    print(word_break("ilikesamsung", ["i", "like", "sam", "sung", "samsung", "mobile"]))
+    print(word_break("catsandog", ["cats", "dog", "sand", "and", "cat"]))
+    print(word_break("storybook", ["story", "book"]))
+    print(word_break("monkeyandonkey", ["monkey", "donkey", "and"]))
+    print(word_break("applepenapple", ["apple", "pen"]))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
