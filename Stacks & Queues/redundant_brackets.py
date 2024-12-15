d53cf237ae37d22455eff8d8dc6d28e9c5f33571
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-15 18:48:16 +0530

    Redundant Brackets using Stack

--- Stacks & Queues/redundant_brackets.py
+++ Stacks & Queues/redundant_brackets.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/expression-contains-redundant-bracket-or-not/0
+# Solution - https://www.geeksforgeeks.org/problems/expression-contains-redundant-bracket-or-not/0
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -40,20 +44,34 @@ class Stack:
 class Solution:
     @staticmethod
     def redundant_brackets(expression: str) -> bool:
+        """
+            Time complexity is O(n) and space complexity is O(n).
+        """
+
         stack = Stack()
         for i in range(len(expression)):
             char = expression[i]
+            # if the character is either an operator or opening bracket, push it to the stack.
             if char in ["(", "+", "-", "*", "/"]:
                 stack.push(char)
+            # if the character is a closing bracket
             elif char == ")":
+                # assume the expression to have redundant brackets.
                 redundant_present = True
+                # while we don't find the opening bracket for this closing bracket...
                 while stack.top() != "(":
+                    # check if the top is an operator or not.
                     if stack.top() in ["+", "-", "*", "/"]:
+                        # if it is an operator, then this bracket is not redundant.
                         redundant_present = False
+                    # pop the top element from stack.
                     stack.pop()
+                # if `redundant_present` is still true, return True.
                 if redundant_present:
                     return True
+                # pop the opening bracket
                 stack.pop()
+        # return False if every character has been checked.
         return False
 
 
