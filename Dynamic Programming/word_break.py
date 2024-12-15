2991290ffa8ebb27319667c5deb06cfb18013567
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-15 20:01:27 +0530

    Word Break - Dynamic Programming

--- Dynamic Programming/word_break.py
+++ Dynamic Programming/word_break.py
@@ -1,3 +1,8 @@
+# Problem link - https://www.geeksforgeeks.org/problems/word-break1352/1
+# Solution - https://www.youtube.com/watch?v=oBUpyPZ08zU
+# Solution (Front Partitioning Logic) - https://www.youtube.com/watch?v=_H8V5hJUGd0
+
+
 def recursive():
     """
         Time complexity is O(2^n) and space complexity is O(n).
@@ -95,8 +100,12 @@ def tabulation():
     print(word_break("applepenapple", ["apple", "pen"]))
 
 
+print("Recursive Solution")
 recursive()
 print()
+print("Memoized Solution")
 memoized()
 print()
+print("Tabulation Solution")
 tabulation()
+print()
