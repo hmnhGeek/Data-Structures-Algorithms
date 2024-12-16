cc94d9ce3f4927626e2da0afed4197f41265d00f
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-16 09:49:22 +0530

    Path with minimum effort

--- Practice Set 2/Graphs/G37 - Path With Minimum Effort.py
+++ Practice Set 2/Graphs/G37 - Path With Minimum Effort.py
@@ -31,3 +31,50 @@ class Queue:
         self.length -= 1
         return item
 
+
+class Solution:
+    @staticmethod
+    def _get_neighbours(mtx, i, j, n, m):
+        neighbours = []
+        if 0 <= i - 1 < n:
+            neighbours.append((i - 1, j))
+        if 0 <= j + 1 < m:
+            neighbours.append((i, j + 1))
+        if 0 <= i + 1 < n:
+            neighbours.append((i + 1, j))
+        if 0 <= j - 1 < m:
+            neighbours.append((i, j - 1))
+        return neighbours
+
+    @staticmethod
+    def get_min_effort(mtx):
+        n, m = len(mtx), len(mtx[0])
+        queue = Queue()
+        distances = [[1e6 for _ in range(m)] for _ in range(n)]
+        distances[0][0] = 0
+        queue.push((0, 0, 0))
+        while not queue.is_empty():
+            x, y, max_diff = queue.pop()
+            neighbours = Solution._get_neighbours(mtx, x, y, n, m)
+            for neighbour in neighbours:
+                p, q = neighbour
+                diff = abs(mtx[x][y] - mtx[p][q])
+                if distances[p][q] > max(max_diff, diff):
+                    distances[p][q] = max(max_diff, diff)
+                    queue.push((p, q, max(max_diff, diff)))
+        return distances[n - 1][m - 1]
+
+
+print(
+    Solution.get_min_effort(
+        [
+            [1, 2, 2],
+            [3, 8, 2],
+            [5, 3, 5]
+        ]
+    )
+)
+
+print(Solution.get_min_effort([[7, 7], [7, 7]]))
+print(Solution.get_min_effort([[1, 2, 3], [3, 8, 4], [5, 3, 5]]))
+print(Solution.get_min_effort([[1, 2, 1, 1, 1], [1, 2, 1, 2, 1], [1, 2, 1, 2, 1], [1, 2, 1, 2, 1], [1, 1, 1, 2, 1]]))
