aef434d93897e876257b6d3b39f05b9dffdcb275
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-16 14:11:08 +0530

    Subarray count with k distinct integers

--- Practice Set 3/Sliding Windows/L11 - Subarray with k different integers.py
+++ Practice Set 3/Sliding Windows/L11 - Subarray with k different integers.py
@@ -1,6 +1,16 @@
+# Problem link - https://leetcode.com/problems/subarrays-with-k-different-integers/
+# Solution - https://www.youtube.com/watch?v=7wYGbV_LsX4&list=PLgUwDviBIf0q7vrFA_HEWcqRqMpCXzYAL&index=11
+
+
 class Solution:
     @staticmethod
     def _count_less_than_equal_to(arr, k):
+        """
+            Time complexity is O(n) and space complexity is O(1).
+
+            The code is similar to L10 and L9.
+        """
+
         if k < 0:
             return 0
         left = right = 0
@@ -9,6 +19,7 @@ class Solution:
         d = {i: 0 for i in arr}
         while right < n:
             d[arr[right]] += 1
+            # while the count of distinct integers > k, shrink from left
             while sum(1 for v in d.values() if v != 0) > k:
                 d[arr[left]] -= 1
                 left += 1
@@ -18,6 +29,9 @@ class Solution:
 
     @staticmethod
     def get_subarray_count(arr, k):
+        """
+            Time complexity is O(2n) and space complexity is O(1).
+        """
         return Solution._count_less_than_equal_to(arr, k) - Solution._count_less_than_equal_to(arr, k - 1)
 
 
