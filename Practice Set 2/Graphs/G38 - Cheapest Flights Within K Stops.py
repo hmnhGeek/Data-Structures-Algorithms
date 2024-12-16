7e9704317b639a8c81426b9ec46d16dd8459fb8d
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-16 11:37:44 +0530

    Cheapest flights with at most k stops

--- Practice Set 2/Graphs/G38 - Cheapest Flights Within K Stops.py
+++ Practice Set 2/Graphs/G38 - Cheapest Flights Within K Stops.py
@@ -29,4 +29,41 @@ class Queue:
         self.head = self.head.next
         del node
         self.length -= 1
+        return item
 
+
+class Solution:
+    @staticmethod
+    def cheapest_flights(graph, source, destination, k):
+        if source not in graph or destination not in graph:
+            return
+        queue = Queue()
+        queue.push((source, 0, k + 1))
+        distances = {i: 1e6 for i in graph}
+        distances[source] = 0
+        while not queue.is_empty():
+            node, distance, stops = queue.pop()
+            if stops == 0:
+                continue
+            for adj in graph[node]:
+                adj_node, wt = adj
+                if distances[adj_node] > distance + wt:
+                    distances[adj_node] = distance + wt
+                    queue.push((adj_node, distances[adj_node], stops - 1))
+        return distances[destination]
+
+
+print(
+    Solution.cheapest_flights(
+        {
+            0: [[1, 5], [3, 2]],
+            1: [[2, 5], [4, 1]],
+            2: [],
+            3: [[1, 2]],
+            4: [[2, 1]]
+        },
+        0,
+        2,
+        2
+    )
+)
