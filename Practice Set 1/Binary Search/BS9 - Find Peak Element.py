06d15e0c49751ca41157579309f3ca6a28340037
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-17 09:56:26 +0530

    Find Peak Element using binary search

--- Practice Set 1/Binary Search/BS9 - Find Peak Element.py
+++ Practice Set 1/Binary Search/BS9 - Find Peak Element.py
@@ -11,7 +11,7 @@ class Solution:
             return arr[-1]
         low, high = 1, len(arr) - 2
         while low <= high:
-            mid = int(low + (high - low)/2)
+            mid = int(low + (high - low) / 2)
             if arr[mid] > arr[mid - 1] and arr[mid] > arr[mid + 1]:
                 return arr[mid]
             elif arr[mid - 1] < arr[mid] < arr[mid + 1]:
@@ -26,3 +26,8 @@ class Solution:
 print(Solution.get_peak([1, 5, 1, 2, 1]))
 print(Solution.get_peak([1, 8, 1, 5, 3]))
 print(Solution.get_peak([1, 2, 1]))
+print(Solution.get_peak([1, 2, 3, 1]))
+print(Solution.get_peak([1, 2, 1, 3, 5, 6, 4]))
+print(Solution.get_peak([1, 2, 4, 5, 7, 8, 3]))
+print(Solution.get_peak([10, 20, 15, 2, 23, 90, 80]))
+print(Solution.get_peak([1, 2, 3]))
