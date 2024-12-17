c9f0eb727207dfca4e6833a596d38e41ba54a57e
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-17 18:14:40 +0530

    Max profit by selling stocks

--- Practice Set 2/Dynamic Programming/DP35 - Best Time to Buy and Sell Stock.py
+++ Practice Set 2/Dynamic Programming/DP35 - Best Time to Buy and Sell Stock.py
@@ -1,10 +1,25 @@
+# Problem link - https://www.naukri.com/code360/problems/stocks-are-profitable_893405?source=youtube&campaign=striver_dp_videos
+# Solution - https://www.youtube.com/watch?v=excAOvwF_Wk&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=36
+
+
 def max_profit(prices):
+    """
+        Time complexity is O(n) and space complexity is O(1).
+    """
+
+    # store the last day's price as max selling price for now.
     sp = prices[-1]
+    # assume the profit to be 0.
     profit = 0
+    # loop from the second last index till 0th index
     for i in range(-2, -len(prices) - 1, -1):
+        # get the earning by calculation sp - cp (on ith day)
         earning = sp - prices[i]
+        # update the max profit
         profit = max(profit, earning)
+        # also update the max selling price, to ensure that we always sell at max price.
         sp = max(sp, prices[i])
+    # return the max profit.
     return profit
 
 
