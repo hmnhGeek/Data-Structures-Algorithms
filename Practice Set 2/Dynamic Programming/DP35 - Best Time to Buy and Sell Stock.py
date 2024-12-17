349bba0b9887f06b31b5dfadeebf19bf39ac033e
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-17 18:12:50 +0530

    Max profit by selling stocks

--- Practice Set 2/Dynamic Programming/DP35 - Best Time to Buy and Sell Stock.py
+++ Practice Set 2/Dynamic Programming/DP35 - Best Time to Buy and Sell Stock.py
@@ -9,3 +9,11 @@ def max_profit(prices):
 
 
 print(max_profit([7, 1, 5, 3, 6, 4]))
+print(max_profit([2, 100, 150, 120]))
+print(max_profit([1, 2, 3, 4]))
+print(max_profit([2, 2, 2, 2]))
+print(max_profit([17, 20, 11, 9, 12, 6]))
+print(max_profit([98, 101, 66, 72]))
+print(max_profit([7, 6, 4, 3, 1]))
+print(max_profit([1, 3, 6, 9, 11]))
+print(max_profit([7, 10, 1, 3, 6, 9, 2]))
