ddcf10305d5e918d2c3c57eb8cacdb2d2a6d62dd
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-17 18:41:02 +0530

    Space optimized stocks 2

--- Practice Set 2/Dynamic Programming/DP36 - Buy and Sell Stock - II.py
+++ Practice Set 2/Dynamic Programming/DP36 - Buy and Sell Stock - II.py
@@ -94,8 +94,41 @@ def tabulation():
     print(max_profit([4, 2, 2, 2, 4]))
 
 
+def space_optimized():
+    """
+        Time complexity is O(2n) and space complexity is O(1).
+    """
+    def max_profit(arr):
+        n = len(arr)
+        nxt = {True: 0, False: 0}
+        for i in range(n - 1, -1, -1):
+            curr = {True: 0, False: 0}
+            for can_buy in [True, False]:
+                if can_buy:
+                    curr[can_buy] = max(
+                        -arr[i] + nxt[False],
+                        nxt[True]
+                    )
+                else:
+                    curr[can_buy] = max(
+                        arr[i] + nxt[True],
+                        nxt[False]
+                    )
+            nxt = curr
+        return nxt[True]
+
+    print(max_profit([7, 1, 5, 3, 6, 4]))
+    print(max_profit([1, 2, 3, 4, 5, 6, 7]))
+    print(max_profit([7, 6, 5, 4, 3, 2, 1]))
+    print(max_profit([1, 2, 3, 4, 5]))
+    print(max_profit([100, 180, 260, 310, 40, 535, 695]))
+    print(max_profit([4, 2, 2, 2, 4]))
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
+print()
+space_optimized()
