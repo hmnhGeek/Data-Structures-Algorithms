cbf6b5d33f99f9c84f4d4580405836e404efe9fe
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-17 09:59:18 +0530

    Find Peak Element using binary search

--- Practice Set 1/Binary Search/BS9 - Find Peak Element.py
+++ Practice Set 1/Binary Search/BS9 - Find Peak Element.py
@@ -1,25 +1,46 @@
 class Solution:
     @staticmethod
     def get_peak(arr):
+        # if there are no elements, return no peak
         if len(arr) == 0:
             return -1
+
+        # if there's only one element, return it as peak
         if len(arr) == 1:
             return arr[0]
+
+        # if 0th element is greater than 1st, return 0th element as peak
         if arr[0] > arr[1]:
             return arr[0]
+
+        # if last element is greater than second last, return last element as peak
         if arr[-1] > arr[-2]:
             return arr[-1]
+
+        # define a search space from 1 to n - 2
         low, high = 1, len(arr) - 2
+
+        # typical Binary Search
         while low <= high:
             mid = int(low + (high - low) / 2)
+
+            # if mid-element is a peak, return mid-element.
             if arr[mid] > arr[mid - 1] and arr[mid] > arr[mid + 1]:
                 return arr[mid]
+
+            # if there is an increasing line to the right side, peak must lie on the right side.
             elif arr[mid - 1] < arr[mid] < arr[mid + 1]:
                 low = mid + 1
+
+            # if there is a downward slope, peak must lie on the left side.
             elif arr[mid - 1] > arr[mid] > arr[mid + 1]:
                 high = mid - 1
+
+            # else, if mid-element is a local minima, the peak can be on both sides, here, we move to the right side.
             else:
                 low = mid + 1
+
+        # if no peak is found, return -1.
         return -1
 
 
