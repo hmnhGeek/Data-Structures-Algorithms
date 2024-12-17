c40524f2448ecef2a2d12c09ab7b98f1b5f31304
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-17 10:19:55 +0530

    Minimum multiplications to reach end

--- Practice Set 2/Graphs/G39 - Minimum Multiplications to Reach End.py
+++ Practice Set 2/Graphs/G39 - Minimum Multiplications to Reach End.py
@@ -35,19 +35,36 @@ class Queue:
 class Solution:
     @staticmethod
     def get_min(start, arr, end):
+        """
+            Time complexity is O(10**5 * n) and space complexity is O(10**5), where n is the size of array.
+        """
+
+        # define distances for all 10**5 nodes and mark source node distance as 0.
         distances = {i: 1e6 for i in range(10**5)}
         distances[start] = 0
+
+        # define a queue and push the start node.
         queue = Queue()
         queue.push((distances[start], start))
+
+        # typical Dijkstra
         while not queue.is_empty():
             distance, node = queue.pop()
             for i in arr:
+                # get the next node
                 next_node = (i * node) % 10**5
+                # and if the distance of the next node is larger than distance + 1, update the variable and push the
+                # next node into the queue.
                 if distances[next_node] > distance + 1:
                     distances[next_node] = distance + 1
                     queue.push((distances[next_node], next_node))
+
+                # we could wait for the queue to get empty, but since the operations are large, we can return as soon
+                # as we find the end node.
                 if next_node == end:
                     return distances[next_node]
+
+        # return -1 if end node was not found.
         return -1
 
 
