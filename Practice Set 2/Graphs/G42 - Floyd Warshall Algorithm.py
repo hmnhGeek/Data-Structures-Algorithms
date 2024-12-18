82db07bfd1a847f71f6121be4fdec903681ba10e
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-18 09:02:19 +0530

    Floyd Warshall Algorithm

--- Practice Set 2/Graphs/G42 - Floyd Warshall Algorithm.py
+++ Practice Set 2/Graphs/G42 - Floyd Warshall Algorithm.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/floyd-warshall-algorithm-dp-16/#floyd-warshall-algorithm
+# Solution - https://www.youtube.com/watch?v=YbY8cVwWAvw&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=42
+
+
 class Solution:
     @staticmethod
     def _get_adj_mtx(graph):
@@ -7,21 +11,33 @@ class Solution:
             for adj in graph[node]:
                 adj_node, wt = adj
                 mtx[node][adj_node] = wt
+        # self node wt should be 0.
         for i in range(n):
             mtx[i][i] = 0
         return mtx
 
     @staticmethod
     def floyd_warshall(graph):
+        """
+            Time complexity is O(V^3) and space is O(V^2).
+        """
+
+        # get the adjacency matrix from a graph using O(V^2) time and O(V^2) space.
         adj_mtx = Solution._get_adj_mtx(graph)
         n = len(graph)
+
+        # using a via node, relax all the edges in the adjacency matrix in O(V^3) time.
         for via_node in graph:
             for u in range(n):
                 for v in range(n):
                     adj_mtx[u][v] = min(adj_mtx[u][v], adj_mtx[u][via_node] + adj_mtx[via_node][v])
+
+        # In O(V) time check if there's a negative cycle or not.
         for i in range(n):
             if adj_mtx[i][i] < 0:
                 return "Negative cycle detected!"
+
+        # print the adjacency matrix after getting all the shortest distances.
         for i in range(n):
             for j in range(n):
                 print(adj_mtx[i][j] if adj_mtx[i][j] != 1e6 else -1, end=" ")
