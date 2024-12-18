32d27e49e7163d075b1f1a3d00d9e2ef77d56c9d
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-18 12:00:21 +0530

    Memoized Stocks IV

--- Practice Set 2/Dynamic Programming/DP38 - Buy and Stock Sell IV.py
+++ Practice Set 2/Dynamic Programming/DP38 - Buy and Stock Sell IV.py
@@ -34,4 +34,46 @@ def recursive():
     print(buy_and_sell([90, 80, 70, 60, 50], 1))
 
 
+def memoized():
+    """
+        Time complexity is O(2nk) and space complexity is O(n + 2nk).
+    """
+    def solve(arr, i, can_buy, tx, k, n, dp):
+        if tx == k:
+            return 0
+        if i == n:
+            return 0
+        if dp[i][can_buy][tx] is not None:
+            return dp[i][can_buy][tx]
+        if can_buy:
+            dp[i][can_buy][tx] = max(
+                -arr[i] + solve(arr, i + 1, False, tx, k, n, dp),
+                solve(arr, i + 1, True, tx, k, n, dp)
+            )
+        else:
+            dp[i][can_buy][tx] = max(
+                arr[i] + solve(arr, i + 1, True, tx + 1, k, n, dp),
+                solve(arr, i + 1, False, tx, k, n, dp)
+            )
+        return dp[i][can_buy][tx]
+
+    def buy_and_sell(arr, k):
+        n = len(arr)
+        dp = {i: {True: {j: None for j in range(k + 1)}, False: {j: None for j in range(k + 1)}} for i in range(n + 1)}
+        return solve(arr, 0, True, 0, k, n, dp)
+
+    print(buy_and_sell([3, 2, 6, 5, 0, 3], 2))
+    print(buy_and_sell([8, 5, 1, 3, 10], 2))
+    print(buy_and_sell([10, 8, 6, 2], 2))
+    print(buy_and_sell([2, 6, 5, 2], 0))
+    print(buy_and_sell([1, 2, 3, 5], 2))
+    print(buy_and_sell([2, 4, 1], 2))
+    print(buy_and_sell([10, 22, 5, 75, 65, 80], 2))
+    print(buy_and_sell([12, 14, 17, 10, 14, 13, 12, 15], 3))
+    print(buy_and_sell([100, 30, 15, 10, 8, 25, 80], 3))
+    print(buy_and_sell([90, 80, 70, 60, 50], 1))
+
+
 recursive()
+print()
+memoized()
