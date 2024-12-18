6a062711ba1986dc99775c36b913ea3880693735
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-18 09:17:09 +0530

    Longest Substring Without Repeating Characters

--- Practice Set 4/L3 - Longest Substring Without Repeating Characters.py
+++ Practice Set 4/L3 - Longest Substring Without Repeating Characters.py
@@ -1,20 +1,44 @@
+# Problem link - https://www.geeksforgeeks.org/length-of-the-longest-substring-without-repeating-characters/
+# Solution - https://www.youtube.com/watch?v=-zSxTJkcdAo&list=PLgUwDviBIf0q7vrFA_HEWcqRqMpCXzYAL&index=3
+
+
 class Solution:
     @staticmethod
     def longest_substring(string):
+        """
+            Time complexity is O(n) and space complexity is O(1).
+        """
+
+        # define a window
         left = right = 0
         n = len(string)
+
+        # define a character tracking dictionary
         d = {i: 0 for i in string}
+
+        # define result variables
         longest_length = 0
         start_index = -1
+
+        # while there is ground to cover.
         while right < n:
+            # increment the right index character
             d[string[right]] += 1
+
+            # if there is any character having freq > 1, then shrink only one unit from left.
             if any(v > 1 for v in d.values()):
                 d[string[left]] -= 1
                 left += 1
+
+            # if all the characters are unique, i.e., there is either 0 freq or 1 freq, update the result variables.
             if all(v <= 1 for v in d.values()):
                 longest_length = max(longest_length, right - left + 1)
                 start_index = left
+
+            # increment right index.
             right += 1
+
+        # return the longest substring.
         return string[start_index:start_index+longest_length] if start_index != -1 else ""
 
 
