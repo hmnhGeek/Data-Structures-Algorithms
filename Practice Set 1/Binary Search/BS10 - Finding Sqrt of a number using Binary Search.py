d329aaba6d15a19fa0e90bcca73f235f1a16c731
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-18 12:19:14 +0530

    Square root using Binary Search

--- Practice Set 1/Binary Search/BS10 - Finding Sqrt of a number using Binary Search.py
+++ Practice Set 1/Binary Search/BS10 - Finding Sqrt of a number using Binary Search.py
@@ -1,17 +1,29 @@
 class Solution:
     @staticmethod
     def get_sqrt(n):
+        # if the number is negative, no square root can be found.
         if n < 0:
             return -1
+
+        # define a search space.
         low, high = 0, n
+
+        # typical binary search
         while low <= high:
             mid = int(low + (high - low)/2)
+
+            # if mid is the exact square root of n, return it directly.
             if mid * mid == n:
                 return mid
+
+            # if mid^2 > n, we are way higher, reduce the search space to left side.
             if mid * mid > n:
                 high = mid - 1
             else:
+                # if mid^2 < n, we are way lower, reduce the search space to right side.
                 low = mid + 1
+
+        # high will point to the correct square root value.
         return high
 
 
