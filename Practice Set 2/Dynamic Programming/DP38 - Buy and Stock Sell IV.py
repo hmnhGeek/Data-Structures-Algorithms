ddd92388c99ae2bcc72e8f05161e0c79526b568a
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-18 12:04:59 +0530

    Space Optimized Stocks IV

--- Practice Set 2/Dynamic Programming/DP38 - Buy and Stock Sell IV.py
+++ Practice Set 2/Dynamic Programming/DP38 - Buy and Stock Sell IV.py
@@ -108,8 +108,46 @@ def tabulation():
     print(buy_and_sell([90, 80, 70, 60, 50], 1))
 
 
+def space_optimized():
+    """
+        Time complexity is O(2nk) and space complexity is O(2k).
+    """
+    def buy_and_sell(arr, k):
+        n = len(arr)
+        nxt = {True: {j: 0 for j in range(k + 1)}, False: {j: 0 for j in range(k + 1)}}
+        for i in range(n - 1, -1, -1):
+            curr = {True: {j: 0 for j in range(k + 1)}, False: {j: 0 for j in range(k + 1)}}
+            for can_buy in [True, False]:
+                for tx in range(k - 1, -1, -1):
+                    if can_buy:
+                        curr[can_buy][tx] = max(
+                            -arr[i] + nxt[False][tx],
+                            nxt[True][tx]
+                        )
+                    else:
+                        curr[can_buy][tx] = max(
+                            arr[i] + nxt[True][tx + 1],
+                            nxt[False][tx]
+                        )
+            nxt = curr
+        return nxt[True][0]
+
+    print(buy_and_sell([3, 2, 6, 5, 0, 3], 2))
+    print(buy_and_sell([8, 5, 1, 3, 10], 2))
+    print(buy_and_sell([10, 8, 6, 2], 2))
+    print(buy_and_sell([2, 6, 5, 2], 0))
+    print(buy_and_sell([1, 2, 3, 5], 2))
+    print(buy_and_sell([2, 4, 1], 2))
+    print(buy_and_sell([10, 22, 5, 75, 65, 80], 2))
+    print(buy_and_sell([12, 14, 17, 10, 14, 13, 12, 15], 3))
+    print(buy_and_sell([100, 30, 15, 10, 8, 25, 80], 3))
+    print(buy_and_sell([90, 80, 70, 60, 50], 1))
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
+print()
+space_optimized()
