57c7ede7d9f019d4ad03bdc0fb537734127e9b28
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-19 11:25:46 +0530

    Koko eating bananas

--- Practice Set 1/Binary Search/BS12 - Koko Eating Bananas.py
+++ Practice Set 1/Binary Search/BS12 - Koko Eating Bananas.py
@@ -1,11 +1,12 @@
 import math
 
+
 class Solution:
     @staticmethod
     def _koko_can_eat(arr, mid, hrs):
         consumed_hrs = 0
         for i in range(len(arr)):
-            consumed_hrs += math.ceil(arr[i]/mid)
+            consumed_hrs += math.ceil(arr[i] / mid)
         return consumed_hrs <= hrs
 
     @staticmethod
@@ -15,7 +16,7 @@ class Solution:
         low = 1
         high = max(arr)
         while low <= high:
-            mid = int(low + (high - low)/2)
+            mid = int(low + (high - low) / 2)
             is_possible = Solution._koko_can_eat(arr, mid, hrs)
             if is_possible:
                 high = mid - 1
@@ -25,3 +26,8 @@ class Solution:
 
 
 print(Solution.koko([3, 6, 7, 11], 8))
+print(Solution.koko([3, 6, 2, 8], 7))
+print(Solution.koko([7, 15, 6, 3], 8))
+print(Solution.koko([25, 12, 8, 14, 19], 5))
+print(Solution.koko([30, 11, 23, 4, 20], 5))
+print(Solution.koko([30, 11, 23, 4, 20], 6))
