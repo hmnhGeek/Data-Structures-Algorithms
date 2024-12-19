80de8d280f58dc701936a9ea68511a1265c49a15
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-19 12:04:02 +0530

    Fruits into basket

---
+++ Practice Set 4/L5 - Fruit Into Baskets.py
@@ -0,0 +1,21 @@
+class Solution:
+    @staticmethod
+    def fruits_into_baskets(arr):
+        left = right = 0
+        n = len(arr)
+        longest_length = 0
+        start_index = -1
+        d = {i: 0 for i in arr}
+        while right < n:
+            d[arr[right]] += 1
+            if sum(1 for v in d.values() if v != 0) > 2:
+                d[arr[left]] -= 1
+                left += 1
+            if sum(1 for v in d.values() if v != 0) <= 2:
+                longest_length = max(longest_length, right - left + 1)
+                start_index = left
+            right += 1
+        return arr[start_index:start_index+longest_length] if start_index != -1 else []
+
+
+print(Solution.fruits_into_baskets([3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4]))
