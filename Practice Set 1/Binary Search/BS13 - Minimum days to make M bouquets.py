e6a2060c45fa6ad91dea4115cde963deebc64a98
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-19 11:44:01 +0530

    Minimum days to make M bouquets

---
+++ Practice Set 1/Binary Search/BS13 - Minimum days to make M bouquets.py
@@ -0,0 +1,32 @@
+class Solution:
+    @staticmethod
+    def _is_possible(arr, mid, m, k):
+        flowers_used = 0
+        bouquets_formed = 0
+        for i in range(len(arr)):
+            if arr[i] <= mid:
+                flowers_used += 1
+            else:
+                flowers_used = 0
+            if flowers_used == k:
+                bouquets_formed += 1
+        return bouquets_formed >= m
+
+    @staticmethod
+    def min_days_for_m_bouquets(arr, m, k):
+        n = len(arr)
+        if m * k > n:
+            return -1
+        low, high = min(arr), max(arr)
+        while low <= high:
+            mid = int(low + (high - low)/2)
+            is_possible = Solution._is_possible(arr, mid, m, k)
+            if is_possible:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return low
+
+
+print(Solution.min_days_for_m_bouquets([7, 7, 7, 7, 13, 11, 12, 7], 2, 3))
+print(Solution.min_days_for_m_bouquets([1, 10, 3, 10, 2], 3, 2))
