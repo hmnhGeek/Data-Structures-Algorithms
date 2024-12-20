6e1f52990e15ab34286223e59d5967087ea5d12b
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-20 11:52:18 +0530

    Kruskal's Algorithm using Disjoint Set

--- Practice Set 2/Graphs/G47 - Kruskal's Algorithm.py
+++ Practice Set 2/Graphs/G47 - Kruskal's Algorithm.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/minimum-spanning-tree/1
+# Solution - https://www.youtube.com/watch?v=DMnDM_sxVig&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=47
+
+
 class DisjointSet:
     def __init__(self, nodes):
         self.ranks = {i: 0 for i in nodes}
@@ -40,17 +44,35 @@ class Solution:
 
     @staticmethod
     def get_mst(graph):
+        """
+            Time complexity is O(V + 2E + E*log(E)) and space complexity is O(V + E).
+        """
+
+        # get the edges from the graph in O(V + E) time and O(E) space.
         edges = Solution._get_edges(graph)
+
+        # sort the edges by weight in O(E*log(E)) time.
         edges.sort(key=lambda x: x[0])
+
+        # define MST variables
         mst = set()
         mst_wt = 0
+
+        # define a disjoint set for the graph in O(V) space.
         ds = DisjointSet([i for i in graph])
+
+        # loop on the edges in O(E) time.
         for edge in edges:
             wt, src, dst = edge
+            # check if both source and destination are not in the same component.
             if not ds.in_same_component(src, dst):
+                # if they are not, union them in disjoint set in O(1) time.
                 ds.union(src, dst)
+                # add the edge in MST
                 mst.add((src, dst))
                 mst_wt += wt
+
+        # return MST
         return mst, mst_wt
 
 
