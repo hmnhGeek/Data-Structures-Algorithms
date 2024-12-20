fc8bda76aee6cb7dd047273bd74ad9e81f80cd84
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-20 10:18:48 +0530

    Printing LIS

--- Practice Set 3/Dynamic Programming/DP42 - Printing the Longest Increasing Subsequence.py
+++ Practice Set 3/Dynamic Programming/DP42 - Printing the Longest Increasing Subsequence.py
@@ -13,7 +13,14 @@ def print_lis(arr):
         result.append(arr[start_index])
         start_index = parents[start_index]
     result.append(arr[start_index])
-    return result[-1:-len(result)-1:-1]
+    return result[-1:-len(result) - 1:-1]
 
 
 print(print_lis([5, 4, 11, 1, 16, 8]))
+print(print_lis([1, 2, 2]))
+print(print_lis([10, 20, 3, 40]))
+print(print_lis([10, 22, 9, 33, 21, 50, 41, 60, 80]))
+print(print_lis([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]))
+print(print_lis([1]))
+print(print_lis([5, 6, 3, 4, 7, 6]))
+print(print_lis([1, 2, 3, 4, 5]))
