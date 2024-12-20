fc1a8ed9281b33a5b635a2874d33c7f20b4e35d0
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-20 12:12:01 +0530

    Find the Smallest Divisor Given a Threshold

--- Practice Set 1/Binary Search/BS14 - Find the Smallest Divisor Given a Threshold.py
+++ Practice Set 1/Binary Search/BS14 - Find the Smallest Divisor Given a Threshold.py
@@ -6,7 +6,7 @@ class Solution:
     def _threshold_not_breached(arr, mid, threshold):
         val = 0
         for i in range(len(arr)):
-            val += ceil(arr[i]/mid)
+            val += ceil(arr[i] / mid)
         return val <= threshold
 
     @staticmethod
@@ -15,7 +15,7 @@ class Solution:
             return -1
         low, high = 1, max(arr)
         while low <= high:
-            mid = int(low + (high - low)/2)
+            mid = int(low + (high - low) / 2)
             is_possible = Solution._threshold_not_breached(arr, mid, threshold)
             if is_possible:
                 high = mid - 1
@@ -25,3 +25,10 @@ class Solution:
 
 
 print(Solution.find_smallest([1, 2, 5, 9], 6))
+print(Solution.find_smallest([1, 2, 3, 4, 5], 8))
+print(Solution.find_smallest([8, 4, 2, 3], 10))
+print(Solution.find_smallest([2, 3, 5, 7, 11], 11))
+print(Solution.find_smallest([44, 22, 33, 11, 1], 5))
+print(Solution.find_smallest([2, 3, 7, 4, 10], 5))
+print(Solution.find_smallest([7, 3, 3, 6], 10))
+print(Solution.find_smallest([1, 1, 1, 1], 4))
