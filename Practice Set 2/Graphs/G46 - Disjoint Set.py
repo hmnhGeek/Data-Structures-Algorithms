45296eb883e6e966effc94d2d3e65a72fd2078a6
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-20 10:31:58 +0530

    Disjoint Set by Ranks

---
+++ Practice Set 2/Graphs/G46 - Disjoint Set.py
@@ -0,0 +1,30 @@
+class DisjointSetByRank:
+    def __init__(self, nodes):
+        self.ranks = {i: 0 for i in nodes}
+        self.parents = {i: i for i in nodes}
+
+    def find_ultimate_parent(self, node):
+        if self.parents[node] == node:
+            return node
+        self.parents[node] = self.find_ultimate_parent(self.parents[node])
+        return self.parents[node]
+
+    def union(self, node1, node2):
+        ulp_node1 = self.find_ultimate_parent(node1)
+        ulp_node2 = self.find_ultimate_parent(node2)
+
+        if ulp_node1 == ulp_node2:
+            return
+
+        if self.ranks[ulp_node1] < self.ranks[ulp_node2]:
+            self.parents[ulp_node1] = ulp_node2
+        elif self.ranks[ulp_node2] < self.ranks[ulp_node1]:
+            self.parents[ulp_node2] = ulp_node1
+        else:
+            self.parents[ulp_node2] = ulp_node1
+            self.ranks[ulp_node1] += 1
+
+    def in_same_components(self, node1, node2):
+        return self.find_ultimate_parent(node1) == self.find_ultimate_parent(node2)
+
+
