82b04bb278b97fec3c13ca70afea829185d46fc6
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-20 10:44:38 +0530

    Disjoint Set by Sizes

--- Practice Set 2/Graphs/G46 - Disjoint Set.py
+++ Practice Set 2/Graphs/G46 - Disjoint Set.py
@@ -1,3 +1,6 @@
+# Video link - https://www.youtube.com/watch?v=aBxjDBC4M1U&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=47
+
+
 class DisjointSetByRank:
     def __init__(self, nodes):
         self.ranks = {i: 0 for i in nodes}
