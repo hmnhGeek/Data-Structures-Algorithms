0d2b9056d60c855cb9cb2933528a943c8390c3e7
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-20 15:09:56 +0530

    Find the Smallest Divisor Given a Threshold

--- Practice Set 1/Binary Search/BS14 - Find the Smallest Divisor Given a Threshold.py
+++ Practice Set 1/Binary Search/BS14 - Find the Smallest Divisor Given a Threshold.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.naukri.com/code360/problems/smallest-divisor-with-the-given-limit_1755882
+# Solution - https://www.youtube.com/watch?v=UvBKTVaG6U8&list=PLgUwDviBIf0pMFMWuuvDNMAkoQFi-h0ZF&index=16
+
+
 from math import ceil
 
 
@@ -11,16 +15,29 @@ class Solution:
 
     @staticmethod
     def find_smallest(arr, threshold):
+        """
+            Time complexity is O(n*log(max)) and space complexity is O(1).
+        """
+
+        # if threshold < length of array, then no divisor can achieve the desired result as any number used as a divisor
+        # will yield 1 as quotient only.
         if threshold < len(arr):
             return -1
+
+        # define a search space.
         low, high = 1, max(arr)
         while low <= high:
             mid = int(low + (high - low) / 2)
             is_possible = Solution._threshold_not_breached(arr, mid, threshold)
             if is_possible:
+                # if it is possible to get quotient sum <= threshold with mid, then reduce high to increase quotient sum
+                # and check if lower side can give use results as well.
                 high = mid - 1
             else:
+                # else increase denominator and reduce quotient sum to come within <= threshold.
                 low = mid + 1
+
+        # return `low` index which points the correct answer.
         return low
 
 
