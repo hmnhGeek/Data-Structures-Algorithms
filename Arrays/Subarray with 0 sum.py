c05f626c2c29c0b26fbaabe781479889a4c02f2f
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-22 12:52:54 +0530

    Subarray with 0 sum

--- Arrays/Subarray with 0 sum.py
+++ Arrays/Subarray with 0 sum.py
@@ -1,17 +1,30 @@
 class Solution:
     @staticmethod
     def subarray_with_0_sum(arr):
+        # define a dictionary to store the indices of the prefix sums. Note that we have explicitly added prefix sum 0
+        # with index -1 to denote the starting of the array.
         d = {0: -1}
+
+        # store sum tracker and max length
         _sum = 0
         max_length = 0
+
+        # store the starting index of the sub array.
         start_index = -1
+
+        # loop in the array
         for i in range(len(arr)):
             _sum += arr[i]
+            # if the sum is in the array as prefix sum, then arr[d[_sum] + 1 : i + 1] subarray will have a sum 0.
+            # Therefore, update the result variables.
             if _sum in d:
                 start_index = d[_sum] + 1
                 max_length = i - d[_sum]
             else:
+                # else, assign this prefix sum in the dictionary.
                 d[_sum] = i
+
+        # return the subarray.
         return arr[start_index:start_index+max_length] if start_index != -1 else []
 
 
