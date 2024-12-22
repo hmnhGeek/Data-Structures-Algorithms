26051b83c1693cb4447abd8df873fb0763d24a4d
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-22 14:59:33 +0530

    Triplets in DLL

--- LinkedList/triplets_in_dll_target_sum.py
+++ LinkedList/triplets_in_dll_target_sum.py
@@ -34,3 +34,38 @@ class DoublyLinkedList:
         print()
 
 
+class Solution:
+    @staticmethod
+    def solve(dll: DoublyLinkedList, target: int):
+        n = dll.length
+        i = dll.head
+        ans = set()
+        while i is not None:
+            j, k = i.next, dll.tail
+            while j and k and j != k and j.prev != k and k.next != j:
+                tgt = i.data + j.data + k.data
+                if tgt == target:
+                    ans.add((i.data, j.data, k.data))
+                    j = j.next
+                    k = k.prev
+                elif tgt > target:
+                    k = k.prev
+                else:
+                    j = j.next
+            i = i.next
+        return ans
+
+
+def test(l, target):
+    dll = DoublyLinkedList()
+    dll.build(*l)
+    print(Solution.solve(dll, target))
+
+
+test([1, 2, 3, 4], 8)
+test([1, 2, 3, 4, 5, 6, 7], 17)
+test([1, 2, 3, 8, 9], 13)
+test([1, 2, 3, 4, 5, 6, 7, 8, 9], 15)
+test([7, 33, 88, 91], 40)
+test([3, 7, 9, 23, 45], 19)
+test([8, 13, 16], 37)
