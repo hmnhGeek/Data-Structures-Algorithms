89a59a47743fba37d3c721e56c566d15ef8e69e0
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-23 17:38:25 +0530

    Implemented a doubly linked list class

---
+++ LinkedList/Sort a nearly sorted doubly linked list.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = self.next = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = self.tail = None
+        self.length = 0
+
+    def is_empty(self):
+        return self.length == 0
+
+    def push(self, x):
+        node = Node(x)
+        if self.is_empty():
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = node
+        self.length += 1
+
+    def show(self):
+        curr = self.head
+        while curr is not None:
+            print(curr.data, end=" ")
+            curr = curr.next
+        print()
+
+    def build(self, *args):
+        for i in args:
+            self.push(i)
+
