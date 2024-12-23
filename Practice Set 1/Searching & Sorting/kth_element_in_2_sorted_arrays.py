75d849c83205568e1f64d12792d113929cd200c9
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-23 11:27:07 +0530

    Kth element in 2 sorted arrays

--- Practice Set 1/Searching & Sorting/kth_element_in_2_sorted_arrays.py
+++ Practice Set 1/Searching & Sorting/kth_element_in_2_sorted_arrays.py
@@ -2,10 +2,12 @@ class Solution:
     @staticmethod
     def get_kth(arr1, arr2, k):
         n1, n2 = len(arr1), len(arr2)
+        if k > n1 + n2 or k <= 0:
+            return -1
         if n1 > n2:
             return Solution.get_kth(arr2, arr1, k)
-        low = 0
-        high = n1
+        low = max(0, k - n2)
+        high = min(k, n1)
         while low <= high:
             mid1 = int(low + (high - low)/2)
             mid2 = k - mid1
@@ -24,3 +26,6 @@ class Solution:
 
 print(Solution.get_kth([2, 3, 6, 7, 9], [1, 4, 8, 10], 5))
 print(Solution.get_kth([100, 112, 256, 349, 770], [72, 86, 113, 119, 265, 445, 892], 7))
+print(Solution.get_kth([2, 3, 45], [4, 6, 7, 8], 4))
+print(Solution.get_kth([2, 3, 6, 7, 10], [1, 4, 8, 10], 4))
+print(Solution.get_kth([1, 2, 3, 5, 6], [4, 7, 8, 9, 100], 6))
