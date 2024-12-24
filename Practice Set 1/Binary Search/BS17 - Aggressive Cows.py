1f825d32577d5ea6e52fc67eb1ef9831e659593b
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-24 14:33:43 +0530

    Aggressive Cows

--- Practice Set 1/Binary Search/BS17 - Aggressive Cows.py
+++ Practice Set 1/Binary Search/BS17 - Aggressive Cows.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/aggressive-cows/0
+# Solution - https://www.youtube.com/watch?v=R_Mfw4ew-Vo&list=PLgUwDviBIf0pMFMWuuvDNMAkoQFi-h0ZF&index=18
+
+
 class QuickSort:
     @staticmethod
     def _get_partition_index(arr, low, high):
@@ -29,27 +33,50 @@ class QuickSort:
 class Solution:
     @staticmethod
     def _cow_placement_possible(arr, mid, k):
+        """
+            Time complexity is O(n) and space complexity is O(1).
+        """
+
+        # assume that you've placed 1st cow at index 0.
         cows_placed = 1
         last_cow_placed_at = 0
+
+        # start placing from stall at index = 1
         for i in range(1, len(arr)):
+            # if the distance between current and last stall is more than equal to mid, place the next cow here.
             if arr[i] - arr[last_cow_placed_at] >= mid:
                 cows_placed += 1
                 last_cow_placed_at = i
+        # check if all k cows have been placed or not.
         return cows_placed >= k
 
     @staticmethod
     def aggressive_cows(arr, k):
+        """
+            Time complexity is O(n*log(n)) and space complexity is O(1).
+        """
+
+        # edge case
         if k <= 0:
             return
+
+        # sort the array in O(n*log(n)) time.
         QuickSort.sort(arr)
+
+        # define a search space
         low, high = 1, max(arr)
         while low <= high:
             mid = int(low + (high - low)/2)
+            # check if it is possible to place k cows with mid-distance.
             is_possible = Solution._cow_placement_possible(arr, mid, k)
             if is_possible:
+                # if possible, let's try to increase distance a bit more.
                 low = mid + 1
             else:
+                # if it is not possible, let's decrease the distance between the cows.
                 high = mid - 1
+
+        # return the correct answer.
         return high
 
 
