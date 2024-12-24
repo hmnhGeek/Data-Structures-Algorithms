59d5e4bbb5f52a1f7a28a70e15f30a37c48f341f
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-24 11:12:18 +0530

    Memoized Longest Divisible Subset

--- Practice Set 2/Dynamic Programming/DP44 - Longest Divisible Subset.py
+++ Practice Set 2/Dynamic Programming/DP44 - Longest Divisible Subset.py
@@ -35,4 +35,35 @@ def recursive():
     print(lds([2, 4, 3, 8]))
 
 
+def memoized():
+    """
+        Time complexity is O(n^2) and space complexity is O(n^2 + n).
+    """
+    def solve(arr, index, prev_index, dp):
+        if index == 0:
+            return 1 if mutual_divisibility(arr, 0, prev_index) else 0
+        if dp[index][prev_index] is not None:
+            return dp[index][prev_index]
+        left = 0
+        if mutual_divisibility(arr, index, prev_index):
+            left = 1 + solve(arr, index - 1, index, dp)
+        right = solve(arr, index - 1, prev_index, dp)
+        dp[index][prev_index] = max(left, right)
+        return dp[index][prev_index]
+
+    def lds(arr):
+        n = len(arr)
+        dp = {i: {j: None for j in range(1, n + 1)} for i in range(n)}
+        return solve(arr, n - 1, n, dp)
+
+    print(lds([1, 16, 7, 8, 4]))
+    print(lds([1, 2, 5]))
+    print(lds([3, 3, 3]))
+    print(lds([1, 2, 4, 8]))
+    print(lds([1, 2, 3]))
+    print(lds([2, 4, 3, 8]))
+
+
 recursive()
+print()
+memoized()
