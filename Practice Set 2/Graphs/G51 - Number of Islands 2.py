8f9e32589aebd1c83b4e7b50da6307f10692384c
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-25 17:40:15 +0530

    Number of Islands 2

--- Practice Set 2/Graphs/G51 - Number of Islands 2.py
+++ Practice Set 2/Graphs/G51 - Number of Islands 2.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/number-of-islands/1
+# Solution - https://www.youtube.com/watch?v=Rn6B-Q4SNyA&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=51
+
+
 class DisjointSet:
     def __init__(self, nodes):
         self.sizes = {i: 1 for i in nodes}
@@ -50,22 +54,39 @@ class Solution:
 
     @staticmethod
     def num_islands_2(n, m, arr):
+        """
+            Time complexity is O(nm) and space complexity is O(nm).
+        """
+
+        # get the number of nodes and construct a visited and disjoint set objects.
         num_nodes = n * m
         visited = [[0 for _ in range(m)] for _ in range(n)]
         ds = DisjointSet([i for i in range(num_nodes)])
+
+        # store a results list.
         result = []
+
+        # loop on all the cells from the problem.
         for cell in arr:
             i, j = cell
+            # get the node
             node = i * m + j
+            # if the node is not visited
             if visited[i][j] == 0:
+                # visit it
                 visited[i][j] = 1
+                # get all the neighbours with value 1.
                 neighbours = Solution._get_valid_neighbours(visited, i, j, n, m)
                 for neighbour in neighbours:
                     x, y = neighbour
                     adj_node = x * m + y
+                    # if these nodes are not in the same component, connect them
                     if not ds.in_same_component(node, adj_node):
                         ds.union(node, adj_node)
+            # get the number of components using visited matrix because we want components only where (i, j) cell value
+            # is 1.
             result.append(ds.get_num_components(visited, m))
+        # return the final result
         return result
 
 
