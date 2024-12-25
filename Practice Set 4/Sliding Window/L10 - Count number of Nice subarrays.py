929e189e799d62102eee5a03989bf0687e949258
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-25 19:34:07 +0530

    Count Nice Subarrays

--- Practice Set 4/Sliding Window/L10 - Count number of Nice subarrays.py
+++ Practice Set 4/Sliding Window/L10 - Count number of Nice subarrays.py
@@ -1,23 +1,50 @@
+# Problem link - https://leetcode.com/problems/count-number-of-nice-subarrays/description/
+# Solution - https://www.youtube.com/watch?v=j_QOv9OT9Og&list=PLgUwDviBIf0q7vrFA_HEWcqRqMpCXzYAL&index=10
+
+
 class Solution:
     @staticmethod
     def _count_less_than_equal_to(arr, k):
+        """
+            Time complexity is O(n) and space complexity is O(1).
+        """
+
+        # edge case
         if k < 0:
             return 0
+
+        # define window variables
         left = right = 0
         n = len(arr)
+
+        # define tracking variables
         _sum = count = 0
+
+        # while there is ground to cover
         while right < n:
+            # add 1 to sum if it is an odd number else add 0.
             _sum += arr[right] % 2
+
+            # while sum has breached k limit, shrink from left continuously.
             while _sum > k:
                 _sum -= arr[left] % 2
                 left += 1
+
+            # if sum is within k bound, update the count.
             if _sum <= k:
                 count += (right - left + 1)
+
+            # increment right index.
             right += 1
+
+        # return the count of sub-arrays whose odd number count is <= k.
         return count
 
     @staticmethod
     def count_nice_subarrays(arr, k):
+        """
+            Time complexity is O(n) and space complexity is O(1).
+        """
         return Solution._count_less_than_equal_to(arr, k) - Solution._count_less_than_equal_to(arr, k - 1)
 
 
