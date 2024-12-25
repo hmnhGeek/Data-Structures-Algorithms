d787b6049e5a4e227ad357b4e3b5a38fbf057fbd
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-25 17:16:57 +0530

    Disjoint Set by Sizes

--- Practice Set 2/Graphs/G51 - Number of Islands 2.py
+++ Practice Set 2/Graphs/G51 - Number of Islands 2.py
@@ -26,3 +26,47 @@ class DisjointSet:
     def in_same_component(self, node1, node2):
         return self.find_ultimate_parent(node1) == self.find_ultimate_parent(node2)
 
+    def get_num_components(self, visited, m):
+        count = 0
+        for i in self.parents:
+            if self.parents[i] == i and visited[i // m][i % m] == 1:
+                count += 1
+        return count
+
+
+class Solution:
+    @staticmethod
+    def _get_valid_neighbours(visited, i, j, n, m):
+        neighbours = []
+        if 0 <= i - 1 < n and visited[i - 1][j] == 1:
+            neighbours.append((i - 1, j))
+        if 0 <= j + 1 < m and visited[i][j + 1] == 1:
+            neighbours.append((i, j + 1))
+        if 0 <= i + 1 < n and visited[i + 1][j] == 1:
+            neighbours.append((i + 1, j))
+        if 0 <= j - 1 < m and visited[i][j - 1] == 1:
+            neighbours.append((i, j - 1))
+        return neighbours
+
+    @staticmethod
+    def num_islands_2(n, m, arr):
+        num_nodes = n * m
+        visited = [[0 for _ in range(m)] for _ in range(n)]
+        ds = DisjointSet([i for i in range(num_nodes)])
+        result = []
+        for cell in arr:
+            i, j = cell
+            node = i * m + j
+            if visited[i][j] == 0:
+                visited[i][j] = 1
+                neighbours = Solution._get_valid_neighbours(visited, i, j, n, m)
+                for neighbour in neighbours:
+                    x, y = neighbour
+                    adj_node = x * m + y
+                    if not ds.in_same_component(node, adj_node):
+                        ds.union(node, adj_node)
+            result.append(ds.get_num_components(visited, m))
+        return result
+
+
+print(Solution.num_islands_2(4, 5, [(1, 1), (0, 1), (3, 3), (3, 4)]))
