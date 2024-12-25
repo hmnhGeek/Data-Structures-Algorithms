8efbfe9c2cad8ce52aebd9a46440e88d39b19fa1
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-25 18:13:06 +0530

    Book Allocation Problem

--- Practice Set 1/Binary Search/BS18 - Allocate Books.py
+++ Practice Set 1/Binary Search/BS18 - Allocate Books.py
@@ -1,6 +1,14 @@
+# Problem link - https://www.geeksforgeeks.org/problems/allocate-minimum-number-of-pages0937/1
+# Solution - https://www.youtube.com/watch?v=Z0hwjftStI4&list=PLgUwDviBIf0pMFMWuuvDNMAkoQFi-h0ZF&index=19
+
+
 class Solution:
     @staticmethod
     def _allocation_possible(arr, mid, k):
+        """
+            Time complexity is O(n) and space complexity is O(1).
+        """
+
         students = 0
         pages = 0
         for i in range(len(arr)):
@@ -9,20 +17,31 @@ class Solution:
             else:
                 students += 1
                 pages = arr[i]
+
+        # at end, if pages are still pending, assign them to one more student.
         if pages > 0:
             students += 1
             pages = 0
+
+        # if less than or equal to k students have been assigned books, we must lower down the number of pages.
         if students <= k:
             return 0
+        # else we can increase the number of pages so that exactly k students get the books.
         return 1
 
     @staticmethod
     def allocate(arr, k):
+        """
+            Time complexity is O(n * log(sum - max)) and space complexity is O(1).
+        """
+
+        # if k is <= 0 or number of students is more than the number of books, return -1.
         if k <= 0 or k > len(arr):
             return -1
         low, high = max(arr), sum(arr)
         while low <= high:
             mid = int(low + (high - low)/2)
+            # check if allocation to all k students is possible if each student gets at max mid-number of pages.
             is_possible = Solution._allocation_possible(arr, mid, k)
             if is_possible == 0:
                 high = mid - 1
