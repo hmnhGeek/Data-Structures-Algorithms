6d6e30dfc0192f521469facbb80451781275e2f9
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-26 19:44:32 +0530

    Minimize the max distance between the gas stations

--- Practice Set 1/Binary Search/BS20 - Minimise Maximum Distance between Gas Stations.py
+++ Practice Set 1/Binary Search/BS20 - Minimise Maximum Distance between Gas Stations.py
@@ -13,20 +13,45 @@ class Solution:
     def minimize_distance(arr, k):
         if k <= 0:
             return
+
+        # low and high denote the distance where gas stations can be placed.
+        # assume you can place all the gas stations at 0 max-distance only.
         low = 0
+
+        # assume high to be 0 for now; we will make it point to current max distance between all the gas stations.
         high = 0
         n = len(arr)
+
+        # for n - 1 slots, update `high` to point to the max distance, because we have to minimize the distance. We
+        # cannot place outside the array bounds, as that will only increase the distance.
         for i in range(n - 1):
             high = max(high, arr[i + 1] - arr[i])
 
+        # as per question, we are allowed to have error variance of 10^(-6).
         delta = 1e-6
+
+        # while the variance is more than delta...
         while high - low > delta:
+            # compute mid, or the assumed max-distance. We cannot have any two gas stations placed in such a way that
+            # the distance between them is more than this mid-distance. Let's see if we can place exactly k-gas stations
+            # with this mid-distance or not.
             mid = (low + high) / 2
+
+            # find the number of gas stations place with at max mid-distance.
             num_placed = Solution._num_gas_stations_placed(arr, mid, n)
+
+            # if we require more than `k` gas stations just to maintain mid-distance as max permissible distance between
+            # k gas stations, then we must increase this mid-distance so that we require less gas stations to maintain
+            # the max distance value.
             if num_placed > k:
                 low = mid
             else:
+                # else, this would mean that we were able to place <= k gas stations with mid-distance as max
+                # permissible distance between any two consecutive gas stations. Since we need to minimize this max
+                # distance, let's reduce `high` now.
                 high = mid
+
+        # finally, `high` would point to the correct max-distance that has been minimized.
         return high
 
 
