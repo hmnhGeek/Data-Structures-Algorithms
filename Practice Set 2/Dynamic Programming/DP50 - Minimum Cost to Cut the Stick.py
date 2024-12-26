50eb27466b6a0017a906206edc2cf1e47880407d
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-26 15:11:16 +0530

    Implemented Merge Sort

---
+++ Practice Set 2/Dynamic Programming/DP50 - Minimum Cost to Cut the Stick.py
@@ -0,0 +1,42 @@
+class MergeSort:
+    @staticmethod
+    def _merge(arr, low, high):
+        mid = int(low + (high - low)/2)
+        left, right = arr[low:mid+1], arr[mid+1:high+1]
+        i, j, merged = 0, 0, []
+
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                merged.append(left[i])
+                i += 1
+            else:
+                merged.append(right[j])
+                j += 1
+
+        while i < len(left):
+            merged.append(left[i])
+            i += 1
+
+        while j < len(right):
+            merged.append(right[j])
+            j += 1
+
+        return merged
+
+    @staticmethod
+    def _sort(arr, low, high):
+        if low >= high:
+            return
+        mid = int(low + (high - low)/2)
+        MergeSort._sort(arr, low, mid)
+        MergeSort._sort(arr, mid + 1, high)
+        arr[low:high+1] = MergeSort._merge(arr, low, high)
+
+    @staticmethod
+    def sort(arr):
+        return MergeSort._sort(arr, 0, len(arr) - 1)
+
+
+def recursive():
+    def min_cost_to_cut_stick(arr):
+        pass
