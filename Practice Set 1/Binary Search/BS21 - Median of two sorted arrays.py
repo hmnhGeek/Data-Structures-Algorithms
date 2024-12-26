af1d081c7192411faa866e1625c614368b4e4b18
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-26 18:50:22 +0530

    Median of two sorted arrays

---
+++ Practice Set 1/Binary Search/BS21 - Median of two sorted arrays.py
@@ -0,0 +1,28 @@
+class Solution:
+    @staticmethod
+    def get_median(arr1, arr2):
+        n1, n2 = len(arr1), len(arr2)
+        if n1 > n2:
+            return Solution.get_median(arr2, arr1)
+        low, high = 0, n1
+        left = (n1 + n2 + 1)//2
+        while low <= high:
+            mid1 = int(low + (high - low)/2)
+            mid2 = left - mid1
+            l1 = arr1[mid1 - 1] if 0 <= mid1 - 1 < n1 else -1e6
+            l2 = arr2[mid2 - 1] if 0 <= mid2 - 1 < n2 else -1e6
+            r1 = arr1[mid1] if 0 <= mid1 < n1 else 1e6
+            r2 = arr2[mid2] if 0 <= mid2 < n2 else 1e6
+            if l1 <= r2 and l2 <= r1:
+                if (n1 + n2) % 2 == 0:
+                    return (max(l1, l2) + min(r1, r2))/2
+                return max(l1, l2)
+            elif l1 > r2:
+                high = mid1 - 1
+            else:
+                low = mid1 + 1
+        return -1
+
+
+print(Solution.get_median([1, 3, 4, 7, 10, 12], [2, 3, 6, 15]))
+print(Solution.get_median([2, 3, 4], [1, 3]))
