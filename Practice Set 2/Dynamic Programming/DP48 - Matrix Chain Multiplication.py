c8e1e10f3be1b9c5d4b6eafede1963f161600bfc
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-26 14:44:28 +0530

    Recursive Matrix Chain Multiplication

---
+++ Practice Set 2/Dynamic Programming/DP48 - Matrix Chain Multiplication.py
@@ -0,0 +1,27 @@
+def recursive():
+    """
+        Time complexity is O(2^n) and space complexity is O(n).
+    """
+    def solve(arr, i, j):
+        if i == j:
+            return 0
+        min_cost = 1e6
+        for k in range(i, j):
+            cost = arr[i - 1] * arr[k] * arr[j] + solve(arr, i, k) + solve(arr, k + 1, j)
+            min_cost = min(min_cost, cost)
+        return min_cost
+
+    def mcm(arr):
+        n = len(arr)
+        return solve(arr, 1, n - 1)
+
+    print(mcm([10, 20, 30, 40, 50]))
+    print(mcm([10, 20, 30, 40]))
+    print(mcm([4, 5, 3, 2]))
+    print(mcm([10, 15, 20, 25]))
+    print(mcm([1, 4, 3, 2]))
+    print(mcm([2, 1, 3, 4]))
+    print(mcm([1, 2, 3, 4, 3]))
+
+
+recursive()
