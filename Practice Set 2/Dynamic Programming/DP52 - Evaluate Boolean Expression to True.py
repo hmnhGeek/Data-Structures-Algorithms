b5a3d9c94761d8d1c5fd83755e17da94851466fc
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-27 12:36:46 +0530

    Memoized boolean evaluation to true

--- Practice Set 2/Dynamic Programming/DP52 - Evaluate Boolean Expression to True.py
+++ Practice Set 2/Dynamic Programming/DP52 - Evaluate Boolean Expression to True.py
@@ -49,4 +49,62 @@ def recursive():
     print(evaluate("T^F|F"))
 
 
+def memoized():
+    """
+        Time complexity is O(2 * n^3) and space complexity is O(n + 2n^2).
+    """
+
+    def solve(expr, i, j, to_true, dp):
+        if i > j:
+            return 0
+        if i == j:
+            if to_true:
+                return 1 if expr[i] == "T" else 0
+            else:
+                return 1 if expr[i] == "F" else 0
+
+        if dp[i][j][to_true] is not None:
+            return dp[i][j][to_true]
+
+        num_ways = 0
+        for k in range(i + 1, j, 2):
+            operator = expr[k]
+
+            left_to_true = solve(expr, i, k - 1, True, dp)
+            left_to_false = solve(expr, i, k - 1, False, dp)
+            right_to_true = solve(expr, k + 1, j, True, dp)
+            right_to_false = solve(expr, k + 1, j, False, dp)
+
+            if operator == "&":
+                if to_true:
+                    num_ways += (left_to_true * right_to_true)
+                else:
+                    num_ways += (left_to_true * right_to_false) + (left_to_false * right_to_true) + (left_to_false * right_to_false)
+            elif operator == "|":
+                if to_true:
+                    num_ways += (left_to_true * right_to_false) + (left_to_false * right_to_true) + (left_to_true * right_to_true)
+                else:
+                    num_ways += (left_to_false * right_to_false)
+            else:
+                if to_true:
+                    num_ways += (left_to_true * right_to_false) + (left_to_false * right_to_true)
+                else:
+                    num_ways += (left_to_true * right_to_true) + (left_to_false * right_to_false)
+        dp[i][j][to_true] = num_ways
+        return dp[i][j][to_true]
+
+    def evaluate(expr):
+        n = len(expr)
+        dp = {i: {j: {True: None, False: None} for j in range(n)} for i in range(n)}
+        return solve(expr, 0, n - 1, True, dp)
+
+    print(evaluate("F|T^F"))
+    print(evaluate("T|T&F"))
+    print(evaluate("T^T^F"))
+    print(evaluate("T|T&F^T"))
+    print(evaluate("T^F|F"))
+
+
 recursive()
+print()
+memoized()
