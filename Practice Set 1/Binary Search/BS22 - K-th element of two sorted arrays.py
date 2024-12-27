d360b4f647da8ba4d5253b006553a549481b128a
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-27 14:36:19 +0530

    Kth element in two sorted arrays

---
+++ Practice Set 1/Binary Search/BS22 - K-th element of two sorted arrays.py
@@ -0,0 +1,24 @@
+class Solution:
+    @staticmethod
+    def find_kth_element(arr1, arr2, k):
+        n1, n2 = len(arr1), len(arr2)
+        if n1 > n2:
+            return Solution.find_kth_element(arr2, arr1, k)
+        low, high = 0, n1
+        while low <= high:
+            mid1 = int(low + (high - low)/2)
+            mid2 = k - mid1
+            l1 = arr1[mid1 - 1] if 0 <= mid1 - 1 < n1 else -1e6
+            l2 = arr2[mid2 - 1] if 0 <= mid2 - 1 < n2 else -1e6
+            r1 = arr1[mid1] if 0 <= mid1 < n1 else 1e6
+            r2 = arr2[mid2] if 0 <= mid2 < n2 else 1e6
+            if l1 <= r2 and l2 <= r1:
+                return max(l1, l2)
+            elif l1 > r2:
+                high = mid1 - 1
+            else:
+                low = mid1 + 1
+        return -1
+
+
+print(Solution.find_kth_element([2, 3, 6, 7, 9], [1, 4, 8, 10], 4))
