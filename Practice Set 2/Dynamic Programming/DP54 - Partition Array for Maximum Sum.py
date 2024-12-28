e1065959c825b9f1e12701fff78a77ee1977600e
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-28 13:43:56 +0530

    Memoized partition array for maximum sum

--- Practice Set 2/Dynamic Programming/DP54 - Partition Array for Maximum Sum.py
+++ Practice Set 2/Dynamic Programming/DP54 - Partition Array for Maximum Sum.py
@@ -24,4 +24,36 @@ def recursive():
     print(partition([1], 1))
 
 
+def memoized():
+    """
+        Time complexity is O(n^2) and space complexity is O(n^2 + n).
+    """
+
+    def solve(arr, i, n, k, dp):
+        if i == n:
+            return 0
+        if dp[i]:
+            return dp[i]
+        max_item = -1e6
+        max_cost = -1e6
+        for j in range(i, min(n, i + k)):
+            max_item = max(max_item, arr[j])
+            length = j - i + 1
+            cost = (length * max_item) + solve(arr, j + 1, n, k, dp)
+            max_cost = max(max_cost, cost)
+        dp[i] = max_cost
+        return dp[i]
+
+    def partition(arr, k):
+        n = len(arr)
+        dp = {i: None for i in range(n + 1)}
+        return solve(arr, 0, n, k, dp)
+
+    print(partition([1, 15, 7, 9, 2, 5, 10], 3))
+    print(partition([1, 4, 1, 5, 7, 3, 6, 1, 9, 9, 3], 4))
+    print(partition([1], 1))
+
+
 recursive()
+print()
+memoized()
