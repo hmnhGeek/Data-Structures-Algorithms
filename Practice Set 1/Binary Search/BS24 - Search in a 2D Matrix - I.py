022b9cb4e6e026fb1dea3961b7d98749e24fc9d1
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-28 12:00:34 +0530

    Search in a 2D Matrix - I

--- Practice Set 1/Binary Search/BS24 - Search in a 2D Matrix - I.py
+++ Practice Set 1/Binary Search/BS24 - Search in a 2D Matrix - I.py
@@ -1,18 +1,33 @@
+# Problem link - https://leetcode.com/problems/search-a-2d-matrix/
+# Solution - https://www.youtube.com/watch?v=JXU4Akft7yk&list=PLgUwDviBIf0pMFMWuuvDNMAkoQFi-h0ZF&index=26
+
+
 class Solution:
     @staticmethod
     def search(mtx, target):
+        """
+            Time complexity is O(log(nm)) and space complexity is O(1).
+        """
+
         n, m = len(mtx), len(mtx[0])
+
+        # since the entire matrix if flattened to an array, is sorted, define search space of 0 --> nm - 1.
         low = 0
         high = n * m - 1
         while low <= high:
             mid = int(low + (high - low) / 2)
             x, y = mid // m, mid % m
+
+            # if mid-element is target, return its coordinates.
             if mtx[x][y] == target:
                 return x, y
             if mtx[x][y] > target:
+                # if mid-element is larger than target, lower down the search space to left side.
                 high = mid - 1
             else:
+                # if mid-element is smaller than target, lower down the search space to right side.
                 low = mid + 1
+        # return -1, -1 if element is not found.
         return -1, -1
 
 
