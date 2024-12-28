aee0ad69d4a4fd9f2511ef9f9badf9eb7fb3ad08
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-28 14:57:45 +0530

    BFS traversal of a graph

--- Practice Set 3/Graphs/G5 - BFS.py
+++ Practice Set 3/Graphs/G5 - BFS.py
@@ -31,3 +31,58 @@ class Queue:
         self.length -= 1
         return item
 
+
+class Solution:
+    @staticmethod
+    def bfs(graph, source):
+        if source not in graph:
+            return
+        visited = {i: False for i in graph}
+        queue = Queue()
+        queue.push(source)
+        while not queue.is_empty():
+            node = queue.pop()
+            if not visited[node]:
+                print(node, end=" ")
+            visited[node] = True
+            for adj_node in graph[node]:
+                if not visited[adj_node]:
+                    queue.push(adj_node)
+        print()
+
+
+Solution.bfs(
+    {
+        1: [2, 6],
+        2: [1, 3, 4],
+        3: [2],
+        4: [2, 5],
+        5: [4, 7],
+        6: [1, 7, 8],
+        7: [5, 6],
+        8: [6]
+    },
+    1
+)
+
+Solution.bfs(
+    {
+        0: [1, 2, 3],
+        1: [0],
+        2: [0, 4],
+        3: [0],
+        4: [2]
+    },
+    0
+)
+
+Solution.bfs(
+    {
+        0: [1, 2],
+        1: [0, 2],
+        2: [1, 0, 3, 4],
+        3: [2],
+        4: [2]
+    },
+    0
+)
