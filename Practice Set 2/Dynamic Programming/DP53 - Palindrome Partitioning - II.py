766324961accd192df0e70e6d3bf80579c433efc
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-28 13:17:07 +0530

    Memoized Palindrome Partitioning 2

--- Practice Set 2/Dynamic Programming/DP53 - Palindrome Partitioning - II.py
+++ Practice Set 2/Dynamic Programming/DP53 - Palindrome Partitioning - II.py
@@ -43,24 +43,28 @@ def recursive():
 
 def memoized():
     """
-        Time complexity is exponential and space complexity is O(n).
+        Time complexity is O(n^2 * m) where m is the length of longest temp string and space complexity is O(n * m).
     """
 
-    def solve(string, i, n):
+    def solve(string, i, n, dp):
         if i == n:
             return 0
+        if dp[i] is not None:
+            return dp[i]
         min_cost = 1e6
         temp = ""
         for j in range(i, n):
             temp += string[j]
-            if temp == temp[-1:-len(temp)-1:-1]:
-                cost = 1 + solve(string, j + 1, n)
+            if is_palindrome(temp):
+                cost = 1 + solve(string, j + 1, n, dp)
                 min_cost = min(min_cost, cost)
-        return min_cost
+        dp[i] = min_cost
+        return dp[i]
 
     def get_min_partitions_count(string):
         n = len(string)
-        return solve(string, 0, n) - 1
+        dp = {i: None for i in range(n + 1)}
+        return solve(string, 0, n, dp) - 1
 
     print(get_min_partitions_count("aaccb"))
     print(get_min_partitions_count("ababa"))
@@ -74,3 +78,5 @@ def memoized():
 
 
 recursive()
+print()
+memoized()
