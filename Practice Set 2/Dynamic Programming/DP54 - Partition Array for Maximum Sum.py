297f020efa4bf27f9653348c7594f609a70a45b7
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-28 13:45:27 +0530

    Tabulation partition array for maximum sum

--- Practice Set 2/Dynamic Programming/DP54 - Partition Array for Maximum Sum.py
+++ Practice Set 2/Dynamic Programming/DP54 - Partition Array for Maximum Sum.py
@@ -54,6 +54,32 @@ def memoized():
     print(partition([1], 1))
 
 
+def tabulation():
+    """
+        Time complexity is O(n^2) and space complexity is O(n).
+    """
+    def partition(arr, k):
+        n = len(arr)
+        dp = {i: -1e6 for i in range(n + 1)}
+        dp[n] = 0
+        for i in range(n - 1, -1, -1):
+            max_item = -1e6
+            max_cost = -1e6
+            for j in range(i, min(n, i + k)):
+                max_item = max(max_item, arr[j])
+                length = j - i + 1
+                cost = (length * max_item) + dp[j + 1]
+                max_cost = max(max_cost, cost)
+            dp[i] = max_cost
+        return dp[0]
+
+    print(partition([1, 15, 7, 9, 2, 5, 10], 3))
+    print(partition([1, 4, 1, 5, 7, 3, 6, 1, 9, 9, 3], 4))
+    print(partition([1], 1))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
