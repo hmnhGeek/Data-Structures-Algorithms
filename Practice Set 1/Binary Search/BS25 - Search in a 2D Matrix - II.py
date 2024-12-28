19a07e514c31ab4cc063f096fe0524ed5ccce868
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-28 12:13:55 +0530

    Search in a 2D Matrix - II

--- Practice Set 1/Binary Search/BS25 - Search in a 2D Matrix - II.py
+++ Practice Set 1/Binary Search/BS25 - Search in a 2D Matrix - II.py
@@ -1,17 +1,29 @@
+# Problem link - https://leetcode.com/problems/search-a-2d-matrix-ii/
+# Solution - https://www.youtube.com/watch?v=9ZbB397jU4k&list=PLgUwDviBIf0pMFMWuuvDNMAkoQFi-h0ZF&index=27
+
+
 class Solution:
     @staticmethod
     def search(mtx, target):
+        """
+            Time complexity is O(n + m) and space complexity is O(1).
+        """
+
         n, m = len(mtx), len(mtx[0])
+        # define the search space.
         row, col = 0, m - 1
         while 0 <= row < n and 0 <= col < m:
+            # if the value is at row, col, return them.
             if mtx[row][col] == target:
                 return row, col
+            # if the target is smaller than (row, col) element, it will not be present in this col.
             if mtx[row][col] > target:
                 col -= 1
             else:
+                # else, if target > (row, col) element, then it will lie possibly in the next row.
                 row += 1
+        # return -1s if not found.
         return -1, -1
-
 
 
 print(
