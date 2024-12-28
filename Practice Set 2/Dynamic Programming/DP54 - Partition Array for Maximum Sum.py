b306b4f31bf1fc836feedf680ef111b9695ce3dd
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-28 13:45:55 +0530

    Partition array for maximum sum

--- Practice Set 2/Dynamic Programming/DP54 - Partition Array for Maximum Sum.py
+++ Practice Set 2/Dynamic Programming/DP54 - Partition Array for Maximum Sum.py
@@ -1,3 +1,7 @@
+# Problem link - https://leetcode.com/problems/partition-array-for-maximum-sum/description/
+# Solution - https://www.youtube.com/watch?v=PhWWJmaKfMc&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=55
+
+
 def recursive():
     """
         Time complexity is exponential and space complexity is O(n^2).
