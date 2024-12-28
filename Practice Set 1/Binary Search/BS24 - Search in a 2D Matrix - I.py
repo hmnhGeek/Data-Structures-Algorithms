b939a4095326fe94e5c1a77788073baa16e5637c
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-28 11:58:32 +0530

    Search in a 2D Matrix - I

--- Practice Set 1/Binary Search/BS24 - Search in a 2D Matrix - I.py
+++ Practice Set 1/Binary Search/BS24 - Search in a 2D Matrix - I.py
@@ -3,9 +3,9 @@ class Solution:
     def search(mtx, target):
         n, m = len(mtx), len(mtx[0])
         low = 0
-        high = n*m - 1
+        high = n * m - 1
         while low <= high:
-            mid = int(low + (high - low)/2)
+            mid = int(low + (high - low) / 2)
             x, y = mid // m, mid % m
             if mtx[x][y] == target:
                 return x, y
@@ -24,5 +24,26 @@ print(
             [20, 21, 23, 29]
         ],
         23
+    )
+)
+
+print(
+    Solution.search(
+        [
+            [3, 4, 7, 9],
+            [12, 13, 16, 18],
+            [20, 21, 23, 29]
+        ],
+        28
+    )
+)
+
+print(Solution.search([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3))
+print(Solution.search([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 13))
+print(
+    Solution.search(
+        [[18, 21, 27],
+         [38, 55, 67]],
+        55
     )
 )
