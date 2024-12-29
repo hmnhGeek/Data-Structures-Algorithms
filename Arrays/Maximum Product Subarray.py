1d3c5490089274fd7475f576c821281621e8c207
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-29 14:40:37 +0530

    Max product subarray

--- Arrays/Maximum Product Subarray.py
+++ Arrays/Maximum Product Subarray.py
@@ -2,7 +2,7 @@ class Solution:
     @staticmethod
     def get_max_product(arr):
         prefix = suffix = 1
-        max_product = 1
+        max_product = -1e6
         n = len(arr)
         for i in range(n):
             if prefix == 0:
@@ -18,3 +18,6 @@ class Solution:
 print(Solution.get_max_product([-2, 6, -3, -10, 0, 2]))
 print(Solution.get_max_product([-1, -3, -10, 0, 6]))
 print(Solution.get_max_product([2, 3, 4]))
+print(Solution.get_max_product([2,3,-2,4]))
+print(Solution.get_max_product([-2,0,-1]))
+print(Solution.get_max_product([-1, -3, -10, 0, 60]))
