11651e61723de2ebf93def2884b593f2804f3d8f
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-29 20:44:55 +0530

    Stack permutations

--- Stacks & Queues/stack_permutation.py
+++ Stacks & Queues/stack_permutation.py
@@ -37,3 +37,26 @@ class Stack:
         return self.head.data
 
 
+class Solution:
+    @staticmethod
+    def is_stack_permutation(arr1, arr2):
+        if len(arr1) != len(arr2):
+            return False
+        stack = Stack()
+        i, j = 0, 0
+        while i < len(arr1):
+            while stack.top() == arr2[j]:
+                stack.pop()
+                j += 1
+            stack.push(arr1[i])
+            i += 1
+        while j < len(arr2) and stack.top() == arr2[j]:
+            stack.pop()
+            j += 1
+        return stack.is_empty()
+
+
+print(Solution.is_stack_permutation([1, 2, 3], [2, 1, 3]))
+print(Solution.is_stack_permutation([1, 2, 3], [3, 1, 2]))
+print(Solution.is_stack_permutation([1,2,3,4,5],[4, 5, 3, 2, 1]))
+print(Solution.is_stack_permutation([1,2,3,4,5],[4, 3, 5, 1, 2]))
