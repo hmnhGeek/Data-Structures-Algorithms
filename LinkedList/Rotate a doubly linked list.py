32011646b2ea493bac84a5ff8b31e86db9c7c5a9
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-29 16:10:05 +0530

    Rotate a doubly linked list

--- LinkedList/Rotate a doubly linked list.py
+++ LinkedList/Rotate a doubly linked list.py
@@ -1,3 +1,6 @@
+# Problem link - https://www.naukri.com/code360/problems/rotate-dll_1115782
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -33,9 +36,15 @@ class DoublyLinkedList:
         print()
 
     def rotate(self, p):
+        """
+            Time complexity is O(p) and space complexity is O(1).
+        """
+
+        # if p is out of range, do nothing.
         if p < 0 or p > self.get_length():
             return
 
+        # go to the pth node in O(p) time.
         curr = self.head
         counter = 1
         while counter != p:
@@ -43,14 +52,23 @@ class DoublyLinkedList:
             counter += 1
 
         head, tail = self.head, self.tail
+
+        # make the tail's next point to the current head.
         tail.next = head
+        # make the head's prev point to the current tail.
+        head.prev = tail
+
+        # also, store the reference to pth node's next as it will become head.
         next_head = curr.next
 
+        # if pth node's next is not None
         if curr.next is not None:
+            # ensure that next_head's prev is None as it will become head now.
             curr.next.prev = None
+            # and the pth node will become tail, so ensure that its next is None.
             curr.next = None
 
-        head.prev = tail
+        # update head and tail of the DLL.
         self.head, self.tail = next_head, curr
 
 
