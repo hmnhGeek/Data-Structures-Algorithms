9b2303c355937828897475b3f0510d374ac0ef3e
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-29 15:05:22 +0530

    Rotate matrix by 90 degree clockwise

--- Practice Set 3/Matrix/Rotate a matrix 90 degrees clockwise.py
+++ Practice Set 3/Matrix/Rotate a matrix 90 degrees clockwise.py
@@ -1,3 +1,6 @@
+# Problem link - https://www.geeksforgeeks.org/rotate-a-matrix-by-90-degree-in-clockwise-direction-without-using-any-extra-space/
+
+
 class Solution:
     @staticmethod
     def _reverse_arr(mtx, i, n):
@@ -10,6 +13,7 @@ class Solution:
     @staticmethod
     def _transpose(mtx, n):
         for i in range(n):
+            # start from `i + 1` cell so that we don't swap the cells two times and get back the same matrix.
             for j in range(i + 1, n):
                 mtx[i][j], mtx[j][i] = mtx[j][i], mtx[i][j]
 
@@ -20,9 +24,19 @@ class Solution:
 
     @staticmethod
     def rotate_90_deg_clockwise(mtx):
+        """
+            Time complexity is O(n^2) and space complexity is O(1).
+        """
+
         n = len(mtx)
+
+        # take the transpose of the matrix in O(n^2) time.
         Solution._transpose(mtx, n)
+
+        # Laterally invert each row of the matrix in O(n^2) time.
         Solution._lateral_invert(mtx, n)
+
+        # print the rotated matrix.
         for i in range(n):
             print(mtx[i])
         print()
@@ -44,3 +58,5 @@ Solution.rotate_90_deg_clockwise(
         [13, 14, 15, 16]
     ]
 )
+
+Solution.rotate_90_deg_clockwise([[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]])
