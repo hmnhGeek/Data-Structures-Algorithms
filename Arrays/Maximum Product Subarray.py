e87be9da8578f90e950b60c3129f1c4153fb1030
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-29 14:38:56 +0530

    Max product subarray

---
+++ Arrays/Maximum Product Subarray.py
@@ -0,0 +1,20 @@
+class Solution:
+    @staticmethod
+    def get_max_product(arr):
+        prefix = suffix = 1
+        max_product = 1
+        n = len(arr)
+        for i in range(n):
+            if prefix == 0:
+                prefix = 1
+            if suffix == 0:
+                suffix = 1
+            prefix = prefix * arr[i]
+            suffix = suffix * arr[n - i - 1]
+            max_product = max(max_product, prefix, suffix)
+        return max_product
+
+
+print(Solution.get_max_product([-2, 6, -3, -10, 0, 2]))
+print(Solution.get_max_product([-1, -3, -10, 0, 6]))
+print(Solution.get_max_product([2, 3, 4]))
