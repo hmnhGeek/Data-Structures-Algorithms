ebb1c5571a097fbdff163124221d584b6c0b8bdc
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-29 20:48:24 +0530

    Stack Permutations

--- Stacks & Queues/stack_permutation.py
+++ Stacks & Queues/stack_permutation.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/stack-permutations-check-if-an-array-is-stack-permutation-of-other/
+# Solution - https://www.youtube.com/watch?v=q4CbTMLogyk&t=663s
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -40,23 +44,39 @@ class Stack:
 class Solution:
     @staticmethod
     def is_stack_permutation(arr1, arr2):
+        """
+            Time complexity is O(n + m) and space complexity is O(n).
+        """
+
+        # if the length don't match, return False
         if len(arr1) != len(arr2):
             return False
+
+        # create a stack and two pointers for traversing the array.
         stack = Stack()
         i, j = 0, 0
+
+        # while the first array can be traversed
         while i < len(arr1):
+            # while stack's top is same as arr2[j], pop and move to next character in arr2.
             while stack.top() == arr2[j]:
                 stack.pop()
                 j += 1
+
+            # when no match is found, push the ith character into stack.
             stack.push(arr1[i])
             i += 1
+
+        # now if arr2 still has some items to traverse and top of stack matches with jth character, pop and increment j.
         while j < len(arr2) and stack.top() == arr2[j]:
             stack.pop()
             j += 1
+
+        # if stack is not yet empty, it means not a stack permutation.
         return stack.is_empty()
 
 
 print(Solution.is_stack_permutation([1, 2, 3], [2, 1, 3]))
 print(Solution.is_stack_permutation([1, 2, 3], [3, 1, 2]))
-print(Solution.is_stack_permutation([1,2,3,4,5],[4, 5, 3, 2, 1]))
-print(Solution.is_stack_permutation([1,2,3,4,5],[4, 3, 5, 1, 2]))
+print(Solution.is_stack_permutation([1, 2, 3, 4, 5], [4, 5, 3, 2, 1]))
+print(Solution.is_stack_permutation([1, 2, 3, 4, 5], [4, 3, 5, 1, 2]))
