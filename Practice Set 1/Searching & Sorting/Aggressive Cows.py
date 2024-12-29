77c0df25c27ae43855fc2d8b51d554f35b4a861e
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-29 15:39:57 +0530

    Aggressive Cows using Binary Search

--- Practice Set 1/Searching & Sorting/Aggressive Cows.py
+++ Practice Set 1/Searching & Sorting/Aggressive Cows.py
@@ -25,3 +25,31 @@ class QuickSort:
     def sort(arr):
         QuickSort._sort(arr, 0, len(arr) - 1)
 
+
+class Solution:
+    @staticmethod
+    def _find_num_cows_placed(arr, mid):
+        cows_placed = 1
+        last_index = 0
+        for i in range(1, len(arr)):
+            if arr[i] - arr[last_index] >= mid:
+                cows_placed += 1
+                last_index = i
+        return cows_placed
+
+    @staticmethod
+    def aggressive_cows(arr, k):
+        QuickSort.sort(arr)
+        low, high = 0, max(arr) - min(arr)
+        while low <= high:
+            mid = int(low + (high - low)/2)
+            cows_placed = Solution._find_num_cows_placed(arr, mid)
+            if cows_placed < k:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return high
+
+
+print(Solution.aggressive_cows([0, 3, 4, 7, 10, 9], 4))
+print(Solution.aggressive_cows([1, 2, 3], 2))
