bff09a35933a07a96e6d1927cd1f172802780e31
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-29 15:58:30 +0530

    Implemented a doubly linked list class

---
+++ LinkedList/Rotate a doubly linked list.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = self.next = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = self.tail = None
+        self.length = 0
+
+    def is_empty(self):
+        return self.length == 0
+
+    def get_length(self):
+        return self.length
+
+    def push(self, x):
+        node = Node(x)
+        if self.is_empty():
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = node
+        self.length += 1
+
+    def show(self):
+        curr = self.head
+        while curr is not None:
+            print(curr.data, end=" ")
+            curr = curr.next
+        print()
+
