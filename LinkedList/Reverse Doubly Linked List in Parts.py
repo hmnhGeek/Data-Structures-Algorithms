ef8d9596b08cbd955ab987ccc454909b48640163
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-30 15:22:45 +0530

    Reversing a DLL in parts

--- LinkedList/Reverse Doubly Linked List in Parts.py
+++ LinkedList/Reverse Doubly Linked List in Parts.py
@@ -39,3 +39,57 @@ class DoublyLinkedList:
             curr = next_curr
         self.head, self.tail = self.tail, self.head
 
+    def _get_kth_node(self, prev, start_node, k):
+        counter = 1
+        prev = None
+        while counter != k and start_node is not None:
+            prev = start_node
+            start_node = start_node.next
+            counter += 1
+        if start_node is None:
+            return prev
+        return start_node
+
+    def reverse_in_parts(self, k):
+        if k == self.length:
+            self.reverse()
+        if k <= 0:
+            return
+        start_node = curr = self.head
+        last_node = None
+        while curr is not None:
+            kth_node = self._get_kth_node(None, curr, k)
+            temp = kth_node.next
+            kth_node.next = None
+            if temp is not None:
+                temp.prev = None
+            sub_dll = DoublyLinkedList()
+            sub_dll.head = start_node
+            sub_dll.tail = kth_node
+            sub_dll.reverse()
+
+            if last_node is not None:
+                last_node.next = kth_node
+                kth_node.prev = last_node
+            else:
+                self.head = kth_node
+
+            if temp is None:
+                self.tail = start_node
+
+            last_node = start_node
+            curr = start_node = temp
+
+
+def test(*args):
+    k = args[-1]
+    n = len(args)
+    dll = DoublyLinkedList()
+    for i in range(n - 1):
+        dll.push(args[i])
+    dll.show()
+    dll.reverse_in_parts(4)
+    dll.show()
+
+
+test(1, 2, 3, 4, 5, 6, 4)
