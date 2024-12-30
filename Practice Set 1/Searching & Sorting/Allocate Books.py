3218a390cb988480aeea4818474b6e23d0142aef
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-30 14:45:55 +0530

    Allocate Books using Binary Search

---
+++ Practice Set 1/Searching & Sorting/Allocate Books.py
@@ -0,0 +1,35 @@
+class Solution:
+    @staticmethod
+    def _find_how_many_students_allocated(arr, mid):
+        students = 0
+        pages = 0
+        for i in range(len(arr)):
+            if pages + arr[i] <= mid:
+                pages += arr[i]
+            else:
+                students += 1
+                pages = arr[i]
+        if pages > 0:
+            students += 1
+            pages = 0
+        return students
+
+    @staticmethod
+    def allocate_books(arr, k):
+        n = len(arr)
+        if k > n or k <= 0:
+            return -1
+        low, high = max(arr), sum(arr)
+        while low <= high:
+            mid = int(low + (high - low)/2)
+            allocated_students = Solution._find_how_many_students_allocated(arr, mid)
+            if allocated_students > k:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return low
+
+
+print(Solution.allocate_books([12, 34, 67, 90], 2))
+print(Solution.allocate_books([15, 17, 20], 5))
+print(Solution.allocate_books([22, 23, 67], 1))
