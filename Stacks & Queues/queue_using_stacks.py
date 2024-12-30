78025d2ef9f704c709458b3dc2e93c2d98f56880
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-30 15:57:39 +0530

    Implemented a queue using 2 stacks

--- Stacks & Queues/queue_using_stacks.py
+++ Stacks & Queues/queue_using_stacks.py
@@ -31,3 +31,34 @@ class Stack:
         self.length -= 1
         return item
 
+
+class Queue:
+    def __init__(self):
+        self.s1 = Stack()
+        self.s2 = Stack()
+
+    def push(self, x):
+        self.s1.push(x)
+
+    def pop(self):
+        while not self.s1.is_empty():
+            self.s2.push(self.s1.pop())
+        item = self.s2.pop()
+        while not self.s2.is_empty():
+            self.s1.push(self.s2.pop())
+        return item
+
+
+# Example 1
+q1 = Queue()
+q1.push(2)
+q1.push(3)
+print(q1.pop())
+q1.push(4)
+print(q1.pop())
+
+# Example 2
+q2 = Queue()
+q2.push(2)
+print(q2.pop())
+print(q2.pop())
