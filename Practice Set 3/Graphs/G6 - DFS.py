d3f1bf36387137d834ffd2d981264f290168092f
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-31 12:24:28 +0530

    DFS using recursion

--- Practice Set 3/Graphs/G6 - DFS.py
+++ Practice Set 3/Graphs/G6 - DFS.py
@@ -1,19 +1,37 @@
+# Problem link - https://www.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1
+# Solution - https://www.youtube.com/watch?v=Qzf1a--rhp8&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=6
+
+
 class Solution:
     @staticmethod
     def _dfs(graph, node, visited, traversal):
+        # mark the node as visited and append it to the traversal
         visited[node] = True
         traversal.append(node)
+
+        # now loop on the adjacent nodes of this node from the graph
         for adj_node in graph[node]:
+            # and if that adjacent node is not yet visited, perform a DFS starting from this adjacent node as well.
             if not visited[adj_node]:
                 Solution._dfs(graph, adj_node, visited, traversal)
 
     @staticmethod
     def dfs(graph, source):
+        """
+            Time complexity is O(V + E) and space complexity is O(V).
+        """
+
+        # if the source node is not in the graph, return from the method
         if source not in graph:
             return
+
+        # create a visited array and a traversal array to store the traversals.
         visited = {i: False for i in graph}
         traversal = []
+
+        # loop on the nodes of the graph
         for node in graph:
+            # if the node is not yet visited, perform a DFS starting from that node.
             if not visited[node]:
                 Solution._dfs(graph, node, visited, traversal)
         return traversal
