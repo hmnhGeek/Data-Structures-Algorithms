b075416815ffddff8c6cbcc45b8abce58407b871
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-31 12:35:19 +0530

    Find number of provinces using DFS

--- Practice Set 3/Graphs/G7 - Number of Provinces.py
+++ Practice Set 3/Graphs/G7 - Number of Provinces.py
@@ -1,6 +1,11 @@
+# Problem link - https://www.geeksforgeeks.org/problems/number-of-provinces/1
+# Solution - https://www.youtube.com/watch?v=ACzkVtewUYA&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=7
+
+
 class Solution:
     @staticmethod
     def _dfs(graph, node, visited):
+        # mark the node as visited and perform dfs on adjacent nodes.
         visited[node] = True
         for adj_node in graph[node]:
             if not visited[adj_node]:
@@ -18,13 +23,27 @@ class Solution:
 
     @staticmethod
     def find_num_provinces(mtx):
+        """
+            Time complexity is O(V + E + V^2) and space complexity is O(V + E).
+        """
+
+        # get the graph from the matrix in O(V^2) time and O(V + E) space.
         graph = Solution._get_graph(mtx)
+
+        # define a visited array of size V to use in DFS.
         visited = {i: False for i in graph}
+
+        # get the number of provinces in this variable.
         num_provinces = 0
+
+        # loop on the nodes of the graph
         for node in graph:
+            # if this node is not yet visited, this is a new component
             if not visited[node]:
                 num_provinces += 1
                 Solution._dfs(graph, node, visited)
+
+        # return the number of provinces.
         return num_provinces
 
 
