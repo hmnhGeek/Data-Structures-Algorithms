d9940dd5ab13d6f5faff5201e4b2de47c19f3733
hmnhGeek <hmnhsharma97@gmail.com>
2024-12-31 17:38:04 +0530

    Choose and swap

--- Greedy/Choose and Swap.py
+++ Greedy/Choose and Swap.py
@@ -8,14 +8,26 @@ class Solution:
         """
             Time complexity is O(n) and space complexity is O(n).
         """
-
+        original = string
         string = [i for i in string]
+
+        # get a sorted hash map of characters in string.
         hash_map = sorted(set(string))
+
+        # loop on the characters of the string
         for i in range(len(string)):
+            # remove the current character from the set
             hash_map.remove(string[i])
+
+            # if set has become empty, simply return the original string
             if len(hash_map) == 0:
-                return "".join(string)
+                return original
+
+            # get the least valued character from front of the set.
             character = list(hash_map)[0]
+
+            # if this character is smaller than ith character, change both character and ith character in the string
+            # using just one iteration of this nested for loop.
             if ord(character) < ord(string[i]):
                 second_character = string[i]
                 for j in range(len(string)):
@@ -24,6 +36,8 @@ class Solution:
                     elif string[j] == second_character:
                         string[j] = character
                 break
+
+        # return the lexicographically smallest string now.
         return "".join(string)
 
 
