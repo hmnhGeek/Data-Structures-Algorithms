7b68c1068d05ebb933ea1b2446d1a7d1a39eb64e
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-02 15:11:02 +0530

    First and last occurrences of an element in a sorted array

---
+++ Practice Set 2/Binary Search/BS3 - First and Last Occurrences in Array.py
@@ -0,0 +1,46 @@
+class Solution:
+    @staticmethod
+    def get_first_occurrence(arr, x):
+        low, high = 0, len(arr) - 1
+        while low <= high:
+            mid = int(low + (high - low)/2)
+            if arr[mid] >= x:
+                high = mid - 1
+            else:
+                low = mid + 1
+        if low not in range(len(arr)):
+            return -1
+        if arr[low] == x:
+            return low
+        return -1
+
+    @staticmethod
+    def get_last_occurrence(arr, x):
+        low, high = 0, len(arr) - 1
+        while low <= high:
+            mid = int(low + (high - low) / 2)
+            if arr[mid] <= x:
+                low = mid + 1
+            else:
+                high = mid - 1
+        if high not in range(len(arr)):
+            return -1
+        if arr[high] == x:
+            return high
+        return -1
+
+    @staticmethod
+    def get_occurrences(arr, x):
+        return Solution.get_first_occurrence(arr, x), Solution.get_last_occurrence(arr, x)
+
+
+
+print(Solution.get_occurrences([2, 4, 6, 8, 8, 8, 11, 13], 8))
+print(Solution.get_occurrences([2, 4, 6, 8, 8, 8, 11, 13], 80))
+print(Solution.get_occurrences([0, 1, 1, 5], 1))
+print(Solution.get_occurrences([0, 0, 1, 1, 2, 2, 2, 2], 2))
+print(Solution.get_occurrences([1, 3, 3, 5], 2))
+print(Solution.get_occurrences([1, 3, 5, 5, 5, 5, 67, 123, 125], 5))
+print(Solution.get_occurrences([1, 3, 5, 5, 5, 5, 7, 123, 125], 7))
+print(Solution.get_occurrences([5, 7, 7, 8, 8, 10], 6))
+print(Solution.get_occurrences([], 0))
