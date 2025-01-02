8daa32dddbb097673ef2b00dedfc49c534d19a63
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-02 12:02:57 +0530

    Recursive climbing stairs

---
+++ Practice Set 3/Dynamic Programming/DP2 - Climbing Stairs.py
@@ -0,0 +1,27 @@
+def recursive():
+    """
+        Time complexity is O(2^n) and space complexity is O(n).
+    """
+
+    def solve(n):
+        if n == 0:
+            return 1
+        left = 0
+        if n - 1 >= 0:
+            left = solve(n - 1)
+        right = 0
+        if n - 2 >= 0:
+            right = solve(n - 2)
+        return left + right
+
+    def climbing_stairs(n):
+        return solve(n)
+
+    print(climbing_stairs(3))
+    print(climbing_stairs(2))
+    print(climbing_stairs(4))
+    print(climbing_stairs(5))
+    print(climbing_stairs(1))
+
+
+recursive()
