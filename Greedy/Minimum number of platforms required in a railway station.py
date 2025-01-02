d7b789fd4e97e97cc89b4c39e766cabc58840ece
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-02 17:01:55 +0530

    Minimum number of platforms required

--- Greedy/Minimum number of platforms required in a railway station.py
+++ Greedy/Minimum number of platforms required in a railway station.py
@@ -1,18 +1,37 @@
+# Problem link - https://www.geeksforgeeks.org/maximum-trains-stoppage-can-provided/
+# Solution - https://www.youtube.com/watch?v=AsGzwR_FWok
+
+
 class Solution:
     @staticmethod
     def min_platforms(arrivals, departures):
+        """
+            Time complexity is O(n * log(n)) and space complexity is O(n).
+        """
+
+        # sort arrivals and departures so that we can find the overlaps.
         arrivals.sort()
         departures.sort()
+
+        # define tracking variables
         i, j = 0, 0
+
+        # define result variables
         count_min_platforms = 0
         platforms = 0
+
+        # traverse on both the arrays.
         while i < len(arrivals) and j < len(departures):
+            # if arrival time is <= departure time during tracking, we need a platform to accommodate
             if arrivals[i] <= departures[j]:
                 platforms += 1
                 i += 1
             else:
+                # if a train has departed, reduce the count of used platforms.
                 platforms -= 1
                 j += 1
+
+            # update the min platforms required
             count_min_platforms = max(count_min_platforms, platforms)
         return count_min_platforms
 
