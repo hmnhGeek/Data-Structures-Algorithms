d56e3233bb9ddf2af9f3e5b51628be0ca1dfa9c9
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-02 12:09:06 +0530

    Space optimized climbing stairs

--- Practice Set 3/Dynamic Programming/DP2 - Climbing Stairs.py
+++ Practice Set 3/Dynamic Programming/DP2 - Climbing Stairs.py
@@ -78,8 +78,36 @@ def tabulation():
     print(climbing_stairs(1))
 
 
+def space_optimized():
+    """
+        Time complexity is O(n) and space complexity is O(1).
+    """
+    def climbing_stairs(n):
+        prev = 1
+        prev2 = 0
+        for i in range(1, n + 1):
+            left = 0
+            if i - 1 >= 0:
+                left = prev
+            right = 0
+            if i - 2 >= 0:
+                right = prev2
+            curr = left + right
+            prev2 = prev
+            prev = curr
+        return prev
+
+    print(climbing_stairs(3))
+    print(climbing_stairs(2))
+    print(climbing_stairs(4))
+    print(climbing_stairs(5))
+    print(climbing_stairs(1))
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
+print()
+space_optimized()
