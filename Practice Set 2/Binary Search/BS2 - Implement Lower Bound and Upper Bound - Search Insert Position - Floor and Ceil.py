f9ce7c9a9e1fbcd803e062f8c1773eeb5995f6f0
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-02 14:46:41 +0530

    Ceil value using binary search

--- Practice Set 2/Binary Search/BS2 - Implement Lower Bound and Upper Bound - Search Insert Position - Floor and Ceil.py
+++ Practice Set 2/Binary Search/BS2 - Implement Lower Bound and Upper Bound - Search Insert Position - Floor and Ceil.py
@@ -50,6 +50,21 @@ class Solution:
                 high = mid - 1
         return arr[high] if high in range(len(arr)) else -1
 
+    @staticmethod
+    def get_ceil(arr, x):
+        """
+            Time complexity is O(log(n)) and space complexity is O(1).
+        """
+
+        low, high = 0, len(arr) - 1
+        while low <= high:
+            mid = int(low + (high - low) / 2)
+            if arr[mid] >= x:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return arr[low] if low in range(len(arr)) else -1
+
 
 print("Lower Bound")
 print(Solution.get_lower_bound([3, 5, 8, 15, 19], 5))
@@ -78,3 +93,9 @@ print(Solution.get_floor([10, 20, 30, 40, 50], 25))
 print(Solution.get_floor([10, 20, 30, 40, 50], 30))
 print(Solution.get_floor([10, 25, 30, 40, 50], 35))
 
+print()
+
+print("Ceil Value")
+print(Solution.get_ceil([10, 20, 30, 40, 50], 25))
+print(Solution.get_ceil([10, 20, 30, 40, 50], 30))
+print(Solution.get_ceil([10, 25, 30, 40, 50], 35))
