0b7a83bbdae7c947ae12b926d85c61d9a7cee4ce
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-03 13:34:41 +0530

    Fruits into baskets

--- Practice Set 5/Sliding Windows/L5 - Fruit Into Baskets.py
+++ Practice Set 5/Sliding Windows/L5 - Fruit Into Baskets.py
@@ -1,20 +1,35 @@
 class Solution:
     @staticmethod
     def fruits_into_baskets(arr):
+        # define window variables
         left = right = 0
         n = len(arr)
+
+        # define tracking variables
         d = {i: 0 for i in arr}
+
+        # define result variables
         longest_length = 0
         start_index = -1
+
+        # while there is ground to cover...
         while right < n:
             d[arr[right]] += 1
+
+            # if more than 2 baskets are used, shrink just one unit from left.
             if sum(1 for v in d.values() if v > 0) > 2:
                 d[arr[left]] -= 1
                 left += 1
+
+            # if baskets are within limit, update the result variables.
             if sum(1 for v in d.values() if v > 0) <= 2:
                 longest_length = max(longest_length, right - left + 1)
                 start_index = left
+
+            # increment right index.
             right += 1
+
+        # return the subarray.
         return arr[start_index:start_index+longest_length] if start_index != -1 else []
 
 
