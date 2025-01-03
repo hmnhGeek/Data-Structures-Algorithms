339b90c67a319b25c54b05957e8f87b4f8343a16
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-03 13:32:41 +0530

    Fruits into baskets

---
+++ Practice Set 5/Sliding Windows/L5 - Fruit Into Baskets.py
@@ -0,0 +1,27 @@
+class Solution:
+    @staticmethod
+    def fruits_into_baskets(arr):
+        left = right = 0
+        n = len(arr)
+        d = {i: 0 for i in arr}
+        longest_length = 0
+        start_index = -1
+        while right < n:
+            d[arr[right]] += 1
+            if sum(1 for v in d.values() if v > 0) > 2:
+                d[arr[left]] -= 1
+                left += 1
+            if sum(1 for v in d.values() if v > 0) <= 2:
+                longest_length = max(longest_length, right - left + 1)
+                start_index = left
+            right += 1
+        return arr[start_index:start_index+longest_length] if start_index != -1 else []
+
+
+print(Solution.fruits_into_baskets([3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4]))
+print(Solution.fruits_into_baskets([1, 2, 1]))
+print(Solution.fruits_into_baskets([0, 1, 2, 2]))
+print(Solution.fruits_into_baskets([1, 2, 3, 2, 2]))
+print(Solution.fruits_into_baskets([3, 1, 2, 2, 2, 2]))
+print(Solution.fruits_into_baskets([1, 1, 2, 3]))
+print(Solution.fruits_into_baskets([1, 2, 3, 4]))
