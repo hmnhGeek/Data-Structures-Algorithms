216451900dad77997f6d81b7fe33e217fed7c631
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-04 15:28:51 +0530

    Memoized count all palindromic subsequences

--- Strings/count_all_palindromic_subsequences.py
+++ Strings/count_all_palindromic_subsequences.py
@@ -27,4 +27,38 @@ def recursive():
     print(count("bccb"))
 
 
+def memoized():
+    """
+        Time complexity is O(n^2) and space complexity is O(2n).
+    """
+
+    def solve(string, i, j, dp):
+        if i > j:
+            return 0
+        if i == j:
+            return 1
+        if dp[i][j] is not None:
+            return dp[i][j]
+        # if there's a match, add 1 to count and solve for f(i + 1, j) and f(i, j - 1)
+        if string[i] == string[j]:
+            dp[i][j] = 1 + solve(string, i + 1, j, dp) + solve(string, i, j - 1, dp)
+        else:
+            # if there's no match, solve for f(i + 1, j) and f(i, j - 1) and subtract f(i + 1, j - 1).
+            dp[i][j] = solve(string, i + 1, j, dp) + solve(string, i, j - 1, dp) - solve(string, i + 1, j - 1, dp)
+        return dp[i][j]
+
+    def count(string):
+        n = len(string)
+        dp = {i: {j: None for j in range(n)} for i in range(n)}
+        return solve(string, 0, n - 1, dp)
+
+    print(count("abcd"))
+    print(count("aab"))
+    print(count("geeksforgeeks"))
+    print(count("103301"))
+    print(count("bccb"))
+
+
 recursive()
+print()
+memoized()
