9d3555434a6ec354e5557817e39d34a8016bfcca
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-04 13:28:39 +0530

    Common elements in all the rows of a matrix

--- Practice Set 3/Matrix/Common elements in all rows of a given matrix.py
+++ Practice Set 3/Matrix/Common elements in all rows of a given matrix.py
@@ -1,13 +1,26 @@
 class Solution:
     @staticmethod
     def get_commons(mtx):
+        """
+            Time complexity is O(nm) and space complexity is O(m).
+        """
+
         n, m = len(mtx), len(mtx[0])
         if n == 1:
             return mtx[0]
+
+        # get the common elements into a hashmap of size O(m) in worst case by taking into account common elements from
+        # the first two rows.
         hash_map = set(mtx[0]).intersection(set(mtx[1]))
+
+        # now loop on the rest of the rows...
         for i in range(2, n):
+            # form a temp set in O(m) time for this ith row
             temp_set = set(mtx[i])
+            # take intersection with hashmap in another O(m) time to filter out only the common elements into hashmap.
             hash_map = hash_map.intersection(temp_set)
+
+        # finally, return the hashmap storing all the common elements in the matrix.
         return hash_map
 
 
