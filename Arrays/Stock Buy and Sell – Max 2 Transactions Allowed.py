068349cb10c1e4a4e6082a8677a9c8221087597e
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-05 09:43:34 +0530

    Tabulation stocks - max 2 transactions allowed

--- Arrays/Stock Buy and Sell – Max 2 Transactions Allowed.py
+++ Arrays/Stock Buy and Sell – Max 2 Transactions Allowed.py
@@ -70,6 +70,39 @@ def memoized():
     print(buy_sell([1, 2, 3, 4, 5]))
 
 
+def tabulation():
+    """
+        Time complexity is O(2*2*n) and space complexity is O(4n).
+    """
+    def buy_sell(arr):
+        n = len(arr)
+        dp = {i: {j: {k: 0 for k in range(3)} for j in [True, False]} for i in range(n + 1)}
+        for index in range(n - 1, -1, -1):
+            for can_buy in [True, False]:
+                for k in range(1, 3):
+                    if can_buy:
+                        dp[index][can_buy][k] = max(
+                            -arr[index] + dp[index + 1][False][k],
+                            dp[index + 1][True][k]
+                        )
+                    else:
+                        dp[index][can_buy][k] = max(
+                            arr[index] + dp[index + 1][True][k - 1],
+                            dp[index + 1][False][k]
+                        )
+        return dp[0][True][2]
+
+    print(buy_sell([10, 22, 5, 75, 65, 80]))
+    print(buy_sell([100, 30, 15, 10, 8, 25, 80]))
+    print(buy_sell([90, 80, 70, 60, 50]))
+    print(buy_sell([10, 30, 50, 60]))
+    print(buy_sell([2, 30, 15, 10, 8, 25, 80]))
+    print(buy_sell([3, 3, 5, 0, 0, 3, 1, 4]))
+    print(buy_sell([1, 2, 3, 4, 5]))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
