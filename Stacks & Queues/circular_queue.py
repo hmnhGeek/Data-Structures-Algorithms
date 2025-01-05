31b7449b4dc26425980d6bfe1c327e4ad488ff48
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-05 13:49:45 +0530

    Implemented a circular queue

--- Stacks & Queues/circular_queue.py
+++ Stacks & Queues/circular_queue.py
@@ -1,3 +1,10 @@
+# Problem link - https://www.geeksforgeeks.org/introduction-to-circular-queue/
+
+"""
+    All operations are O(1) time and O(1) space.
+"""
+
+
 class CircularQueue:
     def __init__(self, n):
         self.queue = [None] * n
@@ -9,18 +16,26 @@ class CircularQueue:
         if self.size == self.capacity:
             print("Queue is full.")
             return
+
+        # wherever is the front pointer, add the current size and take modulo from capacity to get the rear index.
         rear = (self.front + self.size) % self.capacity
+        # set the element at rear index to x and increment the size.
         self.queue[rear] = x
         self.size += 1
+        # print the queue
         print(self.queue)
 
     def pop(self):
         if self.size == 0:
             print("Queue is empty.")
             return
+
+        # get the item at the front
         item = self.queue[self.front]
+        # set the item at front index to None and decrement the size.
         self.queue[self.front] = None
         self.size -= 1
+        # and increment the front pointer (but take modulo)
         self.front = (self.front + 1) % self.capacity
         print(self.queue)
         return item
