2cca1bce5b137bc5cc5303a42b6318b98a1cd5cd
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-05 12:11:52 +0530

    Created a Linked List class

---
+++ LinkedList/Sort a linked list of 0s, 1s and 2s.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = self.tail = None
+
+    def push(self, x):
+        node = Node(x)
+        if self.head is None:
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+
+    def show(self):
+        curr = self.head
+        while curr is not None:
+            print(curr.data, end=" ")
+            curr = curr.next
+        print()
+
+
