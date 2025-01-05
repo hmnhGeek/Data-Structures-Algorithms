a44fa613a163804a45f5c2675c8c3a91b655fd95
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-05 11:34:31 +0530

    Weighted job scheduling

--- Practice Set 1/Searching & Sorting/weighted_job_scheduling.py
+++ Practice Set 1/Searching & Sorting/weighted_job_scheduling.py
@@ -207,3 +207,27 @@ print(
         [60, 50, 70, 30]
     )
 )
+
+print(
+    TabulationSolution.schedule(
+        [1, 2, 3, 3],
+        [3, 4, 5, 6],
+        [50, 10, 40, 70]
+    )
+)
+
+print(
+    TabulationSolution.schedule(
+        [1, 2, 3, 4, 6],
+        [3, 5, 10, 6, 9],
+        [20, 20, 100, 70, 60]
+    )
+)
+
+print(
+    TabulationSolution.schedule(
+        [1, 1, 1],
+        [2, 3, 4],
+        [5, 6, 4]
+    )
+)
