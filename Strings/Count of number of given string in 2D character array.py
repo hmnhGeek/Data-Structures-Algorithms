0ccd00682a30857febf35b750159dc392557b49e
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-05 15:24:16 +0530

    Count of number of given string in 2D character array

---
+++ Strings/Count of number of given string in 2D character array.py
@@ -0,0 +1,55 @@
+class Solution:
+    @staticmethod
+    def _solve(mtx, i, j, word, index, w, n, m):
+        count = 0
+        if 0 <= i < n and 0 <= j < m and word[index] == mtx[i][j]:
+            temp = mtx[i][j]
+            mtx[i][j] = 0
+            index += 1
+            if index == w:
+                count = 1
+            else:
+                count += Solution._solve(mtx, i, j + 1, word, index, w, n, m)
+                count += Solution._solve(mtx, i + 1, j, word, index, w, n, m)
+                count += Solution._solve(mtx, i, j - 1, word, index, w, n, m)
+                count += Solution._solve(mtx, i - 1, j, word, index, w, n, m)
+            mtx[i][j] = temp
+        return count
+
+    @staticmethod
+    def count(mtx, word):
+        n, m = len(mtx), len(mtx[0])
+        count = 0
+        for i in range(n):
+            for j in range(m):
+                count += Solution._solve(mtx, i, j, word, 0, len(word), n, m)
+        return count
+
+
+print(
+    Solution.count(
+        [
+            ['D', 'D', 'D', 'G', 'D', 'D'],
+            ['B', 'B', 'D', 'E', 'B', 'S'],
+            ['B', 'S', 'K', 'E', 'B', 'K'],
+            ['D', 'D', 'D', 'D', 'D', 'E'],
+            ['D', 'D', 'D', 'D', 'D', 'E'],
+            ['D', 'D', 'D', 'D', 'D', 'G']
+        ],
+        "GEEKS"
+    )
+)
+
+print(
+    Solution.count(
+        [
+            ['B', 'B', 'M', 'B', 'B', 'B'],
+            ['C', 'B', 'A', 'B', 'B', 'B'],
+            ['I', 'B', 'G', 'B', 'B', 'B'],
+            ['G', 'B', 'I', 'B', 'B', 'B'],
+            ['A', 'B', 'C', 'B', 'B', 'B'],
+            ['M', 'C', 'I', 'G', 'A', 'M']
+        ],
+        "MAGIC"
+    )
+)
