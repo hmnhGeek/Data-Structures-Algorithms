b75b4746f146a2f3f6674ba7bf17a73b21f496de
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-09 17:49:31 +0530

    Memoized ninja training

--- Practice Set 3/Dynamic Programming/DP7 - Ninja's Training.py
+++ Practice Set 3/Dynamic Programming/DP7 - Ninja's Training.py
@@ -67,4 +67,72 @@ def recursive():
     )
 
 
+def memoized():
+    """
+        Time complexity is O(nk) and space complexity is O(n + nk).
+    """
+
+    def solve(mtx, i, j, k, dp):
+        if i == 0:
+            return mtx[0][j]
+        if dp[i][j] is not None:
+            return dp[i][j]
+        ans = -1e6
+        next_indices = get_next_activities_indices(j, k)
+        for index in next_indices:
+            ans = max(ans, mtx[i][j] + solve(mtx, i - 1, index, k, dp))
+        dp[i][j] = ans
+        return dp[i][j]
+
+    def ninjas_training(mtx):
+        n, k = len(mtx), len(mtx[0])
+        ans = -1e6
+        dp = {i: {j: None for j in range(k)} for i in range(n)}
+        for j in range(k):
+            ans = max(ans, solve(mtx, n - 1, j, k, dp))
+        return ans
+
+    print(
+        ninjas_training(
+            [
+                [1, 2, 5],
+                [3, 1, 1],
+                [3, 3, 3]
+            ]
+        )
+    )
+
+    print(
+        ninjas_training(
+            [
+                [10, 50, 1],
+                [5, 100, 11]
+            ]
+        )
+    )
+
+    print(
+        ninjas_training(
+            [
+                [10, 40, 70],
+                [20, 50, 80],
+                [30, 60, 90]
+            ]
+        )
+    )
+
+    print(
+        ninjas_training(
+            [
+                [18, 11, 19],
+                [4, 13, 7],
+                [1, 8, 13]
+            ]
+        )
+    )
+
+
 recursive()
+print()
+memoized()
+print()
