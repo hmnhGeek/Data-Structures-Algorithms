180ac73b2248f88f0deb4bdbebca977e13133abc
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-09 17:02:56 +0530

    Distance of nearest cell having 1

--- Practice Set 3/Graphs/G13 - Distance of nearest cell having 1.py
+++ Practice Set 3/Graphs/G13 - Distance of nearest cell having 1.py
@@ -31,3 +31,49 @@ class Queue:
         self.length -= 1
         return item
 
+
+class Solution:
+    @staticmethod
+    def _get_neighbours(mtx, i, j, n, m):
+        neighbours = []
+        if 0 <= i - 1 < n and mtx[i - 1][j] == 0:
+            neighbours.append((i - 1, j))
+        if 0 <= j + 1 < m and mtx[i][j + 1] == 0:
+            neighbours.append((i, j + 1))
+        if 0 <= i + 1 < n and mtx[i + 1][j] == 0:
+            neighbours.append((i + 1, j))
+        if 0 <= j - 1 < m and mtx[i][j - 1] == 0:
+            neighbours.append((i, j - 1))
+        return neighbours
+
+    @staticmethod
+    def find_distance(mtx):
+        n, m = len(mtx), len(mtx[0])
+        visited = [[False for _ in range(m)] for _ in range(n)]
+        distances = [[1e6 for _ in range(m)] for _ in range(n)]
+        queue = Queue()
+        for i in range(n):
+            for j in range(m):
+                if mtx[i][j] == 1:
+                    queue.push((i, j, 0))
+        while not queue.is_empty():
+            i, j, distance = queue.pop()
+            visited[i][j] = True
+            distances[i][j] = min(distances[i][j], distance)
+            neighbours = Solution._get_neighbours(mtx, i, j, n, m)
+            for neighbour in neighbours:
+                x, y = neighbour
+                if not visited[x][y]:
+                    queue.push((x, y, distance + 1))
+        return distances
+
+
+print(
+    Solution.find_distance(
+        [
+            [0, 0, 0],
+            [0, 1, 0],
+            [1, 0, 1]
+        ]
+    )
+)
