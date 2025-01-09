b330ad69be25f766f78d754fc5703ae56bb7753f
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-09 17:57:11 +0530

    Tabulation ninja training

--- Practice Set 3/Dynamic Programming/DP7 - Ninja's Training.py
+++ Practice Set 3/Dynamic Programming/DP7 - Ninja's Training.py
@@ -69,7 +69,7 @@ def recursive():
 
 def memoized():
     """
-        Time complexity is O(nk) and space complexity is O(n + nk).
+        Time complexity is O(nk^2) and space complexity is O(n + nk).
     """
 
     def solve(mtx, i, j, k, dp):
@@ -132,7 +132,70 @@ def memoized():
     )
 
 
+def tabulation():
+    """
+        Time complexity is O(nk^2) and space complexity is O(n + nk).
+    """
+    def ninjas_training(mtx):
+        n, k = len(mtx), len(mtx[0])
+        ans = -1e6
+        for j in range(k):
+            dp = {i: {j: -1e6 for j in range(k)} for i in range(n)}
+            for q in dp[0]:
+                dp[0][q] = mtx[0][q]
+            for i in range(1, n):
+                for j0 in range(k):
+                    res = -1e6
+                    next_indices = get_next_activities_indices(j0, k)
+                    for index in next_indices:
+                        res = max(res, mtx[i][j0] + dp[i - 1][index])
+                    dp[i][j0] = res
+            ans = max(ans, dp[n - 1][j])
+        return ans
+
+    print(
+        ninjas_training(
+            [
+                [1, 2, 5],
+                [3, 1, 1],
+                [3, 3, 3]
+            ]
+        )
+    )
+
+    print(
+        ninjas_training(
+            [
+                [10, 50, 1],
+                [5, 100, 11]
+            ]
+        )
+    )
+
+    print(
+        ninjas_training(
+            [
+                [10, 40, 70],
+                [20, 50, 80],
+                [30, 60, 90]
+            ]
+        )
+    )
+
+    print(
+        ninjas_training(
+            [
+                [18, 11, 19],
+                [4, 13, 7],
+                [1, 8, 13]
+            ]
+        )
+    )
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
 print()
