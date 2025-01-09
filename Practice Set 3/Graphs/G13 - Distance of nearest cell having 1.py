f1a70afcdd73ffc84ec0a0cadf77ddcbbbda0a2d
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-09 17:07:36 +0530

    Distance of nearest cell having 1

--- Practice Set 3/Graphs/G13 - Distance of nearest cell having 1.py
+++ Practice Set 3/Graphs/G13 - Distance of nearest cell having 1.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/distance-of-nearest-cell-having-1-1587115620/1
+# Solution - https://www.youtube.com/watch?v=edXdVwkYHF8&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=13
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -48,23 +52,39 @@ class Solution:
 
     @staticmethod
     def find_distance(mtx):
+        """
+            Time complexity is O(mn) and space complexity is O(mn).
+        """
+
         n, m = len(mtx), len(mtx[0])
+
+        # create a visited and distances matrix, initialized with False and inf for all the cells.
         visited = [[False for _ in range(m)] for _ in range(n)]
         distances = [[1e6 for _ in range(m)] for _ in range(n)]
+
+        # create a queue and push all the 1s into it with distance as 0. This will take O(nm) time.
         queue = Queue()
         for i in range(n):
             for j in range(m):
                 if mtx[i][j] == 1:
                     queue.push((i, j, 0))
+
+        # while not queue is empty (standard BFS)
         while not queue.is_empty():
+            # pop the node and the minimum distance from 1
             i, j, distance = queue.pop()
+            # mark the node as visited
             visited[i][j] = True
+            # update the distance matrix
             distances[i][j] = min(distances[i][j], distance)
+            # get the neighbours of the node
             neighbours = Solution._get_neighbours(mtx, i, j, n, m)
+            # loop on the neighbours and if it is not visited, push it to queue with 1 unit incremented distance.
             for neighbour in neighbours:
                 x, y = neighbour
                 if not visited[x][y]:
                     queue.push((x, y, distance + 1))
+        # return the distances.
         return distances
 
 
