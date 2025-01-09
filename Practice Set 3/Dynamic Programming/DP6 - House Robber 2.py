4041df50d7ee5fea6e1f132d3c3bec020897e925
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-09 17:24:37 +0530

    House robber 2 (tabulation)

--- Practice Set 3/Dynamic Programming/DP6 - House Robber 2.py
+++ Practice Set 3/Dynamic Programming/DP6 - House Robber 2.py
@@ -44,6 +44,27 @@ def memoized():
     print(house_robber([1, 5, 2, 1, 6]))
 
 
+def tabulation():
+    def house_robber(arr):
+        n = len(arr)
+        dp = {i: 0 for i in range(-2, n)}
+        for index in range(n):
+            left = arr[index] + dp[index - 2]
+            right = dp[index - 1]
+            dp[index] = max(left, right)
+        return dp[n - 1]
+
+    print(house_robber([2, 1, 4, 9]))
+    print(house_robber([1, 2, 4]))
+    print(house_robber([1, 2, 3, 5, 4]))
+    print(house_robber([1, 2, 3, 1, 3, 5, 8, 1, 9]))
+    print(house_robber([2, 7, 9, 3, 1]))
+    print(house_robber([1, 2, 3, 1]))
+    print(house_robber([1, 5, 2, 1, 6]))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
