683cd3598f540ed0b24d62998613982b015e4937
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-10 11:13:23 +0530

    Space Optimized Unique Paths 2

--- Practice Set 3/Dynamic Programming/DP9 - Unique Paths 2.py
+++ Practice Set 3/Dynamic Programming/DP9 - Unique Paths 2.py
@@ -215,9 +215,85 @@ def tabulation():
     )
 
 
+def space_optimized():
+    """
+        Time complexity is O(nm) and space complexity is O(nm).
+    """
+    def unique_paths(mtx):
+        n, m = len(mtx), len(mtx[0])
+        if mtx[0][0] == -1 or mtx[n - 1][m - 1] == -1:
+            return 0
+        prev = {j: 0 for j in range(m)}
+        prev[0] = 1
+        for i in range(n):
+            curr = {j: 0 for j in range(m)}
+            curr[0] = prev[0]
+            for j in range(m):
+                if i == 0 and j == 0:
+                    continue
+                left = 0
+                if 0 <= i - 1 < n and mtx[i - 1][j] == 0:
+                    left = prev[j]
+                right = 0
+                if 0 <= j - 1 < m and mtx[i][j - 1] == 0:
+                    right = curr[j - 1]
+                curr[j] = left + right
+            prev = curr
+        return prev[m - 1]
+
+    print(
+        unique_paths(
+            [
+                [0, 0, 0],
+                [0, -1, 0],
+                [0, 0, 0]
+            ]
+        )
+    )
+
+    print(
+        unique_paths(
+            [
+                [0, 0],
+                [0, 0]
+            ]
+        )
+    )
+
+    print(
+        unique_paths(
+            [
+                [0, -1],
+                [-1, 0]
+            ]
+        )
+    )
+
+    print(
+        unique_paths(
+            [
+                [0, -1],
+                [0, 0]
+            ]
+        )
+    )
+
+    print(
+        unique_paths(
+            [
+                [0, 0, -1, 0, -1],
+                [0, 0, 0, -1, 0],
+                [-1, 0, 0, -1, 0],
+                [0, 0, 0, 0, 0]
+            ]
+        )
+    )
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
 print()
+space_optimized()
