167e8268c802d805c7b3599caacd8005dcc8c79e
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-10 14:40:03 +0530

    Single element in a sorted array

--- Practice Set 2/Binary Search/BS8 - Single Element in a Sorted Array.py
+++ Practice Set 2/Binary Search/BS8 - Single Element in a Sorted Array.py
@@ -6,15 +6,15 @@ class Solution:
             return None
         if n == 1:
             return arr[0]
-        if n == 2 and arr[0] != arr[1]:
+
+        if arr[0] != arr[1]:
             return arr[0]
-        if n == 3:
-            if arr[0] != arr[1]:
-                return arr[0]
+        if arr[-1] != arr[-2]:
             return arr[-1]
+
         low, high = 1, n - 2
         while low <= high:
-            mid = int(low + (high - low)/2)
+            mid = int(low + (high - low) / 2)
             if arr[mid] != arr[mid - 1] and arr[mid] != arr[mid + 1]:
                 return arr[mid]
             if (mid % 2 == 1 and arr[mid - 1] == arr[mid]) or (mid % 2 == 0 and arr[mid] == arr[mid + 1]):
@@ -27,3 +27,6 @@ class Solution:
 print(Solution.get_element([1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6]))
 print(Solution.get_element([1, 1, 2]))
 print(Solution.get_element([1, 2, 2]))
+print(Solution.get_element([1, 1, 2, 2, 4, 5, 5]))
+print(Solution.get_element([1, 1, 3, 5, 5]))
+print(Solution.get_element([1, 1, 4, 4, 15]))
