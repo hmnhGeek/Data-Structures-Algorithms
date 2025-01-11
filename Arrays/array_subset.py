d8bbae034ef510c3aa89a6bd704dab7c1c223eee
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-11 12:52:25 +0530

    Array subset problem

--- Arrays/array_subset.py
+++ Arrays/array_subset.py
@@ -1,13 +1,30 @@
+# Problem link - https://www.geeksforgeeks.org/problems/array-subset-of-another-array2317/1
+
+
 class Solution:
     @staticmethod
     def is_subset(a, b):
+        """
+            We are checking for b in `a`.
+
+            Time complexity is O(m + n) and space complexity is O(n).
+        """
+
+        # if b is of size greater than that of a, then b cannot be a subset of a.
         if len(b) > len(a):
             return False
         m, n = len(a), len(b)
+
+        # initialize a dictionary of size `n` which will store the status of b's elements found in a.
         found_status = {i: False for i in b}
+
+        # now loop in `a`.
         for i in range(m):
+            # if a[i] is present in dictionary, mark it as found.
             if a[i] in found_status:
                 found_status[a[i]] = True
+
+        # b will be a subset of a only if all found_status values are True.
         return all(v for v in found_status.values())
 
 
