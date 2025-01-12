83c422d3fd02c1d7c201ad75e247e43e49593a73
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-12 17:12:20 +0530

    Does triplet sum equal to target exists - custom quick sort

--- Arrays/triplet_sum.py
+++ Arrays/triplet_sum.py
@@ -1,7 +1,35 @@
+class QuickSort:
+    @staticmethod
+    def _get_partition_index(arr, low, high):
+        pivot = arr[low]
+        i, j = low, high
+        while i < j:
+            while arr[i] <= pivot and i <= high - 1:
+                i += 1
+            while arr[j] > pivot and j >= low + 1:
+                j -= 1
+            if i < j:
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[low], arr[j] = arr[j], arr[low]
+        return j
+
+    @staticmethod
+    def _sort(arr, low, high):
+        if low >= high:
+            return
+        partition_index = QuickSort._get_partition_index(arr, low, high)
+        QuickSort._sort(arr, low, partition_index - 1)
+        QuickSort._sort(arr, partition_index + 1, high)
+
+    @staticmethod
+    def sort(arr):
+        QuickSort._sort(arr, 0, len(arr) - 1)
+
+
 class Solution:
     @staticmethod
     def is_present(arr, target):
-        arr.sort()
+        QuickSort.sort(arr)
         n = len(arr)
         for i in range(n - 2):
             j, k = i + 1, n - 1
