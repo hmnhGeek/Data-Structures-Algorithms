39bd8d02b6b474046d2930b4bc87de5f571219cd
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-16 11:33:49 +0530

    Recursive triangle DP problem

---
+++ Practice Set 3/Dynamic Programming/DP11 - Triangle.py
@@ -0,0 +1,92 @@
+def recursive():
+    """
+        Time complexity is O(2^{nm}) and space complexity is O(n + m).
+    """
+    def solve(mtx, i, j, n):
+        if i == j == 0:
+            return mtx[0][0]
+        left = 1e6
+        if 0 <= i - 1 < n and 0 <= j < i:
+            left = mtx[i][j] + solve(mtx, i - 1, j, n)
+        right = 1e6
+        if 0 <= i - 1 < n and j >= 1:
+            right = mtx[i][j] + solve(mtx, i - 1, j - 1, n)
+        return min(left, right)
+
+    def triangle(mtx):
+        n = len(mtx)
+        min_ans = 1e6
+        for j in range(n):
+            cost = solve(mtx, n - 1, j, n)
+            min_ans = min(min_ans, cost)
+        return min_ans
+
+    print(
+        triangle(
+            [
+                [1],
+                [2, 3],
+                [3, 6, 7],
+                [8, 9, 6, 10]
+            ]
+        )
+    )
+
+    print(
+        triangle(
+            [
+                [2],
+                [3, 4],
+                [6, 5, 7],
+                [4, 1, 8, 3]
+            ]
+        )
+    )
+
+    print(triangle([[-10]]))
+
+    print(
+        triangle(
+            [
+                [1],
+                [2, 3],
+                [4, 5, 6],
+                [7, 8, 9, 10]
+            ]
+        )
+    )
+
+    print(
+        triangle(
+            [
+                [5],
+                [-1, 3],
+                [22, 1, -9]
+            ]
+        )
+    )
+
+    print(
+        triangle(
+            [
+                [2],
+                [3, 7],
+                [8, 5, 6],
+                [6, 1, 9, 3]
+            ]
+        )
+    )
+
+    print(
+        triangle(
+            [
+                [3],
+                [6, 9],
+                [8, 7, 1],
+                [9, 6, 8, 2]
+            ]
+        )
+    )
+
+
+recursive()
