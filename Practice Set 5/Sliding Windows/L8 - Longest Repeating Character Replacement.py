1499641ef5a3a037b520293ef5cb1dd8b12a1db1
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-16 18:10:24 +0530

    Longest repeating character replacement

--- Practice Set 5/Sliding Windows/L8 - Longest Repeating Character Replacement.py
+++ Practice Set 5/Sliding Windows/L8 - Longest Repeating Character Replacement.py
@@ -1,20 +1,35 @@
 class Solution:
     @staticmethod
     def longest_repeating_character_replacement(string, k):
+        # define variables
         left = right = 0
         n = len(string)
+
+        # define tracking dictionary
         d = {i: 0 for i in string}
+
+        # define result variables
         longest_length = 0
         start_index = -1
+
+        # while there is ground to cover
         while right < n:
             d[string[right]] += 1
+
+            # if length - max_freq > k, shrink just 1 unit from left
             if (right - left + 1) - max(d.values()) > k:
                 d[string[left]] -= 1
                 left += 1
+
+            # else update result variables
             if (right - left + 1) - max(d.values()) <= k:
                 longest_length = max(longest_length, right - left + 1)
                 start_index = left
+
+            # increment right index
             right += 1
+
+        # return the substring
         return string[start_index:start_index+longest_length] if start_index != -1 else ""
 
 
