00cf1473b80f25bb2d701370fc3725529025e6d8
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-16 18:52:53 +0530

    Check if it is possible to survive on Island

--- Greedy/Check if it is possible to survive on Island.py
+++ Greedy/Check if it is possible to survive on Island.py
@@ -14,12 +14,22 @@ class Solution:
 
         if n < m:
             return -1
+
+        # find the actual number of days you can buy the food (excluding sundays)
         num_sundays = s//7
         available_days_to_buy = s - num_sundays
+
+        # find the amount of food needed to survive in total
         total_food_needed = s * m
+
+        # find the number of days you're required to buy to store the complete ration.
         days_food_bought = ceil(total_food_needed/n)
+
+        # if the days required to buy < available days to buy, return answer.
         if days_food_bought <= available_days_to_buy:
             return days_food_bought
+
+        # else return -1.
         return -1
 
 
