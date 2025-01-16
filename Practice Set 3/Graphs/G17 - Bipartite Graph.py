233c01aa5c6d35d59ddf87a79acf555b0785e02b
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-16 17:10:16 +0530

    Bipartite Graph using BFS

--- Practice Set 3/Graphs/G17 - Bipartite Graph.py
+++ Practice Set 3/Graphs/G17 - Bipartite Graph.py
@@ -32,3 +32,99 @@ class Queue:
         return item
 
 
+class Solution:
+    @staticmethod
+    def _bfs(graph, node, color, colors):
+        queue = Queue()
+        queue.push((node, color))
+        while not queue.is_empty():
+            curr, col = queue.pop()
+            colors[curr] = col
+            for adj_node in graph[curr]:
+                if colors[adj_node] is None:
+                    queue.push((adj_node, 0 if col == 1 else 1))
+                elif colors[adj_node] == col:
+                    return False
+        return True
+
+    @staticmethod
+    def is_bipartite(graph):
+        colors = {i: None for i in graph}
+        for node in graph:
+            if colors[node] is None:
+                if not Solution._bfs(graph, node, 0, colors):
+                    return False
+        return True
+
+
+print(
+    Solution.is_bipartite(
+        {
+            1: [2],
+            2: [3, 6],
+            3: [2, 4],
+            4: [3, 5, 7],
+            5: [4, 6],
+            6: [2, 5],
+            7: [4, 8],
+            8: [7]
+        }
+    )
+)
+
+print(
+    Solution.is_bipartite(
+        {
+            1: [2],
+            2: [1, 3, 5],
+            3: [2, 4],
+            4: [3, 5, 6],
+            5: [2, 4],
+            6: [4]
+        }
+    )
+)
+
+print(
+    Solution.is_bipartite(
+        {
+            0: [1],
+            1: [0, 2],
+            2: [1]
+        }
+    )
+)
+
+print(
+    Solution.is_bipartite(
+        {
+            0: [2, 3],
+            1: [2],
+            2: [0, 1, 3],
+            3: [0, 2]
+        }
+    )
+)
+
+print(
+    Solution.is_bipartite(
+        {
+            0: [1, 2, 3],
+            1: [0, 2],
+            2: [0, 1, 3],
+            3: [0, 2]
+        }
+    )
+)
+
+print(
+    Solution.is_bipartite(
+        {
+            0: [1, 3],
+            1: [0, 2],
+            2: [1, 3],
+            3: [0, 2]
+        }
+    )
+)
+
