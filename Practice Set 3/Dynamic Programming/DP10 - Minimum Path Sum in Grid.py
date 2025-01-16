3723816b7630989a0ac4a72c53049fa8d57c99d4
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-16 10:48:24 +0530

    Minimum path sum in grid - Memoized

--- Practice Set 3/Dynamic Programming/DP10 - Minimum Path Sum in Grid.py
+++ Practice Set 3/Dynamic Programming/DP10 - Minimum Path Sum in Grid.py
@@ -58,4 +58,70 @@ def recursive():
     print(min_path_sum([[1, 2, 3], [4, 5, 6]]))
 
 
+def memoized():
+    """
+        Time complexity is O(nm) and space complexity is O(n + m + nm).
+    """
+
+    def solve(mtx, i, j, n, m, dp):
+        if i == 0 and j == 0:
+            return mtx[0][0]
+        if dp[i][j] is not None:
+            return dp[i][j]
+        left = 1e6
+        if 0 <= i - 1 < n:
+            left = mtx[i][j] + solve(mtx, i - 1, j, n, m, dp)
+        right = 1e6
+        if 0 <= j - 1 < m:
+            right = mtx[i][j] + solve(mtx, i, j - 1, n, m, dp)
+        dp[i][j] = min(left, right)
+        return dp[i][j]
+
+    def min_path_sum(mtx):
+        n, m = len(mtx), len(mtx[0])
+        dp = {i: {j: None for j in range(m)} for i in range(n)}
+        return solve(mtx, n - 1, m - 1, n, m, dp)
+
+    print(
+        min_path_sum(
+            [
+                [5, 9, 6],
+                [11, 5, 2]
+            ]
+        )
+    )
+
+    print(
+        min_path_sum(
+            [
+                [5]
+            ]
+        )
+    )
+
+    print(
+        min_path_sum(
+            [
+                [1, 2, 3],
+                [4, 5, 4],
+                [7, 5, 9]
+            ]
+        )
+    )
+
+    print(
+        min_path_sum(
+            [
+                [5, 6],
+                [1, 2]
+            ]
+        )
+    )
+
+    print(min_path_sum([[1, 3, 1], [1, 5, 1], [4, 2, 1]]))
+    print(min_path_sum([[1, 2, 3], [4, 5, 6]]))
+
+
 recursive()
+print()
+memoized()
