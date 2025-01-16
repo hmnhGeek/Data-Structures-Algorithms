f5f6349e694559799b3480bf18b9e668e5c97297
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-16 17:13:52 +0530

    Bipartite Graph using BFS

--- Practice Set 3/Graphs/G17 - Bipartite Graph (BFS).py
+++ Practice Set 3/Graphs/G17 - Bipartite Graph (BFS).py
@@ -1,3 +1,7 @@
+# Problem link - https://leetcode.com/problems/is-graph-bipartite/description/
+# Solution - https://www.youtube.com/watch?v=-vu34sct1g8&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=17
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -60,6 +64,10 @@ class Solution:
 
     @staticmethod
     def is_bipartite(graph):
+        """
+            Time complexity is O(V + E) and space complexity is O(V).
+        """
+
         # create a dictionary to store the colors on the nodes
         colors = {i: None for i in graph}
 
