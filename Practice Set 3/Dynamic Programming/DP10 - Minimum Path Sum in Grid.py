1b657184236c58aeed9ab00e1ea2bc0b357e6397
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-16 10:46:31 +0530

    Minimum path sum in grid

---
+++ Practice Set 3/Dynamic Programming/DP10 - Minimum Path Sum in Grid.py
@@ -0,0 +1,61 @@
+def recursive():
+    """
+        Time complexity is O(2^{n + m}) and space complexity is O(n + m).
+    """
+
+    def solve(mtx, i, j, n, m):
+        if i == 0 and j == 0:
+            return mtx[0][0]
+        left = 1e6
+        if 0 <= i - 1 < n:
+            left = mtx[i][j] + solve(mtx, i - 1, j, n, m)
+        right = 1e6
+        if 0 <= j - 1 < m:
+            right = mtx[i][j] + solve(mtx, i, j - 1, n, m)
+        return min(left, right)
+
+    def min_path_sum(mtx):
+        n, m = len(mtx), len(mtx[0])
+        return solve(mtx, n - 1, m - 1, n, m)
+
+    print(
+        min_path_sum(
+            [
+                [5, 9, 6],
+                [11, 5, 2]
+            ]
+        )
+    )
+
+    print(
+        min_path_sum(
+            [
+                [5]
+            ]
+        )
+    )
+
+    print(
+        min_path_sum(
+            [
+                [1, 2, 3],
+                [4, 5, 4],
+                [7, 5, 9]
+            ]
+        )
+    )
+
+    print(
+        min_path_sum(
+            [
+                [5, 6],
+                [1, 2]
+            ]
+        )
+    )
+
+    print(min_path_sum([[1, 3, 1], [1, 5, 1], [4, 2, 1]]))
+    print(min_path_sum([[1, 2, 3], [4, 5, 6]]))
+
+
+recursive()
