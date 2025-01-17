12552890393087e3781ba43a7c0feb3ec2dd8797
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-17 14:11:03 +0530

    Tabulation Maximum Falling Path Sum

--- Practice Set 3/Dynamic Programming/DP12 - Maximum Falling Path Sum.py
+++ Practice Set 3/Dynamic Programming/DP12 - Maximum Falling Path Sum.py
@@ -140,6 +140,78 @@ def memoized():
     )
 
 
+def tabulation():
+    """
+        Time complexity is O(nm^2) and space complexity is O(nm).
+    """
+    def max_falling_path(mtx):
+        n, m = len(mtx), len(mtx[0])
+        ans = -1e6
+        for start_j in range(m):
+            dp = {i: {j: -1e6 for j in range(m)} for i in range(n)}
+            for j in dp[0]:
+                dp[0][j] = mtx[0][j]
+            for i in range(1, n):
+                for j in range(m):
+                    left = -1e6
+                    if 0 <= i - 1 < n and 0 <= j - 1 < m:
+                        left = mtx[i][j] + dp[i - 1][j - 1]
+                    middle = -1e6
+                    if 0 <= i - 1 < n:
+                        middle = mtx[i][j] + dp[i - 1][j]
+                    right = -1e6
+                    if 0 <= i - 1 < n and 0 <= j + 1 < m:
+                        right = mtx[i][j] + dp[i - 1][j + 1]
+                    dp[i][j] = max(left, middle, right)
+            cost = dp[n - 1][start_j]
+            ans = max(ans, cost)
+        return ans
+
+    print(
+        max_falling_path(
+            [
+                [1, 2, 10, 4],
+                [100, 3, 2, 1],
+                [1, 1, 20, 2],
+                [1, 2, 2, 1]
+            ]
+        )
+    )
+
+    print(
+        max_falling_path(
+            [
+                [10, 2, 3],
+                [3, 7, 2],
+                [8, 1, 5]
+            ]
+        )
+    )
+
+    print(
+        max_falling_path(
+            [
+                [1, 2, 3],
+                [9, 8, 7],
+                [4, 5, 6]
+            ]
+        )
+    )
+
+    print(
+        max_falling_path(
+            [
+                [10, 10, 2, -13, 20, 4],
+                [1, -9, -81, 30, 2, 5],
+                [0, 10, 4, -79, 2, -10],
+                [1, -5, 2, 20, -11, 4]
+            ]
+        )
+    )
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
