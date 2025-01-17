791cd335ef17ad4540422e17a45d77d4548cfc3d
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-17 11:36:16 +0530

    Bipartite Graph using DFS

--- Practice Set 3/Graphs/G18 - Bipartite Graph (DFS).py
+++ Practice Set 3/Graphs/G18 - Bipartite Graph (DFS).py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/bipartite-graph/1
+# Solution - https://www.youtube.com/watch?v=KG5YFfR0j8A&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=18
+
+
 class Solution:
     @staticmethod
     def _dfs(graph, node, color, colors):
@@ -19,6 +23,10 @@ class Solution:
 
     @staticmethod
     def is_bipartite(graph):
+        """
+            Time complexity is O(V + E) and space complexity is O(V).
+        """
+
         # initialize a colors array to store the colors
         colors = {i: None for i in graph}
 
