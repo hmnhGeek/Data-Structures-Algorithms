cae8891112962c502ec88fbb242d903f1658575d
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-17 17:43:12 +0530

    Tabulation cherry pickup 2

--- Practice Set 3/Dynamic Programming/DP13 - Cherry Pickup II.py
+++ Practice Set 3/Dynamic Programming/DP13 - Cherry Pickup II.py
@@ -245,6 +245,131 @@ def memoized():
     )
 
 
+def tabulation():
+    """
+        Time complexity is O(m^2 * m^2 * n) and space complexity is O(nm^2).
+    """
+    def cherry_pickup(mtx):
+        n, m = len(mtx), len(mtx[0])
+        max_chocos = 0
+        dp = {i: {a: {b: 0 for b in range(m)} for a in range(m)} for i in range(n)}
+
+        for a in dp[0]:
+            for b in dp[0][a]:
+                if a == 0:
+                    # then chocolates for sure will contain mtx[0][0]
+                    _sum = mtx[0][0]
+                    # if bob is also at its place, i.e., (0, m - 1), then add bob's chocolates also.
+                    if b == m - 1:
+                        _sum += mtx[0][m - 1]
+                    # finally, return the chocolates
+                    dp[0][a][b] = _sum
+                else:
+                    # however, if alice is not at (0, 0)
+
+                    # and bob is at its place, then return only bob's chocolates
+                    if b == m - 1:
+                        dp[0][a][b] = mtx[0][m - 1]
+                    else:
+                        # if both of them are at wrong places, return 0 chocolates.
+                        dp[0][a][b] = 0
+
+        # initialize starting points of alice and bob by looping m^2 times.
+        for alice in range(m):
+            for bob in range(m):
+                for i in range(1, n):
+                    for a in range(m):
+                        for b in range(m):
+                            # initialize a cost variable to find the max chocolates collected from all the paths.
+                            cost = 0
+                            for da in [-1, 0, 1]:
+                                for db in [-1, 0, 1]:
+                                    next_a, next_b = a + da, b + db
+                                    if 0 <= next_a < m and 0 <= next_b < m and 0 <= i - 1 < n:
+                                        count = dp[i - 1][next_a][next_b]
+                                        # if both alice and bob are at same cell, add chocolate only once.
+                                        if a == b:
+                                            count += mtx[i][a]
+                                        else:
+                                            # else add chocolates of both
+                                            count += mtx[i][a] + mtx[i][b]
+                                        # update the max chocolates
+                                        cost = max(cost, count)
+                            # return the max chocolates obtained.
+                            dp[i][a][b] = cost
+
+                # retrieve the max chocolates obtained
+                cost = dp[n - 1][alice][bob]
+                # update the max chocolates
+                max_chocos = max(max_chocos, cost)
+        # return the max chocolates obtained.
+        return max_chocos
+
+    print(
+        cherry_pickup(
+            [
+                [2, 3, 1, 2],
+                [3, 4, 2, 2],
+                [5, 6, 3, 5]
+            ]
+        )
+    )
+
+    print(
+        cherry_pickup(
+            [
+                [1, 1],
+                [1, 2]
+            ]
+        )
+    )
+
+    print(
+        cherry_pickup(
+            [
+                [3, 7],
+                [7, 6]
+            ]
+        )
+    )
+
+    print(
+        cherry_pickup(
+            [
+                [4, 5],
+                [3, 7],
+                [4, 2]
+            ]
+        )
+    )
+
+    print(cherry_pickup([[3, 1, 1], [2, 5, 1], [1, 5, 5], [2, 1, 1]]))
+    print(
+        cherry_pickup(
+            [
+                [4, 1, 2],
+                [3, 6, 1],
+                [1, 6, 6],
+                [3, 1, 2]
+            ]
+        )
+    )
+
+    print(
+        cherry_pickup(
+            [
+                [2, 0, 0, 0, 0, 0, 2],
+                [2, 1, 0, 0, 0, 4, 0],
+                [2, 0, 10, 0, 1, 0, 0],
+                [0, 3, 0, 6, 5, 0, 0],
+                [1, 0, 3, 4, 0, 0, 6]
+            ]
+        )
+    )
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
