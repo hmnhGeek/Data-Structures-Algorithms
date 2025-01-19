c5d081ffdbe6601a20e6d50b81ab8e729591272d
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-19 11:29:21 +0530

    Created a linked list class

---
+++ Practice Set 2/Heap/Merge K sorted linked lists.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = self.tail = None
+        self.length = 0
+
+    def is_empty(self):
+        return self.length == 0
+
+    def push(self, x):
+        node = Node(x)
+        if self.is_empty():
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+        self.length += 1
+
+    def build(self, *args):
+        for i in args:
+            self.push(i)
+
+    def show(self):
+        curr = self.head
+        while curr is not None:
+            print(curr.data, end=" ")
+            curr = curr.next
+        print()
+
