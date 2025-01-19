803623da5963aa03a60efdf71ae8629364f87742
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-19 13:20:16 +0530

    Trap Rainwater - Optimal Solution

--- Arrays/Trapping Rain Water.py
+++ Arrays/Trapping Rain Water.py
@@ -117,9 +117,41 @@ class Solution:
         return water_collected
 
 
+print("Better Solution")
 print(Solution.trap_rainwater([3, 0, 1, 0, 4, 0, 2]))
 print(Solution.trap_rainwater([3, 0, 2, 0, 4]))
 print(Solution.trap_rainwater([1, 2, 3, 4]))
 print(Solution.trap_rainwater([2, 1, 5, 3, 1, 0, 4]))
 print(Solution.trap_rainwater([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
 print(Solution.trap_rainwater([4, 2, 0, 3, 2, 5]))
+print()
+
+
+class OptimalSolution:
+    @staticmethod
+    def trap_rainwater(arr):
+        n = len(arr)
+        left_max, right_max, left, right, total_water = 0, 0, 0, n - 1, 0
+        while left < right:
+            if arr[left] <= arr[right]:
+                if left_max > arr[left]:
+                    total_water += (left_max - arr[left])
+                else:
+                    left_max = arr[left]
+                left += 1
+            else:
+                if right_max > arr[right]:
+                    total_water += (right_max - arr[right])
+                else:
+                    right_max = arr[right]
+                right -= 1
+        return total_water
+
+
+print("Optimal Solution")
+print(OptimalSolution.trap_rainwater([3, 0, 1, 0, 4, 0, 2]))
+print(OptimalSolution.trap_rainwater([3, 0, 2, 0, 4]))
+print(OptimalSolution.trap_rainwater([1, 2, 3, 4]))
+print(OptimalSolution.trap_rainwater([2, 1, 5, 3, 1, 0, 4]))
+print(OptimalSolution.trap_rainwater([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
+print(OptimalSolution.trap_rainwater([4, 2, 0, 3, 2, 5]))
