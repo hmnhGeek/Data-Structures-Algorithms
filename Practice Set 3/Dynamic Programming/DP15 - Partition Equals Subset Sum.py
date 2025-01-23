2edc714e44c170d520eb148b6a2ebf21575ba23a
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-23 10:02:24 +0530

    Partition equals subset sum

--- Practice Set 3/Dynamic Programming/DP15 - Partition Equals Subset Sum.py
+++ Practice Set 3/Dynamic Programming/DP15 - Partition Equals Subset Sum.py
@@ -116,10 +116,39 @@ def space_optimized():
     print(subset_sum([3, 34, 4, 12, 5, 2], 30))
 
 
-recursive()
-print()
-memoized()
-print()
-tabulation()
-print()
-space_optimized()
+class Solution:
+    @staticmethod
+    def _subset_sum(arr, target):
+        n = len(arr)
+        prev = {j: False for j in range(target + 1)}
+        prev[0] = True
+        prev[arr[0]] = True
+        for i in range(1, n):
+            curr = {j: False for j in range(target + 1)}
+            curr[0] = True
+            for k in range(target + 1):
+                left = False
+                if arr[i] <= k:
+                    left = prev[k - arr[i]]
+                right = prev[k]
+                curr[k] = left or right
+            prev = curr
+        return prev[target]
+
+    @staticmethod
+    def partition_equals_subset_sum(arr):
+        _sum = sum(arr)
+        if _sum % 2 == 1:
+            return False
+        target = _sum // 2
+        return Solution._subset_sum(arr, target)
+
+
+print(Solution.partition_equals_subset_sum([2, 3, 3, 3, 4, 5]))
+print(Solution.partition_equals_subset_sum([3, 1, 1, 2, 2, 1]))
+print(Solution.partition_equals_subset_sum([5, 6, 5, 11, 6]))
+print(Solution.partition_equals_subset_sum([2, 2, 1, 1, 1, 1, 1, 3, 3]))
+print(Solution.partition_equals_subset_sum([8, 7, 6, 12, 4, 5]))
+print(Solution.partition_equals_subset_sum([1, 5, 11, 5]))
+print(Solution.partition_equals_subset_sum([1, 3, 5]))
+print(Solution.partition_equals_subset_sum([1, 2, 3, 5]))
