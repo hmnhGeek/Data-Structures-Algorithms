680c8086deaac084eb71d150e19bd12c8f9d4fac
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-23 10:53:35 +0530

    Find eventual safe states using DFS

--- Practice Set 3/Graphs/G20 - Find Eventual Safe States - DFS.py
+++ Practice Set 3/Graphs/G20 - Find Eventual Safe States - DFS.py
@@ -1,26 +1,42 @@
 class Solution:
     @staticmethod
     def _dfs(graph, node, visited, path_visited, safe_nodes):
+        # mark this node as visited and path visited
         visited[node] = True
         path_visited[node] = True
+
+        # loop on the adjacent nodes of the graph
         for adj_node in graph[node]:
+            # if this node is not visited
             if not visited[adj_node]:
+                # and if this adj_node leads to a cycle, mark it as unsafe.
                 if Solution._dfs(graph, adj_node, visited, path_visited, safe_nodes):
                     safe_nodes[adj_node] = False
                     return True
+            # or if, this node is visited and path visited, then there's a cycle, again mark this adj_node as unsafe.
             elif path_visited[adj_node]:
                 safe_nodes[adj_node] = False
                 return True
+
+        # unmark this node from path visited.
         path_visited[node] = False
+
+        # no cycle was detected, return False.
         return False
 
     @staticmethod
     def find_safe_states(graph):
+        # create visited and path visited arrays to detect cycles
         visited = {i: False for i in graph}
         path_visited = {i: False for i in graph}
+
+        # create a dictionary to store the safe nodes. Assume all of them are safe initially.
         safe_nodes = {i: True for i in graph}
+
+        # loop on the graph components.
         for node in graph:
             if not visited[node]:
+                # if there is a cycle from this node, mark this node as unsafe.
                 if Solution._dfs(graph, node, visited, path_visited, safe_nodes):
                     safe_nodes[node] = False
         return safe_nodes
