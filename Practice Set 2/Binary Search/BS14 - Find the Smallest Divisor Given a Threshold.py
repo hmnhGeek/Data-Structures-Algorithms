965178ffeddcedcc63b4c9e97b5c7c55ae9fe311
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-23 11:33:16 +0530

    Find the Smallest Divisor Given a Threshold

--- Practice Set 2/Binary Search/BS14 - Find the Smallest Divisor Given a Threshold.py
+++ Practice Set 2/Binary Search/BS14 - Find the Smallest Divisor Given a Threshold.py
@@ -11,17 +11,26 @@ class Solution:
 
     @staticmethod
     def smallest_divisor(arr, threshold):
+        # if the count of array elements is more than threshold, then we can never find the answer.
         if threshold < len(arr):
             return -1
+
+        # create a search space.
         n = len(arr)
         low, high = 1, max(arr)
         while low <= high:
             mid = int(low + (high - low)/2)
+            # find the value obtained by using mid as divisor.
             val = Solution._get_val(arr, mid)
+
+            # if the value is <= threshold, then we can try for even lower divisor
             if val <= threshold:
                 high = mid - 1
             else:
+                # else, if the value > threshold, we must increase the denominator to lower down the value.
                 low = mid + 1
+
+        # return low, which points to the minimum divisor required.
         return low
 
 
