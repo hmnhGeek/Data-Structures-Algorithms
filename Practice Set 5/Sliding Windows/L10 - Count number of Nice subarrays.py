c81a364022f22bc2adb56930c7f6f8c58e32c71b
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-23 11:16:17 +0530

    Count nice subarrays

---
+++ Practice Set 5/Sliding Windows/L10 - Count number of Nice subarrays.py
@@ -0,0 +1,30 @@
+class Solution:
+    @staticmethod
+    def _count_less_than_equal_to(arr, k):
+        if k < 0:
+            return 0
+
+        left = right = 0
+        n = len(arr)
+        _sum = count = 0
+
+        while right < n:
+            _sum += arr[right] % 2
+            while _sum > k:
+                _sum -= arr[left] % 2
+                left += 1
+            if _sum <= k:
+                count += (right - left + 1)
+            right += 1
+        return count
+
+    @staticmethod
+    def get_nice_subarray_count(arr, k):
+        return Solution._count_less_than_equal_to(arr, k) - Solution._count_less_than_equal_to(arr, k - 1)
+
+
+print(Solution.get_nice_subarray_count([1, 5, 2, 1, 1], 3))
+print(Solution.get_nice_subarray_count([2, 4, 6], 1))
+print(Solution.get_nice_subarray_count([2, 2, 2, 1, 2, 2, 1, 2, 2, 2], 2))
+print(Solution.get_nice_subarray_count([2, 5, 6, 9], 2))
+print(Solution.get_nice_subarray_count([2, 2, 5, 6, 9, 2, 11], 2))
