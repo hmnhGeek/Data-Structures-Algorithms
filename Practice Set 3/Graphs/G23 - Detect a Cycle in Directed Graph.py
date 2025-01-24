add1be0b2bac7c5188323dea47a7a0d850d2e91c
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-24 10:42:37 +0530

    Detect cycle in a DAG using BFS

--- Practice Set 3/Graphs/G23 - Detect a Cycle in Directed Graph.py
+++ Practice Set 3/Graphs/G23 - Detect a Cycle in Directed Graph.py
@@ -32,3 +32,73 @@ class Queue:
         return item
 
 
+class Solution:
+    @staticmethod
+    def has_cycle(graph):
+        indegrees = {i: 0 for i in graph}
+        Solution._update_indegrees(graph, indegrees)
+        queue = Queue()
+        for node in graph:
+            if indegrees[node] == 0:
+                queue.push(node)
+        count = 0
+        while not queue.is_empty():
+            node = queue.pop()
+            count += 1
+            for adj_node in graph[node]:
+                indegrees[adj_node] -= 1
+                if indegrees[adj_node] == 0:
+                    queue.push(adj_node)
+        if count == len(graph):
+            return False
+        return True
+
+    @staticmethod
+    def _update_indegrees(graph, indegrees):
+        for node in graph:
+            for adj_node in graph[node]:
+                indegrees[adj_node] += 1
+
+
+print(
+    Solution.has_cycle(
+        {
+            0: [1],
+            1: [2],
+            2: [3],
+            3: [3]
+        }
+    )
+)
+
+print(
+    Solution.has_cycle(
+        {
+            0: [1],
+            1: [2],
+            2: []
+        }
+    )
+)
+
+print(
+    Solution.has_cycle(
+        {
+            0: [1, 2],
+            1: [2],
+            2: [0, 3],
+            3: [3]
+        }
+    )
+)
+
+print(
+    Solution.has_cycle(
+        {
+            0: [1, 2],
+            1: [2],
+            2: [3],
+            3: []
+        }
+    )
+)
