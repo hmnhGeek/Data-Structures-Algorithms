020fede485c1d5c9879220a882e43e603a589a21
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-24 13:38:39 +0530

    Maximize sum after K negations

--- Greedy/Maximize array sum after K negations.py
+++ Greedy/Maximize array sum after K negations.py
@@ -33,3 +33,31 @@ class MergeSort:
         MergeSort._sort(arr, mid + 1, high)
         arr[low:high+1] = MergeSort._merge(arr, low, high)
 
+
+class Solution:
+    @staticmethod
+    def get_min(arr):
+        min_index = -1
+        min_elem = 1e6
+        for i in range(len(arr)):
+            if arr[i] < min_elem:
+                min_elem = arr[i]
+                min_index = i
+        return min_index
+
+    @staticmethod
+    def maximize_sum(arr, k):
+        MergeSort.sort(arr)
+        for i in range(len(arr)):
+            if arr[i] < 0:
+                arr[i] = -arr[i]
+                k -= 1
+        min_index = Solution.get_min(arr)
+        while k != 0:
+            arr[min_index] = -arr[min_index]
+            k -= 1
+        return sum(arr)
+
+
+print(Solution.maximize_sum([1, 2, -3, 4, 5], 1))
+print(Solution.maximize_sum([5, -2, 5, -4, 5, -12, 5, 5, 5, 20], 5))
