9b327b09d8eedbbdf3713010be830aa6382a4a5e
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-24 10:54:19 +0530

    Kth missing number using Binary Search

--- Practice Set 2/Binary Search/BS16 - Kth Missing Positive Number.py
+++ Practice Set 2/Binary Search/BS16 - Kth Missing Positive Number.py
@@ -1,18 +1,37 @@
+# Problem link - https://www.geeksforgeeks.org/k-th-missing-element-in-sorted-array/
+# Solution - https://www.youtube.com/watch?v=uZ0N_hZpyps&list=PLgUwDviBIf0pMFMWuuvDNMAkoQFi-h0ZF&index=17
+
+
 class Solution:
     @staticmethod
     def get_kth_missing(arr, k):
+        """
+            Time complexity is O(log(n)) and space complexity is O(1).
+        """
+
+        # edge case
         if k <= 0:
             return -1
+
+        # define a search space.
         low, high = 0, len(arr) - 1
         while low <= high:
             mid = int(low + (high - low)/2)
+
+            # number of missing integers
             num_missing = arr[mid] - mid - 1
+
+            # if missing < k, we must move to right to accommodate more missing numbers.
             if num_missing < k:
                 low = mid + 1
+
+            # else we must move left
             elif num_missing == k:
                 high = mid - 1
             else:
                 high = mid - 1
+
+        # return k + low, which points to the kth missing number.
         return k + low
 
 
