eeb775c08205feb0ce89af6d2f581ed708d53398
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-24 09:01:02 +0530

    Memoized subset sum

--- Practice Set 3/Dynamic Programming/DP16 - Partition A Set Into Two Subsets With Minimum Absolute Sum Difference.py
+++ Practice Set 3/Dynamic Programming/DP16 - Partition A Set Into Two Subsets With Minimum Absolute Sum Difference.py
@@ -27,4 +27,39 @@ def recursive():
     print(subset_sum([3, 34, 4, 12, 5, 2], 30))
 
 
+def memoized():
+    """
+        Time complexity is O(n * target) and space complexity is O(n + n * target).
+    """
+
+    def solve(arr, i, j, dp):
+        if j == 0:
+            return True
+        if i == 0:
+            return arr[0] == j
+        if dp[i][j] is not None:
+            return dp[i][j]
+        left = False
+        if arr[i] <= j:
+            left = solve(arr, i - 1, j - arr[i], dp)
+        right = solve(arr, i - 1, j, dp)
+        dp[i][j] = left or right
+        return dp[i][j]
+
+    def subset_sum(arr, target):
+        n = len(arr)
+        dp = {i: {j: None for j in range(target + 1)} for i in range(n)}
+        return solve(arr, n - 1, target, dp)
+
+    print(subset_sum([1, 2, 3, 4], 4))
+    print(subset_sum([4, 3, 2, 1], 5))
+    print(subset_sum([2, 5, 1, 6, 7], 4))
+    print(subset_sum([6, 1, 2, 1], 4))
+    print(subset_sum([1, 7, 2, 9, 10], 6))
+    print(subset_sum([3, 34, 4, 12, 5, 2], 9))
+    print(subset_sum([3, 34, 4, 12, 5, 2], 30))
+
+
 recursive()
+print()
+memoized()
