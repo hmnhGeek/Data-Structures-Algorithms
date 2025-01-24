338ab2620daa8667a2c1ac1a2a1c576b97870d55
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-24 11:10:49 +0530

    Used quick sort to sort the array

--- Practice Set 2/Binary Search/BS17 - Aggressive Cows.py
+++ Practice Set 2/Binary Search/BS17 - Aggressive Cows.py
@@ -1,9 +1,37 @@
+class QuickSort:
+    @staticmethod
+    def _get_partition_index(arr, low, high):
+        pivot = arr[low]
+        i, j = low, high
+        while i < j:
+            while arr[i] <= pivot and i <= high - 1:
+                i += 1
+            while arr[j] > pivot and j >= low + 1:
+                j -= 1
+            if i < j:
+                arr[i], arr[j] = arr[j], arr[i]
+            arr[low], arr[j] = arr[j], arr[low]
+        return j
+
+    @staticmethod
+    def _sort(arr, low, high):
+        if low >= high:
+            return
+        partition_index = QuickSort._get_partition_index(arr, low, high)
+        QuickSort._sort(arr, low, partition_index - 1)
+        QuickSort._sort(arr, partition_index + 1, high)
+
+    @staticmethod
+    def sort(arr):
+        QuickSort._sort(arr, 0, len(arr) - 1)
+
+
 class Solution:
     @staticmethod
     def aggressive_cows(arr, k):
         if k <= 0 or k > len(arr):
             return -1
-        arr.sort()
+        QuickSort.sort(arr)
         low, high = 0, max(arr)
         while low <= high:
             mid = int(low + (high - low)/2)
