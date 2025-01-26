8b3feebaba51f90f035f9c229797cc89ead05326
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-26 16:49:29 +0530

    Added reverse method in linked list

--- Practice Set 1/Linked Lists/Delete nodes having greater value on right.py
+++ Practice Set 1/Linked Lists/Delete nodes having greater value on right.py
@@ -21,6 +21,10 @@ class LinkedList:
             self.tail = node
         self.length += 1
 
+    def build(self, *args):
+        for i in args:
+            self.push(i)
+
     def show(self):
         curr = self.head
         while curr is not None:
@@ -37,3 +41,46 @@ class LinkedList:
             curr = next_curr
         self.head, self.tail = self.tail, self.head
 
+
+class Solution:
+    @staticmethod
+    def delete(l: LinkedList):
+        l.reverse()
+        prev, curr = l.head, l.head.next
+        while curr is not None:
+            if curr.data < prev.data:
+                prev.next = curr.next
+                temp = curr
+                if temp == l.tail:
+                    l.tail = prev
+                del temp
+                l.length -= 1
+            else:
+                prev = curr
+            curr = curr.next
+        l.reverse()
+
+
+l1 = LinkedList()
+l1.build(10, 20, 30, 40, 50, 60)
+l1.show()
+Solution.delete(l1)
+l1.show()
+
+l2 = LinkedList()
+l2.build(12, 15, 10, 11, 5, 6, 2, 3)
+l2.show()
+Solution.delete(l2)
+l2.show()
+
+l3 = LinkedList()
+l3.build(5, 2, 13, 3, 8)
+l3.show()
+Solution.delete(l3)
+l3.show()
+
+l4 = LinkedList()
+l4.build(1, 1, 1, 1)
+l4.show()
+Solution.delete(l4)
+l4.show()
