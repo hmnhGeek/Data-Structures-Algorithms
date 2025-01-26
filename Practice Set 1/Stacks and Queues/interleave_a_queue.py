76a04c4519aedc4473a960f79d085d0381c3d074
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-26 12:33:35 +0530

    Interleave in a queue

--- Practice Set 1/Stacks and Queues/interleave_a_queue.py
+++ Practice Set 1/Stacks and Queues/interleave_a_queue.py
@@ -31,3 +31,57 @@ class Queue:
         self.length -= 1
         return item
 
+    def show(self):
+        curr = self.head
+        while curr is not None:
+            print(curr.data, end=" ")
+            curr = curr.next
+        print()
+
+
+class Solution:
+    @staticmethod
+    def _get_mid_node(queue: Queue) -> Node:
+        counter = 0
+        curr = queue.head
+        while counter != queue.length // 2:
+            curr = curr.next
+            counter += 1
+        return curr
+
+    @staticmethod
+    def interleave(queue: Queue):
+        if queue.length % 2 == 1:
+            return queue
+
+        temp_queue = Queue()
+        i, j = queue.head, Solution._get_mid_node(queue)
+        temp = j
+        while i != temp.next:
+            while not temp_queue.is_empty():
+                queue.push(temp_queue.pop())
+            temp_queue.push(i.data)
+            temp_queue.push(j.data)
+            i = i.next
+            j = j.next
+            queue.pop()
+        while i.next != j:
+            queue.pop()
+            i = i.next
+
+
+# Example 1
+q = Queue()
+for i in [1, 2, 3, 4]:
+    q.push(i)
+q.show()
+Solution.interleave(q)
+q.show()
+
+# Example 2
+q = Queue()
+for i in [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]:
+    q.push(i)
+q.show()
+Solution.interleave(q)
+q.show()
