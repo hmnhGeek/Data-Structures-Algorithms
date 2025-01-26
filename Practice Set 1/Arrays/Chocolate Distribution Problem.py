294d56a959c58375dcf1501886eb2b85799bae57
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-26 18:01:03 +0530

    Chocolate distribution problem

--- Practice Set 1/Arrays/Chocolate Distribution Problem.py
+++ Practice Set 1/Arrays/Chocolate Distribution Problem.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/chocolate-distribution-problem3825/1
+# Solution - https://www.youtube.com/watch?v=oYNU1TD9W5Y
+
+
 class MergeSort:
     @staticmethod
     def sort(arr):
@@ -5,8 +9,8 @@ class MergeSort:
 
     @staticmethod
     def _merge(arr, low, high):
-        mid = int(low + (high - low)/2)
-        left, right = arr[low:mid+1], arr[mid+1:high+1]
+        mid = int(low + (high - low) / 2)
+        left, right = arr[low:mid + 1], arr[mid + 1:high + 1]
         i, j = 0, 0
         merged = []
         while i < len(left) and j < len(right):
@@ -28,25 +32,38 @@ class MergeSort:
     def _sort(arr, low, high):
         if low >= high:
             return
-        mid = int(low + (high - low)/2)
+        mid = int(low + (high - low) / 2)
         MergeSort._sort(arr, low, mid)
         MergeSort._sort(arr, mid + 1, high)
-        arr[low:high+1] = MergeSort._merge(arr, low, high)
+        arr[low:high + 1] = MergeSort._merge(arr, low, high)
 
 
 class Solution:
     @staticmethod
     def chocolate_distribution(arr, k):
+        """
+            Time complexity is O(n * log(n)) and space complexity is O(1).
+        """
+
+        # sort the array in O(n * log(n)) time.
         MergeSort.sort(arr)
+
         diff = 1e6
         n = len(arr)
+
+        # loop on the windows
         for i in range(n - k + 1):
             up = arr[i + k - 1]
             low = arr[i]
+            # update the difference
             diff = min(diff, up - low)
+
+        # return the minimum difference
         return diff
 
 
 print(Solution.chocolate_distribution([3, 4, 1, 9, 56, 7, 9, 12], 5))
 print(Solution.chocolate_distribution([7, 3, 2, 4, 9, 12, 56], 3))
 print(Solution.chocolate_distribution([3, 4, 1, 9, 56], 5))
+print(Solution.chocolate_distribution([7, 3, 2, 4, 9, 12, 56], 5))
+print(Solution.chocolate_distribution([6, 8, 11, 21, 90, 49], 4))
