829887ebaf3d8874bc50af451f1b70b382748c2b
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-28 18:50:00 +0530

    Course schedule problem using topological sort

--- Practice Set 3/Graphs/G24 - Course Schedule.py
+++ Practice Set 3/Graphs/G24 - Course Schedule.py
@@ -31,3 +31,64 @@ class Queue:
         self.length -= 1
         return item
 
+
+class Solution:
+    @staticmethod
+    def _get_graph(courses):
+        graph = {}
+        for course in courses:
+            graph[course[0]] = []
+            graph[course[1]] = []
+        indegrees = {i: 0 for i in graph}
+        for course in courses:
+            nxt, prev = course
+            graph[prev].append(nxt)
+            indegrees[nxt] += 1
+        return graph, indegrees
+
+    @staticmethod
+    def _get_topological_sort(graph, indegrees):
+        queue = Queue()
+        topo = []
+        for node in indegrees:
+            if indegrees[node] == 0:
+                queue.push(node)
+        while not queue.is_empty():
+            node = queue.pop()
+            topo.append(node)
+            for adj_node in graph[node]:
+                indegrees[adj_node] -= 1
+                if indegrees[adj_node] == 0:
+                    queue.push(adj_node)
+        return topo
+
+    @staticmethod
+    def course_schedule(courses):
+        graph, indegrees = Solution._get_graph(courses)
+        topo_sort = Solution._get_topological_sort(graph, indegrees)
+        if len(topo_sort) == len(graph):
+            return topo_sort
+        return []
+
+
+print(
+    Solution.course_schedule(
+        [
+            [1, 0]
+        ]
+    )
+)
+
+print(
+    Solution.course_schedule(
+        [
+            [1, 0],
+            [2, 0],
+            [3, 1],
+            [3, 2]
+        ]
+    )
+)
+
+print(Solution.course_schedule([[0, 1], [1, 0]]))
+print(Solution.course_schedule([[1, 2], [2, 3], [2, 4], [3, 4], [4, 3]]))
