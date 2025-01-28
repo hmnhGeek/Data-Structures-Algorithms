72c26f1dbaa9089d8229d44051d70db977720057
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-28 20:37:10 +0530

    Implemented quick sort class

---
+++ Greedy/Max Value Permutation.py
@@ -0,0 +1,27 @@
+class QuickSort:
+    @staticmethod
+    def sort(arr):
+        QuickSort._sort(arr, 0, len(arr) - 1)
+
+    @staticmethod
+    def _sort(arr, low, high):
+        if low >= high:
+            return
+        partition_index = QuickSort._get_partition_index(arr, low, high)
+        QuickSort._sort(arr, low, partition_index - 1)
+        QuickSort._sort(arr, partition_index + 1, high)
+
+    @staticmethod
+    def _get_partition_index(arr, low, high):
+        i, j = low, high
+        pivot = arr[low]
+        while i < j:
+            while arr[i] <= pivot and i <= high - 1:
+                i += 1
+            while arr[j] > pivot and j >= low + 1:
+                j -= 1
+            if i < j:
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[low], arr[j] = arr[j], arr[low]
+        return j
+
