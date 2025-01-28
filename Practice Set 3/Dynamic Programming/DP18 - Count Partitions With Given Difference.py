c8c6516b614a71badadb3db08d8ee265d00b9cd0
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-28 18:15:23 +0530

    Count partitions with given difference

--- Practice Set 3/Dynamic Programming/DP18 - Count Partitions With Given Difference.py
+++ Practice Set 3/Dynamic Programming/DP18 - Count Partitions With Given Difference.py
@@ -122,10 +122,43 @@ def space_optimized():
     print(subset_sum([3, 34, 4, 12, 5, 2], 30))
 
 
-recursive()
-print()
-memoized()
-print()
-tabulation()
-print()
-space_optimized()
+class Solution:
+    @staticmethod
+    def _subset_sum(arr, target):
+        n = len(arr)
+        prev = {j: 0 for j in range(target + 1)}
+
+        prev[0] = 1
+        if arr[0] in prev:
+            prev[arr[0]] = 1
+
+        for i in range(1, n):
+            curr = {j: False for j in range(target + 1)}
+            curr[0] = 1
+            for j in range(target + 1):
+                left = False
+                if arr[i] <= j:
+                    left = prev[j - arr[i]]
+                right = prev[j]
+                curr[j] = left + right
+            prev = curr
+        return prev[target]
+
+    @staticmethod
+    def count_subsets(arr, d):
+        s = sum(arr)
+        count = 0
+        for s1 in range(1, s + 1):
+            s2 = s - s1
+            if s1 >= s2 and s1 - s2 == d:
+                count += Solution._subset_sum(arr, s1)
+        return count
+
+
+print(Solution.count_subsets([5, 2, 6, 4], 3))
+print(Solution.count_subsets([1, 1, 1, 1], 0))
+print(Solution.count_subsets([4, 6, 3], 1))
+print(Solution.count_subsets([3, 1, 1, 2, 1], 0))
+print(Solution.count_subsets([3, 2, 2, 5, 1], 1))
+print(Solution.count_subsets([1, 2, 1, 0, 1, 3, 3], 11))
+print(Solution.count_subsets([1, 2, 3, 1, 2], 1))
