ddb89e2d603c2ad88eca9fe8563e4063978ba63c
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-01 12:36:28 +0530

    Max points from a deck of cards

--- Practice Set 6/Sliding Windows/L2 - Maximum Points You Can Obtain from Cards.py
+++ Practice Set 6/Sliding Windows/L2 - Maximum Points You Can Obtain from Cards.py
@@ -1,19 +1,37 @@
 class Solution:
     @staticmethod
     def get_max_points(cards, k):
+        """
+            Time complexity is O(k) and space complexity is O(1).
+        """
+
+        # if the k value is out of scope, return -1
         if k <= 0 or k > len(cards):
             return -1
+
+        # if all the cards are required, return the sum simply.
         if k == len(cards):
             return sum(cards)
+
+        # else initialize i and j
         i = k - 1
         j = len(cards)
+
+        # initialize sums
         _sum = pts = sum(cards[:k])
+
+        # while cards from front can be picked.
         while i >= 0:
+            # remove the ith card
             _sum = _sum - cards[i]
             i -= 1
             j -= 1
+            # add the jth card
             _sum += cards[j]
+            # update the max points obtained.
             pts = max(pts, _sum)
+
+        # return the max points obtained.
         return pts
 
 
