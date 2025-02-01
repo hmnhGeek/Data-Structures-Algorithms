84ead7ef12fae7668cee6a651932b479e36719be
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-01 18:11:10 +0530

    Median of 2 sorted arrays using 2 pointers

---
+++ Practice Set 2/Binary Search/BS21 - Median of two sorted arrays (Two Pointer Approach).py
@@ -0,0 +1,82 @@
+class Solution:
+    @staticmethod
+    def _even_sized_median(arr1, arr2, n1, n2, m):
+        elem1, elem2 = None, None
+        i, j = 0, 0
+        counter = 0
+        last_used = None
+        while i < len(arr1) and j < len(arr2):
+            if arr1[i] <= arr2[j]:
+                last_used = arr1[i]
+                i += 1
+            else:
+                last_used = arr2[j]
+                j += 1
+            counter += 1
+            if counter == m:
+                elem1 = last_used
+            elif counter == m + 1:
+                elem2 = last_used
+        while i < len(arr1):
+            last_used = arr1[i]
+            i += 1
+            counter += 1
+            if counter == m:
+                elem1 = last_used
+            elif counter == m + 1:
+                elem2 = last_used
+        while j < len(arr2):
+            last_used = arr2[j]
+            j += 1
+            counter += 1
+            if counter == m:
+                elem1 = last_used
+            elif counter == m + 1:
+                elem2 = last_used
+        return (elem1 + elem2)/2
+
+    @staticmethod
+    def _odd_sized_median(arr1, arr2, n1, n2, m):
+        elem = None
+        i, j = 0, 0
+        counter = 0
+        last_used = None
+        while i < len(arr1) and j < len(arr2):
+            if arr1[i] <= arr2[j]:
+                last_used = arr1[i]
+                i += 1
+            else:
+                last_used = arr2[j]
+                j += 1
+            counter += 1
+            if counter == m:
+                elem = last_used
+
+        while i < len(arr1):
+            last_used = arr1[i]
+            i += 1
+            counter += 1
+            if counter == m:
+                elem = last_used
+
+        while j < len(arr2):
+            last_used = arr2[j]
+            j += 1
+            counter += 1
+            if counter == m:
+                elem = last_used
+
+        return elem
+
+    @staticmethod
+    def get_median(arr1, arr2):
+        n1, n2 = len(arr1), len(arr2)
+        n = n1 + n2
+        if n % 2 == 0:
+            return Solution._even_sized_median(arr1, arr2, n1, n2, n//2)
+        return Solution._odd_sized_median(arr1, arr2, n1, n2, (n + 1)//2)
+
+
+print(Solution.get_median([15, 16, 17], [10]))
+print(Solution.get_median([2, 3, 4], [1, 3]))
+print(Solution.get_median([1, 3, 4, 7, 10, 12], [2, 3, 6, 15]))
