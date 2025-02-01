db7a5353060d79d62d31b5fbe58dc9c61ee8df6a
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-01 17:18:29 +0530

    Defined a Max Heap for slots

--- Practice Set 2/Binary Search/BS20 - Minimise Maximum Distance between Gas Stations.py
+++ Practice Set 2/Binary Search/BS20 - Minimise Maximum Distance between Gas Stations.py
@@ -57,3 +57,77 @@ class LinearSolution:
 print(LinearSolution.minimize_the_max_distance([1,2,3,4,5,6,7], 6))
 print(LinearSolution.minimize_the_max_distance([1, 2, 3, 4, 5], 4))
 print(LinearSolution.minimize_the_max_distance(list(range(1, 11)), 1))
+
+
+class Slot:
+    def __init__(self, length, index):
+        self.length = length
+        self.index = index
+
+
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def is_empty(self):
+        return len(self.heap) == 0
+
+    def get_lci(self, pi):
+        lci = 2*pi + 1
+        return lci if lci in range(len(self.heap)) else None
+
+    def get_rci(self, pi):
+        rci = 2*pi + 2
+        return rci if rci in range(len(self.heap)) else None
+
+    def get_pi(self, ci):
+        if ci == 0:
+            return
+        pi = int((ci - 1)/2)
+        return pi if pi in range(len(self.heap)) else None
+
+    def get_max_child_index(self, lci, rci):
+        if lci is None and rci is None:
+            return
+        if lci is None:
+            return rci
+        if rci is None:
+            return lci
+        max_child_index = lci
+        if self.heap[rci].length > self.heap[max_child_index].length:
+            max_child_index = rci
+        return max_child_index
+
+    def max_heapify_up(self, start_index):
+        if start_index == 0:
+            return
+        pi = self.get_pi(start_index)
+        lci, rci = self.get_lci(pi), self.get_rci(pi)
+        max_child_index = self.get_max_child_index(lci, rci)
+        if max_child_index is not None:
+            if self.heap[pi].length < self.heap[max_child_index].length:
+                self.heap[pi], self.heap[max_child_index] = self.heap[max_child_index], self.heap[pi]
+            self.max_heapify_up(pi)
+
+    def max_heapify_down(self, pi):
+        lci, rci = self.get_lci(pi), self.get_rci(pi)
+        max_child_index = self.get_max_child_index(lci, rci)
+        if max_child_index is not None:
+            if self.heap[pi].length < self.heap[max_child_index].length:
+                self.heap[pi], self.heap[max_child_index] = self.heap[max_child_index], self.heap[pi]
+            self.max_heapify_down(max_child_index)
+
+    def insert(self, x: Slot):
+        self.heap.append(x)
+        self.max_heapify_up(len(self.heap) - 1)
+
+    def pop(self):
+        if self.is_empty():
+            return
+        item = self.heap[0]
+        self.heap[0], self.heap[-1] = self.heap[-1], self.heap[0]
+        del self.heap[-1]
+        self.max_heapify_down(0)
+        return item
+
+
