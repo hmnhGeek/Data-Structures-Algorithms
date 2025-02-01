7d063739a3952be1bf3c3acf4eed7cfc8938698c
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-01 17:30:20 +0530

    Used max heap to solve the gas stations placement problem

--- Practice Set 2/Binary Search/BS20 - Minimise Maximum Distance between Gas Stations.py
+++ Practice Set 2/Binary Search/BS20 - Minimise Maximum Distance between Gas Stations.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/minimize-max-distance-to-gas-station/1
+# Solution - https://www.youtube.com/watch?v=kMSBvlZ-_HA&list=PLgUwDviBIf0pMFMWuuvDNMAkoQFi-h0ZF&index=21
+
+
 class LinearSolution:
     @staticmethod
     def minimize_the_max_distance(arr, k):
