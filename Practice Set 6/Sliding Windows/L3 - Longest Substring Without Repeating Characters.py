4ccbc6e9833bf0bde449c9485c17629f12355340
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-02 18:11:36 +0530

    Longest Substring Without Repeating Characters

--- Practice Set 6/Sliding Windows/L3 - Longest Substring Without Repeating Characters.py
+++ Practice Set 6/Sliding Windows/L3 - Longest Substring Without Repeating Characters.py
@@ -1,20 +1,36 @@
 class Solution:
     @staticmethod
     def get_longest(string):
+        # define window variables
         n = len(string)
         left = right = 0
+
+        # define tracking dictionary
         d = {i: 0 for i in string}
+
+        # define result variables
         longest_length = 0
         start_index = -1
+
+        # while there is ground to cover.
         while right < n:
+            # increment right indexed value count.
             d[string[right]] += 1
+
+            # if any character has freq > 1, there's duplicate; shrink from left.
             if any(v > 1 for v in d.values()):
                 d[string[left]] -= 1
                 left += 1
+
+            # if all the characters in the window are unique, then update the result variables.
             if all(v <= 1 for v in d.values()):
                 longest_length = max(longest_length, right - left + 1)
                 start_index = left
+
+            # increment right index.
             right += 1
+
+        # return the substring.
         return string[start_index:start_index+longest_length] if start_index != -1 else ""
 
 
