73a71f5d03b06c33536a4dda3d3623731eaa2f8e
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-02 19:00:56 +0530

    Word Ladder - I using BFS

--- Practice Set 3/Graphs/G29 - Word Ladder - 1.py
+++ Practice Set 3/Graphs/G29 - Word Ladder - 1.py
@@ -32,3 +32,32 @@ class Queue:
         return item
 
 
+class Solution:
+    @staticmethod
+    def word_ladder(word_list, start_word, end_word):
+        h = set(word_list)
+        queue = Queue()
+        queue.push((start_word, 1))
+        result = 0
+        if start_word in h:
+            h.remove(start_word)
+        while not queue.is_empty():
+            word, level = queue.pop()
+            if word == end_word:
+                return level
+            for i in range(len(word)):
+                word_split = [j for j in word]
+                for char in "abcdefghijklmnopqrstuvwxyz":
+                    word_split[i] = char
+                    new_word = "".join(word_split)
+                    if new_word in h:
+                        queue.push((new_word, level + 1))
+                        h.remove(new_word)
+        return result
+
+
+print(Solution.word_ladder(["des","der","dfr","dgt","dfs"], "der", "dfs"))
+print(Solution.word_ladder(["geek", "gefk"], "gedk", "geek"))
+print(Solution.word_ladder(["poon", "plee", "same", "poie","plea","plie","poin"], "toon", "plea"))
+print(Solution.word_ladder(["hot","dot","dog","lot","log","cog"], "hit", "cog"))
+print(Solution.word_ladder(["hot","dot","dog","lot","log"], "hit", "cog"))
