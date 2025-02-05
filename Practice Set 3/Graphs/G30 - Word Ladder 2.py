c906f052963150cd4ec737668b3cc13bf1e097da
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-05 11:01:25 +0530

    Word Ladder - 2

--- Practice Set 3/Graphs/G30 - Word Ladder 2.py
+++ Practice Set 3/Graphs/G30 - Word Ladder 2.py
@@ -31,3 +31,56 @@ class Queue:
         self.length -= 1
         return item
 
+
+class Solution:
+    @staticmethod
+    def word_ladder(word_list, start_word, end_word):
+        hash_map = set(word_list)
+        queue = Queue()
+        used_on_level = set()
+        level = 0
+        used_on_level.add(start_word)
+        queue.push([start_word, ])
+        result = []
+
+        for word in used_on_level:
+            if word in hash_map:
+                hash_map.remove(word)
+        used_on_level.clear()
+
+        while not queue.is_empty():
+            vector = queue.pop()
+            word = vector[-1]
+
+            if len(vector) > level:
+                level += 1
+                for w in used_on_level:
+                    if w in hash_map:
+                        hash_map.remove(w)
+                used_on_level.clear()
+
+            if word == end_word:
+                if len(result) == 0:
+                    result.append(vector)
+                elif len(result[0]) == len(vector):
+                    result.append(vector)
+
+            for c in range(len(word)):
+                original = word
+                temp = [i for i in original]
+                for alp in "abcdefghijklmnopqrstuvwxyz":
+                    temp[c] = alp
+                    new_word = "".join(temp)
+                    if new_word in hash_map:
+                        next_vector = [j for j in vector] + [new_word, ]
+                        queue.push(next_vector)
+                    used_on_level.add(new_word)
+                    temp = [i for i in original]
+
+        return result
+
+
+print(Solution.word_ladder(["des","der","dfr","dgt","dfs"], "der", "dfs"))
+print(Solution.word_ladder(["geek", "gefk"], "gedk", "geek"))
+print(Solution.word_ladder(["hot","dot","dog","lot","log","cog"], "hit", "cog"))
+print(Solution.word_ladder(["hot","dot","dog","lot","log"], "hit", "cog"))
