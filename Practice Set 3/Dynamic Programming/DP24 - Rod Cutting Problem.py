c991871b459f9df6e9f42b3e1dd5f8caf1e55321
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-05 10:16:51 +0530

    Space optimized rod cutting problem

--- Practice Set 3/Dynamic Programming/DP24 - Rod Cutting Problem.py
+++ Practice Set 3/Dynamic Programming/DP24 - Rod Cutting Problem.py
@@ -84,9 +84,41 @@ def tabulation():
     print(rod_cut([3], 1))
 
 
+def space_optimized():
+    """
+        Time complexity is O(n^2) and space complexity is O(n).
+    """
+    def rod_cut(costs, rod_length):
+        n = len(costs)
+        prev = {j: -1e6 for j in range(rod_length + 1)}
+        prev[0] = 0
+        for j in prev:
+            prev[j] = j * costs[0]
+        for i in range(1, n):
+            curr = {j: -1e6 for j in range(rod_length + 1)}
+            curr[0] = 0
+            for j in range(rod_length + 1):
+                left = -1e6
+                if i + 1 <= j:
+                    left = costs[i] + curr[j - i - 1]
+                right = prev[j]
+                curr[j] = max(left, right)
+            prev = curr
+        return prev[rod_length]
+
+    print(rod_cut([2, 5, 7, 8, 10], 5))
+    print(rod_cut([3, 5, 8, 9, 10, 17, 17, 20], 8))
+    print(rod_cut([3, 5, 6, 7, 10, 12], 6))
+    print(rod_cut([1, 10, 3, 1, 3, 1, 5, 9], 8))
+    print(rod_cut([1, 5, 8, 9, 10, 17, 17, 20], 8))
+    print(rod_cut([3], 1))
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
+print()
+space_optimized()
 print()
