c4ea9aa7eb54eb6d951667021b6d3c3e7f097824
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-05 12:05:20 +0530

    Max consecutive ones III

--- Practice Set 6/Sliding Windows/L4 - Max Consecutive Ones III.py
+++ Practice Set 6/Sliding Windows/L4 - Max Consecutive Ones III.py
@@ -1,24 +1,42 @@
 class Solution:
     @staticmethod
     def max_consecutive_1s(arr, k):
+        # edge case
         if k < 0:
             return []
+
+        # define window variables
         left = right = 0
         n = len(arr)
+
+        # define tracking variable
         zero_count = 0
+
+        # define result variables
         longest_length = 0
         start_index = -1
+
+        # while there is ground to cover...
         while right < n:
+            # increment the zero count if needed.
             if arr[right] == 0:
                 zero_count += 1
+
+            # shrink just one unit from left if needed.
             if zero_count > k:
                 if arr[left] == 0:
                     zero_count -= 1
                 left += 1
+
+            # update the result variables.
             if zero_count <= k:
                 longest_length = max(longest_length, right - left + 1)
                 start_index = left
+
+            # increment the right index.
             right += 1
+
+        # return the sub-array.
         return arr[start_index:start_index+longest_length] if start_index != -1 else []
 
 
