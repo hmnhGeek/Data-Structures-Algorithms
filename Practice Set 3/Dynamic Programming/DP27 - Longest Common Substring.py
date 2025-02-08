2c686b08f37cedc008b1a802d39641640aa3466b
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-08 13:52:56 +0530

    Longest Common Substring - Space optimized

--- Practice Set 3/Dynamic Programming/DP27 - Longest Common Substring.py
+++ Practice Set 3/Dynamic Programming/DP27 - Longest Common Substring.py
@@ -1,6 +1,9 @@
 class Solution:
     @staticmethod
     def longest_common_substring(s1, s2):
+        """
+            Time complexity is O(n1 * n2) and space complexity is O(n1 * n2).
+        """
         n1, n2 = len(s1), len(s2)
         dp = {i: {j: 0 for j in range(n2 + 1)} for i in range(n1 + 1)}
         ans = 0
@@ -13,6 +16,25 @@ class Solution:
                     dp[i][j] = 0
         return ans
 
+    @staticmethod
+    def optimized_lcs(s1, s2):
+        """
+            Time complexity is O(n1 * n2) and space complexity is O(n2).
+        """
+        n1, n2 = len(s1), len(s2)
+        prev = {j: 0 for j in range(n2 + 1)}
+        ans = 0
+        for i in range(1, n1 + 1):
+            curr = {j: 0 for j in range(n2 + 1)}
+            for j in range(1, n2 + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    curr[j] = 1 + prev[j - 1]
+                    ans = max(ans, curr[j])
+                else:
+                    curr[j] = 0
+            prev = curr
+        return ans
+
 
 print(Solution.longest_common_substring("abcd", "abzd"))
 print(Solution.longest_common_substring("abcjklp", "acjkp"))
@@ -21,3 +43,11 @@ print(Solution.longest_common_substring("tyfg", "cvbnuty"))
 print(Solution.longest_common_substring("GeeksforGeeks", "GeeksQuiz"))
 print(Solution.longest_common_substring("abcdxyz", "xyzabcd"))
 print(Solution.longest_common_substring("abc", ""))
+print()
+print(Solution.optimized_lcs("abcd", "abzd"))
+print(Solution.optimized_lcs("abcjklp", "acjkp"))
+print(Solution.optimized_lcs("wasdijkl", "wsdjkl"))
+print(Solution.optimized_lcs("tyfg", "cvbnuty"))
+print(Solution.optimized_lcs("GeeksforGeeks", "GeeksQuiz"))
+print(Solution.optimized_lcs("abcdxyz", "xyzabcd"))
+print(Solution.optimized_lcs("abc", ""))
