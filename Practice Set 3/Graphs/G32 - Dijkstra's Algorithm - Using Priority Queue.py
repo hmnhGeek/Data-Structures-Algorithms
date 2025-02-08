af6e42d6e6da9125a19094089e91304634f6210d
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-08 14:08:17 +0530

    Dijkstra's Algorithm using PQ

--- Practice Set 3/Graphs/G32 - Dijkstra's Algorithm - Using Priority Queue.py
+++ Practice Set 3/Graphs/G32 - Dijkstra's Algorithm - Using Priority Queue.py
@@ -61,3 +61,58 @@ class MinHeap:
         self.min_heapify_down(0)
         return item
 
+
+class Solution:
+    @staticmethod
+    def get_shortest_path(graph, source):
+        if source not in graph:
+            return
+        pq = MinHeap()
+        distances = {i: 1e6 for i in graph}
+        distances[source] = 0
+        pq.insert((distances[source], source))
+        while not pq.is_empty():
+            distance, node = pq.pop()
+            for adj in graph[node]:
+                adj_node, wt = adj
+                if distances[adj_node] > distance + wt:
+                    distances[adj_node] = distance + wt
+                    pq.insert((distances[adj_node], adj_node))
+        return list(distances.values())
+
+
+print(
+    Solution.get_shortest_path(
+        {
+            0: [[1, 4], [2, 4]],
+            1: [[0, 4], [2, 2]],
+            2: [[0, 4], [1, 2], [3, 3], [5, 6], [4, 1]],
+            3: [[2, 3], [5, 2]],
+            4: [[2, 1], [5, 3]],
+            5: [[3, 2], [2, 6], [4, 3]]
+        },
+        0
+    )
+)
+
+
+print(
+    Solution.get_shortest_path(
+        {
+            0: [[1, 9]],
+            1: [[0, 9]]
+        },
+        0
+    )
+)
+
+print(
+    Solution.get_shortest_path(
+        {
+            0: [[1, 1], [2, 6]],
+            1: [[0, 1], [2, 3]],
+            2: [[0, 6], [1, 3]]
+        },
+        2
+    )
+)
