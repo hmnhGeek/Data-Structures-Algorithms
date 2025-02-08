485f08ff051c207d26a5fc4b25ccf8de1b3aac57
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-08 13:28:52 +0530

    Printing LCS

--- Practice Set 3/Dynamic Programming/DP26 - Print Longest Common Subsequence.py
+++ Practice Set 3/Dynamic Programming/DP26 - Print Longest Common Subsequence.py
@@ -76,8 +76,39 @@ def tabulation():
     print(lcs("ABC", "CBA"))
 
 
-recursive()
-print()
-memoized()
-print()
-tabulation()
+class Solution:
+    @staticmethod
+    def lcs(s1, s2):
+        """
+            Time complexity is O(n1 * n2 + n1 + n2) and space complexity is O(n1 * n2).
+        """
+
+        n1, n2 = len(s1), len(s2)
+        dp = {i: {j: 0 for j in range(n2 + 1)} for i in range(n1 + 1)}
+        for i in range(1, n1 + 1):
+            for j in range(1, n2 + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        i, j = n1, n2
+        result = ""
+        while i > 0 and j > 0:
+            if s1[i - 1] == s2[j - 1]:
+                result += s1[i - 1]
+                i -= 1
+                j -= 1
+            elif dp[i - 1][j] > dp[i][j - 1]:
+                i -= 1
+            else:
+                j -= 1
+        return result[-1:-len(result)-1:-1]
+
+
+print(Solution.lcs("adebc", "dcadb"))
+print(Solution.lcs("ab", "defg"))
+print(Solution.lcs("abcde", "ace"))
+print(Solution.lcs("abc", "abc"))
+print(Solution.lcs("abc", "acd"))
+print(Solution.lcs("AGGTAB", "GXTXAYB"))
+print(Solution.lcs("ABC", "CBA"))
