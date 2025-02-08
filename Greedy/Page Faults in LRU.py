33b69cf91de3237829c684574689d2c995cd6050
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-08 17:23:55 +0530

    Finding page faults in OS using simple array as queue

--- Greedy/Page Faults in LRU.py
+++ Greedy/Page Faults in LRU.py
@@ -31,3 +31,29 @@ class Queue:
         self.length -= 1
         return item
 
+
+class Solution:
+    @staticmethod
+    def get_page_faults(arr, cap):
+        queue = []
+        page_faults = 0
+        n = len(arr)
+        for i in range(n):
+            if len(queue) < cap:
+                if arr[i] not in queue:
+                    queue.append(arr[i])
+                    page_faults += 1
+                else:
+                    idx = queue.index(arr[i])
+                    queue[idx], queue[-1] = queue[-1], queue[idx]
+            elif arr[i] not in queue:
+                queue.pop(0)
+                queue.append(arr[i])
+                page_faults += 1
+            else:
+                idx = queue.index(arr[i])
+                queue[idx], queue[-1] = queue[-1], queue[idx]
+        return page_faults
+
+
+print(Solution.get_page_faults([5, 0, 1, 3, 2, 4, 1, 0, 5], 4))
