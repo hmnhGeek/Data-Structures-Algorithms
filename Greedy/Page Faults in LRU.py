8cc096b48900d5fd9406589a000bf7b34ffb8407
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-08 17:32:41 +0530

    Replace array with a linked list

--- Greedy/Page Faults in LRU.py
+++ Greedy/Page Faults in LRU.py
@@ -4,7 +4,7 @@ class Node:
         self.next = None
 
 
-class Queue:
+class LinkedList:
     def __init__(self):
         self.head = self.tail = None
         self.length = 0
@@ -21,38 +21,54 @@ class Queue:
             self.tail = node
         self.length += 1
 
-    def pop(self):
+    def get_node(self, x):
+        curr = self.head
+        while curr is not None:
+            if curr.data == x:
+                return curr
+            curr = curr.next
+        return None
+
+    def swap(self, n1, n2):
+        temp = n1.data
+        n1.data = n2.data
+        n2.data = temp
+
+    def pop_front(self):
         if self.is_empty():
             return
         item = self.head.data
-        node = self.head
         self.head = self.head.next
-        del node
         self.length -= 1
         return item
+
+    def __len__(self):
+        return self.length
 
 
 class Solution:
     @staticmethod
     def get_page_faults(arr, cap):
-        queue = []
+        queue = LinkedList()
         page_faults = 0
         n = len(arr)
         for i in range(n):
+            node = queue.get_node(arr[i])
             if len(queue) < cap:
-                if arr[i] not in queue:
-                    queue.append(arr[i])
+                if node is None:
+                    queue.push(arr[i])
                     page_faults += 1
                 else:
-                    idx = queue.index(arr[i])
-                    queue[idx], queue[-1] = queue[-1], queue[idx]
-            elif arr[i] not in queue:
-                queue.pop(0)
-                queue.append(arr[i])
+                    tail = queue.tail
+                    queue.swap(node, tail)
+            elif node is None:
+                queue.pop_front()
+                queue.push(arr[i])
                 page_faults += 1
             else:
-                idx = queue.index(arr[i])
-                queue[idx], queue[-1] = queue[-1], queue[idx]
+                node = queue.get_node(arr[i])
+                tail = queue.tail
+                queue.swap(node, tail)
         return page_faults
 
 
