26bb12e0e9e171186d5514929b0f87c1cc7cb5cf
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-13 10:18:27 +0530

    Min insertions to make a string palindrome

--- Practice Set 3/Dynamic Programming/DP29 - Minimum Insertions to Make String Palindrome.py
+++ Practice Set 3/Dynamic Programming/DP29 - Minimum Insertions to Make String Palindrome.py
@@ -100,10 +100,32 @@ def space_optimized():
     print(lcs("ABC", "CBA"))
 
 
-recursive()
-print()
-memoized()
-print()
-tabulation()
-print()
-space_optimized()
+class Solution:
+    @staticmethod
+    def _lcs(s1, s2):
+        n1, n2 = len(s1), len(s2)
+        prev = {j: 0 for j in range(n2 + 1)}
+        for i in range(1, n1 + 1):
+            curr = {j: 0 for j in range(n2 + 1)}
+            for j in range(1, n2 + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    curr[j] = 1 + prev[j - 1]
+                else:
+                    curr[j] = max(prev[j], curr[j - 1])
+            prev = curr
+        return prev[n2]
+
+    @staticmethod
+    def min_insertions(s):
+        n = len(s)
+        lps_length = Solution._lcs(s, s[-1:-len(s)-1:-1])
+        return n - lps_length
+
+
+print(Solution.min_insertions("abca"))
+print(Solution.min_insertions("abcdefg"))
+print(Solution.min_insertions("aaaaa"))
+print(Solution.min_insertions("zzazz"))
+print(Solution.min_insertions("mbadm"))
+print(Solution.min_insertions("leetcode"))
+
