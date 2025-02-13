b552959a399bbf5f1e18dedff8bf22a30e211f1d
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-13 12:36:46 +0530

    Code for upper bound

--- Practice Set 3/Binary Search/BS2 - Lower and Upper Bounds.py
+++ Practice Set 3/Binary Search/BS2 - Lower and Upper Bounds.py
@@ -10,6 +10,17 @@ class Solution:
                 low = mid + 1
         return low if low in range(len(arr)) else -1
 
+    @staticmethod
+    def get_upper_bound(arr, x):
+        low, high = 0, len(arr) - 1
+        while low <= high:
+            mid = int(low + (high - low)/2)
+            if arr[mid] <= x:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return low if low in range(len(arr)) else -1
+
 
 print("Lower Bound")
 print(Solution.get_lower_bound([3, 5, 8, 15, 19], 5))
@@ -19,3 +30,14 @@ print(Solution.get_lower_bound([3, 5, 8, 15, 19], 800))
 print(Solution.get_lower_bound([3, 5, 8, 15, 19], 2))
 print(Solution.get_lower_bound([1, 2, 2, 3, 3, 5], 0))
 print(Solution.get_lower_bound([1, 2, 2, 3, 3, 5], -10))
+
+print()
+
+print("Upper Bound")
+print(Solution.get_upper_bound([3, 5, 8, 15, 19], 5))
+print(Solution.get_upper_bound([3, 5, 8, 15, 19], 8))
+print(Solution.get_upper_bound([3, 5, 8, 15, 19], 9))
+print(Solution.get_upper_bound([3, 5, 8, 15, 19], 800))
+print(Solution.get_upper_bound([3, 5, 8, 15, 19], 2))
+print(Solution.get_upper_bound([1, 2, 2, 3, 3, 5], 0))
+print(Solution.get_upper_bound([1, 2, 2, 3, 3, 5], -10))
