fcdf1433ac62c4ffde4b9b68157de58befcddd78
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-13 20:44:05 +0530

    Path with minimum effort using PQ approach of Dijkstra

--- Practice Set 3/Graphs/G37 - Path With Minimum Effort.py
+++ Practice Set 3/Graphs/G37 - Path With Minimum Effort.py
@@ -75,7 +75,7 @@ print(Solution.get_min_effort(
 
 
 class MinHeap:
-    def __int__(self):
+    def __init__(self):
         self.heap = []
 
     def is_empty(self):
@@ -140,3 +140,59 @@ class MinHeap:
         return item
 
 
+class OptimalSolution:
+    @staticmethod
+    def _get_neighbours(mtx, i, j, n, m):
+        neighbours = []
+        if 0 <= i - 1 < n:
+            neighbours.append((i - 1, j))
+        if 0 <= j + 1 < m:
+            neighbours.append((i, j + 1))
+        if 0 <= i + 1 < n:
+            neighbours.append((i + 1, j))
+        if 0 <= j - 1 < m:
+            neighbours.append((i, j - 1))
+        return neighbours
+
+    @staticmethod
+    def get_min_effort(mtx):
+        n, m = len(mtx), len(mtx[0])
+        pq = MinHeap()
+        distances = [[1e6 for _ in range(m)] for _ in range(n)]
+        distances[0][0] = 0
+        pq.insert((0, 0, 0))
+        while not pq.is_empty():
+            distance, i, j = pq.pop()
+            neighbours = OptimalSolution._get_neighbours(mtx, i, j, n, m)
+            for neighbour in neighbours:
+                x, y = neighbour
+                path_effort = max(distance, abs(mtx[x][y] - mtx[i][j]))
+                if distances[x][y] > path_effort:
+                    distances[x][y] = path_effort
+                    pq.insert((path_effort, x, y))
+        return distances[n - 1][m - 1]
+
+
+print()
+print("Optimal Solution")
+print(
+    OptimalSolution.get_min_effort(
+        [
+            [1, 2, 2],
+            [3, 8, 2],
+            [5, 3, 5]
+        ]
+    )
+)
+
+print(OptimalSolution.get_min_effort([[7, 7], [7, 7]]))
+print(OptimalSolution.get_min_effort([[1, 2, 3], [3, 8, 4], [5, 3, 5]]))
+print(OptimalSolution.get_min_effort([[1, 2, 1, 1, 1], [1, 2, 1, 2, 1], [1, 2, 1, 2, 1], [1, 2, 1, 2, 1], [1, 1, 1, 2, 1]]))
+print(OptimalSolution.get_min_effort([[1, 8, 8], [3, 8, 9], [5, 3, 5]]))
+print(OptimalSolution.get_min_effort(
+    [
+        [1, 3, 1],
+        [9, 9, 3],
+        [9, 9, 1]
+    ]
+))
