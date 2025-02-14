77fbdd5e17f807f74fba8cc0bb6c60dd2c4d2fda
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-15 02:53:01 +0530

    Sort a matrix

--- Practice Set 4/Matrix/Sorted Matrix.py
+++ Practice Set 4/Matrix/Sorted Matrix.py
@@ -25,3 +25,70 @@ class QuickSort:
         arr[low], arr[j] = arr[j], arr[low]
         return j
 
+
+class Solution:
+    @staticmethod
+    def _get_flattened(mtx, n, m):
+        flat = []
+        for i in range(n):
+            for j in range(m):
+                flat.append(mtx[i][j])
+        return flat
+
+    @staticmethod
+    def _rebuild(mtx, flat, n, m):
+        counter = 0
+        for i in range(n):
+            for j in range(m):
+                mtx[i][j] = flat[counter]
+                counter += 1
+
+    @staticmethod
+    def sort_matrix(mtx):
+        n, m = len(mtx), len(mtx[0])
+        flattened = Solution._get_flattened(mtx, n, m)
+        QuickSort.sort(flattened)
+        Solution._rebuild(mtx, flattened, n, m)
+        for i in range(n):
+            print(mtx[i])
+        print()
+
+
+
+print(
+    Solution.sort_matrix(
+        [
+            [10, 20, 30, 40],
+            [15, 25, 35, 45],
+            [27, 29, 37, 48],
+            [32, 33, 39, 50]
+        ]
+    )
+)
+
+print(
+    Solution.sort_matrix(
+        [
+            [1, 5, 3], [2, 8, 7], [4, 6, 9]
+        ]
+    )
+)
+
+print(
+    Solution.sort_matrix(
+        [
+            [5, 4, 7],
+            [1, 3, 8],
+            [2, 9, 6]
+        ]
+    )
+)
+
+print(
+    Solution.sort_matrix(
+        [
+            [5, 4, 7],
+            [1, 3, 8]
+        ]
+    )
+)
