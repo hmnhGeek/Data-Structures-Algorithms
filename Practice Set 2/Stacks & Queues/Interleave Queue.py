e7c2677cbf996fdaf75e4a4d6f0989dbcf381d9e
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-15 04:43:36 +0530

    Created queue class

---
+++ Practice Set 2/Stacks & Queues/Interleave Queue.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Queue:
+    def __init__(self):
+        self.head = self.tail = None
+        self.length = 0
+
+    def is_empty(self):
+        return self.length == 0
+
+    def push(self, x):
+        node = Node(x)
+        if self.is_empty():
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+        self.length += 1
+
+    def pop(self):
+        if self.is_empty():
+            return
+        item = self.head.data
+        node = self.head
+        self.head = self.head.next
+        del node
+        self.length -= 1
+        return item
+
+    def __str__(self):
+        curr = self.head
+        result = ""
+        while curr is not None:
+            result += f"{curr.data} "
+            curr = curr.next
+        return result
+
