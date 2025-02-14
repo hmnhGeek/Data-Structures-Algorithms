8e5c306f104d1751152d62b599064238327ce153
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-15 04:44:25 +0530

    Created stack class

--- Practice Set 2/Stacks & Queues/Interleave Queue.py
+++ Practice Set 2/Stacks & Queues/Interleave Queue.py
@@ -39,3 +39,39 @@ class Queue:
             curr = curr.next
         return result
 
+
+class Stack:
+    def __init__(self):
+        self.head = self.tail = None
+        self.length = 0
+
+    def is_empty(self):
+        return self.length == 0
+
+    def push(self, x):
+        node = Node(x)
+        if self.is_empty():
+            self.head = self.tail = node
+        else:
+            node.next = self.head
+            self.head = node
+        self.length += 1
+
+    def pop(self):
+        if self.is_empty():
+            return
+        item = self.head.data
+        node = self.head
+        self.head = self.head.next
+        del node
+        self.length -= 1
+        return item
+
+    def __str__(self):
+        curr = self.head
+        result = ""
+        while curr is not None:
+            result += f"{curr.data} "
+            curr = curr.next
+        return result
+
