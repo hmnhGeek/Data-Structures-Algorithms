933fd5b940bc83a9cf9ba1d0abb5634d9d3389a4
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-15 04:18:26 +0530

    EKO SPOJ

--- Practice Set 1/Searching & Sorting/EKO SPOJ.py
+++ Practice Set 1/Searching & Sorting/EKO SPOJ.py
@@ -9,17 +9,31 @@ class Solution:
 
     @staticmethod
     def solve(arr, w):
+        """
+            Time complexity is O(n * log(max(arr))) and space complexity O(1).
+        """
+
+        # saw can be on ground to collect max wood or at max ht tree to collect no wood.
         low = 0
         high = max(arr)
         while low <= high:
             mid = int(low + (high - low)/2)
+            # get the wood amount collected at mid-ht saw.
             wood_collected = Solution._collect(arr, mid)
+
+            # if wood collected is the required amount, try increasing the ht of the saw to reduce wood destruction.
             if wood_collected == w:
                 low = mid + 1
+
+            # if collected wood < w, we must cut more wood, thus reduce high.
             elif wood_collected < w:
                 high = mid - 1
+
+            # if > w wood is collected, increase low to reduce wood wastage.
             else:
                 low = mid + 1
+
+        # high would point to the correct answer.
         return high
 
 
