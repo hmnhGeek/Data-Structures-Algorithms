66d1132442c9be6563d2b1f400afa0841efa8669
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-15 05:04:11 +0530

    Interleave in a queue

--- Practice Set 2/Stacks & Queues/Interleave Queue.py
+++ Practice Set 2/Stacks & Queues/Interleave Queue.py
@@ -31,6 +31,13 @@ class Queue:
         self.length -= 1
         return item
 
+    def get_mid(self):
+        slow = fast = self.head
+        while slow and fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
     def __str__(self):
         curr = self.head
         result = ""
@@ -75,3 +82,49 @@ class Stack:
             curr = curr.next
         return result
 
+
+class Solution:
+    @staticmethod
+    def interleave(q: Queue):
+        stack = Stack()
+        mid = q.get_mid()
+        stack.push(mid)
+        curr = q.head
+        while curr != mid:
+            node = stack.pop()
+            stack.push(curr)
+            stack.push(node)
+            if node.next is not None:
+                stack.push(node.next)
+            curr = curr.next
+        while not q.is_empty():
+            q.pop()
+        while not stack.is_empty():
+            q.push(stack.pop().data)
+        while not q.is_empty():
+            stack.push(q.pop())
+        while not stack.is_empty():
+            q.push(stack.pop())
+
+
+q = Queue()
+for i in [1, 2, 3, 4]:
+    q.push(i)
+print(q)
+Solution.interleave(q)
+print(q)
+
+q = Queue()
+q.push(11)
+q.push(12)
+q.push(13)
+q.push(14)
+q.push(15)
+q.push(16)
+q.push(17)
+q.push(18)
+q.push(19)
+q.push(20)
+print(q)
+Solution.interleave(q)
+print(q)
