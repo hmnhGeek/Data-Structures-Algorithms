04daa0e83a173cdb8d8641f37d8f2bd5437198b4
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-14 13:35:05 +0530

    Minimum multiplications to reach end

--- Practice Set 3/Graphs/G39 - Minimum Multiplications to Reach End.py
+++ Practice Set 3/Graphs/G39 - Minimum Multiplications to Reach End.py
@@ -31,3 +31,26 @@ class Queue:
         self.length -= 1
         return item
 
+
+class Solution:
+    @staticmethod
+    def get_min(start, arr, end):
+        queue = Queue()
+        distances = {i: 1e6 for i in range(10**5)}
+        distances[start] = 0
+        queue.push((distances[start], start))
+        while not queue.is_empty():
+            distance, node = queue.pop()
+            if node == end:
+                return distance
+            for item in arr:
+                adj_node = node * item
+                adj_node = adj_node % 10**5
+                if distances[adj_node] > distance + 1:
+                    distances[adj_node] = distance + 1
+                    queue.push((distance + 1, adj_node))
+        return -1
+
+
+print(Solution.get_min(3, [2, 5, 7], 30))
+print(Solution.get_min(7, [3, 4, 65], 66175))
