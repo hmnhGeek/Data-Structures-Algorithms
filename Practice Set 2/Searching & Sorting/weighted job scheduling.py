d1524fe22682dbd03883cc89e7cb3120d8d8e22a
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-16 18:30:29 +0530

    Tabulation weighted job scheduling problem

--- Practice Set 2/Searching & Sorting/weighted job scheduling.py
+++ Practice Set 2/Searching & Sorting/weighted job scheduling.py
@@ -83,6 +83,38 @@ class MemoizedSolution:
         return low
 
 
+class TabulationSolution:
+    @staticmethod
+    def schedule(start_times, end_times, profits):
+        """
+            Time complexity is O(n * log(n)) and space complexity is O(n).
+        """
+        jobs = [Job(start_times[i], end_times[i], profits[i]) for i in range(len(start_times))]
+        jobs.sort(key=lambda x: x.start_time)
+        dp = {i: -1e6 for i in range(len(jobs) + 1)}
+        dp[len(jobs)] = 0
+        for i in range(len(jobs) - 1, -1, -1):
+            left = jobs[i].profit + dp[TabulationSolution._get_next_job(jobs, i, len(jobs))]
+            right = dp[i + 1]
+            dp[i] = max(left, right)
+        return dp[0]
+
+    @staticmethod
+    def _get_next_job(jobs, i, n):
+        """
+            Time complexity is O(log(n)) and space complexity is O(1).
+        """
+        low, high = i + 1, n - 1
+        while low <= high:
+            mid = int(low + (high - low)/2)
+            possible_next_job = jobs[mid]
+            if possible_next_job.start_time >= jobs[i].end_time:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return low
+
+
 print("Recursive Approach")
 print(
     RecursiveSolution.schedule(
@@ -161,6 +193,48 @@ print(
 
 print(
     MemoizedSolution.schedule(
+        [1, 1, 1],
+        [2, 3, 4],
+        [5, 6, 4]
+    )
+)
+
+print("Tabulation Approach")
+print(
+    TabulationSolution.schedule(
+        [1, 3, 6, 2],
+        [2, 5, 19, 100],
+        [50, 20, 100, 200]
+    )
+)
+
+print(
+    TabulationSolution.schedule(
+        [1, 2, 4, 5],
+        [3, 5, 6, 7],
+        [60, 50, 70, 30]
+    )
+)
+
+
+print(
+    TabulationSolution.schedule(
+        [1, 2, 3, 3],
+        [3, 4, 5, 6],
+        [50, 10, 40, 70]
+    )
+)
+
+print(
+    TabulationSolution.schedule(
+        [1, 2, 3, 4, 6],
+        [3, 5, 10, 6, 9],
+        [20, 20, 100, 70, 60]
+    )
+)
+
+print(
+    TabulationSolution.schedule(
         [1, 1, 1],
         [2, 3, 4],
         [5, 6, 4]
