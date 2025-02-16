822bee8cdb59c6bbc27462890b65ffd7f8430c17
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-16 12:02:29 +0530

    Created a linked list class

---
+++ Practice Set 1/Linked Lists/Kth from End of Linked List.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = self.tail = None
+        self.length = 0
+
+    def is_empty(self):
+        return self.length == 0
+
+    def push(self, x):
+        node = Node(x)
+        if self.is_empty():
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+        self.length += 1
+
+    def build(self, *args):
+        for i in args:
+            self.push(i)
+
+    def __str__(self):
+        result = "["
+        curr = self.head
+        while curr != self.tail:
+            result += f"{curr.data}, "
+            curr = curr.next
+        result += f"{self.tail.data}]"
+        return result
+
