bf7f60ea99c503fbb0787364ce45e707ff041862
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-20 14:48:02 +0530

    Implemented quick sort

--- Greedy/Defense of a Kingdom.py
+++ Greedy/Defense of a Kingdom.py
@@ -1,3 +1,31 @@
+class QuickSort:
+    @staticmethod
+    def sort(arr):
+        QuickSort._sort(arr, 0, len(arr) - 1)
+
+    @staticmethod
+    def _sort(arr, low, high):
+        if low >= high:
+            return
+        partition_index = QuickSort._get_partition_index(arr, low, high)
+        QuickSort._sort(arr, low, partition_index - 1)
+        QuickSort._sort(arr, partition_index + 1, high)
+
+    @staticmethod
+    def _get_partition_index(arr, low, high):
+        pivot = arr[low]
+        i, j = low, high
+        while i < j:
+            while arr[i] <= pivot and i <= high - 1:
+                i += 1
+            while arr[j] > pivot and j >= low + 1:
+                j -= 1
+            if i < j:
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[low], arr[j] = arr[j], arr[low]
+        return j
+
+
 class Solution:
     @staticmethod
     def defense(n, m, mtx):
@@ -7,8 +35,8 @@ class Solution:
             ys.append(mtx[i][1])
         xs.append(n + 1)
         ys.append(m + 1)
-        xs.sort()
-        ys.sort()
+        QuickSort.sort(xs)
+        QuickSort.sort(ys)
         max_x, max_y = 0, 0
         for i in range(1, len(xs)):
             max_x = max(max_x, xs[i] - xs[i - 1] - 1)
