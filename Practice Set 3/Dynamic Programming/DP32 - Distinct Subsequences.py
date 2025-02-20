6d2ecf8b055be987aa686e9723d4ff91a58534cf
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-20 11:29:42 +0530

    Recursive distinct subsequences

---
+++ Practice Set 3/Dynamic Programming/DP32 - Distinct Subsequences.py
@@ -0,0 +1,28 @@
+def recursive():
+    """
+        Time complexity is exponential and space complexity is O(n1 + n2).
+    """
+    def solve(s1, i, s2, j):
+        if j == 0:
+            return 1
+        if i == 0:
+            return 0
+        if s1[i - 1] == s2[j - 1]:
+            return solve(s1, i - 1, s2, j - 1) + solve(s1, i - 1, s2, j)
+        else:
+            return solve(s1, i - 1, s2, j)
+
+    def distinct_subsequences(s1, s2):
+        n1, n2 = len(s1), len(s2)
+        return solve(s1, n1, s2, n2)
+
+    print(distinct_subsequences("babgbag", "bag"))
+    print(distinct_subsequences("brootgroot", "brt"))
+    print(distinct_subsequences("dingdingdingding", "ing"))
+    print(distinct_subsequences("aaaaa", "a"))
+    print(distinct_subsequences("rabbbit", "rabbit"))
+    print(distinct_subsequences("banana", "ban"))
+    print(distinct_subsequences("geeksforgeeks", "ge"))
+
+
+recursive()
