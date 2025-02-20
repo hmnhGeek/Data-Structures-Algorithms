0720c7e5c4deb2098b13203e87924e79dad95017
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-20 12:06:47 +0530

    Memoized distinct subsequences

--- Practice Set 3/Dynamic Programming/DP32 - Distinct Subsequences.py
+++ Practice Set 3/Dynamic Programming/DP32 - Distinct Subsequences.py
@@ -25,4 +25,37 @@ def recursive():
     print(distinct_subsequences("geeksforgeeks", "ge"))
 
 
+def memoized():
+    """
+        Time complexity is O(n1 * n2) and space complexity is O(n1 + n2 + n1 * n2).
+    """
+    def solve(s1, i, s2, j, dp):
+        if j == 0:
+            return 1
+        if i == 0:
+            return 0
+        if dp[i][j] is not None:
+            return dp[i][j]
+        if s1[i - 1] == s2[j - 1]:
+            dp[i][j] = solve(s1, i - 1, s2, j - 1, dp) + solve(s1, i - 1, s2, j, dp)
+        else:
+            dp[i][j] = solve(s1, i - 1, s2, j, dp)
+        return dp[i][j]
+
+    def distinct_subsequences(s1, s2):
+        n1, n2 = len(s1), len(s2)
+        dp = {i: {j: None for j in range(n2 + 1)} for i in range(n1 + 1)}
+        return solve(s1, n1, s2, n2, dp)
+
+    print(distinct_subsequences("babgbag", "bag"))
+    print(distinct_subsequences("brootgroot", "brt"))
+    print(distinct_subsequences("dingdingdingding", "ing"))
+    print(distinct_subsequences("aaaaa", "a"))
+    print(distinct_subsequences("rabbbit", "rabbit"))
+    print(distinct_subsequences("banana", "ban"))
+    print(distinct_subsequences("geeksforgeeks", "ge"))
+
+
 recursive()
+print()
+memoized()
