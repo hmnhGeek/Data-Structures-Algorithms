62c67a4589c362cbdd10f966a809585396864b2c
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-20 09:39:00 +0530

    Get minimum in a rotated sorted array

--- Practice Set 3/Binary Search/BS6 - Minimum in Rotated Sorted Array.py
+++ Practice Set 3/Binary Search/BS6 - Minimum in Rotated Sorted Array.py
@@ -1,23 +1,35 @@
 class Solution:
     @staticmethod
     def get_min(arr):
+        # define the search space.
         low, high = 0, len(arr) - 1
+
+        # hold the minimum value in `ans` variable.
         ans = 1e6
+
+        # typical binary search
         while low <= high:
             mid = int(low + (high - low)/2)
 
+            # if all the variables are same
             if arr[low] == arr[mid] == arr[high]:
+                # update the minimum value and update the search space.
                 ans = min(ans, arr[low])
                 low += 1
                 high -= 1
                 continue
 
+            # if the left part is sorted
             if arr[low] <= arr[mid]:
+                # update the ans with minimum of low and ans.
                 ans = min(ans, arr[low])
                 low = mid + 1
             else:
+                # update the ans with minimum of mid and ans.
                 ans = min(ans, arr[mid])
                 high = mid - 1
+
+        # return the `ans` which now holds the minimum value.
         return ans
 
 
