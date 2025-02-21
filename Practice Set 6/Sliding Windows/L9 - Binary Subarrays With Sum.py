374e92e19ddb9bce0a135ce7a3d9250c8ba4573b
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-21 20:42:42 +0530

    Binary Subarray Count

--- Practice Set 6/Sliding Windows/L9 - Binary Subarrays With Sum.py
+++ Practice Set 6/Sliding Windows/L9 - Binary Subarrays With Sum.py
@@ -1,19 +1,37 @@
 class Solution:
     @staticmethod
     def _count_less_than(arr, k):
+        """
+            Time complexity is O(n) and space complexity is O(1).
+        """
+
+        # edge case
         if k < 0:
             return 0
+
+        # define window variables
         left = right = 0
         n = len(arr)
+
+        # define tracking variables
         _sum = count = 0
+
+        # while there is ground to cover.
         while right < n:
+            # update sum
             _sum += arr[right]
+
+            # shrink continuously from left till the sum > k
             while _sum > k:
                 _sum -= arr[left]
                 left += 1
+
+            # update the count of subarrays whose sum <= k.
             if _sum <= k:
                 count += (right - left + 1)
             right += 1
+
+        # return the count.
         return count
 
     @staticmethod
