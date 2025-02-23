5c11b219b5c144984a6232abe163feadadfae07e
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-23 16:29:22 +0530

    Arithmetic Number

--- Practice Set 1/Searching & Sorting/Arithmetic Number.py
+++ Practice Set 1/Searching & Sorting/Arithmetic Number.py
@@ -1,7 +1,16 @@
+# Problem link - https://www.geeksforgeeks.org/problems/arithmetic-number2815/1
+
+
 class Solution:
     @staticmethod
     def arithmetic_number(a, b, c):
+        """
+            Time and space complexities both are O(1).
+        """
+
         return ((b - a)/c) == ((b - a)//c)
 
 
 print(Solution.arithmetic_number(1, 3, 2))
+print(Solution.arithmetic_number(1, 2, 3))
+print(Solution.arithmetic_number(1, 2, 4))
