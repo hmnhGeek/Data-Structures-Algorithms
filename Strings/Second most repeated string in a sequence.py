1f42b37d0d7921c27547d351698904093818e227
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-23 16:20:38 +0530

    Second most repeated character

--- Strings/Second most repeated string in a sequence.py
+++ Strings/Second most repeated string in a sequence.py
@@ -4,14 +4,28 @@ from collections import Counter
 class Solution:
     @staticmethod
     def get_second_most(arr):
+        """
+            Time complexity is O(n) and space complexity is O(n * |s|).
+        """
+
+        # This will take O(n) time and O(n*|s|) space.
         d = dict(Counter(arr))
+
+        # This will take O(n) time.
         max_val = max(d.values())
+
+        # store second max key and its value.
         second_max = 0
         second_max_key = -1
+
+        # loop in O(n) time
         for k, v in d.items():
+            # update second max value
             if second_max < v < max_val:
                 second_max = v
                 second_max_key = k
+
+        # return second max key.
         return second_max_key
 
 
