67221bd56f6d3f9769ea0f9091e836164fb211db
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-26 16:20:01 +0530

    Find specific pair in a matrix

---
+++ Practice Set 3/Matrix/Specific Pair in Matrix.py
@@ -0,0 +1,33 @@
+class Solution:
+    @staticmethod
+    def specific_pair(mtx):
+        n, m = len(mtx), len(mtx[0])
+        max_mtx = [[None for _ in range(m)] for _ in range(n)]
+        max_mtx[n - 1][m - 1] = mtx[n - 1][m - 1]
+        for i in range(n - 2, -1, -1):
+            max_mtx[i][m - 1] = max(mtx[i][m - 1], max_mtx[i + 1][m - 1])
+        for j in range(m - 2, -1, -1):
+            max_mtx[n - 1][j] = max(mtx[n - 1][j], max_mtx[n - 1][j + 1])
+        for i in range(n - 2, -1, -1):
+            for j in range(m - 2, -1, -1):
+                max_mtx[i][j] = max(
+                    max_mtx[i][j + 1],
+                    max_mtx[i + 1][j],
+                    mtx[i][j]
+                )
+        max_diff = -1e6
+        for i in range(n - 1):
+            for j in range(m - 1):
+                max_diff = max(max_diff, max_mtx[i + 1][j + 1] - mtx[i][j])
+        print(max_diff)
+
+
+Solution.specific_pair(
+    [
+        [1, 2, -1, -4, -20],
+        [-8, -3, 4, 2, 1],
+        [3, 8, 6, 1, 3],
+        [-4, -1, 1, 7, -6],
+        [0, -4, 10, -5, 1]
+    ]
+)
