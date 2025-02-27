53175b11022a947506080d11bde755416cbef642
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-27 08:33:18 +0530

    nth root of x using binary search

---
+++ Practice Set 3/Binary Search/BS11 - Find the Nth root of an Integer.py
@@ -0,0 +1,41 @@
+class Solution:
+    @staticmethod
+    def _get_flag(x, mid, n):
+        product = 1
+        for i in range(n):
+            product *= mid
+            if product > x:
+                return 2
+        if product == x:
+            return 1
+        return 0
+
+    @staticmethod
+    def nth_root_of_x(x, n):
+        if x < 0 or n < 0:
+            return -1
+        low, high = 0, x
+        while low <= high:
+            mid = int(low + (high - low)/2)
+            flag = Solution._get_flag(x, mid, n)
+            if flag == 2:
+                high = mid - 1
+            elif flag == 1:
+                return mid
+            else:
+                low = mid + 1
+        return -1
+
+
+print(Solution.nth_root_of_x(27, 3))
+print(Solution.nth_root_of_x(26, 3))
+print(Solution.nth_root_of_x(8, 3))
+print(Solution.nth_root_of_x(343, 3))
+print(Solution.nth_root_of_x(81, 4))
+print(Solution.nth_root_of_x(81, 3))
+print(Solution.nth_root_of_x(1000, 2))
+print(Solution.nth_root_of_x(100, 2))
+print(Solution.nth_root_of_x(10**9, 10))
+print(Solution.nth_root_of_x(9, 2))
+print(Solution.nth_root_of_x(14, 1))
+print(Solution.nth_root_of_x(9, 3))
