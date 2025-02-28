516d5c7830808c83f960fbdaf7d715a704e72bcf
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-28 10:15:01 +0530

    Disjoint Set by sizes

--- Practice Set 3/Graphs/G46b - Disjoint Set - Union by Size.py
+++ Practice Set 3/Graphs/G46b - Disjoint Set - Union by Size.py
@@ -1,7 +1,7 @@
 class DisjointSet:
     def __init__(self, nodes):
         self.parents = {i: i for i in nodes}
-        self.ranks = {i: 0 for i in nodes}
+        self.sizes = {i: 1 for i in nodes}
 
     def find_ultimate_parent(self, node):
         if self.parents[node] == node:
@@ -12,28 +12,25 @@ class DisjointSet:
     def union(self, node1, node2):
         ulp_node1 = self.find_ultimate_parent(node1)
         ulp_node2 = self.find_ultimate_parent(node2)
-
         if ulp_node1 == ulp_node2:
             return
-
-        if self.ranks[ulp_node1] < self.ranks[ulp_node2]:
+        if self.sizes[ulp_node1] < self.sizes[ulp_node2]:
             self.parents[ulp_node1] = ulp_node2
-        elif self.ranks[ulp_node2] < self.ranks[ulp_node1]:
-            self.parents[ulp_node2] = ulp_node1
+            self.sizes[ulp_node2] += self.sizes[ulp_node1]
         else:
-            self.parents[ulp_node1] = ulp_node2
-            self.ranks[ulp_node2] += 1
+            self.parents[ulp_node2] = ulp_node1
+            self.sizes[ulp_node1] += self.sizes[ulp_node2]
 
     def in_same_component(self, node1, node2):
         return self.find_ultimate_parent(node1) == self.find_ultimate_parent(node2)
 
 
-dsr = DisjointSet([i for i in range(1, 8)])
-dsr.union(1, 2)
-dsr.union(2, 3)
-dsr.union(4, 5)
-dsr.union(6, 7)
-dsr.union(5, 6)
-print(dsr.in_same_component(3, 7))
-dsr.union(3, 7)
-print(dsr.in_same_component(3, 7))
+ds = DisjointSet([i for i in range(1, 8)])
+ds.union(1, 2)
+ds.union(2, 3)
+ds.union(4, 5)
+ds.union(6, 7)
+ds.union(5, 6)
+print(ds.in_same_component(3, 7))
+ds.union(3, 7)
+print(ds.in_same_component(3, 7))
