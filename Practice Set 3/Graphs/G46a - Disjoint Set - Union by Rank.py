20d671544b69a5a7149b427855fa69edc2976c55
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-28 10:10:50 +0530

    Disjoint Set by ranks

---
+++ Practice Set 3/Graphs/G46a - Disjoint Set - Union by Rank.py
@@ -0,0 +1,39 @@
+class DisjointSet:
+    def __init__(self, nodes):
+        self.parents = {i: i for i in nodes}
+        self.ranks = {i: 0 for i in nodes}
+
+    def find_ultimate_parent(self, node):
+        if self.parents[node] == node:
+            return node
+        self.parents[node] = self.find_ultimate_parent(self.parents[node])
+        return self.parents[node]
+
+    def union(self, node1, node2):
+        ulp_node1 = self.find_ultimate_parent(node1)
+        ulp_node2 = self.find_ultimate_parent(node2)
+
+        if ulp_node1 == ulp_node2:
+            return
+
+        if self.ranks[ulp_node1] < self.ranks[ulp_node2]:
+            self.parents[ulp_node1] = ulp_node2
+        elif self.ranks[ulp_node2] < self.ranks[ulp_node1]:
+            self.parents[ulp_node2] = ulp_node1
+        else:
+            self.parents[ulp_node1] = ulp_node2
+            self.ranks[ulp_node2] += 1
+
+    def in_same_component(self, node1, node2):
+        return self.find_ultimate_parent(node1) == self.find_ultimate_parent(node2)
+
+
+dsr = DisjointSet([i for i in range(1, 8)])
+dsr.union(1, 2)
+dsr.union(2, 3)
+dsr.union(4, 5)
+dsr.union(6, 7)
+dsr.union(5, 6)
+print(dsr.in_same_component(3, 7))
+dsr.union(3, 7)
+print(dsr.in_same_component(3, 7))
