7c1fd46d180df7f1a322d2d56a49462746f8730b
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-01 15:22:25 +0530

    Get kth ancestor in a binary tree

--- Practice Set 1/Binary Trees/kth anscestor.py
+++ Practice Set 1/Binary Trees/kth anscestor.py
@@ -7,24 +7,43 @@ class Node:
 class Solution:
     @staticmethod
     def get_kth_ancestor(root: Node, node, k):
+        # store the kth ancestor in a list of 1 size, assuming initially that no kth ancestor exists.
         kth = [-1]
-        Solution.kth_ancestor(root, node, k, kth)
+
+        # recursively find the kth ancestor in O(n) time and O(h) space.
+        Solution._kth_ancestor(root, node, k, kth)
+
+        # return the kth ancestor.
         return kth[0]
 
     @staticmethod
-    def kth_ancestor(root: Node, node: int, k: int, kth):
+    def _kth_ancestor(root: Node, node: int, k: int, kth):
+        # if root is None, return None as node was not found.
         if root is None:
             return None
+
+        # if root's data is same as node, return 1; basically we have started back tracking by 1 unit distance.
         if root.data == node:
             return 1
-        left = Solution.kth_ancestor(root.left, node, k, kth)
-        right = Solution.kth_ancestor(root.right, node, k, kth)
+
+        # recursively check for node in left and right subtrees.
+        left = Solution._kth_ancestor(root.left, node, k, kth)
+        right = Solution._kth_ancestor(root.right, node, k, kth)
+
+        # if left distance or right distance becomes equivalent to `k`, then root is the kth ancestor, update the
+        # ancestor list passed by reference.
         if left == k or right == k:
             kth[0] = root.data
+
+        # if node was not found in left and right subtrees, return None.
         if left is None and right is None:
             return None
+
+        # if node was not found in left but in right subtree, return right + 1 to back track.
         if left is None:
             return right + 1
+
+        # else return left + 1 for back tracking.
         return left + 1
 
 
