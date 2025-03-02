60f6b56a7f1e3f81bf45740578cd5f7acb427163
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-02 12:34:28 +0530

    Minimum Adjacent Swaps for Bracket Balancing

--- Strings/Minimum Adjacent Swaps for Bracket Balancing.py
+++ Strings/Minimum Adjacent Swaps for Bracket Balancing.py
@@ -6,21 +6,31 @@ class Solution:
         """
 
         n = len(string)
+
+        # create utility variables
         opened = closed = min_swaps = unbalanced = 0
+
+        # loop in the string
         for i in range(n):
+            # if an opening bracket is found...
             if string[i] == '[':
                 opened += 1
                 """
-                    We only swap when unbalanced > 0 because that ensures we are actually 
-                    correcting an existing imbalance. If unbalanced == 0, it means the 
-                    sequence is already balanced up to that point, so no swap is needed.
+                    Since an opening bracket is found, we only swap when unbalanced > 0 
+                    because that ensures we are actually correcting an existing imbalance. 
+                    If unbalanced == 0, it means the sequence is already balanced up to 
+                    that point, so no swap is needed.
                 """
                 if unbalanced > 0:
                     min_swaps += unbalanced
+                    # since balancing is completed, reduce one from unbalanced.
                     unbalanced -= 1
             else:
+                # if a closed bracket is found, update the unbalanced amount.
                 closed += 1
                 unbalanced = closed - opened
+
+        # return the min_swaps.
         return min_swaps
 
 
