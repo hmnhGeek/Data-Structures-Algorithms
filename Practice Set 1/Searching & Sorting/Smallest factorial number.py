8962e34a2f6ea17dc7a24ab3b343751bc6da5fe2
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-02 11:53:38 +0530

    Smallest factorial number with n trailing zeros

--- Practice Set 1/Searching & Sorting/Smallest factorial number.py
+++ Practice Set 1/Searching & Sorting/Smallest factorial number.py
@@ -1,5 +1,6 @@
 from math import floor
 
+
 class Solution:
     @staticmethod
     def _get_num_zeros(mid):
@@ -12,15 +13,28 @@ class Solution:
 
     @staticmethod
     def get_smallest(n):
+        """
+            Overall time complexity is O(log2(n) * log5(n)) and space complexity is O(1).
+        """
+
+        # edge case
         if n == 0:
             return 0
+
+        # this search space is given in problem.
         low, high = 1, 10**4
         while low <= high:
+            # assume mid to be the least number having n trailing zeros.
             mid = int(low + (high - low)/2)
+
+            # count the actual trailing zeros in its factorial
             num_zeros = Solution._get_num_zeros(mid)
+
+            # if zeros >= n, let's try for lower number
             if num_zeros > n:
                 high = mid - 1
             elif num_zeros < n:
+                # else try for larger number
                 low = mid + 1
             else:
                 high = mid - 1
