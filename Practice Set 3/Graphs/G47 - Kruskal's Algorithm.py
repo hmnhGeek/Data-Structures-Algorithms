334f3995d0c123cd8105be85b9b15c784edfdcd3
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-13 13:10:34 +0530

    Kruskal's Algorithm using Disjoint Set by size

--- Practice Set 3/Graphs/G47 - Kruskal's Algorithm.py
+++ Practice Set 3/Graphs/G47 - Kruskal's Algorithm.py
@@ -24,3 +24,53 @@ class DisjointSet:
     def in_same_component(self, node1, node2):
         return self.find_ultimate_parent(node1) == self.find_ultimate_parent(node2)
 
+
+class Solution:
+    @staticmethod
+    def _get_edges(graph):
+        edges = []
+        for node in graph:
+            for adj in graph[node]:
+                adj_node, wt = adj
+                edges.append((wt, node, adj_node))
+        return edges
+
+    @staticmethod
+    def get_mst(graph):
+        edges = Solution._get_edges(graph)
+        edges.sort(key=lambda x: x[0])
+        disjoint_set = DisjointSet([i for i in graph])
+        mst = []
+        mst_wt = 0
+        for edge in edges:
+            wt, node1, node2 = edge
+            if disjoint_set.in_same_component(node1, node2):
+                continue
+            mst.append((node1, node2))
+            mst_wt += wt
+            disjoint_set.union(node1, node2)
+        return mst, mst_wt
+
+
+print(
+    Solution.get_mst(
+        {
+            1: [[2, 2], [4, 1], [5, 4]],
+            2: [[1, 2], [3, 3], [4, 3], [6, 7]],
+            3: [[2, 3], [4, 5], [6, 8]],
+            4: [[5, 9], [1, 1], [2, 3], [3, 5]],
+            5: [[1, 4], [4, 9]],
+            6: [[2, 7], [3, 8]]
+        }
+    )
+)
+
+print(
+    Solution.get_mst(
+        {
+            0: [[1, 5], [2, 1]],
+            1: [[0, 5], [2, 3]],
+            2: [[0, 1], [1, 3]]
+        }
+    )
+)
