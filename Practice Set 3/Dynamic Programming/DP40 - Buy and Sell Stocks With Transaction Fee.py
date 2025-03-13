2f6025e9d55c9ff07b851376cb2d13c519c0b3c4
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-13 12:46:47 +0530

    Space optimized buy and sell stocks with transaction fee

--- Practice Set 3/Dynamic Programming/DP40 - Buy and Sell Stocks With Transaction Fee.py
+++ Practice Set 3/Dynamic Programming/DP40 - Buy and Sell Stocks With Transaction Fee.py
@@ -64,7 +64,7 @@ def memoized():
 
 def tabulation():
     """
-        Time complexity is O(n) and space complexity is O(3n).
+        Time complexity is O(n) and space complexity is O(2n).
     """
     def buy_and_sell(arr, fee):
         n = len(arr)
@@ -91,8 +91,41 @@ def tabulation():
     print(buy_and_sell([7, 1, 5, 3, 6, 4], 1))
 
 
+def space_optimized():
+    """
+        Time complexity is O(n) and space complexity is O(1).
+    """
+    def buy_and_sell(arr, fee):
+        n = len(arr)
+        nxt = {j: 0 for j in [True, False]}
+        for index in range(n - 1, -1, -1):
+            curr = {j: 0 for j in [True, False]}
+            for can_buy in [True, False]:
+                if can_buy:
+                    curr[can_buy] = max(
+                        -arr[index] + nxt[not can_buy],
+                        nxt[can_buy]
+                    )
+                else:
+                    curr[can_buy] = max(
+                        arr[index] - fee + nxt[not can_buy],
+                        nxt[can_buy]
+                    )
+            nxt = curr
+        return nxt[True]
+
+    print(buy_and_sell([1, 3, 2, 8, 4, 9], 2))
+    print(buy_and_sell([1, 2, 3], 1))
+    print(buy_and_sell([1, 3, 5, 6], 2))
+    print(buy_and_sell([1, 3, 7, 5, 10, 3], 3))
+    print(buy_and_sell([6, 1, 7, 2, 8, 4], 2))
+    print(buy_and_sell([7, 1, 5, 3, 6, 4], 1))
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
+print()
+space_optimized()
