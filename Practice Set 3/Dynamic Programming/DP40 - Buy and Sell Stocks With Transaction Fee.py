de244438590729f8dbaa84061fb43b276b13153a
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-13 12:45:22 +0530

    Tabulation buy and sell stocks with transaction fee

--- Practice Set 3/Dynamic Programming/DP40 - Buy and Sell Stocks With Transaction Fee.py
+++ Practice Set 3/Dynamic Programming/DP40 - Buy and Sell Stocks With Transaction Fee.py
@@ -62,6 +62,37 @@ def memoized():
     print(buy_and_sell([7, 1, 5, 3, 6, 4], 1))
 
 
+def tabulation():
+    """
+        Time complexity is O(n) and space complexity is O(3n).
+    """
+    def buy_and_sell(arr, fee):
+        n = len(arr)
+        dp = {i: {j: 0 for j in [True, False]} for i in range(n + 2)}
+        for index in range(n - 1, -1, -1):
+            for can_buy in [True, False]:
+                if can_buy:
+                    dp[index][can_buy] = max(
+                        -arr[index] + dp[index + 1][not can_buy],
+                        dp[index + 1][can_buy]
+                    )
+                else:
+                    dp[index][can_buy] = max(
+                        arr[index] - fee + dp[index + 1][not can_buy],
+                        dp[index + 1][can_buy]
+                    )
+        return dp[0][True]
+
+    print(buy_and_sell([1, 3, 2, 8, 4, 9], 2))
+    print(buy_and_sell([1, 2, 3], 1))
+    print(buy_and_sell([1, 3, 5, 6], 2))
+    print(buy_and_sell([1, 3, 7, 5, 10, 3], 3))
+    print(buy_and_sell([6, 1, 7, 2, 8, 4], 2))
+    print(buy_and_sell([7, 1, 5, 3, 6, 4], 1))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
