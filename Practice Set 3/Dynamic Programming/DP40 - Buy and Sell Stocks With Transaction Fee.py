73c2d7089ac05674b8746439e2324c40ecb3f9c3
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-13 12:42:58 +0530

    Memoized buy and sell stocks with transaction fee

--- Practice Set 3/Dynamic Programming/DP40 - Buy and Sell Stocks With Transaction Fee.py
+++ Practice Set 3/Dynamic Programming/DP40 - Buy and Sell Stocks With Transaction Fee.py
@@ -28,4 +28,40 @@ def recursive():
     print(buy_and_sell([7, 1, 5, 3, 6, 4], 1))
 
 
+def memoized():
+    """
+        Time complexity is O(n) and space complexity is O(3n).
+    """
+    def solve(arr, index, can_buy, fee, n, dp):
+        if index >= n:
+            return 0
+        if dp[index][can_buy] is not None:
+            return dp[index][can_buy]
+        if can_buy:
+            dp[index][can_buy] = max(
+                -arr[index] + solve(arr, index + 1, not can_buy, fee, n, dp),
+                solve(arr, index + 1, can_buy, fee, n, dp)
+            )
+        else:
+            dp[index][can_buy] = max(
+                arr[index] - fee + solve(arr, index + 1, not can_buy, fee, n, dp),
+                solve(arr, index + 1, can_buy, fee, n, dp)
+            )
+        return dp[index][can_buy]
+
+    def buy_and_sell(arr, fee):
+        n = len(arr)
+        dp = {i: {j: None for j in [True, False]} for i in range(n + 2)}
+        return solve(arr, 0, True, fee, n, dp)
+
+    print(buy_and_sell([1, 3, 2, 8, 4, 9], 2))
+    print(buy_and_sell([1, 2, 3], 1))
+    print(buy_and_sell([1, 3, 5, 6], 2))
+    print(buy_and_sell([1, 3, 7, 5, 10, 3], 3))
+    print(buy_and_sell([6, 1, 7, 2, 8, 4], 2))
+    print(buy_and_sell([7, 1, 5, 3, 6, 4], 1))
+
+
 recursive()
+print()
+memoized()
