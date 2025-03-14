53bee86cea2796671a21408c03c2b8ba1b28a3b4
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-14 18:03:09 +0530

    Minimum days to make M bouquets

---
+++ Practice Set 3/Binary Search/BS13 - Minimum days to make M bouquets.py
@@ -0,0 +1,39 @@
+class Solution:
+    @staticmethod
+    def _find_formed_bouquets_count(arr, mid, k):
+        bouquets = 0
+        flowers_used = 0
+        n = len(arr)
+        for i in range(n):
+            if arr[i] > mid:
+                flowers_used = 0
+                continue
+            flowers_used += 1
+            if flowers_used == k:
+                bouquets += 1
+                flowers_used = 0
+        return bouquets
+
+    @staticmethod
+    def min_days_for_m_bouquets(arr, m, k):
+        if m * k > len(arr):
+            return -1
+        low, high = 1, max(arr)
+        while low <= high:
+            mid = int(low + (high - low)/2)
+            bouquets_formed = Solution._find_formed_bouquets_count(arr, mid, k)
+            if bouquets_formed < m:
+                low = mid + 1
+            elif bouquets_formed == m:
+                high = mid - 1
+            else:
+                high = mid - 1
+        return low
+
+
+print(Solution.min_days_for_m_bouquets([7, 7, 7, 7, 13, 11, 12, 7], 2, 3))
+print(Solution.min_days_for_m_bouquets([1, 10, 3, 10, 2], 3, 2))
+print(Solution.min_days_for_m_bouquets([1, 2, 1, 2, 7, 2, 2, 3, 1], 2, 3))
+print(Solution.min_days_for_m_bouquets([1, 1, 1, 1], 1, 1))
+print(Solution.min_days_for_m_bouquets([3, 4, 2, 7, 13, 8, 5], 3, 2))
+print(Solution.min_days_for_m_bouquets([5, 5, 5, 5, 10, 5, 5], 2, 3))
