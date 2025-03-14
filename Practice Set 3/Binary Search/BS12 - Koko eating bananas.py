45f3a16e533b0498bb90052794830837aea5b453
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-14 17:18:52 +0530

    Koko eating bananas

--- Practice Set 3/Binary Search/BS12 - Koko eating bananas.py
+++ Practice Set 3/Binary Search/BS12 - Koko eating bananas.py
@@ -11,16 +11,30 @@ class Solution:
 
     @staticmethod
     def koko(arr, h):
+        # define the search space.
         low, high = 1, max(arr)
+
+        # typical binary search
         while low <= high:
+            # get the mid-rate.
             mid = int(low + (high - low)/2)
+
+            # get the hours taken by koko to consume all the bananas at mid-rate.
             hours_taken = Solution._hours_taken(arr, mid)
+
+            # if hours taken < h, then we can try even for a lower rate.
             if hours_taken < h:
                 high = mid - 1
+
+            # same case when hours taken = h
             elif hours_taken == h:
                 high = mid - 1
+
+            # otherwise, we must increase the rate.
             else:
                 low = mid + 1
+
+        # low points to the correct rate.
         return low
 
 
