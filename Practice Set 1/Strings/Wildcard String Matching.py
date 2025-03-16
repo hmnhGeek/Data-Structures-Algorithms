430a2ead0261d94c7a2ae1ee4c3771cccca26843
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-16 14:01:56 +0530

    Tabulation wildcard matching

--- Practice Set 1/Strings/Wildcard String Matching.py
+++ Practice Set 1/Strings/Wildcard String Matching.py
@@ -75,6 +75,42 @@ def memoized():
     print(match("**", ""))
 
 
+def tabulation():
+    """
+        Time complexity is O(n1 * n2) and space complexity is O(n1 * n2).
+    """
+    def match(string, pattern):
+        n1, n2 = len(string), len(pattern)
+        dp = {i: {j: False for j in range(n2 + 1)} for i in range(n1 + 1)}
+        for i in dp:
+            all_stars = True
+            for k in range(i, 0, -1):
+                if string[k - 1] != "*":
+                    all_stars = False
+            dp[i][0] = all_stars
+        dp[0][0] = True
+        for i in range(1, n1 + 1):
+            for j in range(1, n2 + 1):
+                if string[i - 1] == pattern[j - 1] or string[i - 1] == "?":
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif string[i - 1] == "*":
+                    dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
+                else:
+                    dp[i][j] = False
+        return dp[n1][n2]
+
+    print(match("?ay", "ray"))
+    print(match("ab*cd", "abdefcd"))
+    print(match("ab?d", "abcc"))
+    print(match("ba*a?", "baaabab"))
+    print(match("a", "aa"))
+    print(match("*", "aa"))
+    print(match("?a", "cb"))
+    print(match("**", ""))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
