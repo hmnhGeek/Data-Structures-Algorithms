cafae6dcddf51a3d1c4e34cc74dfae0b5079b4e2
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-19 10:13:47 +0530

    Print the longest increasing subsequence

--- Practice Set 4/Dynamic Programming/DP42 - Printing the Longest Increasing Subsequence.py
+++ Practice Set 4/Dynamic Programming/DP42 - Printing the Longest Increasing Subsequence.py
@@ -2,21 +2,34 @@ class Solution:
     @staticmethod
     def print_lis(arr):
         n = len(arr)
+
+        # create a dp and parents dictionaries to store the LIS length and the parents of each index respectively.
         dp = {i: 1 for i in range(n)}
         parents = {i: i for i in range(n)}
+
+        # loop in the array
         for i in range(n):
+            # loop for the previous index
             for prev in range(i):
+                # if the prev index value can form an increasing subsequence with current index `i`.
                 if arr[prev] < arr[i]:
+                    # update the LIS length if possible and also update the parent.
                     if 1 + dp[prev] > dp[i]:
                         dp[i] = 1 + dp[prev]
                         parents[i] = prev
+
+        # get the last index of LIS using the max value of dp.
         last_element_index_of_lis = max(dp, key=dp.get)
+
+        # back track to form the LIS.
         j = last_element_index_of_lis
         lis = []
         while parents[j] != j:
             lis.append(arr[j])
             j = parents[j]
         lis.append(arr[j])
+
+        # return the reverse of the backtracked array as the correct LIS.
         return lis[-1:-len(lis)-1:-1]
 
 
