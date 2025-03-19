31e62fb01813ca473f921ac145e4e1cfe9aec6e0
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-19 09:47:30 +0530

    Length of the longest increasing subsequence using recursion

--- Practice Set 4/Dynamic Programming/DP41 - Longest Increasing Subsequence.py
+++ Practice Set 4/Dynamic Programming/DP41 - Longest Increasing Subsequence.py
@@ -1,16 +1,26 @@
+def get_val_at(arr, index, n):
+    if index not in range(n):
+        return 1e6
+    return arr[index]
+
+
 def recursive():
-    def solve(arr, index, prev):
+    """
+        Time complexity is exponential and space complexity is O(n).
+    """
+
+    def solve(arr, index, prev, n):
         if index == 0:
-            return 1 if arr[0] < prev else 0
+            return 1 if arr[0] < get_val_at(arr, prev, n) else 0
         left = -1e6
-        if arr[index] < prev:
-            left = 1 + solve(arr, index - 1, arr[index])
-        right = solve(arr, index - 1, prev)
+        if arr[index] < get_val_at(arr, prev, n):
+            left = 1 + solve(arr, index - 1, index, n)
+        right = solve(arr, index - 1, prev, n)
         return max(left, right)
 
     def get_lis_length(arr):
         n = len(arr)
-        return solve(arr, n - 1, 1e6)
+        return solve(arr, n - 1, 1e6, n)
 
     print(get_lis_length([10, 9, 2, 5, 3, 7, 101, 18]))
     print(get_lis_length([5, 4, 11, 1, 16, 8]))
