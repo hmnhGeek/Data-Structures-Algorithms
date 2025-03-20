e2eff9d8cf32fb7c68564d10bb817254bcc7916f
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-20 11:48:30 +0530

    Minimum window substring

--- Practice Set 6/Sliding Windows/L12 - Minimum Window Substring.py
+++ Practice Set 6/Sliding Windows/L12 - Minimum Window Substring.py
@@ -11,21 +11,39 @@ class Solution:
 
     @staticmethod
     def min_window_substring(s, t):
+        """
+            Time complexity is O(26n) and space complexity is O(26).
+        """
+
+        # define window variables
         n = len(s)
         left = right = 0
+
+        # define tracking variables
         d = {i: 0 for i in s}
         req = dict(Counter(t))
+
+        # define result variables
         length = 1e6
         start_index = -1
+
+        # while there is ground to cover...
         while right < n:
+            # increment the right indexed value.
             d[s[right]] += 1
+
+            # while the condition is satisfied, i.e., all elements of t are present in s, continuously shrink from left.
             while Solution._condition_satisfied(d, req):
                 if length > right - left + 1:
                     length = right - left + 1
                     start_index = left
                 d[s[left]] -= 1
                 left += 1
+
+            # increment right index
             right += 1
+
+        # return the substring.
         return s[start_index:start_index+length] if start_index != -1 else ""
 
 
