e432bf37f636485182a6cdfaccad15fb88666275
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-21 20:22:10 +0530

    Aggressive Cows

--- Practice Set 3/Binary Search/BS17 - Aggressive Cows.py
+++ Practice Set 3/Binary Search/BS17 - Aggressive Cows.py
@@ -1,3 +1,31 @@
+class QuickSort:
+    @staticmethod
+    def _get_partition_index(arr, low, high):
+        pivot = arr[low]
+        i, j = low, high
+        while i < j:
+            while arr[i] <= pivot and i <= high - 1:
+                i += 1
+            while arr[j] > pivot and j >= low + 1:
+                j -= 1
+            if i < j:
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[low], arr[j] = arr[j], arr[low]
+        return j
+
+    @staticmethod
+    def sort(arr):
+        QuickSort._sort(arr, 0, len(arr) - 1)
+
+    @staticmethod
+    def _sort(arr, low, high):
+        if low >= high:
+            return
+        partition_index = QuickSort._get_partition_index(arr, low, high)
+        QuickSort._sort(arr, low, partition_index - 1)
+        QuickSort._sort(arr, partition_index + 1, high)
+
+
 class Solution:
     @staticmethod
     def _num_placed(arr, mid):
@@ -14,7 +42,7 @@ class Solution:
         if k <= 0:
             return -1
 
-        arr.sort()
+        QuickSort.sort(arr)
         low, high = 0, arr[-1]
         while low <= high:
             mid = int(low + (high - low)/2)
