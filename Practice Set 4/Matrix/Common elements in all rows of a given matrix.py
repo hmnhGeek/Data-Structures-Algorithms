cc359a1bdea3a93e14b8622962689425161f2d1f
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-23 15:15:31 +0530

    Find Common elements in all rows of a given matrix

--- Practice Set 4/Matrix/Common elements in all rows of a given matrix.py
+++ Practice Set 4/Matrix/Common elements in all rows of a given matrix.py
@@ -9,17 +9,27 @@ class Solution:
         """
 
         n, m = len(mtx), len(mtx[0])
+
+        # if there is only one row in the matrix, return that row itself.
         if n == 1:
             return mtx[0]
+
+        # find common elements in the first two rows of the matrix.
         set1 = set(mtx[0])
         set2 = set(mtx[1])
         prev = set1.intersection(set2)
+
+        # if there were only 2 rows, return the common elements between them.
         if n == 2:
             return list(prev)
+
+        # else iterate on the remaining rows by taking common elements.
         i = 2
         while i < n:
             prev = prev.intersection(set(mtx[i]))
             i += 1
+
+        # finally, return the common elements in all the rows.
         return list(prev)
 
 
