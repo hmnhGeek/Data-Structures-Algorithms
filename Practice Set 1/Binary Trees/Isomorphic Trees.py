53ba3cc94e7ea197d688c59cf0db3d7e0d5e3c16
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-23 14:52:05 +0530

    Isomorphic binary trees check

--- Practice Set 1/Binary Trees/Isomorphic Trees.py
+++ Practice Set 1/Binary Trees/Isomorphic Trees.py
@@ -7,12 +7,25 @@ class Node:
 class Solution:
     @staticmethod
     def is_isomorphic(root1: Node, root2: Node):
+        # if both the trees are empty, then they are isomorphic
         if root1 is None and root2 is None:
             return True
+
+        # if one of the trees are empty, but the other is not, then they are not isomorphic
         if root1 is None or root2 is None:
             return False
+
+        # if both the trees are not empty, then check if the data on the current node matches or not.
+        # if the data does not match, return False as they are not isomorphic.
         if root1.data != root2.data:
             return False
+
+        # finally, if the data on both the root nodes are same, use recursion on subtrees.
+        # if the left subtree of 1st tree is same as left subtree of 2nd tree and right subtree of 1st tree is
+        # same as right subtree of 2nd tree, then the subtrees are same-isomorphic.
+        # or, if the left subtree of 1st tree is same as right subtree of 2nd tree and vice versa, then they are
+        # cross-isomorphic.
+        # return true if the trees are either same-isomorphic or cross-isomorphic.
         return (
             (Solution.is_isomorphic(root1.left, root2.left) and Solution.is_isomorphic(root1.right, root2.right))
             or
