dde8fd665c6af46df71cfab116c1b504b4b3601b
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-23 15:09:00 +0530

    EKO SPOJ

--- Practice Set 2/Searching & Sorting/EKO SPOJ.py
+++ Practice Set 2/Searching & Sorting/EKO SPOJ.py
@@ -12,15 +12,26 @@ class Solution:
     def eko(arr, k):
         if k < 0:
             return -1
+
+        # saw can be on ground to collect max wood or at max ht tree to collect no wood.
         low = 0
         high = max(arr)
         while low <= high:
             mid = int(low + (high - low)/2)
+
+            # get the wood amount collected at mid-ht saw.
             wood_collected = Solution._wood_collected(arr, mid)
+
+            # if wood collected is the required amount or greater than it, try increasing the ht of the saw to reduce
+            # wood destruction.
             if wood_collected >= k:
                 low = mid + 1
+
+            # if collected wood < w, we must cut more wood, thus reduce high.
             else:
                 high = mid - 1
+
+        # high would point to the correct answer.
         return high
 
 
