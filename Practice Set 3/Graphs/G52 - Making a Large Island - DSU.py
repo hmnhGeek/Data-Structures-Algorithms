4791f47e9c280b443fe82dc77ed4642f35922d52
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-28 11:10:00 +0530

    Making a large island using disjoint set

--- Practice Set 3/Graphs/G52 - Making a Large Island - DSU.py
+++ Practice Set 3/Graphs/G52 - Making a Large Island - DSU.py
@@ -33,3 +33,107 @@ class DisjointSet:
                 count += 1
         return count
 
+
+class Solution:
+    @staticmethod
+    def _get_neighbours(mtx, x, y, n, m):
+        neighbours = []
+        if 0 <= x - 1 < n and mtx[x - 1][y] == 1:
+            neighbours.append((x - 1, y))
+        if 0 <= y + 1 < m and mtx[x][y + 1] == 1:
+            neighbours.append((x, y + 1))
+        if 0 <= x + 1 < n and mtx[x + 1][y] == 1:
+            neighbours.append((x + 1, y))
+        if 0 <= y - 1 < m and mtx[x][y - 1] == 1:
+            neighbours.append((x, y - 1))
+        return neighbours
+
+    @staticmethod
+    def _connect_islands(mtx, disjoint_set, n, m):
+        for i in range(n):
+            for j in range(m):
+                if mtx[i][j] == 1:
+                    node = i * m + j
+                    neighbours = Solution._get_neighbours(mtx, i, j, n, m)
+                    for neighbour in neighbours:
+                        x, y = neighbour
+                        adj_node = x * m + y
+                        if not disjoint_set.in_same_component(node, adj_node):
+                            disjoint_set.union(node, adj_node)
+
+    @staticmethod
+    def make_large_island(mtx):
+        n, m = len(mtx), len(mtx[0])
+        disjoint_set = DisjointSet([i for i in range(n * m)])
+        Solution._connect_islands(mtx, disjoint_set, n, m)
+        hash_set = set()
+        largest_island_size = max(disjoint_set.sizes.values())
+        for i in range(n):
+            for j in range(m):
+                if mtx[i][j] == 0:
+                    neighbours = Solution._get_neighbours(mtx, i, j, n, m)
+                    for neighbour in neighbours:
+                        x, y = neighbour
+                        adj_node = x * m + y
+                        hash_set.add(disjoint_set.find_ultimate_parent(adj_node))
+                    island_size = 0
+                    for parent in hash_set:
+                        island_size += disjoint_set.sizes[parent]
+                    island_size += 1
+                    largest_island_size = max(largest_island_size, island_size)
+                    hash_set.clear()
+        return largest_island_size
+
+
+print(
+    Solution.make_large_island(
+        [
+            [1, 1],
+            [0, 1]
+        ]
+    )
+)
+
+print(
+    Solution.make_large_island(
+        [
+            [1, 0, 1],
+            [1, 0, 1],
+            [1, 0, 1]
+        ]
+    )
+)
+
+print(
+    Solution.make_large_island(
+        [
+            [1, 1, 0, 1, 1],
+            [1, 1, 0, 1, 1],
+            [1, 1, 0, 1, 1],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 1, 1],
+            [0, 0, 1, 1, 1]
+        ]
+    )
+)
+
+print(
+    Solution.make_large_island(
+        [
+            [1, 0, 1, 1, 0],
+            [1, 0, 0, 1, 0],
+            [0, 1, 1, 0, 1],
+            [1, 0, 1, 0, 1],
+            [0, 1, 0, 1, 0]
+        ]
+    )
+)
+
+print(
+    Solution.make_large_island(
+        [
+            [1, 1],
+            [1, 1]
+        ]
+    )
+)
