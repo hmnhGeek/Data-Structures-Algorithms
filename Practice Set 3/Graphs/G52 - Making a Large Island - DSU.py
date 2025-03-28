4217ac478c3ec9fad76d1c45ab92002d9b2bbda3
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-28 10:56:59 +0530

    Disjoint set by size

---
+++ Practice Set 3/Graphs/G52 - Making a Large Island - DSU.py
@@ -0,0 +1,35 @@
+class DisjointSet:
+    def __init__(self, nodes):
+        self.sizes = {i: 1 for i in nodes}
+        self.parents = {i: i for i in nodes}
+
+    def find_ultimate_parent(self, node):
+        if self.parents[node] == node:
+            return node
+        self.parents[node] = self.find_ultimate_parent(self.parents[node])
+        return self.parents[node]
+
+    def union(self, node1, node2):
+        ulp_node1 = self.find_ultimate_parent(node1)
+        ulp_node2 = self.find_ultimate_parent(node2)
+
+        if ulp_node1 == ulp_node2:
+            return
+
+        if self.sizes[ulp_node1] < self.sizes[ulp_node2]:
+            self.parents[ulp_node1] = ulp_node2
+            self.sizes[ulp_node2] += self.sizes[ulp_node1]
+        else:
+            self.parents[ulp_node2] = ulp_node1
+            self.sizes[ulp_node1] += self.sizes[ulp_node2]
+
+    def in_same_component(self, node1, node2):
+        return self.find_ultimate_parent(node1) == self.find_ultimate_parent(node2)
+
+    def num_components(self):
+        count = 0
+        for i in self.parents:
+            if i == self.parents[i]:
+                count += 1
+        return count
+
