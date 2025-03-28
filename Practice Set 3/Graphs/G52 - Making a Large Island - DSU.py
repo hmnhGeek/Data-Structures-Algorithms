f9f5bf738247109587659bf8d3545ec1a8e3a05e
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-28 11:13:30 +0530

    Making a large island using disjoint set

--- Practice Set 3/Graphs/G52 - Making a Large Island - DSU.py
+++ Practice Set 3/Graphs/G52 - Making a Large Island - DSU.py
@@ -64,23 +64,43 @@ class Solution:
     @staticmethod
     def make_large_island(mtx):
         n, m = len(mtx), len(mtx[0])
+
+        # define a disjoint set of size O(nm)
         disjoint_set = DisjointSet([i for i in range(n * m)])
+
+        # connect all the islands given in problem in O(nm) time.
         Solution._connect_islands(mtx, disjoint_set, n, m)
+
+        # define a hash set to store the ultimate parents of all the adjacent components of a 0-cell.
         hash_set = set()
+
+        # store the size of largest island.
         largest_island_size = max(disjoint_set.sizes.values())
+
+        # loop in the matrix in O(nm) time (it will not be O(nm) in avg case but only in worst case).
         for i in range(n):
             for j in range(m):
+                # if a 0-cell is found.
                 if mtx[i][j] == 0:
+                    # get the neighbouring cells of this 0-cell which are 1.
                     neighbours = Solution._get_neighbours(mtx, i, j, n, m)
+
+                    # loop in the neighbours and add their ultimate parents into the hash set.
                     for neighbour in neighbours:
                         x, y = neighbour
                         adj_node = x * m + y
                         hash_set.add(disjoint_set.find_ultimate_parent(adj_node))
+
+                    # get the island's size which will be formed if this 0-cell is added to the group of islands.
                     island_size = 0
                     for parent in hash_set:
                         island_size += disjoint_set.sizes[parent]
                     island_size += 1
+
+                    # update the largest island size.
                     largest_island_size = max(largest_island_size, island_size)
+
+                    # restore the hash set.
                     hash_set.clear()
         return largest_island_size
 
