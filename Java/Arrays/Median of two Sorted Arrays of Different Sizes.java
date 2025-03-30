0663d41efa22b422bf4a1839f113647e5f72383d
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-30 12:48:25 +0530

    Added more examples

--- Java/Arrays/Median of two Sorted Arrays of Different Sizes.java
+++ Java/Arrays/Median of two Sorted Arrays of Different Sizes.java
@@ -8,6 +8,9 @@ class Solution {
         System.out.println(getMedian(Arrays.asList(-5, 3, 6, 12, 15), Arrays.asList(-12, -10, -6, -3, 4, 10)));
         System.out.println(getMedian(Arrays.asList(1, 12, 15, 26, 38), Arrays.asList(2, 13, 17, 30, 45, 60)));
         System.out.println(getMedian(List.of(), Arrays.asList(2, 4, 5, 6)));
+        System.out.println(getMedian(Arrays.asList(2, 3, 5, 8), Arrays.asList(10, 12, 14, 16, 18, 20)));
+        System.out.println(getMedian(Arrays.asList(1, 3), List.of(2)));
+        System.out.println(getMedian(Arrays.asList(1, 2), Arrays.asList(3, 4)));
     }
 
     private static Double getMedian(List<Integer> arr1, List<Integer> arr2) {
