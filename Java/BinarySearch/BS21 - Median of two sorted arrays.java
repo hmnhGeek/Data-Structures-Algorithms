7a63e6d2eb9cf9b5f900c3f21512600d92524575
hmnhGeek <hmnhsharma97@gmail.com>
2025-03-31 13:20:11 +0530

    Median of two sorted arrays using binary search

--- Java/BinarySearch/BS21 - Median of two sorted arrays.java
+++ Java/BinarySearch/BS21 - Median of two sorted arrays.java
@@ -123,6 +123,36 @@ class BetterSolutionBS21 {
     }
 }
 
+class OptimalSolution {
+    public static Double getMedian(List<Integer> a, List<Integer> b) {
+        if (b.size() < a.size()) {
+            return getMedian(b, a);
+        }
+        int n = a.size() + b.size();
+        int low = 0, high = a.size();
+        int left = (n + 1)/2;
+        while (low <= high) {
+            int mid1 = (low + (high - low)/2);
+            int mid2 = left - mid1;
+            int l1 = 0 <= mid1 - 1 && mid1 - 1 < a.size() ? a.get(mid1 - 1) : Integer.MIN_VALUE;
+            int l2 = 0 <= mid2 - 1 && mid2 - 1 < b.size() ? b.get(mid2 - 1) : Integer.MIN_VALUE;
+            int r1 = 0 <= mid1 && mid1 < a.size() ? a.get(mid1) : Integer.MAX_VALUE;
+            int r2 = 0 <= mid2 && mid2 < b.size() ? b.get(mid2) : Integer.MAX_VALUE;
+            if (l1 > r2) {
+                high = mid1 - 1;
+            } else if (l2 > r1) {
+                low = mid1 + 1;
+            } else {
+                if (n % 2 == 0) {
+                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                }
+                return (double) Math.max(l1, l2);
+            }
+        }
+        return 0.0;
+    }
+}
+
 class Main {
     public static void main(String[] args) {
         System.out.println("Naive Solution.");
@@ -140,5 +170,13 @@ class Main {
         System.out.println(BetterSolutionBS21.getMedian(Arrays.asList(2, 3, 5, 8), Arrays.asList(10, 12, 14, 16, 18, 20)));
         System.out.println(BetterSolutionBS21.getMedian(Arrays.asList(1, 3), List.of(2)));
         System.out.println(BetterSolutionBS21.getMedian(Arrays.asList(1, 2), Arrays.asList(3, 4)));
+
+        System.out.println("Optimal Solution.");
+        System.out.println(OptimalSolution.getMedian(Arrays.asList(-5, 3, 6, 12, 15), Arrays.asList(-12, -10, -6, -3, 4, 10)));
+        System.out.println(OptimalSolution.getMedian(Arrays.asList(1, 12, 15, 26, 38), Arrays.asList(2, 13, 17, 30, 45, 60)));
+        System.out.println(OptimalSolution.getMedian(List.of(), Arrays.asList(2, 4, 5, 6)));
+        System.out.println(OptimalSolution.getMedian(Arrays.asList(2, 3, 5, 8), Arrays.asList(10, 12, 14, 16, 18, 20)));
+        System.out.println(OptimalSolution.getMedian(Arrays.asList(1, 3), List.of(2)));
+        System.out.println(OptimalSolution.getMedian(Arrays.asList(1, 2), Arrays.asList(3, 4)));
     }
 }
