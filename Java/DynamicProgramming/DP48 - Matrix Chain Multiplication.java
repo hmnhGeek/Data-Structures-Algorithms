f5a70da118139c8ba161715f4fbafe32b3e2c6f0
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-04 20:55:37 +0530

    Tabulation MCM

--- Java/DynamicProgramming/DP48 - Matrix Chain Multiplication.java
+++ Java/DynamicProgramming/DP48 - Matrix Chain Multiplication.java
@@ -14,9 +14,9 @@ class MCMRecursive {
         if (i.equals(j)) {
             return 0;
         }
-        Integer minValue = Integer.MAX_VALUE;
+        int minValue = Integer.MAX_VALUE;
         for (int k = i; k < j; k += 1) {
-            Integer cost = (arr.get(i - 1) * arr.get(k) * arr.get(j)) + MCMRecursive.solve(arr, i, k) + MCMRecursive.solve(arr, k + 1, j);
+            int cost = (arr.get(i - 1) * arr.get(k) * arr.get(j)) + MCMRecursive.solve(arr, i, k) + MCMRecursive.solve(arr, k + 1, j);
             minValue = Math.min(minValue, cost);
         }
         return minValue;
@@ -65,6 +65,40 @@ class MCMMemoized {
     }
 }
 
+class MCMTabulation {
+    /**
+     * Time complexity is O(n^3) and space complexity is O(n^2).
+     */
+
+    public static Integer getMinCostForMultiplication(List<Integer> dimensions) {
+        int n = dimensions.size();
+        Map<Integer, Map<Integer, Integer>> dp = new HashMap<>();
+        for (int i = 1; i < n; i += 1) {
+            Map<Integer, Integer> v = new HashMap<>();
+            for (int j = 1; j < n; j += 1) {
+                v.put(j, 0);
+            }
+            dp.put(i, v);
+        }
+        for (int i = n - 1; i >= 1; i -= 1) {
+            for (int j = 1; j <= n - 1; j += 1) {
+                if (i >= j) {
+                    continue;
+                }
+                int minValue = Integer.MAX_VALUE;
+                for (int k = i; k < j; k += 1) {
+                    int cost = (dimensions.get(i - 1) * dimensions.get(k) * dimensions.get(j)) + dp.get(i).get(k) + dp.get(k + 1).get(j);
+                    minValue = Math.min(minValue, cost);
+                }
+                Map<Integer, Integer> v = dp.get(i);
+                v.put(j, minValue);
+                dp.put(i, v);
+            }
+        }
+        return dp.get(1).get(n - 1);
+    }
+}
+
 class Main {
     public static void main(String[] args) {
         System.out.println("Recursive Solution");
@@ -84,5 +118,14 @@ class Main {
         System.out.println(MCMMemoized.getMinCostForMultiplication(Arrays.asList(1, 4, 3, 2)));
         System.out.println(MCMMemoized.getMinCostForMultiplication(Arrays.asList(2, 1, 3, 4)));
         System.out.println(MCMMemoized.getMinCostForMultiplication(Arrays.asList(1, 2, 3, 4, 3)));
+
+        System.out.println("Tabulation Solution");
+        System.out.println(MCMTabulation.getMinCostForMultiplication(Arrays.asList(10, 20, 30, 40, 50)));
+        System.out.println(MCMTabulation.getMinCostForMultiplication(Arrays.asList(10, 20, 30, 40)));
+        System.out.println(MCMTabulation.getMinCostForMultiplication(Arrays.asList(4, 5, 3, 2)));
+        System.out.println(MCMTabulation.getMinCostForMultiplication(Arrays.asList(10, 15, 20, 25)));
+        System.out.println(MCMTabulation.getMinCostForMultiplication(Arrays.asList(1, 4, 3, 2)));
+        System.out.println(MCMTabulation.getMinCostForMultiplication(Arrays.asList(2, 1, 3, 4)));
+        System.out.println(MCMTabulation.getMinCostForMultiplication(Arrays.asList(1, 2, 3, 4, 3)));
     }
 }
