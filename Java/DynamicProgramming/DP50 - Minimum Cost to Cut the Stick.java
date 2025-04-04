bfb44e632136e328e085a103f9dbe2c3a37faae0
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-04 21:38:52 +0530

    Utilized custom quick sort

--- Java/DynamicProgramming/DP50 - Minimum Cost to Cut the Stick.java
+++ Java/DynamicProgramming/DP50 - Minimum Cost to Cut the Stick.java
@@ -1,10 +1,48 @@
 package DynamicProgramming;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
+class QuickSort {
+    public static <T extends Comparable<T>> void sort(List<T> arr) {
+        QuickSort.sort(arr, 0, arr.size() - 1);
+    }
+
+    private static <T extends Comparable<T>> void sort(List<T> arr, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int partitionIndex = QuickSort.getPartitionIndex(arr, low, high);
+        QuickSort.sort(arr, low, partitionIndex - 1);
+        QuickSort.sort(arr, partitionIndex + 1, high);
+    }
+
+    private static <T extends Comparable<T>> Integer getPartitionIndex(List<T> arr, int low, int high) {
+        T pivot = arr.get(low);
+        int i = low, j = high;
+        while (i < j) {
+            while (arr.get(i).compareTo(pivot) <= 0 && i <= high - 1) {
+                i += 1;
+            }
+            while (arr.get(j).compareTo(pivot) > 0 && j >= low + 1) {
+                j -= 1;
+            }
+            if (i < j) {
+                Collections.swap(arr, i, j);
+            }
+        }
+        Collections.swap(arr, low, j);
+        return j;
+    }
+}
+
 class DP50Recursive {
+    /**
+     * Time complexity is exponential and space complexity is O(n).
+     */
+
     private static Integer solve(List<Integer> arr, Integer i, Integer j) {
         if (i > j) {
             return 0;
@@ -19,7 +57,7 @@ class DP50Recursive {
 
     public static Integer getMinCostToCutRod(List<Integer> arr, Integer length) {
         int n = arr.size();
-        arr.sort(null);
+        QuickSort.sort(arr);
         List<Integer> rod = Stream.of(List.of(0), arr, List.of(length)).flatMap(List::stream).toList();
         return DP50Recursive.solve(rod, 1, n);
     }
