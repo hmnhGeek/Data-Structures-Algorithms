754a10b7c430f4d7ade388e519d43fcd2471f126
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-06 08:15:46 +0530

    Median of two sorted arrays of different size.

--- Python/Practice Set 1/Arrays/Median of two sorted arrays of different size.py
+++ Python/Practice Set 1/Arrays/Median of two sorted arrays of different size.py
@@ -3,25 +3,41 @@ class Solution:
     def get_median(arr1, arr2):
         n1, n2 = len(arr1), len(arr2)
         if n2 < n1:
+            # always perform binary search on shorter array.
             return Solution.get_median(arr2, arr1)
+
+        # number elements on the left side.
         n = n1 + n2
         median_term = (n + 1)//2
+
+        # define the search space by using shorter array.
         low, high = 0, n1
         while low <= high:
+            # take mid1 number of elements from shorter array
             mid1 = int(low + (high - low)/2)
+
+            # take left - mid1 number of elements from the longer array.
             mid2 = median_term - mid1
+
+            # define l1, l2, r1 and r2.
             l1 = arr1[mid1 - 1] if 0 <= mid1 - 1 < n1 else -1e6
             l2 = arr2[mid2 - 1] if 0 <= mid2 - 1 < n2 else -1e6
             r1 = arr1[mid1] if 0 <= mid1 < n1 else 1e6
             r2 = arr2[mid2] if 0 <= mid2 < n2 else 1e6
+
+            # if l1 > r2, that means we have taken more from shorter array, reduce.
             if l1 > r2:
                 high = mid1 - 1
             elif l2 > r1:
+                # else, it means we have taken less from shorter array, increase.
                 low = mid1 + 1
             else:
+                # if the condition is valid, return the median
                 if n % 2 == 0:
                     return (max(l1, l2) + min(r1, r2))/2
                 return max(l1, l2)
+
+        # return dummy value.
         return -1
 
 
