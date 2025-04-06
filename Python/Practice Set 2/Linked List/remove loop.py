fb6e5750a6046b2c17dff48cd42ed44571da7f5a
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-06 10:28:51 +0530

    Created a method to detect loop in a linked list

--- Python/Practice Set 2/Linked List/remove loop.py
+++ Python/Practice Set 2/Linked List/remove loop.py
@@ -26,13 +26,26 @@ class LinkedList:
         while curr is not None:
             print(curr.data, end=" ")
             curr = curr.next
+        print()
 
     def build(self, *args):
         for i in args:
             self.push(i)
+
+    def has_loop(self):
+        slow = fast = self.head
+        while slow and fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
 
 
 # Example 1
 l1 = LinkedList()
 l1.build(1, 2, 3, 4, 5)
 l1.show()
+print(l1.has_loop())
+l1.tail.next = l1.head.next
+print(l1.has_loop())
