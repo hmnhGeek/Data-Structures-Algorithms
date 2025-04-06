99244b2b024e4588151369605a3e36c70b8f60d5
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-06 09:58:34 +0530

    Weighted Job scheduling using memoization and binary search

--- Python/Practice Set 3/Searching & Sorting/Weighted Job Scheduling.py
+++ Python/Practice Set 3/Searching & Sorting/Weighted Job Scheduling.py
@@ -37,6 +37,40 @@ class RecursiveSolution:
         return RecursiveSolution._find_max_profit(jobs, 0, n)
 
 
+class MemoizedSolution:
+    @staticmethod
+    def _find_next_possible_job_index(jobs: List[Job], index: int, n: int) -> int:
+        end_time_of_current_job = jobs[index].end_time
+        low, high = index + 1, n - 1
+        while low <= high:
+            mid = int(low + (high - low)/2)
+            if jobs[mid].start_time >= end_time_of_current_job:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return low
+
+    @staticmethod
+    def _find_max_profit(jobs: List[Job], index: int, n: int, dp) -> int:
+        if index >= n:
+            return 0
+        if dp[index] is not None:
+            return dp[index]
+        next_possible_job_index = MemoizedSolution._find_next_possible_job_index(jobs, index, n)
+        left = jobs[index].profit + MemoizedSolution._find_max_profit(jobs, next_possible_job_index, n, dp)
+        right = MemoizedSolution._find_max_profit(jobs, index + 1, n, dp)
+        dp[index] = max(left, right)
+        return dp[index]
+
+    @staticmethod
+    def schedule(jobs: List[Job]) -> int:
+        jobs.sort(key=lambda x: x.start_time)
+        n = len(jobs)
+        dp = {i: None for i in range(n)}
+        return MemoizedSolution._find_max_profit(jobs, 0, n, dp)
+
+
+print("Recursive Solution")
 print(
     RecursiveSolution.schedule(
         [
@@ -84,6 +118,63 @@ print(
 
 print(
     RecursiveSolution.schedule(
+        [
+            Job(1, 2, 5),
+            Job(1, 3, 6),
+            Job(1, 4, 4)
+        ]
+    )
+)
+
+print()
+print("Memoized Solution")
+print(
+    MemoizedSolution.schedule(
+        [
+            Job(1, 2, 50),
+            Job(3, 5, 20),
+            Job(6, 19, 100),
+            Job(2, 100, 200)
+        ]
+    )
+)
+
+print(
+    MemoizedSolution.schedule(
+        [
+            Job(1, 3, 60),
+            Job(2, 5, 50),
+            Job(4, 6, 70),
+            Job(5, 7, 30)
+        ]
+    )
+)
+
+print(
+    MemoizedSolution.schedule(
+        [
+            Job(1, 3, 50),
+            Job(2, 4, 10),
+            Job(3, 5, 40),
+            Job(3, 6, 70)
+        ]
+    )
+)
+
+print(
+    MemoizedSolution.schedule(
+        [
+            Job(1, 3, 20),
+            Job(2, 5, 20),
+            Job(3, 10, 100),
+            Job(4, 6, 70),
+            Job(6, 9, 60)
+        ]
+    )
+)
+
+print(
+    MemoizedSolution.schedule(
         [
             Job(1, 2, 5),
             Job(1, 3, 6),
