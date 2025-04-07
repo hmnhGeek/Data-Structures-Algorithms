b630c460256036f54533afce8fdf204d1cf614d5
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-07 11:15:04 +0530

    Most stones removed using DSU

--- Python/Practice Set 3/Graphs/G53 - Most Stones Removed with Same Row or Column - DSU.py
+++ Python/Practice Set 3/Graphs/G53 - Most Stones Removed with Same Row or Column - DSU.py
@@ -29,28 +29,42 @@ class DisjointSet:
 class Solution:
     @staticmethod
     def remove_stones(stones):
+        # get the max row and max column indices using a for loop in O(n) time.
         max_row, max_col = -1e6, -1e6
         for stone in stones:
             max_row = max(max_row, stone[0])
             max_col = max(max_col, stone[1])
 
+        # create the disjoint set of size O(m), where m denotes `max_row + max_col` order.
         nodes = [i for i in range(max_row + 1)] + [j + max_row + 1 for j in range(max_col + 1)]
         disjoint_set = DisjointSet(nodes)
 
+        # create a set to store the rows and columns (or nodes in terms of disjoint set) which have at least one
+        # stone in them.
         nodes_having_stones = set()
+
+        # now loop on all the stones in O(n) time.
         for stone in stones:
+            # get the nodes and union them.
             node1, node2 = stone[0], stone[1] + max_row + 1
             disjoint_set.union(node1, node2)
+
+            # add these nodes into the set also.
             nodes_having_stones.add(node1)
             nodes_having_stones.add(node2)
 
+        # count the number of components using only the nodes which had at least one stone in them.
         num_components = 0
-        for node in disjoint_set.parents:
+        for node in nodes_having_stones:
             if disjoint_set.parents[node] == node:
                 num_components += 1
+
+        # return the max possible stones count that can be removed.
         return len(stones) - num_components
 
 
 print(Solution.remove_stones([[0, 0], [0, 1], [1, 0], [1, 2], [2, 1], [2, 2]]))
 print(Solution.remove_stones([[0, 0], [0, 2], [1, 1], [2, 0], [2, 2]]))
 print(Solution.remove_stones([[0, 0]]))
+print(Solution.remove_stones([[0, 1], [1, 0], [0, 0]]))
+print(Solution.remove_stones([[2, 0], [2, 1], [3, 1], [3, 2], [5, 5]]))
