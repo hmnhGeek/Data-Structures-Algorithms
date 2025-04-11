5c5d894ee4634e97600f0ae6a92be2b416776bd0
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-11 17:06:05 +0530

    Median of two sorted arrays of different size.

--- Python/Practice Set 3/Binary Search/BS21 - Median of two Sorted Arrays of Different Sizes.py
+++ Python/Practice Set 3/Binary Search/BS21 - Median of two Sorted Arrays of Different Sizes.py
@@ -2,10 +2,16 @@ class Solution:
     @staticmethod
     def get_median(arr1, arr2):
         n1, n2 = len(arr1), len(arr2)
+
+        # always perform binary search on the shorter array.
         if n1 > n2:
             return Solution.get_median(arr2, arr1)
+
+        # find how many elements should be present on the left.
         n = n1 + n2
         left = (n + 1)//2
+
+        # pick no one from a1 or pick all from a1.
         low, high = 0, n1
         while low <= high:
             mid1 = int(low + (high - low)/2)
@@ -14,10 +20,16 @@ class Solution:
             l2 = arr2[mid2 - 1] if 0 <= mid2 - 1 < n2 else -1e6
             r1 = arr1[mid1] if 0 <= mid1 < n1 else 1e6
             r2 = arr2[mid2] if 0 <= mid2 < n2 else 1e6
+
+            # if l1 > r2, we have picked more from a1, reduce!
             if l1 > r2:
                 high = mid1 - 1
+
+            # if l2 > r1, we have picked more from a2, increase from a1.
             elif l2 > r1:
                 low = mid1 + 1
+
+            # else if the condition is valid, return the median.
             else:
                 if n % 2 == 0:
                     return (max(l1, l2) + min(r1, r2))/2
