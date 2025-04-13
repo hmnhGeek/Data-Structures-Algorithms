28448345543c67ddf585a908dac70d3b58ba16be
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-13 21:02:29 +0530

    First node in a loop

--- Java/LinkedLists/Problem5/Find First Node of Loop in Linked List.java
+++ Java/LinkedLists/Problem5/Find First Node of Loop in Linked List.java
@@ -1,6 +1,8 @@
 package LinkedLists.Problem5;
 
 
+import java.util.Arrays;
+
 class Node<T> {
     private T data;
     private Node<T> next;
@@ -33,6 +35,14 @@ class LinkedList<T> {
         this.length = 0;
     }
 
+    public Node<T> getHead() {
+        return head;
+    }
+
+    public Node<T> getTail() {
+        return tail;
+    }
+
     public boolean isEmpty() {
         return this.length.equals(0);
     }
@@ -47,5 +57,38 @@ class LinkedList<T> {
             this.tail = node;
         }
         this.length += 1;
+    }
+
+    public void build(T... args) {
+        Arrays.stream(args).toList().forEach(this::push);
+    }
+}
+
+
+class Solution {
+    public static void main(String[] args) {
+        // Example 1
+        LinkedList<Integer> linkedList1 = new LinkedList<>();
+        linkedList1.build(1, 3, 2, 4, 5);
+        System.out.println(getLoopStartNode(linkedList1));
+        linkedList1.getTail().setNext(linkedList1.getHead().getNext());
+        System.out.println(getLoopStartNode(linkedList1).getData());
+    }
+
+    public static <T> Node<T> getLoopStartNode(LinkedList<T> linkedList) {
+        Node<T> slow = linkedList.getHead(), fast = linkedList.getHead();
+        while (slow != null && fast != null && fast.getNext() != null) {
+            slow = slow.getNext();
+            fast = fast.getNext().getNext();
+            if (slow == fast) {
+                slow = linkedList.getHead();
+                while (slow != fast) {
+                    slow = slow.getNext();
+                    fast = fast.getNext();
+                }
+                return slow;
+            }
+        }
+        return null;
     }
 }
