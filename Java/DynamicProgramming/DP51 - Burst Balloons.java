959dcb7fbd5d948260003c4e7a38846637191b7a
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-14 16:33:18 +0530

    Tabulation Burst Balloons

--- Java/DynamicProgramming/DP51 - Burst Balloons.java
+++ Java/DynamicProgramming/DP51 - Burst Balloons.java
@@ -91,3 +91,57 @@ class MemoizedDP51 {
         System.out.println(getMaximumCoins(Arrays.asList(1, 5)));
     }
 }
+
+class TabulationDP51 {
+    /**
+     * Time complexity is O(n^3) and space complexity is O(n^2).
+     */
+    public static Integer getMaximumCoins(List<Integer> balloons) {
+        int n = balloons.size();
+        List<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.addAll(balloons);
+        arr.add(1);
+
+        Map<Integer, Map<Integer, Integer>> dp = new HashMap<>();
+        for (int i = 0; i <= n + 1; i += 1) {
+            Map<Integer, Integer> submap = new HashMap<>();
+            for (int j = 0; j <= n + 1; j += 1) {
+                if (i > j) {
+                    submap.put(j, 0);
+                } else {
+                    submap.put(j, Integer.MIN_VALUE);
+                }
+            }
+            dp.put(i, submap);
+        }
+
+        for (int i = n; i >= 1; i -= 1) {
+            for (int j = 1; j <= n; j += 1) {
+                if (i > j) {
+                    continue;
+                }
+                Integer maxCoins = Integer.MIN_VALUE;
+                for (int index = i; index <= j; index += 1) {
+                    Integer coins = arr.get(i - 1) * arr.get(index) * arr.get(j + 1) + dp.get(i).get(index - 1) + dp.get(index + 1).get(j);
+                    maxCoins = Math.max(coins, maxCoins);
+                }
+                Map<Integer, Integer> mp = dp.get(i);
+                mp.put(j, maxCoins);
+                dp.put(i, mp);
+            }
+        }
+
+        return dp.get(1).get(n);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMaximumCoins(Arrays.asList(3, 1, 5, 8)));
+        System.out.println(getMaximumCoins(Arrays.asList(7, 1, 8)));
+        System.out.println(getMaximumCoins(Arrays.asList(9, 1)));
+        System.out.println(getMaximumCoins(Arrays.asList(1, 2, 3, 4, 5)));
+        System.out.println(getMaximumCoins(Arrays.asList(1, 5, 2, 8)));
+        System.out.println(getMaximumCoins(Arrays.asList(5, 10)));
+        System.out.println(getMaximumCoins(Arrays.asList(1, 5)));
+    }
+}
