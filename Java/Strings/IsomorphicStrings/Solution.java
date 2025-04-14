b21a1e13cf353f46de018c19a645bdf84fba4a32
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-14 21:08:52 +0530

    Isomorphic Strings

--- Java/Strings/IsomorphicStrings/Solution.java
+++ Java/Strings/IsomorphicStrings/Solution.java
@@ -1,3 +1,7 @@
+// Problem link - https://www.geeksforgeeks.org/problems/isomorphic-strings-1587115620/1
+// Solution - https://www.youtube.com/watch?v=1UNjU3YMUCs
+
+
 package Strings.IsomorphicStrings;
 
 import java.util.HashMap;
@@ -11,6 +15,9 @@ public class Solution {
     }
 
     public static Boolean areIsomorphic(String s1, String s2) {
+        /*
+            Time complexity is O(n) and space complexity is O(n).
+         */
         if (s1.length() != s2.length()) return false;
         Map<Character, Character> map = new HashMap<>();
         for (int i = 0; i < s1.length(); i += 1) {
