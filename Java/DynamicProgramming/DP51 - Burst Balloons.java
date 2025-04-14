2d1eb187640e79ead07b474b15a73ae16890a3f2
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-14 16:29:37 +0530

    Memoized Burst Balloons

--- Java/DynamicProgramming/DP51 - Burst Balloons.java
+++ Java/DynamicProgramming/DP51 - Burst Balloons.java
@@ -1,9 +1,7 @@
 package DynamicProgramming;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 
 class RecursiveDP51 {
@@ -29,6 +27,58 @@ class RecursiveDP51 {
         arr.addAll(balloons);
         arr.add(1);
         return solve(arr, 1, n);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMaximumCoins(Arrays.asList(3, 1, 5, 8)));
+        System.out.println(getMaximumCoins(Arrays.asList(7, 1, 8)));
+        System.out.println(getMaximumCoins(Arrays.asList(9, 1)));
+        System.out.println(getMaximumCoins(Arrays.asList(1, 2, 3, 4, 5)));
+        System.out.println(getMaximumCoins(Arrays.asList(1, 5, 2, 8)));
+        System.out.println(getMaximumCoins(Arrays.asList(5, 10)));
+        System.out.println(getMaximumCoins(Arrays.asList(1, 5)));
+    }
+}
+
+class MemoizedDP51 {
+    /**
+     * Time complexity is O(n^3) and space complexity is O(n + n^2).
+     */
+    private static Integer solve(List<Integer> arr, Integer i, Integer j, Map<Integer, Map<Integer, Integer>> dp) {
+        if (i > j) {
+            return 0;
+        }
+        if (dp.get(i).get(j) != null) {
+            return dp.get(i).get(j);
+        }
+        Integer maxCoins = Integer.MIN_VALUE;
+        for (int index = i; index <= j; index += 1) {
+            Integer coins = arr.get(i - 1) * arr.get(index) * arr.get(j + 1) + solve(arr, i, index - 1, dp) + solve(arr, index + 1, j, dp);
+            maxCoins = Math.max(coins, maxCoins);
+        }
+        Map<Integer, Integer> mp = dp.get(i);
+        mp.put(j, maxCoins);
+        dp.put(i, mp);
+        return dp.get(i).get(j);
+    }
+
+    public static Integer getMaximumCoins(List<Integer> balloons) {
+        int n = balloons.size();
+        List<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.addAll(balloons);
+        arr.add(1);
+
+        Map<Integer, Map<Integer, Integer>> dp = new HashMap<>();
+        for (int i = 1; i <= n; i += 1) {
+            Map<Integer, Integer> submap = new HashMap<>();
+            for (int j = 1; j <= n; j += 1) {
+                submap.put(j, null);
+            }
+            dp.put(i, submap);
+        }
+
+        return solve(arr, 1, n, dp);
     }
 
     public static void main(String[] args) {
