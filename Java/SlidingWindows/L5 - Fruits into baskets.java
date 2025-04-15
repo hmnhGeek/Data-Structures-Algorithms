f94e4903d104ffef65b43bca19dad8daa977affa
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-15 16:59:09 +0530

    Fruits into baskets

--- Java/SlidingWindows/L5 - Fruits into baskets.java
+++ Java/SlidingWindows/L5 - Fruits into baskets.java
@@ -15,15 +15,26 @@ class SolutionL5 {
     }
 
     private static <T> List<T> getMaxFruitsCollected(List<T> trees, Integer numBasketsAllowed) {
+        // get the window variables
         int n = trees.size();
         int left = 0, right = 0;
+
+        // get the result variables
         Integer startIndex = -1;
         int numCollected = 0;
+
+        // define the tracking variable
         Map<T, Integer> baskets = new HashMap<>();
 
+        // while there is ground to cover...
         while (right < n) {
+            // get the element at right index to add.
             T fruitToAdd = trees.get(right);
+
+            // increment the count of this element in the baskets variable.
             baskets.put(fruitToAdd, baskets.getOrDefault(fruitToAdd, 0) + 1);
+
+            // if we have breached the basket's allowed size, decrement 1 unit from left.
             if (baskets.size() > numBasketsAllowed) {
                 T fruitToRemove = trees.get(left);
                 baskets.put(fruitToRemove, baskets.get(fruitToRemove) - 1);
@@ -32,15 +43,23 @@ class SolutionL5 {
                 }
                 left += 1;
             }
+
+            // else update the resultant variables.
             if (baskets.size() <= numBasketsAllowed) {
                 numCollected = Math.max(numCollected, right - left + 1);
                 startIndex = left;
             }
+
+            // increment right index.
             right += 1;
         }
+
+        // return the sublist if start index is not -1.
         if (startIndex != -1) {
             return trees.subList(startIndex, startIndex + numCollected);
         }
+
+        // else return a blank list.
         return new ArrayList<>();
     }
 }
