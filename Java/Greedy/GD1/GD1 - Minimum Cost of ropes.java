df48cb5fbe49fd3a944ac87dd039623c0f0289bb
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-15 19:58:21 +0530

    Minimum cost to connect two ropes

--- Java/Greedy/GD1/GD1 - Minimum Cost of ropes.java
+++ Java/Greedy/GD1/GD1 - Minimum Cost of ropes.java
@@ -65,15 +65,29 @@ class MinHeap<T extends Comparable<T>> implements Heap<T> {
 class SolutionGD1 {
     public static void main(String[] args) {
         System.out.println(getMinCostToConnectRopes(Arrays.asList(4, 3, 2, 6)));
+        System.out.println(getMinCostToConnectRopes(Arrays.asList(4, 2, 7, 6, 9)));
+        System.out.println(getMinCostToConnectRopes(List.of(10)));
     }
 
     public static Integer getMinCostToConnectRopes(List<Integer> ropes) {
+        /*
+            Time complexity is O(n * log(n)) and space complexity is O(n).
+         */
+
+        // create a min heap and push all the `n` ropes into it in O(n * log(n)) time.
         MinHeap<Integer> minHeap = new MinHeap<>();
         ropes.forEach(minHeap::insert);
+
+        // define a cost variable which will store the total cost to connect all the ropes.
         Integer cost = 0;
+
+        // until there is just one rope in the min heap, i.e., it will run for n-iterations...
         while (minHeap.getHeap().size() != 1) {
+            // get the first and the second-smallest ropes.
             Integer firstRope = minHeap.pop(), secondRope = minHeap.pop();
+            // update the cost.
             cost += (firstRope + secondRope);
+            // push back into the min heap in O(n * log(n)) time.
             minHeap.insert(firstRope + secondRope);
         }
         return cost;
