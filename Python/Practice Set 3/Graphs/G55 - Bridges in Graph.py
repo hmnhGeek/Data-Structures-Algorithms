85caa79c5de55bfbc29796e2839eecaa4daa4d56
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-16 14:37:43 +0530

    Bridges in a graph

--- Python/Practice Set 3/Graphs/G55 - Bridges in Graph.py
+++ Python/Practice Set 3/Graphs/G55 - Bridges in Graph.py
@@ -3,30 +3,50 @@ class Solution:
 
     @staticmethod
     def _dfs(graph, node, parent, tin, low, visited, bridges):
+        # mark the node as visited
         visited[node] = True
+
+        # assign the latest time and increment the timer.
         tin[node] = low[node] = Solution._timer
         Solution._timer += 1
+
+        # loop on the adjacent nodes.
         for adj_node in graph[node]:
+            # if the adjacent node is the parent itself, do nothing.
             if adj_node == parent:
                 continue
+
+            # if the adjacent node is not yet visited, perform a dfs on the adjacent node.
             elif not visited[adj_node]:
                 Solution._dfs(graph, adj_node, node, tin, low, visited, bridges)
+
+                # once the dfs on the adjacent node is completed, make the node inherit the lowest time from it.
                 low[node] = min(low[node], low[adj_node])
+
+                # if low time of adjacent node is more than low time of node, then it means we cannot reach this
+                # adjacent node except via node. Thus, this edge is a bridge.
                 if low[node] < low[adj_node]:
                     bridges.append((node, adj_node))
             else:
+                # else if the adjacent node is already visited, simply update the lowest time of the node.
                 low[node] = min(low[node], low[adj_node])
 
     @staticmethod
     def get_bridges(graph):
         Solution._timer = 1
+
+        # create tracking variables
         tin = {i: -1 for i in graph}
         low = {i: -1 for i in graph}
         visited = {i: False for i in graph}
+
+        # create list which will store the bridges and update it using DFS.
         bridges = []
         for node in graph:
             if not visited[node]:
                 Solution._dfs(graph, node, None, tin, low, visited, bridges)
+
+        # return the bridges.
         return bridges
 
 
