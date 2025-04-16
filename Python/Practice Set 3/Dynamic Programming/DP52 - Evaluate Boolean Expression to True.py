6e7b98b9e17597a8dfe98dafa087d257064b7727
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-16 11:59:12 +0530

    Evaluate boolean expression to true using tabulation

--- Python/Practice Set 3/Dynamic Programming/DP52 - Evaluate Boolean Expression to True.py
+++ Python/Practice Set 3/Dynamic Programming/DP52 - Evaluate Boolean Expression to True.py
@@ -81,7 +81,8 @@ def memoized():
                     num_ways += (left_true * right_false + right_true * left_false)
                 else:
                     num_ways += (left_true * right_true + left_false * right_false)
-        return num_ways
+        dp[i][j][evaluate_to] = num_ways
+        return dp[i][j][evaluate_to]
 
     def evaluate(expression):
         n = len(expression)
@@ -95,7 +96,55 @@ def memoized():
     print(evaluate("T^F|F"))
 
 
+def tabulation():
+    """
+        Time complexity is O(n^3) and space complexity is O(n^2).
+    """
+    def evaluate(expression):
+        n = len(expression)
+        dp = {i: {j: {True: 0, False: 0} for j in range(n)} for i in range(n)}
+        for i in dp:
+            dp[i][i][True] = 1 if expression[i] == 'T' else 0
+            dp[i][i][False] = 1 if expression[i] == 'F' else 0
+        for i in range(n - 1, -1, -1):
+            for j in range(n):
+                if i >= j:
+                    continue
+                for evaluate_to in [True, False]:
+                    num_ways = 0
+                    for index in range(i + 1, j, 2):
+                        operator = expression[index]
+                        left_true = dp[i][index - 1][True]
+                        left_false = dp[i][index - 1][False]
+                        right_true = dp[index + 1][j][True]
+                        right_false = dp[index + 1][j][False]
+                        if operator == "&":
+                            if evaluate_to:
+                                num_ways += (left_true * right_true)
+                            else:
+                                num_ways += (left_true * right_false + right_true * left_false + left_false * right_false)
+                        elif operator == "|":
+                            if evaluate_to:
+                                num_ways += (left_true * right_true + left_true * right_false + left_false * right_true)
+                            else:
+                                num_ways += (left_false * right_false)
+                        else:
+                            if evaluate_to:
+                                num_ways += (left_true * right_false + right_true * left_false)
+                            else:
+                                num_ways += (left_true * right_true + left_false * right_false)
+                    dp[i][j][evaluate_to] = num_ways
+        return dp[0][n - 1][True]
+
+    print(evaluate("F|T^F"))
+    print(evaluate("T|T&F"))
+    print(evaluate("T^T^F"))
+    print(evaluate("T|T&F^T"))
+    print(evaluate("T^F|F"))
+
+
 recursive()
 print()
 memoized()
 print()
+tabulation()
