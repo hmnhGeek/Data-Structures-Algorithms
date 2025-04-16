97f2fed98a9a0cb87d11677bca85d32acba6d359
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-16 11:25:08 +0530

    Burst balloons using tabulation

--- Python/Practice Set 3/Dynamic Programming/DP51 - Burst Balloons.py
+++ Python/Practice Set 3/Dynamic Programming/DP51 - Burst Balloons.py
@@ -56,6 +56,36 @@ def memoized():
     print(burst_balloons([1, 5]))
 
 
+def tabulation():
+    """
+        Time complexity is O(n^3) and space complexity is O(n^2).
+    """
+    def burst_balloons(arr):
+        n = len(arr)
+        arr = [1] + arr + [1]
+        dp = {i: {j: 0 for j in range(n + 2)} for i in range(n + 2)}
+        for i in range(n, 0, -1):
+            for j in range(1, n + 1):
+                if i > j:
+                    continue
+                max_coins_collected = 0
+                for index in range(i, j + 1):
+                    coins = arr[i - 1] * arr[index] * arr[j + 1] + dp[i][index - 1] + dp[index + 1][j]
+                    max_coins_collected = max(max_coins_collected, coins)
+                dp[i][j] = max_coins_collected
+        return dp[1][n]
+
+    print(burst_balloons([3, 1, 5, 8]))
+    print(burst_balloons([7, 1, 8]))
+    print(burst_balloons([9, 1]))
+    print(burst_balloons([1, 2, 3, 4, 5]))
+    print(burst_balloons([1, 5, 2, 8]))
+    print(burst_balloons([5, 10]))
+    print(burst_balloons([1, 5]))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
