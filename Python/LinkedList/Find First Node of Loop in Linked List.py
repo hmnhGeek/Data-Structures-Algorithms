a783fd8cf0001c7661dfe227bcf1cbc94c5d0a0e
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-17 21:40:59 +0530

    Implemented a linked list class

---
+++ Python/LinkedList/Find First Node of Loop in Linked List.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = self.tail = None
+        self.length = 0
+
+    def is_empty(self):
+        return self.length == 0
+
+    def push(self, x):
+        node = Node(x)
+        if self.is_empty():
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+        self.length += 1
+
+    def pop(self):
+        if self.is_empty():
+            return
+        item = self.head.data
+        node = self.head
+        self.head = self.head.next
+        del node
+        return item
+
+    def build(self, *args):
+        for i in args:
+            self.push(i)
+
+    def __str__(self):
+        if self.length == 0:
+            return "[]"
+        if self.length == 1:
+            return f"[{self.head.data}]"
+
+        result = f"[{self.head.data}, "
+        curr = self.head.next
+        while curr and curr != self.tail:
+            result += f"{curr.data}, "
+            curr = curr.next
+        result += f"{self.tail.data}]"
+        return result
+
