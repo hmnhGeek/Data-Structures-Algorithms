981a4f896a1efb974b22b66a645da221f485311b
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-17 11:15:12 +0530

    Search an element in a 2D Matrix

--- Python/Practice Set 4/Matrix/Search in 2D Matrix.py
+++ Python/Practice Set 4/Matrix/Search in 2D Matrix.py
@@ -1,6 +1,13 @@
+# Problem link - https://www.naukri.com/code360/problems/search-in-a-2d-matrix_980531
+
+
 class Solution:
     @staticmethod
     def search(mtx, element):
+        """
+            Time complexity is O(log(nm)) and space complexity is O(1).
+        """
+
         n, m = len(mtx), len(mtx[0])
         low, high = 0, n*m - 1
         while low <= high:
