e5d85466d9d1cec73add8ed893661939d97c3e91
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-18 17:43:13 +0530

    Binary Tree to BST

--- Python/Practice Set 1/Binary Search Tree/Binary Tree to BST.py
+++ Python/Practice Set 1/Binary Search Tree/Binary Tree to BST.py
@@ -1,3 +1,31 @@
+class QuickSort:
+    @staticmethod
+    def sort(arr):
+        QuickSort._sort(arr, 0, len(arr) - 1)
+
+    @staticmethod
+    def _sort(arr, low, high):
+        if low >= high:
+            return
+        partition_index = QuickSort._get_partition_index(arr, low, high)
+        QuickSort._sort(arr, low, partition_index - 1)
+        QuickSort._sort(arr, partition_index + 1, high)
+
+    @staticmethod
+    def _get_partition_index(arr, low, high):
+        pivot = arr[low]
+        i, j = low, high
+        while i < j:
+            while arr[i] <= pivot and i <= high - 1:
+                i += 1
+            while arr[j] > pivot and j >= low + 1:
+                j -= 1
+            if i < j:
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[low], arr[j] = arr[j], arr[low]
+        return j
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -29,7 +57,7 @@ class Solution:
 
         # sort the inorder nodes data in O(n * log(n)) time.
         sorted_inorder = [i.data for i in inorder]
-        sorted_inorder.sort()
+        QuickSort.sort(sorted_inorder)
 
         # now assign the inorder nodes with data from sorted inorder list in O(n) time.
         for i in range(len(inorder)):
