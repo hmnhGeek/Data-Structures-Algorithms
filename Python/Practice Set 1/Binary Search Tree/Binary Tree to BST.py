531e042ccdbfc2aac00d2a5fb44e5a35e8e2165e
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-18 17:37:58 +0530

    Binary Tree to BST

--- Python/Practice Set 1/Binary Search Tree/Binary Tree to BST.py
+++ Python/Practice Set 1/Binary Search Tree/Binary Tree to BST.py
@@ -32,3 +32,28 @@ class Solution:
             Solution._get_inorder(root.left, inorder)
             inorder.append(root)
             Solution._get_inorder(root.right, inorder)
+
+
+# Example 1
+n1, n2, n3 = Node(1), Node(2), Node(3)
+n1.left = n2
+n1.right = n3
+Solution.convert_to_bst(n1)
+Solution.show_tree(n1)
+
+# Example 2
+n1, n2, n3, n4 = Node(1), Node(2), Node(3), Node(4)
+n1.left = n2
+n2.left = n4
+n1.right = n3
+Solution.convert_to_bst(n1)
+Solution.show_tree(n1)
+
+# Example 3
+n10, n2, n7, n8, n4 = Node(10), Node(2), Node(7), Node(8), Node(4)
+n10.left = n2
+n10.right = n7
+n2.left = n8
+n2.right = n4
+Solution.convert_to_bst(n10)
+Solution.show_tree(n10)
