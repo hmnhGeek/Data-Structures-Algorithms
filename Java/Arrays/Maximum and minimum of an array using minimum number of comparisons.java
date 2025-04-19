d62b560cf8595f4193ee4aa78dbf3152c5bd2f6b
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-19 20:32:58 +0530

    Maximum and minimum of an array using minimum number of comparisons

--- Java/Arrays/Maximum and minimum of an array using minimum number of comparisons.java
+++ Java/Arrays/Maximum and minimum of an array using minimum number of comparisons.java
@@ -1,3 +1,5 @@
+// Problem link - https://www.geeksforgeeks.org/maximum-and-minimum-in-an-array/
+
 package Arrays;
 
 import java.util.Arrays;
@@ -7,6 +9,10 @@ import java.util.Map;
 
 class Solution2 {
     public static Map<String, Integer> getMaxMin(List<Integer> arr) {
+        /*
+            Time complexity is O(n) and space complexity is O(1).
+         */
+
         int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
         for (int i = 0; i < arr.size(); i += 1) {
             min = Math.min(min, arr.get(i));
@@ -21,5 +27,6 @@ class Solution2 {
     public static void main(String[] args) {
         System.out.println(Solution2.getMaxMin(Arrays.asList(3, 5, 4, 1, 9)));
         System.out.println(Solution2.getMaxMin(Arrays.asList(22, 14, 8, 17, 35, 3)));
+        System.out.println(Solution2.getMaxMin(Arrays.asList(2, 1, 4, 6, 8, 8, 6)));
     }
 }
