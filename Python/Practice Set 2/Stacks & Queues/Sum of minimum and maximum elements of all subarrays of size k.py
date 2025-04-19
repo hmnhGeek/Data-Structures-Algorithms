97ccb6ba55a150b602e46dba030df8aab6f0cdd2
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-19 19:51:25 +0530

    Sliding window minimum using deque

--- Python/Practice Set 2/Stacks & Queues/Sum of minimum and maximum elements of all subarrays of size k.py
+++ Python/Practice Set 2/Stacks & Queues/Sum of minimum and maximum elements of all subarrays of size k.py
@@ -57,6 +57,10 @@ class Deque:
 class SlidingWindowUtils:
     @staticmethod
     def get_max(arr, k):
+        """
+            Time complexity is O(n) and space complexity is O(n).
+        """
+
         # edge case
         if k <= 0:
             return
@@ -99,6 +103,52 @@ class SlidingWindowUtils:
         # return the final result.
         return result
 
+    @staticmethod
+    def get_min(arr, k):
+        """
+            Time complexity is O(n) and space complexity is O(n).
+        """
 
-print(SlidingWindowUtils.get_max([1, 3, -1, -3, 5, 3, 2, 1, 6], 3))
-print(SlidingWindowUtils.get_max([2, 5, -1, 7, -3, -1, -2], 4))
+        # edge case
+        if k <= 0:
+            return
+        n = len(arr)
+
+        # create a blank deque
+        deque = Deque()
+
+        # create a result array.
+        result = []
+
+        # loop just on the first window.
+        for i in range(k):
+            elem = arr[i]
+
+            # continuously pop back from the deque to maintain a linearly ascending order.
+            while deque.length != 0 and deque.back() > elem:
+                deque.pop_back()
+
+            # once the last element of deque is <= current element, push the current element.
+            deque.push(elem)
+
+        # append the max value of first window.
+        result.append(deque.front())
+
+        # now loop on the remaining indices...
+        for i in range(k, n):
+            # if the (i - k)th element of the array (which is being removed), is the maximum element, then pop from the
+            # front of the deque.
+            if arr[i - k] == deque.front():
+                deque.pop_front()
+
+            # do the same as we did for the first window.
+            elem = arr[i]
+            while deque.length != 0 and deque.back() > elem:
+                deque.pop_back()
+            deque.push(elem)
+            result.append(deque.front())
+
+        # return the final result.
+        return result
+
+
