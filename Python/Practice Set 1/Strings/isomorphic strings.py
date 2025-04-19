4bc9d1bce1602a0a9314c5bb672fec6da6248c97
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-19 20:10:09 +0530

    Isomorphic strings

--- Python/Practice Set 1/Strings/isomorphic strings.py
+++ Python/Practice Set 1/Strings/isomorphic strings.py
@@ -1,15 +1,24 @@
 class Solution:
     @staticmethod
     def are_isomorphic(s1, s2):
+        # if the length of the strings are not same, return false.
         if len(s1) != len(s2):
             return False
+
+        # initialize a map to store characters.
         mp = {}
         for i in range(len(s1)):
             ss = s1[i]
             tt = s2[i]
+
+            # if the mp[ss] != tt or tt is present already as a value but not for this ss key, then return false.
             if (ss in mp and mp[ss] != tt) or (ss not in mp and tt in mp.values()):
                 return False
+
+            # ss --> tt
             mp[ss] = tt
+
+        # return True finally.
         return True
 
 
