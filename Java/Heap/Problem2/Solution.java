81ae1fc01098edee3b411da2e33093897102ddf7
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-20 14:05:34 +0530

    Created min heap class

--- Java/Heap/Problem2/Solution.java
+++ Java/Heap/Problem2/Solution.java
@@ -1,3 +1,5 @@
+// Problem link - https://www.geeksforgeeks.org/heap-sort/
+
 package Heap.Problem2;
 
 import java.util.ArrayList;
@@ -6,6 +8,9 @@ import java.util.List;
 
 public class Solution {
     public static <T extends Comparable<T>> List<T> heapSort(List<T> arr) {
+        /*
+            Time complexity is O(n * log(n)) and space complexity is O(n).
+         */
         MinHeap<T> minHeap = new MinHeap<>();
         arr.forEach(minHeap::insert);
         List<T> result = new ArrayList<>();
@@ -17,5 +22,6 @@ public class Solution {
 
     public static void main(String[] args) {
         System.out.println(heapSort(Arrays.asList(9, 4, 3, 8, 10, 2, 5)));
+        System.out.println(heapSort(Arrays.asList(2, 9, 0, 4, 7, 2, 3, 9, 0, 7, 5, 3, 2, 4)));
     }
 }
