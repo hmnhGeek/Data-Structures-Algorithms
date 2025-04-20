dd6adb7c7a39f0e2dd7997a234f831dce5415f17
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-20 13:41:13 +0530

    Get height of a binary tree

--- Java/BinaryTrees/Problem3/Height of Binary Tree.java
+++ Java/BinaryTrees/Problem3/Height of Binary Tree.java
@@ -1,3 +1,6 @@
+// Problem link - https://www.geeksforgeeks.org/problems/height-of-binary-tree/1
+
+
 package BinaryTrees.Problem3;
 
 
@@ -35,9 +38,15 @@ class Node<T> {
 
 class Solution {
     public static <T> Integer getHeight(Node<T> root) {
+        /*
+            Time complexity is O(n) and space complexity is O(n).
+         */
+
+        // if a null node is found, return 0.
         if (root == null) {
             return 0;
         }
+        // else apply the height formula recursively.
         return 1 + Math.max(getHeight(root.getLeft()), getHeight(root.getRight()));
     }
 
@@ -53,5 +62,21 @@ class Solution {
         n8.setLeft(n5);
         n8.setRight(n11);
         System.out.println(getHeight(n12));
+
+        // Example 2
+        Node<Integer> n1 = new Node<>(1);
+        Node<Integer> n2 = new Node<>(2);
+        Node<Integer> n3 = new Node<>(3);
+        Node<Integer> n4 = new Node<>(4);
+        Node<Integer> n51 = new Node<>(5);
+        Node<Integer> n6 = new Node<>(6);
+        Node<Integer> n7 = new Node<>(7);
+        n1.setLeft(n2);
+        n2.setLeft(n4);
+        n1.setRight(n3);
+        n3.setRight(n5);
+        n5.setLeft(n6);
+        n5.setRight(n7);
+        System.out.println(getHeight(n1));
     }
 }
