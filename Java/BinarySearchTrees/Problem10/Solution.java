bb91ac09f788a14a8727ab239b8fb1287d4d30b4
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-23 19:18:07 +0530

    Code to balance a binary search tree

--- Java/BinarySearchTrees/Problem10/Solution.java
+++ Java/BinarySearchTrees/Problem10/Solution.java
@@ -25,11 +25,33 @@ class CustomBinarySearchTree<T extends Comparable<T>> extends BinarySearchTree<T
 }
 
 public class Solution {
+    private static <T extends Comparable<T>> void balancedInsert(CustomBinarySearchTree<T> bst, List<T> inorder, int low, int high) {
+        if (low > high) return;
+        int mid = (low + (high - low)/2);
+        T data = inorder.get(mid);
+        bst.insert(data);
+        balancedInsert(bst, inorder, low, mid - 1);
+        balancedInsert(bst, inorder, mid + 1, high);
+    }
+
+    public static <T extends Comparable<T>> CustomBinarySearchTree<T> getBalancedBst(CustomBinarySearchTree<T> bst) {
+        List<T> inorder = bst.getInorder();
+        CustomBinarySearchTree<T> balancedBst = new CustomBinarySearchTree<>();
+        balancedInsert(balancedBst, inorder, 0, inorder.size() - 1);
+        return balancedBst;
+    }
+
+    private static <T extends Comparable<T>> CustomBinarySearchTree<T> constructBst(List<T> nodes) {
+        CustomBinarySearchTree<T> bst = new CustomBinarySearchTree<>();
+        nodes.forEach(bst::insert);
+        return bst;
+    }
+
     public static void main(String[] args) {
-        CustomBinarySearchTree<Integer> bst = new CustomBinarySearchTree<>();
-        for (Integer i : Arrays.asList(6,8,4,6,3,8,9,2)) {
-            bst.insert(i);
-        };
-        System.out.println(bst.getInorder());
+        // Example 1
+        CustomBinarySearchTree<Integer> bst1 = constructBst(Arrays.asList(30, 20, 10));
+        bst1.show();
+        CustomBinarySearchTree<Integer> balancedBst1 = getBalancedBst(bst1);
+        balancedBst1.show();
     }
 }
