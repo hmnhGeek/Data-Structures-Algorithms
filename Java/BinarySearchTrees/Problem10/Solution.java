d25bc10480a4f8ffa0173c7e5a8a8885e77bf071
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-23 19:21:56 +0530

    Code to balance a binary search tree

--- Java/BinarySearchTrees/Problem10/Solution.java
+++ Java/BinarySearchTrees/Problem10/Solution.java
@@ -1,3 +1,5 @@
+// Problem link - https://www.geeksforgeeks.org/convert-normal-bst-balanced-bst/
+
 package BinarySearchTrees.Problem10;
 
 import java.util.ArrayList;
@@ -26,6 +28,9 @@ class CustomBinarySearchTree<T extends Comparable<T>> extends BinarySearchTree<T
 
 public class Solution {
     private static <T extends Comparable<T>> void balancedInsert(CustomBinarySearchTree<T> bst, List<T> inorder, int low, int high) {
+        /*
+            Time complexity is O(n) and space complexity is O(log(n)).
+         */
         if (low > high) return;
         int mid = (low + (high - low)/2);
         T data = inorder.get(mid);
@@ -35,9 +40,20 @@ public class Solution {
     }
 
     public static <T extends Comparable<T>> CustomBinarySearchTree<T> getBalancedBst(CustomBinarySearchTree<T> bst) {
+        /*
+            Time complexity is O(n) and space complexity is O(n).
+         */
+
+        // get inorder of the current unbalanced BST in O(n) time and O(n) space.
         List<T> inorder = bst.getInorder();
+
+        // create a blank BST (balanced).
         CustomBinarySearchTree<T> balancedBst = new CustomBinarySearchTree<>();
+
+        // Recursively populate the nodes of the balanced BST in O(n) time and O(log(n)) space.
         balancedInsert(balancedBst, inorder, 0, inorder.size() - 1);
+
+        // retur the balanced BST of size O(n).
         return balancedBst;
     }
 
@@ -53,5 +69,11 @@ public class Solution {
         bst1.show();
         CustomBinarySearchTree<Integer> balancedBst1 = getBalancedBst(bst1);
         balancedBst1.show();
+
+        // Example 2
+        CustomBinarySearchTree<Integer> bst2 = constructBst(Arrays.asList(4, 3, 5, 2, 6, 1, 7));
+        bst2.show();
+        CustomBinarySearchTree<Integer> balancedBst2 = getBalancedBst(bst2);
+        balancedBst2.show();
     }
 }
