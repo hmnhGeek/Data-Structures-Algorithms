1c32ff0fdca85a776cecdca439e7c23bbad82132
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-23 15:25:33 +0530

    Created a BST class

---
+++ Java/BinarySearchTrees/Problem10/BinarySearchTree.java
@@ -0,0 +1,257 @@
+package BinarySearchTrees.Problem10;
+
+class Node<T extends Comparable<T>> {
+    private T data;
+    private Node<T> left;
+    private Node<T> right;
+    private Node<T> parent;
+    private Integer size;
+    private Integer height;
+    private Integer diameter;
+
+    public Node(T data) {
+        this.data = data;
+        this.size = this.height = this.diameter = 1;
+        this.left = this.right = this.parent = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<T> left) {
+        this.left = left;
+    }
+
+    public Node<T> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<T> right) {
+        this.right = right;
+    }
+
+    public Node<T> getParent() {
+        return parent;
+    }
+
+    public void setParent(Node<T> parent) {
+        this.parent = parent;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(Integer diameter) {
+        this.diameter = diameter;
+    }
+}
+
+
+public class BinarySearchTree<T extends Comparable<T>> {
+    private Node<T> root;
+    private Integer diameter;
+
+    public BinarySearchTree() {
+        this.root = null;
+        this.diameter = 0;
+    }
+
+    private void recalculateAugmentation(Node<T> parent) {
+        this.diameter = 0;
+        while (parent != null) {
+            Integer leftSize = parent.getLeft() != null ? parent.getLeft().getSize() : 0;
+            Integer rightSize = parent.getRight() != null ? parent.getRight().getSize() : 0;
+            Integer leftHt = parent.getLeft() != null ? parent.getLeft().getHeight() : 0;
+            Integer rightHt = parent.getRight() != null ? parent.getRight().getHeight() : 0;
+            parent.setSize(leftSize + rightSize + 1);
+            parent.setHeight(1 + Math.max(leftHt, rightHt));
+            parent.setDiameter(1 + leftHt + rightHt);
+            this.diameter = Math.max(this.diameter, parent.getDiameter());
+            parent = parent.getParent();
+        }
+    }
+
+    private void insert(Node<T> start, Node<T> node) {
+        if (start == null || node == null) {
+            return;
+        }
+        if (node.getData().compareTo(start.getData()) >= 0) {
+            if (start.getRight() != null) {
+                insert(start.getRight(), node);
+                return;
+            }
+            start.setRight(node);
+            node.setParent(start);
+            recalculateAugmentation(start);
+        }
+        if (start.getLeft() != null) {
+            insert(start.getLeft(), node);
+            return;
+        }
+        start.setLeft(node);
+        node.setParent(start);
+        recalculateAugmentation(start);
+        return;
+    }
+
+    public void insert(T x) {
+        Node<T> node = new Node<>(x);
+        if (this.root == null) {
+            this.root = node;
+            this.diameter = 1;
+            return;
+        }
+        insert(this.root, node);
+    }
+
+    private Node<T> getLeftmostLeaf(Node<T> node) {
+        if (node == null) {
+            return null;
+        }
+        while (node.getLeft() != null) {
+            node = node.getLeft();
+        }
+        return node;
+    }
+
+    private Node<T> getRightmostLeaf(Node<T> node) {
+        if (node == null) {
+            return null;
+        }
+        while (node.getRight() != null) {
+            node = node.getRight();
+        }
+        return node;
+    }
+
+    private Node<T> getSuccessor(Node<T> node) {
+        if (node == null) {
+            return null;
+        }
+        if (node.getRight() != null) {
+            return getLeftmostLeaf(node.getRight());
+        }
+        Node<T> parent = node.getParent();
+        if (parent == null) return null;
+        while (parent.getLeft() != node) {
+            node = node.getParent();
+            parent = parent.getParent();
+            if (parent == null) return null;
+        }
+        return parent;
+    }
+
+    private Node<T> getPredecessor(Node<T> node) {
+        if (node == null) {
+            return null;
+        }
+        if (node.getLeft() != null) {
+            return getRightmostLeaf(node.getLeft());
+        }
+        Node<T> parent = node.getParent();
+        if (parent == null) return null;
+        while (parent.getRight() != node) {
+            node = node.getParent();
+            parent = parent.getParent();
+            if (parent == null) return null;
+        }
+        return parent;
+    }
+
+    private void delete(Node<T> node) {
+        if (node == null) {
+            return;
+        }
+        if (node.getLeft() == null && node.getRight() == null) {
+            Node<T> parent = node.getParent();
+            if (parent != null) {
+                if (parent.getLeft() == node) {
+                    parent.setLeft(null);
+                } else {
+                    parent.setRight(null);
+                }
+            } else {
+                this.root = null;
+                this.diameter = 0;
+            }
+            recalculateAugmentation(parent);
+            return;
+        }
+        if (node.getRight() != null) {
+            Node<T> successor = getSuccessor(node);
+            T successorData = successor.getData(), nodeData = node.getData();
+            successor.setData(nodeData);
+            node.setData(successorData);
+            delete(successor);
+            return;
+        }
+        Node<T> predecessor = getPredecessor(node);
+        T predecessorData = predecessor.getData(), nodeData = node.getData();
+        predecessor.setData(nodeData);
+        node.setData(predecessorData);
+        return;
+    }
+
+    public Node<T> getNode(Node<T> start, T x) {
+        if (start == null || x == null) {
+            return null;
+        }
+        if (x.compareTo(start.getData()) > 0) {
+            return getNode(start.getRight(), x);
+        }
+        if (x.compareTo(start.getData()) < 0) {
+            return getNode(start.getLeft(), x);
+        }
+        return start;
+    }
+
+    public void delete(T x) {
+        Node<T> node = getNode(this.root, x);
+        if (node != null) delete(node);
+    }
+
+    private void show(Node<T> start) {
+        if (start != null) {
+            show(start.getLeft());
+            StringBuilder string = new StringBuilder(String.format("Data = %s", start.getData()));
+            if (start == this.root) {
+                string.append(String.format(" (root), "));
+            }
+            string.append(String.format("size = %d", start.getSize()));
+            string.append(String.format("ht = %d", start.getHeight()));
+            string.append(String.format("d = %d", start.getDiameter()));
+            System.out.println(string);
+            show(start.getRight());
+        }
+    }
+
+    public void show() {
+        show(this.root);
+    }
+}
