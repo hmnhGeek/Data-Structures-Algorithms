1bd434b6647d115ec5cca84392a5f68ca5a5f8b5
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-24 18:09:08 +0530

    DFS in a graph

--- Java/Graphs/G6/Solution.java
+++ Java/Graphs/G6/Solution.java
@@ -44,5 +44,29 @@ public class Solution {
         graph1.put(7, List.of(3, 8));
         graph1.put(8, List.of(4, 7));
         System.out.println(Solution.dfs(graph1));
+
+        Map<Integer, List<Integer>> graph2 = new HashMap<>();
+        graph2.put(0, List.of(1, 2, 3));
+        graph2.put(1, List.of(0));
+        graph2.put(2, List.of(0, 4));
+        graph2.put(3, List.of(0));
+        graph2.put(4, List.of(2));
+        System.out.println(Solution.dfs(graph2));
+
+        Map<Integer, List<Integer>> graph3 = new HashMap<>();
+        graph3.put(0, List.of(1, 2));
+        graph3.put(1, List.of(0, 2));
+        graph3.put(2, List.of(1, 0, 3, 4));
+        graph3.put(3, List.of(2));
+        graph3.put(4, List.of(2));
+        System.out.println(Solution.dfs(graph3));
+
+        Map<Integer, List<Integer>> graph4 = new HashMap<>();
+        graph4.put(0, List.of(1));
+        graph4.put(1, List.of(0, 2));
+        graph4.put(2, List.of(1));
+        graph4.put(3, List.of(4));
+        graph4.put(4, List.of(3));
+        System.out.println(Solution.dfs(graph4));
     }
 }
