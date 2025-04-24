dfb65f933f043cbf112f886ca8f479a1306032d3
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-24 17:49:35 +0530

    BFS of a graph using queue

--- Java/Graphs/G5/Solution.java
+++ Java/Graphs/G5/Solution.java
@@ -43,5 +43,21 @@ public class Solution {
         graph1.put(7, List.of(5, 6));
         graph1.put(8, List.of(6));
         System.out.println(bfs(graph1, 1));
+
+        Map<Integer, List<Integer>> graph2 = new HashMap<>();
+        graph2.put(0, List.of(1, 2, 3));
+        graph2.put(1, List.of(0));
+        graph2.put(2, List.of(0, 4));
+        graph2.put(3, List.of(0));
+        graph2.put(4, List.of(2));
+        System.out.println(Solution.bfs(graph2, 0));
+
+        Map<Integer, List<Integer>> graph3 = new HashMap<>();
+        graph3.put(0, List.of(1, 2));
+        graph3.put(1, List.of(0, 2));
+        graph3.put(2, List.of(1, 0, 3, 4));
+        graph3.put(3, List.of(2));
+        graph3.put(4, List.of(2));
+        System.out.println(Solution.bfs(graph3, 0));
     }
 }
