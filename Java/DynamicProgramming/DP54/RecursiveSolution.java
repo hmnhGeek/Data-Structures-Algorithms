6268f2f35b1b7375b11f4bcea376a15547d44e60
hmnhGeek <hmnhsharma97@gmail.com>
2025-04-27 12:09:49 +0530

    Recursive solution partition array for max sum

---
+++ Java/DynamicProgramming/DP54/RecursiveSolution.java
@@ -0,0 +1,22 @@
+package DynamicProgramming.DP54;
+
+import java.util.List;
+
+public class RecursiveSolution {
+    private static Integer solve(List<Integer> arr, Integer i, Integer k, Integer n) {
+        if (i.equals(n)) return 0;
+        Integer maxCost = Integer.MIN_VALUE;
+        Integer maxValue = Integer.MIN_VALUE;
+        for (int j = i; j < Math.min(i + k, n); j += 1) {
+            maxValue = Math.max(arr.get(j), maxValue);
+            Integer cost = (maxValue * (j - i + 1)) + solve(arr, j + 1, k, n);
+            maxCost = Math.max(maxCost, cost);
+        }
+        return maxCost;
+    }
+
+    public static Integer maxSumPartition(List<Integer> arr, Integer k) {
+        int n = arr.size();
+        return solve(arr, 0, k, n);
+    }
+}
