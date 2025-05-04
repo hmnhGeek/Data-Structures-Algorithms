f9bad01f2f6a51816e275661566d062ffe156527
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-04 16:27:08 +0530

    DFS in a graph

--- Python/Practice Set 4/Graphs/G6 - DFS.py
+++ Python/Practice Set 4/Graphs/G6 - DFS.py
@@ -27,3 +27,30 @@ graph1 = {
     8: [4, 7]
 }
 Solution.dfs(graph1)
+
+graph2 = {
+    0: [1, 2, 3],
+    1: [0],
+    2: [0, 4],
+    3: [0],
+    4: [2]
+}
+Solution.dfs(graph2)
+
+graph3 = {
+    0: [1, 2],
+    1: [0, 2],
+    2: [1, 0, 3, 4],
+    3: [2],
+    4: [2]
+}
+Solution.dfs(graph3)
+
+graph4 = {
+    0: [1],
+    1: [0, 2],
+    2: [1],
+    3: [4],
+    4: [3]
+}
+Solution.dfs(graph4)
