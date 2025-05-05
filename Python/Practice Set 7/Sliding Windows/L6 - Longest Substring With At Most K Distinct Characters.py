3b11efe201819fff70964d804b586c107c076332
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-05 17:13:06 +0530

    Longest substring with at most k distinct characters

--- Python/Practice Set 7/Sliding Windows/L6 - Longest Substring With At Most K Distinct Characters.py
+++ Python/Practice Set 7/Sliding Windows/L6 - Longest Substring With At Most K Distinct Characters.py
@@ -1,23 +1,40 @@
 class Solution:
     @staticmethod
     def get_longest_substring(string, k):
+        # edge case
         if k <= 0:
             return
+
+        # define window variables
         n = len(string)
-        d = {i: 0 for i in string}
         left = right = 0
+
+        # define tracking variables
+        d = {i: 0 for i in string}
+
+        # define result variables
         longest_length = -1e6
         start_index = -1
+
+        # while there is ground to cover...
         while right < n:
+            # increment the count of right indexed character
             d[string[right]] += 1
+
+            # if the distinct character count breaches the k-limit, shrink just one unit from left.
             if sum(v > 0 for v in d.values()) > k:
                 d[string[left]] -= 1
                 left += 1
             else:
+                # else, update the result variables
                 if longest_length < right - left + 1:
                     longest_length = right - left + 1
                     start_index = left
+
+            # increment the right index.
             right += 1
+
+        # return the substring.
         return string[start_index:start_index+longest_length] if start_index != -1 else ""
 
 
