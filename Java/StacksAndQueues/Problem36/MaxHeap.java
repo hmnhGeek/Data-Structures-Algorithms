3d380c1f02a7def0e0837cef2a79da1c6c5b0769
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-10 15:06:48 +0530

    Implemented a max heap

---
+++ Java/StacksAndQueues/Problem36/MaxHeap.java
@@ -0,0 +1,53 @@
+package StacksAndQueues.Problem36;
+
+import java.util.Collections;
+import java.util.List;
+
+public class MaxHeap<T extends Comparable<T>> implements Heap<T> {
+    private List<T> heap;
+
+    @Override
+    public List<T> getHeap() {
+        return this.heap;
+    }
+
+    @Override
+    public Integer getChildIndex(Integer lci, Integer rci) {
+        if (lci == null || rci == null) {
+            return null;
+        }
+        if (lci == null) return rci;
+        if (rci == null) return lci;
+        Integer maxChildIndex = lci;
+        if (getHeap().get(rci).compareTo(getHeap().get(maxChildIndex)) > 0) {
+            maxChildIndex = rci;
+        }
+        return maxChildIndex;
+    }
+
+    @Override
+    public void heapifyUp(Integer startIndex) {
+        if (startIndex.equals(0)) return;
+        Integer pi = getPi(startIndex);
+        Integer lci = getLci(pi), rci = getRci(pi);
+        Integer maxChildIndex = getChildIndex(lci, rci);
+        if (maxChildIndex != null) {
+            if (getHeap().get(pi).compareTo(getHeap().get(maxChildIndex)) < 0) {
+                Collections.swap(getHeap(), pi, maxChildIndex);
+            }
+            heapifyUp(pi);
+        }
+    }
+
+    @Override
+    public void heapifyDown(Integer pi) {
+        Integer lci = getLci(pi), rci = getRci(pi);
+        Integer maxChildIndex = getChildIndex(lci, rci);
+        if (maxChildIndex != null) {
+            if (getHeap().get(pi).compareTo(getHeap().get(maxChildIndex)) < 0) {
+                Collections.swap(getHeap(), pi, maxChildIndex);
+            }
+            heapifyDown(maxChildIndex);
+        }
+    }
+}
