017ca27457b3ab35e58e50f4e0ec30bb52307edc
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-10 13:14:04 +0530

    Reverse a string

--- Java/Strings/Problem1/Solution.java
+++ Java/Strings/Problem1/Solution.java
@@ -1,7 +1,12 @@
+// Problem link - https://leetcode.com/problems/reverse-string/description/
+
 package Strings.Problem1;
 
 public class Solution {
     public static String reverseString(String string) {
+        /*
+            Time complexity is O(n) and space complexity is O(n).
+         */
         StringBuilder stringBuilder = new StringBuilder();
         int n = string.length();
         for (int i = n - 1; i >= 0; i -= 1) {
