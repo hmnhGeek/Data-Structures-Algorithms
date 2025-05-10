a4d4f319558b7b6cb44e15465713f22cc5e8f9a4
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-10 15:51:40 +0530

    Implemented heap interface

---
+++ Java/Heap/Problem3/Heap.java
@@ -0,0 +1,43 @@
+package Heap.Problem3;
+
+import java.util.Collections;
+import java.util.List;
+
+public interface Heap<T extends Comparable<T>> {
+    List<T> getHeap();
+    Integer getChildIndex(Integer lci, Integer rci);
+    void heapifyUp(Integer startIndex);
+    void heapifyDown(Integer pi);
+
+    default Integer getLci(Integer pi) {
+        Integer lci = 2*pi + 1;
+        return 0 <= lci && lci < getHeap().size() ? lci : 0;
+    }
+
+    default Integer getRci(Integer pi) {
+        Integer rci = 2*pi + 2;
+        return 0 <= rci && rci < getHeap().size() ? rci : 0;
+    }
+
+    default Integer getPi(Integer ci) {
+        if (ci.equals(0)) return null;
+        Integer pi = (ci - 1)/2;
+        return 0 <= pi && pi < getHeap().size() ? pi : 0;
+    }
+
+    default void insert(T x) {
+        getHeap().add(x);
+        heapifyUp(getHeap().size() - 1);
+    }
+
+    default T pop() {
+        if (getHeap().isEmpty()) {
+            return null;
+        }
+        T item = getHeap().getFirst();
+        Collections.swap(getHeap(), 0, getHeap().size() - 1);
+        getHeap().removeLast();
+        heapifyDown(0);
+        return item;
+    }
+}
