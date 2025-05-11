21f64a45036a0fefc3772e71c1093a263b44d1ef
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-11 13:54:55 +0530

    Allocate books using binary search

--- Java/SearchingAndSorting/Problem30/Solution.java
+++ Java/SearchingAndSorting/Problem30/Solution.java
@@ -1,3 +1,6 @@
+// Problem link - https://www.geeksforgeeks.org/problems/allocate-minimum-number-of-pages0937/1
+// Solution - https://www.youtube.com/watch?v=Z0hwjftStI4&list=PLgUwDviBIf0pMFMWuuvDNMAkoQFi-h0ZF&index=19
+
 package SearchingAndSorting.Problem30;
 
 import java.util.Arrays;
@@ -21,10 +24,17 @@ public class Solution {
     }
 
     public static Integer allocateBooks(List<Integer> pages, Integer numStudents) {
+        /*
+            Time complexity is O(n * log(sum - max)) and space complexity is O(1).
+         */
+
+        // if k is <= 0 or number of students is more than the number of books, return -1.
         if (numStudents <= 0 || numStudents > pages.size()) return -1;
         int low = Collections.max(pages), high = pages.stream().mapToInt(Integer::intValue).sum();
         while (low <= high) {
             int mid = (low + (high - low)/2);
+
+            // check if allocation to all k students is possible if each student gets at max mid-number of pages.
             int numStudentsAcquired = getPages(pages, mid);
             if (numStudentsAcquired == numStudents) {
                 high = mid - 1;
