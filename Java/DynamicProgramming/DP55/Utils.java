c44efe70ae4f96d38a088cbe34e9793f7856379a
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-15 18:02:37 +0530

    Maximum area in a histogram

--- Java/DynamicProgramming/DP55/Utils.java
+++ Java/DynamicProgramming/DP55/Utils.java
@@ -10,25 +10,42 @@ public class Utils {
     }
 
     public static Integer getMaxAreaInHistogram(List<Integer> arr) {
+        // create required variables.
         Stack<Integer> stack = new Stack<>();
         int n = arr.size();
         int maxArea = Integer.MIN_VALUE;
+
         for (int i = 0; i < n; i += 1) {
+            // while the stack has some elements and top element is greater than ith element, we must pop, because we
+            // want to maintain linearly increasing order in the stack.
             while (!stack.isEmpty() && arr.get(getStackTop(stack)) > arr.get(i)) {
+                // get the bar, which is the top element on the stack.
                 Integer bar = arr.get(stack.pop());
+
+                // element just before the top (new top now), will be the left boundary of the bar.
                 Integer leftBoundaryIndex = getStackTop(stack);
+
+                // for the top element, ith bar will be the right boundary.
                 Integer rightBoundaryIndex = i;
+
+                // get the area formed by this bar and update max area.
                 Integer area = bar * (rightBoundaryIndex - leftBoundaryIndex - 1);
                 maxArea = Math.max(area, maxArea);
             }
+
+            // finally, once there is no greater element on top of the stack, push `i`.
             stack.push(i);
         }
+
+        // once the iteration is over, stack will contain all those elements whose right boundary is `n`.
         while (!stack.isEmpty()) {
             Integer bar = arr.get(stack.pop());
             Integer leftBoundaryIndex = getStackTop(stack);
             Integer area = bar * (n - leftBoundaryIndex - 1);
             maxArea = Math.max(area, maxArea);
         }
+
+        // return the max area.
         return maxArea;
     }
 }
