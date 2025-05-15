f7def025c8559a799a9b14b79663852be914115c
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-15 07:31:34 +0530

    Created a stack class

---
+++ Java/DynamicProgramming/DP55/Stack.java
@@ -0,0 +1,63 @@
+package DynamicProgramming.DP55;
+
+
+class Node<T extends Comparable<T>> {
+    private T data;
+    private Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+}
+
+
+public class Stack<T extends Comparable<T>> {
+    private Node<T> head;
+    private Node<T> tail;
+    private Integer length;
+
+    public Stack() {
+        this.head = this.tail = null;
+        this.length = 0;
+    }
+
+    public boolean isEmpty() {
+        return this.length.equals(0);
+    }
+
+    public void push(T x) {
+        Node<T> node = new Node<>(x);
+        if (isEmpty()) {
+            this.head = this.tail = node;
+        } else {
+            node.setNext(this.head);
+            this.head = node;
+        }
+        this.length += 1;
+    }
+
+    public T pop() {
+        if (isEmpty()) return null;
+        T item = this.head.getData();
+        this.head = this.head.getNext();
+        this.length -= 1;
+        return item;
+    }
+}
