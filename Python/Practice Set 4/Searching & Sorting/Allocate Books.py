3c6114434785e3e45fd64eff90e14f391e63769f
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-17 19:17:24 +0530

    Allocate Books using binary search

--- Python/Practice Set 4/Searching & Sorting/Allocate Books.py
+++ Python/Practice Set 4/Searching & Sorting/Allocate Books.py
@@ -1,16 +1,28 @@
+# Problem link - https://www.geeksforgeeks.org/problems/allocate-minimum-number-of-pages0937/1
+# Solution - https://www.youtube.com/watch?v=Z0hwjftStI4&list=PLgUwDviBIf0pMFMWuuvDNMAkoQFi-h0ZF&index=19
+
+
 class Solution:
     @staticmethod
     def allocate_books(arr, k):
+        """
+            Time complexity is O(n * log(sum - max)) and space complexity is O(1).
+        """
+
         if k <= 0 or k > len(arr):
             return -1
 
+        # define the search space.
         low = max(arr)
         high = sum(arr)
+
         while low <= high:
             mid = int(low + (high - low)/2)
             students_allocated = Solution._allocate(arr, mid)
             if students_allocated == k:
                 high = mid - 1
+
+            # if more students could be allocated, then increase the page count so that we lower down to k.
             elif students_allocated > k:
                 low = mid + 1
             else:
