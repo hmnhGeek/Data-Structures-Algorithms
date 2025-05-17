fde3fa44752f6d27d8ebae523b961631dc47eb50
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-17 19:08:46 +0530

    Row with max 1s

--- Python/Practice Set 4/Matrix/row with max 1s.py
+++ Python/Practice Set 4/Matrix/row with max 1s.py
@@ -1,3 +1,6 @@
+# Problem link - https://www.geeksforgeeks.org/problems/row-with-max-1s0023/1
+
+
 class Solution:
     @staticmethod
     def _get_ones_count(mtx, i, low, high, m):
@@ -12,14 +15,27 @@ class Solution:
 
     @staticmethod
     def max1s_row(mtx):
+        """
+            Time complexity is O(n * log(m)) and space complexity is O(1).
+        """
+
         n, m = len(mtx), len(mtx[0])
+
+        # store the row index and max count of ones in a variable.
         max_1s = 0
         row = -1
+
+        # loop on the rows
         for i in range(n):
+            # count the ones in each row in O(log(m)) time.
             ones_count = Solution._get_ones_count(mtx, i, 0, m - 1, m)
+
+            # if ones count found > max count of ones, update the result variables.
             if ones_count > max_1s:
                 row = i
                 max_1s = ones_count
+
+        # return the index of the first row with max ones.
         return row
 
 
