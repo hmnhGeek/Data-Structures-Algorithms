7cea96ae12d23a62371e0b2ad19f526672795a99
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-18 17:50:42 +0530

    Max area in a matrix

--- Python/Practice Set 3/Dynamic Programming/DP55 - Max Rectangle.py
+++ Python/Practice Set 3/Dynamic Programming/DP55 - Max Rectangle.py
@@ -40,33 +40,52 @@ class Stack:
 class Solution:
     @staticmethod
     def _max_area_in_histogram(histogram):
+        # create required variables.
         stack = Stack()
         n = len(histogram)
         max_area = 0
+
+        # loop in the array
         for i in range(n):
+            # while the stack has some elements and top element is greater than ith element, we must pop, because we
+            # want to maintain linearly increasing order in the stack.
             while not stack.is_empty() and histogram[stack.top()] > histogram[i]:
+                # get the bar, which is the top element on the stack.
                 bar = histogram[stack.pop()]
+                # for the top element, ith bar will be the right boundary.
                 rb = i
+                # element just before the top (new top now), will be the left boundary of the bar.
                 lb = stack.top()
+                # get the area formed by this bar and update max area.
                 area = bar * (rb - lb - 1)
                 max_area = max(max_area, area)
+            # finally, once there is no greater element on top of the stack, push `i`.
             stack.push(i)
+
+        # once the iteration is over, stack will contain all those elements whose right boundary is `n`.
         while not stack.is_empty():
             bar = histogram[stack.pop()]
             lb = stack.top()
             area = bar * (n - lb - 1)
             max_area = max(max_area, area)
+
+        # return the max area.
         return max_area
 
     @staticmethod
     def max_area_in_mtx(mtx):
         n, m = len(mtx), len(mtx[0])
+
+        # create a previous row. It will take O(m) extra space.
         prev_row = [0] * m
         max_area = 0
         for i in range(n):
+            # create a histogram with another O(m) space and update the max area.
             histogram = [mtx[i][j] + prev_row[j] if mtx[i][j] == 1 else 0 for j in range(m)]
+            # get the max area in O(m) time and O(m) space.
             area = Solution._max_area_in_histogram(histogram)
             max_area = max(max_area, area)
+            # remember to update previous row.
             prev_row = histogram
         return max_area
 
