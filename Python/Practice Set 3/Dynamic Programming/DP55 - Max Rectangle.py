65eb6fe114de38404fc67db6e3cb7100085397c8
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-18 17:41:33 +0530

    Implemented a stack class

---
+++ Python/Practice Set 3/Dynamic Programming/DP55 - Max Rectangle.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.head = self.tail = None
+        self.length = 0
+
+    def is_empty(self):
+        return self.length == 0
+
+    def push(self, x):
+        node = Node(x)
+        if self.is_empty():
+            self.head = self.tail = node
+        else:
+            node.next = self.head
+            self.head = node
+        self.length += 1
+
+    def pop(self):
+        if self.is_empty():
+            return -1
+        item = self.head.data
+        node = self.head
+        self.head = self.head.next
+        del node
+        self.length -= 1
+        return item
+
+    def top(self):
+        if self.is_empty():
+            return -1
+        return self.head.data
+
