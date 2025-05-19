22da9abc24c0249491f2919ae85408e017fd754d
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-19 12:55:55 +0530

    Median in a row-wise sorted matrix

--- Java/BinarySearch/BS27/Solution.java
+++ Java/BinarySearch/BS27/Solution.java
@@ -5,19 +5,34 @@ import java.util.List;
 
 public class Solution {
     public static Integer getMedianFromMatrix(List<List<Integer>> mtx) {
+        // get the dimensions of the matrix.
         int n = mtx.size(), m = mtx.getFirst().size();
+
+        // push the first column into the min heap.
         MinHeap<HeapElement> minHeap = new MinHeap<>();
         for (int i = 0; i < n; i += 1) {
             minHeap.insert(new HeapElement(mtx.get(i).getFirst(), i, 0));
         }
+
+        // get the median index
         int medianIndex = (n * m)/2;
+
+        // define a counter which will keep track of the median index (when to stop).
         int counter = -1;
+
+        // while the counter has not reached the median element. This will run for O(nm/2) time.
         while (counter != medianIndex) {
+            // get the smallest element in O(log(n)) time & increment the counter value.
             HeapElement heapElement = minHeap.pop();
             counter += 1;
+
+            // if counter reached median index, return the median value.
             if (counter == medianIndex) {
                 return heapElement.getValue();
             }
+
+            // if the next element to the popped element is available, then push it into the min heap in
+            // O(log(n)) time.
             int colIndex = heapElement.getCol();
             if (0 <= colIndex + 1 && colIndex + 1 < m) {
                 minHeap.insert(new HeapElement(mtx.get(heapElement.getRow()).get(colIndex + 1), heapElement.getRow(), colIndex + 1));
