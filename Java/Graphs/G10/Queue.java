6a2ddd946f8da990b19424e630a84cb0cf31e650
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-23 20:21:24 +0530

    Implemented a queue

---
+++ Java/Graphs/G10/Queue.java
@@ -0,0 +1,61 @@
+package Graphs.G10;
+
+class Node<T> {
+    private T data;
+    private Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+}
+
+public class Queue<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private Integer length;
+
+    public Queue() {
+        this.head = this.tail = null;
+        this.length = 0;
+    }
+
+    public void push(T x) {
+        Node<T> node = new Node<>(x);
+        if (this.length.equals(0)) {
+            this.head = this.tail = node;
+        } else {
+            this.tail.setNext(node);
+            this.tail = node;
+        }
+        this.length += 1;
+    }
+
+    public boolean isEmpty() {
+        return this.length.equals(0);
+    }
+
+    public T pop() {
+        if (isEmpty()) return null;
+        T item = this.head.getData();
+        this.head = this.head.getNext();
+        this.length -= 1;
+        return item;
+    }
+}
---
+++ Java/Graphs/G10/Solution.java
@@ -0,0 +1,4 @@
+package Graphs.G10;
+
+public class Solution {
+}
