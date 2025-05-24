ca3b371eba6cc0d2fcf7674c3d7410770c5cb4d0
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-24 19:48:26 +0530

    Number of Substrings Containing All Three Characters

--- Java/SlidingWindows/L7/Solution.java
+++ Java/SlidingWindows/L7/Solution.java
@@ -13,25 +13,47 @@ public class Solution {
     }
 
     public static Integer solve(String string, Integer k) {
+        /*
+            Time complexity is O(n) and space complexity is O(1).
+         */
+
+        // edge case
         if (k < 0) return 0;
+
+        // get window variables
         int n = string.length();
         int left = 0, right = 0;
+
+        // get tracking variable
         Map<Character, Integer> d = new HashMap<>();
         d.put('a', 0);
         d.put('b', 0);
         d.put('c', 0);
+
+        // get result variable
         int count = 0;
+
+        // while there is ground to cover
         while (right < n) {
+            // increment right indexed count
             d.put(string.charAt(right), d.get(string.charAt(right)) + 1);
+
+            // while count of characters exceed k, shrink from left
             while (characterCount(d) > k) {
                 d.put(string.charAt(left), d.get(string.charAt(left)) - 1);
                 left += 1;
             }
+
+            // else, update the count variable
             if (characterCount(d) <= k) {
                 count += (right - left + 1);
             }
+
+            // increment right index.
             right += 1;
         }
+
+        // return the count.
         return count;
     }
 
