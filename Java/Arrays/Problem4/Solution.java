47c9e94de1d9e68e8e5cd40dcd452667c95259d4
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-25 17:19:56 +0530

    Dutch National Flag Algorithm

--- Java/Arrays/Problem4/Solution.java
+++ Java/Arrays/Problem4/Solution.java
@@ -7,15 +7,29 @@ import java.util.List;
 public class Solution {
     public static void dnfSort(List<Integer> arr) {
         int n = arr.size();
+
+        // initialize low, mid and high pointers
         int low = 0, mid = 0, high = n - 1;
+
+        /*
+            The idea is that all elements from 0 to low - 1 will be 0s.
+            All elements from mid to high - 1 will be 1s.
+            All elements from high till end will be 2s.
+         */
+
+        // run a loop till mid <= high...
         while (mid <= high) {
+            // if a 0 is encountered at mid.
             if (arr.get(mid) == 0) {
+                // swap with low and increment both mid and low.
                 Collections.swap(arr, low, mid);
                 low += 1;
                 mid += 1;
             } else if (arr.get(mid) == 1) {
+                // if mid-element is 1, then simply increment mid.
                 mid += 1;
             } else {
+                // swap mid and high element and decrement only high; don't change mid.
                 Collections.swap(arr, mid, high);
                 high -= 1;
             }
