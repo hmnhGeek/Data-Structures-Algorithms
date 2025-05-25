98793f5fe315ab2dad84551f1624fcb57a9a516c
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-25 17:46:07 +0530

    Check if string is palindrome or not

--- Java/Strings/Problem2/Solution.java
+++ Java/Strings/Problem2/Solution.java
@@ -1,7 +1,12 @@
+// Problem link - https://www.geeksforgeeks.org/problems/palindrome-string0817/1
+
 package Strings.Problem2;
 
 public class Solution {
     public static boolean isPalindrome(String string) {
+        /*
+            Time complexity is O(n) and space complexity is O(1).
+         */
         int n = string.length();
         int low = 0, high = n - 1;
         while (low <= high) {
