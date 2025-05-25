5b8c8e06a48d2a38b4fd8ebb074e517b32f3996f
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-25 17:44:40 +0530

    Check if string is palindrome or not

---
+++ Java/Strings/Problem2/Solution.java
@@ -0,0 +1,24 @@
+package Strings.Problem2;
+
+public class Solution {
+    public static boolean isPalindrome(String string) {
+        int n = string.length();
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            if (string.charAt(low) == string.charAt(high)) {
+                low += 1;
+                high -= 1;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("abba"));
+        System.out.println(isPalindrome("abc"));
+        System.out.println(isPalindrome("a"));
+        System.out.println(isPalindrome("acbca"));
+    }
+}
