bbc5d269b3fccfcc55c281271f8835d9d65a6391
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-25 17:54:44 +0530

    Build a linked list class

---
+++ Java/LinkedLists/Problem8/Solution.java
@@ -0,0 +1,9 @@
+package LinkedLists.Problem8;
+
+public class Solution {
+    public static void main(String[] args) {
+        LinkedList<Integer> l1 = new LinkedList<>();
+        l1.build(3,2,5,8,6,8,9,4);
+        System.out.println(l1);
+    }
+}
