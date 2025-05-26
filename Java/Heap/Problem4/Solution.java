9a202f8a50475cfca85b9a069fd39ea6ab646fae
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-26 20:58:38 +0530

    K largest elements in an array

--- Java/Heap/Problem4/Solution.java
+++ Java/Heap/Problem4/Solution.java
@@ -25,5 +25,17 @@ public class Solution {
     public static void main(String[] args) {
         List<Integer> arr1 = Arrays.asList(12, 5, 787, 1, 23);
         System.out.println(getKLargestElements(arr1, 2));
+
+        List<Integer> arr2 = Arrays.asList(1, 23, 12, 9, 30, 2, 50);
+        System.out.println(getKLargestElements(arr2, 3));
+
+        List<Integer> arr3 = Arrays.asList(12, 23);
+        System.out.println(getKLargestElements(arr3, 1));
+
+        List<Integer> arr4 = Arrays.asList(11, 5, 12, 9, 44, 17, 2);
+        System.out.println(getKLargestElements(arr4, 2));
+
+        List<Integer> arr5 = Arrays.asList(11, 3, 4, 6);
+        System.out.println(getKLargestElements(arr5, 3));
     }
 }
