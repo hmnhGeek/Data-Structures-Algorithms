c2674f55a0eeec548d27596fc76a957d743bd34c
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-26 21:57:54 +0530

    Stream first non-repeating character in a string

--- Java/StacksAndQueues/Problem37/Solution.java
+++ Java/StacksAndQueues/Problem37/Solution.java
@@ -5,22 +5,35 @@ import java.util.Map;
 
 public class Solution {
     public static String streamFirstNonRepeating(String string) {
+        // These will take O(26) space.
         Map<Character, Node<Character>> addressMap = getDefaultAddressMap();
         Map<Character, Boolean> repeatedMap = getDefaultRepeatedMap();
+
+        // create a new doubly linked list.
         DoublyLinkedList<Character> doublyLinkedList = new DoublyLinkedList<>();
+
+        // result string
         StringBuilder stringBuilder = new StringBuilder();
 
+        // loop in the string in O(n) time. All internal operations are O(1).
         for (Character character : string.toCharArray()) {
+            // if the character is already repeated, do nothing.
             if (repeatedMap.get(character)) {
                 continue;
             }
+
+            // if the character was not yet repeated, and it has been seen second time now, mark it as repeated and,
+            // erase it from DLL.
             else if (addressMap.get(character) != null) {
                 repeatedMap.put(character, true);
                 doublyLinkedList.deleteNode(addressMap.get(character));
             } else {
+                // else, add this character in dll and put its address in address map.
                 Node<Character> node = doublyLinkedList.push(character);
                 addressMap.put(character, node);
             }
+
+            // build the result string.
             if (doublyLinkedList.isEmpty()) {
                 stringBuilder.append("#");
             } else {
