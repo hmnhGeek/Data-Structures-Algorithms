376b2f18e4e326f4b12ba53c254c4be41de3b41f
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-26 21:00:59 +0530

    K largest elements in an array

--- Java/Heap/Problem4/Solution.java
+++ Java/Heap/Problem4/Solution.java
@@ -1,24 +1,38 @@
+// Problem link - https://www.geeksforgeeks.org/problems/k-largest-elements4206/1
+
+
 package Heap.Problem4;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
     public static List<Integer> getKLargestElements(List<Integer> arr, Integer k) {
+        /*
+            Time complexity is O(n * log(k)) and space complexity is O(k).
+         */
         if (k <= 0 || k >= arr.size()) return null;
+
+        // create a new min heap
         MinHeap<Integer> minHeap = new MinHeap<>();
+
+        // insert first k elements into this heap in O(k * log(k)) time and O(k) space.
         for (int i = 0; i < k; i += 1) {
             minHeap.insert(arr.get(i));
         }
+
+        // loop on the remaining array in n - k iterations.
         int i = k;
         while (i < arr.size()) {
+            // if the top element of the heap is less than current element, insert current element at its place in O(log(k)) time.
             if (minHeap.getHeap().getFirst() < arr.get(i)) {
                 minHeap.pop();
                 minHeap.insert(arr.get(i));
             }
             i += 1;
         }
+
+        // return the k sized array.
         return minHeap.getHeap();
     }
 
