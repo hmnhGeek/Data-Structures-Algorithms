ffc4f638c6184b1417f6c62ac423d04d7b5f7927
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-31 14:56:51 +0530

    Recursive frog jump

---
+++ Python/Practice Set 4/Dynamic Programming/DP3 - Frog Jump.py
@@ -0,0 +1,28 @@
+def recursive():
+    """
+        Time complexity is O(2^n) and space complexity is O(n).
+    """
+    def solve(arr, i):
+        if i == 0:
+            return 0
+        left = 1e6
+        if 0 <= i - 1:
+            left = abs(arr[i] - arr[i - 1]) + solve(arr, i - 1)
+        right = 1e6
+        if 0 <= i - 2:
+            right = abs(arr[i] - arr[i - 2]) + solve(arr, i - 2)
+        return min(left, right)
+
+    def frog_jump(arr):
+        n = len(arr)
+        return solve(arr, n - 1)
+
+    print(frog_jump([30, 10, 60, 10, 60, 50]))
+    print(frog_jump([10, 20, 30, 10]))
+    print(frog_jump([10, 50, 10]))
+    print(frog_jump([7, 4, 4, 2, 6, 6, 3, 4]))
+    print(frog_jump([4, 8, 3, 10, 4, 4]))
+    print(frog_jump([30, 20, 50, 10, 40]))
+
+
+recursive()
