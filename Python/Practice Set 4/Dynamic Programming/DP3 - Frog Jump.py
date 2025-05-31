ce13d324ebe9b5949bbbae1e13821cb99e3cc21b
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-31 14:59:51 +0530

    Tabulation frog jump

--- Python/Practice Set 4/Dynamic Programming/DP3 - Frog Jump.py
+++ Python/Practice Set 4/Dynamic Programming/DP3 - Frog Jump.py
@@ -27,7 +27,7 @@ def recursive():
 
 def memoized():
     """
-        Time complexity is O(2^n) and space complexity is O(n).
+        Time complexity is O(n) and space complexity is O(2n).
     """
     def solve(arr, i, dp):
         if i == 0:
@@ -56,6 +56,34 @@ def memoized():
     print(frog_jump([30, 20, 50, 10, 40]))
 
 
+def tabulation():
+    """
+        Time complexity is O(n) and space complexity is O(n).
+    """
+    def frog_jump(arr):
+        n = len(arr)
+        dp = {i: 1e6 for i in range(n)}
+        dp[0] = 0
+        for i in range(1, n):
+            left = 1e6
+            if 0 <= i - 1:
+                left = abs(arr[i] - arr[i - 1]) + dp[i - 1]
+            right = 1e6
+            if 0 <= i - 2:
+                right = abs(arr[i] - arr[i - 2]) + dp[i - 2]
+            dp[i] = min(left, right)
+        return dp[n - 1]
+
+    print(frog_jump([30, 10, 60, 10, 60, 50]))
+    print(frog_jump([10, 20, 30, 10]))
+    print(frog_jump([10, 50, 10]))
+    print(frog_jump([7, 4, 4, 2, 6, 6, 3, 4]))
+    print(frog_jump([4, 8, 3, 10, 4, 4]))
+    print(frog_jump([30, 20, 50, 10, 40]))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
