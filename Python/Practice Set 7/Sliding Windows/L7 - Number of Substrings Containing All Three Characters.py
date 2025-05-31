4465c1ce791bcbe976c5599703860560b239d864
hmnhGeek <hmnhsharma97@gmail.com>
2025-05-31 15:15:28 +0530

    Number of Substrings Containing All Three Characters

--- Python/Practice Set 7/Sliding Windows/L7 - Number of Substrings Containing All Three Characters.py
+++ Python/Practice Set 7/Sliding Windows/L7 - Number of Substrings Containing All Three Characters.py
@@ -1,18 +1,34 @@
 class Solution:
     @staticmethod
     def solve(string, k):
+        # window variables
         n = len(string)
         left = right = 0
+
+        # tracking variable
         d = {'a': 0, 'b': 0, 'c': 0}
+
+        # result variable
         count = 0
+
+        # while there is ground to cover...
         while right < n:
+            # increment the right indexed count
             d[string[right]] += 1
+
+            # while the number of characters have breached `k` value...
             while sum(1 for v in d.values() if v > 0) > k:
+                # shrink continuously from left.
                 d[string[left]] -= 1
                 left += 1
             else:
+                # else increment the count.
                 count += (right - left + 1)
+
+            # increment the right index
             right += 1
+
+        # return the count.
         return count
 
     @staticmethod
