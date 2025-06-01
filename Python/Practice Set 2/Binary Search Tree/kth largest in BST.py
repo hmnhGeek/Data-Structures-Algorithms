ee40ea5aa874bc0c20e9b4de2984ab124804bac4
hmnhGeek <hmnhsharma97@gmail.com>
2025-06-01 14:30:11 +0530

    Logic to insert into BST

---
+++ Python/Practice Set 2/Binary Search Tree/kth largest in BST.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = self.right = self.parent = None
+        self.size = self.ht = self.d = 1
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+        self.d = 0
+
+    def recalc_aug(self, parent):
+        self.d = 0
+        while parent is not None:
+            left_sz, right_sz = parent.left.size if parent.left else 0, parent.right.size if parent.right else 0
+            left_ht, right_ht = parent.left.ht if parent.left else 0, parent.right.ht if parent.right else 0
+            parent.size = 1 + left_sz + right_sz
+            parent.ht = 1 + max(left_ht, right_ht)
+            parent.d = 1 + left_ht + right_ht
+            self.d = max(self.d, parent.d)
+            parent = parent.parent
+
+    def _insert(self, start, node):
+        if start is None or node is None:
+            return
+        if node.data >= start.data:
+            if start.right is not None:
+                return self._insert(start.right, node)
+            start.right = node
+            node.parent = start
+            self.recalc_aug(start)
+            return
+        if start.left is not None:
+            return self._insert(start.left, node)
+        start.left = node
+        node.parent = start
+        self.recalc_aug(start)
+        return
+
+    def insert(self, x):
+        node = Node(x)
+        if self.root is None:
+            self.root = node
+            self.d = 1
+            return
+        return self._insert(self.root, node)
