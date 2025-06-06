260be64fd224b4ca5327d610d6b2562beb2a1fa7
hmnhGeek <hmnhsharma97@gmail.com>
2025-06-06 09:21:39 +0530

    Implemented a linked list class

---
+++ Python/Practice Set 1/Linked Lists/Move last element to front of a given Linked List.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = self.tail = None
+        self.length = 0
+
+    def push(self, x):
+        node = Node(x)
+        if self.length == 0:
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+        self.length += 1
+
+    def build(self, *args):
+        for i in args:
+            self.push(i)
+
+    def __len__(self):
+        return self.length
+
+    def __str__(self):
+        if self.length == 0:
+            return "[]"
+        if self.length == 1:
+            return f"[{self.head.data}]"
+        result = f"[{self.head.data}, "
+        curr = self.head.next
+        while curr != self.tail:
+            result += f"{curr.data}, "
+            curr = curr.next
+        result += f"{self.tail.data}]"
+        return result
+
