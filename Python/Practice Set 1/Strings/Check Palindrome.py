214dd28cd3f84f65eaebe18ca0faea29b6e01124
hmnhGeek <hmnhsharma97@gmail.com>
2025-06-06 09:14:43 +0530

    Check if string is a palindrome or not

--- Python/Practice Set 1/Strings/Check Palindrome.py
+++ Python/Practice Set 1/Strings/Check Palindrome.py
@@ -1,6 +1,12 @@
+# Problem link - https://www.geeksforgeeks.org/problems/palindrome-string0817/1
+
+
 class Solution:
     @staticmethod
     def is_palindrome(string: str) -> bool:
+        """
+            Time complexity is O(n) and space complexity is O(1).
+        """
         i, j = 0, len(string) - 1
         while i <= j:
             if string[i] == string[j]:
