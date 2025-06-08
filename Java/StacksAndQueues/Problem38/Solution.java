77db66d0aa0a18c15e2cd55e3f617beb37b5cd30
hmnhGeek <hmnhsharma97@gmail.com>
2025-06-08 18:33:05 +0530

    Next smaller elements

--- Java/StacksAndQueues/Problem38/Solution.java
+++ Java/StacksAndQueues/Problem38/Solution.java
@@ -6,5 +6,6 @@ public class Solution {
     public static void main(String[] args) {
         System.out.println(Utility.getNextSmaller(Arrays.asList(4, 8, 5, 2, 25)));
         System.out.println(Utility.getNextSmaller(Arrays.asList(13, 7, 6, 12)));
+        System.out.println(Utility.getNextSmaller(Arrays.asList(2,6,8,9,2,6,9,8,5,3)));
     }
 }
--- Java/StacksAndQueues/Problem38/Utility.java
+++ Java/StacksAndQueues/Problem38/Utility.java
@@ -10,14 +10,10 @@ public class Utility {
         Stack<T> stack = new Stack<>();
         List<T> result = new ArrayList<>();
         while (i >= 0) {
-            if (stack.isEmpty()) {
-                result.add(null);
-            } else {
-                while (!stack.isEmpty() && stack.top().compareTo(arr.get(i)) > 0) {
-                    stack.pop();
-                }
-                result.add(stack.top());
+            while (!stack.isEmpty() && stack.top().compareTo(arr.get(i)) >= 0) {
+                stack.pop();
             }
+            result.add(stack.top());
             stack.push(arr.get(i));
             i -= 1;
         }
