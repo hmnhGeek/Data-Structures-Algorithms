69a8ce56a9eae0df6d0d8a2513a7d898ac132c80
hmnhGeek <hmnhsharma97@gmail.com>
2025-06-08 17:09:50 +0530

    Move all negative integers to front

--- Java/Arrays/Problem5/Solution.java
+++ Java/Arrays/Problem5/Solution.java
@@ -8,20 +8,32 @@ public class Solution {
     public static void moveNegatives(List<Integer> arr) {
         int n = arr.size();
         int i = 0;
+
+        // make low to point to the first positive integer.
         while (i < n && arr.get(i) < 0) {
             i += 1;
         }
         int low = i;
+
+        // make mid to point to low + 1.
         int mid = i + 1;
+
+        // while mid is within bounds...
         while (mid < n) {
+            // if the mid-element is negative...
             if (arr.get(mid) < 0) {
+                // swap with the first positive at low.
                 Collections.swap(arr, low, mid);
+
+                // move low to the next first positive
                 while (low < n && arr.get(low) < 0) {
                     low += 1;
                 }
+                // again, move mid to low + 1.
                 mid = low + 1;
                 continue;
             }
+            // else, if mid element is already positive, increment mid only.
             mid += 1;
         }
     }
