666f8b4cffa8d19679738fba1d0674413a1774ee
hmnhGeek <hmnhsharma97@gmail.com>
2025-06-08 18:10:12 +0530

    Get duplicates from a string

---
+++ Java/Strings/Problem3/Solution.java
@@ -0,0 +1,26 @@
+package Strings.Problem3;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+    public static List<Character> getDuplicates(String string) {
+        List<Character> result = new ArrayList<>();
+        Map<Character, Integer> counter = new HashMap<>();
+        for (Character character : string.toCharArray()) {
+            counter.put(character, counter.getOrDefault(character, 0) + 1);
+        }
+        for (Character character : counter.keySet()) {
+            if (counter.get(character) > 1) {
+                result.add(character);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getDuplicates("geeksforgeeks"));
+    }
+}
