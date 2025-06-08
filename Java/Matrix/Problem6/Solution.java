51cfbe2afb41a3cd5f498449165ca528eb90f399
hmnhGeek <hmnhsharma97@gmail.com>
2025-06-08 17:52:11 +0530

    Max rectangle in a matrix

--- Java/Matrix/Problem6/Solution.java
+++ Java/Matrix/Problem6/Solution.java
@@ -4,7 +4,41 @@ import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
+        System.out.println("Max area in histogram");
         System.out.println(Utility.getMaxAreaInHistogram(Arrays.asList(60, 20, 50, 40, 10, 50, 60)));
         System.out.println(Utility.getMaxAreaInHistogram(Arrays.asList(3, 5, 1, 7, 5, 9)));
+        System.out.println();
+        System.out.println("Max Rectangle");
+        System.out.println(
+                Utility.maxRectangle(
+                        Arrays.asList(
+                                Arrays.asList(0, 1, 1, 0),
+                                Arrays.asList(1, 1, 1, 1),
+                                Arrays.asList(1, 1, 1, 1),
+                                Arrays.asList(1, 1, 0, 0)
+                        )
+                )
+        );
+
+        System.out.println(
+                Utility.maxRectangle(
+                        Arrays.asList(
+                                Arrays.asList(0, 1, 1),
+                                Arrays.asList(1, 1, 1),
+                                Arrays.asList(0, 1, 1)
+                        )
+                )
+        );
+
+        System.out.println(
+                Utility.maxRectangle(
+                        Arrays.asList(
+                                Arrays.asList(1, 0, 1, 0, 0),
+                                Arrays.asList(1, 0, 1, 1, 1),
+                                Arrays.asList(1, 1, 1, 1, 1),
+                                Arrays.asList(1, 0, 0, 1, 0)
+                        )
+                )
+        );
     }
 }
--- Java/Matrix/Problem6/Utility.java
+++ Java/Matrix/Problem6/Utility.java
@@ -1,9 +1,13 @@
 package Matrix.Problem6;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Utility {
     public static Integer getMaxAreaInHistogram(List<Integer> histogram) {
+        /*
+            Time complexity is O(n) and space complexity is O(n).
+         */
         Stack<Integer> stack = new Stack<>();
         int n = histogram.size();
         Integer maxArea = 0;
@@ -24,5 +28,35 @@ public class Utility {
             maxArea = Math.max(area, maxArea);
         }
         return maxArea;
+    }
+
+    public static Integer maxRectangle(List<List<Integer>> matrix) {
+        int n = matrix.size(), m = matrix.getFirst().size();
+        Integer maxArea = 0;
+        List<Integer> prevRow = new ArrayList<>();
+        for (int j = 0; j < m; j += 1) {
+            prevRow.add(0);
+        }
+        for (int i = 0; i < n; i += 1) {
+            List<Integer> histogram = matrix.get(i);
+            List<Integer> rectifiedHistogram = rectifyHistogram(histogram, prevRow);
+            Integer area = getMaxAreaInHistogram(rectifiedHistogram);
+            maxArea = Math.max(maxArea, area);
+            prevRow = rectifiedHistogram;
+        }
+        return maxArea;
+    }
+
+    private static List<Integer> rectifyHistogram(List<Integer> histogram, List<Integer> prevRow) {
+        int m = histogram.size();
+        List<Integer> rectifiedHistogram = new ArrayList<>();
+        for (int j = 0; j < m; j += 1) {
+            if (histogram.get(j).equals(0)) {
+                rectifiedHistogram.add(0);
+            } else {
+                rectifiedHistogram.add(prevRow.get(j) + histogram.get(j));
+            }
+        }
+        return rectifiedHistogram;
     }
 }
