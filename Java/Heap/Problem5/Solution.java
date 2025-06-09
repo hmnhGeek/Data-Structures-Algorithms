887df8cc33c01316180fbc8064968622c6d6f215
hmnhGeek <hmnhsharma97@gmail.com>
2025-06-09 11:42:38 +0530

    Get k smallest in an array

--- Java/Heap/Problem5/Solution.java
+++ Java/Heap/Problem5/Solution.java
@@ -1,3 +1,5 @@
+// Problem link - https://www.geeksforgeeks.org/kth-smallest-largest-element-in-unsorted-array/
+
 package Heap.Problem5;
 
 import java.util.Arrays;
@@ -11,12 +13,23 @@ public class Solution {
     }
 
     public static Integer kSmallest(List<Integer> arr, Integer k) {
+        /*
+            Time complexity is O(n * log(k)) and space complexity is O(k).
+         */
+
+        // heap will take at max k elements and so, O(k) space.
         MaxHeap<Integer> maxHeap = new MaxHeap<>();
+
+        // This will take k * log(k) time.
         for (int i = 0; i < k; i += 1) {
             maxHeap.insert(arr.get(i));
         }
+
+        // loop on the remaining items in n - k iterations.
         int i = k;
         while (i < arr.size()) {
+            // if top element is larger than ith element, pop the top and insert this ith element into the heap.
+            // this is a log(k) operation.
             if (arr.get(i).compareTo(maxHeap.getHeap().getFirst()) < 0) {
                 maxHeap.pop();
                 maxHeap.insert(arr.get(i));
