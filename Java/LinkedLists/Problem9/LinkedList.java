11044e2fc8dcae510445b7c86b0ce5f0c5e0753e
hmnhGeek <hmnhsharma97@gmail.com>
2025-06-13 09:47:06 +0530

    Implemented a linked list class

---
+++ Java/LinkedLists/Problem9/LinkedList.java
@@ -0,0 +1,75 @@
+package LinkedLists.Problem9;
+
+
+class Node<T> {
+    private T data;
+    private Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+}
+
+
+public class LinkedList<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private Integer length;
+
+    public LinkedList() {
+        this.head = this.tail = null;
+        this.length = 0;
+    }
+
+    public boolean isEmpty() {
+        return this.length.equals(0);
+    }
+
+    public void push(T x) {
+        Node<T> node = new Node<>(x);
+        if (isEmpty()) {
+            this.head = this.tail = node;
+        } else {
+            this.tail.setNext(node);
+            this.tail = node;
+        }
+        this.length += 1;
+    }
+
+    public void build(T... args) {
+        for (T x : args) {
+            push(x);
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (isEmpty()) return "[]";
+        if (this.length.equals(1)) return String.format("[%s]", this.head.getData());
+        Node<T> curr = this.head;
+        StringBuilder result = new StringBuilder("[");
+        while (curr != this.tail) {
+            result.append(String.format("%s, ", curr.getData()));
+            curr = curr.getNext();
+        }
+        result.append(String.format("%s]", this.tail.getData()));
+        return result.toString();
+    }
+}
---
+++ Java/LinkedLists/Problem9/Solution.java
@@ -0,0 +1,9 @@
+package LinkedLists.Problem9;
+
+public class Solution {
+    public static void main(String[] args) {
+        LinkedList<Integer> l1 = new LinkedList<>();
+        l1.build(2,1,7,2,9,3,0,9,0,5);
+        System.out.println(l1);
+    }
+}
