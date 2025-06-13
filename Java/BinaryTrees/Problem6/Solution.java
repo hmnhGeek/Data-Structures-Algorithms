c2f5fd720c593d513b79ccfd7f3d57915cd4db36
hmnhGeek <hmnhsharma97@gmail.com>
2025-06-13 10:29:47 +0530

    Iterative inorder of a binary tree

---
+++ Java/BinaryTrees/Problem6/Solution.java
@@ -0,0 +1,81 @@
+package BinaryTrees.Problem6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode<T> {
+    private T data;
+    private TreeNode<T> left;
+    private TreeNode<T> right;
+
+    public TreeNode(T data) {
+        this.data = data;
+        this.left = this.right = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public TreeNode<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode<T> left) {
+        this.left = left;
+    }
+
+    public TreeNode<T> getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode<T> right) {
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public static <T> List<T> getInorderIterative(TreeNode<T> root) {
+        Stack<TreeNode<T>> stack = new Stack<>();
+        TreeNode<T> curr = root;
+        List<T> result = new ArrayList<>();
+        while (curr != null) {
+            stack.push(curr);
+            curr = curr.getLeft();
+        }
+        while (!stack.isEmpty()) {
+            TreeNode<T> treeNode = stack.pop();
+            result.add(treeNode.getData());
+            TreeNode<T> temp = treeNode.getRight();
+            while (temp != null) {
+                stack.push(temp);
+                temp = temp.getLeft();
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // Example 1
+        TreeNode<Integer> n1 = new TreeNode<>(1);
+        TreeNode<Integer> n2 = new TreeNode<>(2);
+        TreeNode<Integer> n3 = new TreeNode<>(3);
+        TreeNode<Integer> n4 = new TreeNode<>(4);
+        TreeNode<Integer> n5 = new TreeNode<>(5);
+        TreeNode<Integer> n6 = new TreeNode<>(6);
+        TreeNode<Integer> n7 = new TreeNode<>(7);
+        TreeNode<Integer> n8 = new TreeNode<>(8);
+        n1.setLeft(n2);
+        n2.setLeft(n4);
+        n1.setRight(n3);
+        n3.setLeft(n5);
+        n3.setRight(n6);
+        n5.setLeft(n7);
+        n5.setRight(n8);
+        System.out.println(getInorderIterative(n1));
+    }
+}
