e0229bc4606b1a158f120b513f81a1047fcb7b30
hmnhGeek <hmnhsharma97@gmail.com>
2025-06-14 16:06:19 +0530

    Implemented a stack class

--- Java/StacksAndQueues/Problem1/Solution.java
+++ Java/StacksAndQueues/Problem1/Solution.java
@@ -1,4 +1,15 @@
 package StacksAndQueues.Problem1;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Solution {
+    public static void main(String[] args) {
+        List<Integer> arr = Arrays.asList(24,79,23,75,0);
+        Stack<Integer> stack = new Stack<>();
+        arr.forEach(stack::push);
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
 }
