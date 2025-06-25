aaac7dc1393ae267e8fa22ff417f44db35b5827f
hmnhGeek <hmnhsharma97@gmail.com>
2025-06-25 19:41:04 +0530

    Longest character replacement

--- Java/SlidingWindows/L8/Solution.java
+++ Java/SlidingWindows/L8/Solution.java
@@ -9,23 +9,44 @@ import java.util.Map;
 
 public class Solution {
     public static String longestCharacterReplacement(String string, Integer k) {
+        /*
+            Time complexity is O(n) and space complexity is O(1).
+         */
+
+        // define window variables
         int n = string.length();
         int left = 0, right = 0;
+
+        // define result variables
         int longestLength = 0;
         int startIndex = -1;
+
+        // define tracking variables
         Map<Character, Integer> d = getCounter(string);
+
+        // while there is ground to cover...
         while (right < n) {
+            // increment the right indexed value
             d.put(string.charAt(right), d.get(string.charAt(right)) + 1);
+
+            // if the replacement count is greater than k
             if (right - left + 1 - Collections.max(d.values()) > k) {
+                // decrement only one unit from left
                 d.put(string.charAt(left), d.get(string.charAt(left)) - 1);
                 left += 1;
             }
+
+            // if the replacement count is within k, update the result variables
             if (right - left + 1 - Collections.max(d.values()) <= k) {
                 longestLength = Math.max(longestLength, right - left + 1);
                 startIndex = left;
             }
+
+            // increment the right index
             right += 1;
         }
+
+        // return the substring
         return startIndex != -1 ? string.substring(startIndex, startIndex + longestLength) : "";
     }
 
