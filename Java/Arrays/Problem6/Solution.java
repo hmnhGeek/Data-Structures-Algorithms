2a332a98539b644e0774109c079b386f7252069f
hmnhGeek <hmnhsharma97@gmail.com>
2025-06-26 19:18:01 +0530

    Union of two lists

--- Java/Arrays/Problem6/Solution.java
+++ Java/Arrays/Problem6/Solution.java
@@ -1,3 +1,5 @@
+// Problem link - https://www.geeksforgeeks.org/problems/union-of-two-arrays3538/1
+
 package Arrays.Problem6;
 
 import java.util.Arrays;
@@ -7,6 +9,9 @@ import java.util.Set;
 
 public class Solution {
     public static Integer union(List<Integer> a1, List<Integer> a2) {
+        /*
+            Time complexity is O(n + m) and space complexity is O(n + m).
+         */
         Set<Integer> set = new HashSet<>();
         set.addAll(a1);
         set.addAll(a2);
@@ -15,5 +20,7 @@ public class Solution {
 
     public static void main(String[] args) {
         System.out.println(union(Arrays.asList(1, 2, 3, 4, 5), Arrays.asList(1, 2, 3)));
+        System.out.println(union(Arrays.asList(85, 25, 1, 32, 54, 6), Arrays.asList(85, 2)));
+        System.out.println(union(Arrays.asList(1, 2, 1, 1, 2), Arrays.asList(2, 2, 1, 2, 1)));
     }
 }
