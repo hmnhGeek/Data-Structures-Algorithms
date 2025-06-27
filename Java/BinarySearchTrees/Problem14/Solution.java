55666b9b86f569ff9668f3b90fc016e2ee98c252
hmnhGeek <hmnhsharma97@gmail.com>
2025-06-27 12:33:18 +0530

    Solving BST problem

--- Java/BinarySearchTrees/Problem14/Solution.java
+++ Java/BinarySearchTrees/Problem14/Solution.java
@@ -1,3 +1,6 @@
+// Problem link - https://www.geeksforgeeks.org/problems/brothers-from-different-root/1
+
+
 package BinarySearchTrees.Problem14;
 
 import java.util.Arrays;
@@ -8,6 +11,10 @@ public class Solution {
         BinarySearchTree<Integer> bst1 = buildTree(Arrays.asList(5, 3, 7, 2, 4, 6, 8));
         BinarySearchTree<Integer> bst2 = buildTree(Arrays.asList(10, 6, 15, 3, 8, 11, 18));
         System.out.println(countPairs(bst1, bst2, 16));
+
+        BinarySearchTree<Integer> bst3 = buildTree(Arrays.asList(1, 3, 2));
+        BinarySearchTree<Integer> bst4 = buildTree(Arrays.asList(3, 2, 4, 1));
+        System.out.println(countPairs(bst3, bst4, 4));
     }
 
     private static void updateCount(Node<Integer> start, BinarySearchTree<Integer> bst, List<Integer> counter, Integer x) {
@@ -23,6 +30,9 @@ public class Solution {
     }
 
     public static Integer countPairs(BinarySearchTree<Integer> binarySearchTree1, BinarySearchTree<Integer> binarySearchTree2, Integer x) {
+        /*
+            Time complexity is O(n * log(m)) and space complexity is O(n + log(m)).
+         */
         List<Integer> counter = Arrays.asList(0);
         updateCount(binarySearchTree1.getRoot(), binarySearchTree2, counter, x);
         return counter.getFirst();
