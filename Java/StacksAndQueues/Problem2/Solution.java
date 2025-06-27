ad46fff2bf109f94a35d05988b1cbb35e1a20894
hmnhGeek <hmnhsharma97@gmail.com>
2025-06-27 12:40:08 +0530

    Implemented a generic queue class

---
+++ Java/StacksAndQueues/Problem2/Solution.java
@@ -0,0 +1,15 @@
+package StacksAndQueues.Problem2;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new Queue<>();
+        for (Integer i : Arrays.asList(1, 2, 3, 4)) {
+            queue.push(i);
+        }
+        while (!queue.isEmpty()) {
+            System.out.println(queue.pop());
+        }
+    }
+}
