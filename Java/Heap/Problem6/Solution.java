5c0e85c8e41d0970ea59f580dfde6fe40a1a27a2
hmnhGeek <hmnhsharma97@gmail.com>
2025-06-28 14:59:33 +0530

    Merge k sorted arrays using min heap

--- Java/Heap/Problem6/Solution.java
+++ Java/Heap/Problem6/Solution.java
@@ -1,3 +1,5 @@
+// Problem Link - https://www.geeksforgeeks.org/problems/merge-k-sorted-arrays/1
+
 package Heap.Problem6;
 
 import java.util.ArrayList;
@@ -25,15 +27,25 @@ public class Solution {
     }
 
     public static <T extends Comparable<T>> List<T> merge(List<List<T>> matrix) {
+        /*
+            Time complexity is thus O(n*m*log(n)) and space complexity is O(n).
+         */
         int n = matrix.size(), m = matrix.getFirst().size();
         MinHeap<HeapElement<T>> minHeap = new MinHeap<>();
+
+        // This will take O(n * log(n)) time and O(n) space.
         for (int i = 0; i < n; i += 1) {
             minHeap.insert(new HeapElement<>(matrix.get(i).getFirst(), i, 0));
         }
         List<T> result = new ArrayList<>();
+
+        // this loop will run for nm times, i.e, for all elements of the matrix.
         while (!minHeap.isEmpty()) {
+            // This will take O(log(n)) time.
             HeapElement<T> poppedElement = minHeap.pop();
             result.add(poppedElement.getData());
+
+            // This will again take O(log(n)) time.
             if (0 <= poppedElement.getColIndex() + 1 && poppedElement.getColIndex() + 1 < m) {
                 minHeap.insert(new HeapElement<>(matrix.get(poppedElement.getRowIndex()).get(poppedElement.getColIndex() + 1), poppedElement.getRowIndex(), poppedElement.getColIndex() + 1));
             }
