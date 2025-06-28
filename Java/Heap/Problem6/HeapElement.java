b529a0b9a3ad83249fd8a066807eac3704465c30
hmnhGeek <hmnhsharma97@gmail.com>
2025-06-28 14:47:31 +0530

    Implemented a heap element to solve the problem

---
+++ Java/Heap/Problem6/HeapElement.java
@@ -0,0 +1,42 @@
+package Heap.Problem6;
+
+public class HeapElement<T extends Comparable<T>> implements Comparable<T> {
+    private T data;
+    private Integer rowIndex;
+    private Integer colIndex;
+
+    public HeapElement(T data, Integer rowIndex, Integer colIndex) {
+        this.data = data;
+        this.rowIndex = rowIndex;
+        this.colIndex = colIndex;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Integer getRowIndex() {
+        return rowIndex;
+    }
+
+    public void setRowIndex(Integer rowIndex) {
+        this.rowIndex = rowIndex;
+    }
+
+    public Integer getColIndex() {
+        return colIndex;
+    }
+
+    public void setColIndex(Integer colIndex) {
+        this.colIndex = colIndex;
+    }
+
+    @Override
+    public int compareTo(T o) {
+        return this.data.compareTo(o);
+    }
+}
---
+++ Java/Heap/Problem6/Solution.java
@@ -0,0 +1,7 @@
+package Heap.Problem6;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
