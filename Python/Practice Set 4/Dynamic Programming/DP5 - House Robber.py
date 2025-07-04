ec817ca8f3dcdcba7416a75619e621b7bc2e610d
hmnhGeek <hmnhsharma97@gmail.com>
2025-07-04 15:22:06 +0530

    House robber

--- Python/Practice Set 4/Dynamic Programming/DP5 - House Robber.py
+++ Python/Practice Set 4/Dynamic Programming/DP5 - House Robber.py
@@ -22,4 +22,34 @@ def recursive():
     print(house_robber([1, 5, 2, 1, 6]))
 
 
+def memoized():
+    """
+        Time complexity is O(n) and space complexity is O(2n).
+    """
+    def solve(arr, i, dp):
+        if i < 0:
+            return 0
+        if dp[i] is not None:
+            return dp[i]
+        left = arr[i] + solve(arr, i - 2, dp)
+        right = solve(arr, i - 1, dp)
+        dp[i] = max(left, right)
+        return dp[i]
+
+    def house_robber(arr):
+        n = len(arr)
+        dp = {i: None for i in range(n)}
+        return solve(arr, n - 1, dp)
+
+    print(house_robber([2, 1, 4, 9]))
+    print(house_robber([1, 2, 4]))
+    print(house_robber([1, 2, 3, 5, 4]))
+    print(house_robber([1, 2, 3, 1, 3, 5, 8, 1, 9]))
+    print(house_robber([2, 7, 9, 3, 1]))
+    print(house_robber([1, 2, 3, 1]))
+    print(house_robber([1, 5, 2, 1, 6]))
+
+
 recursive()
+print()
+memoized()
