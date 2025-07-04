a971f2893c0639452d780fa66ce8cb4de3548793
hmnhGeek <hmnhsharma97@gmail.com>
2025-07-04 15:46:19 +0530

    Longest repeating character replacement

--- Python/Practice Set 7/Sliding Windows/L8 - Longest Repeating Character Replacement.py
+++ Python/Practice Set 7/Sliding Windows/L8 - Longest Repeating Character Replacement.py
@@ -9,20 +9,36 @@ class Solution:
             Time complexity is O(n) and space complexity is O(26).
         """
 
+        # define window variables
         n = len(string)
         left = right = 0
+
+        # define tracking variables
         d = {i: 0 for i in string}
+
+        # define result variables
         longest_length = 0
         start_index = -1
+
+        # while there is ground to cover...
         while right < n:
+            # increment the right index value
             d[string[right]] += 1
+
+            # if more than k character replacement is required, shrink one unit from left.
             if sum(d.values()) - max(d.values()) > k:
                 d[string[left]] -= 1
                 left += 1
+
+            # else update the result variables.
             if sum(d.values()) - max(d.values()) <= k:
                 longest_length = max(longest_length, right - left + 1)
                 start_index = left
+
+            # increment the right index
             right += 1
+
+        # return the substring.
         return string[start_index:start_index+longest_length] if start_index != -1 else ""
 
 
