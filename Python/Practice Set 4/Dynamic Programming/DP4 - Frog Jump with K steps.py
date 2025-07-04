a4a3c3c994a2b4023067139be0f3b96c908986e0
hmnhGeek <hmnhsharma97@gmail.com>
2025-07-04 15:04:44 +0530

    Frog jump with k distance

--- Python/Practice Set 4/Dynamic Programming/DP4 - Frog Jump with K steps.py
+++ Python/Practice Set 4/Dynamic Programming/DP4 - Frog Jump with K steps.py
@@ -24,4 +24,36 @@ def recursive():
     print(frog_jump([40, 10, 20, 70, 80, 10, 20, 70, 80, 60], 4))
 
 
+def memoized():
+    """
+        Time complexity is O(n) and space complexity is O(2n).
+    """
+    def solve(arr, i, k, dp):
+        if i == 0:
+            return 0
+        if dp[i] is not None:
+            return dp[i]
+        min_energy = 1e6
+        for j in range(1, k + 1):
+            if i - j >= 0:
+                min_energy = min(min_energy, abs(arr[i - j] - arr[i]) + solve(arr, i - j, k, dp))
+        dp[i] = min_energy
+        return dp[i]
+
+    def frog_jump(arr, k):
+        n = len(arr)
+        dp = {i: None for i in range(n)}
+        return solve(arr, n - 1, k, dp)
+
+    print(frog_jump([10, 30, 50, 60, 20, 10], 3))
+    print(frog_jump([10, 30, 50, 60, 20, 10], 2))
+    print(frog_jump([10, 30, 50, 60, 20, 10], 4))
+    print(frog_jump([10, 30, 50, 60, 20, 10], 1))
+    print(frog_jump([10, 30, 40, 50, 20], 3))
+    print(frog_jump([10, 20, 10], 1))
+    print(frog_jump([40, 10, 20, 70, 80, 10, 20, 70, 80, 60], 4))
+
+
 recursive()
+print()
+memoized()
