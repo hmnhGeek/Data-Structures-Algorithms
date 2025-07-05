d1726a117f8f974afa13ea2426a31f0cdf3b5f3b
hmnhGeek <hmnhsharma97@gmail.com>
2025-07-05 09:40:15 +0530

    Union of two arrays

---
+++ Python/Practice Set 1/Arrays/union_of_arrays.py
@@ -0,0 +1,21 @@
+class Solution:
+    @staticmethod
+    def union(arr1, arr2):
+        set1 = set(arr1)
+        set2 = set(arr2)
+        intersection = set1.intersection(set2)
+        result = []
+        for i in set1:
+            if i not in intersection:
+                result.append(i)
+        for i in set2:
+            if i not in intersection:
+                result.append(i)
+        for i in intersection:
+            result.append(i)
+        return result
+
+
+print(Solution.union([1, 2, 3, 2, 1], [3, 2, 2, 3, 3, 2]))
+print(Solution.union([1, 2, 3], [4, 5, 6]))
+print(Solution.union([1, 2, 1, 1, 2], [2, 2, 1, 2, 1]))
