2ccc36ba06b950a4d67221ee3ec973c0b84bd925
hmnhGeek <hmnhsharma97@gmail.com>
2025-07-05 09:11:47 +0530

    Implemented a queue class

---
+++ Python/Practice Set 4/Graphs/G11 - Detect Cycle using BFS.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Queue:
+    def __init__(self):
+        self.head = self.tail = None
+        self.length = 0
+
+    def is_empty(self):
+        return self.length == 0
+
+    def push(self, x):
+        node = Node(x)
+        if self.is_empty():
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+        self.length += 1
+
+    def pop(self):
+        if self.is_empty():
+            return
+        item = self.head.data
+        self.head = self.head.next
+        node = self.head
+        del node
+        self.length -= 1
+        return item
+
