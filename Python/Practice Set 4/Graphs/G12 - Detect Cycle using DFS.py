368f822105dd3e01891e248c441d61beb6c402c5
hmnhGeek <hmnhsharma97@gmail.com>
2025-07-05 09:27:05 +0530

    detect cycle in a graph using DFS

---
+++ Python/Practice Set 4/Graphs/G12 - Detect Cycle using DFS.py
@@ -0,0 +1,96 @@
+class Solution:
+    @staticmethod
+    def dfs(graph, node, parent, visited):
+        visited[node] = True
+        for adj_node in graph[node]:
+            if adj_node == parent:
+                continue
+            elif not visited[adj_node]:
+                if Solution.dfs(graph, adj_node, node, visited):
+                    return True
+            else:
+                return True
+        return False
+
+    @staticmethod
+    def detect_cycle(graph):
+        visited = {i: False for i in graph}
+        for node in graph:
+            if not visited[node]:
+                if Solution.dfs(graph, node, None, visited):
+                    return True
+        return False
+
+
+print(
+    Solution.detect_cycle(
+        {
+            1: [2, 3],
+            2: [1, 5],
+            3: [1, 4, 6],
+            4: [3],
+            5: [2, 7],
+            6: [3, 7],
+            7: [5, 6]
+        }
+    )
+)
+
+print(
+    Solution.detect_cycle(
+        {
+            1: [2, 3],
+            2: [1, 5],
+            3: [1, 4, 6],
+            4: [3],
+            5: [2, 7],
+            6: [3],
+            7: [5]
+        }
+    )
+)
+
+print(
+    Solution.detect_cycle(
+        {
+            0: [1, 2],
+            1: [0, 2],
+            2: [0, 1, 3],
+            3: [2]
+        }
+    )
+)
+
+print(
+    Solution.detect_cycle(
+        {
+            0: [1],
+            1: [0, 2],
+            2: [1, 3],
+            3: [2]
+        }
+    )
+)
+
+print(
+    Solution.detect_cycle(
+        {
+            0: [],
+            1: [2],
+            2: [1, 3],
+            3: [2]
+        }
+    )
+)
+
+print(
+    Solution.detect_cycle(
+        {
+            0: [1],
+            1: [0, 2, 4],
+            2: [1, 3],
+            3: [2, 4],
+            4: [1, 3]
+        }
+    )
+)
