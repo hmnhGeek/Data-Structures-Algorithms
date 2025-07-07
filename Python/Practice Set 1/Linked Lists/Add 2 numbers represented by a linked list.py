7dd1155c6f74f45c5430045ed5cd71e151b7d19e
hmnhGeek <hmnhsharma97@gmail.com>
2025-07-07 09:51:13 +0530

    implemented a linked list class

---
+++ Python/Practice Set 1/Linked Lists/Add 2 numbers represented by a linked list.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = self.tail = None
+        self.length = 0
+
+    def push(self, x):
+        node = Node(x)
+        if self.length == 0:
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+        self.length += 1
+
+    def build(self, *args):
+        for i in args:
+            self.push(i)
+
+    def __str__(self):
+        if self.length == 0:
+            return "[]"
+        if self.length == 1:
+            return f"[{self.head.data}]"
+        if self.length == 2:
+            return f"[{self.head.data}, {self.tail.data}]"
+        result = "["
+        curr = self.head
+        while curr != self.tail:
+            result += f"{curr.data}, "
+            curr = curr.next
+        result += f"{self.tail.data}]"
+        return result
+
+
+l = LinkedList()
+l.build(1, 2, 3, 4, 5, 6, 6)
+print(l)
