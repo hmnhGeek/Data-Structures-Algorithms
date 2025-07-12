8c6cde7362745bbf5ad24d4c0a2a445e238c2292
hmnhGeek <hmnhsharma97@gmail.com>
2025-07-12 21:26:36 +0530

    Intersection of two linked lists

--- Java/LinkedLists/Problem11/Solution.java
+++ Java/LinkedLists/Problem11/Solution.java
@@ -1,3 +1,6 @@
+// Problem link - https://www.geeksforgeeks.org/problems/intersection-of-two-sorted-linked-lists/1
+
+
 package LinkedLists.Problem11;
 
 public class Solution {
@@ -7,9 +10,18 @@ public class Solution {
         LinkedList<Integer> l2 = new LinkedList<>();
         l2.build(2, 4, 6, 8);
         System.out.println(intersect(l1, l2));
+
+        LinkedList<Integer> l3 = new LinkedList<>();
+        LinkedList<Integer> l4 = new LinkedList<>();
+        l3.build(10, 20, 40, 50);
+        l4.build(15, 40);
+        System.out.println(intersect(l3, l4));
     }
 
     public static <T extends Comparable<T>> LinkedList<T> intersect(LinkedList<T> l1, LinkedList<T> l2) {
+        /*
+            Time complexity is O(n + m) and space complexity is O(1).
+         */
         Node<T> curr1 = l1.getHead(), curr2 = l2.getHead();
         LinkedList<T> result = new LinkedList<>();
         while (curr1 != null && curr2 != null) {
