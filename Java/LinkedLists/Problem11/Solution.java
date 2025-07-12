8b975eb0f3ee8f507d60198eb6bf7fb620e2e25a
hmnhGeek <hmnhsharma97@gmail.com>
2025-07-12 21:20:42 +0530

    created a linked list class

---
+++ Java/LinkedLists/Problem11/Solution.java
@@ -0,0 +1,9 @@
+package LinkedLists.Problem11;
+
+public class Solution {
+    public static void main(String[] args) {
+        LinkedList<Integer> l1 = new LinkedList<>();
+        l1.build();
+        System.out.println(l1);
+    }
+}
