003c76a705b19e608bc79c2698d444985df80a11
hmnhGeek <hmnhsharma97@gmail.com>
2025-07-13 13:41:44 +0530

    added printing functionality

--- Java/BinarySearchTrees/Problem15/BinarySearchTree.java
+++ Java/BinarySearchTrees/Problem15/BinarySearchTree.java
@@ -219,4 +219,24 @@ public class BinarySearchTree<T extends Comparable<T>> {
         }
         return getNode(start.getLeft(), x);
     }
+
+    public void show(Node<T> start) {
+        if (start != null) {
+            show(start.getLeft());
+            StringBuilder stringBuilder = new StringBuilder(String.format("Data = %s", start.getData()));
+            if (start == this.root) {
+                stringBuilder.append(" (root),");
+            } else {
+                stringBuilder.append(",");
+            }
+            stringBuilder.append(String.format(" size = %d, height = %d, diameter = %d", start.getSize(), start.getHeight(), start.getDiameter()));
+            System.out.println(stringBuilder);
+            show(start.getRight());
+        }
+    }
+
+    public void show() {
+        show(this.root);
+        System.out.println();
+    }
 }
