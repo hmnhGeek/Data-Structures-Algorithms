dbe9ae4cd3cc298884f8e431f127ef8d9c84ff70
hmnhGeek <hmnhsharma97@gmail.com>
2025-07-19 14:18:01 +0530

    Get upper bound using binary search

--- Java/BinarySearch/BS1/Solution.java
+++ Java/BinarySearch/BS1/Solution.java
@@ -11,13 +11,37 @@ public class Solution {
         System.out.println(getLowerBound(Arrays.asList(1, 2, 2, 3, 3, 5), 0));
         System.out.println(getLowerBound(Arrays.asList(1, 2, 2, 3, 3, 5), 2));
         System.out.println(getLowerBound(Arrays.asList(1, 2, 2, 3, 3, 5), 7));
+        System.out.println("Upper Bound");
+        System.out.println(getUpperBound(Arrays.asList(2, 4, 6, 7), 5));
+        System.out.println(getUpperBound(Arrays.asList(1, 4, 7, 8, 10), 7));
+        System.out.println(getUpperBound(Arrays.asList(1, 2, 5, 6, 10), 10));
+        System.out.println(getUpperBound(Arrays.asList(1, 5, 5, 7, 7, 9, 10), 5));
     }
 
     public static <T extends Comparable<T>> Integer getLowerBound(List<T> arr, T n) {
+        /*
+            Time complexity is O(log(arr.size())) and space complexity is O(1).
+         */
         int low = 0, high = arr.size() - 1;
         while (low <= high) {
             int mid = (low + (high - low)/2);
             if (arr.get(mid).compareTo(n) >= 0) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    public static <T extends Comparable<T>> Integer getUpperBound(List<T> arr, T n) {
+        /*
+            Time complexity is O(log(arr.size())) and space complexity is O(1).
+         */
+        int low = 0, high = arr.size() - 1;
+        while (low <= high) {
+            int mid = (low + (high - low)/2);
+            if (arr.get(mid).compareTo(n) > 0) {
                 high = mid - 1;
             } else {
                 low = mid + 1;
