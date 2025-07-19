951a6fe6b7f7b6da5148ec719421dddf6d16ed0f
hmnhGeek <hmnhsharma97@gmail.com>
2025-07-19 14:47:08 +0530

    Get first and last occurrences indices

--- Java/BinarySearch/BS3/Solution.java
+++ Java/BinarySearch/BS3/Solution.java
@@ -1,5 +1,6 @@
 package BinarySearch.BS3;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
@@ -27,11 +28,25 @@ public class Solution {
         while (low <= high) {
             int mid = (low + (high - low)/2);
             if (arr.get(mid).compareTo(x) <= 0) {
-                low = mid + 1
+                low = mid + 1;
             } else {
                 high = mid - 1;
             }
         }
         return 0 <= high && high < arr.size() ? high : -1;
+    }
+
+    public static <T extends Comparable<T>> List<Integer> getOccurrences(List<T> arr, T x) {
+        Integer first = getFirstOccurrence(arr, x);
+        Integer last = getLastOccurrence(arr, x);
+        if (arr.get(first) != x || arr.get(last) != x) return List.of(-1, -1);
+        return List.of(first, last);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getOccurrences(Arrays.asList(2, 4, 6, 8, 8, 8, 11, 13), 8));
+        System.out.println(getOccurrences(Arrays.asList(0, 1, 1, 5), 1));
+        System.out.println(getOccurrences(Arrays.asList(0, 0, 1, 1, 2, 2, 2, 2), 2));
+        System.out.println(getOccurrences(Arrays.asList(1, 3, 3, 5), 2));
     }
 }
