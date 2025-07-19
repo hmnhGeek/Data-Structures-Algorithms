dc6de1ae83d6aa070fa32f539af59ed6405c7d30
hmnhGeek <hmnhsharma97@gmail.com>
2025-07-19 14:42:51 +0530

    Get last occurrence index

--- Java/BinarySearch/BS3/Solution.java
+++ Java/BinarySearch/BS3/Solution.java
@@ -18,4 +18,20 @@ public class Solution {
         }
         return 0 <= low && low < arr.size() ? low : -1;
     }
+
+    public static <T extends Comparable<T>> Integer getLastOccurrence(List<T> arr, T x) {
+        /*
+            Time complexity is O(log(n)) and space complexity is O(1).
+         */
+        int low = 0, high = arr.size() - 1;
+        while (low <= high) {
+            int mid = (low + (high - low)/2);
+            if (arr.get(mid).compareTo(x) <= 0) {
+                low = mid + 1
+            } else {
+                high = mid - 1;
+            }
+        }
+        return 0 <= high && high < arr.size() ? high : -1;
+    }
 }
