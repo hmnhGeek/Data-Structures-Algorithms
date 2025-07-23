2d110afe4a95337bd3048643b6529d468369c6fc
hmnhGeek <hmnhsharma97@gmail.com>
2025-07-23 08:30:25 +0530

    Implement lower bound in a sorted array

---
+++ Python/Practice Set 4/Binary Search/BS2 -  Implement Lower Bound and Upper Bound.py
@@ -0,0 +1,25 @@
+class Solution:
+    @staticmethod
+    def get_lower_bound(arr, x):
+        """
+            Time complexity is O(log(n)) and space complexity is O(1).
+        """
+        low, high = 0, len(arr) - 1
+        while low <= high:
+            mid = int(low + (high - low)//2)
+            if arr[mid] >= x:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return low if low in range(len(arr)) else -1
+
+
+print("Lower Bound")
+print(Solution.get_lower_bound([3, 5, 8, 15, 19], 5))
+print(Solution.get_lower_bound([3, 5, 8, 15, 19], 8))
+print(Solution.get_lower_bound([3, 5, 8, 15, 19], 9))
+print(Solution.get_lower_bound([3, 5, 8, 15, 19], 800))
+print(Solution.get_lower_bound([3, 5, 8, 15, 19], 2))
+print(Solution.get_lower_bound([1, 2, 2, 3, 3, 5], 0))
+print(Solution.get_lower_bound([1, 2, 2, 3, 3, 5], -10))
+print()
