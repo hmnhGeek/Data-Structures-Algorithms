e7daf0106996f276ace7689f213cce9725b115ee
hmnhGeek <hmnhsharma97@gmail.com>
2025-07-25 09:01:14 +0530

    Rotate array by one element

--- Python/Practice Set 1/Arrays/Rotate Array by One.py
+++ Python/Practice Set 1/Arrays/Rotate Array by One.py
@@ -1,6 +1,12 @@
+# Problem link - https://www.geeksforgeeks.org/problems/cyclically-rotate-an-array-by-one2614/1
+
+
 class Solution:
     @staticmethod
     def rotate(arr):
+        """
+            Time complexity is O(n) and space complexity is O(1).
+        """
         n = len(arr)
         last_elem = arr[-1]
         for i in range(n - 1, 0, -1):
