053fa0418eae739b9fee206cb335e14d26e190f2
hmnhGeek <hmnhsharma97@gmail.com>
2025-07-27 12:17:36 +0530

    Implemented a linked list class

---
+++ Python/Practice Set 1/Linked Lists/intersection of two sorted linked lists.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = self.tail = None
+        self.length = 0
+
+    def is_empty(self):
+        return self.length == 0
+
+    def push(self, x):
+        node = Node(x)
+        if self.is_empty():
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+        self.length += 1
+
+    def build(self, *args):
+        for i in args:
+            self.push(i)
+
+    def __str__(self):
+        if self.is_empty():
+            return "[]"
+        result = "["
+        curr = self.head
+        while curr != self.tail:
+            result += f"{curr.data}, "
+            curr = curr.next
+        result += f"{self.tail.data}]"
+        return result
+
+
+l = LinkedList()
+l.build(1, 2)
+print(l)
