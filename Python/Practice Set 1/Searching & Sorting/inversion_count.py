6e7c0e40ebc5d9b44f8a38aa514d9ef109081de8
hmnhGeek <hmnhsharma97@gmail.com>
2025-07-27 13:10:03 +0530

    Count inversions using merge sort

---
+++ Python/Practice Set 1/Searching & Sorting/inversion_count.py
@@ -0,0 +1,43 @@
+class Solution:
+    @staticmethod
+    def get_inversion_count(arr):
+        count = [0]
+        Solution._merge_sort(arr, 0, len(arr) - 1, count)
+        return count[0]
+
+    @staticmethod
+    def _merge_sort(arr, low, high, count):
+        if low >= high:
+            return
+        mid = int(low + (high - low)/2)
+        Solution._merge_sort(arr, low, mid, count)
+        Solution._merge_sort(arr, mid + 1, high, count)
+        arr[low:high+1] = Solution._merge(arr, low, high, count)
+
+    @staticmethod
+    def _merge(arr, low, high, count):
+        mid = int(low + (high - low)/2)
+        left, right = arr[low:mid+1], arr[mid+1:high+1]
+        i, j = 0, 0
+        merged = []
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                merged.append(left[i])
+                i += 1
+            else:
+                merged.append(right[j])
+                count[0] += (len(left) - i)
+                j += 1
+        while i < len(left):
+            merged.append(left[i])
+            i += 1
+        while j < len(right):
+            merged.append(right[j])
+            j += 1
+        return merged
+
+
+print(Solution.get_inversion_count([2, 4, 1, 3, 5]))
+print(Solution.get_inversion_count([2, 3, 4, 5, 6]))
+print(Solution.get_inversion_count([10, 10, 10]))
+print(Solution.get_inversion_count([5, 3, 2, 4, 1]))
