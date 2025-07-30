bd999560ff9fd69ddc9c8638b6f2174cf51ed2a4
hmnhGeek <hmnhsharma97@gmail.com>
2025-07-30 09:30:58 +0530

    Binary Subarrays with Sum

---
+++ Java/SlidingWindows/L9/Solution.java
@@ -0,0 +1,38 @@
+package SlidingWindows.L9;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    private static Integer sumLessThanEqualTo(List<Integer> arr, Integer sum) {
+        if (sum < 0) {
+            return 0;
+        }
+        int sumTracker = 0;
+        int count = 0;
+        int left = 0, right = 0;
+        int n = arr.size();
+        while (right < n) {
+            sumTracker += arr.get(right);
+            while (sumTracker > sum) {
+                sumTracker -= arr.get(left);
+                left += 1;
+            }
+            count += (right - left + 1);
+            right += 1;
+        }
+        return count;
+    }
+
+    public static Integer binarySubarraysCount(List<Integer> arr, Integer sum) {
+        return sumLessThanEqualTo(arr, sum) - sumLessThanEqualTo(arr, sum - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Solution.binarySubarraysCount(Arrays.asList(1, 0, 1, 0, 1), 2));
+        System.out.println(Solution.binarySubarraysCount(Arrays.asList(0, 0, 0, 0, 0), 0));
+        System.out.println(Solution.binarySubarraysCount(Arrays.asList(1, 0, 1, 1, 0, 1), 2));
+        System.out.println(Solution.binarySubarraysCount(Arrays.asList(1, 0, 1, 1, 1, 0, 1), 3));
+        System.out.println(Solution.binarySubarraysCount(Arrays.asList(1, 1, 0, 1, 1), 5));
+    }
+}
