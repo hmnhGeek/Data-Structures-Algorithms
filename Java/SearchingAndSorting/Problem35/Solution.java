47551de146e029a3b314521ffa141f7983e3840a
hmnhGeek <hmnhsharma97@gmail.com>
2025-07-31 11:32:20 +0530

    Merge sort in place

---
+++ Java/SearchingAndSorting/Problem35/Solution.java
@@ -0,0 +1,12 @@
+package SearchingAndSorting.Problem35;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        List<Integer> l1 = Arrays.asList(1,2,9,6,4,6,2);
+        Utils.sort(l1);
+        System.out.println(l1);
+    }
+}
