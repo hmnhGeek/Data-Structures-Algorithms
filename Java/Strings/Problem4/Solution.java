602163c81eb6bfb1bf41ef36775c4774e90c4bc8
hmnhGeek <hmnhsharma97@gmail.com>
2025-08-01 12:00:03 +0530

    Check if a string is a valid shuffle of two distinct strings

---
+++ Java/Strings/Problem4/Solution.java
@@ -0,0 +1,26 @@
+package Strings.Problem4;
+
+public class Solution {
+    public static boolean isValidShuffle(String s1, String s2, String s) {
+        int n1 = s1.length(), n2 = s2.length(), n = s.length();
+        if (n1 + n2 != n) return false;
+        int i = 0, j = 0, k = 0;
+        while (k < n) {
+            if (i < n1 && s1.charAt(i) == s.charAt(k)) {
+                i += 1;
+            } else if (j < n2 && s2.charAt(j) == s.charAt(k)) {
+                j += 1;
+            } else {
+                return false;
+            }
+            k += 1;
+        }
+        if (i < n1 || j < n2) return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValidShuffle("xy", "12", "1xy2"));
+        System.out.println(isValidShuffle("XY", "12", "Y1X2"));
+    }
+}
