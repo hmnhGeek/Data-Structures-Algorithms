bdd5efc886decb42db9691aac2d2f087405caaf4
hmnhGeek <hmnhsharma97@gmail.com>
2025-08-01 09:52:32 +0530

    Implemented heap interface

---
+++ Java/Matrix/Problem9/Heap/Heap.java
@@ -0,0 +1,45 @@
+package Matrix.Problem9.Heap;
+
+import java.util.Collections;
+import java.util.List;
+
+public interface Heap<T extends Comparable<T>> {
+    List<T> getHeap();
+    Integer getChildIndex(Integer lci, Integer rci);
+    void heapifyUp(Integer startIndex);
+    void heapifyDown(Integer pi);
+
+    default Boolean isEmpty() {
+        return getHeap().isEmpty();
+    }
+
+    default Integer getLci(Integer pi) {
+        int lci = 2*pi + 1;
+        return 0 <= lci && lci < getHeap().size() ? lci : null;
+    }
+
+    default Integer getRci(Integer pi) {
+        int rci = 2*pi + 2;
+        return 0 <= rci && rci < getHeap().size() ? rci : null;
+    }
+
+    default Integer getPi(Integer ci) {
+        if (ci == 0) return null;
+        int pi = (ci - 1)/2;
+        return 0 <= pi && pi < getHeap().size() ? pi : null;
+    }
+
+    default void insert(T x) {
+        getHeap().add(x);
+        heapifyUp(getHeap().size() - 1);
+    }
+
+    default T pop() {
+        if (isEmpty()) return null;
+        T item = getHeap().getFirst();
+        Collections.swap(getHeap(), 0, getHeap().size() - 1);
+        getHeap().removeLast();
+        heapifyDown(0);
+        return item;
+    }
+}
