8dedcd252383c2071ffc4b8667fe8c5b25533f72
hmnhGeek <hmnhsharma97@gmail.com>
2025-08-06 17:44:47 +0530

    K-th Largest Sum Contiguous Subarray

--- Java/Heap/Problem8/Solution.java
+++ Java/Heap/Problem8/Solution.java
@@ -1,3 +1,8 @@
+// Problem link - https://www.geeksforgeeks.org/dsa/k-th-largest-sum-contiguous-subarray/#naive-approach-2-using-prefix-sum-and-sorting-on2-log-n-time-and-on-2-space
+// Solution - https://www.youtube.com/watch?v=xhAL9uxUhGw
+// Prefix sum logic - https://www.youtube.com/watch?v=PhgtNY_-CiY
+
+
 package Heap.Problem8;
 
 import java.util.ArrayList;
@@ -15,14 +20,34 @@ public class Solution {
     }
 
     public static Integer getKthLargest(List<Integer> arr, Integer k) {
+        /*
+            Time complexity is O(n^2 * log(k)) and space complexity is O(n).
+         */
+
         List<Integer> prefixSums = generatePrefixSums(arr);
         MinHeap<Integer> minHeap = new MinHeap<>();
+
+        // loop to calculate the contiguous subarray sums position-wise. This will take n^2 iterations.
         for (int i = 1; i < arr.size() + 1; i += 1) {
+            // loop to traverse all positions that
+            // form contiguous subarray
             for (int j = i; j < arr.size() + 1; j += 1) {
+                // calculates the contiguous subarray
+                // sums from j to i index
                 Integer x = prefixSums.get(j) - prefixSums.get(i - 1);
+
+                // The following ops are of order log(k).
+
+                // if min heap has less than k elements,
+                // then simply push it
                 if (minHeap.getHeap().size() < k) {
                     minHeap.insert(x);
                 } else {
+                    // it the min heap has equal to
+                    // k elements then just check
+                    // if the largest kth element is
+                    // smaller than x then insert
+                    // else its of no use
                     if (minHeap.getHeap().getFirst() < x) {
                         minHeap.pop();
                         minHeap.insert(x);
@@ -30,6 +55,8 @@ public class Solution {
                 }
             }
         }
+        // the top element will be then kth
+        // largest element
         return minHeap.getHeap().getFirst();
     }
 
