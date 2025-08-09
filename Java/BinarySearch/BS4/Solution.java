0a0976ef5d5c2af5ab48c12b87bdaaf9f750861a
hmnhGeek <hmnhsharma97@gmail.com>
2025-08-09 10:42:11 +0530

    Search in a rotated sorted array

--- Java/BinarySearch/BS4/Solution.java
+++ Java/BinarySearch/BS4/Solution.java
@@ -1,3 +1,7 @@
+// Problem link - https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+// Solution - https://www.youtube.com/watch?v=5qGrJbHhqFs&list=PLgUwDviBIf0pMFMWuuvDNMAkoQFi-h0ZF&index=5
+
+
 package BinarySearch.BS4;
 
 import java.util.Arrays;
@@ -5,6 +9,9 @@ import java.util.List;
 
 public class Solution {
     public static Integer findInRotatedSortedArray(List<Integer> arr, Integer x) {
+        /*
+            Time complexity is O(log(n)) and space complexity is O(1).
+         */
         Integer low = 0, high = arr.size() - 1;
         while (low <= high) {
             Integer mid = (low + (high - low)/2);
@@ -27,5 +34,8 @@ public class Solution {
     public static void main(String[] args) {
         System.out.println(findInRotatedSortedArray(Arrays.asList(7, 8, 9, 1, 2, 3, 4, 5, 6), 1));
         System.out.println(findInRotatedSortedArray(Arrays.asList(7, 8, 9, 1, 2, 3, 4, 5), 6));
+        System.out.println(findInRotatedSortedArray(Arrays.asList(4,5,6,7,0,1,2), 0));
+        System.out.println(findInRotatedSortedArray(Arrays.asList(4,5,6,7,0,1,2), 3));
+        System.out.println(findInRotatedSortedArray(Arrays.asList(1), 0));
     }
 }
