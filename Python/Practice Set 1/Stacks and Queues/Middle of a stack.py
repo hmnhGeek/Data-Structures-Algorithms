689969c6760251a1dd286334054d96e3e9661f78
hmnhGeek <hmnhsharma97@gmail.com>
2025-08-16 22:09:41 +0530

    Implemented a deque class

--- Python/Practice Set 1/Stacks and Queues/Middle of a stack.py
+++ Python/Practice Set 1/Stacks and Queues/Middle of a stack.py
@@ -29,3 +29,57 @@ class Stack:
         self.length -= 1
         return item
 
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = self.right = None
+
+
+class Deque:
+    def __init__(self):
+        self.head = self.tail = None
+        self.length = 0
+
+    def is_empty(self):
+        return self.length == 0
+
+    def push_front(self, x):
+        node = Node(x)
+        if self.is_empty():
+            self.head = self.tail = node
+        else:
+            self.head.prev = node
+            node.next = self.head
+            self.head = node
+        self.length += 1
+
+    def push_back(self, x):
+        node = Node(x)
+        if self.is_empty():
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = node
+        self.length += 1
+
+    def pop_front(self):
+        if self.is_empty():
+            return
+        item = self.head.data
+        self.head = self.head.next
+        if self.head is not None:
+            self.head.prev = None
+        self.length -= 1
+        return item
+
+    def pop_back(self):
+        if self.is_empty():
+            return
+        item = self.tail.data
+        self.tail = self.tail.prev
+        if self.tail is not None:
+            self.tail.next = None
+        return item
+
