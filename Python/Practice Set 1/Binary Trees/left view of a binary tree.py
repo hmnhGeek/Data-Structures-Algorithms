411511a39b01ea559c9c122e88ab96076648080f
hmnhGeek <hmnhsharma97@gmail.com>
2025-08-16 23:06:25 +0530

    Left view of a binary tree using a queue

--- Python/Practice Set 1/Binary Trees/left view of a binary tree.py
+++ Python/Practice Set 1/Binary Trees/left view of a binary tree.py
@@ -29,3 +29,50 @@ class Queue:
         self.length -= 1
         return item
 
+
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = self.right = None
+
+
+class Solution:
+    @staticmethod
+    def get_iterative_view(root: TreeNode):
+        queue = Queue()
+        queue.push((root, 0))
+        current_level = 0
+        result = []
+        while not queue.is_empty():
+            node, level = queue.pop()
+            if level == current_level:
+                result.append(node.data)
+                current_level += 1
+            if node.left is not None:
+                queue.push((node.left, level + 1))
+            if node.right is not None:
+                queue.push((node.right, level + 1))
+        return result
+
+
+# Example 1
+n1, n2, n3, n4, n5, n6, n7 = TreeNode(1), TreeNode(2), TreeNode(3), TreeNode(4), TreeNode(5), TreeNode(6), TreeNode(7)
+n1.left = n2
+n2.left = n4
+n5.left = n6
+n1.right = n3
+n3.right = n7
+n2.right = n5
+print(Solution.get_iterative_view(n1))
+
+print()
+# Example 2
+n1, n2, n3, n4, n5, n6, n7, n8 = TreeNode(1), TreeNode(2), TreeNode(3), TreeNode(4), TreeNode(5), TreeNode(6), TreeNode(7), TreeNode(8)
+n1.left = n2
+n2.left = n4
+n4.right = n8
+n2.right = n5
+n1.right = n3
+n3.left = n6
+n3.right = n7
+print(Solution.get_iterative_view(n1))
