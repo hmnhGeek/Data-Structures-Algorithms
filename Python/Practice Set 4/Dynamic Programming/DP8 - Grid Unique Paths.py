fd00e6892b4645f3e549f49b2d59979a9bc1b65f
hmnhGeek <hmnhsharma97@gmail.com>
2025-08-20 12:37:56 +0530

    Memoized grid unique paths

--- Python/Practice Set 4/Dynamic Programming/DP8 - Grid Unique Paths.py
+++ Python/Practice Set 4/Dynamic Programming/DP8 - Grid Unique Paths.py
@@ -21,4 +21,33 @@ def recursive():
     print(get_paths(2, 3))
 
 
+def memoized():
+    """
+        Time complexity is O(nm) and space complexity is O(n + m + nm).
+    """
+
+    def get_paths(n, m):
+        dp = {i: {j: None for j in range(m)} for i in range(n)}
+        return solve(n - 1, m - 1, dp)
+
+    def solve(i, j, dp):
+        if i < 0 or j < 0:
+            return 0
+        if i == 0 and j == 0:
+            return 1
+        if dp[i][j] is not None:
+            return dp[i][j]
+        up = solve(i - 1, j, dp)
+        down = solve(i, j - 1, dp)
+        dp[i][j] = up + down
+        return dp[i][j]
+
+    print(get_paths(3, 7))
+    print(get_paths(3, 2))
+    print(get_paths(2, 2))
+    print(get_paths(2, 3))
+
+
 recursive()
+print()
+memoized()
