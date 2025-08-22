657ad954604771155ed38fc0cac3f6d341aa213a
hmnhGeek <hmnhsharma97@gmail.com>
2025-08-22 11:52:15 +0530

    Reorganize strings using max heap

--- Java/Heap/Problem9/Solution.java
+++ Java/Heap/Problem9/Solution.java
@@ -40,22 +40,43 @@ class HeapElement implements Comparable<HeapElement> {
 
 public class Solution {
     public static String reorganizeStrings(String string) {
+        // get the frequencies of the characters in the string in O(n) time and O(n) space.
         Map<Character, Integer> characterCounts = getCharacterCount(string);
+
+        // initialize a max heap and push the characters with their frequencies into it. This
+        // will take O(n * log(n)) time and O(n) space.
         MaxHeap<HeapElement> maxHeap = new MaxHeap<>();
         for (Character character : characterCounts.keySet()) {
             maxHeap.insert(new HeapElement(character, characterCounts.get(character)));
         }
+
+        // store a prev element to avoid adding adjacent duplicates into result string.
         HeapElement prev = null;
         StringBuilder result = new StringBuilder();
+
+        // start popping elements from max heap. This will take n iterations.
         while (!maxHeap.isEmpty()) {
+            // This will take O(log(n)) time.
             HeapElement poppedElement = maxHeap.pop();
+
+            // add the character to the result string.
             result.append(poppedElement.getCharacter());
+
+            // if there is a prev character with still having frequency > 0, push it to max heap
+            // in O(log(n)) time.
             if (prev != null && prev.getCount() > 0) {
                 maxHeap.insert(prev);
             }
+
+            // update the prev element with new frequency by replacing it with current character.
             prev = new HeapElement(poppedElement.getCharacter(), poppedElement.getCount() - 1);
         }
+
+        // if some character is still left even after emptying the max heap, then it means no valid
+        // result exists.
         if (prev != null && prev.getCount() > 0) return "";
+
+        // else return the result.
         return result.toString();
     }
 
