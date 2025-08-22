aae3e4aed0cd915d07e33ef28ca40de7a9f995e8
hmnhGeek <hmnhsharma97@gmail.com>
2025-08-22 11:47:54 +0530

    Reorganize strings using max heap

--- Java/Heap/Problem9/Solution.java
+++ Java/Heap/Problem9/Solution.java
@@ -1,6 +1,11 @@
 package Heap.Problem9;
 
 
+import Heap.Problem9.MaxHeap.MaxHeap;
+
+import java.util.HashMap;
+import java.util.Map;
+
 class HeapElement implements Comparable<HeapElement> {
     private Character character;
     private Integer count;
@@ -34,5 +39,36 @@ class HeapElement implements Comparable<HeapElement> {
 
 
 public class Solution {
+    public static String reorganizeStrings(String string) {
+        Map<Character, Integer> characterCounts = getCharacterCount(string);
+        MaxHeap<HeapElement> maxHeap = new MaxHeap<>();
+        for (Character character : characterCounts.keySet()) {
+            maxHeap.insert(new HeapElement(character, characterCounts.get(character)));
+        }
+        HeapElement prev = null;
+        StringBuilder result = new StringBuilder();
+        while (!maxHeap.isEmpty()) {
+            HeapElement poppedElement = maxHeap.pop();
+            result.append(poppedElement.getCharacter());
+            if (prev != null && prev.getCount() > 0) {
+                maxHeap.insert(prev);
+            }
+            prev = new HeapElement(poppedElement.getCharacter(), poppedElement.getCount() - 1);
+        }
+        if (prev != null && prev.getCount() > 0) return "";
+        return result.toString();
+    }
 
+    private static Map<Character, Integer> getCharacterCount(String string) {
+        Map<Character, Integer> d = new HashMap<>();
+        for (Character character : string.toCharArray()) {
+            d.put(character, d.getOrDefault(character, 0) + 1);
+        }
+        return d;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reorganizeStrings("aab"));
+        System.out.println(reorganizeStrings("aaab"));
+    }
 }
