6b325dbd52178f3b1336427d53129ee6b93c12cf
hmnhGeek <hmnhsharma97@gmail.com>
2025-08-29 11:23:50 +0530

    Minimize the heights 2

--- Java/Arrays/Problem9/Solution.java
+++ Java/Arrays/Problem9/Solution.java
@@ -4,6 +4,23 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
+    public static Integer minimizeHeights(List<Integer> arr, Integer k) {
+        if (k <= 0) return null;
+        QuickSort.sort(arr);
+        Integer ans = arr.getLast() - arr.getFirst();
+        Integer smallest = arr.getFirst() + k, largest = arr.getLast() - k;
+        Integer mi = 0, mx = 0;
+        for (int i = 0; i < arr.size() - 1; i += 1) {
+            mi = Math.min(smallest, arr.get(i + 1) - k);
+            mx = Math.max(largest, arr.get(i) + k);
+            if (mi < 0) continue;
+            ans = Math.min(ans, mx - mi);
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
+        System.out.println(minimizeHeights(Arrays.asList(1, 5, 8, 10), 2));
+        System.out.println(minimizeHeights(Arrays.asList(3, 9, 12, 16, 20), 3));
     }
 }
