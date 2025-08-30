6da79aaf703c13e0ba317da56ee12fabbc1acfe9
hmnhGeek <hmnhsharma97@gmail.com>
2025-08-30 16:58:30 +0530

    Added logic for finding RLE

---
+++ Java/Strings/Problem5/Solution.java
@@ -0,0 +1,29 @@
+package Strings.Problem5;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    private static String getRle(String s) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int n = s.length();
+        int i = 0;
+        Map<Character, Integer> d = new HashMap<>();
+        while (i < n) {
+            Character c = s.charAt(i);
+            int count = 1;
+            i += 1;
+            while (i < n && s.charAt(i) == c) {
+                count += 1;
+                i += 1;
+            }
+            stringBuilder.append(String.format("%d%s", count, c));
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getRle("11"));
+        System.out.println(getRle("3322251"));
+    }
+}
