c0cda23b2522504761392283e68545c2aed844c5
hmnhGeek <hmnhsharma97@gmail.com>
2025-09-03 16:19:51 +0530

    Reorganize strings using max heap

--- Python/Practice Set 4/Heap/reorganize strings.py
+++ Python/Practice Set 4/Heap/reorganize strings.py
@@ -1,3 +1,6 @@
+# Problem link - https://www.geeksforgeeks.org/rearrange-characters-string-no-two-adjacent/
+
+
 from collections import Counter
 
 
@@ -82,6 +85,9 @@ class HeapElement:
 class Solution:
     @staticmethod
     def reorganize_string(string: str) -> str:
+        """
+            Time complexity is O(n * log(n)) and space complexity is O(n).
+        """
         max_heap = MaxHeap()
         d = dict(Counter(string))
         for i in d:
