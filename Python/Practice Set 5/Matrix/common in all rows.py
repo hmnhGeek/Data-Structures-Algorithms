16e2fd465e59596abe093411363d20e92757bf0e
hmnhGeek <hmnhsharma97@gmail.com>
2025-09-07 14:40:31 +0530

    Common in all rows in a matrix

---
+++ Python/Practice Set 5/Matrix/common in all rows.py
@@ -0,0 +1,33 @@
+from collections import Counter
+
+
+class Solution:
+    @staticmethod
+    def find_commons(mtx):
+        n, m = len(mtx), len(mtx[0])
+        temp = dict(Counter(mtx[0]))
+        for i in range(1, n):
+            row = dict(Counter(mtx[i]))
+            for j in temp:
+                if j in row:
+                    temp[j] = min(temp[j], row[j])
+                else:
+                    temp[j] = 0
+        result = []
+        for i in temp:
+            if temp[i] > 0:
+                result.append(i)
+
+        return result
+
+
+print(
+    Solution.find_commons(
+        [
+            [1, 2, 1, 4, 8],
+            [3, 7, 8, 5, 1],
+            [8, 7, 7, 3, 1],
+            [8, 1, 2, 7, 9]
+        ]
+    )
+)
