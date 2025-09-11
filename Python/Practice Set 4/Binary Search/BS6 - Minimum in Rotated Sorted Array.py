c4a1393dacd6b237528c37d2b51328d873146718
hmnhGeek <hmnhsharma97@gmail.com>
2025-09-11 09:11:30 +0530

    Minimum in a rotated sorted array

---
+++ Python/Practice Set 4/Binary Search/BS6 - Minimum in Rotated Sorted Array.py
@@ -0,0 +1,38 @@
+class Solution:
+    @staticmethod
+    def get_min_in_rotated(arr):
+        n = len(arr)
+        low = 0
+        high = n - 1
+        ans = 1e6
+        while low <= high:
+            mid = int(low + (high - low)/2)
+            if arr[low] == arr[mid] == arr[high]:
+                ans = min(ans, arr[low])
+                low += 1
+                high -= 1
+                continue
+            if arr[low] > arr[mid]:
+                ans = min(ans, arr[mid])
+                high = mid - 1
+            elif arr[mid] > arr[high]:
+                ans = min(ans, arr[low])
+                low = mid + 1
+            else:
+                ans = min(ans, arr[mid])
+                high = mid - 1
+        return ans
+
+
+print(Solution.get_min_in_rotated([4, 5, 6, 7, 0, 1, 2]))
+print(Solution.get_min_in_rotated([4, 1, 2, 3]))
+print(Solution.get_min_in_rotated([3, 4, 5, 1, 2]))
+print(Solution.get_min_in_rotated([3, 4, 1, 2]))
+print(Solution.get_min_in_rotated([25, 30, 5, 10, 15, 20]))
+print(Solution.get_min_in_rotated([11, 13, 15, 17]))
+print(Solution.get_min_in_rotated([7, 8, 1, 2, 3, 4, 5, 6]))
+print(Solution.get_min_in_rotated([1, 2]))
+print(Solution.get_min_in_rotated([2, 1]))
+print(Solution.get_min_in_rotated([3, 3, 3, 3, 3]))
+print(Solution.get_min_in_rotated([1, 2, 2, 3, 3, 3, 5]))
+print(Solution.get_min_in_rotated([5, 5, 5, 5, 1, 2, 3, 3]))
