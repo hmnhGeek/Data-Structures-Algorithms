fd2766a142e747251bba63a62e651a139880f19f
hmnhGeek <hmnhsharma97@gmail.com>
2025-09-14 10:00:42 +0530

    Triangle Space Optimized

--- Python/Practice Set 4/Dynamic Programming/DP11 - Triangle.py
+++ Python/Practice Set 4/Dynamic Programming/DP11 - Triangle.py
@@ -275,9 +275,104 @@ def tabulation():
     )
 
 
+def space_optimized():
+    """
+        Time complexity is O(n^3) and space complexity is O(n).
+    """
+    def triangle(mtx):
+        n = len(mtx)
+        result = 1e6
+        for last_elem_idx in range(n):
+            prev = {j: 1e6 for j in range(n)}
+            prev[0] = mtx[0][0]
+            for i in range(1, n):
+                curr = {j: 1e6 for j in range(n)}
+                for j in range(i + 1):
+                    up = 1e6
+                    if i - 1 >= 0 and j <= i - 1:
+                        up = mtx[i][j] + prev[j]
+                    diagonal = 1e6
+                    if j - 1 >= 0 and i - 1 >= 0:
+                        diagonal = mtx[i][j] + prev[j - 1]
+                    curr[j] = min(up, diagonal)
+                prev = curr
+            ans = prev[last_elem_idx]
+            result = min(result, ans)
+        return result
+
+    print(
+        triangle(
+            [
+                [1],
+                [2, 3],
+                [3, 6, 7],
+                [8, 9, 6, 10]
+            ]
+        )
+    )
+
+    print(
+        triangle(
+            [
+                [2],
+                [3, 4],
+                [6, 5, 7],
+                [4, 1, 8, 3]
+            ]
+        )
+    )
+
+    print(triangle([[-10]]))
+
+    print(
+        triangle(
+            [
+                [1],
+                [2, 3],
+                [4, 5, 6],
+                [7, 8, 9, 10]
+            ]
+        )
+    )
+
+    print(
+        triangle(
+            [
+                [5],
+                [-1, 3],
+                [22, 1, -9]
+            ]
+        )
+    )
+
+    print(
+        triangle(
+            [
+                [2],
+                [3, 7],
+                [8, 5, 6],
+                [6, 1, 9, 3]
+            ]
+        )
+    )
+
+    print(
+        triangle(
+            [
+                [3],
+                [6, 9],
+                [8, 7, 1],
+                [9, 6, 8, 2]
+            ]
+        )
+    )
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
+print()
+space_optimized()
 print()
