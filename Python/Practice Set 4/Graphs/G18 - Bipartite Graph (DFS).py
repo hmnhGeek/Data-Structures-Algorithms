c1b9a4b520ecc53b27e9edb94c24e213bf82921b
hmnhGeek <hmnhsharma97@gmail.com>
2025-09-14 10:16:10 +0530

    Check if a graph is bipartite or not using DFS

--- Python/Practice Set 4/Graphs/G18 - Bipartite Graph (DFS).py
+++ Python/Practice Set 4/Graphs/G18 - Bipartite Graph (DFS).py
@@ -1,24 +1,42 @@
 class Solution:
     @staticmethod
     def _dfs(graph, node, parent, color, node_colors):
+        # color the current node
         node_colors[node] = color
+
+        # loop on the neighbours of the node...
         for adj_node in graph[node]:
+            # if the adjacent node is parent itself, there is nothing to be done.
             if adj_node == parent:
                 continue
+
+            # if the adjacent node is not yet colored, start dfs on it.
             if node_colors[adj_node] is None:
                 if not Solution._dfs(graph, adj_node, node, 0 if color == 1 else 1, node_colors):
                     return False
+
+            # if the adjacent node's color is same as current node's color, it's not bipartite.
             if node_colors[adj_node] == color:
                 return False
+
+        # else it is bipartite.
         return True
 
     @staticmethod
     def is_bipartite(graph):
+        # store the colors of the node
         node_colors = {i: None for i in graph}
+
+        # loop in the nodes of the graph
         for node in graph:
+            # if this starting node is not yet colored, start a DFS from here...
             if node_colors[node] is None:
+                # if this component is not a bipartite component, the whole graph is not bipartite
+                # therefore, return False
                 if not Solution._dfs(graph, node, None, 0, node_colors):
                     return False
+
+        # the graph is bipartite, return True
         return True
 
 
