dc5ce2cc7b75b87f0665f1f13330654bc88ce4d8
hmnhGeek <hmnhsharma97@gmail.com>
2025-09-14 09:53:01 +0530

    Triangle Tabulation

--- Python/Practice Set 4/Dynamic Programming/DP11 - Triangle.py
+++ Python/Practice Set 4/Dynamic Programming/DP11 - Triangle.py
@@ -1,6 +1,6 @@
 def recursive():
     """
-        Time complexity is O(2^{nm}) and space complexity is O(n + m).
+        Time complexity is O(2^{n^2}) and space complexity is O(2n).
     """
     def triangle(mtx):
         n = len(mtx)
@@ -91,7 +91,7 @@ def recursive():
 
 def memoized():
     """
-        Time complexity is O(mn) and space complexity is O(n + m + mn).
+        Time complexity is O(n^3) and space complexity is O(2n + n^2).
     """
     def triangle(mtx):
         n = len(mtx)
@@ -184,7 +184,100 @@ def memoized():
     )
 
 
+def tabulation():
+    """
+        Time complexity is O(n^3) and space complexity is O(n^2).
+    """
+    def triangle(mtx):
+        n = len(mtx)
+        result = 1e6
+        dp = {i: {j: 1e6 for j in range(n)} for i in range(n)}
+        dp[0][0] = mtx[0][0]
+        for last_elem_idx in range(n):
+            for i in range(1, n):
+                for j in range(i + 1):
+                    up = 1e6
+                    if i - 1 >= 0 and j <= i - 1:
+                        up = mtx[i][j] + dp[i - 1][j]
+                    diagonal = 1e6
+                    if j - 1 >= 0 and i - 1 >= 0:
+                        diagonal = mtx[i][j] + dp[i - 1][j - 1]
+                    dp[i][j] = min(up, diagonal)
+            ans = dp[n - 1][last_elem_idx]
+            result = min(result, ans)
+        return result
+
+    print(
+        triangle(
+            [
+                [1],
+                [2, 3],
+                [3, 6, 7],
+                [8, 9, 6, 10]
+            ]
+        )
+    )
+
+    print(
+        triangle(
+            [
+                [2],
+                [3, 4],
+                [6, 5, 7],
+                [4, 1, 8, 3]
+            ]
+        )
+    )
+
+    print(triangle([[-10]]))
+
+    print(
+        triangle(
+            [
+                [1],
+                [2, 3],
+                [4, 5, 6],
+                [7, 8, 9, 10]
+            ]
+        )
+    )
+
+    print(
+        triangle(
+            [
+                [5],
+                [-1, 3],
+                [22, 1, -9]
+            ]
+        )
+    )
+
+    print(
+        triangle(
+            [
+                [2],
+                [3, 7],
+                [8, 5, 6],
+                [6, 1, 9, 3]
+            ]
+        )
+    )
+
+    print(
+        triangle(
+            [
+                [3],
+                [6, 9],
+                [8, 7, 1],
+                [9, 6, 8, 2]
+            ]
+        )
+    )
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
 print()
