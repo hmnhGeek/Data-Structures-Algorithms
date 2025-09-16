c9321ca3783c925488c31c23681bd5c13f75558e
hmnhGeek <hmnhsharma97@gmail.com>
2025-09-16 12:11:13 +0530

    Spiral traversal of a matrix

--- Java/Matrix/Problem1/Solution.java
+++ Java/Matrix/Problem1/Solution.java
@@ -1,3 +1,6 @@
+// Problem link - https://www.geeksforgeeks.org/problems/spirally-traversing-a-matrix-1587115621/1
+
+
 package Matrix.Problem1;
 
 import java.util.ArrayList;
@@ -15,9 +18,32 @@ public class Solution {
                         )
                 )
         );
+
+        System.out.println(
+                getSpiralTraversal(
+                        Arrays.asList(
+                                Arrays.asList(1, 2, 3, 4),
+                                Arrays.asList(5, 6, 7, 8),
+                                Arrays.asList(9, 10, 11, 12),
+                                Arrays.asList(13, 14, 15, 16)
+                        )
+                )
+        );
+
+        System.out.println(
+                getSpiralTraversal(
+                        Arrays.asList(
+                                Arrays.asList(32, 44, 27, 23),
+                                Arrays.asList(54, 28, 50, 62)
+                        )
+                )
+        );
     }
 
     public static List<Integer> getSpiralTraversal(List<List<Integer>> mtx) {
+        /*
+            Time complexity is O(nm) and space complexity is O(mn).
+         */
         int n = mtx.size(), m = mtx.getFirst().size();
         int left = 0, right = m - 1, top = 0, down = n - 1;
         String direction = "R";
