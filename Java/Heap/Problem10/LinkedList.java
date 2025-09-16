fccae6eb0bf87469b4be637749f097535198bcb1
hmnhGeek <hmnhsharma97@gmail.com>
2025-09-16 11:28:12 +0530

    Implemented a linked list class

---
+++ Java/Heap/Problem10/LinkedList.java
@@ -0,0 +1,74 @@
+package Heap.Problem10;
+
+
+class Node<T> {
+    private T data;
+    private Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+}
+
+
+public class LinkedList<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private Integer length;
+
+    public LinkedList() {
+        this.head = this.tail = null;
+        this.length = 0;
+    }
+
+    public boolean isEmpty() {
+        return this.length.equals(0);
+    }
+
+    public void push(T x) {
+        Node<T> node = new Node<>(x);
+        if (isEmpty()) {
+            this.head = this.tail = node;
+        } else {
+            this.tail.setNext(node);
+            this.tail = node;
+        }
+        this.length += 1;
+    }
+
+    public void build(T...args) {
+        for (T x : args) {
+            push(x);
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (this.length == 0) return "[]";
+        StringBuilder result = new StringBuilder("[");
+        Node<T> curr = this.head;
+        while (curr != this.tail) {
+            result.append(String.format("%s, ", curr.getData()));
+            curr = curr.getNext();
+        }
+        result.append(String.format("%s]", this.tail.getData()));
+        return result.toString();
+    }
+}
