516e619fabfd1e1f1941fafb5c483a368c00a2d6
hmnhGeek <hmnhsharma97@gmail.com>
2025-09-19 12:41:52 +0530

    Balanced parenthesis using stack

--- Java/StacksAndQueues/Problem6/Solution.java
+++ Java/StacksAndQueues/Problem6/Solution.java
@@ -34,19 +34,31 @@ public class Solution {
         int i = 0, n = brackets.length();
         while (i < n) {
             Character character = brackets.charAt(i);
+
+            // if it's an opening bracket, simply push it to the stack.
             if (openingBrackets.contains(character)) {
                 stack.push(character);
             } else {
+                // if it is a closing bracket, check if stack is empty. If stack is empty, this closing bracket
+                // is unwanted, return false.
                 if (stack.isEmpty()) return false;
+
+                // find the opening bracket for this closing bracket.
                 Character openingBracket = bracketsMapping.get(character);
+
+                // if the opening bracket is on top of stack, pop it.
                 if (stack.head.data.equals(openingBracket)) {
                     stack.pop();
                 } else {
+                    // else if some other opening bracket is on top, return false as this combination
+                    // is not balanced.
                     return false;
                 }
             }
             i += 1;
         }
+
+        // if at the end, stack is empty, then its balanced else not.
         if (stack.isEmpty()) return true;
         return false;
     }
