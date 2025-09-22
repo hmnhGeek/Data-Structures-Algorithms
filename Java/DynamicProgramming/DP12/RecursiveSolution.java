c34974d4be925369faa0c9214abf6196f3e2ba92
hmnhGeek <hmnhsharma97@gmail.com>
2025-09-22 11:11:22 +0530

    Recursive maximum path sum

---
+++ Java/DynamicProgramming/DP12/RecursiveSolution.java
@@ -0,0 +1,41 @@
+// Problem link - https://www.naukri.com/code360/problems/maximum-path-sum-in-the-matrix_797998?source=youtube&campaign=striver_dp_videos
+// Solution - https://www.youtube.com/watch?v=N_aJ5qQbYA0&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=13
+
+
+package DynamicProgramming.DP12;
+
+import java.util.List;
+
+public class RecursiveSolution {
+    public static Integer getMaxFallingSum(List<List<Integer>> mtx) {
+        /*
+            Time complexity is O(2^{m + n}) and space complexity is O(m + n).
+         */
+        int n = mtx.size(), m = mtx.getFirst().size();
+        Integer result = Integer.MIN_VALUE;
+        for (int endColIndex = 0; endColIndex < m; endColIndex += 1) {
+            Integer pathSum = solve(mtx, n - 1, endColIndex, m);
+            result = Math.max(pathSum, result);
+        }
+        return result;
+    }
+
+    private static Integer solve(List<List<Integer>> mtx, int i, int j, int m) {
+        if (i == 0) {
+            return mtx.getFirst().get(j);
+        }
+        Integer upperLeft = Integer.MIN_VALUE;
+        if (0 <= i - 1 && 0 <= j - 1 && j - 1 < m) {
+            upperLeft = solve(mtx, i - 1, j - 1, m);
+        }
+        Integer up = Integer.MIN_VALUE;
+        if (0 <= i - 1) {
+            up = solve(mtx, i - 1, j, m);
+        }
+        Integer upperRight = Integer.MIN_VALUE;
+        if (0 <= i - 1 && 0 <= j + 1 && j + 1 < m) {
+            upperRight = solve(mtx, i - 1, j + 1, m);
+        }
+        return mtx.get(i).get(j) + Math.max(upperLeft, Math.max(up, upperRight));
+    }
+}
---
+++ Java/DynamicProgramming/DP12/Solution.java
@@ -0,0 +1,53 @@
+package DynamicProgramming.DP12;
+
+import java.util.List;
+
+public class Solution {
+    public static void recursive() {
+        System.out.println(
+                RecursiveSolution.getMaxFallingSum(
+                        List.of(
+                                List.of(1, 2, 10, 4),
+                                List.of(100, 3, 2, 1),
+                                List.of(1, 1, 20, 2),
+                                List.of(1, 2, 2, 1)
+                        )
+                )
+        );
+
+        System.out.println(
+                RecursiveSolution.getMaxFallingSum(
+                        List.of(
+                                List.of(10, 2, 3),
+                                List.of(3, 7, 2),
+                                List.of(8, 1, 5)
+                        )
+                )
+        );
+
+        System.out.println(
+                RecursiveSolution.getMaxFallingSum(
+                        List.of(
+                                List.of(1, 2, 3),
+                                List.of(9, 8, 7),
+                                List.of(4, 5, 6)
+                        )
+                )
+        );
+
+        System.out.println(
+                RecursiveSolution.getMaxFallingSum(
+                        List.of(
+                                List.of(10, 10, 2, -13, 20, 4),
+                                List.of(1, -9, -81, 30, 2, 5),
+                                List.of(0, 10, 4, -79, 2, -10),
+                                List.of(1, -5, 2, 20, -11, 4)
+                        )
+                )
+        );
+    }
+
+    public static void main(String[] args) {
+        recursive();
+    }
+}
