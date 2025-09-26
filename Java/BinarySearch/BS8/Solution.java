1890e03aa6db1c6e106f35bca66b202abcf11446
hmnhGeek <hmnhsharma97@gmail.com>
2025-09-26 12:18:47 +0530

    Find single element in an array

---
+++ Java/BinarySearch/BS8/Solution.java
@@ -0,0 +1,37 @@
+package BinarySearch.BS8;
+
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(Solution.getSingleElement(List.of(3, 3, 7, 7, 10, 11, 11)));
+        System.out.println(Solution.getSingleElement(List.of(1, 1, 2, 2, 4, 5, 5)));
+        System.out.println(Solution.getSingleElement(List.of(1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6)));
+        System.out.println(Solution.getSingleElement(List.of(1, 1, 4, 4, 15)));
+        System.out.println(Solution.getSingleElement(List.of(1, 1, 3, 5, 5)));
+        System.out.println(Solution.getSingleElement(List.of(1, 1, 3, 3, 4, 5, 5, 7, 7, 8, 8)));
+        System.out.println(Solution.getSingleElement(List.of(1, 1, 3, 3, 4, 4, 5, 5, 7, 7, 8)));
+        System.out.println(Solution.getSingleElement(List.of(1, 1, 2, 3, 3, 4, 4, 8, 8)));
+    }
+
+    public static Integer getSingleElement(List<Integer> arr) {
+        int n = arr.size();
+        if (n == 1) return arr.getFirst();
+        if (arr.getFirst() != arr.get(1)) return arr.getFirst();
+        if (arr.getLast() != arr.get(n - 2)) return arr.getLast();
+        int low = 1, high = n - 2;
+        while (low <= high) {
+            int mid = (low + (high - low)/2);
+            if (arr.get(mid) != arr.get(mid - 1) && arr.get(mid) != arr.get(mid + 1)) return arr.get(mid);
+            if (
+                    (mid % 2 == 1 && arr.get(mid - 1) == arr.get(mid)) ||
+                            (mid % 2 == 0 && arr.get(mid) == arr.get(mid + 1))
+            ) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
