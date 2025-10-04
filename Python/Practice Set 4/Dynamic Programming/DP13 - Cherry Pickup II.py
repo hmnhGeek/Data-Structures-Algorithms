3833221fab5be08ae919320cf05f3df9b816f188
hmnhGeek <hmnhsharma97@gmail.com>
2025-10-04 15:01:03 +0530

    Space optimized cherry pickup 2

--- Python/Practice Set 4/Dynamic Programming/DP13 - Cherry Pickup II.py
+++ Python/Practice Set 4/Dynamic Programming/DP13 - Cherry Pickup II.py
@@ -91,7 +91,7 @@ def recursive():
 def memoized():
     def cherry_pickup(mtx):
         """
-            Time complexity is O(m^4 * n) and space complexity is O(m + n + m^2 * n)
+            Time complexity is O(m^4 * n) and space complexity is O(m^2 * n)
         """
         n, m = len(mtx), len(mtx[0])
         max_chocolates = 0
@@ -273,9 +273,104 @@ def tabulation():
     )
 
 
+def space_optimized():
+    def cherry_pickup(mtx):
+        """
+            Time complexity is O(m^4 * n) and space complexity is O(m^2)
+        """
+        n, m = len(mtx), len(mtx[0])
+        max_chocolates = 0
+        for last_alice_index in range(m):
+            for last_bob_index in range(m):
+                prev = {a: {b: 0 for b in range(m)} for a in range(m)}
+                prev[0][m - 1] = mtx[0][0] + mtx[0][m - 1]
+
+                for i in range(1, n):
+                    curr = {a: {b: 0 for b in range(m)} for a in range(m)}
+                    for a in range(m):
+                        for b in range(m):
+                            chocolates = 0
+                            for da in range(-1, 2):
+                                for db in range(-1, 2):
+                                    if 0 <= a + da < m and 0 <= b + db < m:
+                                        if a == b:
+                                            chocolates = max(chocolates, mtx[i][a] + prev[a + da][b + db])
+                                        else:
+                                            chocolates = max(chocolates, mtx[i][a] + mtx[i][b] + prev[a + da][b + db])
+                            curr[a][b] = chocolates
+                    prev = curr
+
+                max_chocolates = max(max_chocolates, prev[last_alice_index][last_bob_index])
+        return max_chocolates
+
+    print(
+        cherry_pickup(
+            [
+                [2, 3, 1, 2],
+                [3, 4, 2, 2],
+                [5, 6, 3, 5]
+            ]
+        )
+    )
+
+    print(
+        cherry_pickup(
+            [
+                [1, 1],
+                [1, 2]
+            ]
+        )
+    )
+
+    print(
+        cherry_pickup(
+            [
+                [3, 7],
+                [7, 6]
+            ]
+        )
+    )
+
+    print(
+        cherry_pickup(
+            [
+                [4, 5],
+                [3, 7],
+                [4, 2]
+            ]
+        )
+    )
+
+    print(cherry_pickup([[3, 1, 1], [2, 5, 1], [1, 5, 5], [2, 1, 1]]))
+    print(
+        cherry_pickup(
+            [
+                [4, 1, 2],
+                [3, 6, 1],
+                [1, 6, 6],
+                [3, 1, 2]
+            ]
+        )
+    )
+
+    print(
+        cherry_pickup(
+            [
+                [2, 0, 0, 0, 0, 0, 2],
+                [2, 1, 0, 0, 0, 4, 0],
+                [2, 0, 10, 0, 1, 0, 0],
+                [0, 3, 0, 6, 5, 0, 0],
+                [1, 0, 3, 4, 0, 0, 6]
+            ]
+        )
+    )
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
+print()
+space_optimized()
 print()
