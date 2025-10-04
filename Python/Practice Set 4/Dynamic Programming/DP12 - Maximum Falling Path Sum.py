e8b0382867807219ce88cf9d91fef0123fe7ffd7
hmnhGeek <hmnhsharma97@gmail.com>
2025-10-04 13:49:17 +0530

    Max falling path sum

---
+++ Python/Practice Set 4/Dynamic Programming/DP12 - Maximum Falling Path Sum.py
@@ -0,0 +1,68 @@
+def recursive():
+    def max_falling_path(mtx):
+        """
+            Time complexity is exponential and space complexity is O(m + n).
+        """
+        n, m = len(mtx), len(mtx[0])
+        result = -1e6
+        for k in range(m):
+            sub_result = solve(mtx, n - 1, k, n, m)
+            result = max(result, sub_result)
+        return result
+
+    def solve(mtx, i, j, n, m):
+        if i == 0:
+            return mtx[i][j]
+        left, up, right = -1e6, -1e6, -1e6
+        if 0 <= i - 1 < n and 0 <= j - 1 < m:
+            left = solve(mtx, i - 1, j - 1, n, m)
+        if 0 <= i - 1 < n:
+            up = solve(mtx, i - 1, j, n, m)
+        if 0 <= i - 1 < n and 0 <= j + 1 < m:
+            right = solve(mtx, i - 1, j + 1, n, m)
+        return mtx[i][j] + max(left, up, right)
+
+    print(
+        max_falling_path(
+            [
+                [1, 2, 10, 4],
+                [100, 3, 2, 1],
+                [1, 1, 20, 2],
+                [1, 2, 2, 1]
+            ]
+        )
+    )
+
+    print(
+        max_falling_path(
+            [
+                [10, 2, 3],
+                [3, 7, 2],
+                [8, 1, 5]
+            ]
+        )
+    )
+
+    print(
+        max_falling_path(
+            [
+                [1, 2, 3],
+                [9, 8, 7],
+                [4, 5, 6]
+            ]
+        )
+    )
+
+    print(
+        max_falling_path(
+            [
+                [10, 10, 2, -13, 20, 4],
+                [1, -9, -81, 30, 2, 5],
+                [0, 10, 4, -79, 2, -10],
+                [1, -5, 2, 20, -11, 4]
+            ]
+        )
+    )
+
+
+recursive()
