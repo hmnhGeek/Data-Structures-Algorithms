4816c4b43d83ee9ef6af66321d8360eed2dcc5d6
hmnhGeek <hmnhsharma97@gmail.com>
2025-10-05 18:40:53 +0530

    Find peak element in an array

---
+++ Python/Practice Set 4/Binary Search/BS9 - Find Peak Element.py
@@ -0,0 +1,35 @@
+class Solution:
+    @staticmethod
+    def get_peak(arr):
+        n = len(arr)
+        if n == 0:
+            return
+        if n == 1:
+            return arr[0]
+        if arr[0] > arr[1]:
+            return arr[0]
+        if arr[-1] > arr[-2]:
+            return arr[-1]
+        low, high = 1, n - 2
+        while low <= high:
+            mid = int(low + (high - low)/2)
+            element = arr[mid]
+            if arr[mid - 1] < element and element > arr[mid + 1]:
+                return element
+            if arr[mid - 1] < element < arr[mid + 1]:
+                low = mid + 1
+            elif arr[mid - 1] > element > arr[mid + 1]:
+                high = mid - 1
+            elif arr[mid - 1] > element and element < arr[mid + 1]:
+                low = mid + 1
+        return -1
+
+
+print(Solution.get_peak([1, 5, 1, 2, 1]))
+print(Solution.get_peak([1, 8, 1, 5, 3]))
+print(Solution.get_peak([1, 2, 1]))
+print(Solution.get_peak([1, 2, 3, 1]))
+print(Solution.get_peak([1, 2, 1, 3, 5, 6, 4]))
+print(Solution.get_peak([1, 2, 4, 5, 7, 8, 3]))
+print(Solution.get_peak([10, 20, 15, 2, 23, 90, 80]))
+print(Solution.get_peak([1, 2, 3]))
