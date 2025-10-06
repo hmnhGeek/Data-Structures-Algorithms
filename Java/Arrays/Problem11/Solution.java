1bfab58c7a6de17b66dc1b717199bcf5648080c9
hmnhGeek <hmnhsharma97@gmail.com>
2025-10-06 12:45:10 +0530

    Get the duplicate number

---
+++ Java/Arrays/Problem11/Solution.java
@@ -0,0 +1,36 @@
+package Arrays.Problem11;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public static Integer getDuplicate(List<Integer> arr) {
+        int n = arr.size();
+        if (n == 0) return -1;
+        int slow = arr.get(0);
+        int fast = arr.get(0);
+
+        // detect cycle
+        while (true) {
+            slow = arr.get(slow);
+            fast = arr.get(arr.get(fast));
+            if (slow == fast) break;
+        }
+
+        // get the cycle starting point
+        slow = arr.get(0);
+        while (slow != fast) {
+            slow = arr.get(slow);
+            fast = arr.get(fast);
+        }
+
+        return slow; // or fast, since they are same.
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getDuplicate(Arrays.asList(3, 1, 3, 4, 2)));
+        System.out.println(getDuplicate(Arrays.asList(3, 3, 3, 3, 3)));
+        System.out.println(getDuplicate(Arrays.asList(1, 3, 4, 2, 2)));
+    }
+}
