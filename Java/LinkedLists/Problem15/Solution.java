1c24a5e3c755658c9175457b254f996a475dd029
hmnhGeek <hmnhsharma97@gmail.com>
2025-10-07 10:47:32 +0530

    Middle node of a linked list

--- Java/LinkedLists/Problem15/Solution.java
+++ Java/LinkedLists/Problem15/Solution.java
@@ -1,7 +1,13 @@
+// Problem link - https://leetcode.com/problems/middle-of-the-linked-list/description/
+
+
 package LinkedLists.Problem15;
 
 public class Solution {
     public static <T> T getMiddle(LinkedList<T> linkedList) {
+        /*
+            Time complexity is O(n) and space complexity is O(1).
+         */
         Node<T> slow = linkedList.head, fast = linkedList.head;
 
         while (fast != null && fast.next != null) {
