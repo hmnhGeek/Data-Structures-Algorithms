01ae8873c1cd3ac5cfe5cf486790ccfbf56464f4
hmnhGeek <hmnhsharma97@gmail.com>
2025-10-11 14:09:16 +0530

    Methods for deleting a node in BST

--- Java/BinarySearchTrees/Problem19/BinarySearchTree.java
+++ Java/BinarySearchTrees/Problem19/BinarySearchTree.java
@@ -123,4 +123,53 @@ public class BinarySearchTree<T extends Comparable<T>> {
         return parent;
     }
 
+    public void delete(Node<T> node) {
+        if (node == null) return;
+        if (node.left == null && node.right == null) {
+            Node<T> parent = node.parent;
+            if (parent != null) {
+                if (parent.left == node) {
+                    parent.left = null;
+                } else {
+                    parent.right = null;
+                }
+            } else {
+                root = null;
+                diameter = 0;
+            }
+            recalcAugmentation(parent);
+            return;
+        }
+
+        if (node.right != null) {
+            Node<T> successor = getSuccessor(node);
+            T temp = successor.data;
+            successor.data = node.data;
+            node.data = temp;
+            delete(successor);
+            return;
+        }
+
+        Node<T> predecessor = getPredecessor(node);
+        T temp = predecessor.data;
+        predecessor.data = node.data;
+        node.data = temp;
+        delete(predecessor);
+        return;
+    }
+
+    public void delete(T x) {
+        Node<T> node = getNode(root, x);
+        if (node != null) {
+            delete(node);
+        }
+    }
+
+    private Node<T> getNode(Node<T> start, T x) {
+        if (start == null || x == null) return null;
+        if (start.data == x) return start;
+        if (x.compareTo(start.data) > 0) return getNode(start.right, x);
+        return getNode(start.left, x);
+    }
+
 }
