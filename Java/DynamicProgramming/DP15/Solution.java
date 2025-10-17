ef6b9fcdf5433fcfd7637b1e587d0b59b3ffbbb8
hmnhGeek <hmnhsharma97@gmail.com>
2025-10-17 11:27:56 +0530

    Tabulation partition equals subset sum

--- Java/DynamicProgramming/DP15/Solution.java
+++ Java/DynamicProgramming/DP15/Solution.java
@@ -25,9 +25,22 @@ public class Solution {
         System.out.println(MemoizedSolution.subsetSum(Arrays.asList(1, 2, 3, 5)));
     }
 
+    public static void tabulation() {
+        System.out.println(TabulationSolution.subsetSum(Arrays.asList(2, 3, 3, 3, 4, 5)));
+        System.out.println(TabulationSolution.subsetSum(Arrays.asList(3, 1, 1, 2, 2, 1)));
+        System.out.println(TabulationSolution.subsetSum(Arrays.asList(5, 6, 5, 11, 6)));
+        System.out.println(TabulationSolution.subsetSum(Arrays.asList(2, 2, 1, 1, 1, 1, 1, 3, 3)));
+        System.out.println(TabulationSolution.subsetSum(Arrays.asList(8, 7, 6, 12, 4, 5)));
+        System.out.println(TabulationSolution.subsetSum(Arrays.asList(1, 5, 11, 5)));
+        System.out.println(TabulationSolution.subsetSum(Arrays.asList(1, 3, 5)));
+        System.out.println(TabulationSolution.subsetSum(Arrays.asList(1, 2, 3, 5)));
+    }
+
     public static void main(String[] args) {
         recursive();
         System.out.println();
         memoized();
+        System.out.println();
+        tabulation();
     }
 }
--- Java/DynamicProgramming/DP15/TabulationSolution.java
+++ Java/DynamicProgramming/DP15/TabulationSolution.java
@@ -4,9 +4,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MemoizedSolution {
+public class TabulationSolution {
     /*
-        Time complexity is O(nk) and space complexity is O(nk + k).
+        Time complexity is O(nk) and space complexity is O(nk).
      */
     public static boolean subsetSum(List<Integer> arr) {
         int n = arr.size();
@@ -14,29 +14,32 @@ public class MemoizedSolution {
         if (k % 2 != 0) return false;
         Map<Integer, Map<Integer, Boolean>> dp = new HashMap<>();
         updateDpArray(dp, n, k);
-        return solve(arr, n - 1, k / 2, dp);
+        for (int i = 0; i < n; i += 1) {
+            dp.get(i).put(0, true);
+        }
+        dp.get(0).put(arr.getFirst(), true);
+
+        int target = k / 2;
+        for (int i = 1; i < n; i += 1) {
+            for (int j = 0; j <= target; j += 1) {
+                boolean left = false;
+                if (arr.get(i) <= j) {
+                    left = dp.get(i - 1).get(j - arr.get(i));
+                }
+                boolean right = dp.get(i - 1).get(j);
+                dp.get(i).put(j, left || right);
+            }
+        }
+        return dp.get(n - 1).get(target);
     }
 
     private static void updateDpArray(Map<Integer, Map<Integer, Boolean>> dp, int n, int k) {
         for (int i = 0; i < n; i += 1) {
             Map<Integer, Boolean> subMap = new HashMap<>();
             for (int j = 0; j < k; j += 1) {
-                subMap.put(j, null);
+                subMap.put(j, false);
             }
             dp.put(i, subMap);
         }
-    }
-
-    private static boolean solve(List<Integer> arr, int i, Integer k, Map<Integer, Map<Integer, Boolean>> dp) {
-        if (k == 0) return true;
-        if (i == 0) return arr.getFirst() == k;
-        if (dp.get(i).get(k) != null) return dp.get(i).get(k);
-        boolean left = false;
-        if (arr.get(i) <= k) {
-            left = solve(arr, i - 1, k - arr.get(i), dp);
-        }
-        boolean right = solve(arr, i - 1, k, dp);
-        dp.get(i).put(k, left || right);
-        return dp.get(i).get(k);
     }
 }
