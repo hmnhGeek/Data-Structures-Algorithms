fe4656b7101fad9909e05c16f1a4b297f398f2aa
hmnhGeek <hmnhsharma97@gmail.com>
2025-10-20 11:42:08 +0530

    Longest Repeating Subsequence

--- Python/Practice Set 1/Strings/Longest Repeating Subsequence.py
+++ Python/Practice Set 1/Strings/Longest Repeating Subsequence.py
@@ -1,3 +1,6 @@
+# Solution for LRS - https://www.youtube.com/watch?v=ZqG89Z-dKpI&t=19s
+
+
 def recursive():
     def lcs(s1, s2):
         n, m = len(s1), len(s2)
@@ -84,10 +87,27 @@ def space_optimized():
     print(lcs("ABC", "CBA"))
 
 
-recursive()
-print()
-memoized()
-print()
-tabulation()
-print()
-space_optimized()
+def get_longest_repeating_subsequence(s):
+    """
+        Time complexity is O(n^2) and space complexity is O(n).
+    """
+    n, m = len(s), len(s)
+    prev = {j: 0 for j in range(m + 1)}
+    for i in range(1, n + 1):
+        curr = {j: 0 for j in range(m + 1)}
+        for j in range(1, m + 1):
+            if s[i - 1] == s[j - 1] and i != j:
+                curr[j] = 1 + prev[j - 1]
+            else:
+                curr[j] = max(curr[j - 1], prev[j])
+        prev = curr
+    return prev[m]
+
+
+print(get_longest_repeating_subsequence("axxxy"))
+print(get_longest_repeating_subsequence("axxzxy"))
+print(get_longest_repeating_subsequence("abc"))
+print(get_longest_repeating_subsequence("aab"))
+print(get_longest_repeating_subsequence("abcab"))
+print(get_longest_repeating_subsequence("ABCBDCD"))
+print(get_longest_repeating_subsequence("BCCB"))
