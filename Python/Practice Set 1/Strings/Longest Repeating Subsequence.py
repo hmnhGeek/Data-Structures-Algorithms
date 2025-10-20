83709d12fd8b5287bf6ed0e3bae83f951dde0a7e
hmnhGeek <hmnhsharma97@gmail.com>
2025-10-20 11:38:36 +0530

    Space Optimized LCS

--- Python/Practice Set 1/Strings/Longest Repeating Subsequence.py
+++ Python/Practice Set 1/Strings/Longest Repeating Subsequence.py
@@ -64,8 +64,30 @@ def tabulation():
     print(lcs("ABC", "CBA"))
 
 
+def space_optimized():
+    def lcs(s1, s2):
+        n, m = len(s1), len(s2)
+        prev = {j: 0 for j in range(m + 1)}
+        for i in range(1, n + 1):
+            curr = {j: 0 for j in range(m + 1)}
+            for j in range(1, m + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    curr[j] = 1 + prev[j - 1]
+                else:
+                    curr[j] = max(curr[j - 1], prev[j])
+            prev = curr
+        return prev[m]
+
+    print(lcs("adebc", "dcadb"))
+    print(lcs("ab", "defg"))
+    print(lcs("AGGTAB", "GXTXAYB"))
+    print(lcs("ABC", "CBA"))
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
+print()
+space_optimized()
