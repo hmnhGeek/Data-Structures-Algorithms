7d3e341c5a5e202ced6a447740f6ac1e7cb3a97c
hmnhGeek <hmnhsharma97@gmail.com>
2025-10-20 11:37:22 +0530

    Tabulation LCS

--- Python/Practice Set 1/Strings/Longest Repeating Subsequence.py
+++ Python/Practice Set 1/Strings/Longest Repeating Subsequence.py
@@ -46,6 +46,26 @@ def memoized():
     print(lcs("ABC", "CBA"))
 
 
+def tabulation():
+    def lcs(s1, s2):
+        n, m = len(s1), len(s2)
+        dp = {i: {j: 0 for j in range(m + 1)} for i in range(n + 1)}
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+        return dp[n][m]
+
+    print(lcs("adebc", "dcadb"))
+    print(lcs("ab", "defg"))
+    print(lcs("AGGTAB", "GXTXAYB"))
+    print(lcs("ABC", "CBA"))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
