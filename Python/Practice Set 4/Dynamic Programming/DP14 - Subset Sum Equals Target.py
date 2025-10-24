3c63cf7f7b8f8d3f0ac76cbb330a3c8fb8786dee
hmnhGeek <hmnhsharma97@gmail.com>
2025-10-24 12:39:01 +0530

    Memoized Subset Sum

--- Python/Practice Set 4/Dynamic Programming/DP14 - Subset Sum Equals Target.py
+++ Python/Practice Set 4/Dynamic Programming/DP14 - Subset Sum Equals Target.py
@@ -26,5 +26,39 @@ def recursive():
     print(subset_sum([3, 34, 4, 12, 5, 2], 30))
 
 
+def memoized():
+    """
+        Time complexity is O(n*k) and space complexity is O(n + n*k).
+    """
+    def subset_sum(arr, target):
+        n = len(arr)
+        dp = {i: {j: None for j in range(target + 1)} for i in range(n)}
+        return solve(arr, n - 1, target, dp)
+
+    def solve(arr, i, j, dp):
+        if j == 0:
+            return True
+        if i == 0:
+            return arr[0] == j
+        if dp[i][j] is not None:
+            return dp[i][j]
+        left = False
+        if j >= arr[i]:
+            left = solve(arr, i - 1, j - arr[i], dp)
+        right = solve(arr, i - 1, j, dp)
+        dp[i][j] = left or right
+        return dp[i][j]
+
+    print(subset_sum([1, 2, 3, 4], 4))
+    print(subset_sum([4, 3, 2, 1], 5))
+    print(subset_sum([2, 5, 1, 6, 7], 4))
+    print(subset_sum([6, 1, 2, 1], 4))
+    print(subset_sum([1, 7, 2, 9, 10], 6))
+    print(subset_sum([3, 34, 4, 12, 5, 2], 9))
+    print(subset_sum([3, 34, 4, 12, 5, 2], 30))
+
+
 recursive()
+print()
+memoized()
 print()
