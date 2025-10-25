5822961018d5d98b8b20bc481af5c4834bbff5bc
hmnhGeek <hmnhsharma97@gmail.com>
2025-10-25 14:49:54 +0530

    Partition equals subset sum

--- Python/Practice Set 4/Dynamic Programming/DP15 - Partition Equal Subset Sum.py
+++ Python/Practice Set 4/Dynamic Programming/DP15 - Partition Equal Subset Sum.py
@@ -1,3 +1,7 @@
+# Problem link - https://leetcode.com/problems/partition-equal-subset-sum/description/
+# Solution - https://www.youtube.com/watch?v=7win3dcgo3k&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=16
+
+
 def recursive():
     """
         Time complexity is O(2^n) and space complexity is O(n).
@@ -116,11 +120,42 @@ def space_optimized():
     print(subset_sum([3, 34, 4, 12, 5, 2], 30))
 
 
-recursive()
-print()
-memoized()
-print()
-tabulation()
-print()
-space_optimized()
-print()
+class Solution:
+    @staticmethod
+    def subset_sum(arr, target):
+        n = len(arr)
+        prev = {j: False for j in range(target + 1)}
+        prev[0] = True
+        prev[arr[0]] = True
+        for i in range(1, n):
+            curr = {j: False for j in range(target + 1)}
+            curr[0] = True
+            for j in range(target + 1):
+                left = False
+                if j >= arr[i]:
+                    left = prev[j - arr[i]]
+                right = prev[j]
+                curr[j] = left or right
+            prev = curr
+        return prev[target]
+
+    @staticmethod
+    def partition_equals_subset_sum(arr):
+        """
+            Time complexity is O(n * (n/2)) and space complexity is O(n/2).
+        """
+        k = sum(arr)
+        if k % 2 != 0:
+            return False
+        target = k // 2
+        return Solution.subset_sum(arr, target)
+
+
+print(Solution.partition_equals_subset_sum([2, 3, 3, 3, 4, 5]))
+print(Solution.partition_equals_subset_sum([3, 1, 1, 2, 2, 1]))
+print(Solution.partition_equals_subset_sum([5, 6, 5, 11, 6]))
+print(Solution.partition_equals_subset_sum([2, 2, 1, 1, 1, 1, 1, 3, 3]))
+print(Solution.partition_equals_subset_sum([8, 7, 6, 12, 4, 5]))
+print(Solution.partition_equals_subset_sum([1, 5, 11, 5]))
+print(Solution.partition_equals_subset_sum([1, 3, 5]))
+print(Solution.partition_equals_subset_sum([1, 2, 3, 5]))
