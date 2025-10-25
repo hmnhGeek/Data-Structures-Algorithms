7c7c0a4f2aafb25734d98072ca8497553a745e86
hmnhGeek <hmnhsharma97@gmail.com>
2025-10-25 14:46:01 +0530

    Space optimized Subset Sum

--- Python/Practice Set 4/Dynamic Programming/DP15 - Partition Equal Subset Sum.py
+++ Python/Practice Set 4/Dynamic Programming/DP15 - Partition Equal Subset Sum.py
@@ -86,9 +86,41 @@ def tabulation():
     print(subset_sum([3, 34, 4, 12, 5, 2], 30))
 
 
+def space_optimized():
+    """
+        Time complexity is O(n*k) and space complexity is O(k).
+    """
+    def subset_sum(arr, target):
+        n = len(arr)
+        prev = {j: False for j in range(target + 1)}
+        prev[0] = True
+        prev[arr[0]] = True
+        for i in range(1, n):
+            curr = {j: False for j in range(target + 1)}
+            curr[0] = True
+            for j in range(target + 1):
+                left = False
+                if j >= arr[i]:
+                    left = prev[j - arr[i]]
+                right = prev[j]
+                curr[j] = left or right
+            prev = curr
+        return prev[target]
+
+    print(subset_sum([1, 2, 3, 4], 4))
+    print(subset_sum([4, 3, 2, 1], 5))
+    print(subset_sum([2, 5, 1, 6, 7], 4))
+    print(subset_sum([6, 1, 2, 1], 4))
+    print(subset_sum([1, 7, 2, 9, 10], 6))
+    print(subset_sum([3, 34, 4, 12, 5, 2], 9))
+    print(subset_sum([3, 34, 4, 12, 5, 2], 30))
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
+print()
+space_optimized()
 print()
