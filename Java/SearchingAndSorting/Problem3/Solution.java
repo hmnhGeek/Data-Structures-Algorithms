e9072f3b151f570de74e379653e676e1e3de84a5
hmnhGeek <hmnhsharma97@gmail.com>
2025-10-26 18:20:08 +0530

    Search in a rotated sorted array

--- Java/SearchingAndSorting/Problem3/Solution.java
+++ Java/SearchingAndSorting/Problem3/Solution.java
@@ -1,3 +1,7 @@
+// Problem link - https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+// Solution - https://www.youtube.com/watch?v=5qGrJbHhqFs&list=PLgUwDviBIf0pMFMWuuvDNMAkoQFi-h0ZF&index=5
+
+
 package SearchingAndSorting.Problem3;
 
 import java.util.Arrays;
@@ -5,6 +9,9 @@ import java.util.List;
 
 public class Solution {
     public static int search(List<Integer> list, Integer target) {
+        /*
+            Time complexity is O(log(n)) and space complexity is O(1).
+         */
         int low = 0, high = list.size() - 1;
         while (low <= high) {
             int mid = (low + (high - low)/2);
@@ -30,5 +37,9 @@ public class Solution {
 
     public static void main(String[] args) {
         System.out.println(search(Arrays.asList(4, 5, 6, 7, 0, 1, 2), 0));
+        System.out.println(search(Arrays.asList(4,5,6,7,0,1,2), 3));
+        System.out.println(search(Arrays.asList(1), 0));
+        System.out.println(search(Arrays.asList(7, 8, 9, 1, 2, 3, 4, 5, 6), 1));
+        System.out.println(search(Arrays.asList(7, 8, 9, 1, 2, 3, 4, 5), 6));
     }
 }
