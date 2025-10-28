a18e16d4901cce701a1adc2bb2406f2d2143a1ff
hmnhGeek <hmnhsharma97@gmail.com>
2025-10-28 20:38:29 +0530

    Created a special stack

--- Java/StacksAndQueues/Problem8/SpecialStack.java
+++ Java/StacksAndQueues/Problem8/SpecialStack.java
@@ -1,5 +1,46 @@
 package StacksAndQueues.Problem8;
 
-public class SpecialStack<T> {
+public class SpecialStack {
+    public Stack<Integer> stack;
+    public Integer minValue;
 
+    public SpecialStack() {
+        this.stack = new Stack<>();
+        this.minValue = Integer.MAX_VALUE;
+    }
+
+    public boolean isEmpty() {
+        return this.stack.isEmpty();
+    }
+
+    public void push(Integer x) {
+        if (isEmpty()) {
+            minValue = x;
+            stack.push(x);
+        } else if (x < minValue) {
+            minValue = x;
+            stack.push(2*x - minValue);
+        } else {
+            stack.push(x);
+        }
+    }
+
+    public Integer pop() {
+        if (isEmpty()) return null;
+        Integer top = stack.head.data;
+        stack.pop();
+        if (top < minValue) {
+            Integer returnVal = 2*minValue - top;
+            minValue = 2*minValue - top;
+            return returnVal;
+        }
+        if (stack.isEmpty()) {
+            minValue = Integer.MAX_VALUE;
+        }
+        return top;
+    }
+
+    public Integer getMin() {
+        return minValue;
+    }
 }
