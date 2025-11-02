db7b8d7922c658ab29c44a7e152e8f44e7781964
hmnhGeek <hmnhsharma97@gmail.com>
2025-11-02 18:27:48 +0530

    Implemented a queue class

---
+++ Java/Graphs/G23/Queue.java
@@ -0,0 +1,47 @@
+package Graphs.G23;
+
+
+class Node<T> {
+    public T data;
+    public Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+public class Queue<T> {
+    public Node<T> head;
+    public Node<T> tail;
+    public Integer length;
+
+    public Queue() {
+        this.head = this.tail = null;
+        this.length = 0;
+    }
+
+    public boolean isEmpty() {
+        return this.length == 0;
+    }
+
+    public void push(T x) {
+        Node<T> node = new Node<>(x);
+        if (isEmpty()) {
+            this.head = this.tail = node;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.length += 1;
+    }
+
+    public T pop() {
+        if (isEmpty()) return null;
+        T item = this.head.data;
+        this.head = this.head.next;
+        this.length -= 1;
+        return item;
+    }
+}
