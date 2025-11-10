617403d1feef3322f0cf73c0c9222ce9582a3a59
hmnhGeek <hmnhsharma97@gmail.com>
2025-11-10 18:17:02 +0530

    Detect a cycle in directed graph using Topological Sort

--- Python/Practice Set 4/Graphs/G23 - Detect a Cycle in Directed Graph.py
+++ Python/Practice Set 4/Graphs/G23 - Detect a Cycle in Directed Graph.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1
+# Solution - https://www.youtube.com/watch?v=iTBaI90lpDQ&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=23
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -31,3 +35,77 @@ class Queue:
         self.length -= 1
         return item
 
+
+class Solution:
+    @staticmethod
+    def has_cycle(graph):
+        """
+            Time complexity is O(V + E) and space complexity is O(V).
+        """
+        indegrees = Solution._get_indegrees(graph)
+        queue = Queue()
+        for node in indegrees:
+            if indegrees[node] == 0:
+                queue.push(node)
+        count = 0
+        while not queue.is_empty():
+            node = queue.pop()
+            count += 1
+            for adj_node in graph[node]:
+                indegrees[adj_node] -= 1
+                if indegrees[adj_node] == 0:
+                    queue.push(adj_node)
+        return count != len(graph)
+
+
+    @staticmethod
+    def _get_indegrees(graph):
+        indegrees = {i: 0 for i in graph}
+        for node in graph:
+            for adj_node in graph[node]:
+                indegrees[adj_node] += 1
+        return indegrees
+
+
+print(
+    Solution.has_cycle(
+        {
+            0: [1],
+            1: [2],
+            2: [3],
+            3: [3]
+        }
+    )
+)
+
+print(
+    Solution.has_cycle(
+        {
+            0: [1],
+            1: [2],
+            2: []
+        }
+    )
+)
+
+print(
+    Solution.has_cycle(
+        {
+            0: [1, 2],
+            1: [2],
+            2: [0, 3],
+            3: [3]
+        }
+    )
+)
+
+print(
+    Solution.has_cycle(
+        {
+            0: [1, 2],
+            1: [2],
+            2: [3],
+            3: []
+        }
+    )
+)
