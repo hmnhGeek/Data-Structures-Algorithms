919817205a020ec991277189f22a012d950c010b
hmnhGeek <hmnhsharma97@gmail.com>
2025-11-10 18:34:44 +0530

    Course schedule

--- Python/Practice Set 4/Graphs/G24 - Course Schedule I and II.py
+++ Python/Practice Set 4/Graphs/G24 - Course Schedule I and II.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/course-schedule/1
+# Solution - https://www.youtube.com/watch?v=WAOfKpxYHR8&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=24
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -31,3 +35,69 @@ class Queue:
         self.length -= 1
         return item
 
+
+class Solution:
+    @staticmethod
+    def course_schedule(mtx):
+        """
+            Overall time complexity is O(V + E) and space complexity is O(V).
+        """
+        graph = Solution._get_graph(mtx)
+        indegrees = Solution._get_indegrees(graph)
+        queue = Queue()
+        for node in indegrees:
+            if indegrees[node] == 0:
+                queue.push(node)
+        result = []
+        while not queue.is_empty():
+            node = queue.pop()
+            result.append(node)
+            for adj_node in graph[node]:
+                indegrees[adj_node] -= 1
+                if indegrees[adj_node] == 0:
+                    queue.push(adj_node)
+        if len(graph) == len(result):
+            return result
+        return None
+
+    @staticmethod
+    def _get_graph(mtx):
+        n = len(mtx)
+        graph = {}
+        for i in range(n):
+            graph[mtx[i][0]] = []
+            graph[mtx[i][1]] = []
+        for i in range(n):
+            graph[mtx[i][1]].append(mtx[i][0])
+        return graph
+
+    @staticmethod
+    def _get_indegrees(graph):
+        indegrees = {i: 0 for i in graph}
+        for node in graph:
+            for adj_node in graph[node]:
+                indegrees[adj_node] += 1
+        return indegrees
+
+
+print(
+    Solution.course_schedule(
+        [
+            [1, 0]
+        ]
+    )
+)
+
+print(
+    Solution.course_schedule(
+        [
+            [1, 0],
+            [2, 0],
+            [3, 1],
+            [3, 2]
+        ]
+    )
+)
+
+print(Solution.course_schedule([[0, 1], [1, 0]]))
+print(Solution.course_schedule([[1, 2], [2, 3], [2, 4], [3, 4], [4, 3]]))
