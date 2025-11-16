74623c01e94b67dd03f00a26e7b56f4b637380bf
hmnhGeek <hmnhsharma97@gmail.com>
2025-11-16 16:24:30 +0530

    Median in a stream

--- Python/Practice Set 3/Heap/Median in a stream.py
+++ Python/Practice Set 3/Heap/Median in a stream.py
@@ -129,3 +129,37 @@ class MaxHeap:
         self.max_heapify_down(0)
         return item
 
+
+class MedianFinder:
+    def __init__(self):
+        self.min_heap = MinHeap()
+        self.max_heap = MaxHeap()
+        self.length = 0
+
+    def push(self, x):
+        self.max_heap.insert(x)
+        self.length += 1
+        if len(self.max_heap.heap) - len(self.min_heap.heap) >= 2:
+            item = self.max_heap.pop()
+            self.min_heap.insert(item)
+
+    def median(self):
+        if self.length == 0:
+            return
+        if self.length % 2 == 1:
+            return self.max_heap.heap[0]
+        return (self.max_heap.heap[0] + self.min_heap.heap[0])/2
+
+
+class Solution:
+    @staticmethod
+    def run(*args):
+        median_finder = MedianFinder()
+        result = []
+        for i in args:
+            median_finder.push(i)
+            result.append(median_finder.median())
+        return result
+
+
+print(Solution.run(5, 15, 1, 3, 2, 8))
