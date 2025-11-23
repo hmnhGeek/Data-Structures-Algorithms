dac42ce6872234def8edce9b689d9c68a5b5ba8c
hmnhGeek <hmnhsharma97@gmail.com>
2025-11-23 16:47:58 +0530

    Check if BT is a max heap or not

---
+++ Java/Heap/Problem13/Solution.java
@@ -0,0 +1,162 @@
+package Heap.Problem13;
+
+
+class TreeNode<T> {
+    public T data;
+    public TreeNode<T> left;
+    public TreeNode<T> right;
+
+    public TreeNode(T data) {
+        this.data = data;
+        this.left = this.right = null;
+    }
+}
+
+
+public class Solution {
+    public static boolean isHeap(TreeNode<Integer> root) {
+        Integer numNodes = getNumberOfNodesInTree(root);
+        if (root == null) return true;
+        return isCompleteBinaryTree(root, 0, numNodes) && isMaxHeap(root);
+    }
+
+    private static boolean isMaxHeap(TreeNode<Integer> root) {
+        if (root == null) return true;
+        if (root.left != null && root.left.data > root.data) return false;
+        if (root.right != null && root.right.data > root.data) return false;
+        return isMaxHeap(root.left) && isMaxHeap(root.right);
+    }
+
+    private static Integer getNumberOfNodesInTree(TreeNode<Integer> root) {
+        if (root == null) return 0;
+        return 1 + getNumberOfNodesInTree(root.left) + getNumberOfNodesInTree(root.right);
+    }
+
+    private static boolean isCompleteBinaryTree(TreeNode<Integer> root, int i, Integer numNodes) {
+        if (root == null) return true;
+        if (i > numNodes) return false;
+        return isCompleteBinaryTree(root.left, 2*i + 1, numNodes) && isCompleteBinaryTree(root.right, 2*i + 2, numNodes);
+    }
+
+    public static void main(String[] args) {
+        // Example 1
+        TreeNode<Integer> n10_e1 = new TreeNode<>(10);
+        TreeNode<Integer> n20_e1 = new TreeNode<>(20);
+        TreeNode<Integer> n30_e1 = new TreeNode<>(30);
+        TreeNode<Integer> n40_e1 = new TreeNode<>(40);
+        TreeNode<Integer> n60_e1 = new TreeNode<>(60);
+
+        n10_e1.left = n20_e1;
+        n10_e1.right = n30_e1;
+        n20_e1.left = n40_e1;
+        n20_e1.right = n60_e1;
+
+        System.out.println(Solution.isHeap(n10_e1));
+
+
+        // Example 2
+        TreeNode<Integer> n5_e2 = new TreeNode<>(5);
+        TreeNode<Integer> n2_e2 = new TreeNode<>(2);
+        TreeNode<Integer> n3_e2 = new TreeNode<>(3);
+
+        n5_e2.left = n2_e2;
+        n5_e2.right = n3_e2;
+
+        System.out.println(Solution.isHeap(n5_e2));
+
+
+        // Example 3
+        TreeNode<Integer> n40_e3 = new TreeNode<>(40);
+        TreeNode<Integer> n36_e3 = new TreeNode<>(36);
+        TreeNode<Integer> n23_e3 = new TreeNode<>(23);
+        TreeNode<Integer> n10_e3 = new TreeNode<>(10);
+        TreeNode<Integer> n23b_e3 = new TreeNode<>(23);
+        TreeNode<Integer> n5_e3 = new TreeNode<>(5);
+        TreeNode<Integer> n6_e3 = new TreeNode<>(6);
+        TreeNode<Integer> n1_e3 = new TreeNode<>(1);
+        TreeNode<Integer> n14_e3 = new TreeNode<>(14);
+
+        n40_e3.left = n36_e3;
+        n40_e3.right = n23_e3;
+        n36_e3.left = n10_e3;
+        n36_e3.right = n23b_e3;
+        n23_e3.left = n5_e3;
+        n23_e3.right = n6_e3;
+        n10_e3.left = n1_e3;
+        n10_e3.right = n14_e3;
+
+        System.out.println(Solution.isHeap(n40_e3));
+
+
+        // Example 4
+        TreeNode<Integer> n40_e4 = new TreeNode<>(40);
+        TreeNode<Integer> n36_e4 = new TreeNode<>(36);
+        TreeNode<Integer> n23_e4 = new TreeNode<>(23);
+        TreeNode<Integer> n10_e4 = new TreeNode<>(10);
+        TreeNode<Integer> n5_e4 = new TreeNode<>(5);
+        TreeNode<Integer> n6_e4 = new TreeNode<>(6);
+        TreeNode<Integer> n1_e4 = new TreeNode<>(1);
+        TreeNode<Integer> n14_e4 = new TreeNode<>(14);
+
+        n40_e4.left = n36_e4;
+        n40_e4.right = n23_e4;
+        n36_e4.left = n10_e4;
+        n23_e4.left = n5_e4;
+        n23_e4.right = n6_e4;
+        n10_e4.left = n1_e4;
+        n5_e4.left = n14_e4;
+
+        System.out.println(Solution.isHeap(n40_e4));
+
+
+        // Example 5
+        TreeNode<Integer> n15_e5 = new TreeNode<>(15);
+        TreeNode<Integer> n14_e5 = new TreeNode<>(14);
+        TreeNode<Integer> n10_e5 = new TreeNode<>(10);
+        TreeNode<Integer> n4_e5 = new TreeNode<>(4);
+        TreeNode<Integer> n5_e5 = new TreeNode<>(5);
+        TreeNode<Integer> n11_e5 = new TreeNode<>(11);
+        TreeNode<Integer> n5b_e5 = new TreeNode<>(5);
+        TreeNode<Integer> n1_e5 = new TreeNode<>(1);
+        TreeNode<Integer> n2_e5 = new TreeNode<>(2);
+
+        n15_e5.left = n14_e5;
+        n15_e5.right = n10_e5;
+        n14_e5.left = n4_e5;
+        n14_e5.right = n5_e5;
+        n10_e5.left = n11_e5;
+        n10_e5.right = n5b_e5;
+        n4_e5.left = n1_e5;
+        n4_e5.right = n2_e5;
+
+        System.out.println(Solution.isHeap(n15_e5));
+
+        // Example 6
+        TreeNode<Integer> n97_img = new TreeNode<>(97);
+
+        TreeNode<Integer> n46_img = new TreeNode<>(46);
+        TreeNode<Integer> n37_img = new TreeNode<>(37);
+
+        TreeNode<Integer> n12_img = new TreeNode<>(12);
+        TreeNode<Integer> n3_img  = new TreeNode<>(3);
+        TreeNode<Integer> n7_img  = new TreeNode<>(7);
+        TreeNode<Integer> n31_img = new TreeNode<>(31);
+
+        TreeNode<Integer> n6_img = new TreeNode<>(6);
+        TreeNode<Integer> n9_img = new TreeNode<>(9);
+
+        n97_img.left = n46_img;
+        n97_img.right = n37_img;
+
+        n46_img.left = n12_img;
+        n46_img.right = n3_img;
+
+        n37_img.left = n7_img;
+        n37_img.right = n31_img;
+
+        n12_img.left = n6_img;
+        n12_img.right = n9_img;
+
+        System.out.println(Solution.isHeap(n97_img));
+    }
+}
