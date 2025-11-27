3b218c1330aa65d67b21dda61a23c44194412c0c
hmnhGeek <hmnhsharma97@gmail.com>
2025-11-27 17:39:46 +0530

    Find the Smallest Divisor Given a Threshold

---
+++ Java/BinarySearch/BS14/Solution.java
@@ -0,0 +1,37 @@
+package BinarySearch.BS14;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Solution {
+    public static Integer getSmallestDivisor(List<Integer> arr, Integer threshold) {
+        int low = 1, high = Collections.max(arr);
+        while (low <= high) {
+            int mid = (low + (high - low)/2);
+            int value = (int) getQuotients(arr, mid);
+            if (value <= threshold) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    private static double getQuotients(List<Integer> arr, Integer mid) {
+        double result = 0;
+        for (Integer integer : arr) {
+            result += Math.ceil((double) integer /mid);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Solution.getSmallestDivisor(List.of(1, 2, 5, 9), 6));
+        System.out.println(Solution.getSmallestDivisor(List.of(1, 2, 3, 4, 5), 8));
+        System.out.println(Solution.getSmallestDivisor(List.of(8, 4, 2, 3), 10));
+        System.out.println(Solution.getSmallestDivisor(List.of(2, 3, 5, 7, 11), 11));
+        System.out.println(Solution.getSmallestDivisor(List.of(44, 22, 33, 11, 1), 5));
+        System.out.println(Solution.getSmallestDivisor(List.of(1, 1, 1, 1), 4));
+    }
+}
