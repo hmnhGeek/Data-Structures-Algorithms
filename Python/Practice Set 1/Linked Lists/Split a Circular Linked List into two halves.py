b6ea9c25e1f6e38e473ca554d2ff56f049d74b9d
hmnhGeek <hmnhsharma97@gmail.com>
2025-12-02 17:07:57 +0530

    Added circular linked list code

---
+++ Python/Practice Set 1/Linked Lists/Split a Circular Linked List into two halves.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = self.tail = None
+        self.length = 0
+
+    def push(self, x):
+        node = Node(x)
+        if self.length == 0:
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+        self.tail.next = self.head
+        self.length += 1
+
+    def build(self, *args):
+        for i in args:
+            self.push(i)
+
+    def __str__(self):
+        if self.length == 0:
+            return "[]"
+        curr = self.head
+        result = "["
+        while curr != self.tail:
+            result += f"{curr.data}, "
+            curr = curr.next
+        result += f"{self.tail.data}]"
+        return result
+
