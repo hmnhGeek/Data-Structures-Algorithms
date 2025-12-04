55b3bde18079858bc242f94b0dd8827c93e622a4
hmnhGeek <hmnhsharma97@gmail.com>
2025-12-04 13:24:33 +0530

    Next greater element using stack

--- Python/Practice Set 1/Stacks and Queues/Next Greater Element.py
+++ Python/Practice Set 1/Stacks and Queues/Next Greater Element.py
@@ -36,3 +36,23 @@ class Stack:
         return self.head.data
 
 
+class Solution:
+    @staticmethod
+    def get_next_greater_elements(arr):
+        n = len(arr)
+        stack = Stack()
+        result = [None] * n
+        for i in range(-1, -n-1, -1):
+            while stack.top() is not None and stack.top() < arr[i]:
+                stack.pop()
+            result[i] = stack.top() if stack.top() else -1
+            stack.push(arr[i])
+        return result
+
+
+print(Solution.get_next_greater_elements([1, 3, 2, 4]))
+print(Solution.get_next_greater_elements([6, 8, 0, 1, 3]))
+print(Solution.get_next_greater_elements([10, 20, 30, 50]))
+print(Solution.get_next_greater_elements([50, 40, 30, 10]))
+print(Solution.get_next_greater_elements([4, 5, 2, 25]))
+print(Solution.get_next_greater_elements([13, 7, 6, 12]))
