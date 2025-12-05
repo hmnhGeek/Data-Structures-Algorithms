bae0d2ac0623f08575f19a22fff0389f62c6eac7
hmnhGeek <hmnhsharma97@gmail.com>
2025-12-05 17:21:34 +0530

    Memoized partition count

--- Python/Practice Set 4/Dynamic Programming/DP18 - Count Partitions With Given Difference.py
+++ Python/Practice Set 4/Dynamic Programming/DP18 - Count Partitions With Given Difference.py
@@ -29,4 +29,42 @@ def recursive():
     print(get_partition_count([35, 2, 8, 22], 0))
 
 
+def memoized():
+    """
+        Time complexity is O(n * k) and space complexity is O(n + nk).
+    """
+    def get_partition_count(arr, target):
+        n = len(arr)
+        dp = {i: {j: None for j in range(target + 1)} for i in range(n)}
+        return solve(arr, n - 1, target, dp)
+
+    def solve(arr, i, j, dp):
+        if j == 0:
+            return 1
+        if i == 0:
+            return 1 if arr[0] == j else 0
+        if dp[i][j] is not None:
+            return dp[i][j]
+        left = 0
+        if arr[i] <= j:
+            left = solve(arr, i - 1, j - arr[i], dp)
+        right = solve(arr, i - 1, j, dp)
+        dp[i][j] = left + right
+        return dp[i][j]
+
+    print(get_partition_count([1, 2, 2, 3], 3))
+    print(get_partition_count([1, 1, 4, 5], 5))
+    print(get_partition_count([1, 1, 1], 2))
+    print(get_partition_count([2, 34, 5], 40))
+    print(get_partition_count([1, 2, 3, 3], 6))
+    print(get_partition_count([1, 1, 1, 1], 1))
+    print(get_partition_count([5, 2, 3, 10, 6, 8], 10))
+    print(get_partition_count([2, 5, 1, 4, 3], 10))
+    print(get_partition_count([5, 7, 8], 3))
+    print(get_partition_count([35, 2, 8, 22], 0))
+
+
 recursive()
+print()
+memoized()
+print()
