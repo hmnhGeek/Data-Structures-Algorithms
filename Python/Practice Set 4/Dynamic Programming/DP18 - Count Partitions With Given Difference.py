f23f611e06c4f806b9efc6f1b0e1e2baf1128e01
hmnhGeek <hmnhsharma97@gmail.com>
2025-12-05 17:19:28 +0530

    Recursive partition count

---
+++ Python/Practice Set 4/Dynamic Programming/DP18 - Count Partitions With Given Difference.py
@@ -0,0 +1,32 @@
+def recursive():
+    """
+        Time complexity is exponential and space complexity is O(n).
+    """
+    def get_partition_count(arr, target):
+        n = len(arr)
+        return solve(arr, n - 1, target)
+    
+    def solve(arr, i, j):
+        if j == 0:
+            return 1
+        if i == 0:
+            return 1 if arr[0] == j else 0
+        left = 0
+        if arr[i] <= j:
+            left = solve(arr, i - 1, j - arr[i])
+        right = solve(arr, i - 1, j)
+        return left + right
+
+    print(get_partition_count([1, 2, 2, 3], 3))
+    print(get_partition_count([1, 1, 4, 5], 5))
+    print(get_partition_count([1, 1, 1], 2))
+    print(get_partition_count([2, 34, 5], 40))
+    print(get_partition_count([1, 2, 3, 3], 6))
+    print(get_partition_count([1, 1, 1, 1], 1))
+    print(get_partition_count([5, 2, 3, 10, 6, 8], 10))
+    print(get_partition_count([2, 5, 1, 4, 3], 10))
+    print(get_partition_count([5, 7, 8], 3))
+    print(get_partition_count([35, 2, 8, 22], 0))
+
+
+recursive()
