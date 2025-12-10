24f0abe924c31030546b833b3d30125ef3c02d21
hmnhGeek <hmnhsharma97@gmail.com>
2025-12-10 17:34:08 +0530

    Utility methods for matrix manipulation

--- Java/PracticeSet1/Matrix/Problem5/Utility.java
+++ Java/PracticeSet1/Matrix/Problem5/Utility.java
@@ -1,5 +1,6 @@
 package PracticeSet1.Matrix.Problem5;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -33,5 +34,28 @@ public class Utility {
         }
         Collections.swap(arr, j, low);
         return j;
+    }
+
+    public static <T> List<T> flattenMatrix(List<List<T>> mtx) {
+        int n = mtx.size();
+        List<T> result = new ArrayList<>();
+        for (int i = 0; i < n; i += 1) {
+            for (int j = 0; j < n; j += 1) {
+                result.add(mtx.get(i).get(j));
+            }
+        }
+        return result;
+    }
+
+    public static <T> List<List<T>> rebuildMatrix(List<T> arr, int n) {
+        List<List<T>> matrix = new ArrayList<>();
+        for (int i = 0; i < n; i += 1) {
+            List<T> row = new ArrayList<>();
+            for (int j = 0; j < n; j += 1) {
+                row.add(arr.get(i*n + j))
+            }
+            matrix.add(row);
+        }
+        return matrix;
     }
 }
