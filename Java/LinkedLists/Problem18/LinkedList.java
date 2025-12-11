0724bc093817ed8dd1968ff4ff015c5f768f692a
hmnhGeek <hmnhsharma97@gmail.com>
2025-12-11 18:12:12 +0530

    Implemented a linked list class

---
+++ Java/LinkedLists/Problem18/LinkedList.java
@@ -0,0 +1,54 @@
+package LinkedLists.Problem18;
+
+
+class Node<T> {
+    public T data;
+    public Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+public class LinkedList<T> {
+    public Node<T> head;
+    public Node<T> tail;
+    public Integer length;
+
+    public LinkedList() {
+        this.head = this.tail = null;
+        this.length = 0;
+    }
+
+    public void push(T x) {
+        Node<T> node = new Node<>(x);
+        if (length == 0) {
+            this.head = this.tail = node;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.length += 1;
+    }
+
+    public void build(T...args) {
+        for (T item : args) {
+            push(item);
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (length == 0) return "[]";
+        Node<T> curr = this.head;
+        StringBuilder stringBuilder = new StringBuilder("[");
+        while (curr != this.tail) {
+            stringBuilder.append(String.format("%s, ", curr.data));
+            curr = curr.next;
+        }
+        stringBuilder.append(String.format("%s]", this.tail.data));
+        return stringBuilder.toString();
+    }
+}
---
+++ Java/LinkedLists/Problem18/Solution.java
@@ -0,0 +1,8 @@
+package LinkedLists.Problem18;
+
+public class Solution {
+    public static void main(String[] args) {
+        LinkedList<Integer> l1 = new LinkedList<>();
+        System.out.println(l1);
+    }
+}
