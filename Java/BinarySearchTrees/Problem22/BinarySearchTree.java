90f145e9d83fd0190a6c87de5f6382fa2cdf1f94
hmnhGeek <hmnhsharma97@gmail.com>
2025-12-13 18:05:46 +0530

    insert logic in BST

---
+++ Java/BinarySearchTrees/Problem22/BinarySearchTree.java
@@ -0,0 +1,74 @@
+package BinarySearchTrees.Problem22;
+
+
+class Node<T> {
+    public T data;
+    public Node<T> left;
+    public Node<T> right;
+    public Node<T> parent;
+    public Integer size;
+    public Integer height;
+    public Integer diameter;
+
+    public Node(T data) {
+        this.data = data;
+        this.left = this.right = this.parent = null;
+        this.size = this.height = this.diameter = 1;
+    }
+}
+
+
+public class BinarySearchTree<T extends Comparable<T>> {
+    public Node<T> root;
+    public Integer diameter;
+
+    public BinarySearchTree() {
+        this.root = null;
+        this.diameter = 0;
+    }
+
+    public void recalcAugmentation(Node<T> parent) {
+        this.diameter = 0;
+        while (parent != null) {
+            Integer leftSize = parent.left != null ? parent.left.size : 0;
+            Integer rightSize = parent.right != null ? parent.right.size : 0;
+            Integer leftHeight = parent.left != null ? parent.left.height : 0;
+            Integer rightHeight = parent.right != null ? parent.right.height : 0;
+            parent.size = 1 + leftSize + rightSize;
+            parent.height = 1 + Math.max(leftHeight, rightHeight);
+            parent.diameter = 1 + leftHeight + rightHeight;
+            parent = parent.parent;
+        }
+    }
+
+    public void insert(T x) {
+        Node<T> node = new Node<>(x);
+        if (this.root == null) {
+            this.root = node;
+            this.diameter = 1;
+            return;
+        }
+        insert(this.root, node);
+    }
+
+    public void insert(Node<T> start, Node<T> node) {
+        if (start == null || node == null) return;
+        if (node.data.compareTo(start.data) >= 0) {
+            if (start.right != null) {
+                insert(start.right, node);
+                return;
+            }
+            start.right = node;
+            node.parent = start;
+            recalcAugmentation(start);
+            return;
+        }
+        if (start.left != null) {
+            insert(start.left, node);
+            return;
+        }
+        start.left = node;
+        node.parent = start;
+        recalcAugmentation(start);
+    }
+}
