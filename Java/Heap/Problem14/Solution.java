196315717dbf74ffa420ab48728b47001bc375e9
hmnhGeek <hmnhsharma97@gmail.com>
2025-12-15 12:22:15 +0530

    Min cost to connect ropes using min heap

--- Java/Heap/Problem14/Solution.java
+++ Java/Heap/Problem14/Solution.java
@@ -1,11 +1,29 @@
 package Heap.Problem14;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
     public static void main(String[] args) {
+        System.out.println(connectRopes(Arrays.asList(4, 3, 2, 6)));
+        System.out.println(connectRopes(Arrays.asList(4, 2, 7, 6, 9)));
+        System.out.println(connectRopes(Arrays.asList(1, 2, 3)));
+        System.out.println(connectRopes(Arrays.asList(1, 2, 3, 4, 5)));
+        System.out.println(connectRopes(Arrays.asList(10)));
+        System.out.println(connectRopes(List.of()));
+    }
 
+    public static Integer connectRopes(List<Integer> ropes) {
+        MinHeap<Integer> minHeap = new MinHeap<>();
+        for (Integer ropeLength : ropes) {
+            minHeap.insert(ropeLength);
+        }
+        int cost = 0;
+        while (minHeap.getHeap().size() > 1) {
+            Integer rope1 = minHeap.pop(), rope2 = minHeap.pop();
+            cost += (rope1 + rope2);
+            minHeap.insert(rope1 + rope2);
+        }
+        return cost;
     }
 }
