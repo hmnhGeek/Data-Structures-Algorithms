70565de220c1fef880bca19f17d5a3a9bc245ebb
hmnhGeek <hmnhsharma97@gmail.com>
2025-12-15 09:22:56 +0530

    Created heap interface

---
+++ Java/Heap/Problem14/Heap.java
@@ -0,0 +1,54 @@
+package Heap.Problem14;
+
+import java.util.Collections;
+import java.util.List;
+
+public interface Heap<T extends Comparable<T>> {
+    List<T> getHeap();
+    Integer getChildIndex(Integer lci, Integer rci);
+    void heapifyUp(Integer startIndex);
+    void heapifyDown(Integer pi);
+
+    default Boolean isEmpty() {
+        return getHeap().isEmpty();
+    }
+
+    default Integer getLci(Integer pi) {
+        Integer lci = 2*pi + 1;
+        if (0 <= lci && lci < getHeap().size()) {
+            return lci;
+        }
+        return null;
+    }
+
+    default Integer getRci(Integer pi) {
+        Integer rci = 2*pi + 2;
+        if (0 <= rci && rci < getHeap().size()) {
+            return rci;
+        }
+        return null;
+    }
+
+    default Integer getPi(Integer ci) {
+        if (ci == 0) return null;
+        Integer pi = (ci - 1)/2;
+        if (0 <= pi && pi < getHeap().size()) {
+            return pi;
+        }
+        return null;
+    }
+
+    default void insert(T x) {
+        getHeap().add(x);
+        heapifyUp(getHeap().size() - 1);
+    }
+
+    default T pop() {
+        if (isEmpty()) return null;
+        T item = getHeap().getFirst();
+        Collections.swap(getHeap(), 0, getHeap().size() - 1);
+        getHeap().removeLast();
+        heapifyDown(0);
+        return item;
+    }
+}
