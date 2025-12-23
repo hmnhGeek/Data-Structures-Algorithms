3613fc076c092003eb2fef7518923ace49674df6
hmnhGeek <hmnhsharma97@gmail.com>
2025-12-23 19:52:57 +0530

    Implemented topological sorting logic

--- Java/Graphs/G27/Utility.java
+++ Java/Graphs/G27/Utility.java
@@ -1,10 +1,31 @@
 package Graphs.G27;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Utility {
-    public static <T> List<T> getTopologicalSort() {
-        return new ArrayList<>();
+    public static <T> Stack<T> getTopologicalSort(Map<T, List<T>> graph) {
+        Map<T, Boolean> visited = new HashMap<>();
+        Stack<T> stack = new Stack<>();
+        for (T node : graph.keySet()) {
+            visited.put(node, false);
+        }
+        for (T node : graph.keySet()) {
+            if (!visited.get(node)) {
+                dfs(graph, node, stack, visited);
+            }
+        }
+        return stack;
+    }
+
+    private static <T> void dfs(Map<T, List<T>> graph, T node, Stack<T> stack, Map<T, Boolean> visited) {
+        visited.put(node, true);
+        for (T adjNode : graph.get(node)) {
+            if (!visited.get(adjNode)) {
+                dfs(graph, adjNode, stack, visited);
+            }
+        }
+        stack.push(node);
     }
 }
