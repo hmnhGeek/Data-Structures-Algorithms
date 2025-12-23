56682f94ea48f7ed9f03b5b01990cf920b487bba
hmnhGeek <hmnhsharma97@gmail.com>
2025-12-23 19:49:48 +0530

    Implemented a stack class for topological sorting

---
+++ Java/Graphs/G27/Utility.java
@@ -0,0 +1,10 @@
+package Graphs.G27;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Utility {
+    public static <T> List<T> getTopologicalSort() {
+        return new ArrayList<>();
+    }
+}
