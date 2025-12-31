6836e4c83a0c79213628f1d5d9789a274f060acc
hmnhGeek <hmnhsharma97@gmail.com>
2025-12-31 15:05:07 +0530

    Space optimized target sum

--- Python/Practice Set 4/Dynamic Programming/DP21 - Target Sum.py
+++ Python/Practice Set 4/Dynamic Programming/DP21 - Target Sum.py
@@ -59,6 +59,28 @@ def tabulation():
     print(target_sum([1, 2, 3, 1], 3))
 
 
+def space_optimized():
+    def target_sum(arr, target):
+        n = len(arr)
+        prev = {j: 0 for j in range(target + 1)}
+        prev[0] = 1
+        prev[arr[0]] = 1
+        for i in range(1, n):
+            curr = {j: 0 for j in range(target + 1)}
+            curr[0] = 1
+            for j in range(target + 1):
+                left = 0
+                if j >= arr[i]:
+                    left = prev[j - arr[i]]
+                right = prev[j]
+                curr[j] = left + right
+            prev = curr
+        return prev[target]
+
+    print(target_sum([1, 2, 3, 1], 3))
+
+
 recursive()
 memoized()
 memoized()
+space_optimized()
