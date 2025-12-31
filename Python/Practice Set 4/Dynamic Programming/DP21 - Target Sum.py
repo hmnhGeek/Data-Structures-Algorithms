339bd53e2944dca78ecb00aa705279519c0df76b
hmnhGeek <hmnhsharma97@gmail.com>
2025-12-31 15:02:19 +0530

    Memoized target sum

--- Python/Practice Set 4/Dynamic Programming/DP21 - Target Sum.py
+++ Python/Practice Set 4/Dynamic Programming/DP21 - Target Sum.py
@@ -17,4 +17,28 @@ def recursive():
     print(target_sum([1, 2, 3, 1], 3))
 
 
+def memoized():
+    def target_sum(arr, target):
+        n = len(arr)
+        dp = {i: {j: None for j in range(target + 1)} for i in range(n)}
+        return solve(arr, n - 1, target, dp)
+
+    def solve(arr, i, j, dp):
+        if j == 0:
+            return 1
+        if i == 0:
+            return 1 if arr[0] == j else 0
+        if dp[i][j] is not None:
+            return dp[i][j]
+        left = 0
+        if j >= arr[i]:
+            left = solve(arr, i - 1, j - arr[i], dp)
+        right = solve(arr, i - 1, j, dp)
+        dp[i][j] = left + right
+        return dp[i][j]
+
+    print(target_sum([1, 2, 3, 1], 3))
+
+
 recursive()
+memoized()
