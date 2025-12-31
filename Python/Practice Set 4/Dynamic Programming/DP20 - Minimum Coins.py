a4b2a5b2b879cd11b9b23249d9e1c395d29f1a05
hmnhGeek <hmnhsharma97@gmail.com>
2025-12-31 14:35:48 +0530

    Memoized minimum coins

--- Python/Practice Set 4/Dynamic Programming/DP20 - Minimum Coins.py
+++ Python/Practice Set 4/Dynamic Programming/DP20 - Minimum Coins.py
@@ -35,5 +35,48 @@ def recursive():
     print(min_coins([4, 6, 2], 5))
 
 
+def memoized():
+    """
+        Time complexity is O(n*target) and space complexity is O(n + n*target).
+    """
+    def min_coins(arr, target):
+        n = len(arr)
+        dp = {i: {j: None for j in range(target + 1)} for i in range(n)}
+        coins = solve(arr, n - 1, target, dp)
+        if coins == 1e6:
+            return -1
+        return coins
+
+    def solve(arr, i, j, dp):
+        if j == 0:
+            return 0
+        if i == 0:
+            if j % arr[0] == 0:
+                return j // arr[0]
+            return 1e6
+        if dp[i][j] is not None:
+            return dp[i][j]
+        left = 1e6
+        if j >= arr[i]:
+            left = 1 + solve(arr, i, j - arr[i], dp)
+        right = solve(arr, i - 1, j, dp)
+        dp[i][j] = min(left, right)
+        return dp[i][j]
+
+    print(min_coins([1, 2, 3], 7))
+    print(min_coins([1], 0))
+    print(min_coins([12, 1, 3], 4))
+    print(min_coins([2, 1], 11))
+    print(min_coins([1, 2, 5], 11))
+    print(min_coins([2], 3))
+    print(min_coins([1, ], 0))
+    print(min_coins([25, 10, 5], 30))
+    print(min_coins([9, 6, 5, 1], 19))
+    print(min_coins([5, 1], 0))
+    print(min_coins([4, 6, 2], 5))
+
+
 recursive()
+print()
+memoized()
 print()
