29efa28dbb016e97847c772c9c45af9a2c31698a
hmnhGeek <hmnhsharma97@gmail.com>
2026-01-02 14:20:15 +0530

    Check if a linked list is palindrome or not

--- Python/Practice Set 1/Linked Lists/Palindrome Linked List.py
+++ Python/Practice Set 1/Linked Lists/Palindrome Linked List.py
@@ -35,3 +35,70 @@ class LinkedList:
             fast = fast.next.next
         return slow
 
+    def reverse(self):
+        if self.is_empty() or self.length == 1:
+            return
+        prev, curr = None, self.head
+        while curr is not None:
+            next_curr = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_curr
+        self.head, self.tail = self.tail, self.head
+
+    def __str__(self):
+        if self.is_empty():
+            return "[]"
+        curr = self.head
+        result = "["
+        while curr != self.tail:
+            result += f"{curr.data}, "
+            curr = curr.next
+        result += f"{curr.data}]"
+        return result
+
+
+class Solution:
+    @staticmethod
+    def check_palindrome(l: LinkedList) -> bool:
+        if l.length == 0 or l.length == 1:
+            return True
+        middle_node = l.get_middle_node()
+        next_to_middle = middle_node.next
+        middle_node.next = None
+        sub_list = LinkedList()
+        sub_list.head = next_to_middle
+        sub_list.tail = l.tail
+        sub_list.length = l.length // 2
+        sub_list.reverse()
+        middle_node.next = sub_list.head
+        l.tail = sub_list.tail
+        i, j = l.head, sub_list.head
+        is_palindrome = True
+        print("Partly reversed list: ", l)
+        while j is not None:
+            if i.data != j.data:
+                is_palindrome = False
+                break
+            i = i.next
+            j = j.next
+        middle_node.next = None
+        sub_list.reverse()
+        middle_node.next = sub_list.head
+        l.tail = sub_list.tail
+        print("Restored list: ", l)
+        return is_palindrome
+
+
+def test(*args):
+    l = LinkedList()
+    l.build(*args)
+    print(Solution.check_palindrome(l))
+    print()
+
+
+test(1, 2, 2, 1)
+test(1, 2, 3, 4)
+test(1, 2, 3, 2, 1)
+test(1, 2, 3, 4, 1)
+test(1, 2, 1, 1, 2, 1)
