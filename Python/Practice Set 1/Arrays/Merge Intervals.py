e27cd5ecdfd761b332f6158abfd167b2339790ad
hmnhGeek <hmnhsharma97@gmail.com>
2026-01-02 13:49:33 +0530

    Merge intervals

--- Python/Practice Set 1/Arrays/Merge Intervals.py
+++ Python/Practice Set 1/Arrays/Merge Intervals.py
@@ -1,9 +1,16 @@
+# Problem link - https://www.naukri.com/code360/problems/merge-overlapping-intervals_1082151
+# Solution - https://www.youtube.com/watch?v=IexN60k62jo&t=306s
+
+
 from typing import List
 
 
 class Solution:
     @staticmethod
     def merge_intervals(intervals: List[List[int]]):
+        """
+            Time complexity is O(n) and space complexity is O(n).
+        """
         if len(intervals) == 0:
             return []
         intervals.sort(key=lambda x: x[0])
@@ -26,3 +33,7 @@ class Solution:
 print(Solution.merge_intervals([[1, 3], [2, 6], [8, 10], [15, 18]]))
 print(Solution.merge_intervals([[1, 4], [4, 5]]))
 print(Solution.merge_intervals([[4, 7], [1, 4]]))
+print(Solution.merge_intervals([[1, 3], [2, 4], [6, 8], [9, 10]]))
+print(Solution.merge_intervals([[7, 8], [1, 5], [2, 4], [4, 6]]))
+print(Solution.merge_intervals([[1, 3], [1, 5], [6, 7]]))
+print(Solution.merge_intervals([[1, 2], [2, 3]]))
