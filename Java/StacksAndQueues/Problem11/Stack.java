3daca965aa4172458d151791ef8685834e00183d
hmnhGeek <hmnhsharma97@gmail.com>
2026-01-11 13:01:30 +0530

    Created a stack class

---
+++ Java/StacksAndQueues/Problem11/Stack.java
@@ -0,0 +1,46 @@
+package StacksAndQueues.Problem11;
+
+
+class Node<T> {
+    public T data;
+    public Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+public class Stack<T> {
+    public Node<T> head, tail;
+    public Integer length;
+
+    public Stack() {
+        head = tail = null;
+        length = 0;
+    }
+
+    public boolean isEmpty() {
+        return length.equals(0);
+    }
+
+    public void push(T x) {
+        Node<T> node = new Node<>(x);
+        if (isEmpty()) {
+            head = tail = node;
+        } else {
+            node.next = head;
+            head = node;
+        }
+        length += 1;
+    }
+
+    public T pop() {
+        if (isEmpty()) return null;
+        T item = head.data;
+        head = head.next;
+        length -= 1;
+        return item;
+    }
+}
