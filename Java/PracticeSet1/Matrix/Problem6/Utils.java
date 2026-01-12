4cddb0dbf971e9ddab46ad262304d85cda5103aa
hmnhGeek <hmnhsharma97@gmail.com>
2026-01-12 13:55:26 +0530

    Helper method for finding max area in histogram

--- Java/PracticeSet1/Matrix/Problem6/Utils.java
+++ Java/PracticeSet1/Matrix/Problem6/Utils.java
@@ -1,9 +1,30 @@
 package PracticeSet1.Matrix.Problem6;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Utils {
     public static Integer getMaxAreaHistogram(List<Integer> arr) {
-        return 0;
+        /*
+            Time complexity is O(n) and space complexity is O(n).
+         */
+        Stack<Integer> stack = new Stack<>();
+        Integer maxArea = 0;
+        for (int i = 0; i < arr.size(); i += 1) {
+            while (!stack.isEmpty() && arr.get(i) < arr.get(stack.top())) {
+                Integer bar = arr.get(stack.pop());
+                Integer top = stack.top() != null ? stack.top() : -1;
+                Integer area = bar * (i - top - 1);
+                maxArea = Math.max(area, maxArea);
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            Integer bar = arr.get(stack.pop());
+            Integer top = stack.top() != null ? stack.top() : -1;
+            Integer area = bar * (arr.size() - top - 1);
+            maxArea = Math.max(area, maxArea);
+        }
+        return maxArea;
     }
 }
