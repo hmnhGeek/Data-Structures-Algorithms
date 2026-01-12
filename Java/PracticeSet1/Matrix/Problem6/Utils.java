ded1442733916ddc5da9ed17149950a0a8cf5aaa
hmnhGeek <hmnhsharma97@gmail.com>
2026-01-12 13:38:24 +0530

    Implemented a stack class

---
+++ Java/PracticeSet1/Matrix/Problem6/Utils.java
@@ -0,0 +1,9 @@
+package PracticeSet1.Matrix.Problem6;
+
+import java.util.List;
+
+public class Utils {
+    public static Integer getMaxAreaHistogram(List<Integer> arr) {
+        return 0;
+    }
+}
