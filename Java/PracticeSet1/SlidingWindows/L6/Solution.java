74850f66f6c09b3c69d0dd53c8f1be48a78f7efe
hmnhGeek <hmnhsharma97@gmail.com>
2026-01-16 12:52:41 +0530

    Longest substring with k distinct characters

---
+++ Java/PracticeSet1/SlidingWindows/L6/Solution.java
@@ -0,0 +1,56 @@
+package PracticeSet1.SlidingWindows.L6;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(longestSubstring("aaabbccd", 2));
+        System.out.println(longestSubstring("abbbbbbc", 2));
+        System.out.println(longestSubstring("abcddefg", 3));
+        System.out.println(longestSubstring("aaaaaaaa", 3));
+        System.out.println(longestSubstring("abcefg", 1));
+        System.out.println(longestSubstring("aabbcc", 1));
+        System.out.println(longestSubstring("aabbcc", 2));
+        System.out.println(longestSubstring("aabbcc", 3));
+        System.out.println(longestSubstring("aaabbb", 3));
+
+    }
+
+    private static Integer getUniqueCount(Map<Character, Integer> d) {
+        int count = 0;
+        for (Character c : d.keySet()) {
+            if (d.get(c) > 0) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+
+    public static String longestSubstring(String string, Integer k) {
+        if (k <= 0) return "";
+        Map<Character, Integer> d = new HashMap<>();
+        for (Character c : string.toCharArray()) {
+            d.putIfAbsent(c, 0);
+        }
+        int left = 0, right = 0;
+        int startIndex = -1;
+        int length = 0;
+        while (right < string.length()) {
+            d.put(string.charAt(right), d.get(string.charAt(right)) + 1);
+            if (getUniqueCount(d) > k) {
+                d.put(string.charAt(left), d.get(string.charAt(left)) - 1);
+                left += 1;
+            }
+            if (right - left + 1 > length) {
+                length = right - left + 1;
+                startIndex = left;
+            }
+            right += 1;
+        }
+        if (startIndex != -1) {
+            return string.substring(startIndex, startIndex + length);
+        }
+        return "";
+    }
+}
