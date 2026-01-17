96f344340572dacde476a4b422b7a9d21e277973
hmnhGeek <hmnhsharma97@gmail.com>
2026-01-17 15:14:49 +0530

    Word Ladder 1

--- Java/Graphs/G29/Solution.java
+++ Java/Graphs/G29/Solution.java
@@ -4,13 +4,11 @@ import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        /*
-        print(Solution.word_ladder(["des","der","dfr","dgt","dfs"], "der", "dfs"))
-print(Solution.word_ladder(["geek", "gefk"], "gedk", "geek"))
-print(Solution.word_ladder(["poon", "plee", "same", "poie","plea","plie","poin"], "toon", "plea"))
-print(Solution.word_ladder(["hot","dot","dog","lot","log","cog"], "hit", "cog"))
-print(Solution.word_ladder(["hot","dot","dog","lot","log"], "hit", "cog"))
-         */
         System.out.println(WordLadder.getMinimumLength(Arrays.asList("des","der","dfr","dgt","dfs"), "der", "dfs"));
+        System.out.println(WordLadder.getMinimumLength(Arrays.asList("geek", "gefk"), "gedk", "geek"));
+        System.out.println(WordLadder.getMinimumLength(Arrays.asList("poon", "plee", "same", "poie","plea","plie","poin"), "toon", "plea"));
+        System.out.println(WordLadder.getMinimumLength(Arrays.asList("hot","dot","dog","lot","log","cog"), "hit", "cog"));
+        System.out.println(WordLadder.getMinimumLength(Arrays.asList("hot","dot","dog","lot","log"), "hit", "cog"));
+
     }
 }
