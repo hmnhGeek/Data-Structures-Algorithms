8774a2c77c1da9b1471f631d42eec0539fcc19a3
hmnhGeek <hmnhsharma97@gmail.com>
2026-01-17 16:19:27 +0530

    Printing the word ladder

--- Java/Graphs/G30/Solution.java
+++ Java/Graphs/G30/Solution.java
@@ -1,19 +1,16 @@
+// Problem link - https://www.geeksforgeeks.org/problems/word-ladder-ii/1
+// Solution - https://www.youtube.com/watch?v=DREutrv2XD0&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=30
+
+
 package Graphs.G30;
 
 import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        /*
-            print(Solution.word_ladder(["des","der","dfr","dgt","dfs"], "der", "dfs"))
-print(Solution.word_ladder(["geek", "gefk"], "gedk", "geek"))
-print(Solution.word_ladder(["hot","dot","dog","lot","log","cog"], "hit", "cog"))
-print(Solution.word_ladder(["hot","dot","dog","lot","log"], "hit", "cog"))
-         */
         System.out.println(WordLadder.getWordLadder(Arrays.asList("des","der","dfr","dgt","dfs"), "der", "dfs"));
         System.out.println(WordLadder.getWordLadder(Arrays.asList("geek", "gefk"), "gedk", "geek"));
         System.out.println(WordLadder.getWordLadder(Arrays.asList("hot","dot","dog","lot","log","cog"), "hit", "cog"));
         System.out.println(WordLadder.getWordLadder(Arrays.asList("hot","dot","dog","lot","log"), "hit", "cog"));
-
     }
 }
