0f2886440f8ea9602db228c5ba7dbc92907a24ae
hmnhGeek <hmnhsharma97@gmail.com>
2026-01-17 15:15:37 +0530

    Word Ladder 1

--- Java/Graphs/G29/Solution.java
+++ Java/Graphs/G29/Solution.java
@@ -1,9 +1,16 @@
+// Problem link - https://www.geeksforgeeks.org/problems/word-ladder/1
+// Solution - https://www.youtube.com/watch?v=tRPda0rcf8E&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=29
+
+
 package Graphs.G29;
 
 import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
+        /*
+            Time complexity is O(n * word_length * 26) and space complexity is O(n).
+         */
         System.out.println(WordLadder.getMinimumLength(Arrays.asList("des","der","dfr","dgt","dfs"), "der", "dfs"));
         System.out.println(WordLadder.getMinimumLength(Arrays.asList("geek", "gefk"), "gedk", "geek"));
         System.out.println(WordLadder.getMinimumLength(Arrays.asList("poon", "plee", "same", "poie","plea","plie","poin"), "toon", "plea"));
