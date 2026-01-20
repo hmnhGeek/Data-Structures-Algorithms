45ac499f6337286c762133256737a7e27ba37a3a
hmnhGeek <hmnhsharma97@gmail.com>
2026-01-20 18:36:17 +0530

    Implemented print functionality is BST

--- Java/BinarySearchTrees/Problem2/BinarySearchTree.java
+++ Java/BinarySearchTrees/Problem2/BinarySearchTree.java
@@ -146,4 +146,23 @@ public class BinarySearchTree<T extends Comparable<T>> {
         }
         return getNode(start.left, x);
     }
+
+    private void show(Node<T> start) {
+        if (start != null) {
+            show(start.left);
+            StringBuilder sb = new StringBuilder(String.format("Data = %s", start.data));
+            if (start == this.root) {
+                sb.append(" (root), ");
+            } else {
+                sb.append(", ");
+            }
+            sb.append(String.format("size = %d, height = %d, diameter = %d", start.size, start.height, start.diameter));
+            System.out.println(sb);
+            show(start.right);
+        }
+    }
+
+    public void show() {
+        show(root);
+    }
 }
