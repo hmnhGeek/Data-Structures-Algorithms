59b3da3ab3b5ed3fb608478bed9819a1eac8c169
hmnhGeek <hmnhsharma97@gmail.com>
2026-01-24 20:02:35 +0530

    Recursive unbounded knapsack

---
+++ Java/DynamicProgramming/DP23/RecursiveSolution.java
@@ -0,0 +1,32 @@
+package DynamicProgramming.DP23;
+
+import java.util.List;
+
+public class RecursiveSolution {
+    /*
+        Time complexity is exponential and space complexity is O(n).
+     */
+    public static Integer getUnboundedKnapsack(List<Integer> weights, List<Integer> values, Integer capacity) {
+        if (capacity < 0 || weights.size() != values.size()) return null;
+        int n = weights.size();
+        return solve(weights, values, n - 1, capacity);
+    }
+
+    private static Integer solve(List<Integer> weights, List<Integer> values, int i, Integer j) {
+        if (j == 0) return 0;
+        if (i == 0) {
+            if (j % weights.getFirst() == 0) {
+                return (j / weights.getFirst()) * values.getFirst();
+            }
+            return 0;
+        }
+        Integer left = Integer.MIN_VALUE;
+        if (j >= weights.get(i)) {
+            left = values.get(i) + solve(weights, values, i, j - weights.get(i));
+        }
+        Integer right = solve(weights, values, i - 1, j);
+        return Math.max(left, right);
+    }
+
+
+}
---
+++ Java/DynamicProgramming/DP23/Solution.java
@@ -0,0 +1,68 @@
+package DynamicProgramming.DP23;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        recursive();
+        System.out.println();
+    }
+
+    private static void recursive() {
+        System.out.println(
+                RecursiveSolution.getUnboundedKnapsack(
+                        Arrays.asList(2, 4, 6),
+                        Arrays.asList(5, 11, 13),
+                        10
+                )
+        );
+
+        System.out.println(
+                RecursiveSolution.getUnboundedKnapsack(
+                        Arrays.asList(5, 10, 20),
+                        Arrays.asList(7, 2, 4),
+                        15
+                )
+        );
+
+        System.out.println(
+                RecursiveSolution.getUnboundedKnapsack(
+                        Arrays.asList(6, 12),
+                        Arrays.asList(4, 17),
+                        3
+                )
+        );
+
+        System.out.println(
+                RecursiveSolution.getUnboundedKnapsack(
+                        Arrays.asList(1, 50),
+                        Arrays.asList(1, 30),
+                        100
+                )
+        );
+
+        System.out.println(
+                RecursiveSolution.getUnboundedKnapsack(
+                        Arrays.asList(1, 3, 4, 5),
+                        Arrays.asList(10, 40, 50, 70),
+                        8
+                )
+        );
+
+        System.out.println(
+                RecursiveSolution.getUnboundedKnapsack(
+                        Arrays.asList(1, 3, 4, 5),
+                        Arrays.asList(6, 1, 7, 7),
+                        8
+                )
+        );
+
+        System.out.println(
+                RecursiveSolution.getUnboundedKnapsack(
+                        Arrays.asList(2, 3, 4, 5),
+                        Arrays.asList(6, 8, 7, 100),
+                        1
+                )
+        );
+    }
+}
