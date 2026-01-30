bb054c0e363f7acc853dd88406801888bae43c2b
hmnhGeek <hmnhsharma97@gmail.com>
2026-01-30 12:13:13 +0530

    Basic structure of next permutation problem

--- Python/Practice Set 1/Arrays/Next Permutation.py
+++ Python/Practice Set 1/Arrays/Next Permutation.py
@@ -10,3 +10,17 @@ class Solution:
         Utility._sort(arr, breakpoint_index + 1)
         return arr
 
+    @staticmethod
+    def _get_breakpoint_index(arr, n):
+        for i in range(n - 2, -1, -1):
+            if arr[i] < arr[i + 1]:
+                return i
+        return -1
+
+    @staticmethod
+    def _get_just_greater(arr, breakpoint_index, n):
+        element = arr[breakpoint_index]
+        for i in range(n - 1, breakpoint_index, -1):
+            if arr[i] > element:
+                return i
+        return -1
