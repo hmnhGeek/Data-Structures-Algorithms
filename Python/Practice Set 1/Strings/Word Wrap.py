d7ce132303722532b21d235f3ca1b2fa9d1b2f89
hmnhGeek <hmnhsharma97@gmail.com>
2026-01-31 18:02:12 +0530

    Memoized word wrap

--- Python/Practice Set 1/Strings/Word Wrap.py
+++ Python/Practice Set 1/Strings/Word Wrap.py
@@ -23,4 +23,37 @@ def recursive():
     print(word_wrap([3, 2, 2], 4))
 
 
+def memoized():
+    """
+        Time complexity is O(nk) and space complexity is O(n + nk).
+    """
+    def word_wrap(arr, k):
+        dp = {i: {j: None for j in range(k + 1)} for i in range(len(arr))}
+        return solve(1, arr[0], arr, k, dp)
+
+    def solve(i, consumed_spaces, arr, k, dp):
+        if i == len(arr):
+            return 0
+
+        if dp[i][consumed_spaces] is not None:
+            return dp[i][consumed_spaces]
+
+        # staying on the same line
+        stays_on_same_line = 1e6
+        new_consumed_spaces = consumed_spaces + 1 + arr[i]
+        if new_consumed_spaces <= k:
+            stays_on_same_line = solve(i + 1, new_consumed_spaces, arr, k, dp)
+
+        # moving to the next line
+        moving_to_next_line = (k - consumed_spaces)**2 + solve(i + 1, arr[i], arr, k, dp)
+        dp[i][consumed_spaces] = min(stays_on_same_line, moving_to_next_line)
+        return dp[i][consumed_spaces]
+
+    print(word_wrap([3, 2, 2, 5], 6))
+    print(word_wrap([3, 2, 2], 4))
+
+
 recursive()
+print()
+memoized()
+print()
