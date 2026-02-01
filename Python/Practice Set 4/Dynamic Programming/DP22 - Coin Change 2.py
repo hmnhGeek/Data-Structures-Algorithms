d0c1970e5574a0a64915248dbab2410af5efe806
hmnhGeek <hmnhsharma97@gmail.com>
2026-02-01 18:30:51 +0530

    Tabulation coin change 2

--- Python/Practice Set 4/Dynamic Programming/DP22 - Coin Change 2.py
+++ Python/Practice Set 4/Dynamic Programming/DP22 - Coin Change 2.py
@@ -60,7 +60,38 @@ def memoized():
     print(coin_change_2([2, 5, 3, 6], 10))
 
 
+def tabulation():
+    """
+        Time complexity is O(nk) and space complexity is O(nk).
+    """
+    def coin_change_2(arr, target):
+        n = len(arr)
+        dp = {i: {j: 0 for j in range(target + 1)} for i in range(n)}
+        for i in range(n):
+            dp[i][0] = 1
+        for j in range(target + 1):
+            if j % arr[0] == 0:
+                dp[0][j] = 1
+        for i in range(1, n):
+            for j in range(target + 1):
+                left = 0
+                if arr[i] <= j:
+                    left = dp[i][j - arr[i]]
+                right = dp[i - 1][j]
+                dp[i][j] = left + right
+        return dp[n - 1][target]
+
+    print(coin_change_2([1, 2, 3], 4))
+    print(coin_change_2([5, 3, 2], 1))
+    print(coin_change_2([1, 2, 5], 5))
+    print(coin_change_2([2], 3))
+    print(coin_change_2([10], 10))
+    print(coin_change_2([2, 5, 3, 6], 10))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
 print()
