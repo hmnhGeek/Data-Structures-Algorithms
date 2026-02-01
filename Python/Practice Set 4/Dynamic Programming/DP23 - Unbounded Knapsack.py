c88e3cfe83811f8380e4e662a4e32b31d5754e55
hmnhGeek <hmnhsharma97@gmail.com>
2026-02-01 19:28:24 +0530

    Space optimized Unbounded Knapsack

--- Python/Practice Set 4/Dynamic Programming/DP23 - Unbounded Knapsack.py
+++ Python/Practice Set 4/Dynamic Programming/DP23 - Unbounded Knapsack.py
@@ -96,9 +96,42 @@ def tabulation():
     print(knapsack([2, 3, 4, 5], [6, 8, 7, 100], 1))
 
 
+def space_optimized():
+    """
+        Time complexity is O(n * capacity) and space complexity is O(capacity).
+    """
+    def knapsack(weights, values, capacity):
+        n = len(weights)
+        prev = {j: 0 for j in range(capacity + 1)}
+        for j in range(capacity + 1):
+            if j % weights[0] == 0:
+                prev[j] = (j // weights[0]) * values[0]
+        for i in range(1, n):
+            curr = {j: -1e6 for j in range(capacity + 1)}
+            curr[0] = 0
+            for j in range(capacity + 1):
+                left = -1e6
+                if weights[i] <= j:
+                    left = values[i] + curr[j - weights[i]]
+                right = prev[j]
+                curr[j] = max(left, right)
+            prev = curr
+        return prev[capacity]
+
+    print(knapsack([2, 4, 6], [5, 11, 13], 10))
+    print(knapsack([5, 10, 20], [7, 2, 4], 15))
+    print(knapsack([6, 12], [4, 17], 3))
+    print(knapsack([1, 50], [1, 30], 100))
+    print(knapsack([1, 3, 4, 5], [10, 40, 50, 70], 8))
+    print(knapsack([1, 3, 4, 5], [6, 1, 7, 7], 8))
+    print(knapsack([2, 3, 4, 5], [6, 8, 7, 100], 1))
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
+print()
+space_optimized()
 print()
