c1459cbb1688e0be06c52cdf7d20b22dea4e50c6
hmnhGeek <hmnhsharma97@gmail.com>
2026-02-01 19:22:45 +0530

    Recursive Unbounded Knapsack

---
+++ Python/Practice Set 4/Dynamic Programming/DP23 - Unbounded Knapsack.py
@@ -0,0 +1,34 @@
+
+
+def recursive():
+    """
+        Time complexity is exponential and space complexity is O(n).
+    """
+    def knapsack(weights, values, capacity):
+        n = len(weights)
+        return solve(weights, values, n - 1, capacity)
+
+    def solve(weights, values, i, j):
+        if j == 0:
+            return 0
+        if i == 0:
+            if j % weights[0] == 0:
+                return (j // weights[0]) * values[0]
+            return 0
+        left = -1e6
+        if weights[i] <= j:
+            left = values[i] + solve(weights, values, i, j - weights[i])
+        right = solve(weights, values, i - 1, j)
+        return max(left, right)
+
+    print(knapsack([2, 4, 6], [5, 11, 13], 10))
+    print(knapsack([5, 10, 20], [7, 2, 4], 15))
+    print(knapsack([6, 12], [4, 17], 3))
+    print(knapsack([1, 50], [1, 30], 100))
+    print(knapsack([1, 3, 4, 5], [10, 40, 50, 70], 8))
+    print(knapsack([1, 3, 4, 5], [6, 1, 7, 7], 8))
+    print(knapsack([2, 3, 4, 5], [6, 8, 7, 100], 1))
+
+
+recursive()
+print()
