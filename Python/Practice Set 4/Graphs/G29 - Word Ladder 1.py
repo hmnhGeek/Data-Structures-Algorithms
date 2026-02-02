6578e1eb5182c99976f9ab8d9328b6377472276f
hmnhGeek <hmnhsharma97@gmail.com>
2026-02-02 20:49:11 +0530

    Word Ladder 1

--- Python/Practice Set 4/Graphs/G29 - Word Ladder 1.py
+++ Python/Practice Set 4/Graphs/G29 - Word Ladder 1.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/word-ladder/1
+# Solution - https://www.youtube.com/watch?v=tRPda0rcf8E&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=29
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -35,6 +39,9 @@ class Queue:
 class Solution:
     @staticmethod
     def word_ladder(word_list, start_word, end_word):
+        """
+            Time complexity is O(n * word_length * 26) and space complexity is O(n).
+        """
         word_list_set = set(word_list)
         queue = Queue()
         queue.push((start_word, 1))
