9a15d67dce0882a590c51e7ac46f6f8957885fd9
hmnhGeek <hmnhsharma97@gmail.com>
2026-02-23 14:30:13 +0530

    Recursive Edit Distance

---
+++ Python/Practice Set 1/Strings/Edit Distance.py
@@ -0,0 +1,33 @@
+def recursive():
+    """
+        Time complexity is exponential and space complexity is O(n1 + n2).
+    """
+
+    def edit_distance(s, t):
+        n, m = len(s), len(t)
+        return solve(s, n - 1, t, m - 1)
+
+    def solve(s, i, t, j):
+        if i < 0:
+            return j + 1
+        if j < 0:
+            return i + 1
+        if s[i] == t[j]:
+            return solve(s, i - 1, t, j - 1)
+        else:
+            return min(
+                1 + solve(s, i - 1, t, j),
+                1 + solve(s, i, t, j - 1),
+                1 + solve(s, i - 1, t, j - 1)
+            )
+
+    print(edit_distance("geek", "gesek"))
+    print(edit_distance("gfg", "gfg"))
+    print(edit_distance("abc", "def"))
+    print(edit_distance("horse", "ros"))
+    print(edit_distance("intention", "execution"))
+    print(edit_distance("abc", "dc"))
+    print(edit_distance("whgtdwhgtdg", "aswcfg"))
+
+
+recursive()
