0d01b3cd60daf11c0f366cca01ca90e272507206
hmnhGeek <hmnhsharma97@gmail.com>
2026-02-23 14:39:13 +0530

    Memoized Edit Distance

--- Python/Practice Set 1/Strings/Edit Distance.py
+++ Python/Practice Set 1/Strings/Edit Distance.py
@@ -30,4 +30,43 @@ def recursive():
     print(edit_distance("whgtdwhgtdg", "aswcfg"))
 
 
+def memoized():
+    """
+        Time complexity is O(nm) and space complexity is O(n + m + nm).
+    """
+
+    def edit_distance(s, t):
+        n, m = len(s), len(t)
+        dp = {i: {j: None for j in range(m + 1)} for i in range(n + 1)}
+        return solve(s, n, t, m, dp)
+
+    def solve(s, i, t, j, dp):
+        if i == 0:
+            return j
+        if j == 0:
+            return i
+        if dp[i][j] is not None:
+            return dp[i][j]
+        if s[i - 1] == t[j - 1]:
+            dp[i][j] = solve(s, i - 1, t, j - 1, dp)
+        else:
+            dp[i][j] = min(
+                1 + solve(s, i - 1, t, j, dp),
+                1 + solve(s, i, t, j - 1, dp),
+                1 + solve(s, i - 1, t, j - 1, dp)
+            )
+        return dp[i][j]
+
+    print(edit_distance("geek", "gesek"))
+    print(edit_distance("gfg", "gfg"))
+    print(edit_distance("abc", "def"))
+    print(edit_distance("horse", "ros"))
+    print(edit_distance("intention", "execution"))
+    print(edit_distance("abc", "dc"))
+    print(edit_distance("whgtdwhgtdg", "aswcfg"))
+
+
 recursive()
+print()
+memoized()
+print()
