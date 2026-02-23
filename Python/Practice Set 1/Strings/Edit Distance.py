9f0f4adec952c20e05e6d0f7159053609ddf60ad
hmnhGeek <hmnhsharma97@gmail.com>
2026-02-23 14:41:37 +0530

    Tabulation Edit Distance

--- Python/Practice Set 1/Strings/Edit Distance.py
+++ Python/Practice Set 1/Strings/Edit Distance.py
@@ -66,7 +66,42 @@ def memoized():
     print(edit_distance("whgtdwhgtdg", "aswcfg"))
 
 
+def tabulation():
+    """
+        Time complexity is O(nm) and space complexity is O(nm).
+    """
+
+    def edit_distance(s, t):
+        n, m = len(s), len(t)
+        dp = {i: {j: 0 for j in range(m + 1)} for i in range(n + 1)}
+        for j in range(m + 1):
+            dp[0][j] = j
+        for i in range(n + 1):
+            dp[i][0] = i
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if s[i - 1] == t[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = min(
+                        1 + dp[i - 1][j],
+                        1 + dp[i][j - 1],
+                        1 + dp[i - 1][j - 1]
+                    )
+        return dp[n][m]
+
+    print(edit_distance("geek", "gesek"))
+    print(edit_distance("gfg", "gfg"))
+    print(edit_distance("abc", "def"))
+    print(edit_distance("horse", "ros"))
+    print(edit_distance("intention", "execution"))
+    print(edit_distance("abc", "dc"))
+    print(edit_distance("whgtdwhgtdg", "aswcfg"))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
 print()
