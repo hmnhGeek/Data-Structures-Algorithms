5a16a6a812524e42de3815073797df529a0e2922
hmnhGeek <hmnhsharma97@gmail.com>
2026-02-24 19:01:09 +0530

    Rod cutting problem memoized

--- Python/Practice Set 4/Dynamic Programming/DP24 - Rod Cutting Problem.py
+++ Python/Practice Set 4/Dynamic Programming/DP24 - Rod Cutting Problem.py
@@ -1,4 +1,7 @@
 def recursive():
+    """
+        Time complexity is exponential and space complexity is O(n).
+    """
     def rod_cut(prices, rod_length):
         n = len(prices)
         return solve(prices, n - 1, rod_length)
@@ -22,5 +25,38 @@ def recursive():
     print(rod_cut([3], 1))
 
 
+def memoized():
+    """
+        Time complexity is O(rod_length * n) and space complexity is O(n + rod_length * n).
+    """
+    def rod_cut(prices, rod_length):
+        n = len(prices)
+        dp = {i: {j: None for j in range(rod_length + 1)} for i in range(-1, n)}
+        return solve(prices, n - 1, rod_length, dp)
+
+    def solve(arr, i, j, dp):
+        if i < 0:
+            return 0
+        if j == 0:
+            return 0
+        if dp[i][j] is not None:
+            return dp[i][j]
+        left = -1e6
+        if i + 1 <= j:
+            left = arr[i] + solve(arr, i, j - i - 1, dp)
+        right = solve(arr, i - 1, j, dp)
+        dp[i][j] = max(left, right)
+        return dp[i][j]
+
+    print(rod_cut([2, 5, 7, 8, 10], 5))
+    print(rod_cut([3, 5, 8, 9, 10, 17, 17, 20], 8))
+    print(rod_cut([3, 5, 6, 7, 10, 12], 6))
+    print(rod_cut([1, 10, 3, 1, 3, 1, 5, 9], 8))
+    print(rod_cut([1, 5, 8, 9, 10, 17, 17, 20], 8))
+    print(rod_cut([3], 1))
+
+
 recursive()
+print()
+memoized()
 print()
