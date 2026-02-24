f9f96e326d73dd62f856407e527e66273c7524ed
hmnhGeek <hmnhsharma97@gmail.com>
2026-02-24 19:30:51 +0530

    LCS length tabulation

--- Python/Practice Set 4/Dynamic Programming/DP25 - Longest Common Subsequence.py
+++ Python/Practice Set 4/Dynamic Programming/DP25 - Longest Common Subsequence.py
@@ -58,7 +58,36 @@ def memoized():
     print(lcs("ABC", "CBA"))
 
 
+def tabulation():
+    """
+        Time complexity is O(nm) and space complexity is O(nm).
+    """
+    def lcs(s1, s2):
+        n, m = len(s1), len(s2)
+        dp = {i: {j: 0 for j in range(m + 1)} for i in range(n + 1)}
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(
+                        dp[i - 1][j],
+                        dp[i][j - 1]
+                    )
+        return dp[n][m]
+
+    print(lcs("adebc", "dcadb"))
+    print(lcs("ab", "defg"))
+    print(lcs("abcde", "ace"))
+    print(lcs("abc", "abc"))
+    print(lcs("abc", "acd"))
+    print(lcs("AGGTAB", "GXTXAYB"))
+    print(lcs("ABC", "CBA"))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
 print()
