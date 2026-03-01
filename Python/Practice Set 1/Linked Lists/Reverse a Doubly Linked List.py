7edc727a5b86ab8584a0bdbfa820b36861182d0b
hmnhGeek <hmnhsharma97@gmail.com>
2026-03-01 10:31:17 +0530

    Implemented a doubly linked list class

---
+++ Python/Practice Set 1/Linked Lists/Reverse a Doubly Linked List.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = self.next = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = self.tail = None
+        self.length = 0
+
+    def is_empty(self):
+        return self.length == 0
+
+    def push(self, x):
+        node = Node(x)
+        if self.is_empty():
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = node
+        self.length += 1
+
+    def __str__(self):
+        if self.is_empty():
+            return "[]"
+        result = "["
+        curr = self.head
+        while curr != self.tail:
+            result += f"{curr.data}, "
+            curr = curr.next
+        result += f"{self.tail.data}]"
+        return result
+
