89081c89267b0a2ca0465d907eabb1af4bc2e30f
hmnhGeek <hmnhsharma97@gmail.com>
2026-03-04 16:44:12 +0530

    Binary Search (Python): Min heap class added

---
+++ Python/Practice Set 4/Binary Search/BS20 - Minimise Maximum Distance between Gas Stations.py
@@ -0,0 +1,70 @@
+class Utility:
+    @staticmethod
+    def swap(arr, i, j):
+        arr[i], arr[j] = arr[j], arr[i]
+
+
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def is_empty(self):
+        return len(self.heap) == 0
+
+    def get_lci(self, pi):
+        lci = 2*pi + 1
+        return lci if lci in range(len(self.heap)) else None
+
+    def get_rci(self, pi):
+        rci = 2*pi + 2
+        return rci if rci in range(len(self.heap)) else None
+
+    def get_pi(self, ci):
+        if ci == 0:
+            return
+        pi = int((ci - 1)/2)
+        return pi if pi in range(len(self.heap)) else None
+
+    def get_min_child_index(self, lci, rci):
+        if lci is None and rci is None:
+            return
+        if lci is None:
+            return rci
+        if rci is None:
+            return lci
+        min_child_index = lci
+        if self.heap[rci] < self.heap[min_child_index]:
+            min_child_index = rci
+        return min_child_index
+
+    def heapify_up(self, start_index):
+        if start_index == 0:
+            return
+        pi = self.get_pi(start_index)
+        lci, rci = self.get_lci(pi), self.get_rci(pi)
+        min_child_index = self.get_min_child_index(lci, rci)
+        if min_child_index is not None:
+            if self.heap[pi] > self.heap[min_child_index]:
+                Utility.swap(self.heap, pi, min_child_index)
+            self.heapify_up(pi)
+
+    def heapify_down(self, pi):
+        lci, rci = self.get_lci(pi), self.get_rci(pi)
+        min_child_index = self.get_min_child_index(lci, rci)
+        if min_child_index is not None:
+            if self.heap[pi] > self.heap[min_child_index]:
+                Utility.swap(self.heap, pi, min_child_index)
+            self.heapify_down(min_child_index)
+
+    def insert(self, x):
+        self.heap.append(x)
+        self.heapify_up(len(self.heap) - 1)
+
+    def pop(self):
+        if self.is_empty():
+            return
+        item = self.heap[0]
+        Utility.swap(self.heap, 0, -1)
+        del self.heap[-1]
+        self.heapify_down(0)
+        return item
