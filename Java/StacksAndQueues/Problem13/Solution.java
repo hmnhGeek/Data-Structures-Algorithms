73a53627af15770e3c29472e25c66e8e0c87adb5
hmnhGeek <hmnhsharma97@gmail.com>
2026-03-16 12:35:42 +0530

    insert at bottom of stack

---
+++ Java/StacksAndQueues/Problem13/Solution.java
@@ -0,0 +1,16 @@
+package StacksAndQueues.Problem13;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        for (Integer i : Arrays.asList(1, 2, 3, 4)) {
+            stack.push(i);
+        }
+        stack.insertAtBottom(10);
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
+}
