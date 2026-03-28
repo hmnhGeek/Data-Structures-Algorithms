b283c2780b80b8cd7755bc1bf8e2a1ed0c65694d
hmnhGeek <hmnhsharma97@gmail.com>
2026-03-28 12:50:16 +0530

    Memoized LCS

--- Python/Practice Set 4/Dynamic Programming/DP26 - Print Longest Common Subsequence.py
+++ Python/Practice Set 4/Dynamic Programming/DP26 - Print Longest Common Subsequence.py
@@ -21,3 +21,34 @@ def recursive():
     print(get_lcs("abc", "acd"))
     print(get_lcs("AGGTAB", "GXTXAYB"))
     print(get_lcs("ABC", "CBA"))
+
+
+def memoized():
+    def get_lcs(s1, s2):
+        n1, n2 = len(s1), len(s2)
+        dp = {i: {j: None for j in range(n2 + 1)} for i in range(n1 + 1)}
+        return solve(s1, n1, s2, n2, dp)
+
+    def solve(s1, i, s2, j, dp):
+        if i == 0 or j == 0:
+            return 0
+        if dp[i][j] is not None:
+            return dp[i][j]
+        if s1[i - 1] == s2[j - 1]:
+            dp[i][j] = 1 + solve(s1, i - 1, s2, j - 1, dp)
+        else:
+            dp[i][j] = max(
+                solve(s1, i - 1, s2, j, dp),
+                solve(s1, i, s2, j - 1, dp)
+            )
+        return dp[i][j]
+
+    print(get_lcs("adebc", "dcadb"))
+    print(get_lcs("ab", "defg"))
+    print(get_lcs("abcde", "ace"))
+    print(get_lcs("abc", "abc"))
+    print(get_lcs("abc", "acd"))
+    print(get_lcs("AGGTAB", "GXTXAYB"))
+    print(get_lcs("ABC", "CBA"))
+
+memoized()
