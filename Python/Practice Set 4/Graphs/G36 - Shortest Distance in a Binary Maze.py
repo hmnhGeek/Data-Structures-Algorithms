1de22856895f74759588e5cde1feb89fa4dc667b
hmnhGeek <hmnhsharma97@gmail.com>
2026-03-29 07:26:40 +0530

    Shortest distance in binary maze

--- Python/Practice Set 4/Graphs/G36 - Shortest Distance in a Binary Maze.py
+++ Python/Practice Set 4/Graphs/G36 - Shortest Distance in a Binary Maze.py
@@ -31,3 +31,112 @@ class Queue:
         self.length -= 1
         return item
 
+
+class Solution:
+    @staticmethod
+    def shortest_distance(graph, source, destination):
+        n = len(graph)
+        m = len(graph[0])
+        distances = [[1e6 for _ in range(m)] for _ in range(n)]
+        distances[source[0]][source[1]] = 0
+        queue = Queue()
+        queue.push((*source, 0))
+        while not queue.is_empty():
+            x, y, d = queue.pop()
+            neighbours = Solution._get_neighbours(graph, x, y, n, m)
+            for neighbour in neighbours:
+                if distances[neighbour[0]][neighbour[1]] > 1 + d:
+                    distances[neighbour[0]][neighbour[1]] = 1 + d
+                    queue.push((*neighbour, 1 + d))
+        return distances[destination[0]][destination[1]]
+
+    @staticmethod
+    def _get_neighbours(graph, x, y, n, m):
+        neighbours = []
+        if 0 <= x + 1 < n and graph[x + 1][y] == 1:
+            neighbours.append((x + 1, y))
+        if 0 <= y - 1 < m and graph[x][y - 1] == 1:
+            neighbours.append((x, y - 1))
+        if 0 <= x - 1 < n and graph[x - 1][y] == 1:
+            neighbours.append((x - 1, y))
+        if 0 <= y + 1 < m and graph[x][y + 1] == 1:
+            neighbours.append((x, y + 1))
+        return neighbours
+
+
+print(
+    Solution.shortest_distance(
+        [
+            [1, 1, 1, 1],
+            [1, 1, 0, 1],
+            [1, 1, 1, 1],
+            [1, 1, 0, 0],
+            [1, 0, 0, 0]
+        ],
+        (0, 1),
+        (2, 2)
+    )
+)
+
+print(
+    Solution.shortest_distance(
+        [
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 0],
+            [1, 0, 1, 0, 1]
+        ],
+        (0, 0),
+        (3, 4)
+    )
+)
+
+print(
+    Solution.shortest_distance(
+        [[1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+         [1, 0, 1, 0, 1, 1, 1, 0, 1, 1],
+         [1, 1, 1, 0, 1, 1, 0, 1, 0, 1],
+         [0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+         [1, 1, 1, 0, 1, 1, 1, 0, 1, 0],
+         [1, 0, 1, 1, 1, 1, 0, 1, 0, 0],
+         [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+         [1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+         [1, 1, 0, 0, 0, 0, 1, 0, 0, 1]],
+        (0, 0),
+        (3, 4)
+    )
+)
+
+print(
+    Solution.shortest_distance(
+        [
+            [1, 1, 1, 1],
+            [0, 1, 1, 0],
+            [0, 0, 1, 1]
+        ],
+        (0, 0),
+        (2, 3)
+    )
+)
+
+print(
+    Solution.shortest_distance(
+        [
+            [1, 1],
+            [0, 1]
+        ],
+        (0, 0),
+        (1, 1)
+    )
+)
+
+print(
+    Solution.shortest_distance(
+        [
+            [1, 0],
+            [0, 1]
+        ],
+        (0, 0),
+        (1, 1)
+    )
+)
