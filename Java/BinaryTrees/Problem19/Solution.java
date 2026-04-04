1ab62b64035a283cc859d17acb150259c7f1d806
hmnhGeek <hmnhsharma97@gmail.com>
2026-04-04 11:31:39 +0530

    Convert a BT to Sum Tree

--- Java/BinaryTrees/Problem19/Solution.java
+++ Java/BinaryTrees/Problem19/Solution.java
@@ -58,5 +58,58 @@ public class Solution {
         // Print transformed tree
         System.out.println("Sum Tree:");
         sol.printTree(n10);
+
+        System.out.println();
+        System.out.println();
+
+        // Create nodes
+        n5 = new Node<>(5);
+        Node<Integer> n1 = new Node<>(1);
+        Node<Integer> n_8 = new Node<>(-8);
+        Node<Integer> n4 = new Node<>(4);
+        n6 = new Node<>(6);
+
+        // Connect nodes
+        n5.right = n1;
+
+        n1.left = n_8;
+        n1.right = n4;
+
+        n_8.left = n6;
+
+        // Print original tree
+        System.out.println("Original Tree:");
+        sol.printTree(n5);
+
+        // Convert to sum tree
+        sol.convertToSumTree(n5);
+
+        // Print sum tree
+        System.out.println("Sum Tree:");
+        sol.printTree(n5);
+
+        System.out.println();
+        System.out.println();
+
+        // Create nodes
+        Node<Integer> n1_root = new Node<>(1);
+        n_2 = new Node<>(-2);
+        Node<Integer> n1_right = new Node<>(1);
+        Node<Integer> n3 = new Node<>(3);
+        Node<Integer> n1_leaf = new Node<>(1);
+
+        // Connect nodes
+        n1_root.left = n_2;
+        n1_root.right = n1_right;
+
+        n_2.left = n3;
+        n_2.right = n1_leaf;
+
+        // Print original tree
+        System.out.println("Original Tree:");
+        sol.printTree(n1_root);
+        sol.convertToSumTree(n1_root);
+        System.out.println("Sum Tree:");
+        sol.printTree(n1_root);
     }
 }
