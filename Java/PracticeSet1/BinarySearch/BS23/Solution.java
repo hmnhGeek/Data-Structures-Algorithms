0d94ce333d295e80085fca42a486743ed89e3ba9
hmnhGeek <hmnhsharma97@gmail.com>
2026-04-10 09:54:01 +0530

    Row with maximum ones

---
+++ Java/PracticeSet1/BinarySearch/BS23/Solution.java
@@ -0,0 +1,73 @@
+package PracticeSet1.BinarySearch.BS23;
+
+import java.util.List;
+
+public class Solution {
+    public static Integer getRowWithMaximumOnes(List<List<Integer>> matrix) {
+        int n = matrix.size(), m = matrix.getFirst().size();
+        Integer maxOnes = 0, rowIndex = null;
+        for (int i = 0; i < n; i += 1) {
+            List<Integer> row = matrix.get(i);
+            Integer ones = Utils.getOnesCount(row);
+            if (ones > maxOnes) {
+                maxOnes = ones;
+                rowIndex = i;
+            }
+        }
+        return rowIndex;
+    }
+
+    public static void main(String[] args) {
+
+        List<List<Integer>> matrix1 = List.of(
+                List.of(0, 0, 1, 1, 1),
+                List.of(0, 0, 0, 0, 0),
+                List.of(0, 1, 1, 1, 1),
+                List.of(0, 0, 0, 0, 0),
+                List.of(0, 1, 1, 1, 1)
+        );
+        System.out.println(Solution.getRowWithMaximumOnes(matrix1));
+
+        List<List<Integer>> matrix2 = List.of(
+                List.of(1, 1, 1),
+                List.of(0, 0, 1),
+                List.of(0, 0, 0)
+        );
+        System.out.println(Solution.getRowWithMaximumOnes(matrix2));
+
+        List<List<Integer>> matrix3 = List.of(
+                List.of(1, 1),
+                List.of(1, 1)
+        );
+        System.out.println(Solution.getRowWithMaximumOnes(matrix3));
+
+        List<List<Integer>> matrix4 = List.of(
+                List.of(0, 0, 0),
+                List.of(0, 1, 1)
+        );
+        System.out.println(Solution.getRowWithMaximumOnes(matrix4));
+
+        List<List<Integer>> matrix5 = List.of(
+                List.of(0, 0),
+                List.of(1, 1),
+                List.of(0, 0)
+        );
+        System.out.println(Solution.getRowWithMaximumOnes(matrix5));
+
+        List<List<Integer>> matrix6 = List.of(
+                List.of(0, 1, 1, 1),
+                List.of(0, 0, 1, 1),
+                List.of(1, 1, 1, 1),
+                List.of(0, 0, 0, 0)
+        );
+        System.out.println(Solution.getRowWithMaximumOnes(matrix6));
+
+        List<List<Integer>> matrix7 = List.of(
+                List.of(0, 0, 1, 1),
+                List.of(0, 1, 1, 1),
+                List.of(0, 0, 1, 1),
+                List.of(0, 0, 0, 0)
+        );
+        System.out.println(Solution.getRowWithMaximumOnes(matrix7));
+    }
+}
