78d130374a1cf17df1f2b7cf9d7f0fafdbcc41f8
hmnhGeek <hmnhsharma97@gmail.com>
2026-04-11 12:13:44 +0530

    Added a doubly linked list class

---
+++ Java/LinkedLists/Problem22/DoublyLinkedList.java
@@ -0,0 +1,35 @@
+package LinkedLists.Problem22;
+
+
+class Node<T> {
+    public T data;
+    public Node<T> prev, next;
+
+    public Node(T data) {
+        this.data = data;
+        this.prev = this.next = null;
+    }
+}
+
+
+public class DoublyLinkedList<T> {
+    public Node<T> head, tail;
+    public Integer length;
+
+    public DoublyLinkedList() {
+        this.head = this.tail = null;
+        this.length = 0;
+    }
+
+    public void push(T x) {
+        Node<T> node = new Node<>(x);
+        if (this.length == 0) {
+            this.head = this.tail = node;
+        } else {
+            this.tail.next = node;
+            node.prev = this.tail;
+            this.tail = node;
+        }
+        this.length += 1;
+    }
+}
