67adb041a47698f62c36762cb07909dab41de707
hmnhGeek <hmnhsharma97@gmail.com>
2026-04-11 12:51:00 +0530

    Balanced paranthesis

---
+++ Java/Strings/Problem16/Solution.java
@@ -0,0 +1,38 @@
+package Strings.Problem16;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    private static final Map<Character, Character> bracketMap = Map.of(
+            ']', '[',
+            '}', '{',
+            ')', '('
+    );
+
+    public static boolean isBalanced(String string) {
+        Stack<Character> stack = new Stack<>();
+        for (Character bracket : string.toCharArray()) {
+            if (bracket == '(' || bracket == '[' || bracket == '{') {
+                stack.push(bracket);
+            } else {
+                // got a closing bracket but stack is empty.
+                if (stack.isEmpty()) return false;
+                if (stack.top() != bracketMap.get(bracket)) return false;
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Solution.isBalanced("{([])}"));          // true
+        System.out.println(Solution.isBalanced("([]"));             // false
+        System.out.println(Solution.isBalanced("()"));              // true
+        System.out.println(Solution.isBalanced("[()]{}{[()()]()}"));// true
+        System.out.println(Solution.isBalanced("[(])"));            // false
+        System.out.println(Solution.isBalanced("()[]{}"));          // true
+        System.out.println(Solution.isBalanced("(]"));              // false
+        System.out.println(Solution.isBalanced("{[(])}"));          // false
+    }
+}
