4fad9bea242b5c828004549711b2cbf17b6d747b
hmnhGeek <hmnhsharma97@gmail.com>
2026-04-18 07:36:29 +0530

    Implemented a doubly linked list

---
+++ Python/Practice Set 1/Linked Lists/Count triplets in a sorted doubly linked list whose sum is equal to a given value x.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = self.next = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = self.tail = None
+        self.length = 0
+
+    def is_empty(self):
+        return self.length == 0
+
+    def push(self, x):
+        node = Node(x)
+        if self.is_empty():
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = node
+        self.length += 1
+
+    def build(self, *args):
+        for i in args:
+            self.push(i)
+
