4f6df87f10922c6dc84c078d6ce151d08d00b0af
hmnhGeek <hmnhsharma97@gmail.com>
2026-04-19 18:39:51 +0530

    Longest common subsequence recursive

---
+++ Python/Practice Set 4/Dynamic Programming/DP28 - Longest Palindromic Subsequence.py
@@ -0,0 +1,24 @@
+def recursive():
+    def lcs(s1, s2):
+        n = len(s1)
+        m = len(s2)
+        return solve(s1, n, s2, m)
+
+    def solve(s1, i, s2, j):
+        if i == 0 or j == 0:
+            return 0
+        if s1[i - 1] == s2[j - 1]:
+            return 1 + solve(s1, i - 1, s2, j - 1)
+        else:
+            return max(
+                solve(s1, i - 1, s2, j),
+                solve(s1, i, s2, j - 1)
+            )
+
+    print(lcs("adebc", "dcadb"))
+    print(lcs("ab", "defg"))
+    print(lcs("abcde", "ace"))
+    print(lcs("abc", "abc"))
+    print(lcs("abc", "acd"))
+    print(lcs("AGGTAB", "GXTXAYB"))
+    print(lcs("ABC", "CBA"))
