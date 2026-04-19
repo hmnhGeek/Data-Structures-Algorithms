d9da70b626ee9f9122d59b5d1e3091951a17b4a6
hmnhGeek <hmnhsharma97@gmail.com>
2026-04-19 18:56:22 +0530

    Longest palindromic subsequence

--- Python/Practice Set 4/Dynamic Programming/DP28 - Longest Palindromic Subsequence.py
+++ Python/Practice Set 4/Dynamic Programming/DP28 - Longest Palindromic Subsequence.py
@@ -108,30 +108,38 @@ def space_optimized():
 
 class Solution:
     @staticmethod
-    def lcs(s1, s2):
+    def lcs2(s1, s2):
         n = len(s1)
         m = len(s2)
-        prev = {j: 0 for j in range(m + 1)}
+        dp = {i: {j: 0 for j in range(m + 1)} for i in range(n + 1)}
         for i in range(1, n + 1):
-            curr = {j: 0 for j in range(m + 1)}
             for j in range(1, m + 1):
                 if s1[i - 1] == s2[j - 1]:
-                    curr[j] = 1 + prev[j - 1]
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
                 else:
-                    curr[j] = max(
-                        prev[j],
-                        curr[j - 1]
+                    dp[i][j] = max(
+                        dp[i - 1][j],
+                        dp[i][j - 1]
                     )
-            prev = curr
-        return prev[m]
+        return dp
 
     @staticmethod
     def longest_palindromic_subsequence(string):
         reversed = string[-1:-len(string)-1:-1]
-        return Solution.lcs(string, reversed)
+        dp = Solution.lcs2(string, reversed)
+        i, j = len(string), len(string)
+        result = ""
+        while i > 0 and j > 0:
+            if string[i - 1] == reversed[j - 1]:
+                result += string[i - 1]
+                i -= 1
+                j -= 1
+            elif dp[i - 1][j] > dp[i][j - 1]:
+                i -= 1
+            else:
+                j -= 1
+        return result
 
 
 print(Solution.longest_palindromic_subsequence("bbbab"))
 print(Solution.longest_palindromic_subsequence("bbabcbcab"))
-# print(Solution.lps_string("bbbab"))
-# print(Solution.lps_string("bbabcbcab"))
