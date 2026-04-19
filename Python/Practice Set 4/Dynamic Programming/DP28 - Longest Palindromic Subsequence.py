2bf0bf1f058ee29d490525a0b948a51e61f6e1f7
hmnhGeek <hmnhsharma97@gmail.com>
2026-04-19 18:42:48 +0530

    Longest common subsequence tabulation

--- Python/Practice Set 4/Dynamic Programming/DP28 - Longest Palindromic Subsequence.py
+++ Python/Practice Set 4/Dynamic Programming/DP28 - Longest Palindromic Subsequence.py
@@ -53,3 +53,28 @@ def memoized():
     print(lcs("AGGTAB", "GXTXAYB"))
     print(lcs("ABC", "CBA"))
 
+
+def tabulation():
+    def lcs(s1, s2):
+        n = len(s1)
+        m = len(s2)
+        dp = {i: {j: 0 for j in range(m + 1)} for i in range(n + 1)}
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(
+                        dp[i - 1][j],
+                        dp[i][j - 1]
+                    )
+        return dp[n][m]
+
+    print(lcs("adebc", "dcadb"))
+    print(lcs("ab", "defg"))
+    print(lcs("abcde", "ace"))
+    print(lcs("abc", "abc"))
+    print(lcs("abc", "acd"))
+    print(lcs("AGGTAB", "GXTXAYB"))
+    print(lcs("ABC", "CBA"))
+
