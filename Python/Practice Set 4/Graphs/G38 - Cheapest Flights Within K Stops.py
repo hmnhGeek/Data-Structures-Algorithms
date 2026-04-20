90df8c09b2fa8c0894428b833cc9ccd275dd5eec
hmnhGeek <hmnhsharma97@gmail.com>
2026-04-20 12:41:25 +0530

    Cheapest flights within k stops

--- Python/Practice Set 4/Graphs/G38 - Cheapest Flights Within K Stops.py
+++ Python/Practice Set 4/Graphs/G38 - Cheapest Flights Within K Stops.py
@@ -30,3 +30,73 @@ class Queue:
         del node
         self.length -= 1
         return item
+
+
+class Solution:
+    @staticmethod
+    def get_cheapest_flight(graph, source, destination, k):
+        n = len(graph)
+        if source not in range(n) or destination not in range(n):
+            return
+        distances = [1e6 for _ in range(n)]
+        distances[source] = 0
+        queue = Queue()
+        queue.push((source, 0, 0))
+        while not queue.is_empty():
+            node, stops_taken, distance = queue.pop()
+            if node == destination:
+                continue
+            if stops_taken > k:
+                continue
+            for adj in graph[node]:
+                adj_node, wt = adj
+                if wt + distance < distances[adj_node]:
+                    if adj_node == destination:
+                        queue.push((adj_node, stops_taken, wt + distance))
+                    else:
+                        queue.push((adj_node, stops_taken + 1, wt + distance))
+                    distances[adj_node] = wt + distance
+        return distances[destination]
+
+
+print(
+    Solution.get_cheapest_flight(
+        {
+            0: [[1, 5], [3, 2]],
+            1: [[2, 5], [4, 1]],
+            2: [],
+            3: [[1, 2]],
+            4: [[2, 1]]
+        },
+        0,
+        2,
+        2
+    )
+)
+
+print(
+    Solution.get_cheapest_flight(
+        {
+            0: [[1, 100]],
+            1: [[2, 100], [3, 600]],
+            2: [[0, 100], [3, 200]],
+            3: []
+        },
+        0,
+        3,
+        1
+    )
+)
+
+print(
+    Solution.get_cheapest_flight(
+        {
+            0: [[1, 100], [2, 500]],
+            1: [[2, 100]],
+            2: []
+        },
+        0,
+        2,
+        0
+    )
+)
