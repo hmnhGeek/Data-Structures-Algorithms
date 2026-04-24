491d42d5ec365e2d34bb277303142ee8d9e687cc
hmnhGeek <hmnhsharma97@gmail.com>
2026-04-24 16:39:56 +0530

    Row with maximum ones

---
+++ Python/Practice Set 4/Binary Search/BS23 - Row with maximum number of 1s.py
@@ -0,0 +1,76 @@
+class Solution:
+    @staticmethod
+    def get_count_of_ones(row):
+        n =  len(row)
+        low, high = 0, n - 1
+        while low <= high:
+            mid = int(low + (high - low)/2)
+            if row[mid] == 1:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return n - low
+
+    @staticmethod
+    def get_row_with_maximum_1s(mtx):
+        n = len(mtx)
+        row_idx = -1
+        max_ones = 0
+        for i in range(n):
+            ones_count = Solution.get_count_of_ones(mtx[i])
+            if ones_count > max_ones:
+                max_ones = ones_count
+                row_idx = i
+        return row_idx
+
+
+matrix1 = [
+    [0, 0, 1, 1, 1],
+    [0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1]
+]
+print(Solution.get_row_with_maximum_1s(matrix1))
+
+matrix2 = [
+    [1, 1, 1],
+    [0, 0, 1],
+    [0, 0, 0]
+]
+print(Solution.get_row_with_maximum_1s(matrix2))
+
+matrix3 = [
+    [1, 1],
+    [1, 1]
+]
+print(Solution.get_row_with_maximum_1s(matrix3))
+
+matrix4 = [
+    [0, 0, 0],
+    [0, 1, 1]
+]
+print(Solution.get_row_with_maximum_1s(matrix4))
+
+matrix5 = [
+    [0, 0],
+    [1, 1],
+    [0, 0]
+]
+print(Solution.get_row_with_maximum_1s(matrix5))
+
+matrix6 = [
+    [0, 1, 1, 1],
+    [0, 0, 1, 1],
+    [1, 1, 1, 1],
+    [0, 0, 0, 0]
+]
+print(Solution.get_row_with_maximum_1s(matrix6))
+
+matrix7 = [
+    [0, 0, 1, 1],
+    [0, 1, 1, 1],
+    [0, 0, 1, 1],
+    [0, 0, 0, 0]
+]
+print(Solution.get_row_with_maximum_1s(matrix7))
