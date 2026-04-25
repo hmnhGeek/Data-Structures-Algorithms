8f1f53a7fef405ab522e81cf124aa9631329c1b8
hmnhGeek <hmnhsharma97@gmail.com>
2026-04-25 17:22:10 +0530

    Implemented a doubly linked list

---
+++ Java/LinkedLists/Problem23/DoublyLinkedList.java
@@ -0,0 +1,57 @@
+package LinkedLists.Problem23;
+
+
+class Node<T extends Comparable<T>> implements Comparable<Node<T>> {
+    public T data;
+    public Node<T> prev, next;
+
+    public Node(T data) {
+        this.data = data;
+        this.prev = this.next = null;
+    }
+
+    @Override
+    public int compareTo(Node<T> o) {
+        return this.data.compareTo(o.data);
+    }
+}
+
+
+public class DoublyLinkedList<T extends Comparable<T>> {
+    public Node<T> head, tail;
+    public Integer length;
+
+    public DoublyLinkedList() {
+        this.head = this.tail = null;
+        this.length = 0;
+    }
+
+    public boolean isEmpty() {
+        return this.length.equals(0);
+    }
+
+    public void push(T x) {
+        Node<T> node = new Node<>(x);
+        if (isEmpty()) {
+            this.head = this.tail = node;
+        } else {
+            node.prev = this.tail;
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.length += 1;
+    }
+
+    @Override
+    public String toString() {
+        if (isEmpty()) return "[]";
+        Node<T> curr = this.head;
+        StringBuilder result = new StringBuilder("[");
+        while (curr != this.tail) {
+            result.append(String.format("%s, ", curr.data));
+            curr = curr.next;
+        }
+        result.append(String.format("%s]", this.tail.data));
+        return result.toString();
+    }
+}
