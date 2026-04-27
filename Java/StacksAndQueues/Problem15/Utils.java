ccac3d9fafaad93019c7e7bae6669a0c3687b870
hmnhGeek <hmnhsharma97@gmail.com>
2026-04-27 15:32:47 +0530

    Merge sort on linked list

--- Java/StacksAndQueues/Problem15/Utils.java
+++ Java/StacksAndQueues/Problem15/Utils.java
@@ -10,4 +10,62 @@ public class Utils {
         }
         return slow;
     }
+
+    public static <T extends Comparable<T>> Stack<T> sort(Stack<T> stack) {
+        if (stack.head == null || stack.head.next == null) return stack;
+        Node<T> middleNode = getMiddleNode(stack.head);
+        Stack<T> leftStack = new Stack<>();
+        leftStack.head = stack.head;
+        leftStack.tail = middleNode;
+        Stack<T> rightStack = new Stack<>();
+        rightStack.head = middleNode.next;
+        rightStack.tail = stack.tail;
+        middleNode.next = null;
+        if (stack.length % 2 == 1) {
+            leftStack.length = (stack.length / 2) + 1;
+        } else {
+            leftStack.length = stack.length / 2;
+        }
+        rightStack.length = stack.length / 2;
+        Stack<T> leftStackSorted = sort(leftStack);
+        Stack<T> rightStackSorted = sort(rightStack);
+        return merge(leftStackSorted, rightStackSorted);
+    }
+
+    private static <T extends Comparable<T>> Stack<T> merge(Stack<T> s1, Stack<T> s2) {
+        Node<T> dummy = new Node<>(null);
+        Node<T> temp = dummy;
+        Stack<T> resultantStack = new Stack<>();
+        resultantStack.length = s1.length + s2.length;
+        Node<T> i = s1.head, j = s2.head;
+        while (i != null && j != null) {
+            if (i.data.compareTo(j.data) <= 0) {
+                temp.next = i;
+                i = i.next;
+            } else {
+                temp.next = j;
+                j = j.next;
+            }
+            temp = temp.next;
+            temp.next = null;
+        }
+
+        while (i != null) {
+            temp.next = i;
+            i = i.next;
+            temp = temp.next;
+            temp.next = null;
+        }
+
+        while (j != null) {
+            temp.next = j;
+            j = j.next;
+            temp = temp.next;
+            temp.next = null;
+        }
+
+        resultantStack.head = dummy.next;
+        resultantStack.tail = temp;
+        return resultantStack;
+    }
 }
