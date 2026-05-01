e59da5bb8bd66c886f71937ce20d38f8d0271ca1
hmnhGeek <hmnhsharma97@gmail.com>
2026-05-01 13:36:51 +0530

    Space optimized LCS length

--- Java/DynamicProgramming/DP30/Solution.java
+++ Java/DynamicProgramming/DP30/Solution.java
@@ -9,6 +9,8 @@ public class Solution {
         System.out.println();
         tabulation();
         System.out.println();
+        spaceOptimized();
+        System.out.println();
     }
 
     private static void recursive() {
@@ -39,5 +41,15 @@ public class Solution {
         System.out.println(TabulationSolution.getLcsLength("abc", "acd"));
         System.out.println(TabulationSolution.getLcsLength("AGGTAB", "GXTXAYB"));
         System.out.println(TabulationSolution.getLcsLength("ABC", "CBA"));
+    }
+
+    private static void spaceOptimized() {
+        System.out.println(SpaceOptimizedSolution.getLcsLength("adebc", "dcadb"));
+        System.out.println(SpaceOptimizedSolution.getLcsLength("ab", "defg"));
+        System.out.println(SpaceOptimizedSolution.getLcsLength("abcde", "ace"));
+        System.out.println(SpaceOptimizedSolution.getLcsLength("abc", "abc"));
+        System.out.println(SpaceOptimizedSolution.getLcsLength("abc", "acd"));
+        System.out.println(SpaceOptimizedSolution.getLcsLength("AGGTAB", "GXTXAYB"));
+        System.out.println(SpaceOptimizedSolution.getLcsLength("ABC", "CBA"));
     }
 }
---
+++ Java/DynamicProgramming/DP30/SpaceOptimizedSolution.java
@@ -0,0 +1,33 @@
+package DynamicProgramming.DP30;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SpaceOptimizedSolution {
+    public static Integer getLcsLength(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+        Map<Integer, Integer> prev = new HashMap<>();
+        for (int j = 0; j <= m; j += 1) {
+            prev.put(j, 0);
+        }
+        for (int i = 1; i <= n; i += 1) {
+            Map<Integer, Integer> curr = new HashMap<>();
+            for (int j = 0; j <= m; j += 1) {
+                curr.put(j, 0);
+            }
+            for (int j = 1; j <= m; j += 1) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    curr.put(j, 1 + prev.get(j - 1));
+                }
+                else {
+                    curr.put(j, Math.max(
+                            prev.get(j),
+                            curr.get(j - 1)
+                    ));
+                }
+            }
+            prev = curr;
+        }
+        return prev.get(m);
+    }
+}
