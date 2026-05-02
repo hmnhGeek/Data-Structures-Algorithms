9ba6f1639f60ec0046309e657d91a9991a678221
hmnhGeek <hmnhsharma97@gmail.com>
2026-05-02 15:52:06 +0530

    Space optimized LCS

--- Java/DynamicProgramming/DP31/SpaceOptimizedSolution.java
+++ Java/DynamicProgramming/DP31/SpaceOptimizedSolution.java
@@ -3,34 +3,35 @@ package DynamicProgramming.DP31;
 import java.util.HashMap;
 import java.util.Map;
 
-public class TabulationSolution {
+public class SpaceOptimizedSolution {
     /*
-        Time complexity is O(nm) and space complexity is O(nm).
+        Time complexity is O(nm) and space complexity is O(m).
      */
     public static Integer getLcsLength(String s1, String s2) {
         int n = s1.length(), m = s2.length();
-        Map<Integer, Map<Integer, Integer>> dp = new HashMap<>();
-        for (int i = 0; i <= n; i += 1) {
-            Map<Integer, Integer> prev = new HashMap<>();
-            for (int j = 0; j <= m; j += 1) {
-                prev.put(j, 0);
-            }
-            dp.put(i, prev);
+        Map<Integer, Integer> prev = new HashMap<>();
+        for (int j = 0; j <= m; j += 1) {
+            prev.put(j, 0);
         }
         for (int i = 1; i <= n; i += 1) {
+            Map<Integer, Integer> curr = new HashMap<>();
+            for (int j = 0; j <= m; j += 1) {
+                curr.put(j, 0);
+            }
             for (int j = 1; j <= m; j += 1) {
                 if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    dp.get(i).put(j, 1 + dp.get(i - 1).get(j - 1));
+                    curr.put(j, 1 + prev.get(j - 1));
                 }
                 else {
-                    dp.get(i).put(j, Math.max(
-                            dp.get(i - 1).get(j),
-                            dp.get(i).get(j - 1)
+                    curr.put(j, Math.max(
+                            prev.get(j),
+                            curr.get(j - 1)
                     ));
                 }
             }
+            prev = curr;
         }
-        return dp.get(n).get(m);
+        return prev.get(m);
     }
 
     public static void main(String[] args) {
