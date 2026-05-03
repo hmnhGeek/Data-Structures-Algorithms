45fe8a494cd2131da3f4bdf9300a45c6788a7137
hmnhGeek <hmnhsharma97@gmail.com>
2026-05-03 18:44:19 +0530

    Search in 2D Matrix II

---
+++ Java/PracticeSet1/BinarySearch/BS25/Solution.java
@@ -0,0 +1,86 @@
+package PracticeSet1.BinarySearch.BS25;
+
+import java.util.List;
+
+public class Solution {
+    public static List<Integer> search(List<List<Integer>> mtx, Integer target) {
+        int n = mtx.size(), m = mtx.getFirst().size();
+        int row = 0, col = m - 1;
+        while (row < n && col >= 0) {
+            Integer elem = mtx.get(row).get(col);
+            if (elem == target) {
+                return List.of(row, col);
+            }
+            if (elem > target) {
+                col -= 1;
+            } else {
+                row += 1;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                Solution.search(
+                        List.of(
+                                List.of(1, 4, 7, 11, 15),
+                                List.of(2, 5, 8, 12, 19),
+                                List.of(3, 6, 9, 16, 22),
+                                List.of(10, 13, 14, 17, 24),
+                                List.of(18, 21, 23, 26, 30)
+                        ),
+                        14
+                )
+        );
+
+        System.out.println(
+                Solution.search(
+                        List.of(
+                                List.of(1, 4, 7, 11, 15),
+                                List.of(2, 5, 8, 12, 19),
+                                List.of(3, 6, 9, 16, 22),
+                                List.of(10, 13, 14, 17, 24),
+                                List.of(18, 21, 23, 26, 30)
+                        ),
+                        5
+                )
+        );
+
+        System.out.println(
+                Solution.search(
+                        List.of(
+                                List.of(1, 4, 7, 11, 15),
+                                List.of(2, 5, 8, 12, 19),
+                                List.of(3, 6, 9, 16, 22),
+                                List.of(10, 13, 14, 17, 24),
+                                List.of(18, 21, 23, 26, 30)
+                        ),
+                        20
+                )
+        );
+
+        System.out.println(
+                Solution.search(
+                        List.of(
+                                List.of(1, 2, 4, 5),
+                                List.of(3, 4, 9, 16),
+                                List.of(7, 10, 14, 29)
+                        ),
+                        8
+                )
+        );
+
+        System.out.println(
+                Solution.search(
+                        List.of(
+                                List.of(1, 2, 3),
+                                List.of(4, 5, 6),
+                                List.of(7, 8, 9)
+                        ),
+                        5
+                )
+        );
+    }
+}
