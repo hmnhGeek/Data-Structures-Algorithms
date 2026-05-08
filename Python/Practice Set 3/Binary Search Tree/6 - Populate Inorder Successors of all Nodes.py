45e5322c586c037d490a450d6d546e57f7ff4434
hmnhGeek <hmnhsharma97@gmail.com>
2026-05-08 17:22:00 +0530

    Insert logic in BST

---
+++ Python/Practice Set 3/Binary Search Tree/6 - Populate Inorder Successors of all Nodes.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = self.right = self.parent = None
+        self.ht = self.size = self.d = 1
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+        self.d = 0
+
+    def insert(self, x):
+        node = Node(x)
+        if self.root is None:
+            self.root = node
+            self.d = 1
+            return
+        self._insert(self.root, node)
+
+    def _insert(self, start, node):
+        if start is None or node is None:
+            return
+        if node.data >= start.data:
+            if start.right is not None:
+                self._insert(start.right, node)
+                return
+            start.right = node
+            node.parent = start
+            self.recalc_augmentation(start)
+            return
+        if start.left is not None:
+            self._insert(start.left, node)
+            return
+        start.left = node
+        node.parent = start
+        self.recalc_augmentation(start)
+        return
+
+    def recalc_augmentation(self, parent):
+        self.d = 0
+        while parent is not None:
+            left_sz = parent.left.size if parent.left is not None else 0
+            left_ht = parent.left.ht if parent.left is not None else 0
+            right_sz = parent.right.size if parent.right is not None else 0
+            right_ht = parent.right.ht if parent.right is not None else 0
+            parent.size = 1 + left_sz + right_sz
+            parent.ht = 1 + max(left_ht, right_ht)
+            parent.d = 1 + left_ht + right_ht
+            self.d = max(self.d, parent.d)
+            parent = parent.parent
