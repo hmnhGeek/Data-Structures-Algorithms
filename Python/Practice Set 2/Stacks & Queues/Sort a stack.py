57ddaa573044239879cafaa507f5b919b9239e8d
hmnhGeek <hmnhsharma97@gmail.com>
2026-05-10 15:58:22 +0530

    Shallow logic for merge sort

--- Python/Practice Set 2/Stacks & Queues/Sort a stack.py
+++ Python/Practice Set 2/Stacks & Queues/Sort a stack.py
@@ -31,3 +31,27 @@ class Stack:
         self.length -= 1
         return item
 
+
+class Solution:
+    @staticmethod
+    def sort(stack: Stack):
+        Solution._sort(stack.head)
+
+    @staticmethod
+    def _sort(stack: Stack):
+        if stack.head is None or stack.head.next is None:
+            return stack
+        middle_node = Solution._get_middle_node(stack)
+        second_head = middle_node.next
+        middle_node.next = None
+        first = Stack()
+        first.head = stack.head
+        first.tail = middle_node
+        first.length = stack.length // 2 if stack.length % 2 == 0 else stack.length // 2 + 1
+        second = Stack()
+        second.head = second_head
+        second.tail = stack.tail
+        second.length = stack.length // 2
+        first = Solution._sort(first)
+        second = Solution._sort(second)
+        return Solution._merge(first, second)
