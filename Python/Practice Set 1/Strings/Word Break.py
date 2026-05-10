1d792030c204e8dab83dd3bfc4b1119ad604ee96
hmnhGeek <hmnhsharma97@gmail.com>
2026-05-10 15:05:28 +0530

    Word break tabulation

--- Python/Practice Set 1/Strings/Word Break.py
+++ Python/Practice Set 1/Strings/Word Break.py
@@ -56,7 +56,34 @@ def memoized():
     print(word_break("applepenapple", ["apple", "pen"]))
 
 
+def tabulation():
+    """
+        Time complexity is O(n^2) and space complexity is O(n).
+    """
+    def word_break(string, dictionary):
+        n = len(string)
+        dp = {i: False for i in range(n + 1)}
+        dp[n] = True
+        for i in range(n - 1, -1, -1):
+            temp = ""
+            for j in range(i, n):
+                temp += string[j]
+                if temp in dictionary and dp[j + 1]:
+                    dp[i] = True
+                    break
+        return dp[0]
+
+    print(word_break("ilike", ["i", "like", "sam", "sung", "samsung", "mobile"]))
+    print(word_break("ilikesamsung", ["i", "like", "sam", "sung", "samsung", "mobile"]))
+    print(word_break("catsandog", ["cats", "dog", "sand", "and", "cat"]))
+    print(word_break("storybook", ["story", "book"]))
+    print(word_break("monkeyandonkey", ["monkey", "donkey", "and"]))
+    print(word_break("applepenapple", ["apple", "pen"]))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
 print()
