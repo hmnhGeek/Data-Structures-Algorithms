5bec735cabb71179cffb7df6b552da1601a02c62
hmnhGeek <hmnhsharma97@gmail.com>
2026-05-10 14:49:30 +0530

    Word break recursive

---
+++ Python/Practice Set 1/Strings/Word Break.py
@@ -0,0 +1,33 @@
+def recursive():
+    def word_break(string, dictionary):
+        return solve(0, string, dictionary, len(string))
+
+    def solve(i, string, dictionary, n):
+        # if `i` has reached end, then it means, 0 to i part is present in the dictionary and nothing is left
+        # after it. So, return True.
+        if i == n:
+            return True
+
+        # before moving to partitioning, check if the whole string is present in the dictionary or not.
+        if string in dictionary:
+            return True
+
+        # now, partition by length
+        for length in range(1, n + 1):
+            # get the substring and check for the second part.
+            substring = string[i:i+length]
+            if substring in dictionary and solve(i + length, string, dictionary, n):
+                return True
+
+        # return false if second part is not found in the dictionary.
+        return False
+
+    print(word_break("ilike", ["i", "like", "sam", "sung", "samsung", "mobile"]))
+    print(word_break("ilikesamsung", ["i", "like", "sam", "sung", "samsung", "mobile"]))
+    print(word_break("catsandog", ["cats", "dog", "sand", "and", "cat"]))
+    print(word_break("storybook", ["story", "book"]))
+    print(word_break("monkeyandonkey", ["monkey", "donkey", "and"]))
+    print(word_break("applepenapple", ["apple", "pen"]))
+
+
+recursive()
