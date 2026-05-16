27cf77545e7f2527fca61810c62a0d92db7c3536
hmnhGeek <hmnhsharma97@gmail.com>
2026-05-16 17:16:30 +0530

    Tabulation LCS length

--- Python/Practice Set 4/Dynamic Programming/DP30 - Minimum Insertions and Deletions to Convert String A to String B.py
+++ Python/Practice Set 4/Dynamic Programming/DP30 - Minimum Insertions and Deletions to Convert String A to String B.py
@@ -52,7 +52,33 @@ def memoized():
     print(get_lcs_length("ABC", "CBA"))
 
 
+def tabulation():
+    """
+        Time complexity is O(nm) and space complexity is O(nm).
+    """
+    def get_lcs_length(s1, s2):
+        n, m = len(s1), len(s2)
+        dp = {i: {j: 0 for j in range(m + 1)} for i in range(n + 1)}
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return dp[n][m]
+
+    print(get_lcs_length("adebc", "dcadb"))
+    print(get_lcs_length("ab", "defg"))
+    print(get_lcs_length("abcde", "ace"))
+    print(get_lcs_length("abc", "abc"))
+    print(get_lcs_length("abc", "acd"))
+    print(get_lcs_length("AGGTAB", "GXTXAYB"))
+    print(get_lcs_length("ABC", "CBA"))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
 print()
