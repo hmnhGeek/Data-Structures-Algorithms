3aafe4f857fcede59f7fbc345a5d04f62397ba98
hmnhGeek <hmnhsharma97@gmail.com>
2026-05-16 16:23:06 +0530

    Find common elements in 3 sorted arrays

---
+++ Python/Practice Set 1/Arrays/Common in 3 Sorted Arrays.py
@@ -0,0 +1,29 @@
+class Solution:
+    @staticmethod
+    def find_common(a, b, c):
+        i, j, k = 0, 0, 0
+        result = []
+        n, m, p = len(a), len(b), len(c)
+        while i < n and j < m and k < p:
+            if a[i] == b[j] == c[k]:
+                if len(result) == 0:
+                    result.append(a[i])
+                elif result[-1] != a[i]:
+                    result.append(a[i])
+                i += 1
+                j += 1
+                k += 1
+            else:
+                min_val = min(a[i], b[j], c[k])
+                if a[i] == min_val:
+                    i += 1
+                elif b[j] == min_val:
+                    j += 1
+                else:
+                    k += 1
+        return result
+
+
+print(Solution.find_common( [1, 5, 10, 20, 40, 80], [6, 7, 20, 80, 100], [3, 4, 15, 20, 30, 70, 80, 120]))
+print(Solution.find_common([1, 2, 3, 4, 5], [6, 7], [8, 9, 10]))
+print(Solution.find_common([1, 1, 1, 2, 2, 2], [1, 1, 2, 2, 2], [1, 1, 1, 1, 2, 2, 2, 2]))
