ec0c2d24fe004fd0165a05c11e06dc00627b6f73
hmnhGeek <hmnhsharma97@gmail.com>
2026-05-17 18:25:44 +0530

    Number of ways to arrive at destination using Queue

--- Python/Practice Set 4/Graphs/G40 - Number of Ways to Arrive at Destination.py
+++ Python/Practice Set 4/Graphs/G40 - Number of Ways to Arrive at Destination.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/number-of-ways-to-arrive-at-destination/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=%2Fnumber-of-ways-to-arrive-at-destination
+# Solution - https://www.youtube.com/watch?v=_-0mx0SmYxA&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=40
+
+
 class Node:
     def __init__(self, data):
         self.data = data
