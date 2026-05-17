5260c5035750c9ee0a51ceb5778d6d76aaa7c9b9
hmnhGeek <hmnhsharma97@gmail.com>
2026-05-17 18:25:29 +0530

    Number of ways to arrive at destination using Queue

--- Python/Practice Set 4/Graphs/G40 - Number of Ways to Arrive at Destination.py
+++ Python/Practice Set 4/Graphs/G40 - Number of Ways to Arrive at Destination.py
@@ -31,3 +31,72 @@ class Queue:
         self.length -= 1
         return item
 
+
+class Solution:
+    @staticmethod
+    def get_num_ways(graph, source, destination):
+        if source not in graph or destination not in graph:
+            return
+        distances = {i: 1e6 for i in graph}
+        distances[source] = 0
+        parents = {i: set() for i in graph}
+        queue = Queue()
+        queue.push((0, source))
+        while not queue.is_empty():
+            d, node = queue.pop()
+            if node == destination:
+                continue
+            for adj_node, wt in graph[node]:
+                if d + wt < distances[adj_node]:
+                    distances[adj_node] = d + wt
+                    parents[adj_node].clear()
+                    parents[adj_node].add(node)
+                    queue.push((d + wt, adj_node))
+                elif d + wt == distances[adj_node]:
+                    parents[adj_node].add(node)
+                    queue.push((d + wt, adj_node))
+        path, paths = [], []
+        Solution._get_paths(parents, destination, path, paths)
+        return paths
+
+    @staticmethod
+    def _get_paths(parents, node, path, paths):
+        if len(parents[node]) == 0:
+            path.append(node)
+            path = path[-1:-len(path)-1:-1]
+            paths.append(path)
+            return
+        for parent in parents[node]:
+            Solution._get_paths(parents, parent, path + [node, ], paths)
+
+
+print(
+    Solution.get_num_ways(
+        {
+            0: [[1, 2], [4, 5], [6, 7]],
+            1: [[0, 2], [2, 3], [3, 3]],
+            2: [[1, 3], [5, 1]],
+            3: [[1, 3], [6, 3], [5, 1]],
+            4: [[0, 5], [6, 2]],
+            5: [[2, 1], [3, 1], [6, 1]],
+            6: [[0, 7], [3, 3], [4, 2], [5, 1]]
+        },
+        0,
+        6
+    )
+)
+
+print(
+    Solution.get_num_ways(
+        {
+            0: [[1, 1], [2, 2], [5, 8]],
+            1: [[0, 1], [2, 3], [3, 3]],
+            2: [[1, 3], [0, 2], [5, 6]],
+            3: [[1, 3], [4, 2]],
+            4: [[3, 2], [5, 2]],
+            5: [[2, 6], [4, 2], [0, 8]]
+        },
+        0,
+        5
+    )
+)
