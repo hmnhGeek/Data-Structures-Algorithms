e5a1f8a49d0dbf6699a3f077ff6d9a76d82c268a
hmnhGeek <hmnhsharma97@gmail.com>
2026-05-23 18:39:47 +0530

    Rotate Doubly linked list by N nodes

--- Java/LinkedLists/Problem24/Solution.java
+++ Java/LinkedLists/Problem24/Solution.java
@@ -1,12 +1,21 @@
+// Problem link - https://www.geeksforgeeks.org/dsa/rotate-doubly-linked-list-n-nodes/
+
+
 package LinkedLists.Problem24;
 
 public class Solution {
     public static void main(String[] args) {
         rotate(2, 1, 2, 3, 4, 5);
         rotate(3, 2, 6, 5, 4);
+        rotate(2, 1, 2, 3, 4, 5, 6);
+        rotate(3, 3, 4, 5, 1);
+        rotate(2, 1, 2, 3, 1);
     }
 
     public static void rotate(Integer p, Integer...args) {
+        /*
+            Time complexity is O(n) and space complexity is O(1).
+         */
         DoublyLinkedList<Integer> dll = new DoublyLinkedList<>();
         dll.build(args);
         System.out.println(dll);
