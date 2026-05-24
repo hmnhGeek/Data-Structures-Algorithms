c72a4cb39f647429bafffdf6bc0cd36f7df4a46a
hmnhGeek <hmnhsharma97@gmail.com>
2026-05-24 10:57:13 +0530

    Get lowest common ancestor in BST

--- Java/PracticeSet1/BinarySearchTree/Problem7/Solution.java
+++ Java/PracticeSet1/BinarySearchTree/Problem7/Solution.java
@@ -7,9 +7,14 @@ public class Solution {
         System.out.println(run(7, 8, 5, 4, 6, 3, 7, 8));
         System.out.println(run(8, 14, 20, 8, 22, 4, 12, 10, 14));
         System.out.println(run(1, 3, 2, 1, 3));
+        System.out.println(run(2, 8, 6, 2, 8, 0, 4, 7, 9, 3, 5));
+        System.out.println(run(2, 4, 6, 2, 8, 0, 4, 7, 9, 3, 5));
     }
 
     public static Integer run(Integer n1, Integer n2, Integer ... args) {
+        /*
+            Time complexity is O(log(n)) and space complexity is O(1).
+         */
         BinarySearchTree<Integer> bst = new BinarySearchTree<>();
         for (Integer i : args) {
             bst.insert(i);
@@ -25,16 +30,11 @@ public class Solution {
         if (root == null) {
             return null;
         }
-        if (Objects.equals(root.data, n1) || Objects.equals(root.data, n2)) {
-            return root.data;
+        if (root.data < n1 && root.data < n2) {
+            return getLca(root.right, n1, n2);
+        } else if (root.data > n1 && root.data > n2) {
+            return getLca(root.left, n1, n2);
         }
-        Integer left = getLca(root.left, n1, n2);
-        Integer right = getLca(root.right, n1, n2);
-        if (left == null && right == null) {
-            return null;
-        }
-        if (left == null) return right;
-        if (right == null) return left;
         return root.data;
     }
 }
