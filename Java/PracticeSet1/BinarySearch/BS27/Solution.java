4e4640b92bcaf25e8b5c7de8a1ca7dc8c21ef436
hmnhGeek <hmnhsharma97@gmail.com>
2026-05-30 12:40:43 +0530

    Implemented a heap element class

--- Java/PracticeSet1/BinarySearch/BS27/Solution.java
+++ Java/PracticeSet1/BinarySearch/BS27/Solution.java
@@ -1,7 +1,18 @@
 package PracticeSet1.BinarySearch.BS27;
 
+import java.util.List;
+
 public class Solution {
     public static void main(String[] args) {
 
+    }
+
+    public static Integer getMedian(List<List<Integer>> mtx) {
+        int n = mtx.size(), m = mtx.getFirst().size();
+        int totalIntegers = n*m;
+        MinHeap<Integer> pq = new MinHeap<>();
+        for (int i = 0; i < n; i += 1) {
+            pq.insert(mtx.get(i).getFirst());
+        }
     }
 }
