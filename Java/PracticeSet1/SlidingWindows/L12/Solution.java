99d004c70f87d459953b99278672b7544544c240
hmnhGeek <hmnhsharma97@gmail.com>
2026-05-30 12:23:02 +0530

    Get minimum window substring

---
+++ Java/PracticeSet1/SlidingWindows/L12/Solution.java
@@ -0,0 +1,52 @@
+package PracticeSet1.SlidingWindows.L12;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static String getMinWindowSubstring(String string, String pattern) {
+        Map<Character, Integer> t = getDictionary(pattern, Boolean.TRUE);
+        Map<Character, Integer> d = getDictionary(string, Boolean.FALSE);
+        Integer startIndex = -1, left = 0, right = 0, length = 0;
+        int n = string.length();
+        while (right < n) {
+            d.put(string.charAt(right), d.get(string.charAt(right)) + 1);
+            while (thereIsAMatch(d, t)) {
+                length = right - left + 1;
+                startIndex = left;
+                d.put(string.charAt(left), d.get(string.charAt(left)) - 1);
+                left += 1;
+            }
+            right += 1;
+        }
+        if (startIndex != -1) {
+            return string.substring(startIndex, startIndex + length);
+        }
+        return "";
+    }
+
+    private static boolean thereIsAMatch(Map<Character, Integer> d, Map<Character, Integer> t) {
+        for (Character c : t.keySet()) {
+            if (d.get(c) < t.get(c)) return false;
+        }
+        return true;
+    }
+
+    private static Map<Character, Integer> getDictionary(String string, Boolean mapValues) {
+        Map<Character, Integer> d = new HashMap<>();
+        for (Character character : string.toCharArray()) {
+            d.put(character, 0);
+        }
+        if (!mapValues) {
+            return d;
+        }
+        for (Character c : string.toCharArray()) {
+            d.put(c, d.get(c) + 1);
+        }
+        return d;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMinWindowSubstring("ddaaabbca", "abc"));
+    }
+}
