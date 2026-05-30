db4260e4e16abdd4fcbca70468f9884a8438fc21
hmnhGeek <hmnhsharma97@gmail.com>
2026-05-30 12:44:16 +0530

    Get median in a sorted matrix

--- Java/PracticeSet1/BinarySearch/BS27/Solution.java
+++ Java/PracticeSet1/BinarySearch/BS27/Solution.java
@@ -1,18 +1,68 @@
 package PracticeSet1.BinarySearch.BS27;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
     public static void main(String[] args) {
+        // Example 1
+        System.out.println(
+                getMedianFromMatrix(
+                        Arrays.asList(
+                                Arrays.asList(1, 3, 5),
+                                Arrays.asList(2, 6, 9),
+                                Arrays.asList(3, 6, 9)
+                        )
+                )
+        );
 
+        // Example 2
+        System.out.println(
+                getMedianFromMatrix(
+                        Arrays.asList(
+                                Arrays.asList(1),
+                                Arrays.asList(2),
+                                Arrays.asList(3)
+                        )
+                )
+        );
+
+        // Example 3
+        System.out.println(
+                getMedianFromMatrix(
+                        Arrays.asList(
+                                Arrays.asList(3),
+                                Arrays.asList(5),
+                                Arrays.asList(8)
+                        )
+                )
+        );
     }
 
-    public static Integer getMedian(List<List<Integer>> mtx) {
+    public static Integer getMedianFromMatrix(List<List<Integer>> mtx) {
         int n = mtx.size(), m = mtx.getFirst().size();
         int totalIntegers = n*m;
-        MinHeap<Integer> pq = new MinHeap<>();
+        MinHeap<HeapElement<Integer>> pq = new MinHeap<>();
         for (int i = 0; i < n; i += 1) {
-            pq.insert(mtx.get(i).getFirst());
+            pq.insert(new HeapElement<>(mtx.get(i).getFirst(), i, 0));
         }
+        int counter = 0;
+        while (!pq.isEmpty()) {
+            HeapElement<Integer> heapElement = pq.pop();
+            counter += 1;
+            if (counter == (totalIntegers/2) + 1) {
+                return heapElement.data;
+            }
+            if (0 <= heapElement.j + 1 && heapElement.j + 1 < m) {
+                pq.insert(
+                        new HeapElement<>(
+                                mtx.get(heapElement.i).get(heapElement.j + 1),
+                                heapElement.i,
+                                heapElement.j + 1
+                        )
+                );
+            }
+        }
+        return null;
     }
 }
