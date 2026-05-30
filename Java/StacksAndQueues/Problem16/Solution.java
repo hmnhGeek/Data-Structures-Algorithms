f565b5a654b1c2695b79480a755a2811d69d5996
hmnhGeek <hmnhsharma97@gmail.com>
2026-05-30 12:07:07 +0530

    Merge overlapping intervals

--- Java/StacksAndQueues/Problem16/Solution.java
+++ Java/StacksAndQueues/Problem16/Solution.java
@@ -37,5 +37,45 @@ public class Solution {
                         )
                 )
         );
+
+        System.out.println(
+                mergeIntervals(
+                        Arrays.asList(
+                                new Interval(7, 8),
+                                new Interval(1, 5),
+                                new Interval(2, 4),
+                                new Interval(4, 6)
+                        )
+                )
+        );
+
+        System.out.println(
+                mergeIntervals(
+                        Arrays.asList(
+                                new Interval(1, 3),
+                                new Interval(2, 6),
+                                new Interval(8, 10),
+                                new Interval(15, 18)
+                        )
+                )
+        );
+
+        System.out.println(
+                mergeIntervals(
+                        Arrays.asList(
+                                new Interval(1, 4),
+                                new Interval(4, 5)
+                        )
+                )
+        );
+
+        System.out.println(
+                mergeIntervals(
+                        Arrays.asList(
+                                new Interval(4, 7),
+                                new Interval(1, 4)
+                        )
+                )
+        );
     }
 }
