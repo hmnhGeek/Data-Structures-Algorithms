4850feb4e406a89ee917ee7d563d2f6672e18d26
hmnhGeek <hmnhsharma97@gmail.com>
2026-06-06 12:00:40 +0530

    Print functionality in BST

--- Python/Practice Set 3/Binary Search Tree/LCA.py
+++ Python/Practice Set 3/Binary Search Tree/LCA.py
@@ -135,3 +135,14 @@ class BinarySearchTree:
         if x >= start.data:
             return self._get_node(start.right, x)
         return self._get_node(start.left, x)
+
+    def _show(self, start):
+        if start:
+            self._show(start.left)
+            print(f"Data = {start.data}{' (root),' if start == self.root else ','} size = {start.size}, height = {start.height}, d = {start.diameter}")
+            self._show(start.right)
+
+    def show(self):
+        self._show(self.root)
+        print()
+
