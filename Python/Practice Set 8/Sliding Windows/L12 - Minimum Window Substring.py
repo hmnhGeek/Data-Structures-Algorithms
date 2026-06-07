c48e3af1f41ed6bb7c459db01c543c927f914880
hmnhGeek <hmnhsharma97@gmail.com>
2026-06-07 12:48:06 +0530

    Minimum window substring

---
+++ Python/Practice Set 8/Sliding Windows/L12 - Minimum Window Substring.py
@@ -0,0 +1,38 @@
+from collections import Counter
+
+
+class Solution:
+    @staticmethod
+    def min_window_substring(string, pattern):
+        t = dict(Counter(pattern))
+        d = {i: 0 for i in string}
+        left = right = 0
+        n = len(string)
+        length, start_index = n, -1
+        while right < n:
+            d[string[right]] += 1
+            while Solution._all_chars_available(d, t):
+                start_index = left
+                length = min(length, right - left + 1)
+                d[string[left]] -= 1
+                left += 1
+            right += 1
+        if start_index != -1:
+            return string[start_index:start_index+length]
+        return ""
+
+    @staticmethod
+    def _all_chars_available(d, t):
+        for character in t:
+            if d[character] < t[character]:
+                return False
+        return True
+
+
+print(Solution.min_window_substring("ddaaabbca", "abc"))
+print(Solution.min_window_substring("ADOBECODEBANC", "ABC"))
+print(Solution.min_window_substring("a", "a"))
+print(Solution.min_window_substring("a", "aa"))
+print(Solution.min_window_substring("timetopractice", "toc"))
+print(Solution.min_window_substring("zoomlazapzo", "oza"))
+print(Solution.min_window_substring("ABBXC", "BXC"))
