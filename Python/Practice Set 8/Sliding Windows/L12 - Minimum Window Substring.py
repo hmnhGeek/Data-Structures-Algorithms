fe97df3e89c82167e31b9af645060929bc884fa0
hmnhGeek <hmnhsharma97@gmail.com>
2026-06-07 12:48:25 +0530

    Minimum window substring

--- Python/Practice Set 8/Sliding Windows/L12 - Minimum Window Substring.py
+++ Python/Practice Set 8/Sliding Windows/L12 - Minimum Window Substring.py
@@ -1,9 +1,16 @@
+# Problem link - https://leetcode.com/problems/minimum-window-substring/
+# Solution - https://www.youtube.com/watch?v=WJaij9ffOIY&list=PLgUwDviBIf0q7vrFA_HEWcqRqMpCXzYAL&index=12
+
+
 from collections import Counter
 
 
 class Solution:
     @staticmethod
     def min_window_substring(string, pattern):
+        """
+            Time complexity is O(26n) and space complexity is O(26).
+        """
         t = dict(Counter(pattern))
         d = {i: 0 for i in string}
         left = right = 0
