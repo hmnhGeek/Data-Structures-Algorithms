128bfeb252b58f1616b29ede8e21c7804d057010
hmnhGeek <hmnhsharma97@gmail.com>
2026-06-13 13:37:28 +0530

    Tabulation edit distance

--- Python/Practice Set 4/Dynamic Programming/DP33 - Edit Distance.py
+++ Python/Practice Set 4/Dynamic Programming/DP33 - Edit Distance.py
@@ -57,7 +57,38 @@ def memoized():
     print(edit_distance("sunday", "saturday"))
 
 
+def tabulation():
+    """
+        Time complexity is O(nm) and space complexity is O(nm).
+    """
+
+    def edit_distance(s, p):
+        n, m = len(s), len(p)
+        dp = {i: {j: 1e6 for j in range(m + 1)} for i in range(n + 1)}
+        for i in range(n + 1):
+            dp[i][0] = i
+        for j in range(m + 1):
+            dp[0][j] = j
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if s[i - 1] == p[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = 1 + min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1])
+        return dp[n][m]
+
+    print(edit_distance("horse", "ros"))
+    print(edit_distance("abc", "dc"))
+    print(edit_distance("whgtdwhgtdg", "aswcfg"))
+    print(edit_distance("intention", "execution"))
+    print(edit_distance("geek", "gesek"))
+    print(edit_distance("cat", "cut"))
+    print(edit_distance("sunday", "saturday"))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
 print()
