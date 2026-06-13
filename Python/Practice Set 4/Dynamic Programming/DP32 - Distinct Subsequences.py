1cdf6e87679f4d830af17292d9b140371a79266f
hmnhGeek <hmnhsharma97@gmail.com>
2026-06-13 12:57:08 +0530

    Space optimized distinct subsequences

--- Python/Practice Set 4/Dynamic Programming/DP32 - Distinct Subsequences.py
+++ Python/Practice Set 4/Dynamic Programming/DP32 - Distinct Subsequences.py
@@ -82,9 +82,39 @@ def tabulation():
     print(distinct_subsequences("geeksforgeeks", "ge"))
 
 
+def space_optimized():
+    """
+        Time complexity is O(mn) and space complexity is O(m).
+    """
+    def distinct_subsequences(string, pattern):
+        n, m = len(string), len(pattern)
+        prev = {j: 0 for j in range(m + 1)}
+        prev[0] = 1
+        for i in range(1, n + 1):
+            curr = {j: 0 for j in range(m + 1)}
+            curr[0] = 1
+            for j in range(1, m + 1):
+                if string[i - 1] == pattern[j - 1]:
+                    curr[j] = prev[j - 1] + prev[j]
+                else:
+                    curr[j] = prev[j]
+            prev = curr
+        return prev[m]
+
+    print(distinct_subsequences("babgbag", "bag"))
+    print(distinct_subsequences("brootgroot", "brt"))
+    print(distinct_subsequences("dingdingdingding", "ing"))
+    print(distinct_subsequences("aaaaa", "a"))
+    print(distinct_subsequences("rabbbit", "rabbit"))
+    print(distinct_subsequences("banana", "ban"))
+    print(distinct_subsequences("geeksforgeeks", "ge"))
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
+print()
+space_optimized()
 print()
