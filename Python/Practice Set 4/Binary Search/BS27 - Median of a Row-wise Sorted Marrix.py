999af05ff13a939819330f39c36aab6c8f7835dc
hmnhGeek <hmnhsharma97@gmail.com>
2026-06-14 12:52:28 +0530

    Median in a row wise sorted matrix

--- Python/Practice Set 4/Binary Search/BS27 - Median of a Row-wise Sorted Marrix.py
+++ Python/Practice Set 4/Binary Search/BS27 - Median of a Row-wise Sorted Marrix.py
@@ -62,3 +62,67 @@ class MinHeap:
         del self.heap[-1]
         self.min_heapify_down(0)
         return item
+
+
+class Solution:
+    @staticmethod
+    def get_median(mtx):
+        n, m = len(mtx), len(mtx[0])
+        pq = MinHeap()
+        for i in range(n):
+            pq.insert((mtx[i][0], i, 0))
+        counter = 0
+        median_index = (n * m//2) + 1
+        while not pq.is_empty():
+            popped = pq.pop()
+            counter += 1
+            if counter == median_index:
+                return popped[0]
+            i, j = popped[1], popped[2]
+            if 0 <= j + 1 < m:
+                pq.insert((mtx[i][j + 1], i, j + 1))
+        return -1
+
+
+print(
+    Solution.get_median(
+        [
+            [1, 3, 5],
+            [2, 6, 9],
+            [3, 6, 9]
+        ]
+    )
+)
+
+print(
+    Solution.get_median(
+        [
+            [1, 3, 4],
+            [2, 5, 6],
+            [7, 8, 9]
+        ]
+    )
+)
+
+print(
+    Solution.get_median(
+        [[1, 5, 7, 9, 11],
+         [2, 3, 4, 8, 9],
+         [4, 11, 14, 19, 20],
+         [6, 10, 22, 99, 100],
+         [7, 15, 17, 24, 28]]
+    )
+)
+
+print(
+    Solution.get_median(
+        [
+            [1, 2, 3, 4, 5],
+            [8, 9, 11, 12, 13],
+            [21, 23, 25, 27, 29]
+        ]
+    )
+)
+
+print(Solution.get_median([[1], [2], [3]]))
+print(Solution.get_median([[3], [5], [8]]))
