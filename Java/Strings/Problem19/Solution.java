f120ee4335935b1ede585878e58eabcbb16d1727
hmnhGeek <hmnhsharma97@gmail.com>
2026-06-19 12:51:22 +0530

    KMP Algorithm

--- Java/Strings/Problem19/Solution.java
+++ Java/Strings/Problem19/Solution.java
@@ -1,5 +1,6 @@
 // Problem link - https://www.geeksforgeeks.org/problems/longest-prefix-suffix2527/1
 // Solution - https://www.youtube.com/watch?v=qases-9gOpk
+// Problem link - https://www.geeksforgeeks.org/dsa/kmp-algorithm-for-pattern-searching/
 
 
 package Strings.Problem19;
@@ -13,14 +14,21 @@ public class Solution {
         System.out.println(Utils.getLPS("abab"));
         System.out.println(Utils.getLPS("aabcdaabc"));
         System.out.println(Utils.getLPS("aaaa"));
+        System.out.println();
+        System.out.println("KMP Run");
+        System.out.println(getKMPMatch("abcab", "ab"));
+        System.out.println(getKMPMatch("aabaacaadaabaaba", "aaba"));
+        System.out.println(getKMPMatch("abracadabra", "abra"));
+        System.out.println(getKMPMatch("abcabcabc", "abc"));
     }
 
     public static List<Integer> getKMPMatch(String text, String pattern) {
         List<Integer> lps = Utils.getLPS(pattern);
         List<Integer> result = new ArrayList<>();
         int i = 0, j = 0;
+        int n = text.length();
         int m = pattern.length();
-        while (j < m) {
+        while (i < n) {
             if (text.charAt(i) == pattern.charAt(j)) {
                 i += 1;
                 j += 1;
