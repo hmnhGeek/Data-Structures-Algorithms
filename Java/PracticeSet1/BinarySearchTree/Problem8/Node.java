cf724d6ab9bc76038abc55eb58357bdb801c3a2d
hmnhGeek <hmnhsharma97@gmail.com>
2026-06-20 17:26:34 +0530

    Created a node class

---
+++ Java/PracticeSet1/BinarySearchTree/Problem8/Node.java
@@ -0,0 +1,13 @@
+package PracticeSet1.BinarySearchTree.Problem8;
+
+public class Node<T extends Comparable<T>> {
+    public T data;
+    public Node<T> left, right, parent;
+    public Integer size, height, diameter;
+
+    public Node(T data) {
+        this.data = data;
+        this.left = this.right = this.parent = null;
+        this.size = this.height = this.diameter = 1;
+    }
+}
