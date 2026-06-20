5d863edd2be1edb30db8c5880f3ad4b3ca93bcc8
hmnhGeek <hmnhsharma97@gmail.com>
2026-06-20 18:11:24 +0530

    Helper methods from BST

--- Java/PracticeSet1/BinarySearchTree/Problem8/BinarySearchTree.java
+++ Java/PracticeSet1/BinarySearchTree/Problem8/BinarySearchTree.java
@@ -101,4 +101,46 @@ public class BinarySearchTree<T extends Comparable<T>> {
         }
         return getNode(start.left, x);
     }
+
+    private Node<T> getLeftmostLeaf(Node<T> node) {
+        if (node == null) return null;
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
+
+    private Node<T> getRightmostLeaf(Node<T> node) {
+        if (node == null) return null;
+        while (node.right != null) {
+            node = node.right;
+        }
+        return node;
+    }
+
+    private Node<T> getSuccessor(Node<T> node) {
+        if (node == null) return null;
+        if (node.right != null) return getLeftmostLeaf(node.right);
+        Node<T> parent = node.parent;
+        if (parent == null) return null;
+        while (parent.left != node) {
+            parent = parent.parent;
+            node = node.parent;
+            if (parent == null) return null;
+        }
+        return parent;
+    }
+
+    private Node<T> getPredecessor(Node<T> node) {
+        if (node == null) return null;
+        if (node.left != null) return getRightmostLeaf(node.left);
+        Node<T> parent = node.parent;
+        if (parent == null) return null;
+        while (parent.right != node) {
+            parent = parent.parent;
+            node = node.parent;
+            if (parent == null) return null;
+        }
+        return parent;
+    }
 }
