dc035a57f9a59b4512cf99ad6922ff502faa8854
hmnhGeek <hmnhsharma97@gmail.com>
2026-06-21 16:21:18 +0530

    Get sliding window maximum

--- Java/PracticeSet1/Heap/Problem3/Solution.java
+++ Java/PracticeSet1/Heap/Problem3/Solution.java
@@ -1,3 +1,7 @@
+// Problem link - https://www.geeksforgeeks.org/dsa/sliding-window-maximum-maximum-of-all-subarrays-of-size-k/#expected-approach-using-deque-on-time-and-ok-space
+// Solution - https://www.youtube.com/watch?v=29OnjVQ-fk4
+
+
 package PracticeSet1.Heap.Problem3;
 
 import java.util.ArrayList;
@@ -6,18 +10,31 @@ import java.util.List;
 
 public class Solution {
     public static List<Integer> getSlidingWindowMaximum(List<Integer> arr, int k) {
+        /*
+            Time complexity is O(n) and space complexity is O(n).
+         */
         int n = arr.size();
         List<Integer> result = new ArrayList<>();
         Deque<Integer> deque = new Deque<>();
         if (n == 0) return result;
         for (int i = 0; i < n; i += 1) {
+            // if the frontal index is outside the left boundary of the window,
+            // then pop from front.
             while (!deque.isEmpty() && deque.getFront() <= i - k) {
                 deque.popFront();
             }
+
+            // if current element is greater than queue's back, continuously pop
+            // from the back so that monotonous decreasing order is maintained.
             while (!deque.isEmpty() && arr.get(i) > arr.get(deque.getBack())) {
                 deque.popBack();
             }
+
+            // push the current index.
             deque.pushBack(i);
+
+            // while `i` is greater than the right boundary of the first window, update
+            // maximums.
             if (i >= k - 1) {
                 result.add(arr.get(deque.getFront()));
             }
