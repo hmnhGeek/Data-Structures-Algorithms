65bdf9c45a6e72cf17b98ab7eefd85bad2616acb
hmnhGeek <hmnhsharma97@gmail.com>
2026-06-22 09:24:12 +0530

    Added docstrings

--- Java/LinkedLists/Problem25/Solution.java
+++ Java/LinkedLists/Problem25/Solution.java
@@ -36,6 +36,14 @@ public class Solution {
         }
     }
 
+    /**
+     * Method to relink the part to the original DLL.
+     * @param dll
+     * @param pointers
+     * @param nextCurr
+     * @param prev
+     * @param <T>
+     */
     private static <T> void relink(DoublyLinkedList<T> dll, List<Node<T>> pointers, Node<T> nextCurr, Node<T> prev) {
         pointers.getLast().next = nextCurr;
         if (nextCurr != null) {
@@ -50,6 +58,13 @@ public class Solution {
         }
     }
 
+    /**
+     * Reverse the DLL part.
+     * @param curr A node denoting the current node.
+     * @param part A Part object.
+     * @return The new head and tail of the part.
+     * @param <T> A generic type.
+     */
     private static <T> List<Node<T>> reversePart(Node<T> curr, Part part) {
         DoublyLinkedList<T> sublist = new DoublyLinkedList<>();
         sublist.head = curr;
@@ -59,6 +74,12 @@ public class Solution {
         return pointers;
     }
 
+    /**
+     * Method to de-link the part from the rest of the DLL.
+     * @param part A {@code Part<T>} generic object.
+     * @return Returns the next node which will become current.
+     * @param <T> A generic type.
+     */
     private static <T> Node<T> delink(Part part) {
         Node<T> nextCurr = part.temp.next;
         if (part.temp != null) {
@@ -70,6 +91,14 @@ public class Solution {
         return nextCurr;
     }
 
+    /**
+     * Method to return the part of the DLL (no de-linking yet).
+     * @param k Denotes the size of the part.
+     * @param curr Current pointer from which k nodes must be traversed.
+     * @return A {@code Part<T>} object containing the last node of the part and the size of the part (could be
+     * {@code k} or less than {@code k}.
+     * @param <T> A generic type denoting the kind of elements stored in the linked list.
+     */
     private static <T> Part<T> getPart(int k, Node<T> curr) {
         Node<T> temp = curr;
         Node<T> prevOfTemp = temp.prev;
@@ -81,6 +110,8 @@ public class Solution {
             counter += 1;
             length += 1;
         }
+
+        // if temp became null before k nodes, then we can just reverse the small part.
         if (temp == null) {
             temp = prevOfTemp;
         }
