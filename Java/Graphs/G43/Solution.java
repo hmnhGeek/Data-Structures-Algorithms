5b2d7f30c6d4487de997c2f6ab7882db55758a1d
hmnhGeek <hmnhsharma97@gmail.com>
2026-06-28 10:38:55 +0530

    Find the City With the Smallest Number of Neighbours at a Threshold Distance

--- Java/Graphs/G43/Solution.java
+++ Java/Graphs/G43/Solution.java
@@ -1,11 +1,12 @@
 package Graphs.G43;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Solution {
-    public static List<List<Integer>> findDistances(Map<Integer, List<List<Integer>>> graph, Integer threshold) {
+    public static List<List<Integer>> findDistances(Map<Integer, List<List<Integer>>> graph) {
         List<List<Integer>> adjMatrix = getMatrixGraph(graph);
 
         for (Integer k : graph.keySet()) {
@@ -44,5 +45,73 @@ public class Solution {
             }
         }
         return mtx;
+    }
+
+    public static Integer findCity(Map<Integer, List<List<Integer>>> graph, Integer threshold) {
+        List<List<Integer>> adjMtx = findDistances(graph);
+        int city = -1;
+        int minReachable = graph.size();
+        for (int i = 0; i < graph.size(); i += 1) {
+            int count = 0;
+            for (int j = 0; j < graph.size(); j += 1) {
+                if (adjMtx.get(i).get(j) <= threshold) {
+                    count += 1;
+                }
+            }
+            if (count <= minReachable) {
+                minReachable = count;
+                city = i;
+            }
+        }
+        return city;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+                Solution.findCity(
+                        buildGraph(
+                                new int[][]{
+                                        {0, 1, 3},
+                                        {1, 2, 1},
+                                        {1, 3, 4},
+                                        {2, 3, 1}
+                                },
+                                4
+                        ),
+                        4
+                )
+        );
+
+        System.out.println(
+                Solution.findCity(
+                        buildGraph(
+                                new int[][]{
+                                        {0, 1, 2},
+                                        {0, 4, 8},
+                                        {1, 2, 3},
+                                        {1, 4, 2},
+                                        {2, 3, 1},
+                                        {3, 4, 1}
+                                },
+                                5
+                        ),
+                        2
+                )
+        );
+    }
+
+    private static Map<Integer, List<List<Integer>>> buildGraph(int[][] edges, int n) {
+        Map<Integer, List<List<Integer>>> graph = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            graph.put(i, new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(List.of(edge[1], edge[2]));
+            graph.get(edge[1]).add(List.of(edge[0], edge[2]));
+        }
+
+        return graph;
     }
 }
