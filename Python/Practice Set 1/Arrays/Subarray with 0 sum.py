4feeaf7b1eb559f925c96cd62120be973b8c1917
hmnhGeek <hmnhsharma97@gmail.com>
2026-06-29 12:47:14 +0530

    Subarray with 0 sum

---
+++ Python/Practice Set 1/Arrays/Subarray with 0 sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    @staticmethod
+    def has_zero_sum(arr, k = 0):
+        d = {0: 1}
+        prefix_sum = count = i = 0
+        n = len(arr)
+        while i < n:
+            prefix_sum += arr[i]
+            if prefix_sum - k in d:
+                count += d[prefix_sum - k]
+            if prefix_sum in d:
+                d[prefix_sum] += 1
+            else:
+                d[prefix_sum] = 1
+            i += 1
+        return count
+
+
+print(Solution.has_zero_sum([4, 2, -3, 1, 6]))
+print(Solution.has_zero_sum([4, 2, 0, 1, 6]))
+print(Solution.has_zero_sum([1, 2, -1]))
+print(Solution.has_zero_sum([1, 1, 1], 2))
+print(Solution.has_zero_sum([1, 2, 3], 3))
