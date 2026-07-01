1af54221aa234f6dd6fcf9330167ebd65203666c
hmnhGeek <hmnhsharma97@gmail.com>
2026-07-01 08:32:50 +0530

    Search in a 2D Matrix

---
+++ Python/Practice Set 6/Matrix/2 - Search in 2D Matrix.py
@@ -0,0 +1,20 @@
+class Solution:
+    @staticmethod
+    def search(mtx, x):
+        n, m = len(mtx), len(mtx[0])
+        low, high = 0, n*m - 1
+        while low <= high:
+            mid = int(low + (high - low)/2)
+            i, j = mid//m, mid%m
+            if mtx[i][j] == x:
+                return i, j
+            if mtx[i][j] > x:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1, -1
+
+
+print(Solution.search([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 13))
+print(Solution.search([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3))
+print(Solution.search([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]], 8))
