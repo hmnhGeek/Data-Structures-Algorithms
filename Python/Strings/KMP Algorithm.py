5b400ff6854170f10d98f075a6f95811bdb25823
hmnhGeek <hmnhsharma97@gmail.com>
2026-07-03 13:43:08 +0530

    Logic to calculate LPS array

---
+++ Python/Strings/KMP Algorithm.py
@@ -0,0 +1,20 @@
+class Solution:
+    @staticmethod
+    def get_lps(pattern):
+        m = len(pattern)
+        lps = [None] * m
+        lps[0] = 0
+        i = 1
+        length = 0
+        while i < m:
+            if pattern[i] == pattern[lps[length]]:
+                length += 1
+                lps[i] = length
+                i += 1
+            else:
+                if length - 1 > 0:
+                    length = lps[length - 1]
+                else:
+                    lps[i] = 0
+                    i += 1
+        return lps
