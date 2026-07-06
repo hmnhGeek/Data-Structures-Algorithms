1732902afb415392a230a1824cc571058b0ad558
hmnhGeek <hmnhsharma97@gmail.com>
2026-07-06 12:10:05 +0530

    Memoized wildcard matching

--- Python/Practice Set 4/Dynamic Programming/DP34 - Wildcard Matching.py
+++ Python/Practice Set 4/Dynamic Programming/DP34 - Wildcard Matching.py
@@ -21,7 +21,7 @@ def recursive():
         if s1[i - 1] == s2[j - 1] or s1[i - 1] == "?":
             return solve(s1, i - 1, s2, j - 1)
         elif s1[i - 1] == "*":
-            return solve(s1, i - 1, s2, j - 1) or solve(s1, i, s2, j - 1)
+            return solve(s1, i - 1, s2, j) or solve(s1, i, s2, j - 1)
         else:
             return False
 
@@ -35,5 +35,48 @@ def recursive():
     print(wildcard_matching("?a", "cb"))
 
 
+def memoized():
+    """
+        Time complexity is O(nm) and space complexity is O(m + n + nm).
+    """
+
+    def wildcard_matching(s1, s2):
+        n, m = len(s1), len(s2)
+        dp = {i: {j: None for j in range(m + 1)} for i in range(n + 1)}
+        return solve(s1, n, s2, m, dp)
+
+    def solve(s1, i, s2, j, dp):
+        if i == 0 and j > 0:
+            return False
+        if i == 0 and j == 0:
+            return True
+        if i > 0 and j == 0:
+            all_stars = True
+            for k in range(i):
+                if s1[k - 1] != "*":
+                    all_stars = False
+            return all_stars
+        if dp[i][j] is not None:
+            return dp[i][j]
+        if s1[i - 1] == s2[j - 1] or s1[i - 1] == "?":
+            dp[i][j] = solve(s1, i - 1, s2, j - 1, dp)
+        elif s1[i - 1] == "*":
+            dp[i][j] = solve(s1, i - 1, s2, j, dp) or solve(s1, i, s2, j - 1, dp)
+        else:
+            dp[i][j] = False
+        return dp[i][j]
+
+    print(wildcard_matching("ab*cd", "abdefcd"))
+    print(wildcard_matching("ab?d", "abcc"))
+    print(wildcard_matching("?ay", "ray"))
+    print(wildcard_matching("ba*a?", "baaabab"))
+    print(wildcard_matching("*", "abc"))
+    print(wildcard_matching("a*ab", "baaabab"))
+    print(wildcard_matching("a?c*", "abcde"))
+    print(wildcard_matching("?a", "cb"))
+
+
 recursive()
+print()
+memoized()
 print()
