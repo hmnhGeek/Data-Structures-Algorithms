7ab6c45faf8c9e3629a8915bbf83cc2a9e51ad7c
hmnhGeek <hmnhsharma97@gmail.com>
2026-07-06 11:54:18 +0530

    Find the City With the Smallest Number of Neighbours at a Threshold Distance

--- Python/Practice Set 4/Graphs/G43 - Find the City With the Smallest Number of Neighbours at a Threshold Distance.py
+++ Python/Practice Set 4/Graphs/G43 - Find the City With the Smallest Number of Neighbours at a Threshold Distance.py
@@ -3,6 +3,30 @@ class Solution:
     def find_city(edges, threshold, n):
         mtx = Solution._get_graph_matrix(edges, n)
 
+        for via in range(n):
+            for i in range(n):
+                for j in range(n):
+                    if mtx[i][via] + mtx[via][j] < mtx[i][j]:
+                        mtx[i][j] = mtx[i][via] + mtx[via][j]
+
+        for i in range(n):
+            if mtx[i][i] < 0:
+                return -1
+
+        city = -1
+        min_cities_reachable = n
+        for i in range(n):
+            count = 0
+            for j in range(n):
+                if i == j:
+                    continue
+                if mtx[i][j] <= threshold:
+                    count += 1
+            if count <= min_cities_reachable:
+                min_cities_reachable = count
+                city = i
+        return city
+
     @staticmethod
     def _get_graph_matrix(edges, n):
         mtx = []
@@ -14,8 +38,33 @@ class Solution:
                 else:
                     row.append(1e6)
             mtx.append(row)
-
         for edge in edges:
             src, dest, wt = edge
             mtx[src][dest] = wt
+            mtx[dest][src] = wt
         return mtx
+
+
+print(
+    Solution.find_city(
+        [[0, 1, 3],
+         [1, 2, 1],
+         [1, 3, 4],
+         [2, 3, 1]],
+        4,
+        4
+    )
+)
+
+print(
+    Solution.find_city(
+        [[0, 1, 2],
+         [0, 4, 8],
+         [1, 2, 3],
+         [1, 4, 2],
+         [2, 3, 1],
+         [3, 4, 1]],
+        2,
+        5
+    )
+)
