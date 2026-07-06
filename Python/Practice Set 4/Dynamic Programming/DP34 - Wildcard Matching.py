56385a27eb93bf7f3fa24176c582ccff35d3ce12
hmnhGeek <hmnhsharma97@gmail.com>
2026-07-06 12:15:25 +0530

    Space optimized wildcard matching

--- Python/Practice Set 4/Dynamic Programming/DP34 - Wildcard Matching.py
+++ Python/Practice Set 4/Dynamic Programming/DP34 - Wildcard Matching.py
@@ -111,9 +111,47 @@ def tabulation():
     print(wildcard_matching("?a", "cb"))
 
 
+def space_optimized():
+    """
+        Time complexity is O(nm) and space complexity is O(m).
+    """
+
+    def wildcard_matching(s1, s2):
+        n, m = len(s1), len(s2)
+        prev = {j: False for j in range(m + 1)}
+        prev[0] = True
+        for i in range(1, n + 1):
+            curr = {j: False for j in range(m + 1)}
+            all_stars = True
+            for k in range(i):
+                if s1[k - 1] != "*":
+                    all_stars = False
+            curr[0] = all_stars
+            for j in range(1, m + 1):
+                if s1[i - 1] == s2[j - 1] or s1[i - 1] == "?":
+                    curr[j] = prev[j - 1]
+                elif s1[i - 1] == "*":
+                    curr[j] = prev[j] or curr[j - 1]
+                else:
+                    curr[j] = False
+            prev = curr
+        return prev[m]
+
+    print(wildcard_matching("ab*cd", "abdefcd"))
+    print(wildcard_matching("ab?d", "abcc"))
+    print(wildcard_matching("?ay", "ray"))
+    print(wildcard_matching("ba*a?", "baaabab"))
+    print(wildcard_matching("*", "abc"))
+    print(wildcard_matching("a*ab", "baaabab"))
+    print(wildcard_matching("a?c*", "abcde"))
+    print(wildcard_matching("?a", "cb"))
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
+print()
+space_optimized()
 print()
