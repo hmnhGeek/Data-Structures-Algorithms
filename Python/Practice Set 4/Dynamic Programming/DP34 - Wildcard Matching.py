9122e7a75787b237df67816a8f18769deb83c740
hmnhGeek <hmnhsharma97@gmail.com>
2026-07-06 12:06:03 +0530

    Recursive wildcard matching

---
+++ Python/Practice Set 4/Dynamic Programming/DP34 - Wildcard Matching.py
@@ -0,0 +1,39 @@
+def recursive():
+    """
+        Time complexity is exponential and space complexity is O(m + n).
+    """
+
+    def wildcard_matching(s1, s2):
+        n, m = len(s1), len(s2)
+        return solve(s1, n, s2, m)
+
+    def solve(s1, i, s2, j):
+        if i == 0 and j > 0:
+            return False
+        if i == 0 and j == 0:
+            return True
+        if i > 0 and j == 0:
+            all_stars = True
+            for k in range(i):
+                if s1[k - 1] != "*":
+                    all_stars = False
+            return all_stars
+        if s1[i - 1] == s2[j - 1] or s1[i - 1] == "?":
+            return solve(s1, i - 1, s2, j - 1)
+        elif s1[i - 1] == "*":
+            return solve(s1, i - 1, s2, j - 1) or solve(s1, i, s2, j - 1)
+        else:
+            return False
+
+    print(wildcard_matching("ab*cd", "abdefcd"))
+    print(wildcard_matching("ab?d", "abcc"))
+    print(wildcard_matching("?ay", "ray"))
+    print(wildcard_matching("ba*a?", "baaabab"))
+    print(wildcard_matching("*", "abc"))
+    print(wildcard_matching("a*ab", "baaabab"))
+    print(wildcard_matching("a?c*", "abcde"))
+    print(wildcard_matching("?a", "cb"))
+
+
+recursive()
+print()
