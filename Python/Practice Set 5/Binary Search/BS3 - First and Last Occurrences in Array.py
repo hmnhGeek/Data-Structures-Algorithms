1e69b8db48c92b6767e47c6d4c6a332402278078
hmnhGeek <hmnhsharma97@gmail.com>
2026-07-09 17:14:27 +0530

    First and last occurrences in a sorted array

--- Python/Practice Set 5/Binary Search/BS3 - First and Last Occurrences in Array.py
+++ Python/Practice Set 5/Binary Search/BS3 - First and Last Occurrences in Array.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.naukri.com/code360/problems/first-and-last-position-of-an-element-in-sorted-array_1082549
+# Solution - https://www.youtube.com/watch?v=hjR1IYVx9lY&list=PLgUwDviBIf0pMFMWuuvDNMAkoQFi-h0ZF&index=4
+
+
 class Solution:
     @staticmethod
     def _first_occurrence(arr, x):
@@ -25,6 +29,9 @@ class Solution:
 
     @staticmethod
     def get_occurrences(arr, x):
+        """
+            Time complexity is O(2 * log(n)) and space complexity is O(1).
+        """
         first = Solution._first_occurrence(arr, x)
         last = Solution._last_occurrence(arr, x)
         if first is not None and arr[first] != x:
