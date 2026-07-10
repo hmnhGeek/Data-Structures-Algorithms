ba60863a627780c6fa5b47c4364dcc1fd174887b
hmnhGeek <hmnhsharma97@gmail.com>
2026-07-10 07:36:09 +0530

    Best time to buy and sell stocks

--- Python/Practice Set 4/Dynamic Programming/DP35 - Best Time to Buy and Sell Stock.py
+++ Python/Practice Set 4/Dynamic Programming/DP35 - Best Time to Buy and Sell Stock.py
@@ -1,4 +1,11 @@
+# Problem link - https://www.naukri.com/code360/problems/stocks-are-profitable_893405?source=youtube&campaign=striver_dp_videos
+# Solution - https://www.youtube.com/watch?v=excAOvwF_Wk&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=36
+
+
 class Solution:
+    """
+        Time complexity is O(n) and space complexity is O(1).
+    """
     @staticmethod
     def get_max_profit(arr):
         profit = 0
