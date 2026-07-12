0c55c50239e6a8ef29aa43c31f1785aac71704fd
hmnhGeek <hmnhsharma97@gmail.com>
2026-07-12 07:48:32 +0530

    Triplets with Smaller Sum

--- Java/SearchingAndSorting/Problem13/Solution.java
+++ Java/SearchingAndSorting/Problem13/Solution.java
@@ -1,3 +1,6 @@
+// Problem link - https://www.geeksforgeeks.org/problems/count-triplets-with-sum-smaller-than-x5549/1
+
+
 package SearchingAndSorting.Problem13;
 
 import java.util.ArrayList;
@@ -8,9 +11,13 @@ public class Solution {
     public static void main(String[] args) {
         System.out.println(getTriplets(Arrays.asList(-2, 0, 1, 3), 2));
         System.out.println(getTriplets(Arrays.asList(5, 1, 3, 4, 7), 12));
+        System.out.println(getTriplets(Arrays.asList(1, 5, 2, 3, 4, 6, 7), 9));
     }
 
     public static List<List<Integer>> getTriplets(List<Integer> arr, int sum) {
+        /*
+            Time complexity is O(n^2 + nlog(n)) and space complexity is O(1).
+         */
         QuickSort.sort(arr);
         int i = 0, n = arr.size();
         List<List<Integer>> result = new ArrayList<>();
