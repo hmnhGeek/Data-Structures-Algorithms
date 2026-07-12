ce8b3c7337c32f814e34f7e9d9f310fe5f15a11d
hmnhGeek <hmnhsharma97@gmail.com>
2026-07-12 07:35:12 +0530

    Implemented quick sort class

---
+++ Java/SearchingAndSorting/Problem13/QuickSort.java
@@ -0,0 +1,35 @@
+package SearchingAndSorting.Problem13;
+
+import java.util.Collections;
+import java.util.List;
+
+public class QuickSort {
+    public static <T extends Comparable<T>> void sort(List<T> arr) {
+        sort(arr, 0, arr.size() - 1);
+    }
+
+    private static <T extends Comparable<T>> void sort(List<T> arr, int low, int high) {
+        if (low >= high) return;
+        int partitionIndex = getPartitionIndex(arr, low, high);
+        sort(arr, low, partitionIndex - 1);
+        sort(arr, partitionIndex + 1, high);
+    }
+
+    private static <T extends Comparable<T>> int getPartitionIndex(List<T> arr, int low, int high) {
+        int i = low, j = high;
+        T pivot = arr.get(low);
+        while (i < j) {
+            while (arr.get(i).compareTo(pivot) <= 0 && i <= high - 1) {
+                i += 1;
+            }
+            while (arr.get(j).compareTo(pivot) > 0 && j >= low + 1) {
+                j -= 1;
+            }
+            if (i < j) {
+                Collections.swap(arr, i, j);
+            }
+        }
+        Collections.swap(arr, low, j);
+        return j;
+    }
+}
---
+++ Java/SearchingAndSorting/Problem13/Solution.java
@@ -0,0 +1,6 @@
+package SearchingAndSorting.Problem13;
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+}
