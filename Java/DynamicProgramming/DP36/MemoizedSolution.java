2c92e7e919b65a0797a26cb6d63713a7c17e958a
hmnhGeek <hmnhsharma97@gmail.com>
2026-07-18 17:02:32 +0530

    Memoized Buy and Sell Stock - II

---
+++ Java/DynamicProgramming/DP36/MemoizedSolution.java
@@ -0,0 +1,39 @@
+package DynamicProgramming.DP36;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemoizedSolution {
+    /*
+        Time complexity is exponential and space complexity is O(n).
+     */
+    public static Integer getMaxProfit(List<Integer> arr) {
+        int n = arr.size();
+        Map<Integer, Map<Boolean, Integer>> dp = new HashMap<>();
+        for (int i = 0; i <= n; i += 1) {
+            Map<Boolean, Integer> subMap = new HashMap<>();
+            subMap.put(true, null);
+            subMap.put(false, null);
+            dp.put(i, subMap);
+        }
+        return solve(arr, 0, true, n, dp);
+    }
+
+    private static Integer solve(List<Integer> arr, int i, boolean j, int n, Map<Integer, Map<Boolean, Integer>> dp) {
+        if (i == n) return 0;
+        if (dp.get(i).get(j) != null) return dp.get(i).get(j);
+        if (j) {
+            dp.get(i).put(j, Math.max(
+                    -arr.get(i) + solve(arr, i + 1, !j, n, dp),
+                    solve(arr, i + 1, j, n, dp)
+            ));
+        } else {
+            dp.get(i).put(j, Math.max(
+                    arr.get(i) + solve(arr, i + 1, !j, n, dp),
+                    solve(arr, i + 1, j, n, dp)
+            ));
+        }
+        return dp.get(i).get(j);
+    }
+}
--- Java/DynamicProgramming/DP36/Solution.java
+++ Java/DynamicProgramming/DP36/Solution.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Solution {
     public static void main(String[] args) {
         recursive();
+        memoized();
     }
 
     private static void recursive() {
@@ -14,6 +15,16 @@ public class Solution {
         System.out.println(RecursiveSolution.getMaxProfit(Arrays.asList(1, 2, 3, 4, 5)));
         System.out.println(RecursiveSolution.getMaxProfit(Arrays.asList(100, 180, 260, 310, 40, 535, 695)));
         System.out.println(RecursiveSolution.getMaxProfit(Arrays.asList(4, 2, 2, 2, 4)));
+        System.out.println();
+    }
+
+    private static void memoized() {
+        System.out.println(MemoizedSolution.getMaxProfit(Arrays.asList(7, 1, 5, 3, 6, 4)));
+        System.out.println(MemoizedSolution.getMaxProfit(Arrays.asList(1, 2, 3, 4, 5, 6, 7)));
+        System.out.println(MemoizedSolution.getMaxProfit(Arrays.asList(7, 6, 5, 4, 3, 2, 1)));
+        System.out.println(MemoizedSolution.getMaxProfit(Arrays.asList(1, 2, 3, 4, 5)));
+        System.out.println(MemoizedSolution.getMaxProfit(Arrays.asList(100, 180, 260, 310, 40, 535, 695)));
+        System.out.println(MemoizedSolution.getMaxProfit(Arrays.asList(4, 2, 2, 2, 4)));
         System.out.println();
     }
 }
