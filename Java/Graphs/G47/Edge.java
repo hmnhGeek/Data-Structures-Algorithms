8903d80dc2022e3f664e0a04805721a5bc71d65b
hmnhGeek <hmnhsharma97@gmail.com>
2026-07-20 16:43:56 +0530

    Implemented quick sort

---
+++ Java/Graphs/G47/Edge.java
@@ -0,0 +1,17 @@
+package Graphs.G47;
+
+public class Edge<T> implements Comparable<Edge<T>> {
+    public Integer weight;
+    public T u, v;
+
+    public Edge(Integer weight, T u, T v) {
+        this.weight = weight;
+        this.u = u;
+        this.v = v;
+    }
+
+    @Override
+    public int compareTo(Edge<T> o) {
+        return this.weight.compareTo(o.weight);
+    }
+}
---
+++ Java/Graphs/G47/Solution.java
@@ -0,0 +1,7 @@
+package Graphs.G47;
+
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+}
