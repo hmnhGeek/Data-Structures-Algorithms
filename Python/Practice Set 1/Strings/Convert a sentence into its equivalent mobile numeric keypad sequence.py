2f9ba1693df4df2f710ae1b4c28e47bbec35f73f
hmnhGeek <hmnhsharma97@gmail.com>
2026-07-24 10:47:11 +0530

    Convert a sentence into its equivalent mobile numeric keypad sequence

---
+++ Python/Practice Set 1/Strings/Convert a sentence into its equivalent mobile numeric keypad sequence.py
@@ -0,0 +1,41 @@
+class Solution:
+    keypad = {
+        'A': '2',
+        'B': '22',
+        'C': '222',
+        'D': '3',
+        'E': '33',
+        'F': '333',
+        'G': '4',
+        'H': '44',
+        'I': '444',
+        'J': '5',
+        'K': '55',
+        'L': '555',
+        'M': '6',
+        'N': '66',
+        'O': '666',
+        'P': '7',
+        'Q': '77',
+        'R': '777',
+        'S': '7777',
+        'T': '8',
+        'U': '88',
+        'V': '888',
+        'W': '9',
+        'X': '99',
+        'Y': '999',
+        'Z': '9999',
+        ' ': '0'
+    }
+
+    @staticmethod
+    def convert_to_numeric_repr(string):
+        result = ""
+        for i in string:
+            result += Solution.keypad[i]
+        return result
+
+
+print(Solution.convert_to_numeric_repr('GEEKSFORGEEKS'))
+print(Solution.convert_to_numeric_repr("HELLO WORLD"))
