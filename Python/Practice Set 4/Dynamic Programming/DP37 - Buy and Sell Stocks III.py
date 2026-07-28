83356195bee061a1cc0cba13d00e2eb9c012ec32
hmnhGeek <hmnhsharma97@gmail.com>
2026-07-28 14:26:12 +0530

    Memoized Buy and Sell Stocks III

--- Python/Practice Set 4/Dynamic Programming/DP37 - Buy and Sell Stocks III.py
+++ Python/Practice Set 4/Dynamic Programming/DP37 - Buy and Sell Stocks III.py
@@ -27,5 +27,40 @@ def recursive():
     print(max_profit([7, 1, 5, 3, 6, 4]))
 
 
+def memoized():
+    """
+        Time complexity is O(3*2*n) and space complexity is O(n + 6n).
+    """
+    def max_profit(arr):
+        n = len(arr)
+        dp = {i: {j: {k: None for k in range(3)} for j in [True, False]} for i in range(n + 1)}
+        return solve(arr, 0, True, 2, n, dp)
+
+    def solve(arr, i, j, k, n, dp):
+        if k == 0 or i >= n:
+            return 0
+        if dp[i][j][k] is not None:
+            return dp[i][j][k]
+        if j:
+            dp[i][j][k] = max(
+                -arr[i] + solve(arr, i + 1, not j, k, n, dp),
+                solve(arr, i + 1, j, k, n, dp)
+            )
+        else:
+            dp[i][j][k] = max(
+                arr[i] + solve(arr, i + 1, not j, k - 1, n, dp),
+                solve(arr, i + 1, j, k, n, dp)
+            )
+        return dp[i][j][k]
+
+    print(max_profit([3, 3, 5, 0, 0, 3, 1, 4]))
+    print(max_profit([1, 3, 1, 2, 4, 8]))
+    print(max_profit([5, 4, 3, 2, 1]))
+    print(max_profit([1, 2, 3, 4, 5]))
+    print(max_profit([7, 1, 5, 3, 6, 4]))
+
+
 recursive()
+print()
+memoized()
 print()
