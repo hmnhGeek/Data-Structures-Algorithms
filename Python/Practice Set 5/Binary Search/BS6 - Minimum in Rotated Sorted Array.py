86a2112799efd3471aa9c8d66e20898828821c08
hmnhGeek <hmnhsharma97@gmail.com>
2026-07-31 12:30:10 +0530

    Minimum in rotated sorted array

--- Python/Practice Set 5/Binary Search/BS6 - Minimum in Rotated Sorted Array.py
+++ Python/Practice Set 5/Binary Search/BS6 - Minimum in Rotated Sorted Array.py
@@ -1,6 +1,13 @@
+# Problem link - https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+# Solution - https://www.youtube.com/watch?v=nhEMDKMB44g&list=PLgUwDviBIf0pMFMWuuvDNMAkoQFi-h0ZF&index=7
+
+
 class Solution:
     @staticmethod
     def get_min_in_rotated(arr):
+        """
+            Time complexity is O(log(n)) and space complexity is O(1).
+        """
         n = len(arr)
         low, high = 0, n - 1
         minimum = 1e6
