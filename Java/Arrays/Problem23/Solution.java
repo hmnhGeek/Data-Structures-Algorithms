157f86c856c6bc7bb45116ca7a4a05711f29d7a5
hmnhGeek <hmnhsharma97@gmail.com>
2026-08-01 13:32:49 +0530

    Maximum Product Subarray

--- Java/Arrays/Problem23/Solution.java
+++ Java/Arrays/Problem23/Solution.java
@@ -1,3 +1,7 @@
+// Problem link - https://www.geeksforgeeks.org/problems/maximum-product-subarray3604/1
+// Solution - https://www.youtube.com/watch?v=hnswaLJvr6g
+
+
 package Arrays.Problem23;
 
 import java.util.Arrays;
@@ -5,6 +9,9 @@ import java.util.List;
 
 public class Solution {
     public static Integer getMaxProduct(List<Integer> arr) {
+        /*
+            Time complexity is O(n) and space complexity is O(1).
+         */
         int n = arr.size();
         int maxProduct = Integer.MIN_VALUE;
         int prefix = 1, suffix = 1;
@@ -24,5 +31,11 @@ public class Solution {
 
     public static void main(String[] args) {
         System.out.println(getMaxProduct(Arrays.asList(-2, 6, -3, -10, 0, 2)));
+        System.out.println(getMaxProduct(Arrays.asList(-1, -3, -10, 0, 6)));
+        System.out.println(getMaxProduct(Arrays.asList(2, 3, 4)));
+        System.out.println(getMaxProduct(Arrays.asList(2, 3, -2, 4)));
+        System.out.println(getMaxProduct(Arrays.asList(-2, 0, -1)));
+        System.out.println(getMaxProduct(Arrays.asList(1, 2, 3, 4, 5, 0)));
+        System.out.println(getMaxProduct(Arrays.asList(1, 2, -3, 0, -4, -5)));
     }
 }
