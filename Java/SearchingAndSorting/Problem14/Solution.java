8c20e19a0f51522be5cacfad1d0f3467b46b5f26
hmnhGeek <hmnhsharma97@gmail.com>
2026-08-02 11:40:32 +0530

    Merge two sorted arrays without using extra space

--- Java/SearchingAndSorting/Problem14/Solution.java
+++ Java/SearchingAndSorting/Problem14/Solution.java
@@ -1,3 +1,7 @@
+// Problem link - https://www.geeksforgeeks.org/problems/merge-two-sorted-arrays-1587115620/1
+// Solution - https://www.youtube.com/watch?v=n7uwj04E0I4
+
+
 package SearchingAndSorting.Problem14;
 
 import java.util.Arrays;
@@ -5,6 +9,9 @@ import java.util.List;
 
 public class Solution {
     public static void mergeSortedArrays(List<Integer> a, List<Integer> b) {
+        /*
+            Time complexity is O(min(n, m) + nlog(n) + mlog(m)) and space complexity is O(1).
+         */
         int n = a.size(), m = b.size();
         int i = n - 1, j = 0;
         while (i >= 0 && j < m) {
@@ -27,5 +34,7 @@ public class Solution {
 
     public static void main(String[] args) {
         mergeSortedArrays(Arrays.asList(2, 4, 7, 10), Arrays.asList(2, 3));
+        mergeSortedArrays(Arrays.asList(1, 5, 9, 10, 15, 20), Arrays.asList(2, 3, 8, 13));
+        mergeSortedArrays(Arrays.asList(0, 1), Arrays.asList(2, 3));
     }
 }
