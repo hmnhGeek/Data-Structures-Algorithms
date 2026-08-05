7bfd9165f9c73373d6f3a86baecc7bb279ba989a
hmnhGeek <hmnhsharma97@gmail.com>
2026-08-05 11:38:29 +0530

    Check if an expression has redundant brackets

---
+++ Java/StacksAndQueues/Problem19/Solution.java
@@ -0,0 +1,33 @@
+package StacksAndQueues.Problem19;
+
+import java.util.List;
+
+public class Solution {
+    private static final List<Character> operators = List.of('+', '-', '/', '*');
+
+    public static boolean hasRedundantBrackets(String expr) {
+        Stack<Character> stack = new Stack<>();
+        for (Character character : expr.toCharArray()) {
+            if (operators.contains(character) || character == '(') {
+                stack.push(character);
+            } else if (character == ')') {
+                if (stack.top() == '(') return true;
+                while (stack.top() != '(') {
+                    stack.pop();
+                }
+                stack.pop();
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Solution.hasRedundantBrackets("((a+b))"));
+        System.out.println(Solution.hasRedundantBrackets("(a+(b)/c)"));
+        System.out.println(Solution.hasRedundantBrackets("(a+b+(c+d))"));
+        System.out.println(Solution.hasRedundantBrackets("(a+b)"));
+        System.out.println(Solution.hasRedundantBrackets("(a+c*b)+(c)"));
+        System.out.println(Solution.hasRedundantBrackets("(a*b+(c/d))"));
+        System.out.println(Solution.hasRedundantBrackets("((a/b))"));
+    }
+}
