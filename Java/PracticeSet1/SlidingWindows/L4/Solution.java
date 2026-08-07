2fbf33281d0decc09f8ace1d132c15d42eccf8c5
hmnhGeek <hmnhsharma97@gmail.com>
2026-08-07 10:26:51 +0530

    Max Consecutive Ones III

--- Java/PracticeSet1/SlidingWindows/L4/Solution.java
+++ Java/PracticeSet1/SlidingWindows/L4/Solution.java
@@ -1,3 +1,7 @@
+// Problem link - https://leetcode.com/problems/max-consecutive-ones-iii/description/
+// Solution - https://www.youtube.com/watch?v=3E4JBHSLpYk&list=PLgUwDviBIf0q7vrFA_HEWcqRqMpCXzYAL&index=4
+
+
 package PracticeSet1.SlidingWindows.L4;
 
 import java.util.Arrays;
@@ -5,6 +9,9 @@ import java.util.List;
 
 public class Solution {
     public static List<Integer> getMaxConsecutiveOnes(List<Integer> arr, int k) {
+        /*
+            Time complexity is O(n) and space complexity is O(1).
+         */
         if (k < 0) return null;
         int n = arr.size();
         int left = 0, right = 0, length = 0, startIndex = -1, countZeros = 0;
