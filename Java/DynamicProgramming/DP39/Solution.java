36e5d7276255ce0c6cc0b2be89e9227ab6524c73
hmnhGeek <hmnhsharma97@gmail.com>
2026-08-09 12:40:32 +0530

    Tabulation buy and sell stocks with cooldown

--- Java/DynamicProgramming/DP39/Solution.java
+++ Java/DynamicProgramming/DP39/Solution.java
@@ -23,8 +23,19 @@ public class Solution {
         System.out.println();
     }
 
+    private static void tabulation() {
+        System.out.println(TabulationSolution.getMaxProfit(Arrays.asList(4, 9, 0, 4, 10)));
+        System.out.println(TabulationSolution.getMaxProfit(Arrays.asList(1, 2, 3, 4)));
+        System.out.println(TabulationSolution.getMaxProfit(Arrays.asList(5, 4, 3)));
+        System.out.println(TabulationSolution.getMaxProfit(Arrays.asList(1, 2, 3, 0, 2)));
+        System.out.println(TabulationSolution.getMaxProfit(Arrays.asList(1)));
+        System.out.println(TabulationSolution.getMaxProfit(Arrays.asList(3, 1, 6, 1, 2, 4)));
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         recursive();
         memoized();
+        tabulation();
     }
 }
