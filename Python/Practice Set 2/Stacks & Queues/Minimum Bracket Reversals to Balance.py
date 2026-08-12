9b675aef428b041e7ad70376038afca8723f1085
hmnhGeek <hmnhsharma97@gmail.com>
2026-08-12 09:43:29 +0530

    Minimum Bracket Reversals to Balance

--- Python/Practice Set 2/Stacks & Queues/Minimum Bracket Reversals to Balance.py
+++ Python/Practice Set 2/Stacks & Queues/Minimum Bracket Reversals to Balance.py
@@ -29,3 +29,37 @@ class Stack:
         self.length -= 1
         return item
 
+
+class Solution:
+    @staticmethod
+    def min_reversals(brackets):
+        n = len(brackets)
+        if n % 2 == 1:
+            return
+        stack = Stack()
+        open_count = close_count = 0
+        for i in range(len(brackets)):
+            bracket = brackets[i]
+            if bracket == "{":
+                stack.push(bracket)
+                open_count += 1
+            elif not stack.is_empty():
+                stack.pop()
+                open_count -= 1
+            else:
+                close_count += 1
+        if open_count % 2 == 0:
+            open_count //= 2
+        else:
+            open_count = (open_count // 2) + 1
+        if close_count % 2 == 0:
+            close_count //= 2
+        else:
+            close_count = (close_count // 2) + 1
+        return open_count + close_count
+
+
+print(Solution.min_reversals("}{{}}{{{"))
+print(Solution.min_reversals("{{}{{{}{{}}{{"))
+print(Solution.min_reversals("{{"))
+print(Solution.min_reversals("}{}{"))
