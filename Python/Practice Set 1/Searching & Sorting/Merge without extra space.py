2b4728282e42aa019a6ad62835f7e5a7c0bbdcea
hmnhGeek <hmnhsharma97@gmail.com>
2026-08-14 10:59:51 +0530

    Merge two sorted arrays without using extra space

--- Python/Practice Set 1/Searching & Sorting/Merge without extra space.py
+++ Python/Practice Set 1/Searching & Sorting/Merge without extra space.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/merge-two-sorted-arrays-1587115620/1
+# Solution - https://www.youtube.com/watch?v=n7uwj04E0I4
+
+
 class QuickSort:
     @staticmethod
     def sort(arr):
@@ -30,6 +34,9 @@ class QuickSort:
 class Solution:
     @staticmethod
     def merge_without_extra_space(arr1, arr2):
+        """
+            Time complexity is O(n + m + nlog(n) + mlog(m)) and space complexity is O(1).
+        """
         i, j = len(arr1) - 1, 0
         while i >= 0 and j < len(arr2):
             if arr1[i] > arr2[j]:
@@ -46,3 +53,5 @@ class Solution:
 
 
 Solution.merge_without_extra_space([2, 4, 7, 10], [2, 3])
+Solution.merge_without_extra_space([1, 5, 9, 10, 15, 20], [2, 3, 8, 13])
+Solution.merge_without_extra_space([0, 1], [2, 3])
