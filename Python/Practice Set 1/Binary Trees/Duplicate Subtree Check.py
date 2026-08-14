cdff9d8d69b80a15f18edf88a17b0b910c36892b
hmnhGeek <hmnhsharma97@gmail.com>
2026-08-14 11:36:06 +0530

    Get duplicate subtrees

---
+++ Python/Practice Set 1/Binary Trees/Duplicate Subtree Check.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = self.right = None
+
+
+class Solution:
+    @staticmethod
+    def find_duplicate_subtrees(root: Node):
+        mp = {}
+        result = []
+        Solution.solve(root, mp, result)
+        print(result)
+
+    @staticmethod
+    def solve(root, mp, result):
+        if root is None:
+            return "N"
+        s = str(root.data) + "," + Solution.solve(root.left, mp, result) + "," + Solution.solve(root.right, mp, result)
+        if s in mp:
+            if mp[s] == 1:
+                result.append(s)
+            mp[s] += 1
+        else:
+            mp[s] = 1
+        return s
+
+
+# Example 1
+one, two, three, four, two1, four2, four3 = Node(1), Node(2), Node(3), Node(4), Node(2), Node(4), Node(4)
+one.left = two
+two.left = four
+three.left = two1
+two1.left = four3
+one.right = three
+three.right = four2
+Solution.find_duplicate_subtrees(one)
+print()
+
+# Example 2
+five, four, six, three, four2, three2, six2 = Node(5), Node(4), Node(6), Node(3), Node(4), Node(3), Node(6)
+five.left = four
+four.left = three
+four2.left = three2
+five.right = six
+four.right = four2
+four2.right = six2
+Solution.find_duplicate_subtrees(five)
