8d024f0a7f0aa57f3973626543218bdcc0ee76cf
hmnhGeek <hmnhsharma97@gmail.com>
2026-08-22 12:19:26 +0530

    Buy and sell stocks IV tabulation

--- Python/Practice Set 4/Dynamic Programming/DP38 - Buy and Stock Sell IV.py
+++ Python/Practice Set 4/Dynamic Programming/DP38 - Buy and Stock Sell IV.py
@@ -61,7 +61,38 @@ def memoized():
     print(buy_sell([7, 1, 5, 3, 6, 4], 2))
 
 
+def tabulation():
+    def buy_sell(arr, max_transactions_allowed):
+        """
+            Time complexity is O(n * k) and space complexity is O(nk).
+        """
+        n = len(arr)
+        dp = {i: {j: {k: 0 for k in range(max_transactions_allowed + 1)} for j in [True, False]} for i in range(n + 1)}
+        for i in range(n - 1, -1, -1):
+            for j in [True, False]:
+                for k in range(1, max_transactions_allowed + 1):
+                    if j:
+                        dp[i][j][k] = max(
+                            -arr[i] + dp[i + 1][not j][k],
+                            dp[i + 1][j][k]
+                        )
+                    else:
+                        dp[i][j][k] = max(
+                            arr[i] + dp[i + 1][not j][k - 1],
+                            dp[i + 1][j][k]
+                        )
+        return dp[0][True][max_transactions_allowed]
+
+    print(buy_sell([3, 3, 5, 0, 0, 3, 1, 4], 2))
+    print(buy_sell([1, 3, 1, 2, 4, 8], 2))
+    print(buy_sell([5, 4, 3, 2, 1], 2))
+    print(buy_sell([1, 2, 3, 4, 5], 2))
+    print(buy_sell([7, 1, 5, 3, 6, 4], 2))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
 print()
