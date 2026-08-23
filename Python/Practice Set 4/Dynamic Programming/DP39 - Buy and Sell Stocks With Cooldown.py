f4f49ddfcee027426722d60c593b224de96abf35
hmnhGeek <hmnhsharma97@gmail.com>
2026-08-23 18:23:48 +0530

    Memoized Buy and Sell Stocks With Cooldown

--- Python/Practice Set 4/Dynamic Programming/DP39 - Buy and Sell Stocks With Cooldown.py
+++ Python/Practice Set 4/Dynamic Programming/DP39 - Buy and Sell Stocks With Cooldown.py
@@ -28,5 +28,41 @@ def recursive():
     print(buy_sell([3, 1, 6, 1, 2, 4]))
 
 
+def memoized():
+    """
+        Time complexity is O(n) and space complexity is O(n + n).
+    """
+    def buy_sell(arr):
+        n = len(arr)
+        dp = {i: {j: None for j in [True, False]} for i in range(n + 2)}
+        return solve(arr, 0, True, n, dp)
+
+    def solve(arr, i, j, n, dp):
+        if i >= n:
+            return 0
+        if dp[i][j] is not None:
+            return dp[i][j]
+        if j:
+            dp[i][j] = max(
+                -arr[i] + solve(arr, i + 1, not j, n, dp),
+                solve(arr, i + 1, j, n, dp)
+            )
+        else:
+            dp[i][j] = max(
+                arr[i] + solve(arr, i + 2, not j, n, dp),
+                solve(arr, i + 1, j, n, dp)
+            )
+        return dp[i][j]
+
+    print(buy_sell([4, 9, 0, 4, 10]))
+    print(buy_sell([1, 2, 3, 4]))
+    print(buy_sell([5, 4, 3]))
+    print(buy_sell([1, 2, 3, 0, 2]))
+    print(buy_sell([1]))
+    print(buy_sell([3, 1, 6, 1, 2, 4]))
+
+
 recursive()
+print()
+memoized()
 print()
