d2df6b713a7e487df0b58f8084afc030f554062e
hmnhGeek <hmnhsharma97@gmail.com>
2026-08-23 18:25:38 +0530

    Tabulation Buy and Sell Stocks With Cooldown

--- Python/Practice Set 4/Dynamic Programming/DP39 - Buy and Sell Stocks With Cooldown.py
+++ Python/Practice Set 4/Dynamic Programming/DP39 - Buy and Sell Stocks With Cooldown.py
@@ -62,7 +62,38 @@ def memoized():
     print(buy_sell([3, 1, 6, 1, 2, 4]))
 
 
+def tabulation():
+    """
+        Time complexity is O(n) and space complexity is O(n).
+    """
+    def buy_sell(arr):
+        n = len(arr)
+        dp = {i: {j: 0 for j in [True, False]} for i in range(n + 2)}
+        for i in range(n - 1, -1, -1):
+            for j in [True, False]:
+                if j:
+                    dp[i][j] = max(
+                        -arr[i] + dp[i + 1][not j],
+                        dp[i + 1][j]
+                    )
+                else:
+                    dp[i][j] = max(
+                        arr[i] + dp[i + 2][not j],
+                        dp[i + 1][j]
+                    )
+        return dp[0][True]
+
+    print(buy_sell([4, 9, 0, 4, 10]))
+    print(buy_sell([1, 2, 3, 4]))
+    print(buy_sell([5, 4, 3]))
+    print(buy_sell([1, 2, 3, 0, 2]))
+    print(buy_sell([1]))
+    print(buy_sell([3, 1, 6, 1, 2, 4]))
+
+
 recursive()
 print()
 memoized()
+print()
+tabulation()
 print()
