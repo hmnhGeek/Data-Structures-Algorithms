b38c946d244ac259754afa8cbe2e6e62def5db5b
hmnhGeek <hmnhsharma97@gmail.com>
2026-08-23 18:16:56 +0530

    Number of operations to make graph connected

--- Python/Practice Set 4/Graphs/G49 - Number of Operations to Make Network Connected.py
+++ Python/Practice Set 4/Graphs/G49 - Number of Operations to Make Network Connected.py
@@ -20,7 +20,7 @@ class DisjointSet:
             self.parents[ulp_node1] = ulp_node2
             self.sizes[ulp_node2] += self.sizes[ulp_node1]
         else:
-            self.sizes[ulp_node2] = ulp_node1
+            self.parents[ulp_node2] = ulp_node1
             self.sizes[ulp_node1] += self.sizes[ulp_node2]
 
     def in_same_components(self, node1, node2):
@@ -32,3 +32,34 @@ class DisjointSet:
             if self.parents[node] == node:
                 count += 1
         return count
+
+
+class Solution:
+    @staticmethod
+    def get_num_ops(edges, n):
+        ds = DisjointSet([i for i in range(n)])
+        extra_edges_count = 0
+        for u, v in edges:
+            if ds.in_same_components(u, v):
+                extra_edges_count += 1
+            ds.union(u, v)
+        if extra_edges_count >= ds.get_num_components() - 1:
+            return ds.get_num_components() - 1
+        return -1
+
+
+print(Solution.get_num_ops([[0, 1], [0, 2], [1, 2]], 4))
+print(Solution.get_num_ops([[0, 1], [0, 2], [0, 3], [1, 2], [1, 3]], 6))
+print(Solution.get_num_ops([[0, 1], [0, 2], [0, 3], [1, 2]], 6))
+print(Solution.get_num_ops(
+    [
+        [1, 2],
+        [2, 3],
+        [0, 3],
+        [0, 1],
+        [0, 2],
+        [4, 5],
+        [5, 6],
+        [7, 8]
+    ], 9
+))
