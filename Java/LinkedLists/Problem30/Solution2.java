328b60f556899ac60ed0820de68ecc93184dd8a8
hmnhGeek <hmnhsharma97@gmail.com>
2026-08-27 12:17:40 +0530

    Clone List with Next and Random

--- Java/LinkedLists/Problem30/Solution2.java
+++ Java/LinkedLists/Problem30/Solution2.java
@@ -1,3 +1,7 @@
+// Problem link - https://www.geeksforgeeks.org/problems/clone-a-linked-list-with-next-and-random-pointer/1
+// Solution - https://www.youtube.com/watch?v=q570bKdrnlw
+
+
 package LinkedLists.Problem30;
 
 public class Solution2 {
@@ -51,6 +55,7 @@ public class Solution2 {
     }
 
     public static void main(String[] args) {
+        // Example 1
         LinkedList<Integer> linkedList = new LinkedList<>();
         linkedList.build(1, 2, 2, 3, -1, 10);
         linkedList.head.next.random = linkedList.tail;
@@ -58,5 +63,32 @@ public class Solution2 {
         System.out.println(linkedList);
         System.out.println(copied);
         System.out.println(linkedList.equals(copied));
+        System.out.println();
+
+        // Example 2
+        LinkedList<Integer> linkedList1 = new LinkedList<>();
+        linkedList1.build(1, 3, 5, 9);
+        linkedList1.head.random = linkedList1.head.next.next;
+        linkedList1.head.next.random = linkedList1.head.next.next;
+        linkedList1.tail.random = linkedList1.head.next.next;
+        LinkedList<Integer> copied1 = deepCopy(linkedList1);
+        System.out.println(linkedList1);
+        System.out.println(copied1);
+        System.out.println(linkedList1.equals(copied1));
+        System.out.println();
+
+        // Example 3
+        LinkedList<Integer> l2 = new LinkedList<>();
+        l2.build(1, 2, 3, 4, 5);
+        l2.head.random = l2.head.next.next;
+        l2.head.next.random = l2.head;
+        l2.head.next.next.random = l2.tail;
+        l2.head.next.next.next.random = l2.head.next.next;
+        l2.tail.random = l2.head.next;
+        LinkedList<Integer> copied2 = deepCopy(l2);
+        System.out.println(l2);
+        System.out.println(copied2);
+        System.out.println(l2.equals(copied2));
+        System.out.println();
     }
 }
