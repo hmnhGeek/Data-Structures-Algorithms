2fb10397691d9bdc222a6f972a45a090fd476c7b
hmnhGeek <hmnhsharma97@gmail.com>
2026-08-28 16:48:22 +0530

    Implemented a stack class using queue

---
+++ Java/StacksAndQueues/Problem20/Solution.java
@@ -0,0 +1,7 @@
+package StacksAndQueues.Problem20;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Java/StacksAndQueues/Problem20/Stack.java
@@ -0,0 +1,35 @@
+package StacksAndQueues.Problem20;
+
+public class Stack<T> {
+    private Queue<T> q1;
+    private Queue<T> q2;
+
+    public Stack() {
+        this.q1 = new Queue<>();
+        this.q2 = new Queue<>();
+    }
+
+    public void push(T x) {
+        while (!q1.isEmpty()) {
+            q2.enqueue(q1.dequeue());
+        }
+        q1.enqueue(x);
+        while (!q2.isEmpty()) {
+            q1.enqueue(q2.dequeue());
+        }
+    }
+
+    public T top() {
+        if (q1.isEmpty()) return null;
+        return q1.front();
+    }
+
+    public Integer size() {
+        return q1.length;
+    }
+
+    public T pop() {
+        if (q1.isEmpty()) return null;
+        return q1.dequeue();
+    }
+}
