dd74cb142cecec73d993418e8f6987a0b350c35a
hmnhGeek <hmnhsharma97@gmail.com>
2026-08-29 11:13:48 +0530

    Merge k Sorted Arrays

--- Java/PracticeSet1/Heap/Problem6/Solution.java
+++ Java/PracticeSet1/Heap/Problem6/Solution.java
@@ -1,3 +1,6 @@
+// Problem link - https://www.geeksforgeeks.org/problems/merge-k-sorted-arrays/1
+
+
 package PracticeSet1.Heap.Problem6;
 
 import java.util.ArrayList;
@@ -33,15 +36,33 @@ public class Solution {
                         )
                 )
         );
+
+        System.out.println(
+                mergeKSortedArrays(
+                        Arrays.asList(
+                                Arrays.asList(1, 2, 3, 4),
+                                Arrays.asList(2, 2, 3, 4),
+                                Arrays.asList(5, 5, 6, 6),
+                                Arrays.asList(7, 8, 9, 9)
+                        )
+                )
+        );
     }
 
     public static <T extends Comparable<T>> List<T> mergeKSortedArrays(List<List<T>> mtx) {
+        /*
+            Time complexity is O({n + k} * log(k)) and space complexity is O(k).
+         */
         MinHeap<HeapElement<T>> minHeap = new MinHeap<>();
         int n = mtx.size(), m = mtx.getFirst().size();
+
+        // This will take O(k * log(k)) and space of O(k).
         for (int i = 0; i < n; i += 1) {
             minHeap.insert(new HeapElement<>(mtx.get(i).getFirst(), i, 0));
         }
         List<T> result = new ArrayList<>();
+
+        // This will run for n times, thus O(n * log(k)) and space complexity is O(k).
         while (!minHeap.isEmpty()) {
             HeapElement<T> heapElement = minHeap.pop();
             result.add(heapElement.data);
