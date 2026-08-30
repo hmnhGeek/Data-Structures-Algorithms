77290da7c06feb9a009bf7b13fc205713e3af12a
hmnhGeek <hmnhsharma97@gmail.com>
2026-08-30 12:50:14 +0530

    Tabulation Buy and Sell Stocks With Transaction Fee

--- Java/DynamicProgramming/DP40/Solution.java
+++ Java/DynamicProgramming/DP40/Solution.java
@@ -23,8 +23,19 @@ public class Solution {
         System.out.println();
     }
 
+    public static void tabulation() {
+        System.out.println(TabulationSolution.getMaxProfit(Arrays.asList(1,3,2,8,4,9),2));
+        System.out.println(TabulationSolution.getMaxProfit(Arrays.asList(1,2,3),1));
+        System.out.println(TabulationSolution.getMaxProfit(Arrays.asList(1,3,5,6),2));
+        System.out.println(TabulationSolution.getMaxProfit(Arrays.asList(1,3,7,5,10,3),3));
+        System.out.println(TabulationSolution.getMaxProfit(Arrays.asList(6,1,7,2,8,4),2));
+        System.out.println(TabulationSolution.getMaxProfit(Arrays.asList(7,1,5,3,6,4),1));
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         recursive();
         memoized();
+        tabulation();
     }
 }
---
+++ Java/DynamicProgramming/DP40/TabulationSolution.java
@@ -0,0 +1,38 @@
+package DynamicProgramming.DP40;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TabulationSolution {
+    /*
+        Time complexity is O(2n) and space complexity is O(2n).
+     */
+    public static Integer getMaxProfit(List<Integer> arr, Integer transactionFee) {
+        int n = arr.size();
+        Map<Integer, Map<Boolean, Integer>> dp = new HashMap<>();
+        for (int i = 0; i <= n; i += 1) {
+            Map<Boolean, Integer> next = new HashMap<>();
+            for (Boolean j : List.of(true, false)) {
+                next.put(j, 0);
+            }
+            dp.put(i, next);
+        }
+        for (int i = n - 1; i >= 0; i -= 1) {
+            for (boolean j : List.of(true, false)) {
+                if (j) {
+                    dp.get(i).put(j, Math.max(
+                            -arr.get(i) + dp.get(i + 1).get(!j),
+                            dp.get(i + 1).get(j)
+                    ));
+                } else {
+                    dp.get(i).put(j, Math.max(
+                            arr.get(i) - transactionFee + dp.get(i + 1).get(!j),
+                            dp.get(i + 1).get(j)
+                    ));
+                }
+            }
+        }
+        return dp.get(0).get(true);
+    }
+}
