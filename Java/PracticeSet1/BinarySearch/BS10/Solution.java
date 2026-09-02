696de2ef6b27d21beed1a32aeff11e3f33b1d4c7
hmnhGeek <hmnhsharma97@gmail.com>
2026-09-02 14:28:58 +0530

    Find square root of a number

---
+++ Java/PracticeSet1/BinarySearch/BS10/Solution.java
@@ -0,0 +1,28 @@
+package PracticeSet1.BinarySearch.BS10;
+
+public class Solution {
+    public static Integer findSqrt(int n) {
+        if (n < 0) return null;
+        if (n == 0) return 0;
+        int low = 1, high = n;
+        while (low <= high) {
+            int mid = (low + (high - low)/2);
+            if (mid * mid == n) return mid;
+            if (mid * mid > n) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return high;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findSqrt(28));
+        System.out.println(findSqrt(0));
+        System.out.println(findSqrt(1));
+        System.out.println(findSqrt(2));
+        System.out.println(findSqrt(36));
+        System.out.println(findSqrt(100));
+    }
+}
