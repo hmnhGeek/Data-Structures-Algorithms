20944c66ddeebcc59abb03fdd73cde80d6f1a7b8
hmnhGeek <hmnhsharma97@gmail.com>
2026-09-04 11:13:34 +0530

    Longest consecutive subsequence using sorting approach

--- Python/Practice Set 1/Arrays/Longest Consecutive Subsequnce.py
+++ Python/Practice Set 1/Arrays/Longest Consecutive Subsequnce.py
@@ -16,6 +16,22 @@ class Solution:
             longest_length = max(longest_length, count)
         return longest_length
 
+    @staticmethod
+    def get_lcs(arr):
+        arr.sort()
+        longest_length = 0
+        count = 1
+        i = 0
+        n = len(arr)
+        while i < n - 1:
+            while i < n - 1 and arr[i + 1] == arr[i] + 1:
+                count += 1
+                i += 1
+            longest_length = max(longest_length, count)
+            count = 1
+            i += 1
+        return longest_length
+
 
 print(Solution.get_longest_consecutive_length([2, 6, 1, 9, 4, 5, 3]))
 print(Solution.get_longest_consecutive_length([1, 9, 3, 10, 4, 20, 2]))
@@ -23,3 +39,10 @@ print(Solution.get_longest_consecutive_length([15, 13, 12, 14, 11, 10, 9]))
 print(Solution.get_longest_consecutive_length([36, 41, 56, 35, 44, 33, 34, 92, 43, 32, 42]))
 print(Solution.get_longest_consecutive_length([100, 4, 200, 1, 3, 2]))
 print(Solution.get_longest_consecutive_length([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]))
+print()
+print(Solution.get_lcs([2, 6, 1, 9, 4, 5, 3]))
+print(Solution.get_lcs([1, 9, 3, 10, 4, 20, 2]))
+print(Solution.get_lcs([15, 13, 12, 14, 11, 10, 9]))
+print(Solution.get_lcs([36, 41, 56, 35, 44, 33, 34, 92, 43, 32, 42]))
+print(Solution.get_lcs([100, 4, 200, 1, 3, 2]))
+print(Solution.get_lcs([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]))
