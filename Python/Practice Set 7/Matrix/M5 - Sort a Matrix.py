cebd0678837488acd0a07909b0fccf3f95bcea55
hmnhGeek <hmnhsharma97@gmail.com>
2026-09-04 11:36:59 +0530

    Sort a matrix using merge sort

--- Python/Practice Set 7/Matrix/M5 - Sort a Matrix.py
+++ Python/Practice Set 7/Matrix/M5 - Sort a Matrix.py
@@ -1,9 +1,51 @@
+class MergeSort:
+    @staticmethod
+    def sort(arr):
+        n = len(arr)
+        MergeSort._sort(arr, 0, n - 1)
+
+    @staticmethod
+    def _sort(arr, low, high):
+        if low >= high:
+            return
+        mid = int(low + (high - low)/2)
+        MergeSort._sort(arr, low, mid)
+        MergeSort._sort(arr, mid + 1, high)
+        arr[low:high+1] = MergeSort._merge(arr, low, high)
+
+    @staticmethod
+    def _merge(arr, low, high):
+        mid = int(low + (high - low)/2)
+        left = arr[low:mid+1]
+        right = arr[mid+1:high+1]
+        i, j = 0, 0
+        merged = []
+
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                merged.append(left[i])
+                i += 1
+            else:
+                merged.append(right[j])
+                j += 1
+
+        while i < len(left):
+            merged.append(left[i])
+            i += 1
+
+        while j < len(right):
+            merged.append(right[j])
+            j += 1
+
+        return merged
+
+
 class Solution:
     @staticmethod
     def sort_matrix(mtx):
         n, m = len(mtx), len(mtx[0])
         flattened_mtx = Solution._flatten(mtx, n, m)
-        flattened_mtx.sort()
+        MergeSort.sort(flattened_mtx)
         return Solution._reconstruct(flattened_mtx, n, m)
 
     @staticmethod
