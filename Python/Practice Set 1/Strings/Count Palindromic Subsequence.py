ec24749e4812103bf5d7cd0aff73e3309a4cff09
hmnhGeek <hmnhsharma97@gmail.com>
2026-09-06 12:11:13 +0530

    Count palindromic subsequences (space optimized)

--- Python/Practice Set 1/Strings/Count Palindromic Subsequence.py
+++ Python/Practice Set 1/Strings/Count Palindromic Subsequence.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/count-palindromic-subsequences/1
+# Solution - https://www.youtube.com/watch?v=vlbA8oUxSV0
+
+
 def recursive():
     def count(string):
         n = len(string)
