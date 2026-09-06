2258f73b603bf111d0636544a8e5bac8423ab6c7
hmnhGeek <hmnhsharma97@gmail.com>
2026-09-06 12:34:06 +0530

    Merge without extra space

--- Python/Practice Set 2/Searching & Sorting/Merge without extra space.py
+++ Python/Practice Set 2/Searching & Sorting/Merge without extra space.py
@@ -1,3 +1,32 @@
+class QuickSort:
+    @staticmethod
+    def sort(arr):
+        n = len(arr)
+        QuickSort._sort(arr, 0, n - 1)
+
+    @staticmethod
+    def _sort(arr, low, high):
+        if low >= high:
+            return
+        partition_index = QuickSort._get_partition_index(arr, low, high)
+        QuickSort._sort(arr, low, partition_index - 1)
+        QuickSort._sort(arr, partition_index + 1, high)
+
+    @staticmethod
+    def _get_partition_index(arr, low, high):
+        i, j = low, high
+        pivot = arr[low]
+        while i < j:
+            while arr[i] <= pivot and i <= high - 1:
+                i += 1
+            while arr[j] > pivot and j >= low + 1:
+                j -= 1
+            if i < j:
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[low], arr[j] = arr[j], arr[low]
+        return j
+
+
 class Solution:
     @staticmethod
     def merge_without_extra_space(a, b):
@@ -10,8 +39,8 @@ class Solution:
                 j += 1
             else:
                 break
-        a.sort()
-        b.sort()
+        QuickSort.sort(a)
+        QuickSort.sort(b)
         print(a, b)
 
 
