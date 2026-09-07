137129330f52ee81ae79ca51f2f10ed6933115a6
hmnhGeek <hmnhsharma97@gmail.com>
2026-09-07 12:49:27 +0530

    Check mirror in N-ary tree

--- Python/Practice Set 2/Binary Tree/Check Mirror in N-ary tree.py
+++ Python/Practice Set 2/Binary Tree/Check Mirror in N-ary tree.py
@@ -1,3 +1,7 @@
+# Problem link - https://www.geeksforgeeks.org/problems/check-mirror-in-n-ary-tree1528/1
+# Solution - https://www.youtube.com/watch?v=oH63SpSshm0
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -33,6 +37,9 @@ class Stack:
 class Solution:
     @staticmethod
     def check_mirror(arr, mirror):
+        """
+            Time complexity is O(e) and space complexity is O(e).
+        """
         mp = {}
         for i in arr:
             mp[i] = Stack()
