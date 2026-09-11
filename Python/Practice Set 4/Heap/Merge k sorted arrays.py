68efd3c3c2c4e7d6ad7cb8dcc53ade87844bfa34
hmnhGeek <hmnhsharma97@gmail.com>
2026-09-11 11:19:45 +0530

    Merge k sorted arrays

--- Python/Practice Set 4/Heap/Merge k sorted arrays.py
+++ Python/Practice Set 4/Heap/Merge k sorted arrays.py
@@ -67,3 +67,61 @@ class MinHeap:
         self.min_heapify_down(0)
         return item
 
+
+class Solution:
+    @staticmethod
+    def merge_k_sorted_lists(mtx):
+        n = len(mtx)
+        if n == 0:
+            return []
+        pq = MinHeap()
+        for i in range(n):
+            if len(mtx[i]) != 0:
+                pq.insert((mtx[i][0], i, 0))
+        result = []
+        while not pq.is_empty():
+            elem, i, j = pq.pop()
+            result.append(elem)
+            if 0 <= j + 1 < len(mtx[i]):
+                pq.insert((mtx[i][j + 1], i, j + 1))
+        return result
+
+
+print(
+    Solution.merge_k_sorted_lists(
+        [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ]
+    )
+)
+
+print(
+    Solution.merge_k_sorted_lists(
+        [
+            [1, 4, 5],
+            [1, 3, 4],
+            [2, 6]
+        ]
+    )
+)
+
+print(
+    Solution.merge_k_sorted_lists([])
+)
+
+print(
+    Solution.merge_k_sorted_lists([[]])
+)
+
+print(
+    Solution.merge_k_sorted_lists(
+        [
+            [1],
+            [2, 4],
+            [3, 7, 9, 11],
+            [13]
+        ]
+    )
+)
