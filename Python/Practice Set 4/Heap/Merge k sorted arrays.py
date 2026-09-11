f85db10ab6b2fa525b2bd2e648724732839a665e
hmnhGeek <hmnhsharma97@gmail.com>
2026-09-11 11:20:42 +0530

    Merge k sorted arrays

--- Python/Practice Set 4/Heap/Merge k sorted arrays.py
+++ Python/Practice Set 4/Heap/Merge k sorted arrays.py
@@ -1,3 +1,6 @@
+# Problem link - https://www.geeksforgeeks.org/problems/merge-k-sorted-arrays/1
+
+
 class MinHeap:
     def __init__(self):
         self.heap = []
@@ -71,6 +74,9 @@ class MinHeap:
 class Solution:
     @staticmethod
     def merge_k_sorted_lists(mtx):
+        """
+            Time complexity is O(n * log(k)) and space complexity is O(k).
+        """
         n = len(mtx)
         if n == 0:
             return []
