ece26590fec512dc170a00d7379c0bc30353b36e
hmnhGeek <hmnhsharma97@gmail.com>
2026-09-13 13:12:19 +0530

    Space optimized DP 40

--- Python/Practice Set 4/Dynamic Programming/DP40 - Buy and Sell Stocks With Transaction Fee.py
+++ Python/Practice Set 4/Dynamic Programming/DP40 - Buy and Sell Stocks With Transaction Fee.py
@@ -88,9 +88,42 @@ def tabulation():
     print(buy_and_sell([7, 1, 5, 3, 6, 4], 1))
 
 
+def space_optimized():
+    def buy_and_sell(arr, transaction_fee):
+        """
+            Time complexity is O(n) and space complexity is O(1).
+        """
+        n = len(arr)
+        nxt = {j: 0 for j in [True, False]}
+        for i in range(n - 1, -1, -1):
+            curr = {j: 0 for j in [True, False]}
+            for j in [True, False]:
+                if j:
+                    curr[j] = max(
+                        -arr[i] + nxt[not j],
+                        nxt[j]
+                    )
+                else:
+                    curr[j] = max(
+                        arr[i] - transaction_fee + nxt[not j],
+                        nxt[j]
+                    )
+            nxt = curr
+        return nxt[True]
+
+    print(buy_and_sell([1, 3, 2, 8, 4, 9], 2))
+    print(buy_and_sell([1, 2, 3], 1))
+    print(buy_and_sell([1, 3, 5, 6], 2))
+    print(buy_and_sell([1, 3, 7, 5, 10, 3], 3))
+    print(buy_and_sell([6, 1, 7, 2, 8, 4], 2))
+    print(buy_and_sell([7, 1, 5, 3, 6, 4], 1))
+
+
 recursive()
 print()
 memoized()
 print()
 tabulation()
+print()
+space_optimized()
 print()
