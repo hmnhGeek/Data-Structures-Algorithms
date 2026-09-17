4f7a8b5fd3689a3d71b45f5b2e1d5f910c685b77
hmnhGeek <hmnhsharma97@gmail.com>
2026-09-17 11:57:02 +0530

    Get zero sum sub-arrays

--- Java/SearchingAndSorting/Problem15/Solution.java
+++ Java/SearchingAndSorting/Problem15/Solution.java
@@ -1,3 +1,7 @@
+// Problem link - https://www.geeksforgeeks.org/problems/zero-sum-subarrays1825/1
+// Solution - https://www.youtube.com/watch?v=xvNwoz-ufXA
+
+
 package SearchingAndSorting.Problem15;
 
 import java.util.Arrays;
@@ -7,6 +11,9 @@ import java.util.Map;
 
 public class Solution {
     private static Integer countSumSubArrays(List<Integer> arr, int k) {
+        /*
+            Time complexity is O(n) and space complexity is O(n).
+         */
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
         int sum = 0, count = 0;
@@ -14,10 +21,8 @@ public class Solution {
             sum += arr.get(i);
             if (map.containsKey(sum - k)) {
                 count += map.get(sum - k);
-                map.put(sum - k, map.get(sum - k) + 1);
-            } else {
-                map.put(sum - k, 1);
             }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         return count;
     }
@@ -27,8 +32,16 @@ public class Solution {
     }
 
     public static void main(String[] args) {
+        System.out.println("Zero sum sub-arrays");
         System.out.println(getZeroSumSubArrays(Arrays.asList(0, 0, 5, 5, 0, 0)));
         System.out.println(getZeroSumSubArrays(Arrays.asList(6, -1, -3, 4, -2, 2, 4, 6, -12, -7)));
         System.out.println(getZeroSumSubArrays(Arrays.asList(0)));
+        System.out.println(getZeroSumSubArrays(Arrays.asList(1, 4, -5)));
+        System.out.println(getZeroSumSubArrays(Arrays.asList(-1, 1, 0, 1)));
+        System.out.println(getZeroSumSubArrays(Arrays.asList(-1, 0, 1, -1)));
+        System.out.println(getZeroSumSubArrays(Arrays.asList(-2, 0, 2)));
+        System.out.println();
+        System.out.println("General Solution");
+        System.out.println(countSumSubArrays(Arrays.asList(1, 2, 3, -3, 1, 1, 1, 4, 2, -3), 3));
     }
 }
