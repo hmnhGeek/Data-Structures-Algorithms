74dab13b01b2a17dd3d7bd98b04525b185e3e838
hmnhGeek <hmnhsharma97@gmail.com>
2026-09-18 12:38:50 +0530

    Sum of nodes on the longest path

--- Java/BinaryTrees/Problem26/Solution.java
+++ Java/BinaryTrees/Problem26/Solution.java
@@ -43,24 +43,83 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        // Root
-        Node<Integer> root = new Node<>(4);
 
-        // Level 1
-        root.left = new Node<>(2);
-        root.right = new Node<>(5);
+        // =========================
+        // Tree 1
+        // =========================
+        //
+        //          4
+        //        /   \
+        //       2     5
+        //      / \   / \
+        //     7   1 2   3
+        //        /
+        //       6
 
-        // Level 2
-        root.left.left = new Node<>(7);
-        root.left.right = new Node<>(1);
+        Node<Integer> root1 = new Node<>(4);
 
-        root.right.left = new Node<>(2);
-        root.right.right = new Node<>(3);
+        root1.left = new Node<>(2);
+        root1.right = new Node<>(5);
 
-        // Level 3
-        root.left.right.left = new Node<>(6);
+        root1.left.left = new Node<>(7);
+        root1.left.right = new Node<>(1);
 
-        // Find maximum path sum
-        System.out.println(getMaxPathSum(root));
+        root1.right.left = new Node<>(2);
+        root1.right.right = new Node<>(3);
+
+        root1.left.right.left = new Node<>(6);
+
+        System.out.println("Tree 1 Max Path Sum: " + getMaxPathSum(root1));
+
+
+        // =========================
+        // Tree 2
+        // =========================
+        //
+        //          1
+        //        /   \
+        //       2     3
+        //      / \   / \
+        //     4   5 6   7
+
+        Node<Integer> root2 = new Node<>(1);
+
+        root2.left = new Node<>(2);
+        root2.right = new Node<>(3);
+
+        root2.left.left = new Node<>(4);
+        root2.left.right = new Node<>(5);
+
+        root2.right.left = new Node<>(6);
+        root2.right.right = new Node<>(7);
+
+        System.out.println("Tree 2 Max Path Sum: " + getMaxPathSum(root2));
+
+
+        // =========================
+        // Tree 3
+        // =========================
+        //
+        //          10
+        //        /    \
+        //       5      15
+        //      / \       \
+        //     3   7       20
+        //    /
+        //   1
+
+        Node<Integer> root3 = new Node<>(10);
+
+        root3.left = new Node<>(5);
+        root3.right = new Node<>(15);
+
+        root3.left.left = new Node<>(3);
+        root3.left.right = new Node<>(7);
+
+        root3.left.left.left = new Node<>(1);
+
+        root3.right.right = new Node<>(20);
+
+        System.out.println("Tree 3 Max Path Sum: " + getMaxPathSum(root3));
     }
 }
