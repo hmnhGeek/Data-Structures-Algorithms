d70beed7e9ea36d95a73f24a3ca1a94052406a01
hmnhGeek <hmnhsharma97@gmail.com>
2026-09-18 11:49:49 +0530

    Implemented a linked list class

---
+++ Java/LinkedLists/Problem31/LinkedList.java
@@ -0,0 +1,57 @@
+package LinkedLists.Problem31;
+
+
+class Node<T> {
+    public T data;
+    public Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+public class LinkedList<T> {
+    public Node<T> head, tail;
+    public Integer length;
+
+    public LinkedList() {
+        this.head = this.tail = null;
+        this.length = 0;
+    }
+
+    public boolean isEmpty() {
+        return this.length.equals(0);
+    }
+
+    public void push(T x) {
+        Node<T> node = new Node<>(x);
+        if (isEmpty()) {
+            this.head = this.tail = node;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.length += 1;
+    }
+
+    public void build(T...args) {
+        for (T x : args) {
+            push(x);
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (isEmpty()) return "[]";
+        StringBuilder sb = new StringBuilder("[");
+        Node<T> curr = this.head;
+        while (curr != this.tail) {
+            sb.append(String.format("%s, ", curr.data));
+            curr = curr.next;
+        }
+        sb.append(String.format("%s]", this.tail.data));
+        return sb.toString();
+    }
+}
