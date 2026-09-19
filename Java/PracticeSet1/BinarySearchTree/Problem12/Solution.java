3ad2601056c524dc035c66565edddb895a2df07e
hmnhGeek <hmnhsharma97@gmail.com>
2026-09-19 12:09:25 +0530

    Kth Largest in BST

--- Java/PracticeSet1/BinarySearchTree/Problem12/Solution.java
+++ Java/PracticeSet1/BinarySearchTree/Problem12/Solution.java
@@ -1,3 +1,5 @@
+// Problem link - https://www.geeksforgeeks.org/problems/kth-largest-element-in-bst/1
+
 package PracticeSet1.BinarySearchTree.Problem12;
 
 import java.util.Arrays;
@@ -7,6 +9,7 @@ public class Solution {
     public static void main(String[] args) {
         test(Arrays.asList(4, 2, 9), 2);
         test(Arrays.asList(10, 2, 11, 1, 5, 3, 6, 4), 7);
+        test(Arrays.asList(2, 4, 8, 9, 3, 6, 9, 5), 5);
     }
 
     private static void test(List<Integer> elements, Integer k) {
@@ -18,10 +21,15 @@ public class Solution {
     }
 
     public static Integer getKthLargest(BinarySearchTree<Integer> bst, Integer k) {
+        /*
+            Overall time complexity is O(n) and space complexity is O(log(n)).
+         */
         if (k <= 0) return null;
+        // This will take O(n) time and O(log(n)) space.
         int n = bst.getNumNodes();
         int position = n - k + 1;
         List<Integer> result = Arrays.asList(null, 0);
+        // This will take O(n) time and O(log(n)) space.
         findKthLargest(bst.root, result, position);
         return result.getFirst();
     }
