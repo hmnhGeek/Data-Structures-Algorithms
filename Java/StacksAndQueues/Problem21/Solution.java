36ed61a46984062a0c04843046475f95d67fc2cb
hmnhGeek <hmnhsharma97@gmail.com>
2026-09-19 12:48:03 +0530

    Implemented a queue class

---
+++ Java/StacksAndQueues/Problem21/Solution.java
@@ -0,0 +1,22 @@
+package StacksAndQueues.Problem21;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4);
+        Stack<Integer> stack = new Stack<>();
+        Queue<Integer> queue = new Queue<>();
+        for (Integer i : list) {
+            stack.push(i);
+            queue.enqueue(i);
+        }
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+        while (!queue.isEmpty()) {
+            System.out.println(queue.dequeue());
+        }
+    }
+}
