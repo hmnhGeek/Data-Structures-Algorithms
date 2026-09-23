b01da20df8e22bc3cd7345e23c8bc9b8bf2f1051
hmnhGeek <hmnhsharma97@gmail.com>
2026-09-23 11:51:15 +0530

    Recursive LDS length

---
+++ Java/DynamicProgramming/DP44/RecursiveSolution.java
@@ -0,0 +1,36 @@
+package DynamicProgramming.DP44;
+
+import java.util.List;
+
+public class RecursiveSolution {
+    public static Integer getLDSLength(List<Integer> arr) {
+        /*
+            Time complexity is exponential and space complexity is O(n).
+         */
+        int n = arr.size();
+        return solve(arr, n - 1, null);
+    }
+
+    private static Integer solve(List<Integer> arr, int i, Integer prev) {
+        if (i == 0) {
+            if (prev == null) return 1;
+            if (arr.get(i) % prev == 0 || prev % arr.get(i) == 0) {
+                return 1;
+            }
+            return 0;
+        }
+
+        if (prev == null) {
+            return Math.max(
+                    1 + solve(arr, i - 1, arr.get(i)),
+                    solve(arr, i - 1, null)
+            );
+        }
+        Integer left = Integer.MIN_VALUE;
+        if (arr.get(i) % prev == 0 || prev % arr.get(i) == 0) {
+            left = 1 + solve(arr, i - 1, arr.get(i));
+        }
+        Integer right = solve(arr, i - 1, prev);
+        return Math.max(left, right);
+    }
+}
--- Java/DynamicProgramming/DP44/Solution.java
+++ Java/DynamicProgramming/DP44/Solution.java
@@ -3,7 +3,20 @@ package DynamicProgramming.DP44;
 import java.util.*;
 
 public class Solution {
+    private static void recursive() {
+        System.out.println(RecursiveSolution.getLDSLength(Arrays.asList(1, 16, 7, 8, 4)));
+        System.out.println(RecursiveSolution.getLDSLength(Arrays.asList(1, 2, 5)));
+        System.out.println(RecursiveSolution.getLDSLength(Arrays.asList(3, 3, 3)));
+        System.out.println(RecursiveSolution.getLDSLength(Arrays.asList(1, 2, 4, 8)));
+        System.out.println(RecursiveSolution.getLDSLength(Arrays.asList(1, 2, 3)));
+        System.out.println(RecursiveSolution.getLDSLength(Arrays.asList(2, 4, 3, 8)));
+        System.out.println();
+    }
+
     public static List<Integer> getLDS(List<Integer> arr) {
+        /*
+            Time complexity is O(n^2) and space complexity is O(n).
+         */
         int n = arr.size();
         Map<Integer, Integer> size = new HashMap<>();
         Map<Integer, Integer> parent = new HashMap<>();
@@ -46,6 +59,7 @@ public class Solution {
     }
 
     public static void main(String[] args) {
+        recursive();
         System.out.println(Solution.getLDS(Arrays.asList(1, 16, 7, 8, 4)));
         System.out.println(Solution.getLDS(Arrays.asList(1, 2, 5)));
         System.out.println(Solution.getLDS(Arrays.asList(3, 3, 3)));
