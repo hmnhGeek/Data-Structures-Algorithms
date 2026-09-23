59e831e89be3ea37cc7733370c9a3480d56706fc
hmnhGeek <hmnhsharma97@gmail.com>
2026-09-23 11:55:32 +0530

    Recursive LDS length

--- Java/DynamicProgramming/DP44/RecursiveSolution.java
+++ Java/DynamicProgramming/DP44/RecursiveSolution.java
@@ -8,29 +8,29 @@ public class RecursiveSolution {
             Time complexity is exponential and space complexity is O(n).
          */
         int n = arr.size();
-        return solve(arr, n - 1, null);
+        return solve(arr, n - 1, n, n);
     }
 
-    private static Integer solve(List<Integer> arr, int i, Integer prev) {
+    private static Integer solve(List<Integer> arr, int i, Integer j, int n) {
         if (i == 0) {
-            if (prev == null) return 1;
-            if (arr.get(i) % prev == 0 || prev % arr.get(i) == 0) {
+            if (j == n) return 1;
+            if (arr.get(i) % arr.get(j) == 0 || arr.get(j) % arr.get(i) == 0) {
                 return 1;
             }
             return 0;
         }
 
-        if (prev == null) {
+        if (j == n) {
             return Math.max(
-                    1 + solve(arr, i - 1, arr.get(i)),
-                    solve(arr, i - 1, null)
+                    1 + solve(arr, i - 1, i, n),
+                    solve(arr, i - 1, j, n)
             );
         }
         Integer left = Integer.MIN_VALUE;
-        if (arr.get(i) % prev == 0 || prev % arr.get(i) == 0) {
-            left = 1 + solve(arr, i - 1, arr.get(i));
+        if (arr.get(i) % arr.get(j) == 0 || arr.get(j) % arr.get(i) == 0) {
+            left = 1 + solve(arr, i - 1, i, n);
         }
-        Integer right = solve(arr, i - 1, prev);
+        Integer right = solve(arr, i - 1, j, n);
         return Math.max(left, right);
     }
 }
