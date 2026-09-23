c9654c3593031c913b95a348f3ad83019581e0fe
hmnhGeek <hmnhsharma97@gmail.com>
2026-09-23 12:00:05 +0530

    Memoized LDS length

--- Java/DynamicProgramming/DP44/MemoizedSolution.java
+++ Java/DynamicProgramming/DP44/MemoizedSolution.java
@@ -1,38 +1,52 @@
-//package DynamicProgramming.DP44;
-//
-//import java.util.List;
-//import java.util.Map;
-//
-//public class MemoizedSolution {
-//    public static Integer getLDSLength(List<Integer> arr) {
-//        /*
-//            Time complexity is exponential and space complexity is O(n).
-//         */
-//        int n = arr.size();
-//        Map
-//        return solve(arr, n - 1, null);
-//    }
-//
-//    private static Integer solve(List<Integer> arr, int i, Integer prev) {
-//        if (i == 0) {
-//            if (prev == null) return 1;
-//            if (arr.get(i) % prev == 0 || prev % arr.get(i) == 0) {
-//                return 1;
-//            }
-//            return 0;
-//        }
-//
-//        if (prev == null) {
-//            return Math.max(
-//                    1 + solve(arr, i - 1, arr.get(i)),
-//                    solve(arr, i - 1, null)
-//            );
-//        }
-//        Integer left = Integer.MIN_VALUE;
-//        if (arr.get(i) % prev == 0 || prev % arr.get(i) == 0) {
-//            left = 1 + solve(arr, i - 1, arr.get(i));
-//        }
-//        Integer right = solve(arr, i - 1, prev);
-//        return Math.max(left, right);
-//    }
-//}
+package DynamicProgramming.DP44;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemoizedSolution {
+    public static Integer getLDSLength(List<Integer> arr) {
+        /*
+            Time complexity is O(n^2) and space complexity is O(n + n^2).
+         */
+        int n = arr.size();
+        Map<Integer, Map<Integer, Integer>> dp = new HashMap<>();
+        for (int i = 0; i < n; i += 1) {
+            Map<Integer, Integer> prev = new HashMap<>();
+            for (int j = 0; j < n + 1; j += 1) {
+                prev.put(j, null);
+            }
+            dp.put(i, prev);
+        }
+        return solve(arr, n - 1, n, n, dp);
+    }
+
+    private static Integer solve(List<Integer> arr, int i, Integer j, int n, Map<Integer, Map<Integer, Integer>> dp) {
+        if (i == 0) {
+            if (j == n) return 1;
+            if (arr.get(i) % arr.get(j) == 0 || arr.get(j) % arr.get(i) == 0) {
+                return 1;
+            }
+            return 0;
+        }
+
+        if (dp.get(i).get(j) != null) {
+            return dp.get(i).get(j);
+        };
+
+        if (j == n) {
+            dp.get(i).put(j, Math.max(
+                    1 + solve(arr, i - 1, i, n, dp),
+                    solve(arr, i - 1, j, n, dp)
+            ));
+        } else {
+            Integer left = Integer.MIN_VALUE;
+            if (arr.get(i) % arr.get(j) == 0 || arr.get(j) % arr.get(i) == 0) {
+                left = 1 + solve(arr, i - 1, i, n, dp);
+            }
+            Integer right = solve(arr, i - 1, j, n, dp);
+            dp.get(i).put(j, Math.max(left, right));
+        }
+        return dp.get(i).get(j);
+    }
+}
--- Java/DynamicProgramming/DP44/Solution.java
+++ Java/DynamicProgramming/DP44/Solution.java
@@ -13,6 +13,16 @@ public class Solution {
         System.out.println();
     }
 
+    private static void memoized() {
+        System.out.println(MemoizedSolution.getLDSLength(Arrays.asList(1, 16, 7, 8, 4)));
+        System.out.println(MemoizedSolution.getLDSLength(Arrays.asList(1, 2, 5)));
+        System.out.println(MemoizedSolution.getLDSLength(Arrays.asList(3, 3, 3)));
+        System.out.println(MemoizedSolution.getLDSLength(Arrays.asList(1, 2, 4, 8)));
+        System.out.println(MemoizedSolution.getLDSLength(Arrays.asList(1, 2, 3)));
+        System.out.println(MemoizedSolution.getLDSLength(Arrays.asList(2, 4, 3, 8)));
+        System.out.println();
+    }
+
     public static List<Integer> getLDS(List<Integer> arr) {
         /*
             Time complexity is O(n^2) and space complexity is O(n).
@@ -60,6 +70,7 @@ public class Solution {
 
     public static void main(String[] args) {
         recursive();
+        memoized();
         System.out.println(Solution.getLDS(Arrays.asList(1, 16, 7, 8, 4)));
         System.out.println(Solution.getLDS(Arrays.asList(1, 2, 5)));
         System.out.println(Solution.getLDS(Arrays.asList(3, 3, 3)));
