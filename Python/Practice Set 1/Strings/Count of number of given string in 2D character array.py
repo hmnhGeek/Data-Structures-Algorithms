651fa2ab641fa3eba3ec52ab503f415ace344ef9
hmnhGeek <hmnhsharma97@gmail.com>
2026-09-26 11:17:07 +0530

    Count of number of given string in 2D character array

--- Python/Practice Set 1/Strings/Count of number of given string in 2D character array.py
+++ Python/Practice Set 1/Strings/Count of number of given string in 2D character array.py
@@ -3,6 +3,8 @@ class Solution:
     def count_of(mtx, word):
         n, m = len(mtx), len(mtx[0])
         count = [0]
+
+        # initiate searches from each cell.
         for i in range(n):
             for j in range(m):
                 Solution._solve(mtx, word, count, 0, i, j, n, m)
@@ -10,19 +12,31 @@ class Solution:
 
     @staticmethod
     def _solve(mtx, word, count, idx, i, j, n, m):
+        # if we have exhausted all the characters of the word, we found it, return 1.
         if idx == len(word):
             return 1
+
+        # if (i, j) does not exist, return 0
         if not (0 <= i < n and 0 <= j < m):
             return 0
+
+        # if w_idx = mtx(i, j), lets perform recursive calls
         if word[idx] == mtx[i][j]:
+            # mark the current cell as '0' to avoid infinite loops
             original = mtx[i][j]
             mtx[i][j] = '0'
+
+            # recursively check for neighbouring cells.
             up = Solution._solve(mtx, word, count, idx + 1, i - 1, j, n, m)
             right = Solution._solve(mtx, word, count, idx + 1, i, j + 1, n, m)
             down = Solution._solve(mtx, word, count, idx + 1, i + 1, j, n, m)
             left = Solution._solve(mtx, word, count, idx + 1, i, j - 1, n, m)
+
+            # if word is found in any of the paths, increment the count
             if up == 1 or right == 1 or down == 1 or left == 1:
                 count[0] += 1
+
+            # backtrack
             mtx[i][j] = original
 
 
